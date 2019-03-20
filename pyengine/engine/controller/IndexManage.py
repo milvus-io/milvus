25d18944f80a91470949fc3d25c4390a18e65263
jinhai <hai.jin@zilliz.com>
2019-03-20 20:55:52 +0800

    Refactor the code into VectorEngine

--- pyengine/engine/controller/IndexManage.py
+++ pyengine/engine/controller/IndexManage.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request
 from flask_restful import Resource, Api
 from engine import app, db
 from engine.model.GroupTable import GroupTable
+from engine.controller.VectorEngine import VectorEngine
 
 # app = Flask(__name__)
 api = Api(app)
@@ -14,12 +15,10 @@ class Vector(Resource):
         self.__parser = reqparse.RequestParser()
         self.__parser.add_argument('vector', type=float, action='append', location=['json'])
 
-    def post(self, groupid):
+    def post(self, group_id):
         args = self.__parser.parse_args()
         vector = args['vector']
-        # add vector into file
-        print("vector: ", vector)
-        return "vector post"
+        return VectorEngine.AddVector(group_id, vector)
 
 
 class VectorSearch(Resource):
@@ -27,7 +26,7 @@ class VectorSearch(Resource):
         self.__parser = reqparse.RequestParser()
         self.__parser.add_argument('vector', type=float, action='append', location=['json'])
 
-    def post(self, groupid):
+    def post(self, group_id):
         args = self.__parser.parse_args()
         print('vector: ', args['vector'])
         # go to search every thing
@@ -37,69 +36,37 @@ class VectorSearch(Resource):
 class Index(Resource):
     def __init__(self):
         self.__parser = reqparse.RequestParser()
-        self.__parser.add_argument('groupid', type=str)
+        # self.__parser.add_argument('group_id', type=str)
 
-    def post(self):
-        # go to create index for specific group
-        return "index post"
+    def post(self, group_id):
+        return VectorEngine.CreateIndex(group_id)
 
 
 class Group(Resource):
     def __init__(self):
         self.__parser = reqparse.RequestParser()
-        self.__parser.add_argument('groupid', type=str)
+        self.__parser.add_argument('group_id', type=str)
 
-    def post(self, groupid):
-        args = self.__parser.parse_args()
-        group = GroupTable.query.filter(GroupTable.group_name==groupid).first()
-        if(group):
-            return jsonify({'code': 1, 'group_name': groupid, 'file_number': group.file_number})
-        else:
-            new_group = GroupTable(groupid)
-            db.session.add(new_group)
-            db.session.commit()
-            return jsonify({'code': 0, 'group_name': groupid, 'file_number': 0})
+    def post(self, group_id):
+        return VectorEngine.AddGroup(group_id)
 
+    def get(self, group_id):
+        return VectorEngine.GetGroup(group_id)
 
-    def get(self, groupid):
-        args = self.__parser.parse_args()
-        group = GroupTable.query.filter(GroupTable.group_name==groupid).first()
-        if(group):
-            return jsonify({'code': 0, 'group_name': groupid, 'file_number': group.file_number})
-        else:
-            return jsonify({'code': 1, 'group_name': groupid, 'file_number': 0}) # not found
-
-    def delete(self, groupid):
-        args = self.__parser.parse_args()
-        group = GroupTable.query.filter(GroupTable.group_name==groupid).first()
-        if(group):
-            # old_group = GroupTable(groupid)
-            db.session.delete(group)
-            db.session.commit()
-            return jsonify({'code': 0, 'group_name': groupid, 'file_number': group.file_number})
-        else:
-            return jsonify({'code': 0, 'group_name': groupid, 'file_number': 0})
+    def delete(self, group_id):
+        return VectorEngine.DeleteGroup(group_id)
 
 
 class GroupList(Resource):
     def get(self):
-        group = GroupTable.query.all()
-        group_list = []
-        for group_tuple in group:
-            group_item = {}
-            group_item['group_name'] = group_tuple.group_name
-            group_item['file_number'] = group_tuple.file_number
-            group_list.append(group_item)
-
-        print(group_list)
-        return jsonify(results = group_list)
+        return VectorEngine.GetGroupList()
 
 
-api.add_resource(Vector, '/vector/add/<groupid>')
-api.add_resource(Group, '/vector/group/<groupid>')
+api.add_resource(Vector, '/vector/add/<group_id>')
+api.add_resource(Group, '/vector/group/<group_id>')
 api.add_resource(GroupList, '/vector/group')
-api.add_resource(Index, '/vector/index')
-api.add_resource(VectorSearch, '/vector/search/<groupid>')
+api.add_resource(Index, '/vector/index/<group_id>')
+api.add_resource(VectorSearch, '/vector/search/<group_id>')
 
 
 # if __name__ == '__main__':
--- pyengine/engine/model/FileTable.py
+++ pyengine/engine/model/FileTable.py
@@ -5,14 +5,17 @@ class FileTable(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     group_name = db.Column(db.String(100))
     filename = db.Column(db.String(100))
-    type = (db.Integer)
+    type = db.Column(db.String(100))
     row_number = db.Column(db.Integer)
+    seq_no = db.Column(db.Integer)
 
-    def __init__(self, group_name, filename, type):
+    def __init__(self, group_name, filename, type, row_number):
         self.group_name = group_name
         self.filename = filename
         self.type = type
-        self.row_number = 0
+        self.row_number = row_number
+        self.type = type
+        self.seq_no = 0
 
     def __repr__(self):
         return '<FileTable $r>' % self.tablename
