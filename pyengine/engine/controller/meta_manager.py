e5f782eda6b90b4c4da4a87003df54ec168f38a4
jinhai <hai.jin@zilliz.com>
2019-04-09 11:34:37 +0800

    Update for group handle

--- pyengine/engine/controller/meta_manager.py
+++ pyengine/engine/controller/meta_manager.py
@@ -1,24 +1,23 @@
 from engine.model.group_table import GroupTable
 from engine.model.file_table import FileTable
 from engine.controller.error_code import ErrorCode
+from engine import db
 
 class MetaManager(object):
 
-    def Sync(self):
+    @staticmethod
+    def Sync():
         db.session.commit()
 
-    def AddGroup(self, group_name, dimension):
-        group = GroupTable.query.filter(GroupTable.group_name==group_id).first()
-        if group:
-            return ErrorCode.ALREADY_EXIST, group_name
-        else:
-            new_group = GroupTable(group_name, dimension)
-            GroupHandler.CreateGroupDirectory(group_id)
+    @staticmethod
+    def AddGroup(group_name, dimension):
+        new_group = GroupTable(group_name, dimension)
 
-            # add into database
-            db.session.add(new_group)
-            self.Sync()
-            return ErrorCode.SUCCESS_CODE, group_name
+        # add into database
+        db.session.add(new_group)
+        db.session.commit()
+
+        return ErrorCode.SUCCESS_CODE, group_name
 
     @staticmethod
     def GetGroup(group_name):
