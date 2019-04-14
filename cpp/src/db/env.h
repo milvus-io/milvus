0352db4023c1cbf540042215d8f2731d40ee0cc6
Xu Peng <xupeng3112@163.com>
2019-04-14 18:24:21 +0800

    feat(db/env): add threading related schedule
    Former-commit-id: d685ead572b229a60aad1342ec821d0b37d58c70

--- cpp/src/db/env.h
+++ cpp/src/db/env.h
@@ -1,23 +1,49 @@
 #ifndef STORAGE_VECENGINE_ENV_H_
 #define STORAGE_VECENGINE_ENV_H_
 
-namespace vecengine {
+#include <condition_variable>
+#include <thread>
+#include <mutex>
+#include <queue>
 
-/* struct Options { */
-/*     std::string _db_location; */
-/*     size_t _mem_sync_interval; */
-/*     size_t _file_merge_trigger_number; */
-/*     size_t _index_file_build_trigger_size; */
-/* }; // Config */
+namespace zilliz {
+namespace vecwise {
+namespace engine {
 
 class Env {
 public:
-    Env() = default;
+    Env();
 
-private:
-    Options _option;
+    Env(const Env&) = delete;
+    Env& operator=(const Env&) = delete;
+
+    void schedule(void (*function_)(void* arg_), void* arg_);
+
+    virtual ~Env();
+
+protected:
+    void backgroud_thread_main();
+    static void BackgroundThreadEntryPoint(Env* env) {
+        env->backgroud_thread_main();
+    }
+
+    struct BGWork {
+      explicit BGWork(void (*function_)(void*), void* arg_)
+          : _function(function_), _arg(arg_)  {}
+
+      void (* const _function)(void*);
+      void* const _arg;
+    };
+
+    std::mutex _bg_work_mutex;
+    std::condition_variable _bg_work_cv;
+    std::queue<BGWork> _bg_work_queue;
+    bool _bg_work_started;
+
 }; // Env
 
-} //namespace vecengine
+} // namespace engine
+} // namespace vecwise
+} // namespace zilliz
 
 #endif // STORAGE_VECENGINE_ENV_H_
