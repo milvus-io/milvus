d4dae8127bffc2a3e01996cd2f17649ed06cae99
Xu Peng <xupeng3112@163.com>
2019-04-15 14:07:05 +0800

    fix(db): fix meta compile error
    Former-commit-id: dde5e3262cc177db2fad98d82ee129718936fa01

--- cpp/src/db/db_meta_impl.cpp
+++ cpp/src/db/db_meta_impl.cpp
@@ -4,63 +4,63 @@ namespace zilliz {
 namespace vecwise {
 namespace engine {
 
-Status DBMetaImpl::DBMetaImpl(DBMetaOptions options_)
+DBMetaImpl::DBMetaImpl(const DBMetaOptions& options_)
     : _options(options_) {
     initialize();
 }
 
 Status DBMetaImpl::initialize() {
     // PXU TODO: Create DB Connection
-    return Status.OK();
+    return Status::OK();
 }
 
 Status DBMetaImpl::add_group(const GroupOptions& options_,
             const std::string& group_id_,
             GroupSchema& group_info_) {
     //PXU TODO
-    return Status.OK();
+    return Status::OK();
 }
 
 Status DBMetaImpl::get_group(const std::string& group_id_, GroupSchema& group_info_) {
     //PXU TODO
-    return Status.OK();
+    return Status::OK();
 }
 
 Status DBMetaImpl::has_group(const std::string& group_id_, bool& has_or_not_) {
     //PXU TODO
-    return Status.OK();
+    return Status::OK();
 }
 
 Status DBMetaImpl::add_group_file(const std::string& group_id_,
                               GroupFileSchema& group_file_info_) {
     //PXU TODO
-    return Status.OK();
+    return Status::OK();
 }
 
 Status DBMetaImpl::has_group_file(const std::string& group_id_,
                               const std::string& file_id_,
                               bool& has_or_not_) {
     //PXU TODO
-    return Status.OK();
+    return Status::OK();
 }
 
 Status DBMetaImpl::get_group_file(const std::string& group_id_,
                               const std::string& file_id_,
                               GroupFileSchema& group_file_info_) {
     //PXU TODO
-    return Status.OK();
+    return Status::OK();
 }
 
 Status DBMetaImpl::get_group_files(const std::string& group_id_,
                                const int date_delta_,
                                GroupFilesSchema& group_files_info_) {
     // PXU TODO
-    return Status.OK();
+    return Status::OK();
 }
 
 Status DBMetaImpl::update_group_file(const GroupFileSchema& group_file_) {
     //PXU TODO
-    return Status.OK();
+    return Status::OK();
 }
 
 } // namespace engine
