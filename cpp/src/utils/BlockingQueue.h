95058b836a940ee0fc35fc7476edcb1fe6b9a72c
groot <yihua.mo@zilliz.com>
2019-04-15 15:58:28 +0800

    add block queue class

---
+++ cpp/src/utils/BlockingQueue.h
@@ -0,0 +1,54 @@
+/*******************************************************************************
+ * Copyright 上海赜睿信息科技有限公司(Zilliz) - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited.
+ * Proprietary and confidential.
+ ******************************************************************************/
+#pragma once
+
+#include <assert.h>
+#include <condition_variable>
+#include <iostream>
+#include <queue>
+#include <vector>
+
+namespace zilliz {
+namespace vecwise {
+namespace server {
+
+template<typename T>
+class BlockingQueue {
+public:
+    BlockingQueue() : mtx(), full_(), empty_() {}
+
+    BlockingQueue(const BlockingQueue &rhs) = delete;
+
+    BlockingQueue &operator=(const BlockingQueue &rhs) = delete;
+
+    void Put(const T &task);
+
+    T Take();
+
+    T Front();
+
+    T Back();
+
+    size_t Size();
+
+    bool Empty();
+
+    void SetCapacity(const size_t capacity);
+
+private:
+    mutable std::mutex mtx;
+    std::condition_variable full_;
+    std::condition_variable empty_;
+    std::queue<T> queue_;
+    size_t capacity_ = 32;
+};
+
+}
+}
+}
+
+
+#include "./BlockingQueue.inl"
