de1bbd03266d06bafa76cbcdaa570336c0ce5499
groot <yihua.mo@zilliz.com>
2019-04-16 15:40:29 +0800

    build debug thrift

--- cpp/third_party/thrift-0.12.0/build.sh
+++ cpp/third_party/thrift-0.12.0/build.sh
@@ -6,7 +6,7 @@ cd -
 
 
 mkdir -p INSTALL_PREFIX
-./configure --prefix=${INSTALL_PREFIX} --without-java --without-python
+./configure --prefix=${INSTALL_PREFIX} CXXFLAGS='-g -O0' CFLAGS='-g -O0' --without-java --without-python
  
 make -j
 make install
