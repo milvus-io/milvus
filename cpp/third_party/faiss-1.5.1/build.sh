dd88c5fc57d29068266a32f6104524d81081d485
groot <yihua.mo@zilliz.com>
2019-04-17 12:15:12 +0800

    build faiss thrift optional

--- cpp/third_party/faiss-1.5.1/build.sh
+++ cpp/third_party/faiss-1.5.1/build.sh
@@ -7,8 +7,31 @@ cd -
 CUDA_PATH="--with-cuda=/usr/local/cuda"
 CUDA_CONFIG=--with-cuda-arch="-gencode=arch=compute_61,code=sm_61"
 
+BUILD_TYPE=Release
+
+while getopts "p:t:uh" arg
+do
+        case $arg in
+             t)
+                BUILD_TYPE=$OPTARG # BUILD_TYPE
+                ;;
+             ?)
+                echo "unkonw argument"
+        exit 1
+        ;;
+        esac
+done
+
+CXXFLAGS='-O3'
+CFLAGS='-O3'
+if [ "$BUILD_TYPE" == "Debug" ];then
+CXXFLAGS='-g -O0'
+CFLAGS='-g -O0'
+fi
+
 mkdir -p INSTALL_PREFIX
-./configure --prefix=${INSTALL_PREFIX} LDFLAGS=-L${INSTALL_PREFIX}/lib ${CUDA_PATH} ${CUDA_CONFIG}
+./configure --prefix=${INSTALL_PREFIX} CXXFLAGS="$CXXFLAGS" CFLAGS="$CFLAGS" LDFLAGS=-L${INSTALL_PREFIX}/lib ${CUDA_PATH} ${CUDA_CONFIG}
  
+make clean
 make -j
 make install
--- cpp/third_party/thrift-0.12.0/build.sh
+++ cpp/third_party/thrift-0.12.0/build.sh
@@ -4,9 +4,30 @@ cd ..
 INSTALL_PREFIX=$(pwd)/build
 cd -
 
+BUILD_TYPE=Release
+
+while getopts "p:t:uh" arg
+do
+        case $arg in
+             t)
+                BUILD_TYPE=$OPTARG # BUILD_TYPE
+                ;;
+             ?)
+                echo "unkonw argument"
+        exit 1
+        ;;
+        esac
+done
+
+CXXFLAGS='-O3'
+CFLAGS='-O3'
+if [ "$BUILD_TYPE" == "Debug" ];then
+CXXFLAGS='-g -O0'
+CFLAGS='-g -O0'
+fi
 
 mkdir -p INSTALL_PREFIX
-./configure --prefix=${INSTALL_PREFIX} CXXFLAGS='-g -O0' CFLAGS='-g -O0' --without-java --without-python
- 
+./configure --prefix=${INSTALL_PREFIX} CXXFLAGS="$CXXFLAGS" CFLAGS="$CFLAGS" --without-java --without-python
+
 make -j
 make install
