dbadb94091753df465d39571066156cc4e2dca30
groot <yihua.mo@zilliz.com>
2019-04-22 14:07:29 +0800

    add id mapper class
    Former-commit-id: f0c14a9905825c0970f19b2bd53da610954c07c0

---
+++ cpp/src/server/VecIdMapper.h
@@ -0,0 +1,22 @@
+/*******************************************************************************
+ * Copyright 上海赜睿信息科技有限公司(Zilliz) - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited.
+ * Proprietary and confidential.
+ ******************************************************************************/
+#pragma once
+
+namespace zilliz {
+namespace vecwise {
+namespace server {
+
+class VecIdMapper {
+public:
+    VecIdMapper();
+    ~VecIdMapper();
+
+
+};
+
+}
+}
+}
--- cpp/src/server/VecServiceHandler.h
+++ cpp/src/server/VecServiceHandler.h
@@ -4,11 +4,6 @@
  * Proprietary and confidential.
  ******************************************************************************/
 #pragma once
-/*******************************************************************************
- * Copyright 上海赜睿信息科技有限公司(Zilliz) - All Rights Reserved
- * Unauthorized copying of this file, via any medium is strictly prohibited.
- * Proprietary and confidential.
- ******************************************************************************/
 #include "utils/Error.h"
 #include "thrift/gen-cpp/VecService.h"
 
