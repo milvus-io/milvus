4b18d9fec15c707dfd1e062fc0c492f53f29bc03
Xu Peng <xupeng3112@163.com>
2019-04-29 15:47:09 +0800

    test(db): add more meta test
    Former-commit-id: b8078e972d1c08b38cd6ff272ec2cd6ab1d895b2

--- cpp/unittest/db/meta_tests.cpp
+++ cpp/unittest/db/meta_tests.cpp
@@ -13,22 +13,22 @@
 
 using namespace zilliz::vecwise::engine;
 
-TEST_F(DBTest, META_TEST) {
-    auto impl = DBMetaImplFactory::Build();
+TEST_F(MetaTest, META_TEST) {
+    /* auto impl = DBMetaImplFactory::Build(); */
 
     auto group_id = "meta_test_group";
 
     meta::GroupSchema group;
     group.group_id = group_id;
-    auto status = impl->add_group(group);
+    auto status = impl_->add_group(group);
     ASSERT_TRUE(status.ok());
 
-    status = impl->get_group(group);
+    status = impl_->get_group(group);
     ASSERT_TRUE(status.ok());
 
     group.group_id = "not_found";
-    status = impl->get_group(group);
+    status = impl_->get_group(group);
     ASSERT_TRUE(!status.ok());
 
-    impl->drop_all();
+    /* impl->drop_all(); */
 }
