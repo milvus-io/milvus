115cf6170631e44810a1b612c87222b862b5c874
Xu Peng <xupeng3112@163.com>
2019-04-29 15:56:07 +0800

    test(db): impl more group test

--- cpp/unittest/db/meta_tests.cpp
+++ cpp/unittest/db/meta_tests.cpp
@@ -21,8 +21,12 @@ TEST_F(MetaTest, GROUP_TEST) {
     auto status = impl_->add_group(group);
     ASSERT_TRUE(status.ok());
 
+    auto gid = group.id;
+    group.id = -1;
     status = impl_->get_group(group);
     ASSERT_TRUE(status.ok());
+    ASSERT_EQ(group.id, gid);
+    ASSERT_EQ(group.group_id, group_id);
 
     group.group_id = "not_found";
     status = impl_->get_group(group);
