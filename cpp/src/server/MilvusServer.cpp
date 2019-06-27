0bab0b5ef44a67251431955d2ac94da282cf7b9e
groot <yihua.mo@zilliz.com>
2019-06-27 19:57:11 +0800

    add more error handling
    Former-commit-id: 21d14f9541d81e2a30a59c4501835d5f549c5360

--- cpp/src/server/MilvusServer.cpp
+++ cpp/src/server/MilvusServer.cpp
@@ -24,6 +24,7 @@
 #include <thrift/concurrency/PosixThreadFactory.h>
 
 #include <thread>
+#include <iostream>
 
 namespace zilliz {
 namespace milvus {
@@ -93,7 +94,8 @@ MilvusServer::StartService() {
             return;
         }
     } catch (apache::thrift::TException& ex) {
-        SERVER_LOG_ERROR << "Server encounter exception: " << ex.what();
+        std::cout << "ERROR! " << ex.what() << std::endl;
+        kill(0, SIGUSR1);
     }
 }
 
