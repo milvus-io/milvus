270b1f078cb85b203edeb0e0cbeb70d93e47b841
groot <yihua.mo@zilliz.com>
2019-06-28 18:25:57 +0800

    MS-129 Add more unitest

--- cpp/unittest/server/config_test.cpp
+++ cpp/unittest/server/config_test.cpp
@@ -10,12 +10,26 @@
 
 using namespace zilliz::milvus;
 
-static const std::string CONFIG_FILE_PATH = "../../../conf/server_config.yaml";
+namespace {
+
+static const std::string CONFIG_FILE_PATH = "./milvus/conf/server_config.yaml";
+static const std::string LOG_FILE_PATH = "./milvus/conf/log_config.conf";
+
+}
 
 TEST(ConfigTest, CONFIG_TEST) {
     server::IConfigMgr* config_mgr = server::IConfigMgr::GetInstance();
-    server::ServerError err = config_mgr->LoadConfigFile(CONFIG_FILE_PATH);
+
+    server::ServerError err = config_mgr->LoadConfigFile("");
+    ASSERT_EQ(err, server::SERVER_UNEXPECTED_ERROR);
+
+    err = config_mgr->LoadConfigFile(LOG_FILE_PATH);
+    ASSERT_EQ(err, server::SERVER_UNEXPECTED_ERROR);
+
+    err = config_mgr->LoadConfigFile(CONFIG_FILE_PATH);
     ASSERT_EQ(err, server::SERVER_SUCCESS);
+
+    config_mgr->Print();
 
     server::ConfigNode& root_config = config_mgr->GetRootNode();
     server::ConfigNode& server_config = root_config.GetChild("server_config");
@@ -43,10 +57,6 @@ TEST(ConfigTest, CONFIG_TEST) {
     auto children = server_config.GetChildren();
     ASSERT_TRUE(children.empty());
 
-    root_config.PrintAll();
-    std::string all = root_config.DumpString();
-    ASSERT_TRUE(!all.empty());
-
     server_config.ClearConfig();
     auto configs = server_config.GetConfig();
     ASSERT_TRUE(configs.empty());
@@ -56,9 +66,20 @@ TEST(ConfigTest, CONFIG_TEST) {
     auto seq = server_config.GetSequence("seq");
     ASSERT_EQ(seq.size(), 2UL);
 
+    server::ConfigNode combine;
+    combine.Combine(server_config);
+
+    combine.PrintAll();
+    std::string all = combine.DumpString();
+    ASSERT_TRUE(!all.empty());
+
     server_config.ClearSequences();
     auto seqs = server_config.GetSequences();
     ASSERT_TRUE(seqs.empty());
+
+    const server::ConfigNode const_node = root_config.GetChild("cache_config");
+    float flt = const_node.GetFloatValue("cpu_cache_capacity");
+    ASSERT_GT(flt, 0.0);
 }
 
 TEST(ConfigTest, SERVER_CONFIG_TEST) {
@@ -73,5 +94,12 @@ TEST(ConfigTest, SERVER_CONFIG_TEST) {
     int32_t cap = node1.GetInt32Value("cpu_cache_capacity");
     ASSERT_GT(cap, 0);
 
+    node1.SetValue("bool", "true");
+    bool bt = node1.GetBoolValue("bool");
+    ASSERT_TRUE(bt);
+
     config.PrintAll();
+
+    const server::ServerConfig const_config = config;
+    server::ConfigNode node = const_config.GetConfig("aaa");
 }
