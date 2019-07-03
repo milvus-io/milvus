781a30be6f2d2f68f638703e726701e53cb0a03c
zhiru <zhiru.zhu@zilliz.com>
2019-07-03 17:24:36 +0800

    update
    Former-commit-id: 9b2e7a5f4a338252a7398c86563194b704332a06

--- cpp/src/server/RequestTask.cpp
+++ cpp/src/server/RequestTask.cpp
@@ -502,11 +502,6 @@ ServerError SearchVectorTask::OnExecute() {
         engine::QueryResults results;
         uint64_t record_count = (uint64_t)record_array_.size();
 
-        SERVER_LOG_DEBUG << "file_id_array_: ";
-        for (auto& file_id : file_id_array_) {
-            SERVER_LOG_DEBUG << file_id;
-        }
-
         if(file_id_array_.empty()) {
             stat = DBWrapper::DB()->Query(table_name_, (size_t) top_k_, record_count, vec_f.data(), dates, results);
         } else {
