434ba458cc8584c5dc13f0c88309d19f8d807947
zhiru <zhiru.zhu@zilliz.com>
2019-07-04 14:04:57 +0800

    update
    Former-commit-id: 12267838390a1c152994efe234d79bc74e299f75

--- cpp/src/db/MySQLConnectionPool.h
+++ cpp/src/db/MySQLConnectionPool.h
@@ -5,6 +5,11 @@
 
 #include "Log.h"
 
+namespace zilliz {
+namespace milvus {
+namespace engine {
+namespace meta {
+
 class MySQLConnectionPool : public mysqlpp::ConnectionPool {
 
 public:
@@ -20,8 +25,7 @@ public:
                         password_(passWord),
                         server_(serverIp),
                         port_(port),
-                        max_pool_size_(maxPoolSize)
-                        {
+                        max_pool_size_(maxPoolSize) {
 
         conns_in_use_ = 0;
 
@@ -34,65 +38,25 @@ public:
         clear();
     }
 
-    // Do a simple form of in-use connection limiting: wait to return
-    // a connection until there are a reasonably low number in use
-    // already.  Can't do this in create() because we're interested in
-    // connections actually in use, not those created.  Also note that
-    // we keep our own count; ConnectionPool::size() isn't the same!
-    mysqlpp::Connection* grab() override {
-        while (conns_in_use_ > max_pool_size_) {
-            sleep(1);
-        }
-
-        ++conns_in_use_;
-        return mysqlpp::ConnectionPool::grab();
-    }
+    mysqlpp::Connection *grab() override;
 
     // Other half of in-use conn count limit
-    void release(const mysqlpp::Connection* pc) override {
-        mysqlpp::ConnectionPool::release(pc);
-//        ENGINE_LOG_DEBUG << "conns_in_use_ in release: " << conns_in_use_ << std::endl;
-        if (conns_in_use_ <= 0) {
-            ENGINE_LOG_WARNING << "MySQLConnetionPool::release: conns_in_use_ is less than zero.  conns_in_use_ = " << conns_in_use_ << std::endl;
-        }
-        else {
-            --conns_in_use_;
-        }
-    }
+    void release(const mysqlpp::Connection *pc) override;
 
-    void set_max_idle_time(int max_idle) {
-        max_idle_time_ = max_idle;
-    }
+    int getConnectionsInUse();
 
-    std::string getDB() {
-        return db_;
-    }
+    void set_max_idle_time(int max_idle);
+
+    std::string getDB();
 
 protected:
 
     // Superclass overrides
-    mysqlpp::Connection* create() override {
-        // Create connection using the parameters we were passed upon
-        // creation.
-        mysqlpp::Connection* conn = new mysqlpp::Connection();
-        conn->set_option(new mysqlpp::ReconnectOption(true));
-        conn->connect(db_.empty() ? 0 : db_.c_str(),
-                      server_.empty() ? 0 : server_.c_str(),
-                      user_.empty() ? 0 : user_.c_str(),
-                      password_.empty() ? 0 : password_.c_str(),
-                      port_);
-        return conn;
-    }
+    mysqlpp::Connection *create() override;
 
-    void destroy(mysqlpp::Connection* cp) override {
-        // Our superclass can't know how we created the Connection, so
-        // it delegates destruction to us, to be safe.
-        delete cp;
-    }
+    void destroy(mysqlpp::Connection *cp) override;
 
-    unsigned int max_idle_time() override {
-        return max_idle_time_;
-    }
+    unsigned int max_idle_time() override;
 
 private:
     // Number of connections currently in use
@@ -106,3 +70,8 @@ private:
 
     unsigned int max_idle_time_;
 };
+
+} // namespace meta
+} // namespace engine
+} // namespace milvus
+} // namespace zilliz
