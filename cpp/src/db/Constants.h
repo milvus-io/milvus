8b3705af26854ac93c3478f2cf0fc6e5527e4b21
zhiru <zhiru.zhu@zilliz.com>
2019-07-05 15:03:40 +0800

    add mem impl

---
+++ cpp/src/db/Constants.h
@@ -0,0 +1,20 @@
+/*******************************************************************************
+ * Copyright 上海赜睿信息科技有限公司(Zilliz) - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited.
+ * Proprietary and confidential.
+ ******************************************************************************/
+#pragma once
+
+namespace zilliz {
+namespace milvus {
+namespace engine {
+
+const size_t K = 1024UL;
+const size_t M = K*K;
+const size_t MAX_TABLE_FILE_MEM = 128 * M;
+
+const int VECTOR_TYPE_SIZE = sizeof(float);
+
+} // namespace engine
+} // namespace milvus
+} // namespace zilliz
