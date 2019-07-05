5f294485b0f88376d62878f6695f8e100d9a3311
quicksilver <zhifeng.zhang@zilliz.com>
2019-07-05 18:07:40 +0800

    MS-161 - Add CI / CD Module to Milvus Project
    Former-commit-id: 9159f7ea40721f0816c9d526e9cd3fcdc0721328

---
+++ ci/jenkinsfile/cleanup_dev.groovy
@@ -0,0 +1,12 @@
+try {
+    sh "helm del --purge ${env.JOB_NAME}-${env.BUILD_NUMBER}"
+
+    if (currentBuild.result == 'ABORTED') {
+        throw new hudson.AbortException("Dev Test Aborted !")
+    } else if (currentBuild.result == 'FAILURE') {
+        error("Dev Test Failure !")
+    }
+} catch (exc) {
+    updateGitlabCommitStatus name: 'Cleanup Dev', state: 'failed'
+    throw exc
+}
