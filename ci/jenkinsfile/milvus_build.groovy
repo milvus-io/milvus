b7044d83a665c6c1461b9838b22ec4482e6fc51b
quicksilver <zhifeng.zhang@zilliz.com>
2019-07-05 18:07:40 +0800

    MS-161 - Add CI / CD Module to Milvus Project

---
+++ ci/jenkinsfile/milvus_build.groovy
@@ -0,0 +1,17 @@
+container('milvus-build-env') {
+    timeout(time: 20, unit: 'MINUTES') {
+        gitlabCommitStatus(name: 'Build Engine') {
+            dir ("milvus_engine") {
+                try {
+                    checkout([$class: 'GitSCM', branches: [[name: "${SEMVER}"]], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'SubmoduleOption',disableSubmodules: false,parentCredentials: true,recursiveSubmodules: true,reference: '',trackingSubmodules: false]], submoduleCfg: [], userRemoteConfigs: [[credentialsId: "${params.GIT_USER}", url: "git@192.168.1.105:megasearch/milvus.git"]]])
+                    dir ("cpp") {
+                        sh "./build.sh -t ${params.BUILD_TYPE} -u -c"
+                    }
+                } catch (exc) {
+                    updateGitlabCommitStatus name: 'Build Engine', state: 'failed'
+                    throw exc
+                }
+            }
+        }
+    }
+}
