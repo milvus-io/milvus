ad4e841664ef55a13c012f12f0933090c5ebc3b2
zhiru <zhiru.zhu@zilliz.com>
2019-07-07 13:50:39 +0800

    add mem manager
    Former-commit-id: 88ceef178607ea2e49eec64c533959a077c3e4a2

--- cpp/unittest/db/mem_test.cpp
+++ cpp/unittest/db/mem_test.cpp
@@ -7,6 +7,11 @@
 #include "db/Factories.h"
 #include "db/Constants.h"
 #include "db/EngineFactory.h"
+#include "metrics/Metrics.h"
+
+#include <thread>
+#include <fstream>
+#include <iostream>
 
 using namespace zilliz::milvus;
 
@@ -29,6 +34,9 @@ namespace {
         vectors.clear();
         vectors.resize(n*TABLE_DIM);
         float* data = vectors.data();
+//        std::random_device rd;
+//        std::mt19937 gen(rd());
+//        std::uniform_real_distribution<> dis(0.0, 1.0);
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < TABLE_DIM; j++) data[TABLE_DIM * i + j] = drand48();
             data[TABLE_DIM * i] += i / 2000.;
@@ -169,7 +177,7 @@ TEST(MEM_TEST, MEM_TABLE_TEST) {
     memTable.GetCurrentMemTableFile(memTableFile);
     ASSERT_EQ(memTableFile->GetCurrentMem(), n_100 * singleVectorMem);
 
-    ASSERT_EQ(memTable.GetStackSize(), 2);
+    ASSERT_EQ(memTable.GetTableFileCount(), 2);
 
     int64_t n_1G = 1024000;
     std::vector<float> vectors_1G;
@@ -183,8 +191,8 @@ TEST(MEM_TEST, MEM_TABLE_TEST) {
     vector_ids = source_1G->GetVectorIds();
     ASSERT_EQ(vector_ids.size(), n_1G);
 
-    int expectedStackSize = 2 + std::ceil((n_1G - n_100) * singleVectorMem / engine::MAX_TABLE_FILE_MEM);
-    ASSERT_EQ(memTable.GetStackSize(), expectedStackSize);
+    int expectedTableFileCount = 2 + std::ceil((n_1G - n_100) * singleVectorMem / engine::MAX_TABLE_FILE_MEM);
+    ASSERT_EQ(memTable.GetTableFileCount(), expectedTableFileCount);
 
     status = memTable.Serialize();
     ASSERT_TRUE(status.ok());
@@ -193,4 +201,127 @@ TEST(MEM_TEST, MEM_TABLE_TEST) {
     ASSERT_TRUE(status.ok());
 }
 
+TEST(MEM_TEST, MEM_MANAGER_TEST) {
+
+    auto options = engine::OptionsFactory::Build();
+    options.meta.path = "/tmp/milvus_test";
+    options.meta.backend_uri = "sqlite://:@:/";
+    auto db_ = engine::DBFactory::Build(options);
+
+    engine::meta::TableSchema table_info = BuildTableSchema();
+    engine::Status stat = db_->CreateTable(table_info);
+
+    engine::meta::TableSchema table_info_get;
+    table_info_get.table_id_ = TABLE_NAME;
+    stat = db_->DescribeTable(table_info_get);
+    ASSERT_STATS(stat);
+    ASSERT_EQ(table_info_get.dimension_, TABLE_DIM);
+
+    std::map<int64_t , std::vector<float>> search_vectors;
+//    std::map<int64_t , std::vector<float>> vectors_ids_map;
+    {
+        engine::IDNumbers vector_ids;
+        int64_t nb = 1024000;
+        std::vector<float> xb;
+        BuildVectors(nb, xb);
+        engine::Status status = db_->InsertVectors(TABLE_NAME, nb, xb.data(), vector_ids);
+        ASSERT_TRUE(status.ok());
+
+//        std::ofstream myfile("mem_test.txt");
+//        for (int64_t i = 0; i < nb; ++i) {
+//            int64_t vector_id = vector_ids[i];
+//            std::vector<float> vectors;
+//            for (int64_t j = 0; j < TABLE_DIM; j++) {
+//                vectors.emplace_back(xb[i*TABLE_DIM + j]);
+////                std::cout << xb[i*TABLE_DIM + j] << std::endl;
+//            }
+//            vectors_ids_map[vector_id] = vectors;
+//        }
+
+        std::this_thread::sleep_for(std::chrono::seconds(3));
+
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_int_distribution<int64_t> dis(0, nb - 1);
+
+        int64_t numQuery = 1000;
+        for (int64_t i = 0; i < numQuery; ++i) {
+            int64_t index = dis(gen);
+            std::vector<float> search;
+            for (int64_t j = 0; j < TABLE_DIM; j++) {
+                search.push_back(xb[index * TABLE_DIM + j]);
+            }
+            search_vectors.insert(std::make_pair(vector_ids[index], search));
+//            std::cout << "index: " << index << " vector_ids[index]: " << vector_ids[index] << std::endl;
+        }
+
+//        for (int64_t i = 0; i < nb; i += 100000) {
+//            std::vector<float> search;
+//            for (int64_t j = 0; j < TABLE_DIM; j++) {
+//                search.push_back(xb[i * TABLE_DIM + j]);
+//            }
+//            search_vectors.insert(std::make_pair(vector_ids[i], search));
+//        }
+
+    }
+
+    int k = 10;
+    for(auto& pair : search_vectors) {
+        auto& search = pair.second;
+        engine::QueryResults results;
+        stat = db_->Query(TABLE_NAME, k, 1, search.data(), results);
+        for(int t = 0; t < k; t++) {
+//            std::cout << "ID=" << results[0][t].first << " DISTANCE=" << results[0][t].second << std::endl;
+
+//            std::cout << vectors_ids_map[results[0][t].first].size() << std::endl;
+//            for (auto& data : vectors_ids_map[results[0][t].first]) {
+//                std::cout << data << " ";
+//            }
+//            std::cout << std::endl;
+        }
+    //        std::cout << "results[0][0].first: " << results[0][0].first << " pair.first: " << pair.first << " results[0][0].second: " << results[0][0].second << std::endl;
+        ASSERT_EQ(results[0][0].first, pair.first);
+        ASSERT_LT(results[0][0].second, 0.00001);
+    }
+
+    stat = db_->DropAll();
+    ASSERT_TRUE(stat.ok());
+
+}
+
+TEST(MEM_TEST, INSERT_TEST) {
+
+    auto options = engine::OptionsFactory::Build();
+    options.meta.path = "/tmp/milvus_test";
+    options.meta.backend_uri = "sqlite://:@:/";
+    auto db_ = engine::DBFactory::Build(options);
+
+    engine::meta::TableSchema table_info = BuildTableSchema();
+    engine::Status stat = db_->CreateTable(table_info);
+
+    engine::meta::TableSchema table_info_get;
+    table_info_get.table_id_ = TABLE_NAME;
+    stat = db_->DescribeTable(table_info_get);
+    ASSERT_STATS(stat);
+    ASSERT_EQ(table_info_get.dimension_, TABLE_DIM);
+
+    auto start_time = METRICS_NOW_TIME;
+
+    int insert_loop = 1000;
+    for (int i = 0; i < insert_loop; ++i) {
+        int64_t nb = 204800;
+        std::vector<float> xb;
+        BuildVectors(nb, xb);
+        engine::IDNumbers vector_ids;
+        engine::Status status = db_->InsertVectors(TABLE_NAME, nb, xb.data(), vector_ids);
+        ASSERT_TRUE(status.ok());
+    }
+    auto end_time = METRICS_NOW_TIME;
+    auto total_time = METRICS_MICROSECONDS(start_time, end_time);
+    std::cout << "total_time(ms) : " << total_time << std::endl;
+
+    stat = db_->DropAll();
+    ASSERT_TRUE(stat.ok());
+
+}
 
