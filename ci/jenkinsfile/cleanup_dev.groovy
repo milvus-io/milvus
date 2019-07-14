e2ab8a5cd26578cffc7fb784ee08ddd87b0da7e4
quicksilver <zhifeng.zhang@zilliz.com>
2019-07-14 21:50:48 +0800

    MS-215 - add milvus cluster CI/CD groovy file

--- ci/jenkinsfile/cleanup_dev.groovy
+++ ci/jenkinsfile/cleanup_dev.groovy
@@ -1,14 +1,13 @@
 try {
-    sh "helm del --purge ${env.JOB_NAME}-${env.BUILD_NUMBER}"
-
-    if (currentBuild.result == 'ABORTED') {
-        throw new hudson.AbortException("Dev Test Aborted !")
-    } else if (currentBuild.result == 'FAILURE') {
-        error("Dev Test Failure !")
+    def result = sh script: "helm status ${env.JOB_NAME}-${env.BUILD_NUMBER}", returnStatus: true
+    if (!result) {
+        sh "helm del --purge ${env.JOB_NAME}-${env.BUILD_NUMBER}"
     }
 } catch (exc) {
-    sh "helm del --purge ${env.JOB_NAME}-${env.BUILD_NUMBER}"
-    updateGitlabCommitStatus name: 'Cleanup Dev', state: 'failed'
+    def result = sh script: "helm status ${env.JOB_NAME}-${env.BUILD_NUMBER}", returnStatus: true
+    if (!result) {
+        sh "helm del --purge ${env.JOB_NAME}-${env.BUILD_NUMBER}"
+    }
     throw exc
 }
 
