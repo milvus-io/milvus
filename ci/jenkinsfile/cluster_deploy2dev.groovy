18b7d178aa15d8f981dc6a2a3b484ccf98d2ae05
quicksilver <zhifeng.zhang@zilliz.com>
2019-07-14 20:23:51 +0800

    update test framework image version to v0.2
    Former-commit-id: cd3b61cd0b25c9f40ad564e31cbb5339f99dee92

--- ci/jenkinsfile/cluster_deploy2dev.groovy
+++ ci/jenkinsfile/cluster_deploy2dev.groovy
@@ -7,15 +7,14 @@ try {
         dir ("milvus/milvus-cluster") {
             sh "helm install --set roServers.image.tag=${DOCKER_VERSION} --set woServers.image.tag=${DOCKER_VERSION} --set expose.type=clusterIP -f ci/values.yaml --name ${env.JOB_NAME}-${env.BUILD_NUMBER}-cluster --namespace milvus-cluster --version 0.3.1 . "
         }
-        timeout(time: 2, unit: 'MINUTES') {
-            waitUntil {
-                def result = sh script: "nc -z -w 2 ${env.JOB_NAME}-${env.BUILD_NUMBER}-cluster-milvus-cluster-proxy.milvus-cluster.svc.cluster.local 19530", returnStatus: true
-                return !result
-            }
+    }
+    timeout(time: 2, unit: 'MINUTES') {
+        waitUntil {
+            def result = sh script: "nc -z -w 3 ${env.JOB_NAME}-${env.BUILD_NUMBER}-cluster-milvus-cluster-proxy.milvus-cluster.svc.cluster.local 19530", returnStatus: true
+            return !result
         }
     }
 } catch (exc) {
-    updateGitlabCommitStatus name: 'Deloy to Dev', state: 'failed'
     echo 'Helm running failed!'
     sh "helm del --purge ${env.JOB_NAME}-${env.BUILD_NUMBER}-cluster"
     throw exc
