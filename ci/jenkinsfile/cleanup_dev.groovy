64b4bfa231e9fe66e527c5c5742efd451945e1c7
quicksilver <zhifeng.zhang@zilliz.com>
2019-07-14 21:08:49 +0800

    fix clean up bug
    Former-commit-id: 86f5976e67c354046d333a02336e30d5b8275ee5

--- ci/jenkinsfile/cleanup_dev.groovy
+++ ci/jenkinsfile/cleanup_dev.groovy
@@ -1,11 +1,11 @@
 try {
-	def result = sh script: "helm status ${env.JOB_NAME}-${env.BUILD_NUMBER}", returnStatus: true
-    if (result) {
+    def result = sh script: "helm status ${env.JOB_NAME}-${env.BUILD_NUMBER}", returnStatus: true
+    if (!result) {
         sh "helm del --purge ${env.JOB_NAME}-${env.BUILD_NUMBER}"
     }
 } catch (exc) {
     def result = sh script: "helm status ${env.JOB_NAME}-${env.BUILD_NUMBER}", returnStatus: true
-    if (result) {
+    if (!result) {
         sh "helm del --purge ${env.JOB_NAME}-${env.BUILD_NUMBER}"
     }
     throw exc
