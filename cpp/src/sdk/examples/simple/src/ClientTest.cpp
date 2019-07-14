c4d26aef484afb71861fd12eac0b12cf012f207c
groot <yihua.mo@zilliz.com>
2019-07-14 16:10:17 +0800

    Fix unittest error

--- cpp/src/sdk/examples/simple/src/ClientTest.cpp
+++ cpp/src/sdk/examples/simple/src/ClientTest.cpp
@@ -18,11 +18,11 @@ namespace {
 
     static const std::string TABLE_NAME = GetTableName();
     static constexpr int64_t TABLE_DIMENSION = 512;
-    static constexpr int64_t BATCH_ROW_COUNT = 10000;
+    static constexpr int64_t BATCH_ROW_COUNT = 100000;
     static constexpr int64_t NQ = 10;
     static constexpr int64_t TOP_K = 10;
     static constexpr int64_t SEARCH_TARGET = 5000; //change this value, result is different
-    static constexpr int64_t ADD_VECTOR_LOOP = 1;
+    static constexpr int64_t ADD_VECTOR_LOOP = 10;
     static constexpr int64_t SECONDS_EACH_HOUR = 3600;
 
 #define BLOCK_SPLITER std::cout << "===========================================" << std::endl;
