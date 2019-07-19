811e6937c6099c696077b94e403e36e0ec57f466
quicksilver <zhifeng.zhang@zilliz.com>
2019-07-19 11:51:43 +0800

    set helm install wait option
    Former-commit-id: 0688e30db4564a74e1fdd511387f6d4bca53fcd5

--- ci/jenkinsfile/cluster_deploy2dev.groovy
+++ ci/jenkinsfile/cluster_deploy2dev.groovy
@@ -5,15 +5,17 @@ try {
     dir ("milvus-helm") {
         checkout([$class: 'GitSCM', branches: [[name: "${SEMVER}"]], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: "${params.GIT_USER}", url: "git@192.168.1.105:megasearch/milvus-helm.git", name: 'origin', refspec: "+refs/heads/${SEMVER}:refs/remotes/origin/${SEMVER}"]]])
         dir ("milvus/milvus-cluster") {
-            sh "helm install --set roServers.image.tag=${DOCKER_VERSION} --set woServers.image.tag=${DOCKER_VERSION} --set expose.type=clusterIP -f ci/values.yaml --name ${env.JOB_NAME}-${env.BUILD_NUMBER}-cluster --namespace milvus-cluster --version 0.3.1 . "
+            sh "helm install --wait --set roServers.image.tag=${DOCKER_VERSION} --set woServers.image.tag=${DOCKER_VERSION} --set expose.type=clusterIP -f ci/values.yaml --name ${env.JOB_NAME}-${env.BUILD_NUMBER}-cluster --namespace milvus-cluster --version 0.3.1 . "
         }
     }
+    /*
     timeout(time: 2, unit: 'MINUTES') {
         waitUntil {
             def result = sh script: "nc -z -w 3 ${env.JOB_NAME}-${env.BUILD_NUMBER}-cluster-milvus-cluster-proxy.milvus-cluster.svc.cluster.local 19530", returnStatus: true
             return !result
         }
     }
+    */
 } catch (exc) {
     echo 'Helm running failed!'
     sh "helm del --purge ${env.JOB_NAME}-${env.BUILD_NUMBER}-cluster"
