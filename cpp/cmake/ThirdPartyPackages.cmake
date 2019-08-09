004546f1ba388a9a300652799d59f3759dd3fdbd
Yu Kun <kun.yu@zilliz.com>
2019-08-09 15:46:32 +0800

    grpc and thrift server run concurrently
    Former-commit-id: 6334b6a91361ff67a51e0bd42ceb69dbfea79840

--- cpp/cmake/ThirdPartyPackages.cmake
+++ cpp/cmake/ThirdPartyPackages.cmake
@@ -2640,7 +2640,7 @@ macro(build_grpc)
     add_dependencies(grpc_protoc grpc_ep)
 endmacro()
 
-if(NOT MILVUS_WITH_THRIFT STREQUAL "ON")
+#if(NOT MILVUS_WITH_THRIFT STREQUAL "ON")
     resolve_dependency(GRPC)
 
     get_target_property(GRPC_INCLUDE_DIR grpc INTERFACE_INCLUDE_DIRECTORIES)
@@ -2651,4 +2651,4 @@ if(NOT MILVUS_WITH_THRIFT STREQUAL "ON")
     include_directories(SYSTEM ${GRPC_THIRD_PARTY_DIR}/protobuf/src)
     link_directories(SYSTEM ${GRPC_PROTOBUF_LIB_DIR})
 
-endif()
+#endif()
--- cpp/src/server/grpc_impl/GrpcMilvusServer.cpp
+++ cpp/src/server/grpc_impl/GrpcMilvusServer.cpp
@@ -4,12 +4,12 @@
  * Proprietary and confidential.
  ******************************************************************************/
 #include "milvus.grpc.pb.h"
-#include "MilvusServer.h"
+#include "GrpcMilvusServer.h"
 #include "../ServerConfig.h"
 #include "../DBWrapper.h"
 #include "utils/Log.h"
 #include "faiss/utils.h"
-#include "RequestHandler.h"
+#include "GrpcRequestHandler.h"
 
 #include <chrono>
 #include <iostream>
@@ -34,7 +34,7 @@ static std::unique_ptr<grpc::Server> server;
 constexpr long MESSAGE_SIZE = -1;
 
 void
-MilvusServer::StartService() {
+GrpcMilvusServer::StartService() {
     if (server != nullptr){
         std::cout << "stopservice!\n";
         StopService();
@@ -44,7 +44,7 @@ MilvusServer::StartService() {
     ConfigNode server_config = config.GetConfig(CONFIG_SERVER);
     ConfigNode engine_config = config.GetConfig(CONFIG_ENGINE);
     std::string address = server_config.GetValue(CONFIG_SERVER_ADDRESS, "127.0.0.1");
-    int32_t port = server_config.GetInt32Value(CONFIG_SERVER_PORT, 19530);
+    int32_t port = server_config.GetInt32Value(CONFIG_SERVER_PORT, 19531);
 
     faiss::distance_compute_blas_threshold = engine_config.GetInt32Value(CONFIG_DCBT, 20);
 
@@ -60,7 +60,7 @@ MilvusServer::StartService() {
     builder.SetDefaultCompressionAlgorithm(GRPC_COMPRESS_STREAM_GZIP);
     builder.SetDefaultCompressionLevel(GRPC_COMPRESS_LEVEL_HIGH);
 
-    RequestHandler service;
+    GrpcRequestHandler service;
 
     builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
     builder.RegisterService(&service);
@@ -71,7 +71,7 @@ MilvusServer::StartService() {
 }
 
 void
-MilvusServer::StopService() {
+GrpcMilvusServer::StopService() {
     if (server != nullptr) {
         server->Shutdown();
     }
