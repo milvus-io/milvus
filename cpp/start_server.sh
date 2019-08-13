bb9b6d36fd6fba12f2a2f021131e71b6efae41aa
Yu Kun <kun.yu@zilliz.com>
2019-08-13 17:29:13 +0800

    rename milvus_grpc_server to milvus_server
    Former-commit-id: 54f295751d413a3af3765ff6334d25392ed17ac8

--- cpp/start_server.sh
+++ cpp/start_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-./cmake_build/src/milvus_grpc_server -c ./conf/server_config.yaml -l ./conf/log_config.conf &
+./cmake_build/src/milvus_server -c ./conf/server_config.yaml -l ./conf/log_config.conf &
 
