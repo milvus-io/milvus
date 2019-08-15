f8d39928a03f760ab2c3ab305bdd40b6a5353a98
Yu Kun <kun.yu@zilliz.com>
2019-08-15 10:32:36 +0800

    add nprobe and preloadtable unittest

--- cpp/src/sdk/interface/ConnectionImpl.cpp
+++ cpp/src/sdk/interface/ConnectionImpl.cpp
@@ -83,9 +83,10 @@ ConnectionImpl::Search(const std::string &table_name,
                              const std::vector<RowRecord> &query_record_array,
                              const std::vector<Range> &query_range_array,
                              int64_t topk,
+                             int64_t nprobe,
                              std::vector<TopKQueryResult> &topk_query_result_array) {
     return client_proxy_->Search(table_name, query_record_array, query_range_array, topk,
-                                       topk_query_result_array);
+                                 nprobe, topk_query_result_array);
 }
 
 Status
