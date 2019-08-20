efa735461e80e0bc1287fb4222ba519e2e794c90
Yu Kun <kun.yu@zilliz.com>
2019-08-20 15:58:46 +0800

    add vector_ids in Insert

--- cpp/src/server/grpc_impl/GrpcRequestTask.cpp
+++ cpp/src/server/grpc_impl/GrpcRequestTask.cpp
@@ -453,7 +453,10 @@ InsertTask::OnExecute() {
 
         //step 4: insert vectors
         auto vec_count = (uint64_t) insert_param_.row_record_array_size();
-        std::vector<int64_t> vec_ids(record_ids_.vector_id_array_size(), 0);
+        std::vector<int64_t> vec_ids(insert_param_.row_id_array_size(), 0);
+        for (auto i = 0; i < insert_param_.row_id_array_size(); i++) {
+            vec_ids[i] = insert_param_.row_id_array(i);
+        }
 
         stat = DBWrapper::DB()->InsertVectors(insert_param_.table_name(), vec_count, vec_f.data(),
                                               vec_ids);
