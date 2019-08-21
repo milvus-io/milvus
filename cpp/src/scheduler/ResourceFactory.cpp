8c9a278b791910d3bceba2588600ee9edf43a0a0
wxyu <xy.wang@zilliz.com>
2019-08-21 18:22:52 +0800

    MS-390 Update resource construct function

--- cpp/src/scheduler/ResourceFactory.cpp
+++ cpp/src/scheduler/ResourceFactory.cpp
@@ -12,13 +12,16 @@ namespace milvus {
 namespace engine {
 
 std::shared_ptr<Resource>
-ResourceFactory::Create(const std::string &name, const std::string &alias) {
+ResourceFactory::Create(const std::string &name,
+                        const std::string &alias,
+                        bool enable_loader,
+                        bool enable_executor) {
     if (name == "disk") {
-        return std::make_shared<DiskResource>(alias);
+        return std::make_shared<DiskResource>(alias, enable_loader, enable_executor);
     } else if (name == "cpu") {
-        return std::make_shared<CpuResource>(alias);
+        return std::make_shared<CpuResource>(alias, enable_loader, enable_executor);
     } else if (name == "gpu") {
-        return std::make_shared<GpuResource>(alias);
+        return std::make_shared<GpuResource>(alias, enable_loader, enable_executor);
     } else {
         return nullptr;
     }
