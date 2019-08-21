d30e432e16f70496dab6ab082b2a00d9eaf8ff05
starlord <chxiehust@gmail.com>
2019-08-21 18:35:57 +0800

    merge 0.3.1
    Former-commit-id: a538685c673319581c756289a4c003fa5833d829

--- cpp/src/wrapper/Index.cpp
+++ cpp/src/wrapper/Index.cpp
@@ -7,16 +7,22 @@
 #if 0
 // TODO: maybe support static search
 #ifdef GPU_VERSION
+
 #include "faiss/gpu/GpuAutoTune.h"
 #include "faiss/gpu/StandardGpuResources.h"
 #include "faiss/gpu/utils/DeviceUtils.h"
+
+
 #endif
 
 #include "Index.h"
 #include "faiss/index_io.h"
 #include "faiss/IndexIVF.h"
 #include "faiss/IVFlib.h"
+#include "faiss/IndexScalarQuantizer.h"
 #include "server/ServerConfig.h"
+#include "src/wrapper/FaissGpuResources.h"
+
 
 namespace zilliz {
 namespace milvus {
@@ -74,8 +80,27 @@ void write_index(const Index_ptr &index, const std::string &file_name) {
 
 Index_ptr read_index(const std::string &file_name) {
     std::shared_ptr<faiss::Index> raw_index = nullptr;
-    raw_index.reset(faiss::read_index(file_name.c_str()));
-    return std::make_shared<Index>(raw_index);
+    faiss::Index *cpu_index = faiss::read_index(file_name.c_str());
+
+    server::ServerConfig &config = server::ServerConfig::GetInstance();
+    server::ConfigNode engine_config = config.GetConfig(server::CONFIG_ENGINE);
+    bool use_hybrid_index_ = engine_config.GetBoolValue(server::CONFIG_USE_HYBRID_INDEX, false);
+
+    if (dynamic_cast<faiss::IndexIVFScalarQuantizer *>(cpu_index) != nullptr && use_hybrid_index_) {
+
+        int device_id = engine_config.GetInt32Value(server::CONFIG_HYBRID_INDEX_GPU, 0);
+        auto gpu_resources = engine::FaissGpuResources::GetGpuResources(device_id);
+        faiss::gpu::GpuClonerOptions clone_option;
+        clone_option.storeInCpu = true;
+        faiss::Index *gpu_index = faiss::gpu::index_cpu_to_gpu(gpu_resources.get(), device_id, cpu_index, &clone_option);
+
+        delete cpu_index;
+        raw_index.reset(gpu_index);
+        return std::make_shared<Index>(raw_index);
+    } else {
+        raw_index.reset(cpu_index);
+        return std::make_shared<Index>(raw_index);
+    }
 }
 
 }
