a55eec571d5bb013a7f3650b47e36fd34d7e157e
Yu Kun <kun.yu@zilliz.com>
2019-08-22 22:14:17 +0800

    add GpuCacheMgr and unittest
    Former-commit-id: e6192cfd62cd1114f9a495647b2ff5260f9ed976

--- cpp/src/cache/GpuCacheMgr.cpp
+++ cpp/src/cache/GpuCacheMgr.cpp
@@ -13,34 +13,38 @@ namespace milvus {
 namespace cache {
 
 std::mutex GpuCacheMgr::mutex_;
-std::unordered_map<uint64_t, GpuCacheMgr*> GpuCacheMgr::instance_;
+std::unordered_map<uint64_t, GpuCacheMgrPtr> GpuCacheMgr::instance_;
 
 namespace {
     constexpr int64_t unit = 1024 * 1024 * 1024;
+
+    void parse_gpu_ids(std::string gpu_ids_str, std::vector<uint64_t>& gpu_ids) {
+        for (auto i = 0; i < gpu_ids_str.length(); ) {
+            if (gpu_ids_str[i] != ',') {
+                int id = 0;
+                while (gpu_ids_str[i] <= '9' && gpu_ids_str[i] >= '0') {
+                    id = id * 10 + gpu_ids_str[i] - '0';
+                    ++i;
+                }
+                gpu_ids.push_back(id);
+            } else {
+                ++i;
+            }
+        }
+    }
 }
 
 GpuCacheMgr::GpuCacheMgr() {
     server::ConfigNode& config = server::ServerConfig::GetInstance().GetConfig(server::CONFIG_CACHE);
     std::string gpu_ids_str = config.GetValue(server::CONFIG_GPU_IDS, "0,1");
-    std::vector<uint64_t> gpu_ids;
-    for (auto i = 0; i < gpu_ids_str.length(); ) {
-        if (gpu_ids_str[i] != ',') {
-            int id = 0;
-            while (gpu_ids_str[i] != ',') {
-                id = id * 10 + gpu_ids_str[i] - '0';
-                ++i;
-            }
-            gpu_ids.push_back(id);
-        } else {
-            ++i;
-        }
-    }
 
-    cache_->set_gpu_ids(gpu_ids);
-
-    int64_t cap = config.GetInt64Value(server::CONFIG_GPU_CACHE_CAPACITY, 1);
+    int64_t cap = config.GetInt64Value(server::CONFIG_GPU_CACHE_CAPACITY, 2);
     cap *= unit;
     cache_ = std::make_shared<Cache>(cap, 1UL<<32);
+
+    std::vector<uint64_t> gpu_ids;
+    parse_gpu_ids(gpu_ids_str, gpu_ids);
+    cache_->set_gpu_ids(gpu_ids);
 
     double free_percent = config.GetDoubleValue(server::GPU_CACHE_FREE_PERCENT, 0.85);
     if (free_percent > 0.0 && free_percent <= 1.0) {
