7476b395cc50a936c5bba21cf72b3e92da78cb15
Yu Kun <kun.yu@zilliz.com>
2019-08-25 16:23:22 +0800

    fix metricbase_test bugs

--- cpp/unittest/metrics/metricbase_test.cpp
+++ cpp/unittest/metrics/metricbase_test.cpp
@@ -22,7 +22,7 @@ TEST(MetricbaseTest, METRICBASE_TEST){
     instance.IndexFileSizeHistogramObserve(1.0);
     instance.BuildIndexDurationSecondsHistogramObserve(1.0);
     instance.CpuCacheUsageGaugeSet(1.0);
-    instance.GpuCacheUsageGaugeSet(1.0);
+    instance.GpuCacheUsageGaugeSet();
     instance.MetaAccessTotalIncrement();
     instance.MetaAccessDurationSecondsHistogramObserve(1.0);
     instance.FaissDiskLoadDurationSecondsHistogramObserve(1.0);
