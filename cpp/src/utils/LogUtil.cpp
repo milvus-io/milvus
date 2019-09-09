916b16f1561a83771a3416eb3a19cf2d667d54bf
Heisenberg <zhuangbo.ye@zilliz.com>
2019-09-09 19:15:27 +0800

    MS-119 The problem of combining the log files
    Former-commit-id: 1fcd0203926d3570af32aecd4c5e1246a21483db

--- cpp/src/utils/LogUtil.cpp
+++ cpp/src/utils/LogUtil.cpp
@@ -9,6 +9,9 @@
 #include <easylogging++.h>
 #include <ctype.h>
 
+#include <string>
+#include <libgen.h>
+
 namespace zilliz {
 namespace milvus {
 namespace server {
@@ -50,11 +53,55 @@ int32_t InitLog(const std::string& log_config_file) {
 #else
     el::Configurations conf(log_config_file);
 #endif
-
     el::Loggers::reconfigureAllLoggers(conf);
+
+    el::Loggers::addFlag(el::LoggingFlag::StrictLogFileSizeCheck);
+    el::Helpers::installPreRollOutCallback(rolloutHandler);
     return 0;
 }
 
+// TODO(yzb) : change the easylogging library to get the log level from parameter rather than filename
+void rolloutHandler(const char* filename, std::size_t size){
+    char *dirc = strdup(filename);
+    char *basec = strdup(filename);
+    char *dir = dirname(dirc);
+    char *base = basename(basec);
+
+    std::string s(base);
+    std::stringstream ss;
+    std::string list[] = {"\\", " ", "\'", "\"", "*", "\?", "{", "}", ";", "<", ">", "|", "^", "&", "$", "#", "!", "`", "~"};
+    std::string::size_type position;
+    for(auto substr : list){
+         position = 0;
+        while((position = s.find_first_of(substr, position)) != std::string::npos){
+            s.insert(position, "\\");
+            position += 2;
+        }
+    }
+    if((position = s.find("global")) != std::string::npos){
+        ss << "mv " << dir << "/" << s << " " << dir << "/" << s << "." << ++global_idx;
+    }
+    else if((position = s.find("debug")) != std::string::npos){
+        ss << "mv " << dir << "/" << s << " " << dir << "/" << s << "." << ++debug_idx;
+    }
+    else if((position = s.find("warning")) != std::string::npos){
+        ss << "mv " << dir << "/" << s << " " << dir << "/" << s << "." << ++warning_idx;
+    }
+    else if((position = s.find("trace")) != std::string::npos){
+        ss << "mv " << dir << "/" << s << " " << dir << "/" << s << "." << ++trace_idx;
+    }
+    else if((position = s.find("error")) != std::string::npos){
+        ss << "mv " << dir << "/" << s << " " << dir << "/" << s << "." << ++error_idx;
+    }
+    else if((position = s.find("fatal")) != std::string::npos){
+        ss << "mv " << dir << "/" << s << " " << dir << "/" << s << "." << ++fatal_idx;
+    }
+    else{
+        ss << "mv " << dir << "/" << s << " " << dir << "/" << s << "." << ++global_idx;
+    }
+//    std::cout << ss.str() << std::endl;
+    system(ss.str().c_str());
+}
 
 }   // server
 }   // milvus
