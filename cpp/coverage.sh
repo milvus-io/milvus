9b7f3b32edd077011bb49637cdb3afc82c937442
Yu Kun <kun.yu@zilliz.com>
2019-09-12 11:15:02 +0800

    fix coverage bugs

--- cpp/coverage.sh
+++ cpp/coverage.sh
@@ -96,10 +96,10 @@ ${LCOV_CMD} -r "${FILE_INFO_OUTPUT}" -o "${FILE_INFO_OUTPUT_NEW}" \
     "*/cmake_build/*_ep-prefix/*" \
     "src/core/cmake_build*" \
     "src/core/thirdparty*" \
-    "src/server*" \
-#    "src/grpc*"\
-#    "src/server/Server.cpp"\
-#    "src/server/grpc_impl/GrpcMilvusServer.cpp"\
+    "src/grpc*"\
+    "src/server/Server.cpp"\
+    "src/server/DBWrapper.cpp"\
+    "src/server/grpc_impl/GrpcMilvusServer.cpp"\
 
 # gen html report
 ${LCOV_GEN_CMD} "${FILE_INFO_OUTPUT_NEW}" --output-directory ${DIR_LCOV_OUTPUT}/
