4ec11416b95652312a31d4be8e0b98e6f9a53c4d
groot <yihua.mo@zilliz.com>
2019-09-16 15:46:58 +0800

    refine cache code
    Former-commit-id: e5c847c80475a6b2046292576eb29a9b5103d3ac

--- cpp/src/cache/CpuCacheMgr.cpp
+++ cpp/src/cache/CpuCacheMgr.cpp
@@ -20,7 +20,7 @@ CpuCacheMgr::CpuCacheMgr() {
     server::ConfigNode& config = server::ServerConfig::GetInstance().GetConfig(server::CONFIG_CACHE);
     int64_t cap = config.GetInt64Value(server::CONFIG_CPU_CACHE_CAPACITY, 16);
     cap *= unit;
-    cache_ = std::make_shared<Cache>(cap, 1UL<<32);
+    cache_ = std::make_shared<Cache<DataObjPtr>>(cap, 1UL<<32);
 
     double free_percent = config.GetDoubleValue(server::CACHE_FREE_PERCENT, 0.85);
     if(free_percent > 0.0 && free_percent <= 1.0) {
@@ -29,6 +29,20 @@ CpuCacheMgr::CpuCacheMgr() {
         SERVER_LOG_ERROR << "Invalid cache_free_percent: " << free_percent <<
          ", defaultly set to " << cache_->freemem_percent();
     }
+}
+
+CpuCacheMgr* CpuCacheMgr::GetInstance() {
+    static CpuCacheMgr s_mgr;
+    return &s_mgr;
+}
+
+engine::VecIndexPtr CpuCacheMgr::GetIndex(const std::string& key) {
+    DataObjPtr obj = GetItem(key);
+    if(obj != nullptr) {
+        return obj->data();
+    }
+
+    return nullptr;
 }
 
 }
