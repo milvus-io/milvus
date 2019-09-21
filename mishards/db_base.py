ce95b50143ed4a57cacd414eeece12cb6d1fe638
peng.xu <peng.xu@zilliz.com>
2019-09-21 13:32:29 +0800

    support sqlite

--- mishards/db_base.py
+++ mishards/db_base.py
@@ -1,5 +1,6 @@
 import logging
 from sqlalchemy import create_engine
+from sqlalchemy.engine.url import make_url
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
 
@@ -12,7 +13,11 @@ class DB:
         uri and self.init_db(uri, echo)
 
     def init_db(self, uri, echo=False):
-        self.engine = create_engine(uri, pool_size=100, pool_recycle=5, pool_timeout=30,
+        url = make_url(uri)
+        if url.get_backend_name() == 'sqlite':
+            self.engine = create_engine(url)
+        else:
+            self.engine = create_engine(uri, pool_size=100, pool_recycle=5, pool_timeout=30,
                 pool_pre_ping=True,
                 echo=echo,
                 max_overflow=0)
