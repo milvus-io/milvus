04524d9c0e34e65561bb41e02fc0f207efe94ff1
xiaojun.lin <xiaojun.lin@zilliz.com>
2019-10-09 17:58:59 +0800

    hybrid enable
    Former-commit-id: 9c89d56f61445c1b1eda80b1403165aba2d3b986

---
+++ cpp/src/core/knowhere/knowhere/index/vector_index/IndexIVFSQHybrid.h
@@ -0,0 +1,79 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+
+#pragma once
+
+#include <memory>
+#include <faiss/index_io.h>
+
+#include "IndexGPUIVFSQ.h"
+#include "Quantizer.h"
+
+
+namespace zilliz {
+namespace knowhere {
+
+struct FaissIVFQuantizer : public Quantizer {
+    faiss::IndexComposition *quantizer = nullptr;
+};
+using FaissIVFQuantizerPtr = std::shared_ptr<FaissIVFQuantizer>;
+
+class IVFSQHybrid : public GPUIVFSQ {
+ public:
+    explicit IVFSQHybrid(const int &device_id) : GPUIVFSQ(device_id) {}
+
+    explicit IVFSQHybrid(std::shared_ptr<faiss::Index> index) : GPUIVFSQ(-1) {gpu_mode = false;}
+
+    explicit IVFSQHybrid(std::shared_ptr<faiss::Index> index, const int64_t &device_id, ResPtr &resource)
+        : GPUIVFSQ(index, device_id, resource) {
+        gpu_mode = true;
+    }
+
+ public:
+    QuantizerPtr
+    LoadQuantizer(const Config &conf);
+
+    void
+    SetQuantizer(QuantizerPtr q);
+
+    IndexModelPtr
+    Train(const DatasetPtr &dataset, const Config &config) override;
+
+    VectorIndexPtr
+    CopyGpuToCpu(const Config &config) override;
+
+    VectorIndexPtr
+    CopyCpuToGpu(const int64_t &device_id, const Config &config) override;
+
+ protected:
+    void
+    search_impl(int64_t n,
+                const float *data,
+                int64_t k,
+                float *distances,
+                int64_t *labels,
+                const Config &cfg) override;
+
+    void LoadImpl(const BinarySet &index_binary) override;
+
+ protected:
+    bool gpu_mode = false;
+};
+
+}
+}
