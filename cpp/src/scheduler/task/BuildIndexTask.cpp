d9579a9a9b3328a69d6b5720f538d63dc5e5887e
Yu Kun <kun.yu@zilliz.com>
2019-10-10 10:15:00 +0800

    clang tidy

--- cpp/src/scheduler/task/BuildIndexTask.cpp
+++ cpp/src/scheduler/task/BuildIndexTask.cpp
@@ -15,6 +15,7 @@
 // specific language governing permissions and limitations
 // under the License.
 
+
 #include "BuildIndexTask.h"
 #include "db/engine/EngineFactory.h"
 #include "metrics/Metrics.h"
