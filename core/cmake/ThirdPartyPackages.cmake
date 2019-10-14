5b986135c88fa1a6911019228ac14060cdca7cf8
zhiru <zhiru.zhu@zilliz.com>
2019-10-14 20:17:20 +0800

    update grpc download md5

--- core/cmake/ThirdPartyPackages.cmake
+++ core/cmake/ThirdPartyPackages.cmake
@@ -372,7 +372,7 @@ else()
     set(GRPC_SOURCE_URL
             "https://github.com/youny626/grpc-milvus/archive/${GRPC_VERSION}.zip")
 endif()
-set(GRPC_MD5 "fdd2656424c0e0e046b21354513fc70f")
+set(GRPC_MD5 "0362ba219f59432c530070b5f5c3df73")
 
 
 # ----------------------------------------------------------------------
