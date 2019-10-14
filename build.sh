4dd19f607d4ff23276864bd1b935fe415eaaa515
peng.xu <peng.xu@zilliz.com>
2019-10-14 15:35:29 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ NORMAL=`tput sgr0`
 YELLOW='\033[1;33m'
 ENDC='\033[0m'
 
+echo -e "${BOLD}MISHARDS_REGISTRY=${MISHARDS_REGISTRY}${ENDC}"
+
 function build_image() {
     dockerfile=$1
     remote_registry=$2
@@ -21,12 +23,17 @@ function build_image() {
 case "$1" in
 
 all)
+    [[ -z $MISHARDS_REGISTRY ]] && {
+        echo -e "${YELLOW}Error: Please set docker registry first:${ENDC}\n\t${BOLD}export MISHARDS_REGISTRY=xxxx${ENDC}"
+        exit 1
+    }
+
     version=""
     [[ ! -z $2 ]] && version=":${2}"
-    build_image "Dockerfile" "registry.zilliz.com/milvus/mishards${version}" "registry.zilliz.com/milvus/mishards"
+    build_image "Dockerfile" "${MISHARDS_REGISTRY}${version}" "${MISHARDS_REGISTRY}"
     ;;
 *)
     echo "Usage: [option...] {base | apps}"
-    echo "all,      Usage: build.sh all [tagname|] => registry.zilliz.com/milvus/mishards:\${tagname}"
+    echo "all,      Usage: build.sh all [tagname|] => ${MISHARDS_REGISTRY}:\${tagname}"
     ;;
 esac
