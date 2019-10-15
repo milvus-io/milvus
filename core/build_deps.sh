179ded112b974b74909510a11d0720a4cae12954
Zhiru <zhiru.zhu@zilliz.com>
2019-10-15 18:13:54 +0800

    add build_deps.sh

--- core/build_deps.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sudo apt-get install -y gfortran libmysqlclient-dev mysql-client libcurl4-openssl-dev
-
-sudo ln -s /usr/lib/x86_64-linux-gnu/libmysqlclient.so /usr/lib/x86_64-linux-gnu/libmysqlclient_r.so
