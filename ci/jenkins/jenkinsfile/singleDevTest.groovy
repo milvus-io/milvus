5a7ed935dd5ad304ce920c48c763876666ed2ea6
test <test@zilliz.com>
2019-10-16 21:37:51 +0800

    update ci/jenkins/jenkinsfile/singleDevTest.groovy
    Former-commit-id: 792fe4cfeb149438c37b06f0705e7158777dafb0

--- ci/jenkins/jenkinsfile/singleDevTest.groovy
+++ ci/jenkins/jenkinsfile/singleDevTest.groovy
@@ -4,7 +4,7 @@ timeout(time: 30, unit: 'MINUTES') {
         sh "pytest . --alluredir=\"test_out/dev/single/sqlite\" --level=1 --ip ${env.PIPELIEN_NAME}-${env.BUILD_NUMBER}-single-gpu-milvus-gpu-engine.milvus.svc.cluster.local"
     }
     // mysql database backend test
-    load "${env.WORKSPACE}/ci/jenkinsfile/cleanupSingleDev.groovy"
+    load "${env.WORKSPACE}/ci/jenkins/jenkinsfile/cleanupSingleDev.groovy"
 
     if (!fileExists('milvus-helm')) {
         dir ("milvus-helm") {
