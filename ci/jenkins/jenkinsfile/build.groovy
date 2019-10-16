7bc08900cf1902600aa8fddce1cf4c3037f8c4ed
test <test@zilliz.com>
2019-10-16 21:31:40 +0800

    update ci/jenkins/jenkinsfile/build.groovy
    Former-commit-id: f8ef19525a1596005552984c39aaa4c58ee71d9e

--- ci/jenkins/jenkinsfile/build.groovy
+++ ci/jenkins/jenkinsfile/build.groovy
@@ -2,9 +2,9 @@ timeout(time: 60, unit: 'MINUTES') {
     dir ("core") {
         sh "git config --global user.email \"test@zilliz.com\""
         sh "git config --global user.name \"test\""
-       //  sh "./build.sh -l"
+        // sh "./build.sh -l"
         withCredentials([usernamePassword(credentialsId: "${params.JFROG_CREDENTIALS_ID}", usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
-            sh "export JFROG_ARTFACTORY_URL='${params.JFROG_ARTFACTORY_URL}' && export JFROG_USER_NAME='${USERNAME}' && export JFROG_PASSWORD='${PASSWORD}' && ./build.sh -t ${params.BUILD_TYPE} -j -u"
+            sh "export JFROG_ARTFACTORY_URL='${params.JFROG_ARTFACTORY_URL}' && export JFROG_USER_NAME='${USERNAME}' && export JFROG_PASSWORD='${PASSWORD}' && ./build.sh -t ${params.BUILD_TYPE} -d /opt/milvus -j -u"
         }
     }
 }
