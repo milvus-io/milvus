6d73e3dd84ef3a883501357a8de9d0a29c3f0e67
test <test@zilliz.com>
2019-10-16 22:41:24 +0800

    update milvus-helm git url
    Former-commit-id: 4610db623210334dce85c1338e37cd5b2063bacb

--- ci/jenkins/jenkinsfile/deploySingle2Dev.groovy
+++ ci/jenkins/jenkinsfile/deploySingle2Dev.groovy
@@ -2,7 +2,7 @@ try {
     sh 'helm init --client-only --skip-refresh --stable-repo-url https://kubernetes.oss-cn-hangzhou.aliyuncs.com/charts'
     sh 'helm repo update'
     dir ('milvus-helm') {
-        checkout([$class: 'GitSCM', branches: [[name: "0.5.0"]], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: "${params.GIT_CREDENTIALS_ID}", url: "https://github.com/zilliz-ci-robot/milvus-helm.git", name: 'origin', refspec: "+refs/heads/0.5.0:refs/remotes/origin/0.5.0"]]])
+        checkout([$class: 'GitSCM', branches: [[name: "0.5.0"]], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: "${params.GIT_CREDENTIALS_ID}", url: "https://github.com/milvus-io/milvus-helm.git", name: 'origin', refspec: "+refs/heads/0.5.0:refs/remotes/origin/0.5.0"]]])
         dir ("milvus-gpu") {
             sh "helm install --wait --timeout 300 --set engine.image.tag=${DOCKER_VERSION} --set expose.type=clusterIP --name ${env.PIPELIEN_NAME}-${env.BUILD_NUMBER}-single-gpu -f ci/values.yaml --namespace milvus ."
         }
