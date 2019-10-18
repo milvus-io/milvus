ea65e4a8350950f8a9d300dbbd53de1161b2c346
milvus-ci-robot <milvus-ci-robot@zilliz.com>
2019-10-18 17:55:19 +0800

    change build.sh
    Former-commit-id: 81634f73a0343fd5c8716755f9aa5784023c98f2

--- ci/jenkins/jenkinsfile/build.groovy
+++ ci/jenkins/jenkinsfile/build.groovy
@@ -1,5 +1,5 @@
 timeout(time: 60, unit: 'MINUTES') {
-    dir ("core") {
+    dir ("ci/jenkins/scripts") {
         sh "./build.sh -l"
         withCredentials([usernamePassword(credentialsId: "${params.JFROG_CREDENTIALS_ID}", usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
             sh "export JFROG_ARTFACTORY_URL='${params.JFROG_ARTFACTORY_URL}' && export JFROG_USER_NAME='${USERNAME}' && export JFROG_PASSWORD='${PASSWORD}' && ./build.sh -t ${params.BUILD_TYPE} -o /opt/milvus -d /opt/milvus -j -u -c"
