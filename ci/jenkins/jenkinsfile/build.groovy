ea47371057936d89b27a90c4ded79b8a1fd191c5
milvus-ci-robot <milvus-ci-robot@zilliz.com>
2019-10-18 14:04:32 +0800

    Add code coverage to Jenkins CI
    Former-commit-id: ac9605ce63d3e25da9d1a473bfd2da3a81bf1250

--- ci/jenkins/jenkinsfile/build.groovy
+++ ci/jenkins/jenkinsfile/build.groovy
@@ -5,7 +5,7 @@ timeout(time: 60, unit: 'MINUTES') {
         sh "./build.sh -l"
         withCredentials([usernamePassword(credentialsId: "${params.JFROG_CREDENTIALS_ID}", usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
             sh "export JFROG_ARTFACTORY_URL='${params.JFROG_ARTFACTORY_URL}' && export JFROG_USER_NAME='${USERNAME}' && export JFROG_PASSWORD='${PASSWORD}' && ./build.sh -t ${params.BUILD_TYPE} -d /opt/milvus -j -u -c"
-            sh "./coverage.sh -u root -p 123456 -t \$POD_ID"
+            sh "./coverage.sh -u root -p 123456 -t \$POD_IP"
         }
     }
 }
