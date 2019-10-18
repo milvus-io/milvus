99b7203d7a96b7771aa241c6171607402089f357
milvus-ci-robot <milvus-ci-robot@zilliz.com>
2019-10-18 19:15:14 +0800

    provide the repository token to upload reports
    Former-commit-id: 4d9e3fbed0f4de1649805b82d03bb763022a304b

--- ci/jenkins/jenkinsfile/coverage.groovy
+++ ci/jenkins/jenkinsfile/coverage.groovy
@@ -2,7 +2,9 @@ timeout(time: 60, unit: 'MINUTES') {
     dir ("ci/jenkins/scripts") {
         sh "./coverage.sh -o /opt/milvus -u root -p 123456 -t \$POD_IP"
         // Set some env variables so codecov detection script works correctly
-        sh "bash <(curl -s https://codecov.io/bash) -f output_new.info || echo 'Codecov did not collect coverage reports'"
+        withCredentials([[$class: 'StringBinding', credentialsId: "${PIPELINE_NAME}-codecov-token", variable: 'CODECOV_TOKEN']]) {
+            sh 'curl -s https://codecov.io/bash | bash -s - -f output_new.info || echo "Codecov did not collect coverage reports"'
+        }
     }
 }
 
