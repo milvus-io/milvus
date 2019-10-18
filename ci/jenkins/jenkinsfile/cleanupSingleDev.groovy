5370fbf1587fcbe6eb5a5460d5bf3c26e91c0654
milvus-ci-robot <milvus-ci-robot@zilliz.com>
2019-10-18 11:46:48 +0800

    Pod's own IP address from inside a container in the Pod
    Former-commit-id: 58c03a81b56a3b86971fe4732599caee37078a39

--- ci/jenkins/jenkinsfile/cleanupSingleDev.groovy
+++ ci/jenkins/jenkinsfile/cleanupSingleDev.groovy
@@ -1,5 +1,4 @@
 try {
-    def jobNames = env.JOB_NAME.split('/')
     sh "helm del --purge ${env.PIPELIEN_NAME}-${env.BUILD_NUMBER}-single-gpu"
 } catch (exc) {
     def helmResult = sh script: "helm status ${env.PIPELIEN_NAME}-${env.BUILD_NUMBER}-single-gpu", returnStatus: true
