668ece48d3984bb4ad0e02cd7b0cd7c36b58b1f7
milvus-ci-robot <milvus-ci-robot@zilliz.com>
2019-10-18 19:58:20 +0800

    rename env.PIPELIEN_NAME to env.PIPELINE_NAME
    Former-commit-id: 0576ff806a6257f44380a2a980b38bcd40c29958

--- ci/jenkins/jenkinsfile/coverage.groovy
+++ ci/jenkins/jenkinsfile/coverage.groovy
@@ -2,7 +2,7 @@ timeout(time: 60, unit: 'MINUTES') {
     dir ("ci/jenkins/scripts") {
         sh "./coverage.sh -o /opt/milvus -u root -p 123456 -t \$POD_IP"
         // Set some env variables so codecov detection script works correctly
-        withCredentials([[$class: 'StringBinding', credentialsId: "${PIPELINE_NAME}-codecov-token", variable: 'CODECOV_TOKEN']]) {
+        withCredentials([[$class: 'StringBinding', credentialsId: "${env.PIPELINE_NAME}-codecov-token", variable: 'CODECOV_TOKEN']]) {
             sh 'curl -s https://codecov.io/bash | bash -s - -f output_new.info || echo "Codecov did not collect coverage reports"'
         }
     }
