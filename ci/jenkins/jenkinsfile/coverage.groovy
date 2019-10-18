6deffa2c883ce35205e83b9954899395a72d6f72
milvus-ci-robot <milvus-ci-robot@zilliz.com>
2019-10-18 19:02:03 +0800

    try to fix  coverage.groovy bug
    Former-commit-id: ff3e85bc41bc7649b3c261c2ba614da1fc128b96

--- ci/jenkins/jenkinsfile/coverage.groovy
+++ ci/jenkins/jenkinsfile/coverage.groovy
@@ -2,7 +2,7 @@ timeout(time: 60, unit: 'MINUTES') {
     dir ("ci/jenkins/scripts") {
         sh "./coverage.sh -o /opt/milvus -u root -p 123456 -t \$POD_IP"
         // Set some env variables so codecov detection script works correctly
-        sh 'bash <(curl -s https://codecov.io/bash) -f output_new.info || echo "Codecov did not collect coverage reports"'
+        sh "bash <(curl -s https://codecov.io/bash) -f output_new.info || echo 'Codecov did not collect coverage reports'"
     }
 }
 
