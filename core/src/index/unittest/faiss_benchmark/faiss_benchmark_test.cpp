4f5906b9bc6316523f2de708b2d3e6cb448aec2d
yudong.cai <yudong.cai@zilliz.com>
2019-10-24 09:44:01 +0800

    #89 update SQ8Hybrid-gpu log
    Former-commit-id: c2e70121ee65ed044c059ac3948b3412353e829e

--- core/src/index/unittest/faiss_benchmark/faiss_benchmark_test.cpp
+++ core/src/index/unittest/faiss_benchmark/faiss_benchmark_test.cpp
@@ -475,7 +475,7 @@ void test_ivfsq8h_gpu(const std::string& ann_test_name,
         long *I = new  faiss::Index::idx_t[NQ * K];
         float *D = new float[NQ * K];
 
-        printf ("\n%s | %s | nprobe=%lu\n", ann_test_name.c_str(), index_key.c_str(), nprobe);
+        printf ("\n%s | %s-gpu | nprobe=%lu\n", ann_test_name.c_str(), index_key.c_str(), nprobe);
         printf ("============================================================================================\n");
         for (size_t t_nq = 10; t_nq <= NQ; t_nq *= 10) {    // nq = {10, 100, 1000}
             for (size_t t_k = 100; t_k <= K; t_k *= 10) {   //  k = {100, 1000}
