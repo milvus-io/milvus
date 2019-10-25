74429c902d0404483cababf902d480d07159e7f9
peng.xu <peng.xu@zilliz.com>
2019-10-25 20:24:03 +0800

    implement plugin framework for tracer

---
+++ shards/tracer/factory.py
@@ -0,0 +1,48 @@
+import os
+import logging
+from functools import partial
+from pluginbase import PluginBase
+
+
+logger = logging.getLogger(__name__)
+
+here = os.path.abspath(os.path.dirname(__file__))
+get_path = partial(os.path.join, here)
+
+PLUGIN_PACKAGE_NAME = 'tracer.plugins'
+plugin_base = PluginBase(package=PLUGIN_PACKAGE_NAME,
+        searchpath=[get_path('./plugins')])
+
+class TracerFactory(object):
+    def __init__(self, searchpath=None):
+        self.plugin_package_name = PLUGIN_PACKAGE_NAME
+        self.tracer_map = {}
+        searchpath = searchpath if searchpath else []
+        searchpath = [searchpath] if isinstance(searchpath, str) else searchpath
+        self.source = plugin_base.make_plugin_source(
+                searchpath=searchpath, identifier=self.__class__.__name__)
+
+        for plugin_name in self.source.list_plugins():
+            plugin = self.source.load_plugin(plugin_name)
+            plugin.setup(self)
+
+    def on_plugin_setup(self, plugin_class):
+        name = getattr(plugin_class, 'name', plugin_class.__name__)
+        self.tracer_map[name.lower()] = plugin_class
+
+    def plugin(self, name):
+        return self.tracer_map.get(name, None)
+
+    def create(self,
+            tracer_type,
+            tracer_config,
+            span_decorator=None,
+            **kwargs):
+        if not tracer_type:
+            return Tracer()
+        plugin_class = self.plugin(tracer_type.lower())
+        if not plugin_class:
+            raise RuntimeError('Tracer Plugin \'{}\' not installed!'.format(tracer_type))
+
+        tracer = plugin_class.create(tracer_config, span_decorator=span_decorator, **kwargs)
+        return tracer
---
+++ shards/tracer/plugins/jaeger_factory.py
@@ -0,0 +1,33 @@
+import logging
+from jaeger_client import Config
+from grpc_opentracing.grpcext import intercept_server
+from grpc_opentracing import open_tracing_server_interceptor
+from tracer import Tracer
+
+logger = logging.getLogger(__name__)
+
+PLUGIN_NAME = __name__
+
+class JaegerFactory:
+    name = 'jaeger'
+    @classmethod
+    def create(cls, tracer_config, span_decorator=None, **kwargs):
+        tracing_config = tracer_config.TRACING_CONFIG
+        service_name = tracer_config.TRACING_SERVICE_NAME
+        validate = tracer_config.TRACING_VALIDATE
+        config = Config(config=tracing_config,
+                        service_name=service_name,
+                        validate=validate)
+
+        tracer = config.initialize_tracer()
+        tracer_interceptor = open_tracing_server_interceptor(
+            tracer,
+            log_payloads=tracer_config.TRACING_LOG_PAYLOAD,
+            span_decorator=span_decorator)
+
+        return Tracer(tracer, tracer_interceptor, intercept_server)
+
+
+def setup(app):
+    logger.debug('Plugin \'{}\' Installed In Package: {}'.format(PLUGIN_NAME, app.plugin_package_name))
+    app.on_plugin_setup(JaegerFactory)
