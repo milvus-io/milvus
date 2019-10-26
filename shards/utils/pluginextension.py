1d16c0ce2eec297e8f1faa6f9997fc27cfc93220
peng.xu <peng.xu@zilliz.com>
2019-10-26 14:03:51 +0800

    fix PluginBase import bug

--- shards/utils/pluginextension.py
+++ shards/utils/pluginextension.py
@@ -1,13 +1,14 @@
-import importlib
+import importlib.util
 from pluginbase import PluginBase, PluginSource
 
 
 class MiPluginSource(PluginSource):
     def load_plugin(self, name):
-        if '.' in name:
-            raise ImportError('Plugin names cannot contain dots.')
-        with self:
-            return importlib.import_module(self.base.package + '.' + name)
+        plugin = super().load_plugin(name)
+        spec = importlib.util.spec_from_file_location(self.base.package + '.' + name, plugin.__file__)
+        plugin = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(plugin)
+        return plugin
 
 
 class MiPluginBase(PluginBase):
