5f1efb051b9b44d3a5c540f7023055da5ca124ce
fishpenguin <kun.yu@zilliz.com>
2019-10-31 14:39:02 +0800

    Add only GPU and only CPU version for IVF_SQ8 and IVF_FLAT
    Former-commit-id: 14f5b095df2988d5f65ecd1cb2628fd3a3791a14

---
+++ core/src/scheduler/optimizer/OnlyGPUPass.cpp
@@ -0,0 +1,66 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+#include "scheduler/optimizer/OnlyGPUPass.h"
+#include "scheduler/SchedInst.h"
+#include "scheduler/task/SearchTask.h"
+#include "scheduler/tasklabel/SpecResLabel.h"
+#include "scheduler/Utils.h"
+#include "server/Config.h"
+
+namespace milvus {
+namespace scheduler {
+
+bool
+OnlyGPUPass::Run(const TaskPtr& task) {
+    if (task->Type() != TaskType::SearchTask)
+        return false;
+
+    auto search_task = std::static_pointer_cast<XSearchTask>(task);
+    if (search_task->file_->engine_type_ != (int) engine::EngineType::FAISS_IVFSQ8 &&
+        search_task->file_->engine_type_ != (int) engine::EngineType::FAISS_IVFFLAT) {
+        return false;
+    }
+
+    server::Config& config = server::Config::GetInstance();
+    std::vector<std::string> search_resources;
+    config.GetResourceConfigSearchResources(search_resources);
+    for (auto &resource : search_resources) {
+        if (resource == "cpu") {
+            return false;
+        }
+    }
+
+    auto gpu_id = get_gpu_pool();
+    if (!gpu_id.empty()) {
+        ResourcePtr res_ptr = ResMgrInst::GetInstance()->GetResource(ResourceType::GPU, gpu_id[specified_gpu_id_]);
+        auto label = std::make_shared<SpecResLabel>(std::weak_ptr<Resource>(res_ptr));
+        task->label() = label;
+    } else {
+        return false;
+    }
+
+    if (specified_gpu_id_ < gpu_id.size() - 1) {
+        ++specified_gpu_id_;
+    } else {
+        specified_gpu_id_ = 0;
+    }
+    return true;
+}
+
+}  // namespace scheduler
+}  // namespace milvus
