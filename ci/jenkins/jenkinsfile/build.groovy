4f0655f086820457dfbc53efc66aa3d69b1cd5f0
jielinxu <isabellexu@126.com>
2019-11-04 11:35:44 +0800

    delete ci files

--- ci/jenkins/jenkinsfile/build.groovy
+++
@@ -1,9 +0,0 @@
-timeout(time: 60, unit: 'MINUTES') {
-    dir ("ci/jenkins/scripts") {
-        sh "./build.sh -l"
-        withCredentials([usernamePassword(credentialsId: "${params.JFROG_CREDENTIALS_ID}", usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
-            sh "export JFROG_ARTFACTORY_URL='${params.JFROG_ARTFACTORY_URL}' && export JFROG_USER_NAME='${USERNAME}' && export JFROG_PASSWORD='${PASSWORD}' && ./build.sh -t ${params.BUILD_TYPE} -o /opt/milvus -d /opt/milvus -j -u -c"
-        }
-    }
-}
-
