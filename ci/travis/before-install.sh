48e869ee2edb279df6af87dc13913c30cc18b75e
quicksilver <zhifeng.zhang@zilliz.com>
2019-11-11 14:12:37 +0800

    update .travis.yml

--- ci/travis/before-install.sh
+++ ci/travis/before-install.sh
@@ -6,6 +6,7 @@ if [[ "${TRAVIS_OS_NAME}" == "linux" ]]; then
   export CCACHE_COMPRESS=1
   export CCACHE_COMPRESSLEVEL=5
   export CCACHE_COMPILERCHECK=content
+  export PATH=/usr/lib/ccache/:$PATH
   ccache --show-stats
 fi
 
