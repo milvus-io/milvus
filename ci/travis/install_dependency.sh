e29cf5a0fe158394ebfb6376ed0a6fb9b98e90f0
quicksilver <zhifeng.zhang@zilliz.com>
2019-11-11 17:00:29 +0800

    Install libarrow-dev on System

--- ci/travis/install_dependency.sh
+++ ci/travis/install_dependency.sh
@@ -9,6 +9,13 @@ sudo apt-key add /tmp/GPG-PUB-KEY-INTEL-SW-PRODUCTS-2019.PUB
 echo "deb https://apt.repos.intel.com/mkl all main" | \
   sudo tee /etc/apt/sources.list.d/intel-mkl.list
 
+sudo wget -O /usr/share/keyrings/apache-arrow-keyring.gpg https://dl.bintray.com/apache/arrow/$(lsb_release --id --short | tr 'A-Z' 'a-z')/apache-arrow-keyring.gpg
+
+sudo tee /etc/apt/sources.list.d/apache-arrow.list <<APT_LINE
+deb [arch=amd64 signed-by=/usr/share/keyrings/apache-arrow-keyring.gpg] https://dl.bintray.com/apache/arrow/$(lsb_release --id --short | tr 'A-Z' 'a-z')/ $(lsb_release --codename --short) main
+deb-src [signed-by=/usr/share/keyrings/apache-arrow-keyring.gpg] https://dl.bintray.com/apache/arrow/$(lsb_release --id --short | tr 'A-Z' 'a-z')/ $(lsb_release --codename --short) main
+APT_LINE
+
 sudo apt-get update -qq
 
 sudo apt-get install -y -q --no-install-recommends \
@@ -20,6 +27,7 @@ sudo apt-get install -y -q --no-install-recommends \
   automake \
   ccache \
   pkg-config \
+  libarrow-dev \
   libboost-filesystem-dev \
   libboost-system-dev \
   libboost-regex-dev \
