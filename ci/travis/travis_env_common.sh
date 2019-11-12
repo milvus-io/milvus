2993ba9f11e5ca5ff06e134de820ece35c7b111c
ZhifengZhang-CN <zhifeng.zhang@zilliz.com>
2019-11-12 16:50:22 +0800

    update travis travis_env_common.sh

--- ci/travis/travis_env_common.sh
+++ ci/travis/travis_env_common.sh
@@ -3,7 +3,7 @@ export MILVUS_BUILD_DIR=${TRAVIS_BUILD_DIR}/core/cmake_build
 export MILVUS_INSTALL_PREFIX=/opt/milvus
 export MILVUS_TRAVIS_COVERAGE=${MILVUS_TRAVIS_COVERAGE:=0}
 
-if ["$MILVUS_TRAVIS_COVERAGE" == "1"]; then
+if [ "${MILVUS_TRAVIS_COVERAGE}" == "1" ]; then
   export MILVUS_CPP_COVERAGE_FILE=${TRAVIS_BUILD_DIR}/output_new.info
 fi
 
