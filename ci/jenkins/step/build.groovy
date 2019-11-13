03f297fff4a81c6efc05d42a690dcf40a42b8246
ZhifengZhang-CN <zhifeng.zhang@zilliz.com>
2019-11-13 13:52:16 +0800

    format server_cpu_config.template and server_gpu_config.template

--- ci/jenkins/step/build.groovy
+++ ci/jenkins/step/build.groovy
@@ -1,7 +1,7 @@
 timeout(time: 60, unit: 'MINUTES') {
     dir ("ci/scripts") {
         withCredentials([usernamePassword(credentialsId: "${params.JFROG_CREDENTIALS_ID}", usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
-            sh "export JFROG_ARTFACTORY_URL='${params.JFROG_ARTFACTORY_URL}' && export JFROG_USER_NAME='${USERNAME}' && export JFROG_PASSWORD='${PASSWORD}' && ./build.sh -t ${params.BUILD_TYPE} -o /opt/milvus -d /opt/milvus -l -j -u -c"
+            sh "export JFROG_ARTFACTORY_URL='${params.JFROG_ARTFACTORY_URL}' && export JFROG_USER_NAME='${USERNAME}' && export JFROG_PASSWORD='${PASSWORD}' && ./build.sh -t ${params.BUILD_TYPE} -o /opt/milvus -l -j -u -c"
         }
     }
 }
