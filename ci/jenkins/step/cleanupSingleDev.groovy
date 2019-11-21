ae59ad1901a0dd98a33ba4ea72caea4e1bc7f1fe
quicksilver <zhifeng.zhang@zilliz.com>
2019-11-21 16:49:01 +0800

    format Jenkinsfile

--- ci/jenkins/step/cleanupSingleDev.groovy
+++ ci/jenkins/step/cleanupSingleDev.groovy
@@ -1,12 +1,12 @@
 try {
-    def helmResult = sh script: "helm status ${env.PIPELINE_NAME}-${env.SEMVER}-${env.BUILD_NUMBER}-single-${env.BINRARY_VERSION}", returnStatus: true
+    def helmResult = sh script: "helm status ${env.HELM_RELEASE_NAME}", returnStatus: true
     if (!helmResult) {
-        sh "helm del --purge ${env.PIPELINE_NAME}-${env.SEMVER}-${env.BUILD_NUMBER}-single-${env.BINRARY_VERSION}"
+        sh "helm del --purge ${env.HELM_RELEASE_NAME}"
     }
 } catch (exc) {
-    def helmResult = sh script: "helm status ${env.PIPELINE_NAME}-${env.SEMVER}-${env.BUILD_NUMBER}-single-${env.BINRARY_VERSION}", returnStatus: true
+    def helmResult = sh script: "helm status ${env.HELM_RELEASE_NAME}", returnStatus: true
     if (!helmResult) {
-        sh "helm del --purge ${env.PIPELINE_NAME}-${env.SEMVER}-${env.BUILD_NUMBER}-single-${env.BINRARY_VERSION}"
+        sh "helm del --purge ${env.HELM_RELEASE_NAME}"
     }
     throw exc
 }
