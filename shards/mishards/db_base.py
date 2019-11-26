c71a5b1519b8e0a9ebba396bf598c7aa49155a3c
peng.xu <peng.xu@zilliz.com>
2019-11-26 11:31:50 +0800

    [skip ci](shards): bug fix for mysql startup

--- shards/mishards/db_base.py
+++ shards/mishards/db_base.py
@@ -28,7 +28,12 @@ class DB:
         if url.get_backend_name() == 'sqlite':
             self.engine = create_engine(url)
         else:
-            self.engine = create_engine(uri, pool_size, pool_recycle, pool_timeout, pool_pre_ping, echo, max_overflow)
+            self.engine = create_engine(uri, pool_size=pool_size,
+                                        pool_recycle=pool_size,
+                                        pool_timeout=pool_timeout,
+                                        pool_pre_ping=pool_pre_ping,
+                                        echo=echo,
+                                        max_overflow=max_overflow)
         self.uri = uri
         self.url = url
 
