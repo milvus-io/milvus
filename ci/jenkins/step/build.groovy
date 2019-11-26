89555795a8f8e0c5ccfc85856bd2f9efad8626a1
quicksilver <zhifeng.zhang@zilliz.com>
2019-11-26 11:45:42 +0800

    add ccache in build stage

--- ci/jenkins/step/build.groovy
+++ ci/jenkins/step/build.groovy
@@ -1,11 +1,14 @@
 timeout(time: 60, unit: 'MINUTES') {
     dir ("ci/scripts") {
         withCredentials([usernamePassword(credentialsId: "${params.JFROG_CREDENTIALS_ID}", usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
+            sh "./check_ccache.sh -l ${params.JFROG_ARTFACTORY_URL}/ccache"
             if ("${env.BINRARY_VERSION}" == "gpu") {
-            	sh "export JFROG_ARTFACTORY_URL='${params.JFROG_ARTFACTORY_URL}' && export JFROG_USER_NAME='${USERNAME}' && export JFROG_PASSWORD='${PASSWORD}' && ./build.sh -t ${params.BUILD_TYPE} -o /opt/milvus -l -g -j -u -c"
-        	} else {
-        		sh "export JFROG_ARTFACTORY_URL='${params.JFROG_ARTFACTORY_URL}' && export JFROG_USER_NAME='${USERNAME}' && export JFROG_PASSWORD='${PASSWORD}' && ./build.sh -t ${params.BUILD_TYPE} -o /opt/milvus -l -m -j -u -c"
-        	}
+                sh "source ./before-install.sh && ./build.sh -t ${params.BUILD_TYPE} -o /opt/milvus -l -g -u -c"
+            } else {
+                sh "source ./before-install.sh && ./build.sh -t ${params.BUILD_TYPE} -o /opt/milvus -l -m -u -c"
+            }
+
+            sh "./update_ccache.sh -l ${params.JFROG_ARTFACTORY_URL}/ccache -u ${USERNAME} -p ${PASSWORD}"
         }
     }
 }
