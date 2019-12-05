e8c3b9745fa16d5548271923af60c6129d291f74
groot <yihua.mo@zilliz.com>
2019-12-05 02:50:54 -0600

    #674 server down in stability test (#708)
    * #670 Random failure of unittest db_test::SEARCH_TEST
    
    * add an important log
    
    * add an important log
    
    * add an important log
    
    * #674 server down in stability test
    
    * add more log
    
    * remove log

--- core/src/db/engine/ExecutionEngineImpl.cpp
+++ core/src/db/engine/ExecutionEngineImpl.cpp
@@ -400,10 +400,11 @@ ExecutionEngineImpl::CopyToGpu(uint64_t device_id, bool hybrid) {
 Status
 ExecutionEngineImpl::CopyToIndexFileToGpu(uint64_t device_id) {
 #ifdef MILVUS_GPU_VERSION
+    // the ToIndexData is only a placeholder, cpu-copy-to-gpu action is performed in
     gpu_num_ = device_id;
     auto to_index_data = std::make_shared<ToIndexData>(PhysicalSize());
     cache::DataObjPtr obj = std::static_pointer_cast<cache::DataObj>(to_index_data);
-    milvus::cache::GpuCacheMgr::GetInstance(device_id)->InsertItem(location_, obj);
+    milvus::cache::GpuCacheMgr::GetInstance(device_id)->InsertItem(location_ + "_placeholder", obj);
 #endif
     return Status::OK();
 }
