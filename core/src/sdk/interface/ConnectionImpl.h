60646419b0e060544b888ce13e882535aa2b6841
yudong.cai <yudong.cai@zilliz.com>
2019-12-11 17:39:22 +0800

    #665 update API interface

--- core/src/sdk/interface/ConnectionImpl.h
+++ core/src/sdk/interface/ConnectionImpl.h
@@ -107,10 +107,10 @@ class ConnectionImpl : public Connection {
     DropPartition(const PartitionParam& param) override;
 
     Status
-    GetConfig(const std::string& parent_node, const std::string& child_node, std::string& value) const override;
+    GetConfig(const std::string& node_name, std::string& value) const override;
 
     Status
-    SetConfig(const std::string& parent_node, const std::string& child_node, const std::string& value) const override;
+    SetConfig(const std::string& node_name, const std::string& value) const override;
 
  private:
     std::shared_ptr<ClientProxy> client_proxy_;
