530bc9031b0be07f77b81e4be16d92912f611347
yhz <413554850@qq.com>
2019-12-12 11:35:27 +0800

    [skip ci] add license in web module

--- core/src/server/web_impl/component/AppComponent.hpp
+++ core/src/server/web_impl/component/AppComponent.hpp
@@ -1,10 +1,19 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
 //
-//  AppComponent.hpp
-//  oatpp-web-starter
+//   http://www.apache.org/licenses/LICENSE-2.0
 //
-//  Created by Leonid on 3/2/18.
-//  Copyright © 2018 lganzzzo. All rights reserved.
-//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
 
 #pragma once
 
