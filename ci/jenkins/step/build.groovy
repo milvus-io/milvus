65990ea3e679fc9570416db16d0df4138e36a502
quicksilver <zhifeng.zhang@zilliz.com>
2019-12-16 17:09:58 +0800

    update Jenkins CI

--- ci/jenkins/step/build.groovy
+++ ci/jenkins/step/build.groovy
@@ -3,9 +3,17 @@ timeout(time: 60, unit: 'MINUTES') {
         withCredentials([usernamePassword(credentialsId: "${params.JFROG_CREDENTIALS_ID}", usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
             def checkResult = sh(script: "./check_ccache.sh -l ${params.JFROG_ARTFACTORY_URL}/ccache", returnStatus: true)
             if ("${BINRARY_VERSION}" == "gpu") {
-                sh ". ./before-install.sh && ./build.sh -t ${params.BUILD_TYPE} -o ${env.MILVUS_INSTALL_PREFIX} -l -g -u -c"
+                if ("${OS_NAME}" == "centos7") {
+                    sh ". ./before-install.sh && source scl_source enable devtoolset-7 && source scl_source enable llvm-toolset-7.0 && ./build.sh -t ${params.BUILD_TYPE} -o ${env.MILVUS_INSTALL_PREFIX} -l -g -u -c"
+                } else {
+                    sh ". ./before-install.sh && ./build.sh -t ${params.BUILD_TYPE} -o ${env.MILVUS_INSTALL_PREFIX} -l -g -u -c"
+                }
             } else {
-                sh ". ./before-install.sh && ./build.sh -t ${params.BUILD_TYPE} -o ${env.MILVUS_INSTALL_PREFIX} -l -u -c"
+                if ("${OS_NAME}" == "centos7") {
+                    sh ". ./before-install.sh && source scl_source enable devtoolset-7 && source scl_source enable llvm-toolset-7.0 && ./build.sh -t ${params.BUILD_TYPE} -o ${env.MILVUS_INSTALL_PREFIX} -l -u -c"
+                } else {
+                    sh ". ./before-install.sh && ./build.sh -t ${params.BUILD_TYPE} -o ${env.MILVUS_INSTALL_PREFIX} -l -u -c"
+                }
             }
             sh "./update_ccache.sh -l ${params.JFROG_ARTFACTORY_URL}/ccache -u ${USERNAME} -p ${PASSWORD}"
         }
