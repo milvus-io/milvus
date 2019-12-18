75df4bf2d39f3002b73214f90b9b683f2a1e396c
quicksilver <zhifeng.zhang@zilliz.com>
2019-12-18 11:23:56 +0800

    add customization option in build.sh

--- ci/jenkins/step/build.groovy
+++ ci/jenkins/step/build.groovy
@@ -3,7 +3,7 @@ timeout(time: 60, unit: 'MINUTES') {
         withCredentials([usernamePassword(credentialsId: "${params.JFROG_CREDENTIALS_ID}", usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
             def checkResult = sh(script: "./check_ccache.sh -l ${params.JFROG_ARTFACTORY_URL}/ccache", returnStatus: true)
             if ("${env.BINRARY_VERSION}" == "gpu") {
-                sh ". ./before-install.sh && ./build.sh -t ${params.BUILD_TYPE} -o ${env.MILVUS_INSTALL_PREFIX} -l -g -u -c"
+                sh ". ./before-install.sh && ./build.sh -t ${params.BUILD_TYPE} -o ${env.MILVUS_INSTALL_PREFIX} -l -g -x -u -c"
             } else {
                 sh ". ./before-install.sh && ./build.sh -t ${params.BUILD_TYPE} -o ${env.MILVUS_INSTALL_PREFIX} -l -u -c"
             }
