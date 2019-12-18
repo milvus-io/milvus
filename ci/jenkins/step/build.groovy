c90545c0f3a4219bb731746311f17fef7f796508
quicksilver <zhifeng.zhang@zilliz.com>
2019-12-18 17:19:15 +0800

    update Jenkins CI

--- ci/jenkins/step/build.groovy
+++ ci/jenkins/step/build.groovy
@@ -3,7 +3,7 @@ timeout(time: 60, unit: 'MINUTES') {
         withCredentials([usernamePassword(credentialsId: "${params.JFROG_CREDENTIALS_ID}", usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
             def checkResult = sh(script: "./check_ccache.sh -l ${params.JFROG_ARTFACTORY_URL}/ccache", returnStatus: true)
 
-            if ("${BINRARY_VERSION}" == "gpu") {
+            if ("${BINARY_VERSION}" == "gpu") {
                 sh "/bin/bash --login -c \". ./before-install.sh && ./build.sh -t ${params.BUILD_TYPE} -o ${env.MILVUS_INSTALL_PREFIX} -l -g -x -u -c\""
             } else {
                 sh "/bin/bash --login -c \". ./before-install.sh && ./build.sh -t ${params.BUILD_TYPE} -o ${env.MILVUS_INSTALL_PREFIX} -l -u -c\""
