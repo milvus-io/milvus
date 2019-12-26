b8e64255a45cdc323d3488e30e3c395512d9476c
del-zhenwu <56623710+del-zhenwu@users.noreply.github.com>
2019-12-26 14:19:52 +0800

    Update pip source (#822)
    * Update pip source
    
    * Update requirements
    
    * Remove query range case

--- ci/jenkins/step/singleDevNightlyTest.groovy
+++ ci/jenkins/step/singleDevNightlyTest.groovy
@@ -1,6 +1,6 @@
 timeout(time: 90, unit: 'MINUTES') {
     dir ("tests/milvus_python_test") {
-        sh 'python3 -m pip install -r requirements.txt'
+        sh 'python3 -m pip install -r requirements.txt -i http://pypi.douban.com/simple --trusted-host pypi.douban.com'
         sh "pytest . --alluredir=\"test_out/dev/single/sqlite\" --ip ${env.HELM_RELEASE_NAME}-engine.milvus.svc.cluster.local"
     }
     // mysql database backend test
