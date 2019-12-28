9ce1e90156e4dd2263961026a9c89a4af8c60928
Cai Yudong <yudong.cai@zilliz.com>
2019-12-28 14:24:19 +0800

    #848 add ready-to-use config files to the Milvus repo for enhanced user experience (#858)

--- core/src/server/Config.h
+++ core/src/server/Config.h
@@ -81,7 +81,7 @@ static const char* CONFIG_METRIC_PROMETHEUS_PORT_DEFAULT = "8080";
 /* engine config */
 static const char* CONFIG_ENGINE = "engine_config";
 static const char* CONFIG_ENGINE_USE_BLAS_THRESHOLD = "use_blas_threshold";
-static const char* CONFIG_ENGINE_USE_BLAS_THRESHOLD_DEFAULT = "20";
+static const char* CONFIG_ENGINE_USE_BLAS_THRESHOLD_DEFAULT = "1100";
 static const char* CONFIG_ENGINE_OMP_THREAD_NUM = "omp_thread_num";
 static const char* CONFIG_ENGINE_OMP_THREAD_NUM_DEFAULT = "0";
 static const char* CONFIG_ENGINE_GPU_SEARCH_THRESHOLD = "gpu_search_threshold";
@@ -96,7 +96,7 @@ static const char* CONFIG_GPU_RESOURCE_ENABLE_DEFAULT = "true";
 static const char* CONFIG_GPU_RESOURCE_ENABLE_DEFAULT = "false";
 #endif
 static const char* CONFIG_GPU_RESOURCE_CACHE_CAPACITY = "cache_capacity";
-static const char* CONFIG_GPU_RESOURCE_CACHE_CAPACITY_DEFAULT = "4";
+static const char* CONFIG_GPU_RESOURCE_CACHE_CAPACITY_DEFAULT = "1";
 static const char* CONFIG_GPU_RESOURCE_CACHE_THRESHOLD = "cache_threshold";
 static const char* CONFIG_GPU_RESOURCE_CACHE_THRESHOLD_DEFAULT = "0.85";
 static const char* CONFIG_GPU_RESOURCE_DELIMITER = ",";
