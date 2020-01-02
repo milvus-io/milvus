30c43402579f618d0dfcc807c2571d93fa9fb76c
Zhiru Zhu <zhiru.zhu@zilliz.com>
2020-01-02 19:29:28 +0800

    add segment writer

---
+++ core/src/segment/SegmentWriter.h
@@ -0,0 +1,47 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+#pragma once
+
+#include "DeletedDocs.h"
+#include "Vectors.h"
+#include "utils/Status.h"
+
+namespace milvus {
+namespace segment {
+
+class SegmentWriter {
+ public:
+    explicit SegmentWriter(const std::string& location);
+
+    Status
+    AddVectors(const std::string& field_name, const void* vector, const segment::doc_id_t* doc_ids, size_t count);
+
+    Status
+    Serialize();
+
+    Status
+    Flush();
+
+ private:
+    const std::string location_;
+    VectorsPtr vectors_ptr_;
+    DeletedDocsPtr deleted_docs_ptr_;
+};
+
+}  // namespace segment
+}  // namespace milvus
