4674bc90d962329ae91e6b00ce5356c0d0a71190
Yukikaze-CZR <zirui.chen@zilliz.com>
2020-01-04 15:55:17 +0800

    hamming dev-test case

--- tests/milvus_python_test/test_table.py
+++ tests/milvus_python_test/test_table.py
@@ -67,6 +67,20 @@ class TestTable:
         status = connect.create_table(param)
         assert status.OK()
 
+    def test_create_table_hamming(self, connect):
+        '''
+        target: test create normal table
+        method: create table with corrent params
+        expected: create status return ok
+        '''
+        table_name = gen_unique_str("test_table")
+        param = {'table_name': table_name,
+                 'dimension': dim,
+                 'index_file_size': index_file_size,
+                 'metric_type': MetricType.HAMMING}
+        status = connect.create_table(param)
+        assert status.OK()
+
     @pytest.mark.level(2)
     def test_create_table_without_connection(self, dis_connect):
         '''
@@ -226,6 +240,23 @@ class TestTable:
         assert res.table_name == table_name
         assert res.metric_type == MetricType.JACCARD
 
+    @pytest.mark.level(2)
+    def test_table_describe_table_name_hamming(self, connect):
+        '''
+        target: test describe table created with correct params
+        method: create table, assert the value returned by describe method
+        expected: table_name equals with the table name created
+        '''
+        table_name = gen_unique_str("test_table")
+        param = {'table_name': table_name,
+                 'dimension': dim,
+                 'index_file_size': index_file_size,
+                 'metric_type': MetricType.HAMMING}
+        connect.create_table(param)
+        status, res = connect.describe_table(table_name)
+        assert res.table_name == table_name
+        assert res.metric_type == MetricType.HAMMING
+
     # TODO: enable
     @pytest.mark.level(2)
     def _test_table_describe_table_name_multiprocessing(self, connect, args):
@@ -319,6 +350,17 @@ class TestTable:
         '''
         status = connect.delete_table(jac_table)
         assert not assert_has_table(connect, jac_table)
+
+    @pytest.mark.level(2)
+    def test_delete_table_hamming(self, connect, ham_table):
+        '''
+        target: test delete table created with correct params
+        method: create table and then delete,
+            assert the value returned by delete method
+        expected: status ok, and no table in tables
+        '''
+        status = connect.delete_table(ham_table)
+        assert not assert_has_table(connect, ham_table)
 
     @pytest.mark.level(2)
     def test_table_delete_without_connection(self, table, dis_connect):
@@ -516,6 +558,20 @@ class TestTable:
         connect.create_table(param)
         assert assert_has_table(connect, table_name)
 
+    def test_has_table_hamming(self, connect):
+        '''
+        target: test if the created table existed
+        method: create table, assert the value returned by has_table method
+        expected: True
+        '''
+        table_name = gen_unique_str("test_table")
+        param = {'table_name': table_name,
+                 'dimension': dim,
+                 'index_file_size': index_file_size,
+                 'metric_type': MetricType.HAMMING}
+        connect.create_table(param)
+        assert assert_has_table(connect, table_name)
+
     @pytest.mark.level(2)
     def test_has_table_without_connection(self, table, dis_connect):
         '''
@@ -586,6 +642,22 @@ class TestTable:
                  'index_file_size': index_file_size,
                  'metric_type': MetricType.JACCARD}
         connect.create_table(param)    
+        status, result = connect.show_tables()
+        assert status.OK()
+        assert table_name in result
+
+    def test_show_tables_hamming(self, connect):
+        '''
+        target: test show tables is correct or not, if table created
+        method: create table, assert the value returned by show_tables method is equal to 0
+        expected: table_name in show tables
+        '''
+        table_name = gen_unique_str("test_table")
+        param = {'table_name': table_name,
+                 'dimension': dim,
+                 'index_file_size': index_file_size,
+                 'metric_type': MetricType.HAMMING}
+        connect.create_table(param)
         status, result = connect.show_tables()
         assert status.OK()
         assert table_name in result
@@ -692,6 +764,14 @@ class TestTable:
         status, ids = connect.add_vectors(jac_table, vectors)
         status = connect.create_index(jac_table, index_params)
         status = connect.preload_table(jac_table)
+        assert status.OK()
+
+    @pytest.mark.level(1)
+    def test_preload_table_hamming(self, connect, ham_table, get_simple_index_params):
+        index_params = get_simple_index_params
+        status, ids = connect.add_vectors(ham_table, vectors)
+        status = connect.create_index(ham_table, index_params)
+        status = connect.preload_table(ham_table)
         assert status.OK()
 
     @pytest.mark.level(1)
