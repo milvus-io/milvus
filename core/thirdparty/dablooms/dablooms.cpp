d19d1c493e93cb87cba6a46a9f6c1bda0780b113
Zhiru Zhu <zhiru.zhu@zilliz.com>
2020-01-06 15:18:03 +0800

    fix free_counting_bloom bug

--- core/thirdparty/dablooms/dablooms.cpp
+++ core/thirdparty/dablooms/dablooms.cpp
@@ -204,7 +204,7 @@ int free_counting_bloom(counting_bloom_t *bloom)
     if (bloom != NULL) {
         free(bloom->hashes);
         bloom->hashes = NULL;
-        free(bloom->bitmap);
+        free_bitmap(bloom->bitmap);
         free(bloom);
         bloom = NULL;
     }
