944e94ce189ec7a66f1dfc399f937e33eb6eed6e
del-zhenwu <56623710+del-zhenwu@users.noreply.github.com>
2020-01-07 16:20:37 +0800

    Enhance testing framework based on kubernetes  (#935)
    * Update framework
    
    * remove files
    
    * Remove files
    
    * Remove ann-acc cases && Update java-sdk cases

--- tests/milvus_benchmark/runner.py
+++ tests/milvus_benchmark/runner.py
@@ -5,24 +5,26 @@ import time
 import random
 from multiprocessing import Process
 import numpy as np
+import sklearn.preprocessing
 from client import MilvusClient
 import utils
 import parser
 
 logger = logging.getLogger("milvus_benchmark.runner")
 
-SERVER_HOST_DEFAULT = "127.0.0.1"
-SERVER_PORT_DEFAULT = 19530
 VECTORS_PER_FILE = 1000000
 SIFT_VECTORS_PER_FILE = 100000
+JACCARD_VECTORS_PER_FILE = 2000000
+
 MAX_NQ = 10001
 FILE_PREFIX = "binary_"
 
 # FOLDER_NAME = 'ann_1000m/source_data'
-SRC_BINARY_DATA_DIR = '/poc/yuncong/yunfeng/random_data'
-SRC_BINARY_DATA_DIR_high = '/test/milvus/raw_data/random'
-SIFT_SRC_DATA_DIR = '/poc/yuncong/ann_1000m/'
-SIFT_SRC_BINARY_DATA_DIR = SIFT_SRC_DATA_DIR + 'source_data'
+SRC_BINARY_DATA_DIR = '/test/milvus/raw_data/random/'
+SIFT_SRC_DATA_DIR = '/test/milvus/raw_data/sift1b/'
+DEEP_SRC_DATA_DIR = '/test/milvus/raw_data/deep1b/'
+JACCARD_SRC_DATA_DIR = '/test/milvus/raw_data/jaccard/'
+HAMMING_SRC_DATA_DIR = '/test/milvus/raw_data/jaccard/'
 SIFT_SRC_GROUNDTRUTH_DATA_DIR = SIFT_SRC_DATA_DIR + 'gnd'
 
 WARM_TOP_K = 1
@@ -44,16 +46,19 @@ GROUNDTRUTH_MAP = {
 }
 
 
-def gen_file_name(idx, table_dimension, data_type):
+def gen_file_name(idx, dimension, data_type):
     s = "%05d" % idx
-    fname = FILE_PREFIX + str(table_dimension) + "d_" + s + ".npy"
+    fname = FILE_PREFIX + str(dimension) + "d_" + s + ".npy"
     if data_type == "random":
-        if table_dimension == 512:
-            fname = SRC_BINARY_DATA_DIR+'/'+fname
-        elif table_dimension >= 4096:
-            fname = SRC_BINARY_DATA_DIR_high+'/'+fname
+        fname = SRC_BINARY_DATA_DIR+fname
     elif data_type == "sift":
-        fname = SIFT_SRC_BINARY_DATA_DIR+'/'+fname
+        fname = SIFT_SRC_DATA_DIR+fname
+    elif data_type == "deep":
+        fname = DEEP_SRC_DATA_DIR+fname
+    elif data_type == "jaccard":
+        fname = JACCARD_SRC_DATA_DIR+fname
+    elif data_type == "hamming":
+        fname = HAMMING_SRC_DATA_DIR+fname
     return fname
 
 
@@ -62,9 +67,15 @@ def get_vectors_from_binary(nq, dimension, data_type):
     if nq > MAX_NQ:
         raise Exception("Over size nq")
     if data_type == "random":
-        file_name = gen_file_name(0, dimension, data_type)
+        file_name = SRC_BINARY_DATA_DIR+'query_%d.npy' % dimension
     elif data_type == "sift":
-        file_name = SIFT_SRC_DATA_DIR+'/'+'query.npy'
+        file_name = SIFT_SRC_DATA_DIR+'query.npy'
+    elif data_type == "deep":
+        file_name = DEEP_SRC_DATA_DIR+'query.npy'
+    elif data_type == "jaccard":
+        file_name = JACCARD_SRC_DATA_DIR+'query.npy'
+    elif data_type == "hamming":
+        file_name = HAMMING_SRC_DATA_DIR+'query.npy'
     data = np.load(file_name)
     vectors = data[0:nq].tolist()
     return vectors
@@ -73,6 +84,21 @@ def get_vectors_from_binary(nq, dimension, data_type):
 class Runner(object):
     def __init__(self):
         pass
+
+    def normalize(self, metric_type, X):
+        if metric_type == "ip":
+            logger.info("Set normalize for metric_type: %s" % metric_type)
+            X = sklearn.preprocessing.normalize(X, axis=1, norm='l2')
+            X = X.astype(np.float32)
+        elif metric_type == "l2":
+            X = X.astype(np.float32)
+        elif metric_type == "jaccard" or metric_type == "hamming":
+            tmp = []
+            for index, item in enumerate(X):
+                new_vector = bytes(np.packbits(item, axis=-1).tolist())
+                tmp.append(new_vector)
+            X = tmp
+        return X
 
     def do_insert(self, milvus, table_name, data_type, dimension, size, ni):
         '''
@@ -101,14 +127,18 @@ class Runner(object):
                 vectors_per_file = 10000
         elif data_type == "sift":
             vectors_per_file = SIFT_VECTORS_PER_FILE
+        elif data_type == "jaccard" or data_type == "hamming":
+            vectors_per_file = JACCARD_VECTORS_PER_FILE
+        else:
+            raise Exception("data_type: %s not supported" % data_type)
         if size % vectors_per_file or ni > vectors_per_file:
             raise Exception("Not invalid table size or ni")
         file_num = size // vectors_per_file
         for i in range(file_num):
             file_name = gen_file_name(i, dimension, data_type)
-            logger.info("Load npy file: %s start" % file_name)
+            # logger.info("Load npy file: %s start" % file_name)
             data = np.load(file_name)
-            logger.info("Load npy file: %s end" % file_name)
+            # logger.info("Load npy file: %s end" % file_name)
             loops = vectors_per_file // ni
             for j in range(loops):
                 vectors = data[j*ni:(j+1)*ni].tolist()
@@ -130,28 +160,26 @@ class Runner(object):
         bi_res["ni_time"] = ni_time
         return bi_res
 
-    def do_query(self, milvus, table_name, top_ks, nqs, nprobe, run_count):
+    def do_query(self, milvus, table_name, top_ks, nqs, nprobe, run_count=1):
+        bi_res = []
         (data_type, table_size, index_file_size, dimension, metric_type) = parser.table_parser(table_name)
         base_query_vectors = get_vectors_from_binary(MAX_NQ, dimension, data_type)
-
-        bi_res = []
-        for index, nq in enumerate(nqs):
+        for nq in nqs:
             tmp_res = []
             vectors = base_query_vectors[0:nq]
             for top_k in top_ks:
                 avg_query_time = 0.0
-                total_query_time = 0.0
+                min_query_time = 0.0
                 logger.info("Start query, query params: top-k: {}, nq: {}, actually length of vectors: {}".format(top_k, nq, len(vectors)))
                 for i in range(run_count):
                     logger.info("Start run query, run %d of %s" % (i+1, run_count))
                     start_time = time.time()
-                    status, query_res = milvus.query(vectors, top_k, nprobe)
-                    total_query_time = total_query_time + (time.time() - start_time)
-                    if status.code:
-                        logger.error("Query failed with message: %s" % status.message)
-                avg_query_time = round(total_query_time / run_count, 2)
-                logger.info("Avarage query time: %.2f" % avg_query_time)
-                tmp_res.append(avg_query_time)
+                    query_res = milvus.query(vectors, top_k, nprobe)
+                    interval_time = time.time() - start_time
+                    if (i == 0) or (min_query_time > interval_time):
+                        min_query_time = interval_time
+                logger.info("Min query time: %.2f" % min_query_time)
+                tmp_res.append(round(min_query_time, 2))
             bi_res.append(tmp_res)
         return bi_res
 
@@ -160,10 +188,7 @@ class Runner(object):
         base_query_vectors = get_vectors_from_binary(MAX_NQ, dimension, data_type)
         vectors = base_query_vectors[0:nq]
         logger.info("Start query, query params: top-k: {}, nq: {}, actually length of vectors: {}".format(top_k, nq, len(vectors)))
-        status, query_res = milvus.query(vectors, top_k, nprobe)
-        if not status.OK():
-            msg = "Query failed with message: %s" % status.message
-            raise Exception(msg)
+        query_res = milvus.query(vectors, top_k, nprobe)
         result_ids = []
         result_distances = []
         for result in query_res:
@@ -181,10 +206,7 @@ class Runner(object):
         base_query_vectors = get_vectors_from_binary(MAX_NQ, dimension, data_type)
         vectors = base_query_vectors[0:nq]
         logger.info("Start query, query params: top-k: {}, nq: {}, actually length of vectors: {}".format(top_k, nq, len(vectors)))
-        status, query_res = milvus.query(vectors, top_k, nprobe)
-        if not status.OK():
-            msg = "Query failed with message: %s" % status.message
-            raise Exception(msg)
+        query_res = milvus.query(vectors, top_k, nprobe)
         # if file existed, cover it
         if os.path.isfile(id_store_name):
             os.remove(id_store_name)
@@ -212,15 +234,17 @@ class Runner(object):
         # get the accuracy
         return self.get_recall_value(flat_id_list, index_id_list)
 
-    def get_recall_value(self, flat_id_list, index_id_list):
+    def get_recall_value(self, true_ids, result_ids):
         """
         Use the intersection length
         """
         sum_radio = 0.0
-        for index, item in enumerate(index_id_list):
-            tmp = set(item).intersection(set(flat_id_list[index]))
+        for index, item in enumerate(result_ids):
+            # tmp = set(item).intersection(set(flat_id_list[index]))
+            tmp = set(true_ids[index]).intersection(set(item))
             sum_radio = sum_radio + len(tmp) / len(item)
-        return round(sum_radio / len(index_id_list), 3)
+            # logger.debug(sum_radio)
+        return round(sum_radio / len(result_ids), 3)
 
     """
     Implementation based on:
