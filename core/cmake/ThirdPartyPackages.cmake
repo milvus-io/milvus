2c994fb32d842fb6bfe5b6a85ffb83911098b621
yhz <413554850@qq.com>
2020-01-08 19:28:02 +0800

    remove EP_COMMON_CMAKE_ARGS in oatpp and oatpp-swagger

--- core/cmake/ThirdPartyPackages.cmake
+++ core/cmake/ThirdPartyPackages.cmake
@@ -379,7 +379,6 @@ macro(build_gtest)
             "${GTEST_PREFIX}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}gtest_main${CMAKE_STATIC_LIBRARY_SUFFIX}")
 
     set(GTEST_CMAKE_ARGS
-            ${EP_COMMON_CMAKE_ARGS}
             "-DCMAKE_INSTALL_PREFIX=${GTEST_PREFIX}"
             "-DCMAKE_INSTALL_LIBDIR=lib"
             -DCMAKE_CXX_FLAGS=${GTEST_CMAKE_CXX_FLAGS}
@@ -1042,7 +1041,6 @@ macro(build_oatpp)
     set(OATPP_DIR_LIB "${OATPP_PREFIX}/lib")
 
     set(OATPP_CMAKE_ARGS
-            ${EP_COMMON_CMAKE_ARGS}
             "-DCMAKE_INSTALL_PREFIX=${OATPP_PREFIX}"
             -DBUILD_SHARED_LIBS=OFF
             -DOATPP_BUILD_TESTS=OFF
@@ -1087,7 +1085,6 @@ macro(build_oatpp_swagger)
     set(OATPP_SWAGGER_INCLUDE_DIR "${OATPP_SWAGGER_PREFIX}/include/oatpp-${OATPP_SWAGGER_VERSION}/oatpp-swagger")
 
     set(OATPP_SWAGGER_CMAKE_ARGS
-            ${EP_COMMON_CMAKE_ARGS}
             "-DCMAKE_INSTALL_PREFIX=${OATPP_SWAGGER_PREFIX}"
             -DBUILD_SHARED_LIBS=OFF
             -DOATPP_DIR_SRC=${OATPP_DIR_SRC}
