72ad2c1131ec3086ee58cf676e43259175fb9e5d
yhz <413554850@qq.com>
2020-01-14 16:07:01 +0800

    check table exists when drop partition (fix #1029)

--- core/src/server/delivery/request/DropPartitionRequest.cpp
+++ core/src/server/delivery/request/DropPartitionRequest.cpp
@@ -47,8 +47,19 @@ DropPartitionRequest::OnExecute() {
     std::string table_name = table_name_;
     std::string partition_name = partition_name_;
     std::string partition_tag = tag_;
+
+    bool exists;
+    auto status = DBWrapper::DB()->HasTable(table_name, exists);
+    if (!status.ok()) {
+        return status;
+    }
+
+    if (!exists) {
+        return Status(SERVER_TABLE_NOT_EXIST, "Table " + table_name_ + " not exists");
+    }
+
     if (!partition_name.empty()) {
-        auto status = ValidationUtil::ValidateTableName(partition_name);
+        status = ValidationUtil::ValidateTableName(partition_name);
         if (!status.ok()) {
             return status;
         }
