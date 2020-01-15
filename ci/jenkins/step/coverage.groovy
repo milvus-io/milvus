9d3dfd4cfa00fc6dad53ca1642b7503afdb9b5f0
quicksilver <zhifeng.zhang@zilliz.com>
2020-01-15 20:56:50 +0800

    Fix Failed to upload bug
    Signed-off-by: groot <yihua.mo@zilliz.com>

--- ci/jenkins/step/coverage.groovy
+++ ci/jenkins/step/coverage.groovy
@@ -5,11 +5,11 @@ timeout(time: 30, unit: 'MINUTES') {
         String formatFlag = "${BINARY_VERSION}-version-${OS_NAME}-unittest".replaceAll("\\.", "_").replaceAll("-", "_")
         if (isNightlyTest) {
 	        withCredentials([[$class: 'StringBinding', credentialsId: "milvus-ci-codecov-token", variable: 'CODECOV_TOKEN']]) {
-	            sh "curl -s https://codecov.io/bash | bash -s - -f output_new.info -n ${formatFlag} -F nightly -F ${BINARY_VERSION}_version_${OS_NAME}_unittest || echo \"Codecov did not collect coverage reports\""
+	            sh "curl -s https://codecov.io/bash | bash -s - -f output_new.info -n ${BINARY_VERSION}-version-${OS_NAME}-unittest -F nightly -F ${formatFlag} || echo \"Codecov did not collect coverage reports\""
 	        }
 	    } else {
             withCredentials([[$class: 'StringBinding', credentialsId: "milvus-ci-codecov-token", variable: 'CODECOV_TOKEN']]) {
-	            sh "curl -s https://codecov.io/bash | bash -s - -f output_new.info -n ${formatFlag} -F ${BINARY_VERSION}_version_${OS_NAME}_unittest || echo \"Codecov did not collect coverage reports\""
+	            sh "curl -s https://codecov.io/bash | bash -s - -f output_new.info -n ${BINARY_VERSION}-version-${OS_NAME}-unittest -F ${formatFlag} || echo \"Codecov did not collect coverage reports\""
 	        }
 	    }
     }
