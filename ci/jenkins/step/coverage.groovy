3fee1c88bf121066803f02f671209e7dec66ad27
quicksilver <zhifeng.zhang@zilliz.com>
2020-01-15 14:00:41 +0800

    Format Codecov

--- ci/jenkins/step/coverage.groovy
+++ ci/jenkins/step/coverage.groovy
@@ -1,6 +1,7 @@
 timeout(time: 30, unit: 'MINUTES') {
     dir ("ci/scripts") {
         sh "./coverage.sh -o ${env.MILVUS_INSTALL_PREFIX} -u root -p 123456 -t \$POD_IP"
+        boolean isNightlyTest = currentBuild.getBuildCauses('hudson.triggers.TimerTrigger$TimerTriggerCause').size() != 0 ? true : false
         if (isNightlyTest) {
 	        withCredentials([[$class: 'StringBinding', credentialsId: "milvus-ci-codecov-token", variable: 'CODECOV_TOKEN']]) {
 	            sh "curl -s https://codecov.io/bash | bash -s - -f output_new.info -n ${BINARY_VERSION}-version-${OS_NAME}-unittest -F nightly -F ${BINARY_VERSION}-version-${OS_NAME}-unittest || echo \"Codecov did not collect coverage reports\""
@@ -12,4 +13,3 @@ timeout(time: 30, unit: 'MINUTES') {
 	    }
     }
 }
-
