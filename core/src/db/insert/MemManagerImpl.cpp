972faf179c4c592fed4366a9014222b51ba772d1
youny626 <zzhu@fandm.edu>
2020-02-10 20:11:52 +0800

    update for wal

--- core/src/db/insert/MemManagerImpl.cpp
+++ core/src/db/insert/MemManagerImpl.cpp
@@ -121,7 +121,7 @@ MemManagerImpl::DeleteVectors(const std::string& table_id, int64_t length, const
 Status
 MemManagerImpl::Flush(const std::string& table_id) {
     ToImmutable(table_id);
-
+    // TODO: There is actually only one memTable in the immutable list
     MemList temp_immutable_list;
     {
         std::unique_lock<std::mutex> lock(mutex_);
@@ -129,8 +129,8 @@ MemManagerImpl::Flush(const std::string& table_id) {
     }
 
     std::unique_lock<std::mutex> lock(serialization_mtx_);
+    auto max_lsn = GetMaxLSN(temp_immutable_list);
     for (auto& mem : temp_immutable_list) {
-        auto max_lsn = GetMaxLSN();
         mem->Serialize(max_lsn);
     }
 
@@ -149,11 +149,12 @@ MemManagerImpl::Flush(std::set<std::string>& table_ids) {
 
     std::unique_lock<std::mutex> lock(serialization_mtx_);
     table_ids.clear();
+    auto max_lsn = GetMaxLSN(temp_immutable_list);
     for (auto& mem : temp_immutable_list) {
-        auto max_lsn = GetMaxLSN();
         mem->Serialize(max_lsn);
         table_ids.insert(mem->GetTableId());
     }
+    meta_->SetGlobalLastLSN(max_lsn);
 
     return Status::OK();
 }
@@ -240,11 +241,11 @@ MemManagerImpl::GetCurrentMem() {
 }
 
 uint64_t
-MemManagerImpl::GetMaxLSN() {
+MemManagerImpl::GetMaxLSN(const MemList& tables) {
     uint64_t max_lsn = 0;
-    for (auto& kv : mem_id_map_) {
-        auto cur_lsn = kv.second->GetLSN();
-        if (kv.second->GetLSN() > max_lsn) {
+    for (auto& table : tables) {
+        auto cur_lsn = table->GetLSN();
+        if (table->GetLSN() > max_lsn) {
             max_lsn = cur_lsn;
         }
     }
