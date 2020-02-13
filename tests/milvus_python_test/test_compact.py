0220c0aebd971372849f69ff758052b8eb384942
sahuang <xiaohaix@student.unimelb.edu.au>
2020-02-13 20:27:23 +0800

    More test cases

--- tests/milvus_python_test/test_compact.py
+++ tests/milvus_python_test/test_compact.py
@@ -16,6 +16,7 @@ nprobe = 1
 top_k = 1
 epsilon = 0.0001
 tag = "1970-01-01"
+nb = 6000
 
 
 class TestCompactBase:
@@ -87,8 +88,6 @@ class TestCompactBase:
         status = connect.compact(table)
         assert status.OK()
         status = connect.flush([table])
-        status, res = connect.search_vectors(table, top_k, nprobe, vector) 
-        logging.getLogger().info(res)
         assert status.OK()
 
     @pytest.mark.timeout(COMPACT_TIMEOUT)
@@ -108,9 +107,185 @@ class TestCompactBase:
         status = connect.compact(table)
         assert status.OK()
         status = connect.flush([table])
-        status, res = connect.search_vectors(table, top_k, nprobe, vector) 
-        logging.getLogger().info(res)
+        assert status.OK()
+
+    @pytest.fixture(
+        scope="function",
+        params=gen_simple_index_params()
+    )
+    def get_simple_index_params(self, request, connect):
+        if str(connect._cmd("mode")[1]) == "CPU":
+            if request.param["index_type"] != IndexType.IVF_SQ8 or request.param["index_type"] != IndexType.IVFLAT or request.param["index_type"] != IndexType.FLAT:
+                pytest.skip("Only support index_type: flat/ivf_flat/ivf_sq8")
+        else:
+            pytest.skip("Only support CPU mode")
+        return request.param
+
+    def test_compact_after_index_created(self, connect, table, get_simple_index_params):
+        '''
+        target: test compact table after index created
+        method: add vector, create index, delete vector and compact
+        expected: status ok, index description no change
+        '''
+        index_params = get_simple_index_params
+        vector = gen_single_vector(dim)
+        status, ids = connect.add_vectors(table, vector)
+        assert status.OK()
+        status = connect.flush([table])
+        assert status.OK()
+        status = connect.create_index(table, index_params) 
+        assert status.OK()
+        status = connect.delete_by_id(table, ids)
+        assert status.OK()
+        status = connect.flush([table])
+        assert status.OK()
+        status = connect.compact(table)
+        assert status.OK()
+    
+    @pytest.mark.timeout(COMPACT_TIMEOUT)
+    def test_add_vector_and_compact_twice(self, connect, table):
+        '''
+        target: test add vector and compact twice
+        method: add vector and compact table twice
+        expected: status ok
+        '''
+        vector = gen_single_vector(dim)
+        status, ids = connect.add_vectors(table, vector)
+        assert status.OK()
+        status = connect.flush([table])
+        assert status.OK()
+        status = connect.compact(table)
+        assert status.OK()
+        status = connect.flush([table])
+        assert status.OK()
+        status = connect.compact(table)
         assert status.OK()
 
     @pytest.mark.timeout(COMPACT_TIMEOUT)
-    
+    def test_add_vector_delete_and_compact_twice(self, connect, table):
+        '''
+        target: test add vector, delete it and compact twice
+        method: add vector, delete it and compact table twice
+        expected: status ok, vectors added and deleted
+        '''
+        vector = gen_single_vector(dim)
+        status, ids = connect.add_vectors(table, vector)
+        assert status.OK()
+        status = connect.flush([table])
+        assert status.OK()
+        status = connect.delete_by_id(table, ids)
+        assert status.OK()
+        status = connect.compact(table)
+        assert status.OK()
+        status = connect.flush([table])
+        assert status.OK()
+        status = connect.compact(table)
+        assert status.OK()
+
+    @pytest.mark.timeout(COMPACT_TIMEOUT)
+    def test_compact_multi_tables(self, connect):
+        '''
+        target: test compact works or not with multiple tables
+        method: create 50 tables, add vectors into them and compact in turn
+        expected: status ok
+        '''
+        nq = 100
+        num_tables = 50
+        vectors = gen_vectors(nq, dim)
+        table_list = []
+        for i in range(num_tables):
+            table_name = gen_unique_str("test_add_multi_table_%d" % i)
+            table_list.append(table_name)
+            param = {'table_name': table_name,
+                     'dimension': dim,
+                     'index_file_size': index_file_size,
+                     'metric_type': MetricType.L2}
+            connect.create_table(param)
+        time.sleep(6)
+        for i in range(num_tables):
+            status, ids = connect.add_vectors(table_name=table_list[i], records=vectors)
+            assert status.OK()
+            status = connect.compact(table_list[i])
+            assert status.OK()
+
+    @pytest.mark.timeout(COMPACT_TIMEOUT)
+    def test_add_vector_after_compact(self, connect, table):
+        '''
+        target: test add vector after compact 
+        method: after compact operation, add vector
+        expected: status ok, vector added
+        '''
+        vectors = gen_vector(nb, dim)
+        status, ids = connect.add_vectors(table, vectors)
+        assert status.OK()
+        status = connect.flush([table])
+        assert status.OK()
+        status = connect.compact(table)
+        assert status.OK()
+        status = connect.flush([table])
+        assert status.OK()
+        vector = gen_single_vector(dim)
+        status, ids = connect.add_vectors(table, vector)
+        assert status.OK()
+
+    @pytest.mark.timeout(COMPACT_TIMEOUT)
+    def test_index_creation_after_compact(self, connect, table, get_simple_index_params):
+        '''
+        target: test index creation after compact
+        method: after compact operation, create index
+        expected: status ok, index description no change
+        '''
+        vectors = gen_vector(nb, dim)
+        status, ids = connect.add_vectors(table, vectors)
+        assert status.OK()
+        status = connect.flush([table])
+        assert status.OK()
+        status = connect.compact(table)
+        assert status.OK()
+        status = connect.flush([table])
+        assert status.OK()
+        index_params = get_simple_index_params
+        status = connect.create_index(table, index_params) 
+        assert status.OK()
+
+    @pytest.mark.timeout(COMPACT_TIMEOUT)
+    def test_delete_vectors_after_compact(self, connect, table):
+        '''
+        target: test delete vectors after compact
+        method: after compact operation, delete vectors
+        expected: status ok, vectors deleted
+        '''
+        vectors = gen_vector(nb, dim)
+        status, ids = connect.add_vectors(table, vectors)
+        assert status.OK()
+        status = connect.flush([table])
+        assert status.OK()
+        status = connect.compact(table)
+        assert status.OK()
+        status = connect.flush([table])
+        assert status.OK()
+        status = connect.delete_by_id(table, ids)
+        assert status.OK()
+        status = connect.flush([table])
+        assert status.OK()
+
+    @pytest.mark.timeout(COMPACT_TIMEOUT)
+    def test_search_after_compact(self, connect, table):
+        '''
+        target: test search after compact
+        method: after compact operation, search vector
+        expected: status ok
+        '''
+        vectors = gen_vector(nb, dim)
+        status, ids = connect.add_vectors(table, vectors)
+        assert status.OK()
+        status = connect.flush([table])
+        assert status.OK()
+        status = connect.compact(table)
+        assert status.OK()
+        status = connect.flush([table])
+        assert status.OK()
+        query_vecs = [vectors[0]]
+        status, res = connect.search_vectors(table, top_k, nprobe, query_vecs) 
+        logging.getLogger().info(res)
+        assert status.OK()
