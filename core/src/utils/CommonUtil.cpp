3ba1a41239596a55b1f25f4553816b100896c211
yudong.cai <yudong.cai@zilliz.com>
2020-02-19 09:22:18 +0800

    #1250 fix code coverage
    Signed-off-by: yudong.cai <yudong.cai@zilliz.com>

--- core/src/utils/CommonUtil.cpp
+++ core/src/utils/CommonUtil.cpp
@@ -228,6 +228,7 @@ CommonUtil::ConvertTime(tm time_struct, time_t& time_integer) {
     time_integer = mktime(&time_struct);
 }
 
+#ifdef MILVUS_ENABLE_PROFILING
 std::string
 CommonUtil::GetCurrentTimeStr() {
     time_t tt;
@@ -241,6 +242,7 @@ CommonUtil::GetCurrentTimeStr() {
                       "_" + std::to_string(t.tm_sec);
     return str;
 }
+#endif
 
 void
 CommonUtil::EraseFromCache(const std::string& item_key) {
