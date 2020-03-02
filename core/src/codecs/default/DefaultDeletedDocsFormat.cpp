17f8d9557da2a0ef2f6f1616426e35dcf8e71e59
yudong.cai <yudong.cai@zilliz.com>
2020-03-02 20:42:09 +0800

    #1434 DefaultDeletedDocsFormat support S3
    Signed-off-by: yudong.cai <yudong.cai@zilliz.com>

--- core/src/codecs/default/DefaultDeletedDocsFormat.cpp
+++ core/src/codecs/default/DefaultDeletedDocsFormat.cpp
@@ -23,6 +23,11 @@
 #include <vector>
 
 #include "segment/Types.h"
+#include "server/Config.h"
+#include "storage/s3/S3IOReader.h"
+#include "storage/s3/S3IOWriter.h"
+#include "storage/file/FileIOReader.h"
+#include "storage/file/FileIOWriter.h"
 #include "utils/Exception.h"
 #include "utils/Log.h"
 
@@ -33,40 +38,59 @@ void
 DefaultDeletedDocsFormat::read(const store::DirectoryPtr& directory_ptr, segment::DeletedDocsPtr& deleted_docs) {
     const std::lock_guard<std::mutex> lock(mutex_);
 
+    bool s3_enable = false;
+    server::Config& config = server::Config::GetInstance();
+    config.GetStorageConfigS3Enable(s3_enable);
+
     std::string dir_path = directory_ptr->GetDirPath();
     const std::string del_file_path = dir_path + "/" + deleted_docs_filename_;
-    FILE* del_file = fopen(del_file_path.c_str(), "rb");
-    if (del_file == nullptr) {
-        std::string err_msg = "Failed to open file: " + del_file_path;
-        ENGINE_LOG_ERROR << err_msg;
-        throw Exception(SERVER_CANNOT_CREATE_FILE, err_msg);
-    }
 
-    auto file_size = boost::filesystem::file_size(boost::filesystem::path(del_file_path));
-    auto deleted_docs_size = file_size / sizeof(segment::offset_t);
-    std::vector<segment::offset_t> deleted_docs_list;
-    deleted_docs_list.resize(deleted_docs_size);
-    fread((void*)(deleted_docs_list.data()), sizeof(segment::offset_t), deleted_docs_size, del_file);
-    deleted_docs = std::make_shared<segment::DeletedDocs>(deleted_docs_list);
-    fclose(del_file);
+    try {
+        std::shared_ptr<storage::IOReader> reader_ptr;
+        if (s3_enable) {
+            reader_ptr = std::make_shared<storage::S3IOReader>(del_file_path);
+        } else {
+            reader_ptr = std::make_shared<storage::FileIOReader>(del_file_path);
+        }
+
+        size_t file_size = reader_ptr->length();
+        std::vector<segment::offset_t> deleted_docs_list;
+        deleted_docs_list.resize(file_size / sizeof(segment::offset_t));
+        reader_ptr->read(deleted_docs_list.data(), file_size);
+
+        deleted_docs = std::make_shared<segment::DeletedDocs>(deleted_docs_list);
+    } catch (std::exception& e) {
+        std::string err_msg = "Failed to read from file: " + del_file_path + ", error: " + e.what();
+        ENGINE_LOG_ERROR << err_msg;
+        throw Exception(SERVER_WRITE_ERROR, err_msg);
+    }
 }
 
 void
 DefaultDeletedDocsFormat::write(const store::DirectoryPtr& directory_ptr, const segment::DeletedDocsPtr& deleted_docs) {
     const std::lock_guard<std::mutex> lock(mutex_);
 
+    bool s3_enable = false;
+    server::Config& config = server::Config::GetInstance();
+    config.GetStorageConfigS3Enable(s3_enable);
+
     std::string dir_path = directory_ptr->GetDirPath();
     const std::string del_file_path = dir_path + "/" + deleted_docs_filename_;
-    FILE* del_file = fopen(del_file_path.c_str(), "ab");  // TODO(zhiru): append mode
-    if (del_file == nullptr) {
-        std::string err_msg = "Failed to open file: " + del_file_path;
-        ENGINE_LOG_ERROR << err_msg;
-        throw Exception(SERVER_CANNOT_CREATE_FILE, err_msg);
-    }
 
-    auto deleted_docs_list = deleted_docs->GetDeletedDocs();
-    fwrite((void*)(deleted_docs_list.data()), sizeof(segment::offset_t), deleted_docs->GetSize(), del_file);
-    fclose(del_file);
+    try {
+        std::shared_ptr<storage::IOWriter> writer_ptr;
+        if (s3_enable) {
+            writer_ptr = std::make_shared<storage::S3IOWriter>(del_file_path);
+        } else {
+            writer_ptr = std::make_shared<storage::FileIOWriter>(del_file_path);
+        }
+        auto deleted_docs_list = deleted_docs->GetDeletedDocs();
+        writer_ptr->write((void*)(deleted_docs_list.data()), sizeof(segment::offset_t) * deleted_docs->GetSize());
+    } catch (std::exception& e) {
+        std::string err_msg = "Failed to write rv file: " + del_file_path + ", error: " + e.what();
+        ENGINE_LOG_ERROR << err_msg;
+        throw Exception(SERVER_WRITE_ERROR, err_msg);
+    }
 }
 
 }  // namespace codec
