2a00f33d727eb695e7c1d4300576815a97623435
JinHai-CN <hai.jin@zilliz.com>
2020-03-06 13:43:31 +0800

    Fix lint error
    Signed-off-by: JinHai-CN <hai.jin@zilliz.com>

--- core/src/server/Config.cpp
+++ core/src/server/Config.cpp
@@ -9,8 +9,6 @@
 // is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
 // or implied. See the License for the specific language governing permissions and limitations under the License.
 
-#include "server/Config.h"
-
 #include <cache/CpuCacheMgr.h>
 #include <cache/GpuCacheMgr.h>
 #include <fiu-local.h>
