fa49c40d942fe26818f568009c0469e13eb72f06
xiaojun.lin <xiaojun.lin@zilliz.com>
2020-03-13 02:42:12 +0800

    update...
    Signed-off-by: xiaojun.lin <xiaojun.lin@zilliz.com>

--- core/src/index/knowhere/knowhere/index/vector_index/impl/nsg/NSG.cpp
+++ core/src/index/knowhere/knowhere/index/vector_index/impl/nsg/NSG.cpp
@@ -9,7 +9,7 @@
 // is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
 // or implied. See the License for the specific language governing permissions and limitations under the License
 
-#include <implrithm>
+#include <algorithm>
 #include <cstdlib>
 #include <cstring>
 #include <iostream>
