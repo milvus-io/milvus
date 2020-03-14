1b60e3bca6bd99c01f4825ba1d3c0275ca92ca1c
quicksilver <zhifeng.zhang@zilliz.com>
2020-03-14 18:14:04 +0800

    Refactor CI workflow (#1657)
    * Increase memory limit in Jenkins CI
    
    Signed-off-by: quicksilver <zhifeng.zhang@zilliz.com>
    
    * Add pod tolerations to slave pods
    
    * Refactor ci/scripts/build.sh
    
    * Refactor ci/scripts/build.sh
    
    * Refactor CI
    
    * Refactor CI
    
    * Refactor CI
    
    * Refactor CI
    
    * Refactor CI
    
    * Refactor CI
    
    * Refactor CI

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -12,92 +12,106 @@ SCRIPTS_DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
 MILVUS_CORE_DIR="${SCRIPTS_DIR}/../../core"
 CORE_BUILD_DIR="${MILVUS_CORE_DIR}/cmake_build"
-BUILD_TYPE="Debug"
-BUILD_UNITTEST="OFF"
-INSTALL_PREFIX="/var/lib/milvus"
-FAISS_ROOT=""
-PRIVILEGES="OFF"
-BUILD_COVERAGE="OFF"
-RUN_CPPLINT="OFF"
-GPU_VERSION="OFF"
-WITH_MKL="OFF"
-FIU_ENABLE="OFF"
-CUDA_COMPILER=/usr/local/cuda/bin/nvcc
 
-while getopts "o:t:b:f:pgulcmih" arg
-do
-        case $arg in
-             o)
-                INSTALL_PREFIX=$OPTARG
-                ;;
-             t)
-                BUILD_TYPE=$OPTARG # BUILD_TYPE
-                ;;
-             b)
-                CORE_BUILD_DIR=$OPTARG # CORE_BUILD_DIR
-                ;;
-             f)
-                FAISS_ROOT=$OPTARG # FAISS ROOT PATH
-                ;;
-             p)
-                PRIVILEGES="ON" # ELEVATED PRIVILEGES
-                ;;
-             g)
-                GPU_VERSION="ON";
-                ;;
-             u)
-                echo "Build and run unittest cases" ;
-                BUILD_UNITTEST="ON";
-                ;;
-             l)
-                RUN_CPPLINT="ON"
-                ;;
-             c)
-                BUILD_COVERAGE="ON"
-                ;;
-             m)
-                WITH_MKL="ON"
-                ;;
-             i)
-                FIU_ENABLE="ON"
-                ;;
-             h) # help
-                echo "
+HELP="
+Usage:
+  $0 [flags] [Arguments]
 
-parameter:
--o: install prefix(default: /var/lib/milvus)
--t: build type(default: Debug)
--b: core code build directory
--f: faiss root path
--p: install command with elevated privileges
--g: gpu version
--u: building unit test options(default: OFF)
--l: run cpplint, clang-format and clang-tidy(default: OFF)
--c: code coverage(default: OFF)
--m: build with MKL(default: OFF)
--i: build FIU_ENABLE(default: OFF)
--h: help
+    clean                     Remove all existing build artifacts and configuration (start over)
+    -i [INSTALL_PREFIX] or --install_prefix=[INSTALL_PREFIX]
+                              Install directory used by install.
+    -t [BUILD_TYPE] or --build_type=[BUILD_TYPE]
+                              Build type (default: Release)
+    -j[N] or --jobs=[N]       Allow N jobs at once; infinite jobs with no arg.
+    -l                        Run cpplint & check clang-format
+    -n                        No make and make install step
+    -g                        Building for the architecture of the GPU in the system
+    --with_mkl                Build with MKL (default: OFF)
+    --with_fiu                Build with FIU (default: OFF)
+    -c or --coverage          Build Code Coverage
+    -u or --tests             Build unittest case
+    -p or --privileges        Install command with elevated privileges
+    -v or --verbose           A level above ‘basic’; includes messages about which makefiles were parsed, prerequisites that did not need to be rebuilt
+    -h or --help              Print help information
 
-usage:
-./build.sh -o \${INSTALL_PREFIX} -t \${BUILD_TYPE} -b \${CORE_BUILD_DIR} -f \${FAISS_ROOT} [-p] [-g] [-u] [-l] [-c] [-m] [-i] [-h]
-                "
-                exit 0
-                ;;
-             ?)
-                echo "ERROR! unknown argument"
-        exit 1
-        ;;
+
+Use \"$0  --help\" for more information about a given command.
+"
+
+ARGS=`getopt -o "i:t:j::lngcupvh" -l "install_prefix::,build_type::,jobs::,with_mkl,with_fiu,coverage,tests,privileges,help" -n "$0" -- "$@"`
+
+eval set -- "${ARGS}"
+
+while true ; do
+        case "$1" in
+                -i|--install_prefix)
+                        # o has an optional argument. As we are in quoted mode,
+                        # an empty parameter will be generated if its optional
+                        # argument is not found.
+                        case "$2" in
+                                "") echo "Option install_prefix, no argument"; exit 1 ;;
+                                *)  INSTALL_PREFIX=$2 ; shift 2 ;;
+                        esac ;;
+                -t|--build_type)
+                        case "$2" in
+                                "") echo "Option build_type, no argument"; exit 1 ;;
+                                *)  BUILD_TYPE=$2 ; shift 2 ;;
+                        esac ;;
+                -j|--jobs)
+                        case "$2" in
+                                "") PARALLEL_LEVEL=""; shift 2 ;;
+                                *)  PARALLEL_LEVEL=$2 ; shift 2 ;;
+                        esac ;;
+                -g) echo "Building for the architecture of the GPU in the system..." ; GPU_VERSION="ON" ; shift ;;
+                --with_mkl) echo "Build with MKL" ; WITH_MKL="ON" ; shift ;;
+                --with_fiu) echo "Build with FIU" ; FIU_ENABLE="ON" ; shift ;;
+                --coverage) echo "Build code coverage" ; BUILD_COVERAGE="ON" ; shift ;;
+                -u|--tests) echo "Build unittest cases" ; BUILD_UNITTEST="ON" ; shift ;;
+                -n) echo "No build and install step" ; COMPILE_BUILD="OFF" ; shift ;;
+                -l) RUN_CPPLINT="ON" ; shift ;;
+                -p|--privileges) PRIVILEGES="ON" ; shift ;;
+                -v|--verbose) VERBOSE="1" ; shift ;;
+                -h|--help) echo -e "${HELP}" ; exit 0 ;;
+                --) shift ; break ;;
+                *) echo "Internal error!" ; exit 1 ;;
         esac
 done
 
-echo -e "===\n=== ccache statistics before build\n==="
-ccache --show-stats
+# Set defaults for vars modified by flags to this script
+CUDA_COMPILER=/usr/local/cuda/bin/nvcc
+INSTALL_PREFIX=${INSTALL_PREFIX:="/var/lib/milvus"}
+VERBOSE=${VERBOSE:=""}
+BUILD_TYPE=${BUILD_TYPE:="Release"}
+BUILD_UNITTEST=${BUILD_UNITTEST:="OFF"}
+BUILD_COVERAGE=${BUILD_COVERAGE:="OFF"}
+COMPILE_BUILD=${COMPILE_BUILD:="ON"}
+GPU_VERSION=${GPU_VERSION:="OFF"}
+RUN_CPPLINT=${RUN_CPPLINT:="OFF"}
+WITH_MKL=${WITH_MKL:="OFF"}
+FIU_ENABLE=${FIU_ENABLE:="OFF"}
+PRIVILEGES=${PRIVILEGES:="OFF"}
+CLEANUP=${CLEANUP:="OFF"}
+PARALLEL_LEVEL=${PARALLEL_LEVEL:="8"}
+
+for arg do
+if [[ $arg == "clean" ]];then
+    echo "Remove all existing build artifacts and configuration..."
+    if [ -d ${CORE_BUILD_DIR} ]; then
+        find ${CORE_BUILD_DIR} -mindepth 1 -delete
+        rmdir ${CORE_BUILD_DIR} || true
+    fi
+    exit 0
+fi
+done
 
 if [[ ! -d ${CORE_BUILD_DIR} ]]; then
     mkdir ${CORE_BUILD_DIR}
 fi
 
-cd ${CORE_BUILD_DIR}
+echo -e "===\n=== ccache statistics before build\n==="
+ccache --show-stats
+
+pushd ${CORE_BUILD_DIR}
 
 CMAKE_CMD="cmake \
 -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX}
@@ -106,7 +120,6 @@ CMAKE_CMD="cmake \
 -DMILVUS_GPU_VERSION=${GPU_VERSION} \
 -DBUILD_UNIT_TEST=${BUILD_UNITTEST} \
 -DBUILD_COVERAGE=${BUILD_COVERAGE} \
--DFAISS_ROOT=${FAISS_ROOT} \
 -DFAISS_WITH_MKL=${WITH_MKL} \
 -DArrow_SOURCE=AUTO \
 -DFAISS_SOURCE=AUTO \
@@ -114,7 +127,6 @@ CMAKE_CMD="cmake \
 ${MILVUS_CORE_DIR}"
 echo ${CMAKE_CMD}
 ${CMAKE_CMD}
-
 
 if [[ ${RUN_CPPLINT} == "ON" ]]; then
     # cpplint check
@@ -143,11 +155,15 @@ if [[ ${RUN_CPPLINT} == "ON" ]]; then
 #    echo "clang-tidy check passed!"
 fi
 
-# compile and build
-make -j6 || exit 1
+if [[ ${COMPILE_BUILD} == "ON" ]];then
+    # compile and build
+    make -j${PARALLEL_LEVEL} VERBOSE=${VERBOSE} || exit 1
 
-if [[ ${PRIVILEGES} == "ON" ]];then
-    sudo make install || exit 1
-else
-    make install || exit 1
+    if [[ ${PRIVILEGES} == "ON" ]];then
+        sudo make install || exit 1
+    else
+        make install || exit 1
+    fi
 fi
+
+popd
