3dc683e679f9224673cccb8b428ced3cc7333f94
quicksilver <zhifeng.zhang@zilliz.com>
2020-03-14 18:14:04 +0800

    Refactor CI workflow (#1657)
    * Increase memory limit in Jenkins CI
    
    
    * Add pod tolerations to slave pods
    
    * Refactor ci/scripts/build.sh
    
    * Refactor ci/scripts/build.sh
    
    * Refactor CI
    
    * Refactor CI
    
    * Refactor CI
    
    * Refactor CI
    
    * Refactor CI
    
    * Refactor CI
    
    * Refactor CI

---
+++ ci/jenkins/step/unittest.groovy
@@ -0,0 +1,5 @@
+timeout(time: 20, unit: 'MINUTES') {
+    dir ("ci/scripts") {
+        sh "./run_unittest.sh -i ${env.MILVUS_INSTALL_PREFIX} --mysql_user=root --mysql_password=123456 --mysql_host=\$POD_IP"
+    }
+}
