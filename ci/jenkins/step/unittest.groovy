8644d341c452c3d9961c18bc19d830f4d1ba8574
quicksilver <zhifeng.zhang@zilliz.com>
2020-03-17 10:36:24 +0800

    #1656 Add timeout for whole Jenkins job (#1676)
    * Increase memory limit in Jenkins CI
    
    Signed-off-by: quicksilver <zhifeng.zhang@zilliz.com>
    
    * Add pod tolerations to slave pods
    
    * Refactor ci/scripts/build.sh
    
    * Refactor ci/scripts/build.sh
    
    * Refactor CI
    
    * Refactor CI
    
    * Refactor CI
    
    * Refactor CI
    
    * Refactor CI
    
    * Refactor CI
    
    * Refactor CI
    
    * Add timeout for whole Jenkins job
    
    * cuda yum repository temporarily disabled

--- ci/jenkins/step/unittest.groovy
+++ ci/jenkins/step/unittest.groovy
@@ -1,5 +1,3 @@
-timeout(time: 20, unit: 'MINUTES') {
-    dir ("ci/scripts") {
-        sh "./run_unittest.sh -i ${env.MILVUS_INSTALL_PREFIX} --mysql_user=root --mysql_password=123456 --mysql_host=\$POD_IP"
-    }
+dir ("ci/scripts") {
+    sh "./run_unittest.sh -i ${env.MILVUS_INSTALL_PREFIX} --mysql_user=root --mysql_password=123456 --mysql_host=\$POD_IP"
 }
