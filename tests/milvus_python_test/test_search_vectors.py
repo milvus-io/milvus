25cb247e2d6ccfc59f41399bf6ced205451a2010
zw <zw@zilliz.com>
2020-03-21 06:13:16 +0000

    Update cases
    Signed-off-by: zw <zw@zilliz.com>

--- tests/milvus_python_test/test_search_vectors.py
+++ tests/milvus_python_test/test_search_vectors.py
@@ -675,7 +675,7 @@ class TestSearchBase:
         status, result = connect.search_vectors(substructure_collection, top_k, query_vecs, params=search_param)
         logging.getLogger().info(status)
         logging.getLogger().info(result)
-        assert abs(result[0][0].distance - min(distance_0, distance_1).astype(float)) <= epsilon
+        assert result[0] == -1
 
     def test_search_distance_superstructure_flat_index(self, connect, superstructure_collection):
         '''
@@ -701,7 +701,7 @@ class TestSearchBase:
         status, result = connect.search_vectors(superstructure_collection, top_k, query_vecs, params=search_param)
         logging.getLogger().info(status)
         logging.getLogger().info(result)
-        assert abs(result[0][0].distance - min(distance_0, distance_1).astype(float)) <= epsilon
+        assert result[0] == -1
 
     def test_search_distance_tanimoto_flat_index(self, connect, tanimoto_collection):
         '''
