971d11036f490e179ec6f53bb08a84ac1ba3454d
lichengming <chengming.li@zilliz.com>
2020-03-23 11:06:40 +0800

    fix lint error on test_hnsw.cpp
    Signed-off-by: lichengming <chengming.li@zilliz.com>

--- core/src/index/unittest/test_hnsw.cpp
+++ core/src/index/unittest/test_hnsw.cpp
@@ -9,47 +9,46 @@
 // is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
 // or implied. See the License for the specific language governing permissions and limitations under the License.
 
-#include <iostream>
 #include <knowhere/index/vector_index/IndexHNSW.h>
 #include <src/index/knowhere/knowhere/index/vector_index/helpers/IndexParameter.h>
-#include "utils.h"
+#include <iostream>
+#include <random>
+#include "./utils.h"
 
-
-int main() {
-    int64_t d = 64;                            // dimension
-    int64_t nb = 10000;                       // database size
-    int64_t nq = 10;//10000;                        // nb of queries
+int
+main() {
+    int64_t d = 64;      // dimension
+    int64_t nb = 10000;  // database size
+    int64_t nq = 10;     // 10000;                        // nb of queries
     faiss::ConcurrentBitsetPtr bitset = std::make_shared<faiss::ConcurrentBitset>(nb);
 
-    int64_t *ids = new int64_t[nb];
-    float *xb = new float[d * nb];
-    float *xq = new float[d * nq];
-//    int64_t *ids = (int64_t*)malloc(nb * sizeof(int64_t));
-//    float* xb = (float*)malloc(d * nb * sizeof(float));
-//    float* xq = (float*)malloc(d * nq * sizeof(float));
+    int64_t* ids = new int64_t[nb];
+    float* xb = new float[d * nb];
+    float* xq = new float[d * nq];
+    //    int64_t *ids = (int64_t*)malloc(nb * sizeof(int64_t));
+    //    float* xb = (float*)malloc(d * nb * sizeof(float));
+    //    float* xq = (float*)malloc(d * nq * sizeof(float));
 
-    for(int i = 0; i < nb; i++) {
-        for(int j = 0; j < d; j++)
-            xb[d * i + j] = drand48();
+    for (int i = 0; i < nb; i++) {
+        for (int j = 0; j < d; j++) xb[d * i + j] = drand48();
         xb[d * i] += i / 1000.;
         ids[i] = i;
     }
     printf("gen xb and ids done! \n");
 
-    srand((unsigned)time(NULL));
+    //    srand((unsigned)time(NULL));
+    auto random_seed = (unsigned)time(NULL);
     printf("delete ids: \n");
-    for(int i = 0; i < nq; i++) {
-        auto tmp = rand()%nb;
+    for (int i = 0; i < nq; i++) {
+        auto tmp = rand_r(&random_seed) % nb;
         printf("%ld\n", tmp);
-//        std::cout << "before delete, test result: " << bitset->test(tmp) << std::endl;
+        //        std::cout << "before delete, test result: " << bitset->test(tmp) << std::endl;
         bitset->set(tmp);
-//        std::cout << "after delete, test result: " << bitset->test(tmp) << std::endl;
-        for(int j = 0; j < d; j++)
-            xq[d * i + j] = xb[d * tmp + j];
-//        xq[d * i] += i / 1000.;
+        //        std::cout << "after delete, test result: " << bitset->test(tmp) << std::endl;
+        for (int j = 0; j < d; j++) xq[d * i + j] = xb[d * tmp + j];
+        //        xq[d * i] += i / 1000.;
     }
     printf("\n");
-
 
     int k = 4;
     int m = 16;
@@ -64,12 +63,12 @@ int main() {
     */
 
     milvus::knowhere::Config base_conf{
-            {milvus::knowhere::meta::DIM, d},
-            {milvus::knowhere::meta::TOPK, k},
-            {milvus::knowhere::IndexParams::M, m},
-            {milvus::knowhere::IndexParams::efConstruction, ef},
-            {milvus::knowhere::Metric::TYPE, milvus::knowhere::Metric::L2},
-        };
+        {milvus::knowhere::meta::DIM, d},
+        {milvus::knowhere::meta::TOPK, k},
+        {milvus::knowhere::IndexParams::M, m},
+        {milvus::knowhere::IndexParams::efConstruction, ef},
+        {milvus::knowhere::Metric::TYPE, milvus::knowhere::Metric::L2},
+    };
     milvus::knowhere::DatasetPtr query_dataset = generate_query_dataset(nq, d, (const void*)xq);
     milvus::knowhere::Config query_conf{
         {milvus::knowhere::meta::DIM, d},
@@ -79,73 +78,57 @@ int main() {
         {milvus::knowhere::Metric::TYPE, milvus::knowhere::Metric::L2},
     };
 
-    printf("start train\n");
     index.Train(base_dataset, base_conf);
-    printf("start add\n");
     index.Add(base_dataset, base_conf);
 
     printf("------------sanity check----------------\n");
-    {       // sanity check
-
+    {  // sanity check
         auto res = index.Query(query_dataset, query_conf);
         printf("Query done!\n");
-        const int64_t *I = res->Get<int64_t*>(milvus::knowhere::meta::IDS);
-        float *D = res->Get<float*>(milvus::knowhere::meta::DISTANCE);
+        const int64_t* I = res->Get<int64_t*>(milvus::knowhere::meta::IDS);
+        float* D = res->Get<float*>(milvus::knowhere::meta::DISTANCE);
 
         printf("I=\n");
-        for(int i = 0; i < 5; i++) {
-            for(int j = 0; j < k; j++)
-                printf("%5ld ", I[i * k + j]);
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < k; j++) printf("%5ld ", I[i * k + j]);
             printf("\n");
         }
 
         printf("D=\n");
-        for(int i = 0; i < 5; i++) {
-            for(int j = 0; j < k; j++)
-                printf("%7g ", D[i * k + j]);
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < k; j++) printf("%7g ", D[i * k + j]);
             printf("\n");
         }
-
     }
 
     printf("---------------search xq-------------\n");
-    {       // search xq
-
+    {  // search xq
         auto res = index.Query(query_dataset, query_conf);
-        const int64_t *I = res->Get<int64_t*>(milvus::knowhere::meta::IDS);
-
+        const int64_t* I = res->Get<int64_t*>(milvus::knowhere::meta::IDS);
 
         printf("I=\n");
-        for(int i = 0; i < nq; i++) {
-            for(int j = 0; j < k; j++)
-                printf("%5ld ", I[i * k + j]);
+        for (int i = 0; i < nq; i++) {
+            for (int j = 0; j < k; j++) printf("%5ld ", I[i * k + j]);
             printf("\n");
         }
-
     }
 
     printf("----------------search xq with delete------------\n");
-    {       // search xq with delete
-
+    {  // search xq with delete
         index.SetBlacklist(bitset);
         auto res = index.Query(query_dataset, query_conf);
         auto I = res->Get<int64_t*>(milvus::knowhere::meta::IDS);
 
         printf("I=\n");
-        for(int i = 0; i < nq; i++) {
-            for(int j = 0; j < k; j++)
-                printf("%5ld ", I[i * k + j]);
+        for (int i = 0; i < nq; i++) {
+            for (int j = 0; j < k; j++) printf("%5ld ", I[i * k + j]);
             printf("\n");
         }
-
     }
 
-
-
-    delete [] xb;
-    delete [] xq;
-    delete [] ids;
+    delete[] xb;
+    delete[] xq;
+    delete[] ids;
 
     return 0;
 }
-
