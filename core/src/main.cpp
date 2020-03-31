333d0a966ba812aebbc9681291a4245b703656f1
Jin Hai <hai.jin@zilliz.com>
2020-03-31 23:28:48 +0800

    Fix compile (#1815)
    * Update CHANGELOG.md
    
    Signed-off-by: jinhai <hai.jin@zilliz.com>
    
    * Update
    
    Signed-off-by: jinhai <hai.jin@zilliz.com>

--- core/src/main.cpp
+++ core/src/main.cpp
@@ -41,7 +41,7 @@ print_banner() {
     std::cout << "  / /|_/ // // /_| |/ / /_/ /\\ \\    " << std::endl;
     std::cout << " /_/  /_/___/____/___/\\____/___/     " << std::endl;
     std::cout << std::endl;
-    std::cout << "Welcome to Milvus!" << std::endl;
+    std::cout << "Welcome to use Milvus!" << std::endl;
     std::cout << "Milvus " << BUILD_TYPE << " version: v" << MILVUS_VERSION << ", built at " << BUILD_TIME << ", with "
 #ifdef WITH_MKL
               << "MKL"
