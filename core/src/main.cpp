2947b4739d50a5feefd2eb01917b08f0d942e216
jinhai <hai.jin@zilliz.com>
2020-03-31 23:32:02 +0800

    Update
    Signed-off-by: jinhai <hai.jin@zilliz.com>

--- core/src/main.cpp
+++ core/src/main.cpp
@@ -26,10 +26,10 @@ void
 print_help(const std::string& app_name) {
     std::cout << std::endl << "Usage: " << app_name << " [OPTIONS]" << std::endl << std::endl;
     std::cout << "  Options:" << std::endl;
-    std::cout << "   -h --help                 Print this help" << std::endl;
-    std::cout << "   -c --conf_file filename   Read configuration from the file" << std::endl;
-    std::cout << "   -d --daemon               Daemonize this application" << std::endl;
-    std::cout << "   -p --pid_file  filename   PID file used by daemonized app" << std::endl;
+    std::cout << "   -h --help                 Print this help." << std::endl;
+    std::cout << "   -c --conf_file filename   Read configuration from the file." << std::endl;
+    std::cout << "   -d --daemon               Daemonize this application." << std::endl;
+    std::cout << "   -p --pid_file  filename   PID file used by daemonized app." << std::endl;
     std::cout << std::endl;
 }
 
