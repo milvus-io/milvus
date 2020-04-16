dde6a4096a21654df01f1c176e1bf161a4847c7a
quicksilver <zhifeng.zhang@zilliz.com>
2020-04-16 09:37:29 +0800

    Implement a retry option for helm deploy failed stages in Jenkins Pipeline (#1907)

--- ci/jenkins/step/singleDevTest.groovy
+++ ci/jenkins/step/singleDevTest.groovy
@@ -3,7 +3,9 @@ dir ('milvus-helm') {
     sh 'helm repo add stable https://kubernetes.oss-cn-hangzhou.aliyuncs.com/charts'
     sh 'helm repo update'
     checkout([$class: 'GitSCM', branches: [[name: "master"]], userRemoteConfigs: [[url: "https://github.com/milvus-io/milvus-helm.git", name: 'origin', refspec: "+refs/heads/master:refs/remotes/origin/master"]]])
-    sh "helm install --wait --timeout 600s --set image.repository=registry.zilliz.com/milvus/engine --set image.tag=${DOCKER_VERSION} --set image.pullPolicy=Always --set service.type=ClusterIP -f ci/db_backend/mysql_${BINARY_VERSION}_values.yaml -f ci/filebeat/values.yaml --namespace milvus ${env.HELM_RELEASE_NAME} ."
+    retry(3) {
+        sh "helm install --wait --timeout 600s --set image.repository=registry.zilliz.com/milvus/engine --set image.tag=${DOCKER_VERSION} --set image.pullPolicy=Always --set service.type=ClusterIP -f ci/db_backend/mysql_${BINARY_VERSION}_values.yaml -f ci/filebeat/values.yaml --namespace milvus ${env.HELM_RELEASE_NAME} ."
+    }
 }
 
 dir ("tests/milvus_python_test") {
