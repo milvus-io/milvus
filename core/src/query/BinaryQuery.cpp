54fb8ef357d153c35f4ba2798a34fd83476abec0
zaqwsxqwer111 <39664031+zaqwsxqwer111@users.noreply.github.com>
2020-04-18 20:28:10 +0800

    remove pragma once from .cpp files (#1982)
    Signed-off-by: zaqwsxqwer111 <2536173042@qq.com>

--- core/src/query/BinaryQuery.cpp
+++ core/src/query/BinaryQuery.cpp
@@ -9,8 +9,6 @@
 // is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
 // or implied. See the License for the specific language governing permissions and limitations under the License.
 
-#pragma once
-
 #include <memory>
 #include <string>
 #include <utility>
