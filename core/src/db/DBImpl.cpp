a5eec9d7b49bcb11d5da2b6ba88cee0641fa4173
groot <yhmo@zeronedata.com>
2020-04-21 01:22:18 -0500

    create index notify (#2003)
    * create index notify
    
    Signed-off-by: yhmo <yihua.mo@zilliz.com>
    
    * typo
    
    Signed-off-by: yhmo <yihua.mo@zilliz.com>
    
    * typo
    
    Signed-off-by: groot <yihua.mo@zilliz.com>

--- core/src/db/DBImpl.cpp
+++ core/src/db/DBImpl.cpp
@@ -1979,6 +1979,7 @@ DBImpl::BackgroundBuildIndex() {
         }
 
         LOG_ENGINE_DEBUG_ << "Background build index thread finished";
+        index_req_swn_.Notify();  // notify CreateIndex check circle
     }
 }
 
@@ -2162,7 +2163,7 @@ DBImpl::WaitCollectionIndexRecursively(const std::string& collection_id, const C
             status = meta_ptr_->UpdateCollectionFilesToIndex(collection_id);
         }
 
-        std::this_thread::sleep_for(std::chrono::seconds(WAIT_BUILD_INDEX_INTERVAL));
+        index_req_swn_.Wait_For(std::chrono::seconds(WAIT_BUILD_INDEX_INTERVAL));
         GetFilesToBuildIndex(collection_id, file_types, collection_files);
         ++times;
 
