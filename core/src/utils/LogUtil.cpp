23ad133c14bcb8847036befbd7ed12af271de5a9
JinHai-CN <hai.jin@zilliz.com>
2020-04-23 10:45:34 +0800

    Fix lint
    Signed-off-by: JinHai-CN <hai.jin@zilliz.com>

--- core/src/utils/LogUtil.cpp
+++ core/src/utils/LogUtil.cpp
@@ -54,31 +54,31 @@ RolloutHandler(const char* filename, std::size_t size, el::Level level) {
     int ret;
     std::string m(std::string(dir) + "/" + s);
     s = m;
-    switch(level){
-    case el::Level::Debug:
-        s.append("." + std::to_string(++debug_idx));
-        ret = rename(m.c_str(), s.c_str());
-        break;
-    case el::Level::Warning:
-        s.append("." + std::to_string(++warning_idx));
-        ret = rename(m.c_str(), s.c_str());
-        break;
-    case el::Level::Trace:
-        s.append("." + std::to_string(++trace_idx));
-        ret = rename(m.c_str(), s.c_str());
-        break;
-    case el::Level::Error:
-        s.append("." + std::to_string(++error_idx));
-        ret = rename(m.c_str(), s.c_str());
-        break;
-    case el::Level::Fatal:
-        s.append("." + std::to_string(++fatal_idx));
-        ret = rename(m.c_str(), s.c_str());
-        break;
-    default:
-        s.append("." + std::to_string(++global_idx));
-        ret = rename(m.c_str(), s.c_str());
-        break;
+    switch (level) {
+        case el::Level::Debug:
+            s.append("." + std::to_string(++debug_idx));
+            ret = rename(m.c_str(), s.c_str());
+            break;
+        case el::Level::Warning:
+            s.append("." + std::to_string(++warning_idx));
+            ret = rename(m.c_str(), s.c_str());
+            break;
+        case el::Level::Trace:
+            s.append("." + std::to_string(++trace_idx));
+            ret = rename(m.c_str(), s.c_str());
+            break;
+        case el::Level::Error:
+            s.append("." + std::to_string(++error_idx));
+            ret = rename(m.c_str(), s.c_str());
+            break;
+        case el::Level::Fatal:
+            s.append("." + std::to_string(++fatal_idx));
+            ret = rename(m.c_str(), s.c_str());
+            break;
+        default:
+            s.append("." + std::to_string(++global_idx));
+            ret = rename(m.c_str(), s.c_str());
+            break;
     }
 }
 
