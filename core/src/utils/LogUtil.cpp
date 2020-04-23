f5b3187ee4d76886fd6fb80bded0bbac2beee434
Tlincy <Tlincy@users.noreply.github.com>
2020-04-23 09:35:51 +0800

    Update LogUtils.cpp: use switch ... case to replace if ... else

--- core/src/utils/LogUtil.cpp
+++ core/src/utils/LogUtil.cpp
@@ -54,27 +54,31 @@ RolloutHandler(const char* filename, std::size_t size, el::Level level) {
     int ret;
     std::string m(std::string(dir) + "/" + s);
     s = m;
-    if (level == el::Level::Global) {
-        s.append("." + std::to_string(++global_idx));
-        ret = rename(m.c_str(), s.c_str());
-    } else if (level == el::Level::Debug) {
+    switch(level){
+    case el::Level::Debug:
         s.append("." + std::to_string(++debug_idx));
         ret = rename(m.c_str(), s.c_str());
-    } else if (level == el::Level::Warning) {
+        break;
+    case el::Level::Warning:
         s.append("." + std::to_string(++warning_idx));
         ret = rename(m.c_str(), s.c_str());
-    } else if (level == el::Level::Trace) {
+        break;
+    case el::Level::Trace:
         s.append("." + std::to_string(++trace_idx));
         ret = rename(m.c_str(), s.c_str());
-    } else if (level == el::Level::Error) {
+        break;
+    case el::Level::Error:
         s.append("." + std::to_string(++error_idx));
         ret = rename(m.c_str(), s.c_str());
-    } else if (level == el::Level::Fatal) {
+        break;
+    case el::Level::Fatal:
         s.append("." + std::to_string(++fatal_idx));
         ret = rename(m.c_str(), s.c_str());
-    } else {
+        break;
+    default:
         s.append("." + std::to_string(++global_idx));
         ret = rename(m.c_str(), s.c_str());
+        break;
     }
 }
 
