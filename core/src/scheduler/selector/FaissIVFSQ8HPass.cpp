c0d5a47eba7014bca432c0974efdf332d6fdeed4
Wang XiangYu <scsven@qq.com>
2020-04-24 19:42:17 +0800

    rename optimizer to selector (#2067)
    * rename
    
    Signed-off-by: wxyu <xy.wang@zilliz.com>
    
    * fix clang-format
    
    Signed-off-by: wxyu <xy.wang@zilliz.com>

--- core/src/scheduler/selector/FaissIVFSQ8HPass.cpp
+++ core/src/scheduler/selector/FaissIVFSQ8HPass.cpp
@@ -8,8 +8,9 @@
 // Unless required by applicable law or agreed to in writing, software distributed under the License
 // is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
 // or implied. See the License for the specific language governing permissions and limitations under the License.
+
 #ifdef MILVUS_GPU_VERSION
-#include "scheduler/optimizer/FaissIVFFlatPass.h"
+#include "scheduler/selector/FaissIVFSQ8HPass.h"
 #include "cache/GpuCacheMgr.h"
 #include "config/Config.h"
 #include "scheduler/SchedInst.h"
@@ -22,50 +23,49 @@ namespace milvus {
 namespace scheduler {
 
 void
-FaissIVFFlatPass::Init() {
-#ifdef MILVUS_GPU_VERSION
+FaissIVFSQ8HPass::Init() {
     server::Config& config = server::Config::GetInstance();
     Status s = config.GetEngineConfigGpuSearchThreshold(threshold_);
     if (!s.ok()) {
-        threshold_ = std::numeric_limits<int32_t>::max();
+        threshold_ = std::numeric_limits<int64_t>::max();
     }
     s = config.GetGpuResourceConfigSearchResources(search_gpus_);
     if (!s.ok()) {
         throw std::exception();
     }
 
-    SetIdentity("FaissIVFFlatPass");
+    SetIdentity("FaissIVFSQ8HPass");
     AddGpuEnableListener();
     AddGpuSearchThresholdListener();
     AddGpuSearchResourcesListener();
-#endif
 }
 
 bool
-FaissIVFFlatPass::Run(const TaskPtr& task) {
+FaissIVFSQ8HPass::Run(const TaskPtr& task) {
     if (task->Type() != TaskType::SearchTask) {
         return false;
     }
 
     auto search_task = std::static_pointer_cast<XSearchTask>(task);
-    if (search_task->file_->engine_type_ != (int)engine::EngineType::FAISS_IVFFLAT) {
+    if (search_task->file_->engine_type_ != (int)engine::EngineType::FAISS_IVFSQ8H) {
         return false;
     }
 
     auto search_job = std::static_pointer_cast<SearchJob>(search_task->job_.lock());
     ResourcePtr res_ptr;
     if (!gpu_enable_) {
-        LOG_SERVER_DEBUG_ << LogOut("[%s][%d] FaissIVFFlatPass: gpu disable, specify cpu to search!", "search", 0);
+        LOG_SERVER_DEBUG_ << LogOut("[%s][%d] FaissIVFSQ8HPass: gpu disable, specify cpu to search!", "search", 0);
         res_ptr = ResMgrInst::GetInstance()->GetResource("cpu");
-    } else if (search_job->nq() < threshold_) {
-        LOG_SERVER_DEBUG_ << LogOut("[%s][%d] FaissIVFFlatPass: nq < gpu_search_threshold, specify cpu to search!",
+    }
+    if (search_job->nq() < threshold_) {
+        LOG_SERVER_DEBUG_ << LogOut("[%s][%d] FaissIVFSQ8HPass: nq < gpu_search_threshold, specify cpu to search!",
                                     "search", 0);
         res_ptr = ResMgrInst::GetInstance()->GetResource("cpu");
     } else {
         auto best_device_id = count_ % search_gpus_.size();
-        LOG_SERVER_DEBUG_ << LogOut("[%s][%d] FaissIVFFlatPass: nq > gpu_search_threshold, specify gpu %d to search!",
+        LOG_SERVER_DEBUG_ << LogOut("[%s][%d] FaissIVFSQ8HPass: nq > gpu_search_threshold, specify gpu %d to search!",
                                     "search", 0, best_device_id);
-        count_++;
+        ++count_;
         res_ptr = ResMgrInst::GetInstance()->GetResource(ResourceType::GPU, search_gpus_[best_device_id]);
     }
     auto label = std::make_shared<SpecResLabel>(res_ptr);
--- core/src/scheduler/selector/FaissIVFSQ8Pass.cpp
+++ core/src/scheduler/selector/FaissIVFSQ8Pass.cpp
@@ -9,7 +9,7 @@
 // is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
 // or implied. See the License for the specific language governing permissions and limitations under the License.
 #ifdef MILVUS_GPU_VERSION
-#include "scheduler/optimizer/FaissIVFSQ8Pass.h"
+#include "scheduler/selector/FaissIVFSQ8Pass.h"
 #include "cache/GpuCacheMgr.h"
 #include "config/Config.h"
 #include "scheduler/SchedInst.h"
