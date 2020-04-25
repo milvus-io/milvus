02b7d11e8e4b066420218ea237466b93673b8555
yxm1536 <62009483+yxm1536@users.noreply.github.com>
2020-04-25 16:57:14 +0800

    Rename MemMenagerFactory.cpp/h to MemManagerFactory.cpp/h (#2093)
    * Rename MemMenagerFactory.cpp to MemManagerFactory.cpp
    
    
    * Rename MemMenagerFactory.h to MemManagerFactory.h
    
    
    * Fix compilation error
    
    
    Co-authored-by: JinHai-CN <hai.jin@zilliz.com>

--- core/src/db/insert/MemManagerFactory.cpp
+++ core/src/db/insert/MemManagerFactory.cpp
@@ -9,7 +9,7 @@
 // is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
 // or implied. See the License for the specific language governing permissions and limitations under the License.
 
-#include "db/insert/MemMenagerFactory.h"
+#include "db/insert/MemManagerFactory.h"
 #include "MemManagerImpl.h"
 #include "utils/Exception.h"
 #include "utils/Log.h"
