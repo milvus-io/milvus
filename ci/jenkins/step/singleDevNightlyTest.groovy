be98cf1643deb7534b4b75d4c76509577ca4940b
quicksilver <zhifeng.zhang@zilliz.com>
2020-04-26 17:39:45 +0800

    When helm deploy failed, print the milvus container logs
    Signed-off-by: quicksilver <zhifeng.zhang@zilliz.com>

--- ci/jenkins/step/singleDevNightlyTest.groovy
+++ ci/jenkins/step/singleDevNightlyTest.groovy
@@ -9,7 +9,7 @@ timeout(time: 180, unit: 'MINUTES') {
                 sh "helm install --wait --timeout 300s --set image.repository=registry.zilliz.com/milvus/engine --set image.tag=${DOCKER_VERSION} --set image.pullPolicy=Always --set service.type=ClusterIP -f ci/db_backend/mysql_${BINARY_VERSION}_values.yaml -f ci/filebeat/values.yaml --namespace milvus ${env.HELM_RELEASE_NAME} ."
             } catch (exc) {
                 sh "helm get manifest --namespace milvus ${env.HELM_RELEASE_NAME} | kubectl describe -n milvus -f - && \
-                    kubectl logs --namespace milvus -l \"app=milvus,release=${env.HELM_RELEASE_NAME}\" -c milvus && exit 1"
+                    kubectl logs --namespace milvus -l \"app=milvus,release=${env.HELM_RELEASE_NAME}\" -c milvus"
                 def helmResult = sh script: "helm status -n milvus ${env.HELM_RELEASE_NAME}", returnStatus: true
                 if (!helmResult) {
                     sh "helm uninstall -n milvus ${env.HELM_RELEASE_NAME}"
