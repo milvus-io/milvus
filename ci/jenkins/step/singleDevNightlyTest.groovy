ce73854cc9c5ea2bd3ae2dd68e6c4c617655bd0f
quicksilver <zhifeng.zhang@zilliz.com>
2020-04-26 16:38:20 +0800

    When helm deploy failed, print the milvus container logs
    Signed-off-by: quicksilver <zhifeng.zhang@zilliz.com>

--- ci/jenkins/step/singleDevNightlyTest.groovy
+++ ci/jenkins/step/singleDevNightlyTest.groovy
@@ -6,7 +6,7 @@ timeout(time: 180, unit: 'MINUTES') {
         checkout([$class: 'GitSCM', branches: [[name: "${env.HELM_BRANCH}"]], userRemoteConfigs: [[url: "https://github.com/milvus-io/milvus-helm.git", name: 'origin', refspec: "+refs/heads/${env.HELM_BRANCH}:refs/remotes/origin/${env.HELM_BRANCH}"]]])
         retry(3) {
             sh "helm install --wait --timeout 300s --set image.repository=registry.zilliz.com/milvus/engine --set image.tag=${DOCKER_VERSION} --set image.pullPolicy=Always --set service.type=ClusterIP -f ci/db_backend/mysql_${BINARY_VERSION}_values.yaml -f ci/filebeat/values.yaml --namespace milvus ${env.HELM_RELEASE_NAME} . || \
-                helm get manifest --namespace milvus ${env.HELM_RELEASE_NAME} | kubectl describe -o wide -n milvus -f - && \
+                helm get manifest --namespace milvus ${env.HELM_RELEASE_NAME} | kubectl describe -n milvus -f - && \
                 kubectl logs --namespace milvus -l \"app=milvus,release=${env.HELM_RELEASE_NAME}\" -c milvus && exit 1"
         }
     }
@@ -27,7 +27,7 @@ timeout(time: 180, unit: 'MINUTES') {
     dir ("milvus-helm") {
         retry(3) {
             sh "helm install --wait --timeout 300s --set image.repository=registry.zilliz.com/milvus/engine --set image.tag=${DOCKER_VERSION} --set image.pullPolicy=Always --set service.type=ClusterIP -f ci/db_backend/sqlite_${BINARY_VERSION}_values.yaml -f ci/filebeat/values.yaml --namespace milvus ${env.HELM_RELEASE_NAME} . || \
-                helm get manifest --namespace milvus ${env.HELM_RELEASE_NAME} | kubectl describe -o wide -n milvus -f - && \
+                helm get manifest --namespace milvus ${env.HELM_RELEASE_NAME} | kubectl describe -n milvus -f - && \
                 kubectl logs --namespace milvus -l \"app=milvus,release=${env.HELM_RELEASE_NAME}\" -c milvus && exit 1"
         }
     }
