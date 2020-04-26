96e856ff0c07c380e304b2acba092e2674e1c4f6
yudong.cai <yudong.cai@zilliz.com>
2020-04-26 17:06:34 +0800

    #2039 add ScalarQuantizerDC_avx.cpp, ScalarQuantizerDC_avx.h and ScalarQuantizerCodec_avx.h
    Signed-off-by: yudong.cai <yudong.cai@zilliz.com>

--- core/src/index/thirdparty/faiss/FaissHook.cpp
+++ core/src/index/thirdparty/faiss/FaissHook.cpp
@@ -7,6 +7,7 @@
 #include <faiss/FaissHook.h>
 #include <faiss/impl/FaissAssert.h>
 #include <faiss/impl/ScalarQuantizerDC.h>
+#include <faiss/impl/ScalarQuantizerDC_avx.h>
 #include <faiss/impl/ScalarQuantizerDC_avx512.h>
 #include <faiss/utils/distances.h>
 #include <faiss/utils/distances_avx.h>
