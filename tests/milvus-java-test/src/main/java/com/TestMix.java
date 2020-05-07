cdc845d0d6ce6cf853bbfc4e5bac68c067795223
zw <zw@milvus.io>
2020-05-07 18:09:26 +0800

    update java tests

--- tests/milvus-java-test/src/main/java/com/TestMix.java
+++ tests/milvus-java-test/src/main/java/com/TestMix.java
@@ -15,53 +15,36 @@ import java.util.stream.Collectors;
 public class TestMix {
     private int dimension = 128;
     private int nb = 100000;
+    int nq = 10;
     int n_list = 8192;
     int n_probe = 20;
     int top_k = 10;
     double epsilon = 0.001;
     int index_file_size = 20;
+    String indexParam = "{\"nlist\":\"1024\"}";
 
-    public List<Float> normalize(List<Float> w2v){
-        float squareSum = w2v.stream().map(x -> x * x).reduce((float) 0, Float::sum);
-        final float norm = (float) Math.sqrt(squareSum);
-        w2v = w2v.stream().map(x -> x / norm).collect(Collectors.toList());
-        return w2v;
-    }
+    List<List<Float>> vectors = Utils.genVectors(nb, dimension, true);
+    List<List<Float>> queryVectors = vectors.subList(0,nq);
 
-    public List<List<Float>> gen_vectors(int nb, boolean norm) {
-        List<List<Float>> xb = new ArrayList<>();
-        Random random = new Random();
-        for (int i = 0; i < nb; ++i) {
-            List<Float> vector = new ArrayList<>();
-            for (int j = 0; j < dimension; j++) {
-                vector.add(random.nextFloat());
-            }
-            if (norm == true) {
-                vector = normalize(vector);
-            }
-            xb.add(vector);
-        }
-        return xb;
-    }
-
-    @Test(dataProvider = "Table", dataProviderClass = MainClass.class)
-    public void test_search_vectors_threads(MilvusClient client, String tableName) throws InterruptedException {
+    @Test(dataProvider = "Collection", dataProviderClass = MainClass.class)
+    public void test_search_vectors_threads(MilvusClient client, String collectionName) throws InterruptedException {
         int thread_num = 10;
         int nq = 5;
-        List<List<Float>> vectors = gen_vectors(nb, false);
-        List<List<Float>> queryVectors = vectors.subList(0,nq);
-        InsertParam insertParam = new InsertParam.Builder(tableName, vectors).build();
+        InsertParam insertParam = new InsertParam.Builder(collectionName).withFloatVectors(vectors).build();
         client.insert(insertParam);
-        Index index = new Index.Builder().withIndexType(IndexType.IVF_SQ8)
-                .withNList(n_list)
+        Index index = new Index.Builder(collectionName, IndexType.IVF_SQ8)
+                .withParamsInJson(indexParam)
                 .build();
-        CreateIndexParam createIndexParam = new CreateIndexParam.Builder(tableName).withIndex(index).build();
-        client.createIndex(createIndexParam);
+        client.createIndex(index);
         ForkJoinPool executor = new ForkJoinPool();
         for (int i = 0; i < thread_num; i++) {
             executor.execute(
                     () -> {
-                        SearchParam searchParam = new SearchParam.Builder(tableName, queryVectors).withNProbe(n_probe).withTopK(top_k).build();
+                        String params = "{\"nprobe\":\"1\"}";
+                        SearchParam searchParam = new SearchParam.Builder(collectionName)
+                                .withFloatVectors(queryVectors)
+                                .withParamsInJson(params)
+                                .withTopK(top_k).build();
                         SearchResponse res_search = client.search(searchParam);
                         assert (res_search.getResponse().ok());
                     });
@@ -71,7 +54,7 @@ public class TestMix {
     }
 
     @Test(dataProvider = "DefaultConnectArgs", dataProviderClass = MainClass.class)
-    public void test_connect_threads(String host, String port) throws ConnectFailedException {
+    public void test_connect_threads(String host, int port) throws ConnectFailedException {
         int thread_num = 100;
         ForkJoinPool executor = new ForkJoinPool();
         for (int i = 0; i < thread_num; i++) {
@@ -99,11 +82,10 @@ public class TestMix {
         executor.shutdown();
     }
 
-    @Test(dataProvider = "Table", dataProviderClass = MainClass.class)
-    public void test_add_vectors_threads(MilvusClient client, String tableName) throws InterruptedException {
+    @Test(dataProvider = "Collection", dataProviderClass = MainClass.class)
+    public void test_add_vectors_threads(MilvusClient client, String collectionName) throws InterruptedException {
         int thread_num = 10;
-        List<List<Float>> vectors = gen_vectors(nb,false);
-        InsertParam insertParam = new InsertParam.Builder(tableName, vectors).build();
+        InsertParam insertParam = new InsertParam.Builder(collectionName).withFloatVectors(vectors).build();
         ForkJoinPool executor = new ForkJoinPool();
         for (int i = 0; i < thread_num; i++) {
             executor.execute(
@@ -116,19 +98,16 @@ public class TestMix {
         executor.shutdown();
 
         Thread.sleep(2000);
-        GetTableRowCountResponse getTableRowCountResponse = client.getTableRowCount(tableName);
-        Assert.assertEquals(getTableRowCountResponse.getTableRowCount(), thread_num * nb);
+        GetCollectionRowCountResponse getCollectionRowCountResponse = client.getCollectionRowCount(collectionName);
+        Assert.assertEquals(getCollectionRowCountResponse.getCollectionRowCount(), thread_num * nb);
     }
 
-    @Test(dataProvider = "Table", dataProviderClass = MainClass.class)
-    public void test_add_vectors_partition_threads(MilvusClient client, String tableName) throws InterruptedException {
+    @Test(dataProvider = "Collection", dataProviderClass = MainClass.class)
+    public void test_add_vectors_partition_threads(MilvusClient client, String collectionName) throws InterruptedException {
         int thread_num = 10;
         String tag = RandomStringUtils.randomAlphabetic(10);
-        String partitionName = RandomStringUtils.randomAlphabetic(10);
-        io.milvus.client.Partition partition = new io.milvus.client.Partition.Builder(tableName, partitionName, tag).build();
-        client.createPartition(partition);
-        List<List<Float>> vectors = gen_vectors(nb,false);
-        InsertParam insertParam = new InsertParam.Builder(tableName, vectors).withPartitionTag(tag).build();
+        client.createPartition(collectionName, tag);
+        InsertParam insertParam = new InsertParam.Builder(collectionName).withFloatVectors(vectors).withPartitionTag(tag).build();
         ForkJoinPool executor = new ForkJoinPool();
         for (int i = 0; i < thread_num; i++) {
             executor.execute(
@@ -141,42 +120,39 @@ public class TestMix {
         executor.shutdown();
 
         Thread.sleep(2000);
-        GetTableRowCountResponse getTableRowCountResponse = client.getTableRowCount(tableName);
-        Assert.assertEquals(getTableRowCountResponse.getTableRowCount(), thread_num * nb);
+        GetCollectionRowCountResponse getCollectionRowCountResponse = client.getCollectionRowCount(collectionName);
+        Assert.assertEquals(getCollectionRowCountResponse.getCollectionRowCount(), thread_num * nb);
     }
 
-    @Test(dataProvider = "Table", dataProviderClass = MainClass.class)
-    public void test_add_index_vectors_threads(MilvusClient client, String tableName) throws InterruptedException {
+    @Test(dataProvider = "Collection", dataProviderClass = MainClass.class)
+    public void test_add_index_vectors_threads(MilvusClient client, String collectionName) throws InterruptedException {
         int thread_num = 50;
-        List<List<Float>> vectors = gen_vectors(nb,false);
-        InsertParam insertParam = new InsertParam.Builder(tableName, vectors).build();
+        InsertParam insertParam = new InsertParam.Builder(collectionName).withFloatVectors(vectors).build();
         ForkJoinPool executor = new ForkJoinPool();
         for (int i = 0; i < thread_num; i++) {
             executor.execute(
                     () -> {
                         InsertResponse res_insert = client.insert(insertParam);
-                        Index index = new Index.Builder().withIndexType(IndexType.IVF_SQ8)
-                                .withNList(n_list)
+                        Index index = new Index.Builder(collectionName, IndexType.IVF_SQ8)
+                                .withParamsInJson("{\"nlist\":\"1024\"}")
                                 .build();
-                        CreateIndexParam createIndexParam = new CreateIndexParam.Builder(tableName).withIndex(index).build();
-                        client.createIndex(createIndexParam);
+                        client.createIndex(index);
                         assert (res_insert.getResponse().ok());
                     });
         }
         executor.awaitQuiescence(300, TimeUnit.SECONDS);
         executor.shutdown();
         Thread.sleep(2000);
-        GetTableRowCountResponse getTableRowCountResponse = client.getTableRowCount(tableName);
-        Assert.assertEquals(getTableRowCountResponse.getTableRowCount(), thread_num * nb);
+        GetCollectionRowCountResponse getCollectionRowCountResponse = client.getCollectionRowCount(collectionName);
+        Assert.assertEquals(getCollectionRowCountResponse.getCollectionRowCount(), thread_num * nb);
     }
 
-    @Test(dataProvider = "Table", dataProviderClass = MainClass.class)
-    public void test_add_search_vectors_threads(MilvusClient client, String tableName) throws InterruptedException {
+    @Test(dataProvider = "Collection", dataProviderClass = MainClass.class)
+    public void test_add_search_vectors_threads(MilvusClient client, String collectionName) throws InterruptedException {
         int thread_num = 50;
         int nq = 5;
-        List<List<Float>> vectors = gen_vectors(nb, true);
         List<List<Float>> queryVectors = vectors.subList(0,nq);
-        InsertParam insertParam = new InsertParam.Builder(tableName, vectors).build();
+        InsertParam insertParam = new InsertParam.Builder(collectionName).withFloatVectors(vectors).build();
         ForkJoinPool executor = new ForkJoinPool();
         for (int i = 0; i < thread_num; i++) {
             executor.execute(
@@ -188,14 +164,17 @@ public class TestMix {
                         } catch (InterruptedException e) {
                             e.printStackTrace();
                         }
-                        SearchParam searchParam = new SearchParam.Builder(tableName, queryVectors).withNProbe(n_probe).withTopK(top_k).build();
+                        SearchParam searchParam = new SearchParam.Builder(collectionName)
+                                .withFloatVectors(queryVectors)
+                                .withParamsInJson("{\"nlist\":\"1024\"}")
+                                .withTopK(top_k).build();
                         SearchResponse res_search = client.search(searchParam);
                         assert (res_search.getResponse().ok());
                         List<List<SearchResponse.QueryResult>> res = client.search(searchParam).getQueryResultsList();
                         double distance = res.get(0).get(0).getDistance();
-                        if (tableName.startsWith("L2")) {
+                        if (collectionName.startsWith("L2")) {
                             Assert.assertEquals(distance, 0.0, epsilon);
-                        }else if (tableName.startsWith("IP")) {
+                        }else if (collectionName.startsWith("IP")) {
                             Assert.assertEquals(distance, 1.0, epsilon);
                         }
                     });
@@ -203,14 +182,13 @@ public class TestMix {
         executor.awaitQuiescence(300, TimeUnit.SECONDS);
         executor.shutdown();
         Thread.sleep(2000);
-        GetTableRowCountResponse getTableRowCountResponse = client.getTableRowCount(tableName);
-        Assert.assertEquals(getTableRowCountResponse.getTableRowCount(), thread_num * nb);
+        GetCollectionRowCountResponse getCollectionRowCountResponse = client.getCollectionRowCount(collectionName);
+        Assert.assertEquals(getCollectionRowCountResponse.getCollectionRowCount(), thread_num * nb);
     }
 
     @Test(dataProvider = "DefaultConnectArgs", dataProviderClass = MainClass.class)
-    public void test_create_insert_delete_threads(String host, String port) {
+    public void test_create_insert_delete_threads(String host, int port) {
         int thread_num = 100;
-        List<List<Float>> vectors = gen_vectors(nb,false);
         ForkJoinPool executor = new ForkJoinPool();
         for (int i = 0; i < thread_num; i++) {
             executor.execute(
@@ -226,15 +204,15 @@ public class TestMix {
                             e.printStackTrace();
                         }
                         assert(client.isConnected());
-                        String tableName = RandomStringUtils.randomAlphabetic(10);
-                        TableSchema tableSchema = new TableSchema.Builder(tableName, dimension)
+                        String collectionName = RandomStringUtils.randomAlphabetic(10);
+                        CollectionMapping tableSchema = new CollectionMapping.Builder(collectionName, dimension)
                                                                  .withIndexFileSize(index_file_size)
                                                                  .withMetricType(MetricType.IP)
                                                                  .build();
-                        client.createTable(tableSchema);
-                        InsertParam insertParam = new InsertParam.Builder(tableName, vectors).build();
+                        client.createCollection(tableSchema);
+                        InsertParam insertParam = new InsertParam.Builder(collectionName).withFloatVectors(vectors).build();
                         client.insert(insertParam);
-                        Response response = client.dropTable(tableName);
+                        Response response = client.dropCollection(collectionName);
                         Assert.assertTrue(response.ok());
                         try {
                             client.disconnect();
