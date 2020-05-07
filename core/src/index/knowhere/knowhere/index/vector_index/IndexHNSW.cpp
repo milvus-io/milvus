d3e2baac2b2d4cd12dee2b4a5abc1442824e2497
sahuang <xiaohai.xu@zilliz.com>
2020-05-07 15:19:35 +0800

    HNSW suspend
    Signed-off-by: sahuang <xiaohai.xu@zilliz.com>

--- core/src/index/knowhere/knowhere/index/vector_index/IndexHNSW.cpp
+++ core/src/index/knowhere/knowhere/index/vector_index/IndexHNSW.cpp
@@ -17,6 +17,7 @@
 #include <utility>
 #include <vector>
 
+#include "faiss/BuilderSuspend.h"
 #include "hnswlib/hnswalg.h"
 #include "hnswlib/space_ip.h"
 #include "hnswlib/space_l2.h"
@@ -123,20 +124,14 @@ IndexHNSW::Add(const DatasetPtr& dataset_ptr, const Config& config) {
     //         }
     //     }
 
-    double t0 = faiss::getmillisecs();
-    index_->addPoint(p_data, p_ids[0]);
     size_t thread_max_num = omp_get_max_threads();
 
 #pragma omp parallel for
     for (int i = 1; i < rows; ++i) {
-        if ((i / thread_max_num) % 20 == 0 && i % (thread_max_num * 20) == 0) {
-            printf("Outer duration = %.2f\n", faiss::getmillisecs() - t0);
-            t0 = faiss::getmillisecs();
+        if ((i / thread_max_num) % 200 == 0) {
+            faiss::BuilderSuspend::check_wait();
         }
-        if ((i / thread_max_num) % 20 == 0 && (i + 1) % thread_max_num == 0) {
-            printf("Inner duration = %.2f\n", faiss::getmillisecs() - t0);
-            t0 = faiss::getmillisecs();
-        }
+
         index_->addPoint(((float*)p_data + Dim() * i), p_ids[i]);
     }
 }
