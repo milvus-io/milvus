e63904867beaa18f367942f0d9a3be77f1dd2adf
Cai Yudong <yudong.cai@zilliz.com>
2020-05-10 00:19:33 +0800

    update debug log (#2275)
    Signed-off-by: yudong.cai <yudong.cai@zilliz.com>

--- core/src/scheduler/selector/FaissFlatPass.h
+++ core/src/scheduler/selector/FaissFlatPass.h
@@ -41,7 +41,7 @@ class FaissFlatPass : public Pass, public server::GpuResourceConfigHandler {
     Run(const TaskPtr& task) override;
 
  private:
-    int64_t count_ = 0;
+    int64_t idx_ = 0;
 };
 
 using FaissFlatPassPtr = std::shared_ptr<FaissFlatPass>;
