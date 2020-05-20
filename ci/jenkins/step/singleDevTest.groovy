40055fcaf03e731e89bbfc5c81d22648a3fff514
del-zhenwu <56623710+del-zhenwu@users.noreply.github.com>
2020-05-20 18:51:50 +0800

    test level 3: restart (#2384)
    * test level 3: restart
    
    Signed-off-by: zw <zw@milvus.io>
    
    * add namepsace
    
    Signed-off-by: zw <zw@milvus.io>
    
    * update
    
    Signed-off-by: zw <zw@milvus.io>
    
    * update
    
    Signed-off-by: zw <zw@milvus.io>
    
    * Update utils.py
    
    Signed-off-by: zw <zw@milvus.io>
    
    * Update test_level_3.py
    
    Signed-off-by: zw <zw@milvus.io>
    
    * Update Jenkinsfile
    
    Signed-off-by: zw <zw@milvus.io>
    
    * debug
    
    Signed-off-by: zw <zw@milvus.io>
    
    * debug
    
    Signed-off-by: zw <zw@milvus.io>
    
    * debug
    
    Signed-off-by: zw <zw@milvus.io>
    
    * debug
    
    Signed-off-by: zw <zw@milvus.io>
    
    * clean compile warning (#2380)
    
    Signed-off-by: yudong.cai <yudong.cai@zilliz.com>
    Signed-off-by: zw <zw@milvus.io>
    
    * delete pod
    
    Signed-off-by: zw <zw@milvus.io>
    
    * update
    
    Signed-off-by: zw <zw@milvus.io>
    
    * debug
    
    Signed-off-by: zw <zw@milvus.io>
    
    * debug
    
    Signed-off-by: zw <zw@milvus.io>
    
    * recover
    
    Signed-off-by: zw <zw@milvus.io>
    
    Co-authored-by: zw <zw@milvus.io>
    Co-authored-by: Cai Yudong <yudong.cai@zilliz.com>

--- ci/jenkins/step/singleDevTest.groovy
+++ ci/jenkins/step/singleDevTest.groovy
@@ -6,7 +6,7 @@ timeout(time: 120, unit: 'MINUTES') {
         checkout([$class: 'GitSCM', branches: [[name: "${env.HELM_BRANCH}"]], userRemoteConfigs: [[url: "https://github.com/milvus-io/milvus-helm.git", name: 'origin', refspec: "+refs/heads/${env.HELM_BRANCH}:refs/remotes/origin/${env.HELM_BRANCH}"]]])
         retry(3) {
             try {
-                sh "helm install --wait --timeout 300s --set image.repository=registry.zilliz.com/milvus/engine --set image.tag=${DOCKER_VERSION} --set image.pullPolicy=Always --set service.type=ClusterIP -f ci/db_backend/mysql_${BINARY_VERSION}_values.yaml -f ci/filebeat/values.yaml --namespace milvus ${env.HELM_RELEASE_NAME} ."
+                sh "helm install --wait --timeout 300s --set image.repository=registry.zilliz.com/milvus/engine --set persistence.enabled=true --set image.tag=${DOCKER_VERSION} --set image.pullPolicy=Always --set service.type=ClusterIP -f ci/db_backend/mysql_${BINARY_VERSION}_values.yaml -f ci/filebeat/values.yaml --namespace milvus ${env.HELM_RELEASE_NAME} ."
             } catch (exc) {
                 def helmStatusCMD = "helm get manifest --namespace milvus ${env.HELM_RELEASE_NAME} | kubectl describe -n milvus -f - && \
                                      kubectl logs --namespace milvus -l \"app=milvus,release=${env.HELM_RELEASE_NAME}\" -c milvus && \
@@ -22,6 +22,7 @@ timeout(time: 120, unit: 'MINUTES') {
     dir ("tests/milvus_python_test") {
         // sh 'python3 -m pip install -r requirements.txt -i http://pypi.douban.com/simple --trusted-host pypi.douban.com'
         sh 'python3 -m pip install -r requirements.txt'
-        sh "pytest . --alluredir=\"test_out/dev/single/mysql\" --level=1 --ip ${env.HELM_RELEASE_NAME}.milvus.svc.cluster.local"
+        sh "pytest . --alluredir=\"test_out/dev/single/mysql\" --level=1 --ip ${env.HELM_RELEASE_NAME}.milvus.svc.cluster.local --service ${env.HELM_RELEASE_NAME}"
+        // sh "pytest test_restart.py --alluredir=\"test_out/dev/single/mysql\" --level=3 --ip ${env.HELM_RELEASE_NAME}.milvus.svc.cluster.local --service ${env.HELM_RELEASE_NAME}"
     }
 }
