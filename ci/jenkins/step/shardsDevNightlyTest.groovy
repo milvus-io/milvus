eefc152290a4be6b37faf71f0c9e7a439d6360d2
del-zhenwu <56623710+del-zhenwu@users.noreply.github.com>
2020-05-28 00:34:28 +0800

    try connect set false  (#2443)
    * update shards deploy method
    
    Signed-off-by: zw <zw@milvus.io>
    
    * update run level
    
    Signed-off-by: zw <zw@milvus.io>
    
    * try connect set false
    
    Signed-off-by: zw <zw@milvus.io>
    
    * update shards deploy branch
    
    Signed-off-by: zw <zw@milvus.io>
    
    Co-authored-by: zw <zw@milvus.io>

--- ci/jenkins/step/shardsDevNightlyTest.groovy
+++ ci/jenkins/step/shardsDevNightlyTest.groovy
@@ -3,7 +3,8 @@ timeout(time: 180, unit: 'MINUTES') {
         sh 'helm version'
         sh 'helm repo add stable https://kubernetes.oss-cn-hangzhou.aliyuncs.com/charts'
         sh 'helm repo update'
-        checkout([$class: 'GitSCM', branches: [[name: "nightly"]], userRemoteConfigs: [[url: "https://github.com/milvus-io/milvus-helm.git", name: 'origin', refspec: "+refs/heads/nightly:refs/remotes/origin/nightly"]]])
+        checkout([$class: 'GitSCM', branches: [[name: "${env.HELM_BRANCH}"]], userRemoteConfigs: [[url: "https://github.com/milvus-io/milvus-helm.git", name: 'origin', refspec: "+refs/heads/${env.HELM_BRANCH}:refs/remotes/origin/${env.HELM_BRANCH}"]]])
+
         retry(3) {
             try {
                 sh "helm install --wait --timeout 300s --set cluster.enabled=true --set persistence.enabled=true --set image.repository=registry.zilliz.com/milvus/engine --set mishards.image.tag=test --set image.tag=${DOCKER_VERSION} --set image.pullPolicy=Always --set service.type=ClusterIP -f ci/db_backend/mysql_${BINARY_VERSION}_values.yaml --namespace milvus ${env.SHARDS_HELM_RELEASE_NAME} ."
