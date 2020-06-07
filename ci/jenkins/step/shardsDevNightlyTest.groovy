43fd7d94bf730c5fde2ee195df24b65cbe881ba7
quicksilver <zhifeng.zhang@zilliz.com>
2020-06-07 15:52:38 +0800

    Update selector.matchLabels (#2500)

--- ci/jenkins/step/shardsDevNightlyTest.groovy
+++ ci/jenkins/step/shardsDevNightlyTest.groovy
@@ -10,7 +10,9 @@ timeout(time: 180, unit: 'MINUTES') {
                 sh "helm install --wait --timeout 300s --set mishards.enabled=true --set persistence.enabled=true --set image.repository=registry.zilliz.com/milvus/engine --set mishards.image.tag=0.9.0-rc --set image.tag=${DOCKER_VERSION} --set image.pullPolicy=Always --set service.type=ClusterIP -f ci/db_backend/mysql_${BINARY_VERSION}_values.yaml --namespace milvus ${env.SHARDS_HELM_RELEASE_NAME} ."
             } catch (exc) {
                 def helmStatusCMD = "helm get manifest --namespace milvus ${env.SHARDS_HELM_RELEASE_NAME} | kubectl describe -n milvus -f - && \
-                                     kubectl logs --namespace milvus -l \"app=milvus,release=${env.SHARDS_HELM_RELEASE_NAME}\" -c milvus && \
+                                     kubectl logs --namespace milvus -l \"app.kubernetes.io/name=milvus,app.kubernetes.io/instance=${env.SHARDS_HELM_RELEASE_NAME},component=writable\" -c milvus && \
+                                     kubectl logs --namespace milvus -l \"app.kubernetes.io/name=milvus,app.kubernetes.io/instance=${env.SHARDS_HELM_RELEASE_NAME},component=readonly\" -c milvus && \
+                                     kubectl logs --namespace milvus -l \"app.kubernetes.io/name=milvus,app.kubernetes.io/instance=${env.SHARDS_HELM_RELEASE_NAME},component=mishards\" && \
                                      helm status -n milvus ${env.SHARDS_HELM_RELEASE_NAME}"
                 sh script: helmStatusCMD, returnStatus: true
                 sh script: "helm uninstall -n milvus ${env.SHARDS_HELM_RELEASE_NAME} && sleep 1m", returnStatus: true
