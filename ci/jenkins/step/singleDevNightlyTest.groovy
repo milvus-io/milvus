dd8495c2600a866206f4d9fb9ee27d96dc62a3cf
quicksilver <zhifeng.zhang@zilliz.com>
2020-06-19 17:07:30 +0800

    Improve Jenkins CI on 0.8.1  (#2623)
    * Archive dev test logs in Jenkins CI
    
    Signed-off-by: quicksilver <zhifeng.zhang@zilliz.com>
    
    * show helm deploy status when helm deploy failed
    
    Signed-off-by: quicksilver <zhifeng.zhang@zilliz.com>
    
    * [skip ci] Ignore no such image in Jenkins CI
    
    Signed-off-by: quicksilver <zhifeng.zhang@zilliz.com>

--- ci/jenkins/step/singleDevNightlyTest.groovy
+++ ci/jenkins/step/singleDevNightlyTest.groovy
@@ -3,13 +3,21 @@ dir ('milvus-helm') {
     sh 'helm repo add stable https://kubernetes.oss-cn-hangzhou.aliyuncs.com/charts'
     sh 'helm repo update'
     checkout([$class: 'GitSCM', branches: [[name: "0.8.0"]], userRemoteConfigs: [[url: "https://github.com/milvus-io/milvus-helm.git", name: 'origin', refspec: "+refs/heads/0.8.0:refs/remotes/origin/0.8.0"]]])
-    sh "helm install --wait --timeout 600s --set image.repository=registry.zilliz.com/milvus/engine --set image.tag=${DOCKER_VERSION} --set image.pullPolicy=Always --set service.type=ClusterIP -f ci/db_backend/mysql_${BINARY_VERSION}_values.yaml -f ci/filebeat/values.yaml --namespace milvus ${env.HELM_RELEASE_NAME} ."
+    try {
+        sh "helm install --wait --timeout 600s --set image.repository=registry.zilliz.com/milvus/engine --set image.tag=${DOCKER_VERSION} --set image.pullPolicy=Always --set service.type=ClusterIP -f ci/db_backend/mysql_${BINARY_VERSION}_values.yaml -f ci/filebeat/values.yaml --namespace milvus ${env.HELM_RELEASE_NAME} ."
+    } catch (exc) {
+        def helmStatusCMD = "helm get manifest --namespace milvus ${env.HELM_RELEASE_NAME} | kubectl describe -n milvus -f - && \
+                             kubectl logs --namespace milvus -l \"app=milvus,release=${env.HELM_RELEASE_NAME}\" -c milvus && \
+                             helm status -n milvus ${env.HELM_RELEASE_NAME}"
+        sh script: helmStatusCMD, returnStatus: true
+        throw exc
+    }
 }
 
 dir ("tests/milvus_python_test") {
     // sh 'python3 -m pip install -r requirements.txt -i http://pypi.douban.com/simple --trusted-host pypi.douban.com'
     sh 'python3 -m pip install -r requirements.txt'
-    sh "pytest . --alluredir=\"test_out/dev/single/mysql\" --ip ${env.HELM_RELEASE_NAME}.milvus.svc.cluster.local"
+    sh "pytest . --alluredir=\"test_out/dev/single/mysql\" --ip ${env.HELM_RELEASE_NAME}.milvus.svc.cluster.local >> ${WORKSPACE}/${env.DEV_TEST_ARTIFACTS}/milvus_mysql_dev_test.log"
 }
 // sqlite database backend test
 load "ci/jenkins/step/cleanupSingleDev.groovy"
@@ -20,8 +28,16 @@ if (!fileExists('milvus-helm')) {
     }
 }
 dir ("milvus-helm") {
-    sh "helm install --wait --timeout 300s --set image.repository=registry.zilliz.com/milvus/engine --set image.tag=${DOCKER_VERSION} --set image.pullPolicy=Always --set service.type=ClusterIP -f ci/db_backend/sqlite_${BINARY_VERSION}_values.yaml -f ci/filebeat/values.yaml --namespace milvus ${env.HELM_RELEASE_NAME} ."
+    try {
+        sh "helm install --wait --timeout 300s --set image.repository=registry.zilliz.com/milvus/engine --set image.tag=${DOCKER_VERSION} --set image.pullPolicy=Always --set service.type=ClusterIP -f ci/db_backend/sqlite_${BINARY_VERSION}_values.yaml -f ci/filebeat/values.yaml --namespace milvus ${env.HELM_RELEASE_NAME} ."
+    } catch (exc) {
+        def helmStatusCMD = "helm get manifest --namespace milvus ${env.HELM_RELEASE_NAME} | kubectl describe -n milvus -f - && \
+                             kubectl logs --namespace milvus -l \"app=milvus,release=${env.HELM_RELEASE_NAME}\" -c milvus && \
+                             helm status -n milvus ${env.HELM_RELEASE_NAME}"
+        sh script: helmStatusCMD, returnStatus: true
+        throw exc
+    }
 }
 dir ("tests/milvus_python_test") {
-    sh "pytest . --alluredir=\"test_out/dev/single/sqlite\" --ip ${env.HELM_RELEASE_NAME}.milvus.svc.cluster.local"
+    sh "pytest . --alluredir=\"test_out/dev/single/sqlite\" --ip ${env.HELM_RELEASE_NAME}.milvus.svc.cluster.local >> ${WORKSPACE}/${env.DEV_TEST_ARTIFACTS}/milvus_sqlite_dev_test.log"
 }
