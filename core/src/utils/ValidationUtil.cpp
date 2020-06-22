ea5cfb0e02e34fe454912ed71dc86bca6930f32c
cmli <chengming.li@zilliz.com>
2020-06-22 16:04:37 +0800

    fix lint error
    Signed-off-by: cmli <chengming.li@zilliz.com>

--- core/src/utils/ValidationUtil.cpp
+++ core/src/utils/ValidationUtil.cpp
@@ -317,7 +317,8 @@ ValidationUtil::ValidateSearchParams(const milvus::json& search_params,
         }
         case (int32_t)engine::EngineType::ANNOY: {
             auto status = CheckParameterRange(search_params, knowhere::IndexParams::search_k,
-                    std::numeric_limits<int64_t>::min(), std::numeric_limits<int64_t>::max());
+                                              std::numeric_limits<int64_t>::min(),
+                                              std::numeric_limits<int64_t>::max());
             if (!status.ok()) {
                 return status;
             }
