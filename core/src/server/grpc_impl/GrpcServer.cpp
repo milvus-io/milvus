65e88faca6ace173a8905daad1393ad185fbd0e9
JinHai-CN <hai.jin@zilliz.com>
2020-06-25 21:33:35 +0800

    Fix compilation error
    Signed-off-by: JinHai-CN <hai.jin@zilliz.com>

--- core/src/server/grpc_impl/GrpcServer.cpp
+++ core/src/server/grpc_impl/GrpcServer.cpp
@@ -51,8 +51,8 @@ class NoReusePortOption : public ::grpc::ServerBuilderOption {
     UpdateArguments(::grpc::ChannelArguments* args) override {
         args->SetInt(GRPC_ARG_ALLOW_REUSEPORT, 0);
         unsigned int grpc_concurrency = 4 * std::thread::hardware_concurrency();
-        grpc_concurrency = max(32, grpc_concurrency);
-        grpc_concurrency = min(256, grpc_concurrency);
+        grpc_concurrency = std::max(32, grpc_concurrency);
+        grpc_concurrency = std::min(256, grpc_concurrency);
         args->SetInt(GRPC_ARG_MAX_CONCURRENT_STREAMS, grpc_concurrency);
     }
 
