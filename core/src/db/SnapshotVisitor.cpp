94229ceb75a76fcd5d7d95cb4190b583cff38136
peng.xu <peng.xu@zilliz.com>
2020-06-27 17:37:18 +0800

    (db/snapshot): add SnapshotVistor
    Signed-off-by: peng.xu <peng.xu@zilliz.com>

---
+++ core/src/db/SnapshotVisitor.cpp
@@ -0,0 +1,31 @@
+// Copyright (C) 2019-2020 Zilliz. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance
+// with the License. You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License
+// is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+// or implied. See the License for the specific language governing permissions and limitations under the License.
+
+#include "db/SnapshotVisitor.h"
+#include "db/snapshot/Snapshots.h"
+
+namespace milvus {
+namespace engine {
+
+SnapshotVisitor::SnapshotVisitor(snapshot::ScopedSnapshotT ss)
+    : ss_(ss) {
+}
+
+SnapshotVisitor::SnapshotVisitor(const std::string& collection_name) {
+    status_ = snapshot::Snapshots::GetInstance().GetSnapshot(ss_, collection_name);
+}
+
+SnapshotVisitor::SnapshotVisitor(snapshot::ID_TYPE collection_id) {
+    status_ = snapshot::Snapshots::GetInstance().GetSnapshot(ss_, collection_id);
+}
+
+}  // namespace engine
+}  // namespace milvus
