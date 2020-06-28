359a458bfff2394ef26465b2f2d0cac6978a71a3
quicksilver <zhifeng.zhang@zilliz.com>
2020-06-28 16:25:18 +0800

    Update nightly cron (#2681)
    * Change Milvus Helm Version to v0.10.1
    
    Signed-off-by: quicksilver <zhifeng.zhang@zilliz.com>
    
    * [skip ci] update nightly cron
    
    Signed-off-by: quicksilver <zhifeng.zhang@zilliz.com>
    
    * [skip ci] update nightly cron
    
    Signed-off-by: quicksilver <zhifeng.zhang@zilliz.com>

--- ci/jenkins/step/singleDevNightlyTest.groovy
+++ ci/jenkins/step/singleDevNightlyTest.groovy
@@ -37,20 +37,20 @@ timeout(time: 180, unit: 'MINUTES') {
             checkout([$class: 'GitSCM', branches: [[name:"${env.HELM_BRANCH}"]], userRemoteConfigs: [[url: "https://github.com/milvus-io/milvus-helm.git", name: 'origin', refspec: "+refs/heads/${env.HELM_BRANCH}:refs/remotes/origin/${env.HELM_BRANCH}"]]])
         }
     }
-    dir ("milvus-helm") {
-        retry(3) {
-            try {
+    retry(3) {
+        try {
+            dir ("milvus-helm/charts/milvus") {
                 sh "helm install --wait --timeout 300s --set image.repository=registry.zilliz.com/milvus/engine --set image.tag=${DOCKER_VERSION} --set image.pullPolicy=Always --set service.type=ClusterIP -f ci/db_backend/sqlite_${BINARY_VERSION}_values.yaml -f ci/filebeat/values.yaml --namespace milvus ${env.HELM_RELEASE_NAME} ."
-            } catch (exc) {
-                def helmStatusCMD = "helm get manifest --namespace milvus ${env.HELM_RELEASE_NAME} | kubectl describe -n milvus -f - && \
-                                     kubectl logs --namespace milvus -l \"app=milvus,release=${env.HELM_RELEASE_NAME}\" -c milvus && \
-                                     helm status -n milvus ${env.HELM_RELEASE_NAME}"
-                def helmResult = sh script: helmStatusCMD, returnStatus: true
-                if (!helmResult) {
-                    sh "helm uninstall -n milvus ${env.HELM_RELEASE_NAME} && sleep 1m"
-                }
-                throw exc
             }
+        } catch (exc) {
+            def helmStatusCMD = "helm get manifest --namespace milvus ${env.HELM_RELEASE_NAME} | kubectl describe -n milvus -f - && \
+                                 kubectl logs --namespace milvus -l \"app=milvus,release=${env.HELM_RELEASE_NAME}\" -c milvus && \
+                                 helm status -n milvus ${env.HELM_RELEASE_NAME}"
+            def helmResult = sh script: helmStatusCMD, returnStatus: true
+            if (!helmResult) {
+                sh "helm uninstall -n milvus ${env.HELM_RELEASE_NAME} && sleep 1m"
+            }
+            throw exc
         }
     }
     dir ("tests/milvus_python_test") {
