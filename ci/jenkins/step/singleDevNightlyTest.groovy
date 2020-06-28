8e09c29f2a64db36eed316066f4b0754621d6fe8
quicksilver <zhifeng.zhang@zilliz.com>
2020-06-28 16:18:05 +0800

    [skip ci] update nightly cron  (#2680)
    * Change Milvus Helm Version to v0.10.1
    
    Signed-off-by: quicksilver <zhifeng.zhang@zilliz.com>
    
    * [skip ci] update nightly cron
    
    Signed-off-by: quicksilver <zhifeng.zhang@zilliz.com>

--- ci/jenkins/step/singleDevNightlyTest.groovy
+++ ci/jenkins/step/singleDevNightlyTest.groovy
@@ -32,7 +32,7 @@ timeout(time: 180, unit: 'MINUTES') {
     // sqlite database backend test
     load "ci/jenkins/step/cleanupSingleDev.groovy"
     
-    if (!fileExists('milvus-helm')) {
+    if (!fileExists('milvus-helm/charts/milvus')) {
         dir ("milvus-helm") {
             checkout([$class: 'GitSCM', branches: [[name:"${env.HELM_BRANCH}"]], userRemoteConfigs: [[url: "https://github.com/milvus-io/milvus-helm.git", name: 'origin', refspec: "+refs/heads/${env.HELM_BRANCH}:refs/remotes/origin/${env.HELM_BRANCH}"]]])
         }
