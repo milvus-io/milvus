7d567788f96edd207c5d059f4e509ad3b8857606
yangxuan <jumpthepig@gmail.com>
2020-07-09 18:12:45 +0800

    Add python tests about param timeout
    Signed-off-by: yangxuan <jumpthepig@gmail.com>

--- tests/milvus_python_test/test_ping.py
+++ tests/milvus_python_test/test_ping.py
@@ -9,7 +9,7 @@ class TestPing:
         '''
         target: test get the server version
         method: call the server_version method after connected
-        expected: version should be the pymilvus version
+        expected: version should be the milvus version
         '''
         status, res = connect.server_version()
         assert res == __version__
@@ -54,6 +54,68 @@ class TestPing:
         assert connect
 
 
+class TestPingWithTimeout:
+    def test_server_version_legal_timeout(self, connect):
+        '''
+        target: test get the server version with legal timeout
+        method: call the server_version method after connected with altering timeout
+        expected: version should be the milvus version
+        '''
+        status, res = connect.server_version(20)
+        assert res == __version__
+
+    @pytest.mark.skip("This case is currently under fix")
+    def test_server_version_negative_timeout(self, connect):
+        '''
+        target: test get the server version with negative timeout
+        method: call the server_version method after connected with altering timeout
+        expected: when timeout is illegal raises an error;
+        '''
+        status = None
+        with pytest.raises(Exception) as e:
+            status, res = connect.server_version(-1)
+        assert status is None
+
+    def test_server_cmd_with_params_version_with_legal_timeout(self, connect):
+        '''
+        target: test cmd: version and timeout
+        method: cmd = "version" , timeout=10
+        expected: when cmd = 'version', return version of server;
+        '''
+        cmd = "version"
+        status, msg = connect._cmd(cmd, 10)
+        logging.getLogger().info(status)
+        logging.getLogger().info(msg)
+        assert status.OK()
+        assert msg == __version__
+
+    @pytest.mark.skip("This case is currently under fix")
+    def test_server_cmd_with_params_version_with_illegal_timeout(self, connect):
+        '''
+        target: test cmd: version and timeout
+        method: cmd = "version" , timeout=-1
+        expected: when timeout is illegal raises an error; 
+        '''
+        cmd = "version"
+        status = None
+        with pytest.raises(Exception) as e:
+            status, res = connect.server_version(-1)
+        assert status is None
+
+    @pytest.mark.skip("This case is currently under fix")
+    def test_server_cmd_with_params_others_with_illegal_timeout(self, connect):
+        '''
+        target: test cmd: lalala, timeout = -1
+        method: cmd = "lalala", timeout = -1
+        expected: when timeout is illegal raises an error;
+        '''
+        cmd = "rm -rf test"
+        status = None
+        with pytest.raises(Exception) as e:
+            status, msg = connect._cmd(cmd, -1)
+        assert status is None
+
+
 class TestPingDisconnect:
     def test_server_version(self, dis_connect):
         '''
@@ -75,4 +137,15 @@ class TestPingDisconnect:
         status = None
         with pytest.raises(Exception) as e:
             status, msg = connect.server_status()
+        assert status is None
+
+    def test_server_version_with_timeout(self, dis_connect):
+        '''
+        target: test get the server status with timeout settings after disconnect
+        method: call the server_status method after connected
+        expected: status returned should be not ok
+        '''
+        status = None
+        with pytest.raises(Exception) as e:
+            status, msg = connect.server_status(100)
         assert status is None
