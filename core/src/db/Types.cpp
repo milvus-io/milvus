cfdd833d18c315dc9ded77af97cc865594e76ac7
Cai Yudong <yudong.cai@zilliz.com>
2020-07-11 21:26:31 +0800

    snapshot integrate (#2814)
    * update HybridQuery
    
    Signed-off-by: yudong.cai <yudong.cai@zilliz.com>
    
    * add IterateHandler
    
    Signed-off-by: yudong.cai <yudong.cai@zilliz.com>
    
    * IterateHandler opt
    
    Signed-off-by: yudong.cai <yudong.cai@zilliz.com>
    
    * opt
    
    Signed-off-by: yudong.cai <yudong.cai@zilliz.com>
    
    * opt
    
    Signed-off-by: yudong.cai <yudong.cai@zilliz.com>
    
    * add SSSegmentReader
    
    Signed-off-by: yudong.cai <yudong.cai@zilliz.com>
    
    * add src/codecs/snapshot
    
    Signed-off-by: yudong.cai <yudong.cai@zilliz.com>
    
    * update latest code
    
    Signed-off-by: yudong.cai <yudong.cai@zilliz.com>
    
    * update SSSegmentReader interface
    
    Signed-off-by: yudong.cai <yudong.cai@zilliz.com>
    
    * add SSSegmentWriter
    
    Signed-off-by: yudong.cai <yudong.cai@zilliz.com>
    
    * update SSSegmentWriter interface
    
    Signed-off-by: yudong.cai <yudong.cai@zilliz.com>
    
    * update unittest
    
    Signed-off-by: yudong.cai <yudong.cai@zilliz.com>
    
    * fix clang-format
    
    Signed-off-by: yudong.cai <yudong.cai@zilliz.com>
    
    * fix unittest/db build fail
    
    Signed-off-by: yudong.cai <yudong.cai@zilliz.com>
    
    * use SSSegmentReader and SSSegmentWriter in SSDBImpl
    
    Signed-off-by: yudong.cai <yudong.cai@zilliz.com>
    
    * fix clang-format
    
    Signed-off-by: yudong.cai <yudong.cai@zilliz.com>
    
    * fix build issue
    
    Signed-off-by: yudong.cai <yudong.cai@zilliz.com>

--- core/src/db/Types.cpp
+++ core/src/db/Types.cpp
@@ -9,29 +9,14 @@
 // is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
 // or implied. See the License for the specific language governing permissions and limitations under the License.
 
-#pragma once
-
-#include <set>
-#include <string>
-#include <vector>
-
-#include "db/meta/MetaTypes.h"
+#include "db/Types.h"
 
 namespace milvus {
 namespace engine {
-namespace snapshot {
 
-using ID_TYPE = int64_t;
-using NUM_TYPE = int64_t;
-using FTYPE_TYPE = int64_t;
-using TS_TYPE = int64_t;
-using LSN_TYPE = int64_t;
-using SIZE_TYPE = uint64_t;
-using MappingT = std::set<ID_TYPE>;
-using IDS_TYPE = std::vector<ID_TYPE>;
+const char* DEFAULT_UID_NAME = "_uid";
+const char* DEFAULT_BLOOM_FILTER_NAME = "_blf";
+const char* DEFAULT_DELETED_DOCS_NAME = "_del";
 
-enum State { PENDING = 0, ACTIVE = 1, DEACTIVE = 2, INVALID = 999 };
-
-}  // namespace snapshot
 }  // namespace engine
 }  // namespace milvus
---
+++ core/src/segment/SSSegmentReader.h
@@ -0,0 +1,78 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+#pragma once
+
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "codecs/Codec.h"
+#include "db/SnapshotVisitor.h"
+#include "segment/Types.h"
+#include "storage/FSHandler.h"
+#include "utils/Status.h"
+
+namespace milvus {
+namespace segment {
+
+class SSSegmentReader {
+ public:
+    explicit SSSegmentReader(const engine::SegmentVisitorPtr& segment_visitor);
+
+    // TODO(zhiru)
+    Status
+    LoadCache(bool& in_cache);
+
+    Status
+    Load();
+
+    Status
+    LoadVectors(const std::string& file_path, off_t offset, size_t num_bytes, std::vector<uint8_t>& raw_vectors);
+
+    Status
+    LoadAttrs(const std::string& field_name, off_t offset, size_t num_bytes, std::vector<uint8_t>& raw_attrs);
+
+    Status
+    LoadUids(const std::string& file_path, std::vector<doc_id_t>& uids);
+
+    Status
+    LoadVectorIndex(const std::string& location, codec::ExternalData external_data,
+                    segment::VectorIndexPtr& vector_index_ptr);
+
+    Status
+    LoadBloomFilter(const std::string file_path, segment::IdBloomFilterPtr& id_bloom_filter_ptr);
+
+    Status
+    LoadDeletedDocs(const std::string& file_path, segment::DeletedDocsPtr& deleted_docs_ptr);
+
+    Status
+    GetSegment(SegmentPtr& segment_ptr);
+
+    Status
+    ReadDeletedDocsSize(size_t& size);
+
+ private:
+    engine::SegmentVisitorPtr segment_visitor_;
+    storage::FSHandlerPtr fs_ptr_;
+    SegmentPtr segment_ptr_;
+};
+
+using SSSegmentReaderPtr = std::shared_ptr<SSSegmentReader>;
+
+}  // namespace segment
+}  // namespace milvus
