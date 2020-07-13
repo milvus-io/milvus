8adca04bce044c2b6bb584a9a5ab955717be66da
fishpenguin <kun.yu@zilliz.com>
2020-07-13 14:17:33 +0800

    Change limit_in_bytes init value to max
    Signed-off-by: fishpenguin <kun.yu@zilliz.com>

--- core/src/config/Config.cpp
+++ core/src/config/Config.cpp
@@ -16,6 +16,7 @@
 #include <chrono>
 #include <fstream>
 #include <iostream>
+#include <limits>
 #include <regex>
 #include <string>
 #include <thread>
@@ -1334,7 +1335,7 @@ Config::CheckCacheConfigCpuCacheCapacity(const std::string& value) {
         int64_t total_mem = 0, free_mem = 0;
         CommonUtil::GetSystemMemInfo(total_mem, free_mem);
 
-        int64_t cgroup_limit_mem = 0;
+        int64_t cgroup_limit_mem = std::numeric_limits<int64_t>::max();
         CommonUtil::GetSysCgroupMemLimit(cgroup_limit_mem);
         if (cgroup_limit_mem < total_mem && cache_size >= cgroup_limit_mem) {
             std::string msg = "Invalid cpu cache size: " + value +
