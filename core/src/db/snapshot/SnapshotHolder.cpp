482bc539500c0dcf27bc043bf64b746ff0dff5d0
XuPeng-SH <xupeng3112@163.com>
2020-07-18 19:30:45 +0800

    (db/snapshot): Update Store (#2897)
    * (db/snapshot): update for row count
    
    Signed-off-by: peng.xu <peng.xu@zilliz.com>
    
    * (db/snapshot): fix bug in NewSegmentOperation
    
    Signed-off-by: peng.xu <peng.xu@zilliz.com>
    
    * (db/snapshot): remove dummy print
    
    Signed-off-by: peng.xu <peng.xu@zilliz.com>
    
    * (db/snapshot): Add some test for row count
    
    Signed-off-by: peng.xu <peng.xu@zilliz.com>
    
    * (db/snapshot): update size logic
    
    Signed-off-by: peng.xu <peng.xu@zilliz.com>
    
    * (db/snapshot): update size logic related ut
    
    Signed-off-by: peng.xu <peng.xu@zilliz.com>
    
    * (db/snapshot): rollback if operation is not done
    
    Signed-off-by: peng.xu <peng.xu@zilliz.com>
    
    * (db/snapshot): clean store
    
    Signed-off-by: peng.xu <peng.xu@zilliz.com>
    
    * (db/snapshot): remove some dependency
    
    Signed-off-by: peng.xu <peng.xu@zilliz.com>
    
    * (db/snapshot): update for store
    
    Signed-off-by: peng.xu <peng.xu@zilliz.com>
    
    * (db/snapshot): update Store.h
    
    Signed-off-by: peng.xu <peng.xu@zilliz.com>
    
    * (db/snapshot): update store related code
    
    Signed-off-by: peng.xu <peng.xu@zilliz.com>
    
    * (db/snapshot): add field element modification operation
    
    Signed-off-by: peng.xu <peng.xu@zilliz.com>
    
    * (db/snapshot): change new operation name
    
    Signed-off-by: peng.xu <peng.xu@zilliz.com>
    
    * (db/snapshot): fix lint error
    
    Signed-off-by: peng.xu <peng.xu@zilliz.com>

--- core/src/db/snapshot/SnapshotHolder.cpp
+++ core/src/db/snapshot/SnapshotHolder.cpp
@@ -36,14 +36,14 @@ SnapshotHolder::~SnapshotHolder() {
 }
 
 Status
-SnapshotHolder::Load(Store& store, ScopedSnapshotT& ss, ID_TYPE id, bool scoped) {
+SnapshotHolder::Load(StorePtr store, ScopedSnapshotT& ss, ID_TYPE id, bool scoped) {
     Status status;
     if (id > max_id_) {
         CollectionCommitPtr cc;
         status = LoadNoLock(id, cc, store);
         if (!status.ok())
             return status;
-        status = Add(id);
+        status = Add(store, id);
         if (!status.ok())
             return status;
     }
@@ -121,7 +121,7 @@ SnapshotHolder::IsActive(Snapshot::Ptr& ss) {
 }
 
 Status
-SnapshotHolder::Add(ID_TYPE id) {
+SnapshotHolder::Add(StorePtr store, ID_TYPE id) {
     Status status;
     {
         std::unique_lock<std::mutex> lock(mutex_);
@@ -140,7 +140,7 @@ SnapshotHolder::Add(ID_TYPE id) {
     }
     Snapshot::Ptr oldest_ss;
     {
-        auto ss = std::make_shared<Snapshot>(id);
+        auto ss = std::make_shared<Snapshot>(store, id);
 
         std::unique_lock<std::mutex> lock(mutex_);
         if (!IsActive(ss)) {
@@ -173,7 +173,7 @@ SnapshotHolder::Add(ID_TYPE id) {
 }
 
 Status
-SnapshotHolder::LoadNoLock(ID_TYPE collection_commit_id, CollectionCommitPtr& cc, Store& store) {
+SnapshotHolder::LoadNoLock(ID_TYPE collection_commit_id, CollectionCommitPtr& cc, StorePtr store) {
     assert(collection_commit_id > max_id_);
     LoadOperationContext context;
     context.id = collection_commit_id;
