8ae04e3e818728b38dfb0533a3269ce62df9dbea
Wang Xiangyu <xy.wang@zilliz.com>
2020-08-06 00:34:26 +0800

    add commit
    Signed-off-by: Wang Xiangyu <xy.wang@zilliz.com>

--- core/src/log/LogMgr.h
+++ core/src/log/LogMgr.h
@@ -21,6 +21,7 @@ namespace milvus {
 
 class LogMgr {
  public:
+    /* InitLog */
     static Status
     InitLog(bool trace_enable, bool debug_enable, bool info_enable, bool warning_enable, bool error_enable,
             bool fatal_enable, const std::string& logs_path, int64_t max_log_file_size, int64_t delete_exceeds);
