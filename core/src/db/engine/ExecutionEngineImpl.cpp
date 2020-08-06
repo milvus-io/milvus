166c0084511fae8c8fc069d1ff8e29536358ff1f
yukun <kun.yu@zilliz.com>
2020-08-06 19:08:42 +0800

    Fix metric_type bug in search (#3155)
    * Fix dsl test case nb bug
    
    
    * Fix dsl test case bug
    
    
    * Add metric_type judge in search
    
    
    * Fix test_search.py
    
    
    * Fix test_db
    
    
    * Fix search metric_type
    
    
    * ci retry
    
    
    Co-authored-by: Wang Xiangyu <xy.wang@zilliz.com>

--- core/src/db/engine/ExecutionEngineImpl.cpp
+++ core/src/db/engine/ExecutionEngineImpl.cpp
@@ -269,6 +269,7 @@ ExecutionEngineImpl::VecSearch(milvus::engine::ExecutionEngineContext& context,
 
     milvus::json conf = vector_param->extra_params;
     conf[knowhere::meta::TOPK] = topk;
+    conf[knowhere::Metric::TYPE] = vector_param->metric_type;
     auto adapter = knowhere::AdapterMgr::GetInstance().GetAdapter(vec_index->index_type());
     if (!adapter->CheckSearch(conf, vec_index->index_type(), vec_index->index_mode())) {
         LOG_ENGINE_ERROR_ << LogOut("[%s][%ld] Illegal search params", "search", 0);
@@ -498,6 +499,7 @@ ExecutionEngineImpl::ProcessTermQuery(faiss::ConcurrentBitsetPtr& bitset, const 
                                       std::unordered_map<std::string, DataType>& attr_type) {
     auto status = Status::OK();
     auto term_query_json = term_query->json_obj;
+    JSON_NULL_CHECK(term_query_json);
     auto term_it = term_query_json.begin();
     if (term_it != term_query_json.end()) {
         const std::string& field_name = term_it.key();
@@ -578,6 +580,7 @@ ExecutionEngineImpl::ProcessRangeQuery(const std::unordered_map<std::string, Dat
 
     auto status = Status::OK();
     auto range_query_json = range_query->json_obj;
+    JSON_NULL_CHECK(range_query_json);
     auto range_it = range_query_json.begin();
     if (range_it != range_query_json.end()) {
         const std::string& field_name = range_it.key();
