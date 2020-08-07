0c80d7f6c5a91975c7bfd5deb98be1f02bf0a6d2
quicksilver <zhifeng.zhang@zilliz.com>
2020-08-07 16:13:49 +0800

    Use ccache with Make (#3164)
    * using ccache if defined
    
    Signed-off-by: quicksilver <zhifeng.zhang@zilliz.com>
    
    * using ccache if defined
    
    Signed-off-by: quicksilver <zhifeng.zhang@zilliz.com>
    
    * using ccache if defined
    
    Signed-off-by: quicksilver <zhifeng.zhang@zilliz.com>
    
    * using ccache if defined
    
    Signed-off-by: quicksilver <zhifeng.zhang@zilliz.com>
    
    * using ccache if defined
    
    Signed-off-by: quicksilver <zhifeng.zhang@zilliz.com>
    
    * using ccache if defined
    
    Signed-off-by: quicksilver <zhifeng.zhang@zilliz.com>
    
    * using ccache if defined
    
    Signed-off-by: quicksilver <zhifeng.zhang@zilliz.com>
    
    * using ccache if defined
    
    Signed-off-by: jeffoverflow <zhifeng.zhang@zilliz.com>
    
    * call "ccache -z" before build start, call "ccache -s" after build finish
    
    Signed-off-by: quicksilver <zhifeng.zhang@zilliz.com>
    Signed-off-by: zongyufen <zongyufen@foxmail.com>

--- core/src/index/cmake/BuildUtilsCore.cmake
+++ core/src/index/cmake/BuildUtilsCore.cmake
@@ -202,3 +202,17 @@ function(ADD_THIRDPARTY_LIB LIB_NAME)
         message(FATAL_ERROR "No static or shared library provided for ${LIB_NAME}")
     endif ()
 endfunction()
+
+MACRO(using_ccache_if_defined KNOWHERE_USE_CCACHE)
+    if (MILVUS_USE_CCACHE)
+        find_program(CCACHE_FOUND ccache)
+        if (CCACHE_FOUND)
+            message(STATUS "Using ccache: ${CCACHE_FOUND}")
+            set_property(GLOBAL PROPERTY RULE_LAUNCH_COMPILE ${CCACHE_FOUND})
+            set_property(GLOBAL PROPERTY RULE_LAUNCH_LINK ${CCACHE_FOUND})
+            # let ccache preserve C++ comments, because some of them may be
+            # meaningful to the compiler
+            set(ENV{CCACHE_COMMENTS} "1")
+        endif (CCACHE_FOUND)
+    endif ()
+ENDMACRO(using_ccache_if_defined)
