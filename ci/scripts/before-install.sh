415157ba01c32351bef1b0bbc7323b8388355a3d
quicksilver <zhifeng.zhang@zilliz.com>
2020-08-07 21:55:51 +0800

    ccache has support for GCC’s precompiled headers
    Signed-off-by: quicksilver <zhifeng.zhang@zilliz.com>

--- ci/scripts/before-install.sh
+++ ci/scripts/before-install.sh
@@ -5,7 +5,7 @@ set -ex
 export CCACHE_COMPRESS=1
 export CCACHE_COMPRESSLEVEL=5
 export CCACHE_COMPILERCHECK=content
-export CCACHE_SLOPPINESS=file_stat_matches,file_stat_matches_ctime,time_macros,include_file_mtime,include_file_ctime,file_stat_matches
+export CCACHE_SLOPPINESS=file_stat_matches,file_stat_matches_ctime,pch_defines,time_macros,include_file_mtime,include_file_ctime
 export PATH=/usr/lib/ccache/:$PATH
 
 set +ex
