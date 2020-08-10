826ead1bf3f547ddbddb85dde2c7ad285eeda50b
quicksilver <zhifeng.zhang@zilliz.com>
2020-08-10 08:41:03 +0800

    Modefied CCACHE_SLOPPINESS options
    Signed-off-by: quicksilver <zhifeng.zhang@zilliz.com>

--- ci/scripts/before-install.sh
+++ ci/scripts/before-install.sh
@@ -7,5 +7,6 @@ export CCACHE_COMPRESSLEVEL=5
 export CCACHE_COMPILERCHECK=content
 export PATH=/usr/lib/ccache/:$PATH
 export CORE_BUILD_DIR="$HOME/milvus_build"
+export CCACHE_BASEDIR="$HOME/milvus_build"
 
 set +ex
