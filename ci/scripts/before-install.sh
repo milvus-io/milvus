8b915396492fed548f05d702ddd5fbaed734f350
quicksilver <zhifeng.zhang@zilliz.com>
2020-08-10 13:56:17 +0800

    Modified workingDir option to Kubernetes Agent
    Signed-off-by: quicksilver <zhifeng.zhang@zilliz.com>

--- ci/scripts/before-install.sh
+++ ci/scripts/before-install.sh
@@ -6,7 +6,6 @@ export CCACHE_COMPRESS=1
 export CCACHE_COMPRESSLEVEL=5
 export CCACHE_COMPILERCHECK=content
 export PATH=/usr/lib/ccache/:$PATH
-export CORE_BUILD_DIR="$HOME/milvus_build"
-export CCACHE_BASEDIR="$HOME/milvus_build"
+export CCACHE_BASEDIR=${WORKSPACE:=""}
 
 set +ex
