8a73fb054c21f12709c989ad911af2cb3f534b94
quicksilver <zhifeng.zhang@zilliz.com>
2020-08-12 15:17:26 +0800

    Update Jenkinsfile
    Signed-off-by: quicksilver <zhifeng.zhang@zilliz.com>

--- ci/jenkins/step/build.groovy
+++ ci/jenkins/step/build.groovy
@@ -1,12 +1,12 @@
 timeout(time: 120, unit: 'MINUTES') {
     dir ("ci/scripts") {
         withCredentials([usernamePassword(credentialsId: "${params.JFROG_CREDENTIALS_ID}", usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
-            if (!env.IS_NIGHTLY_TEST) {
+            if (!isTimeTriggeredBuild()) {
                 def checkResult = sh(script: "./check_ccache.sh -l ${params.JFROG_ARTFACTORY_URL}/ccache", returnStatus: true)
             }
     
             if ("${BINARY_VERSION}" == "gpu") {
-                if (IS_NIGHTLY_TEST) {
+                if (isTimeTriggeredBuild()) {
                     sh "/bin/bash --login -c \". ./before-install.sh && ./build.sh -t ${params.BUILD_TYPE} -j4 -i ${env.MILVUS_INSTALL_PREFIX} --with_fiu --coverage -l -g -u -s '-gencode=arch=compute_61,code=sm_61;-gencode=arch=compute_75,code=sm_75' \""
                 } else {
                     sh "/bin/bash --login -c \". ./before-install.sh && ./build.sh -t ${params.BUILD_TYPE} -j4 -i ${env.MILVUS_INSTALL_PREFIX} --with_fiu --coverage -l -g -u\""
@@ -17,4 +17,11 @@ timeout(time: 120, unit: 'MINUTES') {
             sh "./update_ccache.sh -l ${params.JFROG_ARTFACTORY_URL}/ccache -u ${USERNAME} -p ${PASSWORD}"
         }
     }
+}
+
+boolean isTimeTriggeredBuild() {
+    if (currentBuild.getBuildCauses('hudson.triggers.TimerTrigger$TimerTriggerCause').size() != 0) {
+        return true
+    }
+    return false
 }
