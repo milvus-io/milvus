f498b7732e24ea39f38fdeaa111a9c095adbb99b
zw <zw@milvus.io>
2020-08-13 18:41:36 +0800

    remove douban pip during install python-lib
    Signed-off-by: zw <zw@milvus.io>

--- tests/milvus_python_test/entity/test_search.py
+++ tests/milvus_python_test/entity/test_search.py
@@ -218,7 +218,7 @@ class TestSearchBase:
         '''
         target: test search with different metric_type
         method: build index with L2, and search using IP
-        expected: search ok if flat index, search failed if other indexes 
+        expected: search ok
         '''
         search_metric_type = "IP"
         index_type = get_simple_index["index_type"]
@@ -226,13 +226,9 @@ class TestSearchBase:
         connect.create_index(collection, field_name, get_simple_index)
         search_param = get_search_param(index_type)
         query, vecs = gen_query_vectors(field_name, entities, top_k, nq, metric_type=search_metric_type, search_params=search_param)
-        if index_type != "FLAT":
-            res = connect.search(collection, query)
-            assert len(res) == nq
-            assert len(res[0]) == top_k
-        else:
-            with pytest.raises(Exception) as e:
-                res = connect.search(collection, query)
+        res = connect.search(collection, query)
+        assert len(res) == nq
+        assert len(res[0]) == top_k
 
     @pytest.mark.level(2)
     def test_search_index_partition(self, connect, collection, get_simple_index, get_top_k, get_nq):
