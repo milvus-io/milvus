f2b91352a711777f974a7f77ebe44a6956047157
zongyufen <zongyufen@foxmail.com>
2020-08-17 15:36:56 +0800

    update case with invalid ranges
    Signed-off-by: zongyufen <zongyufen@foxmail.com>

--- tests/milvus_python_test/entity/test_search.py
+++ tests/milvus_python_test/entity/test_search.py
@@ -225,7 +225,8 @@ class TestSearchBase:
         entities, ids = init_data(connect, collection)
         connect.create_index(collection, field_name, get_simple_index)
         search_param = get_search_param(index_type)
-        query, vecs = gen_query_vectors(field_name, entities, top_k, nq, metric_type=search_metric_type, search_params=search_param)
+        query, vecs = gen_query_vectors(field_name, entities, top_k, nq, metric_type=search_metric_type,
+                                        search_params=search_param)
         res = connect.search(collection, query)
         assert len(res) == nq
         assert len(res[0]) == top_k
@@ -1151,27 +1152,33 @@ class TestSearchDSL(object):
         with pytest.raises(Exception) as e:
             res = connect.search(collection, query)
 
-    @pytest.fixture(
-        scope="function",
-        params=gen_invalid_ranges()
-    )
-    def get_invalid_ranges(self, request):
-        return request.param
-
     @pytest.mark.level(2)
-    def test_query_range_invalid_ranges(self, connect, collection, get_invalid_ranges):
+    def test_query_range_string_ranges(self, connect, collection):
         '''
         method: build query with invalid ranges
         expected: raise Exception
         '''
         entities, ids = init_data(connect, collection)
-        ranges = get_invalid_ranges
+        ranges = {"GT": "0", "LT": "1000"}
         range = gen_default_range_expr(ranges=ranges)
         expr = {"must": [gen_default_vector_expr(default_query), range]}
         query = update_query_expr(default_query, expr=expr)
-        logging.getLogger().info(query)
         with pytest.raises(Exception) as e:
             res = connect.search(collection, query)
+
+    @pytest.mark.level(2)
+    def test_query_range_invalid_ranges(self, connect, collection):
+        '''
+        method: build query with invalid ranges
+        expected: 0
+        '''
+        entities, ids = init_data(connect, collection)
+        ranges = {"GT": nb, "LT": 0}
+        range = gen_default_range_expr(ranges=ranges)
+        expr = {"must": [gen_default_vector_expr(default_query), range]}
+        query = update_query_expr(default_query, expr=expr)
+        res = connect.search(collection, query)
+        assert len(res[0]) == 0
 
     @pytest.fixture(
         scope="function",
@@ -1256,7 +1263,7 @@ class TestSearchDSL(object):
         '''
         entities, ids = init_data(connect, collection)
         term_first = gen_default_term_expr()
-        term_second = gen_default_term_expr(field="float", values=[float(i) for i in range(nb//2, nb)])
+        term_second = gen_default_term_expr(field="float", values=[float(i) for i in range(nb // 2, nb)])
         expr = {"must": [gen_default_vector_expr(default_query), term_first, term_second]}
         query = update_query_expr(default_query, expr=expr)
         res = connect.search(collection, query)
@@ -1271,8 +1278,8 @@ class TestSearchDSL(object):
         expected: pass
         '''
         entities, ids = init_data(connect, collection)
-        term_first = {"int64": {"values": [i for i in range(nb//2)]}}
-        term_second = {"float": {"values": [float(i) for i in range(nb//2, nb)]}}
+        term_first = {"int64": {"values": [i for i in range(nb // 2)]}}
+        term_second = {"float": {"values": [float(i) for i in range(nb // 2, nb)]}}
         term = update_term_expr({"term": {}}, [term_first, term_second])
         expr = {"must": [gen_default_vector_expr(default_query), term]}
         query = update_query_expr(default_query, expr=expr)
@@ -1362,7 +1369,7 @@ class TestSearchDSL(object):
         expected: pass
         '''
         term = gen_default_term_expr()
-        range = gen_default_range_expr(ranges={"GT": -1, "LT": nb//2})
+        range = gen_default_range_expr(ranges={"GT": -1, "LT": nb // 2})
         expr = {"must": [gen_default_vector_expr(default_query), term, range]}
         query = update_query_expr(default_query, expr=expr)
         res = connect.search(collection, query)
@@ -1382,7 +1389,6 @@ class TestSearchDSL(object):
         res = connect.search(collection, query)
         assert len(res) == nq
         assert len(res[0]) == 0
-
 
     """
     ******************************************************************
