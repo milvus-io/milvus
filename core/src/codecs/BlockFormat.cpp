f39cc890214a449f3c2e7338e1533f4082f042fb
yudong.cai <yudong.cai@zilliz.com>
2020-08-17 18:05:27 +0800

    code opt
    Signed-off-by: yudong.cai <yudong.cai@zilliz.com>

--- core/src/codecs/BlockFormat.cpp
+++ core/src/codecs/BlockFormat.cpp
@@ -88,12 +88,12 @@ BlockFormat::Read(const storage::FSHandlerPtr& fs_ptr, const std::string& file_p
     }
 
     if (!fs_ptr->reader_ptr_->open(file_path)) {
-        THROW_ERROR(SERVER_CANNOT_OPEN_FILE, "Failed to open file: " + file_path);
+        THROW_ERROR(SERVER_CANNOT_OPEN_FILE, "Fail to open file: " + file_path);
     }
 
     size_t total_num_bytes;
     if (!fs_ptr->reader_ptr_->read(&total_num_bytes, sizeof(size_t))) {
-        THROW_ERROR(SERVER_CANNOT_READ_FILE, "Failed to read file size: " + file_path);
+        THROW_ERROR(SERVER_CANNOT_READ_FILE, "Fail to read file size: " + file_path);
     }
 
     int64_t total_bytes = 0;
@@ -112,7 +112,7 @@ BlockFormat::Read(const storage::FSHandlerPtr& fs_ptr, const std::string& file_p
         int64_t offset = range.offset_ + sizeof(size_t);
         fs_ptr->reader_ptr_->seekg(offset);
         if (!fs_ptr->reader_ptr_->read(raw->data_.data() + poz, range.num_bytes_)) {
-            THROW_ERROR(SERVER_CANNOT_READ_FILE, "Failed to read file data: " + file_path);
+            THROW_ERROR(SERVER_CANNOT_READ_FILE, "Fail to read file data: " + file_path);
         }
         poz += range.num_bytes_;
     }
@@ -128,9 +128,7 @@ BlockFormat::Write(const storage::FSHandlerPtr& fs_ptr, const std::string& file_
     }
 
     if (!fs_ptr->writer_ptr_->open(file_path)) {
-        std::string err_msg = "Failed to open file: " + file_path + ", error: " + std::strerror(errno);
-        LOG_ENGINE_ERROR_ << err_msg;
-        throw Exception(SERVER_CANNOT_CREATE_FILE, err_msg);
+        THROW_ERROR(SERVER_CANNOT_CREATE_FILE, "Fail to open file: " + file_path);
     }
 
     size_t num_bytes = raw->data_.size();
