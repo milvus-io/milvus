954f05df7e52aff406949aec93b3307e61d12ee8
quicksilver <zhifeng.zhang@zilliz.com>
2020-08-18 12:02:03 +0800

    Cleanup Helm Charts
    Signed-off-by: quicksilver <zhifeng.zhang@zilliz.com>

--- .jenkins/modules/DevTest/CleanupMishardsDevTest.groovy
+++ .jenkins/modules/DevTest/CleanupMishardsDevTest.groovy
@@ -3,7 +3,7 @@ if (isTimeTriggeredBuild || "${params.IS_MANUAL_TRIGGER_TYPE}" == "True") {
 	retry(3) {
 	    def helmResult = sh script: "helm status -n milvus ${env.SHARDS_HELM_RELEASE_NAME}", returnStatus: true
 	    if (!helmResult) {
-	        sh "helm uninstall -n milvus ${env.SHARDS_HELM_RELEASE_NAME} && sleep 1m"
+	        sh "helm uninstall -n milvus ${env.SHARDS_HELM_RELEASE_NAME}"
 	    }
 	}
 }
--- .jenkins/modules/DevTest/CleanupSingleNodeDevTest.groovy
+++ .jenkins/modules/DevTest/CleanupSingleNodeDevTest.groovy
@@ -1,6 +1,6 @@
 retry(3) {
     def helmResult = sh script: "helm status -n milvus ${env.HELM_RELEASE_NAME}", returnStatus: true
     if (!helmResult) {
-        sh "helm uninstall -n milvus ${env.HELM_RELEASE_NAME} && sleep 1m"
+        sh "helm uninstall -n milvus ${env.HELM_RELEASE_NAME}"
     }
 }
