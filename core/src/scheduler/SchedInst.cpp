3319e6a8dac2a6c941ecc30c8cadf4d6534f3546
yinghao.zou <yinghao.zou@zilliz.com>
2020-08-19 15:47:22 +0800

    Fix server crash when exiting
    Signed-off-by: yinghao.zou <yinghao.zou@zilliz.com>

--- core/src/scheduler/SchedInst.cpp
+++ core/src/scheduler/SchedInst.cpp
@@ -102,6 +102,7 @@ StopSchedulerService() {
     JobMgrInst::GetInstance()->Stop();
     SchedInst::GetInstance()->Stop();
     ResMgrInst::GetInstance()->Stop();
+    OptimizerInst::GetInstance()->Stop();
 }
 
 }  // namespace scheduler
