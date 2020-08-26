1590b105f7c9ed6180e5c62d46b92d41a7b3e0fb
Cai Yudong <yudong.cai@zilliz.com>
2020-08-26 11:01:03 +0800

    update coverity.sh (#3452)
    * update coverity.sh
    
    Signed-off-by: yudong.cai <yudong.cai@zilliz.com>
    
    * Modify CI workflow (#6)
    
    Signed-off-by: quicksilver <zhifeng.zhang@zilliz.com>
    
    Co-authored-by: quicksilver <zhifeng.zhang@zilliz.com>

--- ci/scripts/run_unittest.sh
+++ ci/scripts/run_unittest.sh
@@ -16,17 +16,13 @@ Usage:
 
     -i [INSTALL_PREFIX] or --install_prefix=[INSTALL_PREFIX]
                               Install directory used by install.
-    --mysql_user=[MYSQL_USER_NAME]    MySQL User Name
-    --mysql_password=[MYSQL_PASSWORD]
-                                      MySQL Password
-    --mysql_host=[MYSQL_HOST]         MySQL Host
     -h or --help                      Print help information
 
 
 Use \"$0  --help\" for more information about a given command.
 "
 
-ARGS=`getopt -o "i:h" -l "install_prefix::,mysql_user::,mysql_password::,mysql_host::,help" -n "$0" -- "$@"`
+ARGS=`getopt -o "i:h" -l "install_prefix::,help" -n "$0" -- "$@"`
 
 eval set -- "${ARGS}"
 
@@ -40,21 +36,6 @@ while true ; do
                                 "") echo "Option install_prefix, no argument"; exit 1 ;;
                                 *)  INSTALL_PREFIX=$2 ; shift 2 ;;
                         esac ;;
-                --mysql_user)
-                        case "$2" in
-                                "") echo "Option mysql_user, no argument"; exit 1 ;;
-                                *)  MYSQL_USER_NAME=$2 ; shift 2 ;;
-                        esac ;;
-                --mysql_password)
-                        case "$2" in
-                                "") echo "Option mysql_password, no argument"; exit 1 ;;
-                                *)  MYSQL_PASSWORD=$2 ; shift 2 ;;
-                        esac ;;
-                --mysql_host)
-                        case "$2" in
-                                "") echo "Option mysql_host, no argument"; exit 1 ;;
-                                *)  MYSQL_HOST=$2 ; shift 2 ;;
-                        esac ;;
                 -h|--help) echo -e "${HELP}" ; exit 0 ;;
                 --) shift ; break ;;
                 *) echo "Internal error!" ; exit 1 ;;
@@ -63,10 +44,6 @@ done
 
 # Set defaults for vars modified by flags to this script
 INSTALL_PREFIX=${INSTALL_PREFIX:="/var/lib/milvus"}
-MYSQL_USER_NAME=${MYSQL_USER_NAME:="root"}
-MYSQL_PASSWORD=${MYSQL_PASSWORD:="123456"}
-MYSQL_HOST=${MYSQL_HOST:="127.0.0.1"}
-MYSQL_PORT=${MYSQL_PORT:="3306"}
 DIR_UNITTEST="${INSTALL_PREFIX}/unittest"
 
 if [ -d ${INSTALL_PREFIX}/lib ]; then
@@ -80,42 +57,14 @@ fi
 
 pushd ${SCRIPTS_DIR}
 
-MYSQL_DB_NAME=milvus_`date +%s%N`
-
-function mysql_exc()
-{
-    cmd=$1
-    mysql -h${MYSQL_HOST} -u${MYSQL_USER_NAME} -p${MYSQL_PASSWORD} -e "${cmd}"
-    if [ $? -ne 0 ]; then
-        echo "mysql $cmd run failed"
-    fi
-}
-
-mysql_exc "CREATE DATABASE IF NOT EXISTS ${MYSQL_DB_NAME};"
-mysql_exc "GRANT ALL PRIVILEGES ON ${MYSQL_DB_NAME}.* TO '${MYSQL_USER_NAME}'@'%';"
-mysql_exc "FLUSH PRIVILEGES;"
-mysql_exc "USE ${MYSQL_DB_NAME};"
-
 for test in `ls ${DIR_UNITTEST}`; do
     echo $test
-    case ${test} in
-        test_db)
-            # set run args for test_db
-            args="mysql://${MYSQL_USER_NAME}:${MYSQL_PASSWORD}@${MYSQL_HOST}:${MYSQL_PORT}/${MYSQL_DB_NAME}"
-            ;;
-        *test_*)
-            args=""
-            ;;
-    esac
     # run unittest
-    ${DIR_UNITTEST}/${test} "${args}"
+    ${DIR_UNITTEST}/${test}
     if [ $? -ne 0 ]; then
-        echo ${args}
         echo ${DIR_UNITTEST}/${test} "run failed"
         exit 1
     fi
 done
-
-mysql_exc "DROP DATABASE IF EXISTS ${MYSQL_DB_NAME};"
 
 popd
