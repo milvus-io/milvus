e8ed6a0bade54b74aec6b1806f52c9c1a29d2068
yudong.cai <yudong.cai@zilliz.com>
2020-08-26 18:38:02 +0800

    update coverage.sh
    Signed-off-by: yudong.cai <yudong.cai@zilliz.com>

--- core/coverage.sh
+++ core/coverage.sh
@@ -5,47 +5,53 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(pwd)/milvus/lib
 LCOV_CMD="lcov"
 LCOV_GEN_CMD="genhtml"
 
-FILE_INFO_BASE="base.info"
-FILE_INFO_MILVUS="server.info"
-FILE_INFO_OUTPUT="output.info"
-FILE_INFO_OUTPUT_NEW="output_new.info"
-DIR_LCOV_OUTPUT="lcov_out"
+BASE_INFO="milvus_base.info"
+TEST_INFO="milvus_test.info"
+TOTAL_INFO="milvus_total.info"
+OUTPUT_INFO="milvus_output.info"
+LCOV_OUTPUT_DIR="lcov_out"
 
-DIR_GCNO="cmake_build"
-DIR_UNITTEST="milvus/unittest"
+GCNO_DIR="cmake_build"
+UNITTEST_DIR="milvus/unittest"
 
 # delete old code coverage info files
-rm -f FILE_INFO_BASE
-rm -f FILE_INFO_MILVUS
-rm -f FILE_INFO_OUTPUT
-rm -f FILE_INFO_OUTPUT_NEW
-rm -rf lcov_out
-rm -f FILE_INFO_BASE FILE_INFO_MILVUS FILE_INFO_OUTPUT FILE_INFO_OUTPUT_NEW
+rm -rf ${LCOV_OUTPUT_DIR}
+rm -f ${BASE_INFO} ${TEST_INFO} ${TOTAL_INFO} ${OUTPUT_INFO}
 
 # get baseline
-${LCOV_CMD} -c -i -d ${DIR_GCNO} -o "${FILE_INFO_BASE}"
+${LCOV_CMD} -c -i -d ${GCNO_DIR} -o "${BASE_INFO}"
 if [ $? -ne 0 ]; then
-    echo "gen baseline coverage run failed"
-    exit -1
+    echo "generate ${BASE_INFO} failed"
+    exit 1
 fi
 
-for test in `ls ${DIR_UNITTEST}`; do
-    echo $test
+# run unittest
+for test in `ls ${UNITTEST_DIR}`; do
+    echo $test "running..."
     # run unittest
-    ./${DIR_UNITTEST}/${test}
+    ./${UNITTEST_DIR}/${test}
     if [ $? -ne 0 ]; then
-        echo ${DIR_UNITTEST}/${test} "run failed"
-        exit -1
+        echo ${UNITTEST_DIR}/${test} "run failed"
+        exit 1
     fi
 done
 
 # gen code coverage
-${LCOV_CMD} -d ${DIR_GCNO} -o "${FILE_INFO_MILVUS}" -c
+${LCOV_CMD} -c -d ${GCNO_DIR} -o "${TEST_INFO}"
+if [ $? -ne 0 ]; then
+    echo "generate ${TEST_INFO} failed"
+    exit 1
+fi
+
 # merge coverage
-${LCOV_CMD} -a ${FILE_INFO_BASE} -a ${FILE_INFO_MILVUS} -o "${FILE_INFO_OUTPUT}"
+${LCOV_CMD} -a ${BASE_INFO} -a ${TEST_INFO} -o "${TOTAL_INFO}"
+if [ $? -ne 0 ]; then
+    echo "generate ${TOTAL_INFO} failed"
+    exit 1
+fi
 
 # remove third party from tracefiles
-${LCOV_CMD} -r "${FILE_INFO_OUTPUT}" -o "${FILE_INFO_OUTPUT_NEW}" \
+${LCOV_CMD} -r "${TOTAL_INFO}" -o "${OUTPUT_INFO}" \
     "/usr/*" \
     "*/cmake_build/*" \
     "*/src/index/thirdparty*" \
@@ -53,8 +59,9 @@ ${LCOV_CMD} -r "${FILE_INFO_OUTPUT}" -o "${FILE_INFO_OUTPUT_NEW}" \
     "*/thirdparty/*"
 
 if [ $? -ne 0 ]; then
-    echo "generate ${FILE_INFO_OUTPUT_NEW} failed"
-    exit -2
+    echo "generate ${OUTPUT_INFO} failed"
+    exit 1
 fi
+
 # gen html report
-${LCOV_GEN_CMD} "${FILE_INFO_OUTPUT_NEW}" --output-directory ${DIR_LCOV_OUTPUT}/
+${LCOV_GEN_CMD} "${OUTPUT_INFO}" --output-directory ${LCOV_OUTPUT_DIR}
