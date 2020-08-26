e9c332235fb53d548a54618d25407b6514db0eda
yudong.cai <yudong.cai@zilliz.com>
2020-08-26 17:32:17 +0800

    optimize run_unittest.sh
    Signed-off-by: yudong.cai <yudong.cai@zilliz.com>

--- ci/scripts/run_unittest.sh
+++ ci/scripts/run_unittest.sh
@@ -20,41 +20,47 @@ Usage:
                               Install directory used by install.
     -b                        Core Code build directory
     -c                        Codecov token
+    -o                        Output info file name
     -h or --help              Print help information
 
 Use \"$0 --help\" for more information about a given command.
 "
 
-ARGS=`getopt -o "i:b:c:h" -l "install_prefix::,help" -n "$0" -- "$@"`
+ARGS=`getopt -o "i:b:c:o:h" -l "install_prefix::,help" -n "$0" -- "$@"`
 
 eval set -- "${ARGS}"
 
 while true ; do
-        case "$1" in
-                -i|--install_prefix)
-                        case "$2" in
-                                "") echo "Option install_prefix, no argument"; exit 1 ;;
-                                *)  INSTALL_PREFIX=$2 ; shift 2 ;;
-                        esac ;;
-                -b)
-                        case "$2" in
-                                "") echo "Option CORE_BUILD_DIR, no argument"; exit 1 ;;
-                                *)  CORE_BUILD_DIR=$2 ; shift 2 ;;
-                        esac ;;
-                -c)
-                        case "$2" in
-                                "") echo "Option CODECOV_TOKEN, no argument"; exit 1 ;;
-                                *)  CODECOV_TOKEN=$2 ; shift 2 ;;
-                        esac ;;
-                -h|--help) echo -e "${HELP}" ; exit 0 ;;
-                --) shift ; break ;;
-                *) echo "Internal error!" ; exit 1 ;;
-        esac
+    case "$1" in
+        -i|--install_prefix)
+            case "$2" in
+                "") echo "Option install_prefix, no argument"; exit 1 ;;
+                *)  INSTALL_PREFIX=$2 ; shift 2 ;;
+            esac ;;
+        -b)
+            case "$2" in
+                "") echo "Option CORE_BUILD_DIR, no argument"; exit 1 ;;
+                *)  CORE_BUILD_DIR=$2 ; shift 2 ;;
+            esac ;;
+        -c)
+            case "$2" in
+                "") echo "Option CODECOV_TOKEN, no argument"; exit 1 ;;
+                *)  CODECOV_TOKEN=$2 ; shift 2 ;;
+            esac ;;
+        -o)
+            case "$2" in
+                "") echo "Option OUTPUT_INFO, no argument"; exit 1 ;;
+                *)  OUTPUT_INFO=$2 ; shift 2 ;;
+            esac ;;
+        -h|--help) echo -e "${HELP}" ; exit 0 ;;
+        --) shift ; break ;;
+        *) echo "Internal error!" ; exit 1 ;;
+    esac
 done
 
 # Set defaults for vars modified by flags to this script
 INSTALL_PREFIX=${INSTALL_PREFIX:="/var/lib/milvus"}
-DIR_UNITTEST="${INSTALL_PREFIX}/unittest"
+UNITTEST_DIR="${INSTALL_PREFIX}/unittest"
 
 MILVUS_CORE_DIR="${SCRIPTS_DIR}/../../core"
 CORE_BUILD_DIR=${CORE_BUILD_DIR:="${MILVUS_CORE_DIR}/cmake_build"}
@@ -62,14 +68,11 @@ CORE_BUILD_DIR=${CORE_BUILD_DIR:="${MILVUS_CORE_DIR}/cmake_build"}
 LCOV_CMD="lcov"
 # LCOV_GEN_CMD="genhtml"
 
-FILE_INFO_BASE="base.info"
-FILE_INFO_MILVUS="server.info"
-FILE_INFO_OUTPUT="output.info"
-FILE_INFO_OUTPUT_NEW="output_new.info"
-DIR_LCOV_OUTPUT="lcov_out"
-
-DIR_GCNO="${CORE_BUILD_DIR}"
-DIR_UNITTEST="${INSTALL_PREFIX}/unittest"
+BASE_INFO="milvus_base.info"
+TEST_INFO="milvus_test.info"
+TOTAL_INFO="milvus_total.info"
+OUTPUT_INFO=${OUTPUT_INFO}
+# LCOV_OUTPUT_DIR="lcov_out"
 
 if [ -d ${INSTALL_PREFIX}/lib ]; then
 	export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${INSTALL_PREFIX}/lib
@@ -83,19 +86,19 @@ fi
 pushd ${SCRIPTS_DIR}
 
 # delete old code coverage info files
-rm -rf ${DIR_LCOV_OUTPUT}
-rm -f ${FILE_INFO_BASE} ${FILE_INFO_MILVUS} ${FILE_INFO_OUTPUT} ${FILE_INFO_OUTPUT_NEW}
+# rm -rf ${LCOV_OUTPUT_DIR}
+rm -f ${BASE_INFO} ${TEST_INFO} ${TOTAL_INFO} ${OUTPUT_INFO}
 
 # get baseline
-${LCOV_CMD} -c -i -d ${DIR_GCNO} -o "${FILE_INFO_BASE}"
+${LCOV_CMD} -c -i -d ${CORE_BUILD_DIR} -o "${BASE_INFO}"
 if [ $? -ne 0 ]; then
-    echo "gen ${FILE_INFO_BASE} failed"
+    echo "gen ${BASE_INFO} failed"
     exit 1
 fi
 
 # run unittest
 for test in `ls ${DIR_UNITTEST}`; do
-    echo "running ... " $test
+    echo $test " running..."
     # run unittest
     ${DIR_UNITTEST}/${test}
     if [ $? -ne 0 ]; then
@@ -105,21 +108,21 @@ for test in `ls ${DIR_UNITTEST}`; do
 done
 
 # gen code coverage
-${LCOV_CMD} -c -d ${DIR_GCNO} -o "${FILE_INFO_MILVUS}"
+${LCOV_CMD} -c -d ${CORE_BUILD_DIR} -o "${TEST_INFO}"
 if [ $? -ne 0 ]; then
-    echo "gen ${FILE_INFO_MILVUS} failed"
+    echo "gen ${TEST_INFO} failed"
     exit 1
 fi
 
 # merge coverage
-${LCOV_CMD} -a ${FILE_INFO_BASE} -a ${FILE_INFO_MILVUS} -o "${FILE_INFO_OUTPUT}"
+${LCOV_CMD} -a ${BASE_INFO} -a ${TEST_INFO} -o "${TOTAL_INFO}"
 if [ $? -ne 0 ]; then
-    echo "gen ${FILE_INFO_OUTPUT} failed"
+    echo "gen ${TOTAL_INFO} failed"
     exit 1
 fi
 
 # remove third party from tracefiles
-${LCOV_CMD} -r "${FILE_INFO_OUTPUT}" -o "${FILE_INFO_OUTPUT_NEW}" \
+${LCOV_CMD} -r "${TOTAL_INFO}" -o "${OUTPUT_INFO}" \
     "/usr/*" \
     "*/cmake_build/*" \
     "*/src/index/thirdparty*" \
@@ -127,13 +130,13 @@ ${LCOV_CMD} -r "${FILE_INFO_OUTPUT}" -o "${FILE_INFO_OUTPUT_NEW}" \
     "*/thirdparty/*"
 
 if [ $? -ne 0 ]; then
-    echo "gen ${FILE_INFO_OUTPUT_NEW} failed"
+    echo "gen ${OUTPUT_INFO} failed"
     exit 1
 fi
 
 if [[ -n ${CODECOV_TOKEN} ]];then
     export CODECOV_TOKEN="${CODECOV_TOKEN}"
-    curl -s https://codecov.io/bash | bash -s - -f ${FILE_INFO_OUTPUT_NEW} || echo "Codecov did not collect coverage reports"
+    curl -s https://codecov.io/bash | bash -s - -f ${OUTPUT_INFO} || echo "Codecov did not collect coverage reports"
 fi
 
 popd
