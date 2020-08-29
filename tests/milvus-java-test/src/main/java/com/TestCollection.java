5a3d2af92caa58ae657d9a15cba19c17f63736cc
Cai Yudong <yudong.cai@zilliz.com>
2020-08-29 13:23:13 +0800

    [skip ci] test new java sdk interface (#3516)
    Signed-off-by: yudong.cai <yudong.cai@zilliz.com>

--- tests/milvus-java-test/src/main/java/com/TestCollection.java
+++ tests/milvus-java-test/src/main/java/com/TestCollection.java
@@ -1,6 +1,5 @@
 package com;
 
-
 import com.alibaba.fastjson.JSONObject;
 import io.milvus.client.*;
 import org.testng.Assert;
@@ -17,17 +16,18 @@ public class TestCollection {
     public MilvusClient setUp() throws ConnectFailedException {
         MilvusClient client = new MilvusGrpcClient();
         ConnectParam connectParam = new ConnectParam.Builder()
-                .withHost("192.168.1.6")
+                .withHost("127.0.0.1")
                 .withPort(19530)
                 .build();
         client.connect(connectParam);
         return client;
     }
+
     @AfterClass
     public void tearDown() throws ConnectFailedException {
         MilvusClient client = setUp();
         List<String> collectionNames = client.listCollections().getCollectionNames();
-//        collectionNames.forEach(collection -> {client.dropCollection(collection);});
+        // collectionNames.forEach(collection -> {client.dropCollection(collection);});
         for(String collection: collectionNames){
             System.out.print(collection+" ");
             client.dropCollection(collection);
@@ -35,8 +35,9 @@ public class TestCollection {
         System.out.println("After Test");
     }
 
+    // case-01
     @Test(dataProvider = "ConnectInstance", dataProviderClass = MainClass.class)
-    public void testCreateCollection(MilvusClient client, String collectionName){
+    public void testCreateCollection(MilvusClient client, String collectionName) {
         CollectionMapping collectionSchema = new CollectionMapping.Builder(collectionName)
                 .withFields(Utils.genDefaultFields(dimension,false))
                 .withParamsInJson(String.format("{\"segment_row_count\": %s}",segmentRowCount))
@@ -46,6 +47,7 @@ public class TestCollection {
         Assert.assertEquals(res.ok(), true);
     }
 
+    // case-02
     @Test(dataProvider = "DisConnectInstance", dataProviderClass = MainClass.class)
     public void testCreateCollectionDisconnect(MilvusClient client, String collectionName){
         CollectionMapping collectionSchema = new CollectionMapping.Builder(collectionName)
@@ -56,6 +58,7 @@ public class TestCollection {
         assert(!res.ok());
     }
 
+    // case-03
     @Test(dataProvider = "ConnectInstance", dataProviderClass = MainClass.class)
     public void testCreateCollectionRepeatably(MilvusClient client, String collectionName){
         CollectionMapping collectionSchema = new CollectionMapping.Builder(collectionName)
@@ -68,6 +71,7 @@ public class TestCollection {
         Assert.assertEquals(resNew.ok(), false);
     }
 
+    // case-04
     @Test(dataProvider = "ConnectInstance", dataProviderClass = MainClass.class)
     public void testCreateCollectionWrongParams(MilvusClient client, String collectionName){
         Integer dim = 0;
@@ -80,6 +84,7 @@ public class TestCollection {
         Assert.assertEquals(res.ok(), false);
     }
 
+    // case-05
     @Test(dataProvider = "ConnectInstance", dataProviderClass = MainClass.class)
     public void testShowCollections(MilvusClient client, String collectionName){
         Integer collectionNum = 10;
@@ -96,12 +101,14 @@ public class TestCollection {
         }
     }
 
+    // case-06
     @Test(dataProvider = "DisConnectInstance", dataProviderClass = MainClass.class)
-    public void testShowCollectionsWithoutConnect(MilvusClient client, String collectionName){
+    public void testShowCollectionsWithoutConnect(MilvusClient client, String collectionName) {
         ListCollectionsResponse res = client.listCollections();
         assert(!res.getResponse().ok());
     }
 
+    // case-07
     @Test(dataProvider = "Collection", dataProviderClass = MainClass.class)
     public void testDropCollection(MilvusClient client, String collectionName) throws InterruptedException {
         Response res = client.dropCollection(collectionName);
@@ -111,6 +118,7 @@ public class TestCollection {
         Assert.assertFalse(collectionNames.contains(collectionName));
     }
 
+    // case-08
     @Test(dataProvider = "Collection", dataProviderClass = MainClass.class)
     public void testDropCollectionNotExisted(MilvusClient client, String collectionName) {
         Response res = client.dropCollection(collectionName+"_");
@@ -119,12 +127,14 @@ public class TestCollection {
         Assert.assertTrue(collectionNames.contains(collectionName));
     }
 
+    // case-09
     @Test(dataProvider = "DisConnectInstance", dataProviderClass = MainClass.class)
     public void testDropCollectionWithoutConnect(MilvusClient client, String collectionName) {
         Response res = client.dropCollection(collectionName);
         assert(!res.ok());
     }
 
+    // case-10
     // TODO
     @Test(dataProvider = "Collection", dataProviderClass = MainClass.class)
     public void testDescribeCollection(MilvusClient client, String collectionName) {
@@ -147,12 +157,14 @@ public class TestCollection {
         Assert.assertEquals(Utils.getParam(segmentParams,"segment_row_count"), segmentRowCount);
     }
 
+    // case-11
     @Test(dataProvider = "DisConnectInstance", dataProviderClass = MainClass.class)
     public void testDescribeCollectionWithoutConnect(MilvusClient client, String collectionName) {
         GetCollectionInfoResponse res = client.getCollectionInfo(collectionName);
         assert(!res.getResponse().ok());
     }
 
+    // case-12
     @Test(dataProvider = "Collection", dataProviderClass = MainClass.class)
     public void testHasCollectionNotExisted(MilvusClient client, String collectionName) {
         HasCollectionResponse res = client.hasCollection(collectionName+"_");
@@ -160,12 +172,14 @@ public class TestCollection {
         Assert.assertFalse(res.hasCollection());
     }
 
+    // case-13
     @Test(dataProvider = "DisConnectInstance", dataProviderClass = MainClass.class)
     public void testHasCollectionWithoutConnect(MilvusClient client, String collectionName) {
         HasCollectionResponse res = client.hasCollection(collectionName);
         assert(!res.getResponse().ok());
     }
 
+    // case-14
     @Test(dataProvider = "Collection", dataProviderClass = MainClass.class)
     public void testHasCollection(MilvusClient client, String collectionName) {
         HasCollectionResponse res = client.hasCollection(collectionName);
