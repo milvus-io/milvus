7a6b64a4824b20e2024d921aa57fa3dc2f877bb7
shengjun.li <shengjun.li@zilliz.com>
2020-09-05 16:37:51 +0800

    add to check query dim
    Signed-off-by: shengjun.li <shengjun.li@zilliz.com>

--- core/src/query/GeneralQuery.h
+++ core/src/query/GeneralQuery.h
@@ -70,6 +70,7 @@ struct RangeQuery {
 using RangeQueryPtr = std::shared_ptr<RangeQuery>;
 
 struct VectorRecord {
+    size_t vector_count;
     std::vector<float> float_data;
     std::vector<uint8_t> binary_data;
 };
