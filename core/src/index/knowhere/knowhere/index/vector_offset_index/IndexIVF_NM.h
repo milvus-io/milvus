1b97f8892bb5bb77746f8a1e2bf233ee22985c39
XuanYang-cn <51370125+XuanYang-cn@users.noreply.github.com>
2020-09-09 19:11:50 +0800

    Fix loadcollection memory usage is one times larger than expect (#3664)
    * rm new rearanged_data to avoid additional memory usage
    
    Signed-off-by: yangxuan <xuan.yang@zilliz.com>
    
    * fix cpu version
    
    Signed-off-by: yangxuan <xuan.yang@zilliz.com>
    
    * fix data_ deconstruct twice error
    
    Signed-off-by: yangxuan <xuan.yang@zilliz.com>
    
    Co-authored-by: yangxuan <xuan.yang@zilliz.com>
    Signed-off-by: shengjun.li <shengjun.li@zilliz.com>

--- core/src/index/knowhere/knowhere/index/vector_offset_index/IndexIVF_NM.h
+++ core/src/index/knowhere/knowhere/index/vector_offset_index/IndexIVF_NM.h
@@ -93,8 +93,13 @@ class IVF_NM : public VecIndex, public OffsetBaseIndex {
 
  protected:
     std::mutex mutex_;
-    std::shared_ptr<uint8_t[]> data_ = nullptr;
     std::vector<size_t> prefix_sum;
+
+    // data_:    if CPU, malloc memory while loading data
+    // ro_codes: if GPU, hold a ptr of read only codes so that
+    //            destruction won't be done twice
+    std::shared_ptr<uint8_t[]> data_ = nullptr;
+    faiss::PageLockMemoryPtr ro_codes = nullptr;
 };
 
 using IVFNMPtr = std::shared_ptr<IVF_NM>;
