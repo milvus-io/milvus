fce933605549b8e6c96fdaff676089297ccaa3ce
Zhiwen Xie <zhiwenxie1900@outlook.com>
2020-09-12 11:36:43 +0800

    update faiss to reduce the memory usage of GPU when nprobe is large

--- core/src/index/thirdparty/faiss/gpu/GpuIndexIVFFlat.cu
+++ core/src/index/thirdparty/faiss/gpu/GpuIndexIVFFlat.cu
@@ -235,29 +235,32 @@ GpuIndexIVFFlat::searchImpl_(int n,
                              float* distances,
                              Index::idx_t* labels,
                              ConcurrentBitsetPtr bitset) const {
-  // Device is already set in GpuIndex::search
-  FAISS_ASSERT(index_);
-  FAISS_ASSERT(n > 0);
+                                   // Device is already set in GpuIndex::search
+    FAISS_ASSERT(index_);
+    FAISS_ASSERT(n > 0);
+    printf("Hello searchImpl_\n");
+    // Data is already resident on the GPU
+    printf("distance指针指向地址: %p", distances);
+    Tensor<float, 2, true> queries(const_cast<float*>(x), {n, (int) this->d});
+    Tensor<float, 2, true> outDistances(distances, {n, k});
+    printf("Outdistance指针指向地址: %p", outDistances.data());
+    static_assert(sizeof(long) == sizeof(Index::idx_t), "size mismatch");
+    Tensor<long, 2, true> outLabels(const_cast<long*>(labels), {n, k});
+    
+    auto stream = resources_->getDefaultStream(device_);
 
-  auto stream = resources_->getDefaultStream(device_);
-
-  // Data is already resident on the GPU
-  Tensor<float, 2, true> queries(const_cast<float*>(x), {n, (int) this->d});
-  Tensor<float, 2, true> outDistances(distances, {n, k});
-
-  static_assert(sizeof(long) == sizeof(Index::idx_t), "size mismatch");
-  Tensor<long, 2, true> outLabels(const_cast<long*>(labels), {n, k});
-
-  if (!bitset) {
     auto bitsetDevice = toDevice<uint8_t, 1>(resources_, device_, nullptr, stream, {0});
-    index_->query(queries, bitsetDevice, nprobe, k, outDistances, outLabels);
-  } else {
-    auto bitsetDevice = toDevice<uint8_t, 1>(resources_, device_,
-                                             const_cast<uint8_t*>(bitset->data()), stream,
-                                             {(int) bitset->size()});
-    index_->query(queries, bitsetDevice, nprobe, k, outDistances, outLabels);
-  }
+    //distances = new float[n*k];
+    // labels = new long[n*k];
+
+    index_->query(queries, nprobe, k, outDistances, outLabels, &distances, &labels, bitsetDevice);
+    printf("end searchImpl_\n");
+
+    printf("outDistance指针指向地址: %p", outDistances.data());
+    printf("distance指针指向地址: %p", distances);
 }
+
+
 
 
 } } // namespace
