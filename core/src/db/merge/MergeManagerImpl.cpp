8a19251201704f9f0536324afedb6bc95bb2dd9c
groot <yihua.mo@zilliz.com>
2020-09-17 18:52:46 +0800

    avoid dead circle when merge failed (#3787)
    * avoid dead circle when merge failed
    
    Signed-off-by: groot <yihua.mo@zilliz.com>
    
    * typo
    
    Signed-off-by: groot <yihua.mo@zilliz.com>
    Signed-off-by: shengjun.li <shengjun.li@zilliz.com>

--- core/src/db/merge/MergeManagerImpl.cpp
+++ core/src/db/merge/MergeManagerImpl.cpp
@@ -21,6 +21,7 @@
 #include "utils/Log.h"
 
 #include <map>
+#include <unordered_set>
 #include <utility>
 
 namespace milvus {
@@ -96,15 +97,32 @@ MergeManagerImpl::MergeSegments(int64_t collection_id, MergeStrategyType type) {
         return status;
     }
 
+    // to avoid dead-circle, ignore failed segments
+    std::unordered_set<snapshot::ID_TYPE> failed_segments;
     while (true) {
         snapshot::ScopedSnapshotT latest_ss;
         STATUS_CHECK(snapshot::Snapshots::GetInstance().GetSnapshot(latest_ss, collection_id));
 
+        // segment must meet two conditions for merging:
+        // 1. the segment's row count is less than segment_row_count
+        // 2. the segment has no index(for any field)
         snapshot::IDS_TYPE segment_ids;
         SnapshotVisitor ss_visitor(latest_ss);
         ss_visitor.SegmentsToMerge(segment_ids);
         if (segment_ids.empty()) {
             break;  // nothing to merge
+        }
+
+        // ignore failed segments in last round
+        if (!failed_segments.empty()) {
+            for (auto iter = segment_ids.begin(); iter != segment_ids.end();) {
+                if (failed_segments.find(*iter) != failed_segments.end()) {
+                    iter = segment_ids.erase(iter);
+                } else {
+                    ++iter;
+                }
+            }
+            failed_segments.clear();
         }
 
         // collect segments info
@@ -150,7 +168,17 @@ MergeManagerImpl::MergeSegments(int64_t collection_id, MergeStrategyType type) {
         // do merge
         for (auto& segments : segment_groups) {
             MergeTask task(options_, latest_ss, segments);
-            task.Execute();
+            status = task.Execute();
+            if (!status.ok()) {
+                // merge failed, these segments will not take part in next round
+                std::string msg;
+                for (auto& id : segments) {
+                    failed_segments.insert(id);
+                    msg += std::to_string(id);
+                    msg += ",";
+                }
+                LOG_ENGINE_ERROR_ << "Failed to merge segments: " << msg << " reason: " << status.message();
+            }
         }
     }
 
