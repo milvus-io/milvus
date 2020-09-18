9d5796bb88a54a63aaf2c4c58f8dd920d1a1b3ba
shengjun.li <shengjun.li@zilliz.com>
2020-09-18 15:26:09 +0800

    fix C++ SDK examples
    Signed-off-by: shengjun.li <shengjun.li@zilliz.com>

--- sdk/examples/utils/Utils.cpp
+++ sdk/examples/utils/Utils.cpp
@@ -371,14 +371,7 @@ Utils::GenDSLJson(nlohmann::json& dsl_json, nlohmann::json& vector_param_json, c
 }
 
 void
-Utils::GenBinaryDSLJson(nlohmann::json& dsl_json, nlohmann::json& vector_param_json, const std::string metric_type) {
-    uint64_t row_num = 10000;
-    std::vector<int64_t> term_value;
-    term_value.resize(row_num);
-    for (uint64_t i = 0; i < row_num; ++i) {
-        term_value[i] = i;
-    }
-
+Utils::GenPureVecDSLJson(nlohmann::json& dsl_json, nlohmann::json& vector_param_json, const std::string metric_type) {
     nlohmann::json bool_json, vector_json;
     std::string placeholder = "placeholder_1";
     vector_json["vector"] = placeholder;
