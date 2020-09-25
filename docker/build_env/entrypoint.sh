86c1e34ca8182c8d1b4c7c9cfe475d553d6635ce
quicksilver <zhifeng.zhang@zilliz.com>
2020-09-25 19:40:18 +0800

    Add entrypoint.sh
    Signed-off-by: quicksilver <zhifeng.zhang@zilliz.com>

---
+++ docker/build_env/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Ensure $HOME exists when starting
+if [ ! -d "${HOME}" ]; then
+  mkdir -p "${HOME}"
+fi
+
+# Setup $PS1 for a consistent and reasonable prompt
+if [ -w "${HOME}" ] && [ ! -f "${HOME}"/.bashrc ]; then
+  echo "PS1='\s-\v \w \$ '" > "${HOME}"/.bashrc
+fi
+
+# Add current (arbitrary) user to /etc/passwd and /etc/group
+if ! whoami &> /dev/null; then
+  if [ -w /etc/passwd ]; then
+    echo "${USER_NAME:-user}:x:$(id -u):0:${USER_NAME:-user} user:${HOME}:/bin/bash" >> /etc/passwd
+    echo "${USER_NAME:-user}:x:$(id -u):" >> /etc/group
+  fi
+fi
+
+exec "$@"
+
