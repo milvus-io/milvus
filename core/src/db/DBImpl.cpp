670b3130142d646bcad4d0451132af6174bdc3cf
yhmo <mummyhome@163.com>
2020-09-26 16:25:03 +0800

    rewrite list id in segment
    Signed-off-by: yhmo <mummyhome@163.com>

--- core/src/db/DBImpl.cpp
+++ core/src/db/DBImpl.cpp
@@ -736,16 +736,56 @@ DBImpl::ListIDInSegment(const std::string& collection_name, int64_t segment_id, 
     // remove delete id from the id list
     segment::DeletedDocsPtr deleted_docs_ptr;
     segment_reader->LoadDeletedDocs(deleted_docs_ptr);
-    if (deleted_docs_ptr) {
-        const std::vector<offset_t>& delete_ids = deleted_docs_ptr->GetDeletedDocs();
-        std::vector<offset_t> temp_ids;
-        temp_ids.reserve(delete_ids.size());
-        std::copy(delete_ids.begin(), delete_ids.end(), std::back_inserter(temp_ids));
-        std::sort(temp_ids.begin(), temp_ids.end(), std::greater<>());
-        for (auto offset : temp_ids) {
-            entity_ids.erase(entity_ids.begin() + offset, entity_ids.begin() + offset + 1);
+//    if (deleted_docs_ptr) {
+//        const std::vector<offset_t>& delete_ids = deleted_docs_ptr->GetDeletedDocs();
+//        std::vector<offset_t> temp_ids;
+//        temp_ids.reserve(delete_ids.size());
+//        std::copy(delete_ids.begin(), delete_ids.end(), std::back_inserter(temp_ids));
+//        std::sort(temp_ids.begin(), temp_ids.end(), std::greater<>());
+//        for (auto offset : temp_ids) {
+//            entity_ids.erase(entity_ids.begin() + offset, entity_ids.begin() + offset + 1);
+//        }
+//    }
+
+    if (deleted_docs_ptr == nullptr) {
+        return Status::OK();
+    }
+
+    // sorted-merge entities id and deleted offsets
+    std::vector<offset_t> delete_offsets = deleted_docs_ptr->GetDeletedDocs();
+    std::sort(delete_offsets.begin(), delete_offsets.end());
+
+    int64_t left_count = entity_ids.size() - delete_offsets.size();
+    IDNumbers entity_ids_left;
+    entity_ids_left.resize(left_count);
+
+    int32_t min_delete_offset = *delete_offsets.begin();
+    int32_t max_delete_offset = *delete_offsets.rbegin();
+
+    if (min_delete_offset > 0) {
+        memcpy(entity_ids_left.data(), entity_ids.data(), min_delete_offset * sizeof(int64_t));
+    }
+
+    if (max_delete_offset < entity_ids.size() - 1) {
+        int64_t copy_count = entity_ids.size() - 1 - max_delete_offset;
+        memcpy(entity_ids_left.data() + left_count - copy_count,
+               entity_ids.data() + max_delete_offset + 1,
+               copy_count * sizeof(int64_t));
+    }
+
+    for (int64_t k = min_delete_offset, n = 0, index = min_delete_offset;
+         k < entity_ids.size(), n < delete_offsets.size();) {
+        if (k == delete_offsets[n]) {
+            k++;
+            n++;
+        } else {
+            entity_ids_left[index] = entity_ids[k];
+            index++;
+            k++;
         }
     }
+
+    entity_ids.swap(entity_ids_left);
 
     return Status::OK();
 }
