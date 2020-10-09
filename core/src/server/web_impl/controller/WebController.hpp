b575915b87efeef2350867b53c4dffcba1600c85
fishpenguin <kun.yu@zilliz.com>
2020-10-09 16:01:30 +0800

    Fix Get method with null payload problem
    Signed-off-by: fishpenguin <kun.yu@zilliz.com>

--- core/src/server/web_impl/controller/WebController.hpp
+++ core/src/server/web_impl/controller/WebController.hpp
@@ -559,9 +559,15 @@ class WebController : public oatpp::web::server::api::ApiController {
     ADD_DEFAULT_CORS(EntityOp)
 
     ENDPOINT("GET", "/collections/{collection_name}/entities", EntityOp, PATH(String, collection_name),
-             QUERIES(QueryParams, query_params), BODY_STRING(String, body_str)) {
-        auto handler = WebRequestHandler();
+             QUERIES(QueryParams, query_params), REQUEST(std::shared_ptr<IncomingRequest>, request)) {
+        String body_str;
+        try {
+            body_str = request->readBodyToString();
+        } catch (...) {
+            body_str = "";
+        }
 
+        auto handler = WebRequestHandler();
         String response;
         auto status_dto = handler.EntityOp(collection_name, query_params, body_str, response);
         switch (*(status_dto->code)) {
