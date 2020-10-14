8514996600f3bf12ae8287af936eb8064c99ebb5
fishpenguin <kun.yu@zilliz.com>
2020-10-14 20:26:43 +0800

    Change ge le to gte lte
    Signed-off-by: fishpenguin <kun.yu@zilliz.com>

--- core/src/index/knowhere/knowhere/index/structured_index/StructuredIndex.h
+++ core/src/index/knowhere/knowhere/index/structured_index/StructuredIndex.h
@@ -24,9 +24,13 @@ enum OperatorType { LT = 0, LE = 1, GT = 3, GE = 4 };
 
 static std::map<std::string, OperatorType> s_map_operator_type = {
     {"LT", OperatorType::LT},
-    {"LE", OperatorType::LE},
+    {"LTE", OperatorType::LE},
     {"GT", OperatorType::GT},
-    {"GE", OperatorType::GE},
+    {"GTE", OperatorType::GE},
+    {"lt", OperatorType::LT},
+    {"lte", OperatorType::LE},
+    {"gt", OperatorType::GT},
+    {"gte", OperatorType::GE},
 };
 
 template <typename T>
