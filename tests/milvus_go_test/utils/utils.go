48f4ad7f90845057e26dbdfd4bac101a8e1cfb03
zw <zw@milvus.io>
2020-10-28 15:20:50 +0800

    fix bug
    Signed-off-by: zw <zw@milvus.io>

--- tests/milvus_go_test/utils/utils.go
+++ tests/milvus_go_test/utils/utils.go
@@ -10,7 +10,7 @@ func init() {
 	rand.Seed(time.Now().UnixNano())
 }
 
-const letterBytes = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 func RandString(n int) string {
 	b := make([]byte, n)
