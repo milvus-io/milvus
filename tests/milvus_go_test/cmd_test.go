c4987e59c346c5efcf30700339541abfa5cabf63
zw <zw@milvus.io>
2020-10-30 17:26:39 +0800

    [skip ci] add cmd case
    Signed-off-by: zw <zw@milvus.io>

--- tests/milvus_go_test/cmd_test.go
+++ tests/milvus_go_test/cmd_test.go
@@ -3,14 +3,29 @@ package main
 import (
 	"testing"
 
-	"github.com/milvus-io/milvus-sdk-go/milvus"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestServerVersion(t *testing.T) {
-	// client := GetClient()
-	client, _ := Collection(true, milvus.VECTORFLOAT)
-	res, status, _ = client.ServerVersion()
+func TestCMDServerVersion(t *testing.T) {
+	client := GetClient()
+	// client, _ := Collection(true, milvus.VECTORFLOAT)
+	res, status, _ := client.ServerVersion()
+	t.Log(res)
+	assert.True(t, status.Ok())
+}
+
+func TestCMDServerStatus(t *testing.T) {
+	client := GetClient()
+	// client, _ := Collection(true, milvus.VECTORFLOAT)
+	res, status, _ := client.ServerStatus()
+	t.Log(res)
+	assert.True(t, status.Ok())
+}
+
+func TestCMDGetConfig(t *testing.T) {
+	client := GetClient()
+	// client, _ := Collection(true, milvus.VECTORFLOAT)
+	res, status, _ := client.GetConfig("cache.cache_size")
 	t.Log(res)
 	assert.True(t, status.Ok())
 }
