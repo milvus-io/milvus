f371da05e32ded87af9afe577b21763178fc115f
ThreadDao <zongyufen@foxmail.com>
2020-10-30 15:27:24 +0800

    merge
    Signed-off-by: ThreadDao <zongyufen@foxmail.com>

--- tests/milvus_go_test/utils/constants.go
+++ tests/milvus_go_test/utils/constants.go
@@ -16,9 +16,9 @@ var DefaultFieldFloatVectorName string = "float_vector"
 var DefaultFieldBinaryVectorName string = "binary_vector"
 var DefaultDimension int = 128
 var DefaultSegmentRowLimit int = 5000
-var DefaultNb = 6000
+var DefaultNb int = 6000
 var DefaultIndexType = milvus.IVFSQ8
-var DefaulltBinaryIndexType = milvus.BINIVFFLAT
+var DefaultBinaryIndexType = milvus.BINIVFFLAT
 var DefaultIntValues = GenDefaultIntValues(DefaultNb)
 var DefaultFloatValues = GenDefaultFloatValues(DefaultNb)
 var DefaultFloatVector = GenFloatVectors(DefaultDimension, 1, false)
@@ -30,9 +30,9 @@ var DefaultBinaryVectors = GenBinaryVectors(DefaultDimension, DefaultNb)
 var L2Indexes = GenIndexes(milvus.L2)
 var IpIndexes = GenIndexes(milvus.IP)
 var DefaultQueries = GenVectorQuery(DefaultMetricType, DefaultIndexType)
-var DefaultBinaryQueries = GenVectorQuery(DefaultBinaryMetricType, DefaulltBinaryIndexType)
+var DefaultBinaryQueries = GenVectorQuery(DefaultBinaryMetricType, DefaultBinaryIndexType)
 var DefaultDsl = GenDSL(DefaultFieldFloatVectorName, DefaultMetricType, DefaultIndexType)
-var DefaultBinaryDsl = GenDSL(DefaultFieldBinaryVectorName, DefaultBinaryMetricType, DefaulltBinaryIndexType)
+var DefaultBinaryDsl = GenDSL(DefaultFieldBinaryVectorName, DefaultBinaryMetricType, DefaultBinaryIndexType)
 
 const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
