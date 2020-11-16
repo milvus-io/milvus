d3cb3060e1daa153e303838674186092b5c0f184
BossZou <40255591+BossZou@users.noreply.github.com>
2020-11-16 15:28:40 +0800

    Opt some code (#4238)
    Signed-off-by: Yhz <yinghao.zou@zilliz.com>
    Signed-off-by: shengjun.li <shengjun.li@zilliz.com>

--- core/unittest/db/test_meta.cpp
+++ core/unittest/db/test_meta.cpp
@@ -29,21 +29,21 @@ TEST_F(MetaTest, ApplyTest) {
     ID_TYPE result_id;
 
     auto collection = std::make_shared<Collection>("meta_test_c1");
-    auto c_ctx = ResourceContextBuilder<Collection>().SetResource(collection).CreatePtr();
+    auto c_ctx = ResourceContextBuilder<Collection>(Op::oAdd).SetResource(collection).CreatePtr();
     auto status = meta_->Execute<Collection>(c_ctx, result_id);
     ASSERT_TRUE(status.ok()) << status.ToString();
     ASSERT_GT(result_id, 0);
     collection->SetID(result_id);
 
     collection->Activate();
-    auto c2_ctx = ResourceContextBuilder<Collection>().SetResource(collection)
-        .SetOp(Op::oUpdate).AddAttr(milvus::engine::meta::F_STATE).CreatePtr();
+    auto c2_ctx = ResourceContextBuilder<Collection>(Op::oUpdate).SetResource(collection)
+        .AddAttr(milvus::engine::meta::F_STATE).CreatePtr();
     status = meta_->Execute<Collection>(c2_ctx, result_id);
     ASSERT_TRUE(status.ok()) << status.ToString();
     ASSERT_GT(result_id, 0);
     ASSERT_EQ(result_id, collection->GetID());
 
-    auto c3_ctx = ResourceContextBuilder<Collection>().SetID(result_id).SetOp(Op::oDelete).CreatePtr();
+    auto c3_ctx = ResourceContextBuilder<Collection>(Op::oDelete).SetID(result_id).CreatePtr();
     status = meta_->Execute<Collection>(c3_ctx, result_id);
     ASSERT_TRUE(status.ok()) << status.ToString();
     ASSERT_GT(result_id, 0);
@@ -54,21 +54,21 @@ TEST_F(MetaTest, SessionTest) {
     ID_TYPE result_id;
 
     auto collection = std::make_shared<Collection>("meta_test_c1");
-    auto c_ctx = ResourceContextBuilder<Collection>().SetResource(collection).CreatePtr();
+    auto c_ctx = ResourceContextBuilder<Collection>(Op::oAdd).SetResource(collection).CreatePtr();
     auto status = meta_->Execute<Collection>(c_ctx, result_id);
     ASSERT_TRUE(status.ok()) << status.ToString();
     ASSERT_GT(result_id, 0);
     collection->SetID(result_id);
 
     auto partition = std::make_shared<Partition>("meta_test_p1", result_id);
-    auto p_ctx = ResourceContextBuilder<Partition>().SetResource(partition).CreatePtr();
+    auto p_ctx = ResourceContextBuilder<Partition>(Op::oAdd).SetResource(partition).CreatePtr();
     status = meta_->Execute<Partition>(p_ctx, result_id);
     ASSERT_TRUE(status.ok()) << status.ToString();
     ASSERT_GT(result_id, 0);
     partition->SetID(result_id);
 
     auto field = std::make_shared<Field>("meta_test_f1", 1, FType::INT64);
-    auto f_ctx = ResourceContextBuilder<Field>().SetResource(field).CreatePtr();
+    auto f_ctx = ResourceContextBuilder<Field>(Op::oAdd).SetResource(field).CreatePtr();
     status = meta_->Execute<Field>(f_ctx, result_id);
     ASSERT_TRUE(status.ok()) << status.ToString();
     ASSERT_GT(result_id, 0);
@@ -76,7 +76,7 @@ TEST_F(MetaTest, SessionTest) {
 
     auto field_element = std::make_shared<FieldElement>(collection->GetID(), field->GetID(),
                                                         "meta_test_f1_fe1", FEType::FET_RAW);
-    auto fe_ctx = ResourceContextBuilder<FieldElement>().SetResource(field_element).CreatePtr();
+    auto fe_ctx = ResourceContextBuilder<FieldElement>(Op::oAdd).SetResource(field_element).CreatePtr();
     status = meta_->Execute<FieldElement>(fe_ctx, result_id);
     ASSERT_TRUE(status.ok()) << status.ToString();
     ASSERT_GT(result_id, 0);
@@ -84,26 +84,26 @@ TEST_F(MetaTest, SessionTest) {
 
     auto session = meta_->CreateSession();
     ASSERT_TRUE(collection->Activate());
-    auto c2_ctx = ResourceContextBuilder<Collection>().SetResource(collection)
-        .SetOp(Op::oUpdate).AddAttr(milvus::engine::meta::F_STATE).CreatePtr();
+    auto c2_ctx = ResourceContextBuilder<Collection>(Op::oUpdate).SetResource(collection)
+        .AddAttr(milvus::engine::meta::F_STATE).CreatePtr();
     status = session->Apply<Collection>(c2_ctx);
     ASSERT_TRUE(status.ok()) << status.ToString();
 
     ASSERT_TRUE(partition->Activate());
-    auto p2_ctx = ResourceContextBuilder<Partition>().SetResource(partition)
-        .SetOp(Op::oUpdate).AddAttr(milvus::engine::meta::F_STATE).CreatePtr();
+    auto p2_ctx = ResourceContextBuilder<Partition>(Op::oUpdate).SetResource(partition)
+        .AddAttr(milvus::engine::meta::F_STATE).CreatePtr();
     status = session->Apply<Partition>(p2_ctx);
     ASSERT_TRUE(status.ok()) << status.ToString();
 
     ASSERT_TRUE(field->Activate());
-    auto f2_ctx = ResourceContextBuilder<Field>().SetResource(field)
-        .SetOp(Op::oUpdate).AddAttr(milvus::engine::meta::F_STATE).CreatePtr();
+    auto f2_ctx = ResourceContextBuilder<Field>(Op::oUpdate).SetResource(field)
+        .AddAttr(milvus::engine::meta::F_STATE).CreatePtr();
     status = session->Apply<Field>(f2_ctx);
     ASSERT_TRUE(status.ok()) << status.ToString();
 
     ASSERT_TRUE(field_element->Activate());
-    auto fe2_ctx = ResourceContextBuilder<FieldElement>().SetResource(field_element)
-        .SetOp(Op::oUpdate).AddAttr(milvus::engine::meta::F_STATE).CreatePtr();
+    auto fe2_ctx = ResourceContextBuilder<FieldElement>(Op::oUpdate).SetResource(field_element)
+        .AddAttr(milvus::engine::meta::F_STATE).CreatePtr();
     status = session->Apply<FieldElement>(fe2_ctx);
     ASSERT_TRUE(status.ok()) << status.ToString();
 
@@ -122,7 +122,7 @@ TEST_F(MetaTest, SelectTest) {
 
     auto collection = std::make_shared<Collection>("meta_test_c1");
     ASSERT_TRUE(collection->Activate());
-    auto c_ctx = ResourceContextBuilder<Collection>().SetResource(collection).CreatePtr();
+    auto c_ctx = ResourceContextBuilder<Collection>(Op::oAdd).SetResource(collection).CreatePtr();
     auto status = meta_->Execute<Collection>(c_ctx, result_id);
     ASSERT_TRUE(status.ok()) << status.ToString();
     ASSERT_GT(result_id, 0);
@@ -136,7 +136,7 @@ TEST_F(MetaTest, SelectTest) {
 
     auto collection2 = std::make_shared<Collection>("meta_test_c2");
     ASSERT_TRUE(collection2->Activate());
-    auto c2_ctx = ResourceContextBuilder<Collection>().SetResource(collection2).CreatePtr();
+    auto c2_ctx = ResourceContextBuilder<Collection>(Op::oAdd).SetResource(collection2).CreatePtr();
     status = meta_->Execute<Collection>(c2_ctx, result_id);
     ASSERT_TRUE(status.ok()) << status.ToString();
     ASSERT_GT(result_id, 0);
@@ -175,7 +175,7 @@ TEST_F(MetaTest, TruncateTest) {
 
     auto collection = std::make_shared<Collection>("meta_test_c1");
     ASSERT_TRUE(collection->Activate());
-    auto c_ctx = ResourceContextBuilder<Collection>().SetResource(collection).CreatePtr();
+    auto c_ctx = ResourceContextBuilder<Collection>(Op::oAdd).SetResource(collection).CreatePtr();
     auto status = meta_->Execute<Collection>(c_ctx, result_id);
     ASSERT_TRUE(status.ok()) << status.ToString();
     ASSERT_GT(result_id, 0);
@@ -197,15 +197,15 @@ TEST_F(MetaTest, MultiThreadRequestTest) {
         for (size_t ii = 0; ii < 30; ii++) {
             std::string collection_name = collection_name_prefix + std::to_string(ii);
             auto collection = std::make_shared<Collection>(collection_name);
-            auto c_ctx = ResourceContextBuilder<Collection>().SetResource(collection).CreatePtr();
+            auto c_ctx = ResourceContextBuilder<Collection>(Op::oAdd).SetResource(collection).CreatePtr();
             auto status = meta_->Execute<Collection>(c_ctx, result_id);
             ASSERT_TRUE(status.ok()) << status.ToString();
             ASSERT_GT(result_id, 0);
 
             collection->SetID(result_id);
             collection->Activate();
-            auto c_ctx2 = ResourceContextBuilder<Collection>().SetResource(collection)
-                .SetOp(Op::oUpdate).AddAttr(milvus::engine::meta::F_STATE).CreatePtr();
+            auto c_ctx2 = ResourceContextBuilder<Collection>(Op::oUpdate).SetResource(collection)
+                .AddAttr(milvus::engine::meta::F_STATE).CreatePtr();
             status = meta_->Execute<Collection>(c_ctx2, result_id);
             ASSERT_TRUE(status.ok()) << status.ToString();
 
@@ -217,14 +217,14 @@ TEST_F(MetaTest, MultiThreadRequestTest) {
             ASSERT_EQ(collection2->GetName(), collection_name);
 
             collection->Deactivate();
-            auto c_ctx3 = ResourceContextBuilder<Collection>().SetResource(collection)
-                .SetOp(Op::oUpdate).AddAttr(milvus::engine::meta::F_STATE).CreatePtr();
+            auto c_ctx3 = ResourceContextBuilder<Collection>(Op::oUpdate).SetResource(collection)
+                .AddAttr(milvus::engine::meta::F_STATE).CreatePtr();
             status = meta_->Execute<Collection>(c_ctx3, result_id);
             ASSERT_TRUE(status.ok()) << status.ToString();
             ASSERT_EQ(result_id, collection->GetID());
 
-            auto c_ctx4 = ResourceContextBuilder<Collection>().SetID(result_id)
-                .SetOp(Op::oDelete).SetTable(Collection::Name).CreatePtr();
+            auto c_ctx4 = ResourceContextBuilder<Collection>(Op::oDelete).SetID(result_id)
+                .SetTable(Collection::Name).CreatePtr();
             status = meta_->Execute<Collection>(c_ctx4, result_id);
             ASSERT_TRUE(status.ok()) << status.ToString();
             CollectionPtr collection3;
@@ -242,7 +242,7 @@ TEST_F(MetaTest, MultiThreadRequestTest) {
             std::string collection_name = collection_name_prefix + std::to_string(jj);
             milvus::json cj{{"segment_row_limit", 1024}};
             auto collection = std::make_shared<Collection>(collection_name, cj);
-            auto c_ctx = ResourceContextBuilder<Collection>().SetResource(collection).SetOp(Op::oAdd).CreatePtr();
+            auto c_ctx = ResourceContextBuilder<Collection>(Op::oAdd).SetResource(collection).CreatePtr();
             status = meta_->Execute<Collection>(c_ctx, result_id);
             ASSERT_TRUE(status.ok()) << status.ToString();
             ASSERT_GT(result_id, 0);
@@ -250,7 +250,7 @@ TEST_F(MetaTest, MultiThreadRequestTest) {
 
             std::string partition_name = collection_name + "_p_" + std::to_string(jj);
             auto partition = std::make_shared<Partition>(partition_name, collection->GetID());
-            auto p_ctx = ResourceContextBuilder<Partition>().SetResource(partition).SetOp(Op::oAdd).CreatePtr();
+            auto p_ctx = ResourceContextBuilder<Partition>(Op::oAdd).SetResource(partition).CreatePtr();
             status = meta_->Execute<Partition>(p_ctx, result_id);
             ASSERT_TRUE(status.ok()) << status.ToString();
             ASSERT_GT(result_id, 0);
@@ -258,7 +258,7 @@ TEST_F(MetaTest, MultiThreadRequestTest) {
 
             std::string segment_name = partition_name + "_s_" + std::to_string(jj);
             auto segment = std::make_shared<Segment>(collection->GetID(), partition->GetID());
-            auto s_ctx = ResourceContextBuilder<Segment>().SetResource(segment).SetOp(Op::oAdd).CreatePtr();
+            auto s_ctx = ResourceContextBuilder<Segment>(Op::oAdd).SetResource(segment).CreatePtr();
             status = meta_->Execute<Segment>(s_ctx, result_id);
             ASSERT_TRUE(status.ok()) << status.ToString();
             ASSERT_GT(result_id, 0);
@@ -267,16 +267,16 @@ TEST_F(MetaTest, MultiThreadRequestTest) {
             auto session = meta_->CreateSession();
 
             collection->Activate();
-            auto c_ctx2 = ResourceContextBuilder<Collection>().SetResource(collection)
-                .SetOp(Op::oUpdate).AddAttr(milvus::engine::meta::F_STATE).CreatePtr();
+            auto c_ctx2 = ResourceContextBuilder<Collection>(Op::oUpdate).SetResource(collection)
+                .AddAttr(milvus::engine::meta::F_STATE).CreatePtr();
             ASSERT_TRUE(session->Apply<Collection>(c_ctx2).ok());
             partition->Activate();
-            auto p_ctx2 = ResourceContextBuilder<Partition>().SetResource(partition)
-                .SetOp(Op::oUpdate).AddAttr(milvus::engine::meta::F_STATE).CreatePtr();
+            auto p_ctx2 = ResourceContextBuilder<Partition>(Op::oUpdate).SetResource(partition)
+                .AddAttr(milvus::engine::meta::F_STATE).CreatePtr();
             ASSERT_TRUE(session->Apply<Partition>(p_ctx2).ok());
             segment->Activate();
-            auto s_ctx2 = ResourceContextBuilder<Segment>().SetResource(segment)
-                .SetOp(Op::oUpdate).AddAttr(milvus::engine::meta::F_STATE).CreatePtr();
+            auto s_ctx2 = ResourceContextBuilder<Segment>(Op::oUpdate).SetResource(segment)
+                .AddAttr(milvus::engine::meta::F_STATE).CreatePtr();
             ASSERT_TRUE(session->Apply<Segment>(s_ctx2).ok());
             std::vector<int64_t> ids;
             status = session->Commit(ids);
