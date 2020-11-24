cf36fa2e673e44d653c3b888bf8155cb9f803328
yinghao.zou <yinghao.zou@zilliz.com>
2020-11-24 17:30:02 +0800

    C1 - remove index module -g
    Signed-off-by: yinghao.zou <yinghao.zou@zilliz.com>

--- core/src/index/unittest/utils.h
+++ core/src/index/unittest/utils.h
@@ -25,7 +25,7 @@ class DataGen {
  public:
     DataGen() {
         std::string cpu_flag;
-        // here we comment A2
+        // here we comment C1
         // faiss::hook_init(cpu_flag);
         std::cout << cpu_flag << std::endl;
     }
