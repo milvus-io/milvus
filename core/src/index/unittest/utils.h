1f699728b4756aaa352b7da5831d488009e0d153
yinghao.zou <yinghao.zou@zilliz.com>
2020-11-25 14:47:57 +0800

    H4
    Signed-off-by: yinghao.zou <yinghao.zou@zilliz.com>

--- core/src/index/unittest/utils.h
+++ core/src/index/unittest/utils.h
@@ -25,7 +25,7 @@ class DataGen {
  public:
     DataGen() {
         std::string cpu_flag;
-        // here we comment H3
+        // here we comment H4
         // faiss::hook_init(cpu_flag);
         std::cout << cpu_flag << std::endl;
     }
