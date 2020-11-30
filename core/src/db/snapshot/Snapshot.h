58d83d1a32a14745b540c20ba6ac8e0dc7070f07
shengjun.li <shengjun.li@zilliz.com>
2020-11-30 14:10:15 +0800

    fix leak
    Signed-off-by: shengjun.li <shengjun.li@zilliz.com>

--- core/src/db/snapshot/Snapshot.h
+++ core/src/db/snapshot/Snapshot.h
@@ -273,6 +273,7 @@ class Snapshot : public ReferenceProxy {
         ReferenceProxy::UnRef();
         if (ref_count_ == 0) {
             UnRefAll();
+            std::apply([](auto&... holder) { ((holder.clear()), ...); }, resources_);
         }
     }
 
