a0729f92028a4e048d4d796aae957a1b7b11b41e
quicksilver <zhifeng.zhang@zilliz.com>
2020-12-02 16:50:57 +0800

    Add regression tests based on the Milvus Python SDK
    Signed-off-by: quicksilver <zhifeng.zhang@zilliz.com>

---
+++ .jenkins/modules/Regression/PythonRegression.groovy
@@ -0,0 +1,37 @@
+try {
+
+    sh 'docker-compose -p ${DOCKER_COMPOSE_PROJECT_NAME} up -d etcd'
+    sh 'docker-compose -p ${DOCKER_COMPOSE_PROJECT_NAME} up -d pulsar'
+    dir ('build/docker/deploy') {
+        sh 'docker-compose -p ${DOCKER_COMPOSE_PROJECT_NAME} pull'
+        sh 'docker-compose -p ${DOCKER_COMPOSE_PROJECT_NAME} up -d'
+    }
+
+    dir ('build/docker/test') {
+        sh 'docker pull ${SOURCE_REPO}/pytest:${SOURCE_TAG} || true'
+        sh 'docker-compose build --force-rm regression'
+        sh 'docker-compose -p ${DOCKER_COMPOSE_PROJECT_NAME} run --rm regression || true'
+        try {
+            withCredentials([usernamePassword(credentialsId: "${env.DOCKER_CREDENTIALS_ID}", usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
+                sh 'docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD} ${DOKCER_REGISTRY_URL}'
+                sh 'docker-compose push regression'
+            }
+        } catch (exc) {
+            throw exc
+        } finally {
+            sh 'docker logout ${DOKCER_REGISTRY_URL}'
+        }
+    }
+} catch(exc) {
+    throw exc
+} finally {
+    sh 'docker-compose -p ${DOCKER_COMPOSE_PROJECT_NAME} rm -f -s -v pulsar'
+    sh 'docker-compose -p ${DOCKER_COMPOSE_PROJECT_NAME} rm -f -s -v etcd'
+    dir ('build/docker/deploy') {
+        sh 'docker-compose -p ${DOCKER_COMPOSE_PROJECT_NAME} down --rmi all -v || true'
+    }
+    dir ('build/docker/test') {
+        sh 'docker-compose -p ${DOCKER_COMPOSE_PROJECT_NAME} run --rm regression /bin/bash -c "rm -rf __pycache__ && rm -rf .pytest_cache"'
+        sh 'docker-compose -p ${DOCKER_COMPOSE_PROJECT_NAME} down --rmi all -v || true'
+    }
+}
