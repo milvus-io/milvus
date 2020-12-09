611ddd1c055d8b387786eacbfec499b132dfc0dd
zw <zw@milvus.io>
2020-12-09 17:22:48 +0800

    add cluster test
    Signed-off-by: zw <zw@milvus.io>

--- .jenkins/modules/DevTest/CleanupClusterDevTest.groovy
+++ .jenkins/modules/DevTest/CleanupClusterDevTest.groovy
@@ -1,9 +1,9 @@
 def isTimeTriggeredBuild = currentBuild.getBuildCauses('hudson.triggers.TimerTrigger$TimerTriggerCause').size() != 0
 if (isTimeTriggeredBuild || "${params.IS_MANUAL_TRIGGER_TYPE}" == "True") {
 	retry(3) {
-	    def helmResult = sh script: "helm status -n milvus ${env.SHARDS_HELM_RELEASE_NAME}", returnStatus: true
+	    def helmResult = sh script: "helm status -n milvus ${env.CLUSTER_HELM_RELEASE_NAME}", returnStatus: true
 	    if (!helmResult) {
-	        sh "helm uninstall -n milvus ${env.SHARDS_HELM_RELEASE_NAME}"
+	        sh "helm uninstall -n milvus ${env.CLUSTER_HELM_RELEASE_NAME}"
 	    }
 	}
 }
---
+++ .jenkins/modules/DevTest/ClusterDevTest.groovy
@@ -0,0 +1,63 @@
+timeout(time: 150, unit: 'MINUTES') {
+    sh "mkdir -p ${env.DEV_TEST_ARTIFACTS}"
+
+    def helmCMD = "helm install --wait --timeout 300s \
+                   --set image.repository=registry.zilliz.com/milvus/engine \
+                   --set image.tag=${DOCKER_VERSION} \
+                   --set image.pullPolicy=Always \
+                   --set service.type=ClusterIP \
+                   --set image.resources.requests.memory=8Gi \
+                   --set image.resources.requests.cpu=4.0 \
+                   --set image.resources.limits.memory=14Gi \
+                   --set image.resources.limits.cpu=6.0 \
+                   -f ci/filebeat/values.yaml \
+                   -f test.yaml \
+                   --namespace milvus"
+
+    def helmStatusCMD = "helm get manifest --namespace milvus ${env.HELM_RELEASE_NAME} | kubectl describe -n milvus -f - && \
+                         kubectl logs --namespace milvus -l \"app.kubernetes.io/name=milvus,app.kubernetes.io/instance=${env.HELM_RELEASE_NAME}\" -c milvus && \
+                         helm status -n milvus ${env.HELM_RELEASE_NAME}"
+
+    def isTimeTriggeredBuild = currentBuild.getBuildCauses('hudson.triggers.TimerTrigger$TimerTriggerCause').size() != 0
+
+    dir ('milvus-helm') {
+        sh "helm version"
+        sh "helm repo add stable https://kubernetes.oss-cn-hangzhou.aliyuncs.com/charts"
+        sh "helm repo update"
+
+        def MILVUS_HELM_URL = "https://github.com/milvus-io/milvus-helm.git"
+        def REF_SPEC = "+refs/heads/${env.HELM_BRANCH}:refs/remotes/origin/${env.HELM_BRANCH}"
+        checkout([$class: 'GitSCM', branches: [[name: "${env.HELM_BRANCH}"]], userRemoteConfigs: [[url: "${MILVUS_HELM_URL}", name: 'origin', refspec: "${REF_SPEC}"]]])
+
+        retry(3) {
+            // test cluster without read nodes
+            try {
+                dir ('charts/milvus') {
+                    writeFile file: 'test.yaml', text: "extraConfiguration:\n  engine:\n    build_index_threshold: 1000\n    max_partition_num: 256"
+                    def helmCMD_mysql = "${helmCMD}" + " --set cluster.enabled=true --set readonly.replicas=0 --set persistence.enabled=true -f ci/db_backend/mysql_${BINARY_VERSION}_values.yaml ${env.CLUSTER_HELM_RELEASE_NAME} ."
+                    sh "${helmCMD_mysql}"
+                }
+            } catch (exc) {
+                sh script: helmStatusCMD, returnStatus: true
+                sh script: "helm uninstall -n milvus ${env.HELM_RELEASE_NAME} && sleep 1m", returnStatus: true
+                throw exc
+            }
+        }
+    }
+
+    dir ("tests/milvus_python_test") {
+        sh 'python3 -m pip install -r requirements.txt'
+        def TESTCASE_LEVEL = 1
+        if (isTimeTriggeredBuild) {
+            TESTCASE_LEVEL = 2
+        }
+        def pytestCMD_mysql = "pytest . \
+                               --level=${TESTCASE_LEVEL} \
+                               -n 4 \
+                               --alluredir=\"test_out/dev/cluster/mysql\" \
+                               --ip ${env.CLUSTER_HELM_RELEASE_NAME}.milvus.svc.cluster.local \
+                               --service ${env.CLUSTER_HELM_RELEASE_NAME} >> \
+                               ${WORKSPACE}/${env.DEV_TEST_ARTIFACTS}/milvus_${BINARY_VERSION}_mysql_dev_test.log"
+        sh "${pytestCMD_mysql}"
+    }
+}
