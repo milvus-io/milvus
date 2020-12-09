9373fb2c9f69680b661c2ac8ecfcca038a5517b9
yudong.cai <yudong.cai@zilliz.com>
2020-12-09 18:18:46 +0800

    clean code
    Signed-off-by: yudong.cai <yudong.cai@zilliz.com>

--- .jenkins/modules/Unittest/Unittest.groovy
+++ .jenkins/modules/Unittest/Unittest.groovy
@@ -7,11 +7,11 @@ timeout(time: 15, unit: 'MINUTES') {
         sh ". ./before-install.sh && ./run_unittest.sh -i ${env.MILVUS_INSTALL_PREFIX} -o ${coverageInfoName}"
         if (isTimeTriggeredBuild) {
             withCredentials([[$class: 'StringBinding', credentialsId: "milvus-ci-codecov-token", variable: 'CODECOV_TOKEN']]) {
-                sh "curl -s https://codecov.io/bash | bash -s - -f ${coverageInfoName} -n ${formatName} -F nightly -F ${formatFlag} || echo \"Codecov did not collect coverage reports\""
+                sh "curl -s https://codecov.io/bash | bash -s -f ${coverageInfoName} -n ${formatName} -F nightly -F ${formatFlag} || echo \"Codecov did not collect coverage reports\""
             }
         } else {
             withCredentials([[$class: 'StringBinding', credentialsId: "milvus-ci-codecov-token", variable: 'CODECOV_TOKEN']]) {
-                sh "curl -s https://codecov.io/bash | bash -s - -f ${coverageInfoName} -n ${formatName} -F ${formatFlag} || echo \"Codecov did not collect coverage reports\""
+                sh "curl -s https://codecov.io/bash | bash -s -f ${coverageInfoName} -n ${formatName} -F ${formatFlag} || echo \"Codecov did not collect coverage reports\""
             }
         }
     }
