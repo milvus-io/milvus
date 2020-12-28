cfe56cf93055c5805585e9b1991db5210912dc21
ThreadDao <zongyufen@foxmail.com>
2020-12-28 18:55:50 +0800

    add case for concurrent search
    Signed-off-by: ThreadDao <zongyufen@foxmail.com>

--- tests/milvus-java-test/src/main/java/com/TestPS.java
+++ tests/milvus-java-test/src/main/java/com/TestPS.java
@@ -1,14 +1,15 @@
-
 package com;
 
 import io.milvus.client.*;
 import org.apache.commons.cli.*;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ForkJoinPool;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class TestPS {
@@ -26,13 +27,12 @@ public class TestPS {
 
 
 
-    public static void main(String[] args) throws ConnectFailedException {
+    public static void main(String[] args) throws ConnectFailedException, ExecutionException, InterruptedException {
         int nb = 10000;
         int nq = 1;
-        int nprobe = 1024;
         int top_k = 2;
-        int loops = 100000000;
-//        int index_file_size = 1024;
+        int loops = 100000;
+        int index_file_size = 1024;
         String collectionName = "random_1m_2048_512_ip_sq8";
 
 
@@ -64,39 +64,31 @@ public class TestPS {
                 .withPort(Integer.parseInt(port))
                 .build();
         MilvusClient client = new MilvusGrpcClient(connectParam);
+        if(client.hasCollection(collectionName).hasCollection()) {
+            client.dropCollection(collectionName);
+        }
+        CollectionMapping tableSchema = new CollectionMapping.Builder(collectionName, dimension)
+            .withIndexFileSize(index_file_size)
+            .withMetricType(MetricType.L2)
+            .build();
+        Response res = client.createCollection(tableSchema);
+        List<Long> vectorIds;
+        vectorIds = Stream.iterate(0L, n -> n)
+                .limit(nb)
+                .collect(Collectors.toList());
+        InsertParam insertParam = new InsertParam.Builder(collectionName).withFloatVectors(vectors).withVectorIds(vectorIds).build();
+    for (int i = 0; i < 100; ++i) {
+        InsertResponse res_insert = client.insert(insertParam);
+        assert (res_insert.getResponse().ok());
+    }
+        System.out.println(client.countEntities(collectionName).getCollectionEntityCount());
 
-//        String collectionName = RandomStringUtils.randomAlphabetic(10);
-//        TableSchema tableSchema = new TableSchema.Builder(collectionName, dimension)
-//                .withIndexFileSize(index_file_size)
-//                .withMetricType(MetricType.IP)
-//                .build();
-//        Response res = client.createTable(tableSchema);
-//        List<Long> vectorIds;
-//        vectorIds = Stream.iterate(0L, n -> n)
-//                .limit(nb)
-//                .collect(Collectors.toList());
-//        InsertParam insertParam = new InsertParam.Builder(collectionName).withFloatVectors(vectors).withVectorIds(vectorIds).build();
-        System.setProperty("java.util.concurrent.ForkJoinPool.common.parallelism", "50");
-        ForkJoinPool executor_search = new ForkJoinPool();
-//        for (int i = 0; i < loops; i++) {
-//            List<List<Float>> queryVectors = Utils.genVectors(nq, dimension, true);
-//            executor_search.execute(
-//                    () -> {
-////                        InsertResponse res_insert = client.insert(insertParam);
-////                        assert (res_insert.getResponse().ok());
-////                        System.out.println("In insert");
-//                        String params = "{\"nprobe\":1024}";
-//                        SearchParam searchParam = new SearchParam.Builder(collectionName)
-//                                .withFloatVectors(queryVectors)
-//                                .withParamsInJson(params)
-//                                .withTopK(top_k).build();
-//                        SearchResponse res_search = client.search(searchParam);
-//                        assert (res_search.getResponse().ok());
-//                    });
-//        }
-
-        IntStream.range(0, loops).parallel().forEach(index -> {
-                        List<List<Float>> queryVectors = Utils.genVectors(nq, dimension, true);
+        ExecutorService executors = Executors.newFixedThreadPool(50);
+        List<Future> resultList = new ArrayList<Future>();
+        for (int i = 0; i < loops; i++) {
+            List<List<Float>> queryVectors = Utils.genVectors(nq, dimension, true);
+            Future future= executors.submit(
+                    () -> {
                         String params = "{\"nprobe\":1024}";
                         SearchParam searchParam = new SearchParam.Builder(collectionName)
                                 .withFloatVectors(queryVectors)
@@ -104,12 +96,26 @@ public class TestPS {
                                 .withTopK(top_k).build();
                         SearchResponse res_search = client.search(searchParam);
                         assert (res_search.getResponse().ok());
-                });
-        executor_search.awaitQuiescence(300, TimeUnit.SECONDS);
-        executor_search.shutdown();
+                    });
+            resultList.add(future);
+        }
+//        IntStream.range(0, loops).parallel().forEach(index -> {
+//                        List<List<Float>> queryVectors = Utils.genVectors(nq, dimension, true);
+//                        String params = "{\"nprobe\":1024}";
+//                        SearchParam searchParam = new SearchParam.Builder(collectionName)
+//                                .withFloatVectors(queryVectors)
+//                                .withParamsInJson(params)
+//                                .withTopK(top_k).build();
+//                        SearchResponse res_search = client.search(searchParam);
+//                        assert (res_search.getResponse().ok());
+//                });
+//        executor_search.awaitQuiescence(300, TimeUnit.SECONDS);
+        executors.shutdown();
         CountEntitiesResponse getTableRowCountResponse = client.countEntities(collectionName);
         System.out.println(getTableRowCountResponse.getCollectionEntityCount());
-
+        for (Future f: resultList) {
+            System.out.println(f.get());
+        }
 //        int thread_num = 50;
 //        ForkJoinPool executor = new ForkJoinPool();
 //        for (int i = 0; i < thread_num; i++) {
