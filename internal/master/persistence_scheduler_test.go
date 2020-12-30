4808b46246cd31197f00d83179d6ccb02d121bed
sunby <bingyi.sun@zilliz.com>
2020-12-30 19:56:54 +0800

    Filter indexable fields in flush scheduler
    Signed-off-by: sunby <bingyi.sun@zilliz.com>

--- internal/master/persistence_scheduler_test.go
+++ internal/master/persistence_scheduler_test.go
@@ -5,6 +5,8 @@ import (
 	"testing"
 	"time"
 
+	"github.com/zilliztech/milvus-distributed/internal/proto/commonpb"
+
 	"github.com/zilliztech/milvus-distributed/internal/proto/etcdpb"
 	"github.com/zilliztech/milvus-distributed/internal/proto/schemapb"
 
@@ -43,7 +45,7 @@ func TestPersistenceScheduler(t *testing.T) {
 			Name: "testcoll",
 			Fields: []*schemapb.FieldSchema{
 				{FieldID: 1},
-				{FieldID: 100},
+				{FieldID: 100, DataType: schemapb.DataType_VECTOR_FLOAT, IndexParams: []*commonpb.KeyValuePair{{Key: "k", Value: "v"}}},
 			},
 		},
 	})
