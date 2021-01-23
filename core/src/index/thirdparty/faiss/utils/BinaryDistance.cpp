1d1cb01a75230856aed3535538b85edf89c2bdd5
shengjun.li <shengjun.li@zilliz.com>
2021-01-23 17:51:20 +0800

    add license
    Signed-off-by: shengjun.li <shengjun.li@zilliz.com>

--- core/src/index/thirdparty/faiss/utils/BinaryDistance.cpp
+++ core/src/index/thirdparty/faiss/utils/BinaryDistance.cpp
@@ -1,17 +1,20 @@
+// Copyright (C) 2019-2020 Zilliz. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance
+// with the License. You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License
+// is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+// or implied. See the License for the specific language governing permissions and limitations under the License
+
 #include <faiss/utils/BinaryDistance.h>
 
-#include <vector>
-#include <memory>
-#include <stdlib.h>
-#include <stdio.h>
-#include <math.h>
-#include <assert.h>
 #include <limits.h>
 #include <omp.h>
 #include <typeinfo>
 
-
-#include <faiss/utils/Heap.h>
 #include <faiss/impl/FaissAssert.h>
 #include <faiss/utils/utils.h>
 #include <faiss/FaissHook.h>
