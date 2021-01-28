f088467ddee9130fb8a26d84323e58320ef5fa4a
sunby <bingyi.sun@zilliz.com>
2021-01-28 10:14:31 +0800

    Add watcher_test and fix some bugs
    Signed-off-by: sunby <bingyi.sun@zilliz.com>

--- internal/dataservice/watcher.go
+++ internal/dataservice/watcher.go
@@ -69,40 +69,47 @@ func (watcher *dataNodeTimeTickWatcher) StartBackgroundLoop(ctx context.Context)
 			log.Println("data node time tick watcher closed")
 			return
 		case msg := <-watcher.msgQueue:
-			segments, err := watcher.allocator.GetSealedSegments()
-			if err != nil {
-				log.Printf("get sealed segments error %s", err.Error())
+			if err := watcher.handleTimeTickMsg(msg); err != nil {
+				log.Println(err.Error())
 				continue
-			}
-			for _, id := range segments {
-				expired, err := watcher.allocator.IsAllocationsExpired(id, msg.Base.Timestamp)
-				if err != nil {
-					log.Printf("check allocations expired error %s", err.Error())
-					continue
-				}
-				if expired {
-					segmentInfo, err := watcher.meta.GetSegment(id)
-					if err != nil {
-						log.Println(err.Error())
-						continue
-					}
-					if err = watcher.meta.SetSegmentState(id, datapb.SegmentState_SegmentSealed); err != nil {
-						log.Println(err.Error())
-						continue
-					}
-					watcher.cluster.FlushSegment(&datapb.FlushSegRequest{
-						Base: &commonpb.MsgBase{
-							MsgType:   commonpb.MsgType_kShowCollections,
-							MsgID:     -1, // todo add msg id
-							Timestamp: 0,  // todo
-							SourceID:  -1, // todo
-						},
-						CollectionID: segmentInfo.CollectionID,
-						SegmentIDs:   []int64{segmentInfo.SegmentID},
-					})
-					watcher.allocator.DropSegment(id)
-				}
 			}
 		}
 	}
+}
+
+func (watcher *dataNodeTimeTickWatcher) handleTimeTickMsg(msg *msgstream.TimeTickMsg) error {
+	segments, err := watcher.allocator.GetSealedSegments()
+	if err != nil {
+		return err
+	}
+	for _, id := range segments {
+		expired, err := watcher.allocator.IsAllocationsExpired(id, msg.Base.Timestamp)
+		if err != nil {
+			log.Printf("check allocations expired error %s", err.Error())
+			continue
+		}
+		if expired {
+			segmentInfo, err := watcher.meta.GetSegment(id)
+			if err != nil {
+				log.Println(err.Error())
+				continue
+			}
+			if err = watcher.meta.SetSegmentState(id, datapb.SegmentState_SegmentSealed); err != nil {
+				log.Println(err.Error())
+				continue
+			}
+			watcher.cluster.FlushSegment(&datapb.FlushSegRequest{
+				Base: &commonpb.MsgBase{
+					MsgType:   commonpb.MsgType_kShowCollections,
+					MsgID:     -1, // todo add msg id
+					Timestamp: 0,  // todo
+					SourceID:  Params.NodeID,
+				},
+				CollectionID: segmentInfo.CollectionID,
+				SegmentIDs:   []int64{segmentInfo.SegmentID},
+			})
+			watcher.allocator.DropSegment(id)
+		}
+	}
+	return nil
 }
