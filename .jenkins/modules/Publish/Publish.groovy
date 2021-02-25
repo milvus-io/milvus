379e4d5fae1955bf194afde804d834db80476bf7
cai.zhang <cai.zhang@zilliz.com>
2021-02-25 10:09:03 +0800

    Integrate dockerfile
    Signed-off-by: cai.zhang <cai.zhang@zilliz.com>

--- .jenkins/modules/Publish/Publish.groovy
+++ .jenkins/modules/Publish/Publish.groovy
@@ -13,43 +13,10 @@ dir ('build/docker/deploy') {
         withCredentials([usernamePassword(credentialsId: "${env.DOCKER_CREDENTIALS_ID}", usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
             sh 'docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD} ${DOKCER_REGISTRY_URL}'
 
+            sh 'docker pull registry.zilliz.com/milvus-distributed/milvus-distributed-dev:latest || true'
             sh 'docker pull ${SOURCE_REPO}/master:${SOURCE_TAG} || true'
             sh 'docker-compose build --force-rm master'
             sh 'docker-compose push master'
-
-            sh 'docker pull ${SOURCE_REPO}/proxyservice:${SOURCE_TAG} || true'
-            sh 'docker-compose build --force-rm proxyservice'
-            sh 'docker-compose push proxyservice'
-
-            sh 'docker pull ${SOURCE_REPO}/proxynode:${SOURCE_TAG} || true'
-            sh 'docker-compose build --force-rm proxynode'
-            sh 'docker-compose push proxynode'
-
-            sh 'docker pull ${SOURCE_REPO}/indexservice:${SOURCE_TAG} || true'
-            sh 'docker-compose build --force-rm indexservice'
-            sh 'docker-compose push indexservice'
-
-            sh 'docker pull ${SOURCE_REPO}/indexnode:${SOURCE_TAG} || true'
-            sh 'docker-compose build --force-rm indexnode'
-            sh 'docker-compose push indexnode'
-
-            sh 'docker pull ${SOURCE_REPO}/queryservice:${SOURCE_TAG} || true'
-            sh 'docker-compose build --force-rm queryservice'
-            sh 'docker-compose push queryservice'
-
-            sh 'docker pull ${SOURCE_REPO}/dataservice:${SOURCE_TAG} || true'
-            sh 'docker-compose build --force-rm dataservice'
-            sh 'docker-compose push dataservice'
-
-            sh 'docker pull registry.zilliz.com/milvus-distributed/milvus-distributed-dev:latest || true'
-            sh 'docker pull ${SOURCE_REPO}/querynode:${SOURCE_TAG} || true'
-            sh 'docker-compose build --force-rm querynode1'
-            sh 'docker-compose push querynode1'
-
-            sh 'docker pull registry.zilliz.com/milvus-distributed/milvus-distributed-dev:latest || true'
-            sh 'docker pull ${SOURCE_REPO}/datanode:${SOURCE_TAG} || true'
-            sh 'docker-compose build --force-rm datanode'
-            sh 'docker-compose push datanode'
         }
     } catch (exc) {
         throw exc
