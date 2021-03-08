e30b18a8f19fbb7183648c0eaba4d38242f053ad
quicksilver <zhifeng.zhang@zilliz.com>
2021-03-08 19:32:46 +0800

    Add publish test images workflows
    Signed-off-by: quicksilver <zhifeng.zhang@zilliz.com>

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -7,7 +7,6 @@ export CCACHE_COMPRESSLEVEL=${CCACHE_COMPRESSLEVEL:="5"}
 export CCACHE_COMPILERCHECK=${CCACHE_COMPILERCHECK:="content"}
 export CCACHE_MAXSIZE=${CCACHE_MAXSIZE:="2G"}
 export CCACHE_DIR=${CCACHE_DIR:="${HOME}/.ccache"}
-export http_proxy="http://proxy.zilliz.tech:1088"
-export https_proxy="http://proxy.zilliz.tech:1088"
+export GOPROXY="https://goproxy.cn"
 
 set +ex
