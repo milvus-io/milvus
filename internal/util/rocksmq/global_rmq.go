b05e1d559b1ba1a11d2be91747a836508b7d6e0b
yukun <kun.yu@zilliz.com>
2021-04-20 05:13:36 +0800

    Add consume in rmq_msgstream

--- internal/util/rocksmq/global_rmq.go
+++ internal/util/rocksmq/global_rmq.go
@@ -1,13 +1,19 @@
 package rocksmq
 
-var rmq *RocksMQ
+var Rmq *RocksMQ
+
+type Consumer struct {
+	GroupName   string
+	ChannelName string
+	MsgNum      chan int
+}
 
 func InitRmq(rocksdbName string, idAllocator IDAllocator) error {
 	var err error
-	rmq, err = NewRocksMQ(rocksdbName, idAllocator)
+	Rmq, err = NewRocksMQ(rocksdbName, idAllocator)
 	return err
 }
 
 func GetRmq() *RocksMQ {
-	return rmq
+	return Rmq
 }
