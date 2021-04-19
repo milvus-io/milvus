b0e5dc376e6aa67ac1bfa723cf59d3da9d448fa2
dragondriver <jiquan.long@zilliz.com>
2021-04-20 05:03:42 +0800

    Implement the InvalidateCollectionMetaCacheTask in Proxy

--- internal/proxynode/impl.go
+++ internal/proxynode/impl.go
@@ -25,7 +25,12 @@ func (node *NodeImpl) UpdateStateCode(code internalpb2.StateCode) {
 }
 
 func (node *NodeImpl) InvalidateCollectionMetaCache(ctx context.Context, request *proxypb.InvalidateCollMetaCacheRequest) (*commonpb.Status, error) {
-	panic("implement me")
+	collectionName := request.CollectionName
+	_ = globalMetaCache.Remove(collectionName) // no need to return error, though collection may be not cached
+	return &commonpb.Status{
+		ErrorCode: commonpb.ErrorCode_SUCCESS,
+		Reason:    "",
+	}, nil
 }
 
 func (node *NodeImpl) CreateCollection(request *milvuspb.CreateCollectionRequest) (*commonpb.Status, error) {
