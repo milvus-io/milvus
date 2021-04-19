e2e5c99f410fa6418b5faae8ea38a0f60f5f990e
FluorineDog <guilin.gou@zilliz.com>
2021-04-20 03:19:18 +0800

    Enable term parser and executor

---
+++ docs/developer_guides/chap08_binlog.md
@@ -0,0 +1,223 @@
+## Binlog
+
+InsertBinlog、DeleteBinlog、DDLBinlog
+
+Binlog is stored in a columnar storage format, every column in schema should be stored in a individual file. Timestamp, schema, row id and primary key  allocated by system are four special columns. Schema column records the DDL of the collection.
+
+
+
+## Event format
+
+Binlog file consists of 4 bytes magic number and a series of events. The first event must be descriptor event. 
+
+### Event format
+
+```
++=====================================+
+| event  | timestamp         0 : 8    | create timestamp
+| header +----------------------------+
+|        | type_code         8 : 1    | event type code 
+|        +----------------------------+
+|        | server_id         9 : 4    | write node id
+|        +----------------------------+
+|        | event_length      13 : 4   | length of event, including header and data
+|        +----------------------------+
+|        | next_position    17 : 4    | offset of next event from the start of file
+|        +----------------------------+
+|        | extra_headers    21 : x-21 | reserved part
++=====================================+
+| event  | fixed part        x : y    | 
+| data   +----------------------------+
+|        | variable part              |
++=====================================+
+```
+
+
+
+### Descriptor Event format
+
+```
++=====================================+
+| event  | timestamp         0 : 8    | create timestamp
+| header +----------------------------+
+|        | type_code         8 : 1    | event type code 
+|        +----------------------------+
+|        | server_id         9 : 4    | write node id
+|        +----------------------------+
+|        | event_length      13 : 4   | length of event, including header and data
+|        +----------------------------+
+|        | next_position    17 : 4    | offset of next event from the start of file
++=====================================+
+| event  | binlog_version   21 : 2    | binlog version 
+| data   +----------------------------+
+|        | server_version   23 : 8    | write node version
+|        +----------------------------+
+|        | commit_id        31 : 8    | commit id of the programe in git
+|        +----------------------------+
+|        | header_length    39 : 1    | header length of other event
+|        +----------------------------+
+|        | collection_id    40 : 8    | collection id
+|        +----------------------------+
+|        | partition_id     48 : 8    | partition id (schema column does not need)
+|        +----------------------------+
+|        | segment_id       56 : 8    | segment id (schema column does not need)
+|        +----------------------------+
+|        | start_timestamp  64 : 1    | minimum timestamp allocated by master of all events in this file
+|        +----------------------------+
+|        | end_timestamp    65 : 1    | maximum timestamp allocated by master of all events in this file
+|        +----------------------------+
+|        | post-header      66 : n    | array of n bytes, one byte per event type that the server knows about
+|        | lengths for all            | 
+|        | event types                |
++=====================================+
+```
+
+
+
+### Type code
+
+```
+DESCRIPTOR_EVENT
+INSERT_EVENT
+DELETE_EVENT
+CREATE_COLLECTION_EVENT
+DROP_COLLECTION_EVENT
+CREATE_PARTITION_EVENT
+DROP_PARTITION_EVENT
+```
+
+DESCRIPTOR_EVENT must appear in all column files and always be the first event.
+
+INSERT_EVENT 可以出现在除DDL binlog文件外的其他列的binlog
+
+DELETE_EVENT 只能用于primary key 的binlog文件（目前只有按照primary key删除）
+
+CREATE_COLLECTION_EVENT、DROP_COLLECTION_EVENT、CREATE_PARTITION_EVENT、DROP_PARTITION_EVENT 只出现在DDL binlog文件
+
+
+
+### Event data part
+
+```
+event data part
+
+INSERT_EVENT:
++================================================+
+| event  | fixed  |  start_timestamp   x : 8     | min timestamp in this event
+| data   | part   +------------------------------+
+|        |        |  end_timestamp     x+8 : 8   | max timestamp in this event
+|        |        +------------------------------+
+|        |        |  reserved     x+16 : y-x-16  | reserved part
+|        +--------+------------------------------+
+|        |variable|  parquet payloI ad           | payload in parquet format
+|        |part    | 														 |
++================================================+
+
+other events is similar with INSERT_EVENT
+
+
+```
+
+
+
+
+
+
+
+### Example
+
+Schema
+
+​	string | int | float(optional) | vector(512)
+
+
+
+Request: 
+
+​	InsertRequest  rows(1W)
+
+​	DeleteRequest pk=1
+
+​	DropPartition partitionTag="abc"
+
+
+
+insert binlogs: 
+
+​	rowid, pk, ts, string, int, float, vector 6 files
+
+​	all events are INSERT_EVENT
+​	float column file contains some NULL value
+
+delete binlogs:
+
+​	pk, ts 2 files
+
+​	pk's events are DELETE_EVENT, ts's events are INSERT_EVENT
+
+DDL binlogs:
+
+​	ddl, ts
+
+​	ddl's event is DROP_PARTITION_EVENT, ts's event is INSERT_EVENT
+
+
+
+C++ interface
+
+```c++
+typedef void* CPayloadWriter
+typedef struct CBuffer {
+  char* data;
+  int length;
+} CBuffer
+
+typedef struct CStatus {
+  int error_code;
+  const char* error_msg;
+} CStatus
+
+  
+// C++ interface
+// writer
+CPayloadWriter NewPayloadWriter(int columnType);
+CStatus AddBooleanToPayload(CPayloadWriter payloadWriter, bool *values, int length);
+CStatus AddInt8ToPayload(CPayloadWriter payloadWriter, int8_t *values, int length);
+CStatus AddInt16ToPayload(CPayloadWriter payloadWriter, int16_t *values, int length);
+CStatus AddInt32ToPayload(CPayloadWriter payloadWriter, int32_t *values, int length);
+CStatus AddInt64ToPayload(CPayloadWriter payloadWriter, int64_t *values, int length);
+CStatus AddFloatToPayload(CPayloadWriter payloadWriter, float *values, int length);
+CStatus AddDoubleToPayload(CPayloadWriter payloadWriter, double *values, int length);
+CStatus AddOneStringToPayload(CPayloadWriter payloadWriter, char *cstr, int str_size);
+CStatus AddBinaryVectorToPayload(CPayloadWriter payloadWriter, uint8_t *values, int dimension, int length);
+CStatus AddFloatVectorToPayload(CPayloadWriter payloadWriter, float *values, int dimension, int length);
+
+CStatus FinishPayloadWriter(CPayloadWriter payloadWriter);
+CBuffer GetPayloadBufferFromWriter(CPayloadWriter payloadWriter);
+int GetPayloadLengthFromWriter(CPayloadWriter payloadWriter);
+CStatus ReleasePayloadWriter(CPayloadWriter handler);
+
+// reader
+CPayloadReader NewPayloadReader(int columnType, uint8_t *buffer, int64_t buf_size);
+CStatus GetBoolFromPayload(CPayloadReader payloadReader, bool **values, int *length);
+CStatus GetInt8FromPayload(CPayloadReader payloadReader, int8_t **values, int *length);
+CStatus GetInt16FromPayload(CPayloadReader payloadReader, int16_t **values, int *length);
+CStatus GetInt32FromPayload(CPayloadReader payloadReader, int32_t **values, int *length);
+CStatus GetInt64FromPayload(CPayloadReader payloadReader, int64_t **values, int *length);
+CStatus GetFloatFromPayload(CPayloadReader payloadReader, float **values, int *length);
+CStatus GetDoubleFromPayload(CPayloadReader payloadReader, double **values, int *length);
+CStatus GetOneStringFromPayload(CPayloadReader payloadReader, int idx, char **cstr, int *str_size);
+CStatus GetBinaryVectorFromPayload(CPayloadReader payloadReader, uint8_t **values, int *dimension, int *length);
+CStatus GetFloatVectorFromPayload(CPayloadReader payloadReader, float **values, int *dimension, int *length);
+
+int GetPayloadLengthFromReader(CPayloadReader payloadReader);
+CStatus ReleasePayloadReader(CPayloadReader payloadReader);
+
+```
+
+
+
+
+
+
+
--- internal/storage/payload_test.go
+++ internal/storage/payload_test.go
@@ -1,54 +1,426 @@
 package storage
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"github.com/zilliztech/milvus-distributed/internal/proto/schemapb"
 )
 
-func TestNewPayloadWriter(t *testing.T) {
-	w, err := NewPayloadWriter(schemapb.DataType_STRING)
-	assert.Nil(t, err)
-	assert.NotNil(t, w)
-	err = w.Close()
-	assert.Nil(t, err)
-}
+func TestPayload_ReaderandWriter(t *testing.T) {
 
-func TestPayLoadString(t *testing.T) {
-	w, err := NewPayloadWriter(schemapb.DataType_STRING)
-	assert.Nil(t, err)
-	err = w.AddOneStringToPayload("hello0")
-	assert.Nil(t, err)
-	err = w.AddOneStringToPayload("hello1")
-	assert.Nil(t, err)
-	err = w.AddOneStringToPayload("hello2")
-	assert.Nil(t, err)
-	err = w.FinishPayloadWriter()
-	assert.Nil(t, err)
-	length, err := w.GetPayloadLengthFromWriter()
-	assert.Nil(t, err)
-	assert.Equal(t, length, 3)
-	buffer, err := w.GetPayloadBufferFromWriter()
-	assert.Nil(t, err)
+	t.Run("TestBool", func(t *testing.T) {
+		w, err := NewPayloadWriter(schemapb.DataType_BOOL)
+		require.Nil(t, err)
+		require.NotNil(t, w)
 
-	r, err := NewPayloadReader(schemapb.DataType_STRING, buffer)
-	assert.Nil(t, err)
-	length, err = r.GetPayloadLengthFromReader()
-	assert.Nil(t, err)
-	assert.Equal(t, length, 3)
-	str0, err := r.GetOneStringFromPayload(0)
-	assert.Nil(t, err)
-	assert.Equal(t, str0, "hello0")
-	str1, err := r.GetOneStringFromPayload(1)
-	assert.Nil(t, err)
-	assert.Equal(t, str1, "hello1")
-	str2, err := r.GetOneStringFromPayload(2)
-	assert.Nil(t, err)
-	assert.Equal(t, str2, "hello2")
+		err = w.AddBoolToPayload([]bool{false, false, false, false})
+		assert.Nil(t, err)
+		err = w.AddDataToPayload([]bool{false, false, false, false})
+		assert.Nil(t, err)
+		err = w.FinishPayloadWriter()
+		assert.Nil(t, err)
 
-	err = r.ReleasePayloadReader()
-	assert.Nil(t, err)
-	err = w.ReleasePayloadWriter()
-	assert.Nil(t, err)
+		length, err := w.GetPayloadLengthFromWriter()
+		assert.Nil(t, err)
+		assert.Equal(t, 8, length)
+		defer w.ReleasePayloadWriter()
+
+		buffer, err := w.GetPayloadBufferFromWriter()
+		assert.Nil(t, err)
+
+		r, err := NewPayloadReader(schemapb.DataType_BOOL, buffer)
+		require.Nil(t, err)
+		length, err = r.GetPayloadLengthFromReader()
+		assert.Nil(t, err)
+		assert.Equal(t, length, 8)
+		bools, err := r.GetBoolFromPayload()
+		assert.Nil(t, err)
+		assert.ElementsMatch(t, []bool{false, false, false, false, false, false, false, false}, bools)
+		ibools, _, err := r.GetDataFromPayload()
+		bools = ibools.([]bool)
+		assert.Nil(t, err)
+		assert.ElementsMatch(t, []bool{false, false, false, false, false, false, false, false}, bools)
+		defer r.ReleasePayloadReader()
+
+	})
+
+	t.Run("TestInt8", func(t *testing.T) {
+		w, err := NewPayloadWriter(schemapb.DataType_INT8)
+		require.Nil(t, err)
+		require.NotNil(t, w)
+
+		err = w.AddInt8ToPayload([]int8{1, 2, 3})
+		assert.Nil(t, err)
+		err = w.AddDataToPayload([]int8{4, 5, 6})
+		assert.Nil(t, err)
+		err = w.FinishPayloadWriter()
+		assert.Nil(t, err)
+
+		length, err := w.GetPayloadLengthFromWriter()
+		assert.Nil(t, err)
+		assert.Equal(t, 6, length)
+		defer w.ReleasePayloadWriter()
+
+		buffer, err := w.GetPayloadBufferFromWriter()
+		assert.Nil(t, err)
+
+		r, err := NewPayloadReader(schemapb.DataType_INT8, buffer)
+		require.Nil(t, err)
+		length, err = r.GetPayloadLengthFromReader()
+		assert.Nil(t, err)
+		assert.Equal(t, length, 6)
+
+		int8s, err := r.GetInt8FromPayload()
+		assert.Nil(t, err)
+		assert.ElementsMatch(t, []int8{1, 2, 3, 4, 5, 6}, int8s)
+
+		iint8s, _, err := r.GetDataFromPayload()
+		int8s = iint8s.([]int8)
+		assert.Nil(t, err)
+
+		assert.ElementsMatch(t, []int8{1, 2, 3, 4, 5, 6}, int8s)
+		defer r.ReleasePayloadReader()
+	})
+
+	t.Run("TestInt16", func(t *testing.T) {
+		w, err := NewPayloadWriter(schemapb.DataType_INT16)
+		require.Nil(t, err)
+		require.NotNil(t, w)
+
+		err = w.AddInt16ToPayload([]int16{1, 2, 3})
+		assert.Nil(t, err)
+		err = w.AddDataToPayload([]int16{1, 2, 3})
+		assert.Nil(t, err)
+		err = w.FinishPayloadWriter()
+		assert.Nil(t, err)
+
+		length, err := w.GetPayloadLengthFromWriter()
+		assert.Nil(t, err)
+		assert.Equal(t, 6, length)
+		defer w.ReleasePayloadWriter()
+
+		buffer, err := w.GetPayloadBufferFromWriter()
+		assert.Nil(t, err)
+
+		r, err := NewPayloadReader(schemapb.DataType_INT16, buffer)
+		require.Nil(t, err)
+		length, err = r.GetPayloadLengthFromReader()
+		assert.Nil(t, err)
+		assert.Equal(t, length, 6)
+		int16s, err := r.GetInt16FromPayload()
+		assert.Nil(t, err)
+		assert.ElementsMatch(t, []int16{1, 2, 3, 1, 2, 3}, int16s)
+
+		iint16s, _, err := r.GetDataFromPayload()
+		int16s = iint16s.([]int16)
+		assert.Nil(t, err)
+		assert.ElementsMatch(t, []int16{1, 2, 3, 1, 2, 3}, int16s)
+		defer r.ReleasePayloadReader()
+	})
+
+	t.Run("TestInt32", func(t *testing.T) {
+		w, err := NewPayloadWriter(schemapb.DataType_INT32)
+		require.Nil(t, err)
+		require.NotNil(t, w)
+
+		err = w.AddInt32ToPayload([]int32{1, 2, 3})
+		assert.Nil(t, err)
+		err = w.AddDataToPayload([]int32{1, 2, 3})
+		assert.Nil(t, err)
+		err = w.FinishPayloadWriter()
+		assert.Nil(t, err)
+
+		length, err := w.GetPayloadLengthFromWriter()
+		assert.Nil(t, err)
+		assert.Equal(t, 6, length)
+		defer w.ReleasePayloadWriter()
+
+		buffer, err := w.GetPayloadBufferFromWriter()
+		assert.Nil(t, err)
+
+		r, err := NewPayloadReader(schemapb.DataType_INT32, buffer)
+		require.Nil(t, err)
+		length, err = r.GetPayloadLengthFromReader()
+		assert.Nil(t, err)
+		assert.Equal(t, length, 6)
+
+		int32s, err := r.GetInt32FromPayload()
+		assert.Nil(t, err)
+		assert.ElementsMatch(t, []int32{1, 2, 3, 1, 2, 3}, int32s)
+
+		iint32s, _, err := r.GetDataFromPayload()
+		int32s = iint32s.([]int32)
+		assert.Nil(t, err)
+		assert.ElementsMatch(t, []int32{1, 2, 3, 1, 2, 3}, int32s)
+		defer r.ReleasePayloadReader()
+	})
+
+	t.Run("TestInt64", func(t *testing.T) {
+		w, err := NewPayloadWriter(schemapb.DataType_INT64)
+		require.Nil(t, err)
+		require.NotNil(t, w)
+
+		err = w.AddInt64ToPayload([]int64{1, 2, 3})
+		assert.Nil(t, err)
+		err = w.AddDataToPayload([]int64{1, 2, 3})
+		assert.Nil(t, err)
+		err = w.FinishPayloadWriter()
+		assert.Nil(t, err)
+
+		length, err := w.GetPayloadLengthFromWriter()
+		assert.Nil(t, err)
+		assert.Equal(t, 6, length)
+		defer w.ReleasePayloadWriter()
+
+		buffer, err := w.GetPayloadBufferFromWriter()
+		assert.Nil(t, err)
+
+		r, err := NewPayloadReader(schemapb.DataType_INT64, buffer)
+		require.Nil(t, err)
+		length, err = r.GetPayloadLengthFromReader()
+		assert.Nil(t, err)
+		assert.Equal(t, length, 6)
+
+		int64s, err := r.GetInt64FromPayload()
+		assert.Nil(t, err)
+		assert.ElementsMatch(t, []int64{1, 2, 3, 1, 2, 3}, int64s)
+
+		iint64s, _, err := r.GetDataFromPayload()
+		int64s = iint64s.([]int64)
+		assert.Nil(t, err)
+		assert.ElementsMatch(t, []int64{1, 2, 3, 1, 2, 3}, int64s)
+		defer r.ReleasePayloadReader()
+	})
+
+	t.Run("TestFloat32", func(t *testing.T) {
+		w, err := NewPayloadWriter(schemapb.DataType_FLOAT)
+		require.Nil(t, err)
+		require.NotNil(t, w)
+
+		err = w.AddFloatToPayload([]float32{1.0, 2.0, 3.0})
+		assert.Nil(t, err)
+		err = w.AddDataToPayload([]float32{1.0, 2.0, 3.0})
+		assert.Nil(t, err)
+		err = w.FinishPayloadWriter()
+		assert.Nil(t, err)
+
+		length, err := w.GetPayloadLengthFromWriter()
+		assert.Nil(t, err)
+		assert.Equal(t, 6, length)
+		defer w.ReleasePayloadWriter()
+
+		buffer, err := w.GetPayloadBufferFromWriter()
+		assert.Nil(t, err)
+
+		r, err := NewPayloadReader(schemapb.DataType_FLOAT, buffer)
+		require.Nil(t, err)
+		length, err = r.GetPayloadLengthFromReader()
+		assert.Nil(t, err)
+		assert.Equal(t, length, 6)
+
+		float32s, err := r.GetFloatFromPayload()
+		assert.Nil(t, err)
+		assert.ElementsMatch(t, []float32{1.0, 2.0, 3.0, 1.0, 2.0, 3.0}, float32s)
+
+		ifloat32s, _, err := r.GetDataFromPayload()
+		float32s = ifloat32s.([]float32)
+		assert.Nil(t, err)
+		assert.ElementsMatch(t, []float32{1.0, 2.0, 3.0, 1.0, 2.0, 3.0}, float32s)
+		defer r.ReleasePayloadReader()
+	})
+
+	t.Run("TestDouble", func(t *testing.T) {
+		w, err := NewPayloadWriter(schemapb.DataType_DOUBLE)
+		require.Nil(t, err)
+		require.NotNil(t, w)
+
+		err = w.AddDoubleToPayload([]float64{1.0, 2.0, 3.0})
+		assert.Nil(t, err)
+		err = w.AddDataToPayload([]float64{1.0, 2.0, 3.0})
+		assert.Nil(t, err)
+		err = w.FinishPayloadWriter()
+		assert.Nil(t, err)
+
+		length, err := w.GetPayloadLengthFromWriter()
+		assert.Nil(t, err)
+		assert.Equal(t, 6, length)
+		defer w.ReleasePayloadWriter()
+
+		buffer, err := w.GetPayloadBufferFromWriter()
+		assert.Nil(t, err)
+
+		r, err := NewPayloadReader(schemapb.DataType_DOUBLE, buffer)
+		require.Nil(t, err)
+		length, err = r.GetPayloadLengthFromReader()
+		assert.Nil(t, err)
+		assert.Equal(t, length, 6)
+
+		float64s, err := r.GetDoubleFromPayload()
+		assert.Nil(t, err)
+		assert.ElementsMatch(t, []float64{1.0, 2.0, 3.0, 1.0, 2.0, 3.0}, float64s)
+
+		ifloat64s, _, err := r.GetDataFromPayload()
+		float64s = ifloat64s.([]float64)
+		assert.Nil(t, err)
+		assert.ElementsMatch(t, []float64{1.0, 2.0, 3.0, 1.0, 2.0, 3.0}, float64s)
+		defer r.ReleasePayloadReader()
+	})
+
+	t.Run("TestAddOneString", func(t *testing.T) {
+		w, err := NewPayloadWriter(schemapb.DataType_STRING)
+		require.Nil(t, err)
+		require.NotNil(t, w)
+
+		err = w.AddOneStringToPayload("hello0")
+		assert.Nil(t, err)
+		err = w.AddOneStringToPayload("hello1")
+		assert.Nil(t, err)
+		err = w.AddOneStringToPayload("hello2")
+		assert.Nil(t, err)
+		err = w.AddDataToPayload("hello3")
+		assert.Nil(t, err)
+		err = w.FinishPayloadWriter()
+		assert.Nil(t, err)
+		length, err := w.GetPayloadLengthFromWriter()
+		assert.Nil(t, err)
+		assert.Equal(t, length, 4)
+		buffer, err := w.GetPayloadBufferFromWriter()
+		assert.Nil(t, err)
+
+		r, err := NewPayloadReader(schemapb.DataType_STRING, buffer)
+		assert.Nil(t, err)
+		length, err = r.GetPayloadLengthFromReader()
+		assert.Nil(t, err)
+		assert.Equal(t, length, 4)
+		str0, err := r.GetOneStringFromPayload(0)
+		assert.Nil(t, err)
+		assert.Equal(t, str0, "hello0")
+		str1, err := r.GetOneStringFromPayload(1)
+		assert.Nil(t, err)
+		assert.Equal(t, str1, "hello1")
+		str2, err := r.GetOneStringFromPayload(2)
+		assert.Nil(t, err)
+		assert.Equal(t, str2, "hello2")
+		str3, err := r.GetOneStringFromPayload(3)
+		assert.Nil(t, err)
+		assert.Equal(t, str3, "hello3")
+
+		istr0, _, err := r.GetDataFromPayload(0)
+		str0 = istr0.(string)
+		assert.Nil(t, err)
+		assert.Equal(t, str0, "hello0")
+
+		istr1, _, err := r.GetDataFromPayload(1)
+		str1 = istr1.(string)
+		assert.Nil(t, err)
+		assert.Equal(t, str1, "hello1")
+
+		istr2, _, err := r.GetDataFromPayload(2)
+		str2 = istr2.(string)
+		assert.Nil(t, err)
+		assert.Equal(t, str2, "hello2")
+
+		istr3, _, err := r.GetDataFromPayload(3)
+		str3 = istr3.(string)
+		assert.Nil(t, err)
+		assert.Equal(t, str3, "hello3")
+
+		err = r.ReleasePayloadReader()
+		assert.Nil(t, err)
+		err = w.ReleasePayloadWriter()
+		assert.Nil(t, err)
+	})
+
+	t.Run("TestBinaryVector", func(t *testing.T) {
+		w, err := NewPayloadWriter(schemapb.DataType_VECTOR_BINARY)
+		require.Nil(t, err)
+		require.NotNil(t, w)
+
+		in := make([]byte, 16)
+		for i := 0; i < 16; i++ {
+			in[i] = 1
+		}
+		in2 := make([]byte, 8)
+		for i := 0; i < 8; i++ {
+			in2[i] = 1
+		}
+
+		err = w.AddBinaryVectorToPayload(in, 8)
+		assert.Nil(t, err)
+		err = w.AddDataToPayload(in2, 8)
+		assert.Nil(t, err)
+		err = w.FinishPayloadWriter()
+		assert.Nil(t, err)
+
+		length, err := w.GetPayloadLengthFromWriter()
+		assert.Nil(t, err)
+		assert.Equal(t, 24, length)
+		defer w.ReleasePayloadWriter()
+
+		buffer, err := w.GetPayloadBufferFromWriter()
+		assert.Nil(t, err)
+
+		r, err := NewPayloadReader(schemapb.DataType_VECTOR_BINARY, buffer)
+		require.Nil(t, err)
+		length, err = r.GetPayloadLengthFromReader()
+		assert.Nil(t, err)
+		assert.Equal(t, length, 24)
+
+		binVecs, dim, err := r.GetBinaryVectorFromPayload()
+		assert.Nil(t, err)
+		assert.Equal(t, 8, dim)
+		assert.Equal(t, 24, len(binVecs))
+		fmt.Println(binVecs)
+
+		ibinVecs, dim, err := r.GetDataFromPayload()
+		assert.Nil(t, err)
+		binVecs = ibinVecs.([]byte)
+		assert.Equal(t, 8, dim)
+		assert.Equal(t, 24, len(binVecs))
+		defer r.ReleasePayloadReader()
+	})
+
+	t.Run("TestFloatVector", func(t *testing.T) {
+		w, err := NewPayloadWriter(schemapb.DataType_VECTOR_FLOAT)
+		require.Nil(t, err)
+		require.NotNil(t, w)
+
+		err = w.AddFloatVectorToPayload([]float32{1.0, 2.0}, 1)
+		assert.Nil(t, err)
+		err = w.AddDataToPayload([]float32{3.0, 4.0}, 1)
+		assert.Nil(t, err)
+		err = w.FinishPayloadWriter()
+		assert.Nil(t, err)
+
+		length, err := w.GetPayloadLengthFromWriter()
+		assert.Nil(t, err)
+		assert.Equal(t, 4, length)
+		defer w.ReleasePayloadWriter()
+
+		buffer, err := w.GetPayloadBufferFromWriter()
+		assert.Nil(t, err)
+
+		r, err := NewPayloadReader(schemapb.DataType_VECTOR_FLOAT, buffer)
+		require.Nil(t, err)
+		length, err = r.GetPayloadLengthFromReader()
+		assert.Nil(t, err)
+		assert.Equal(t, length, 4)
+
+		floatVecs, dim, err := r.GetFloatVectorFromPayload()
+		assert.Nil(t, err)
+		assert.Equal(t, 1, dim)
+		assert.Equal(t, 4, len(floatVecs))
+		assert.ElementsMatch(t, []float32{1.0, 2.0, 3.0, 4.0}, floatVecs)
+
+		ifloatVecs, dim, err := r.GetDataFromPayload()
+		assert.Nil(t, err)
+		floatVecs = ifloatVecs.([]float32)
+		assert.Equal(t, 1, dim)
+		assert.Equal(t, 4, len(floatVecs))
+		assert.ElementsMatch(t, []float32{1.0, 2.0, 3.0, 4.0}, floatVecs)
+		defer r.ReleasePayloadReader()
+	})
 }
