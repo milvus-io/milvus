f2bf503f4754129015251409973b8414c976c275
quicksilver <zhifeng.zhang@zilliz.com>
2021-04-20 02:33:06 +0800

    Running as root is not permitted on build environment

---
+++ build/builder.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# Absolute path to the toplevel milvus distributed directory.
+toplevel=$(dirname "$(cd "$(dirname "${0}")"; pwd)")
+
+pushd "${toplevel}"
+
+if [ "${1-}" = "pull" ]; then
+    docker-compose pull --ignore-pull-failures ubuntu
+    exit 0
+fi
+
+if [ "${1-}" = "gdbserver" ]; then
+	mkdir -p "${DOCKER_VOLUME_DIRECTORY:-.docker}/amd64-ubuntu18.04-gdbserver-cache"
+	chmod -R 777 "${DOCKER_VOLUME_DIRECTORY:-.docker}"
+    docker-compose up -d gdbserver
+    exit 0
+fi
+
+# Attempt to run in the container with the same UID/GID as we have on the host,
+# as this results in the correct permissions on files created in the shared
+# volumes. This isn't always possible, however, as IDs less than 100 are
+# reserved by Debian, and IDs in the low 100s are dynamically assigned to
+# various system users and groups. To be safe, if we see a UID/GID less than
+# 500, promote it to 501. This is notably necessary on macOS Lion and later,
+# where administrator accounts are created with a GID of 20. This solution is
+# not foolproof, but it works well in practice.
+uid=$(id -u)
+gid=$(id -g)
+[ "$uid" -lt 500 ] && uid=501
+[ "$gid" -lt 500 ] && gid=$uid
+
+mkdir -p "${DOCKER_VOLUME_DIRECTORY:-.docker}/amd64-ubuntu18.04-cache"
+chmod -R 777 "${DOCKER_VOLUME_DIRECTORY:-.docker}"
+
+docker-compose pull --ignore-pull-failures ubuntu
+docker-compose build ubuntu
+docker-compose run --rm -u "$uid:$gid" ubuntu "$@"
+
+popd
