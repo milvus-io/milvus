fe4538c6154a0aa2419d8e1fe4cab49cc8e6d583
ophunter233 <chengming.li@zilliz.com>
2021-04-20 06:07:00 +0800

    Stop listen to dd channel in query node

--- internal/querynode/data_sync_service.go
+++ internal/querynode/data_sync_service.go
@@ -13,7 +13,6 @@ type dataSyncService struct {
 	fg  *flowgraph.TimeTickedFlowGraph
 
 	dmStream  msgstream.MsgStream
-	ddStream  msgstream.MsgStream
 	msFactory msgstream.Factory
 
 	replica collectionReplica
@@ -47,24 +46,18 @@ func (dsService *dataSyncService) initNodes() {
 	dsService.fg = flowgraph.NewTimeTickedFlowGraph(dsService.ctx)
 
 	var dmStreamNode node = dsService.newDmInputNode(dsService.ctx)
-	var ddStreamNode node = dsService.newDDInputNode(dsService.ctx)
 
 	var filterDmNode node = newFilteredDmNode(dsService.replica)
-	var ddNode node = newDDNode(dsService.replica)
 
 	var insertNode node = newInsertNode(dsService.replica)
 	var serviceTimeNode node = newServiceTimeNode(dsService.ctx, dsService.replica, dsService.msFactory)
-	var gcNode node = newGCNode(dsService.replica)
 
 	dsService.fg.AddNode(dmStreamNode)
-	dsService.fg.AddNode(ddStreamNode)
 
 	dsService.fg.AddNode(filterDmNode)
-	dsService.fg.AddNode(ddNode)
 
 	dsService.fg.AddNode(insertNode)
 	dsService.fg.AddNode(serviceTimeNode)
-	dsService.fg.AddNode(gcNode)
 
 	// dmStreamNode
 	var err = dsService.fg.SetEdges(dmStreamNode.Name(),
@@ -75,31 +68,13 @@ func (dsService *dataSyncService) initNodes() {
 		log.Fatal("set edges failed in node:", dmStreamNode.Name())
 	}
 
-	// ddStreamNode
-	err = dsService.fg.SetEdges(ddStreamNode.Name(),
-		[]string{},
-		[]string{ddNode.Name()},
-	)
-	if err != nil {
-		log.Fatal("set edges failed in node:", ddStreamNode.Name())
-	}
-
 	// filterDmNode
 	err = dsService.fg.SetEdges(filterDmNode.Name(),
-		[]string{dmStreamNode.Name(), ddNode.Name()},
+		[]string{dmStreamNode.Name()},
 		[]string{insertNode.Name()},
 	)
 	if err != nil {
 		log.Fatal("set edges failed in node:", filterDmNode.Name())
-	}
-
-	// ddNode
-	err = dsService.fg.SetEdges(ddNode.Name(),
-		[]string{ddStreamNode.Name()},
-		[]string{filterDmNode.Name()},
-	)
-	if err != nil {
-		log.Fatal("set edges failed in node:", ddNode.Name())
 	}
 
 	// insertNode
@@ -114,17 +89,9 @@ func (dsService *dataSyncService) initNodes() {
 	// serviceTimeNode
 	err = dsService.fg.SetEdges(serviceTimeNode.Name(),
 		[]string{insertNode.Name()},
-		[]string{gcNode.Name()},
+		[]string{},
 	)
 	if err != nil {
 		log.Fatal("set edges failed in node:", serviceTimeNode.Name())
-	}
-
-	// gcNode
-	err = dsService.fg.SetEdges(gcNode.Name(),
-		[]string{serviceTimeNode.Name()},
-		[]string{})
-	if err != nil {
-		log.Fatal("set edges failed in node:", gcNode.Name())
 	}
 }
