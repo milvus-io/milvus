6bf9a86441aaf54669af83fb9f8badbd5a4dd782
quicksilver <zhifeng.zhang@zilliz.com>
2021-04-20 03:25:54 +0800

    Show ccache statistics

--- .jenkins/modules/Build/Build.groovy
+++ .jenkins/modules/Build/Build.groovy
@@ -6,7 +6,10 @@ timeout(time: 20, unit: 'MINUTES') {
         sh '. ./before-install.sh && unset http_proxy && unset https_proxy && ./check_cache.sh -l $GO_MOD_ARTFACTORY_URL --cache_dir=\$GOPATH/pkg/mod -f milvus-distributed-go-mod-cache.tar.gz || echo \"Go mod artfactory files not found!\"'
     }
 
+    // Zero the cache statistics (but not the configuration options)
+    sh 'ccache -z'
     sh '. ./scripts/before-install.sh && make install'
+    sh 'echo -e "===\n=== ccache statistics after build\n===" && ccache --show-stats'
 
     dir ("scripts") {
         withCredentials([usernamePassword(credentialsId: "${env.JFROG_CREDENTIALS_ID}", usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
