82c3e0a2430411dd6f3d374cc2dd642f7f778762
neza2017 <yefu.chen@zilliz.com>
2021-04-20 04:41:30 +0800

    Use FlushCompletedMsg instead

--- internal/msgstream/msg.go
+++ internal/msgstream/msg.go
@@ -105,7 +105,6 @@ func (it *InsertMsg) Unmarshal(input []byte) (TsMsg, error) {
 }
 
 /////////////////////////////////////////FlushCompletedMsg//////////////////////////////////////////
-// GOOSE TODO remove this
 type FlushCompletedMsg struct {
 	BaseMsg
 	internalpb2.SegmentFlushCompletedMsg
@@ -688,43 +687,5 @@ func (sim *SegmentInfoMsg) Unmarshal(input []byte) (TsMsg, error) {
 	}
 	return &SegmentInfoMsg{
 		SegmentMsg: segMsg,
-	}, nil
-}
-
-/////////////////////////////////////////SegmentFlushCompletedMsg//////////////////////////////////////////
-type SegmentFlushCompletedMsg struct {
-	BaseMsg
-	datapb.SegmentFlushCompletedMsg
-}
-
-func (sfm *SegmentFlushCompletedMsg) Type() MsgType {
-	return sfm.Base.MsgType
-}
-
-func (sfm *SegmentFlushCompletedMsg) GetMsgContext() context.Context {
-	return sfm.MsgCtx
-}
-
-func (sfm *SegmentFlushCompletedMsg) SetMsgContext(ctx context.Context) {
-	sfm.MsgCtx = ctx
-}
-
-func (sfm *SegmentFlushCompletedMsg) Marshal(input TsMsg) ([]byte, error) {
-	sfmsg := input.(*SegmentFlushCompletedMsg)
-	mb, err := proto.Marshal(&sfmsg.SegmentFlushCompletedMsg)
-	if err != nil {
-		return nil, err
-	}
-	return mb, nil
-}
-
-func (sfm *SegmentFlushCompletedMsg) Unmarshal(input []byte) (TsMsg, error) {
-	sfmsg := datapb.SegmentFlushCompletedMsg{}
-	err := proto.Unmarshal(input, &sfmsg)
-	if err != nil {
-		return nil, err
-	}
-	return &SegmentFlushCompletedMsg{
-		SegmentFlushCompletedMsg: sfmsg,
 	}, nil
 }
