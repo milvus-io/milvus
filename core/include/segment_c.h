2bdb27c9b75c90679a1cd646d881f3944ab23909
bigsheeper <yihao.dai@zilliz.com>
2021-04-20 01:18:42 +0800

    Fix reader unites

--- core/include/segment_c.h
+++ core/include/segment_c.h
@@ -6,17 +6,33 @@ extern "C" {
 
 typedef void* CSegmentBase;
 
-CSegmentBase NewSegment(CPartition partition, unsigned long segment_id);
+CSegmentBase
+NewSegment(CPartition partition, unsigned long segment_id);
 
-void DeleteSegment(CSegmentBase segment);
+void
+DeleteSegment(CSegmentBase segment);
 
-int Insert(CSegmentBase c_segment,
-                signed long int size,
-                const unsigned long* primary_keys,
-                const unsigned long int* timestamps,
-                void* raw_data,
-                int sizeof_per_row,
-                signed long int count);
+int
+Insert(CSegmentBase c_segment,
+           signed long int size,
+           const unsigned long* primary_keys,
+           const unsigned long* timestamps,
+           void* raw_data,
+           int sizeof_per_row,
+           signed long int count);
+
+int
+Delete(CSegmentBase c_segment,
+           long size,
+           const unsigned long* primary_keys,
+           const unsigned long* timestamps);
+
+int
+Search(CSegmentBase c_segment,
+           void* fake_query,
+           unsigned long timestamp,
+           long int* result_ids,
+           float* result_distances);
 
 #ifdef __cplusplus
 }
