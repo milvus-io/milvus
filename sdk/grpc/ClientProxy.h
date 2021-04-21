142936ebcb2c18b1068e3e22ac85107087e007a5
yhmo <yihua.mo@zilliz.com>
2021-04-21 16:03:43 +0800

    Refine C++ sdk code
    Signed-off-by: yhmo <yihua.mo@zilliz.com>

--- sdk/grpc/ClientProxy.h
+++ sdk/grpc/ClientProxy.h
@@ -125,20 +125,6 @@ class ClientProxy : public Connection {
     Status
     Compact(const std::string& collection_name) override;
 
-    /*******************************New Interface**********************************/
-
-    Status
-    CreateHybridCollection(const HMapping& mapping) override;
-
-    Status
-    InsertEntity(const std::string& collection_name, const std::string& partition_tag, HEntity& entities,
-                 std::vector<uint64_t>& id_array) override;
-
-    Status
-    HybridSearch(const std::string& collection_name, const std::vector<std::string>& partition_list,
-                 BooleanQueryPtr& boolean_query, const std::string& extra_params,
-                 TopKQueryResult& topk_query_result) override;
-
  private:
     std::shared_ptr<::grpc::Channel> channel_;
     std::shared_ptr<GrpcClient> client_ptr_;
