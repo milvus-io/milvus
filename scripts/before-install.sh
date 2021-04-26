78fec61471219d96ca668d18dfd4f9d1b2905540
quicksilver <zhifeng.zhang@zilliz.com>
2021-04-26 15:02:43 +0800

    Used for local development or CI in Kubernetes in Docker
    Signed-off-by: quicksilver <zhifeng.zhang@zilliz.com>

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -7,6 +7,6 @@ export CCACHE_COMPRESSLEVEL=${CCACHE_COMPRESSLEVEL:="5"}
 export CCACHE_COMPILERCHECK=${CCACHE_COMPILERCHECK:="content"}
 export CCACHE_MAXSIZE=${CCACHE_MAXSIZE:="2G"}
 export CCACHE_DIR=${CCACHE_DIR:="${HOME}/.ccache"}
-export GOPROXY="https://goproxy.cn"
+export GOPROXY=${GOPROXY:="https://goproxy.cn"}
 
 set +ex
