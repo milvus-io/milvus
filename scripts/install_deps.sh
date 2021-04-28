eb338d462082f36515ad58788325e8969e552d3a
quicksilver <zhifeng.zhang@zilliz.com>
2021-04-28 14:46:18 +0800

    Provide install dependencies for Ubuntu and CentOS (#5076)
    * Provide install dependencies for Ubuntu and CentOS
    
    Signed-off-by: quicksilver <zhifeng.zhang@zilliz.com>
    
    * Update scripts/install_deps.sh
    
    Signed-off-by: quicksilver <zhifeng.zhang@zilliz.com>

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env bash
 
-sudo apt install -y g++ gcc make libssl-dev zlib1g-dev libboost-regex-dev \
-libboost-program-options-dev libboost-system-dev libboost-filesystem-dev \
-libboost-serialization-dev python3-dev libboost-python-dev libcurl4-openssl-dev gfortran libtbb-dev
+if [[ ! -x "$(command -v apt)" ]]; then
+    sudo apt install -y g++ gcc make libssl-dev zlib1g-dev libboost-regex-dev \
+    libboost-program-options-dev libboost-system-dev libboost-filesystem-dev \
+    libboost-serialization-dev python3-dev libboost-python-dev libcurl4-openssl-dev gfortran libtbb-dev
+elif [[ ! -x "$(command -v yum)" ]]; then
+    sudo yum install -y epel-release centos-release-scl-rh \
+      && sudo yum install -y make automake openssl-devel zlib-devel tbb-devel \
+      libcurl-devel python3-devel boost-devel boost-python \
+      devtoolset-7-gcc devtoolset-7-gcc-c++ devtoolset-7-gcc-gfortran \
+      llvm-toolset-7.0-clang llvm-toolset-7.0-clang-tools-extra
 
+    echo "source scl_source enable devtoolset-7" | sudo tee -a /etc/profile.d/devtoolset-7.sh
+    echo "source scl_source enable llvm-toolset-7.0" | sudo tee -a /etc/profile.d/llvm-toolset-7.sh
+    echo "export CLANG_TOOLS_PATH=/opt/rh/llvm-toolset-7.0/root/usr/bin" | sudo tee -a /etc/profile.d/llvm-toolset-7.sh
+else
+    echo "Error Install Dependencies ..."
+    exit 1
+fi
