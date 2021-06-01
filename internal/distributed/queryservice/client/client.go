ca3f5b8f61984b388fc05f6ba5589a92053c41b5
godchen <qingxiang.chen@zilliz.com>
2021-06-01 17:46:32 +0800

    Fix queryservice connect error (#5526)
    Signed-off-by: godchen <qingxiang.chen@zilliz.com>

--- internal/distributed/queryservice/client/client.go
+++ internal/distributed/queryservice/client/client.go
@@ -74,10 +74,10 @@ func NewClient(ctx context.Context, address, metaRootPath string, etcdAddr []str
 
 func (c *Client) Init() error {
 	tracer := opentracing.GlobalTracer()
-	ctx, cancel := context.WithTimeout(context.Background(), c.timeout)
-	defer cancel()
 	if c.addr != "" {
 		connectGrpcFunc := func() error {
+			ctx, cancel := context.WithTimeout(context.Background(), c.timeout)
+			defer cancel()
 			log.Debug("queryservice connect ", zap.String("address", c.addr))
 			conn, err := grpc.DialContext(ctx, c.addr, grpc.WithInsecure(), grpc.WithBlock(),
 				grpc.WithUnaryInterceptor(
