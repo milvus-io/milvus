ad4a9a6eae8c9a033fa83fcc3598b6e7de7d826b
wangting0128 <ting.wang@zilliz.com>
2021-06-07 16:09:00 +0800

    [skip ci] Update code of test_info
    Signed-off-by: wangting0128 <ting.wang@zilliz.com>

--- tests20/python_client/config/test_info.py
+++ tests20/python_client/config/test_info.py
@@ -12,9 +12,16 @@ class TestInfo:
             log_path = os.environ[var]
             return str(log_path)
         except Exception as e:
-            log_path = "/tmp/ci_logs"
+            log_path = "/tmp/ci_logs/"
             print("Failed to get environment variables : %s, Use default path : %s" % (str(e), log_path))
             return log_path
+
+    @staticmethod
+    def create_path(log_path):
+        if not os.path.isdir(str(log_path)):
+            print("[modify_file] folder(%s) is not exist." % log_path)
+            print("[modify_file] create path now...")
+            os.makedirs(log_path)
 
     def get_default_config(self):
         """ Make sure the path exists """
@@ -22,6 +29,8 @@ class TestInfo:
         self.log_debug = "%s/ci_test_log.debug" % self.log_dir
         self.log_info = "%s/ci_test_log.log" % self.log_dir
         self.log_err = "%s/ci_test_log.err" % self.log_dir
+
+        self.create_path(self.log_dir)
 
 
 test_info = TestInfo()
