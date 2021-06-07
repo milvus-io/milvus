cfb91ef045d4a589d514550967c53d9f3c15f5d9
quicksilver <zhifeng.zhang@zilliz.com>
2021-06-07 15:37:53 +0800

    [skip ci] Automatically detect [cronjob] messages in your last commit
    Signed-off-by: quicksilver <zhifeng.zhang@zilliz.com>

--- build/ci/jenkins/NightlyCI.groovy
+++ build/ci/jenkins/NightlyCI.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/env groovy
 
-// When you include either a [cronjob] tag within the commit message,
-// scheduling a job that gets automatically triggered by changes.
+// When scheduling a job that gets automatically triggered by changes
+// you need to include either a [cronjob] tag within the commit message.
 String cron_timezone = "TZ=Asia/Shanghai"
 String cron_string = BRANCH_NAME == "master" ? "50 20,22,0,6,11,16 * * * " : ""
 
