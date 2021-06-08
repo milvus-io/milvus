8fb4811eb2e9a816cabe518115fa0a6b43461978
sunby <bingyi.sun@zilliz.com>
2021-06-08 11:01:40 +0800

    Fix params in GetInsertBinlogPaths
    Signed-off-by: sunby <bingyi.sun@zilliz.com>

--- internal/dataservice/grpc_services.go
+++ internal/dataservice/grpc_services.go
@@ -194,7 +194,7 @@ func (s *Server) GetInsertBinlogPaths(ctx context.Context, req *datapb.GetInsert
 			ErrorCode: commonpb.ErrorCode_UnexpectedError,
 		},
 	}
-	p := path.Join(Params.SegmentFlushMetaPath, strconv.FormatInt(req.SegmentID, 10))
+	p := path.Join(Params.SegmentBinlogSubPath, strconv.FormatInt(req.SegmentID, 10))
 	_, values, err := s.kvClient.LoadWithPrefix(p)
 	if err != nil {
 		resp.Status.Reason = err.Error()
