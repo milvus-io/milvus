a74eeefca2363255df5a695e79cc9683b8553344
zw <zw@milvus.io>
2021-06-08 15:14:13 +0800

    update index case
    Signed-off-by: zw <zw@milvus.io>

--- tests20/python_client/base/index_wrapper.py
+++ tests20/python_client/base/index_wrapper.py
@@ -10,19 +10,19 @@ from utils.api_request import api_request
 class ApiIndexWrapper:
     index = None
 
-    def init_index(self, collection, field_name, index_params, name="",check_task=None, check_params=None, **kwargs):
+    def init_index(self, collection, field_name, index_params, name="",check_task=None, check_items=None, **kwargs):
         """ In order to distinguish the same name of index """
         func_name = sys._getframe().f_code.co_name
         res, is_succ = api_request([Index, collection, field_name, index_params, name], **kwargs)
         self.index = res if is_succ is True else None
-        check_result = ResponseChecker(res, func_name, check_task, check_params, is_succ, collection=collection, field_name=field_name,
+        check_result = ResponseChecker(res, func_name, check_task, check_items, is_succ, collection=collection, field_name=field_name,
                                        index_params=index_params, name=name, **kwargs).run()
         return res, check_result
 
-    def drop(self, check_task=None, check_params=None, **kwargs):
+    def drop(self, check_task=None, check_items=None, **kwargs):
         func_name = sys._getframe().f_code.co_name
         res, is_succ = api_request([self.index.drop], **kwargs)
-        check_result = ResponseChecker(res, func_name, check_task, check_params, is_succ, **kwargs).run()
+        check_result = ResponseChecker(res, func_name, check_task, check_items, is_succ, **kwargs).run()
         return res, check_result
 
     @property
