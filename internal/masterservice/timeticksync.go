ca7f0c74975024639e9657f52c4a4d72b18f9b99
yudong.cai <yudong.cai@zilliz.com>
2021-06-11 14:14:46 +0800

    update timeticksync
    Signed-off-by: yudong.cai <yudong.cai@zilliz.com>

--- internal/masterservice/timeticksync.go
+++ internal/masterservice/timeticksync.go
@@ -69,6 +69,9 @@ func (t *timetickSync) UpdateTimeTick(in *internalpb.ChannelTimeTickMsg) error {
 	if len(in.ChannelNames) == 0 {
 		return nil
 	}
+	if len(in.Timestamps) != len(in.ChannelNames) {
+		return fmt.Errorf("Invalid TimeTickMsg")
+	}
 
 	prev, ok := t.proxyTimeTick[in.Base.SourceID]
 	if !ok {
