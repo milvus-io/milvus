bbb3f9005166d1fd9a12acedb7438a62bab5caf0
yanliang567 <82361606+yanliang567@users.noreply.github.com>
2021-06-17 11:49:57 +0800

    Update tests chaos and remove useless code (#5821)
    * [skip ci] Add flush checker in monitor thread
    
    Signed-off-by: yanliang567 <yanliang.qiao@zilliz.com>
    
    * [skip ci] update chaos parser
    
    Signed-off-by: yanliang567 <yanliang.qiao@zilliz.com>
    
    * [skip ci] Update chaos tests
    
    Signed-off-by: yanliang567 <yanliang.qiao@zilliz.com>
    
    * [skip ci] Move chaos to python_client folder
    
    Signed-off-by: yanliang567 <yanliang.qiao@zilliz.com>
    
    * [skip ci] Remove useless code and comments
    
    Signed-off-by: yanliang567 <yanliang.qiao@zilliz.com>
    
    * [skip ci] Use delayed assert for always assert everything
    
    Signed-off-by: yanliang567 <yanliang.qiao@zilliz.com>

--- tests20/python_client/base/collection_wrapper.py
+++ tests20/python_client/base/collection_wrapper.py
@@ -19,74 +19,40 @@ class ApiCollectionWrapper:
         return res, check_result
 
     @property
-    def schema(self, check_task=None, check_items=None):
+    def schema(self):
         return self.collection.schema
-        # func_name = sys._getframe().f_code.co_name
-        # res, check = func_req([self.collection.schema])
-        # check_result = CheckFunc(res, func_name, check_task, check_items, check).run()
-        # return res, check_result
 
     @property
-    def description(self, check_task=None, check_items=None):
+    def description(self):
         return self.collection.description
-        # func_name = sys._getframe().f_code.co_name
-        # res, check = func_req([self.collection.description])
-        # check_result = CheckFunc(res, func_name, check_task, check_items, check).run()
-        # return res, check_result
 
     @property
-    def name(self, check_task=None, check_items=None):
+    def name(self):
         return self.collection.name
-        # func_name = sys._getframe().f_code.co_name
-        # res, check = func_req([self.collection.name])
-        # check_result = CheckFunc(res, func_name, check_task, check_items, check).run()
-        # return res, check_result
 
     @property
-    def is_empty(self, check_task=None, check_items=None):
+    def is_empty(self):
         return self.collection.is_empty
-        # func_name = sys._getframe().f_code.co_name
-        # res, check = func_req([self.collection.is_empty])
-        # check_result = CheckFunc(res, func_name, check_task, check_items, check).run()
-        # return res, check_result
 
     @property
-    def num_entities(self, check_task=None, check_items=None):
+    def num_entities(self):
         return self.collection.num_entities
-        # func_name = sys._getframe().f_code.co_name
-        # res, check = func_req([self.collection.num_entities])
-        # check_result = CheckFunc(res, func_name, check_task, check_items, check).run()
-        # return res, check_result
 
     @property
-    def primary_field(self, check_task=None, check_items=None):
+    def primary_field(self):
         return self.collection.primary_field
-        # func_name = sys._getframe().f_code.co_name
-        # res, check = func_req([self.collection.primary_field])
-        # check_result = CheckFunc(res, func_name, check_task, check_items, check).run()
-        # return res, check_result
 
     def drop(self, check_task=None, check_items=None, **kwargs):
-        log.info("Dropping collection")
         func_name = sys._getframe().f_code.co_name
         res, check = api_request([self.collection.drop], **kwargs)
         check_result = ResponseChecker(res, func_name, check_task, check_items, check, **kwargs).run()
-        if check_result:
-            log.info("Dropped collection")
-        else:
-            log.info("Dropping collection failed")
         return res, check_result
 
     def load(self, partition_names=None, check_task=None, check_items=None, **kwargs):
-        log.info("loading data")
         func_name = sys._getframe().f_code.co_name
         res, check = api_request([self.collection.load, partition_names], **kwargs)
         check_result = ResponseChecker(res, func_name, check_task, check_items, check,
                                        partition_names=partition_names, **kwargs).run()
-        if check_result:
-            log.info("loaded data")
-        else:
-            log.info("loading failed")
         return res, check_result
 
     def release(self, check_task=None, check_items=None, **kwargs):
@@ -118,12 +84,8 @@ class ApiCollectionWrapper:
         return res, check_result
 
     @property
-    def partitions(self, check_task=None, check_items=None):
+    def partitions(self):
         return self.collection.partitions
-        # func_name = sys._getframe().f_code.co_name
-        # res, check = func_req([self.collection.partitions])
-        # check_result = CheckFunc(res, func_name, check_task, check_items, check).run()
-        # return res, check_result
 
     def partition(self, partition_name, check_task=None, check_items=None):
         func_name = sys._getframe().f_code.co_name
@@ -153,12 +115,8 @@ class ApiCollectionWrapper:
         return res, check_result
 
     @property
-    def indexes(self, check_task=None, check_items=None):
+    def indexes(self):
         return self.collection.indexes
-        # func_name = sys._getframe().f_code.co_name
-        # res, check = func_req([self.collection.indexes])
-        # check_result = CheckFunc(res, func_name, check_task, check_items, check).run()
-        # return res, check_result
 
     def index(self, check_task=None, check_items=None):
         func_name = sys._getframe().f_code.co_name
