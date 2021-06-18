343cc85397b4b62c48dd1a67f4d35ca50eac3fa5
yudong.cai <yudong.cai@zilliz.com>
2021-06-18 18:26:25 +0800

    update scripts
    Signed-off-by: yudong.cai <yudong.cai@zilliz.com>

--- scripts/run_go_unittest.sh
+++ scripts/run_go_unittest.sh
@@ -15,17 +15,20 @@ MILVUS_DIR="${ROOT_DIR}/internal/"
 echo $MILVUS_DIR
 
 go test -race -cover "${MILVUS_DIR}/util/sessionutil/..." -failfast
-go test -race -cover "${MILVUS_DIR}/kv/..." "${MILVUS_DIR}/tso/..." "${MILVUS_DIR}/allocator/..." -failfast
+go test -race -cover "${MILVUS_DIR}/kv/..." -failfast
+go test -race -cover "${MILVUS_DIR}/tso/..." -failfast
+go test -race -cover "${MILVUS_DIR}/allocator/..." -failfast
+go test -race -cover "${MILVUS_DIR}/msgstream/..." -failfast
+#go test -race -cover "${MILVUS_DIR}/storage" -failfast
+#go test -race -cover "${MILVUS_DIR}/util/..." -failfast
+
 # TODO: remove to distributed
 #go test -race -cover "${MILVUS_DIR}/proxynode/..." -failfast
 go test -race -cover "${MILVUS_DIR}/datanode/..." -failfast
 go test -race -cover "${MILVUS_DIR}/indexnode/..." -failfast
-#go test -race -cover "${MILVUS_DIR}/msgstream/..." "${MILVUS_DIR}/querynode/..." "${MILVUS_DIR}/storage"   "${MILVUS_DIR}/util/..." -failfast
-go test -cover "${MILVUS_DIR}/querynode/..." -failfast
-#go test -race -cover "${MILVUS_DIR}/msgstream/..." "${MILVUS_DIR}/storage"   "${MILVUS_DIR}/util/..." -failfast
-#go test -race -cover "${MILVUS_DIR}/msgstream/..." "${MILVUS_DIR}/util/..." -failfast
-go test -race -cover "${MILVUS_DIR}/msgstream/..." -failfast
+go test -race -cover "${MILVUS_DIR}/querynode/..." -failfast
 
-go test -race -cover -v "${MILVUS_DIR}/masterservice" "${MILVUS_DIR}/distributed/masterservice" -failfast
-#go test -race -cover "${MILVUS_DIR}/kv/..." "${MILVUS_DIR}/msgstream/..." "${MILVUS_DIR}/master/..." "${MILVUS_DIR}/querynode/..." -failfast
-go test -race -cover "${MILVUS_DIR}/dataservice/..." -failfast
+go test -race -cover -v "${MILVUS_DIR}/distributed/rootcoord" -failfast
+go test -race -cover -v "${MILVUS_DIR}/rootcoord" -failfast
+go test -race -cover -v "${MILVUS_DIR}/dataservice/..." -failfast
+go test -race -cover -v "${MILVUS_DIR}/indexservice/..." -failfast
