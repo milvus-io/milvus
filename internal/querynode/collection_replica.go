bbd8a7e13a65b5e4b7c6f3607df8127a9e7def1c
bigsheeper <yihao.dai@zilliz.com>
2021-06-19 18:38:07 +0800

    Add released partitions and fix search error in empty partition (#5893)
    * rename flowgraphType to loadType
    
    Signed-off-by: bigsheeper <yihao.dai@zilliz.com>
    
    * add load type and released partitions
    
    Signed-off-by: bigsheeper <yihao.dai@zilliz.com>
    
    * filter released partition
    
    Signed-off-by: bigsheeper <yihao.dai@zilliz.com>

--- internal/querynode/collection_replica.go
+++ internal/querynode/collection_replica.go
@@ -92,6 +92,8 @@ type collectionReplica struct {
 	partitions  map[UniqueID]*Partition
 	segments    map[UniqueID]*Segment
 
+	loadType
+
 	excludedSegments map[UniqueID][]*datapb.SegmentInfo // map[collectionID]segmentIDs
 
 	etcdKV *etcdkv.EtcdKV
