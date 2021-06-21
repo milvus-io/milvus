b2939e5cd18f9b1f52330ceb986f28e321352307
quicksilver <zhifeng.zhang@zilliz.com>
2021-06-21 12:00:06 +0800

    Set milvus cluster deployment default to false (#5910)
    Signed-off-by: quicksilver <zhifeng.zhang@zilliz.com>

--- build/ci/jenkins/NightlyCI.groovy
+++ build/ci/jenkins/NightlyCI.groovy
@@ -44,7 +44,6 @@ pipeline {
                     DOCKER_CREDENTIALS_ID = "ba070c98-c8cc-4f7c-b657-897715f359fc"
                     DOKCER_REGISTRY_URL = "registry.zilliz.com"
                     TARGET_REPO = "${DOKCER_REGISTRY_URL}/milvus"
-                    MILVUS_HELM_BRANCH = "rename"
                 }
                 stages {
                     stage('Test') {
@@ -52,13 +51,13 @@ pipeline {
                             container('main') {
                                 dir ('tests/scripts') {
                                     script {
-                                        def standaloneEnabled = "true"
+                                        def clusterEnabled = "false"
                                         if ("${MILVUS_SERVER_TYPE}" == "distributed") {
-                                            standaloneEnabled = "false"
+                                            clusterEnabled = "true"
                                         }
 
                                         sh """
-                                        MILVUS_STANDALONE_ENABLED=${standaloneEnabled} \
+                                        MILVUS_CLUSTER_ENABLED=${clusterEnabled} \
                                         ./e2e-k8s.sh \
                                         --node-image registry.zilliz.com/kindest/node:v1.20.2
                                         """
