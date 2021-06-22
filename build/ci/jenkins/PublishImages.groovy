01dac73080b1f730055d068036877a9a07a60039
quicksilver <zhifeng.zhang@zilliz.com>
2021-06-22 14:02:08 +0800

    [skip ci] Update PublishImages.groovy (#5964)
    Signed-off-by: quicksilver <zhifeng.zhang@zilliz.com>

--- build/ci/jenkins/PublishImages.groovy
+++ build/ci/jenkins/PublishImages.groovy
@@ -36,11 +36,11 @@ pipeline {
                         def gitShortCommit = sh(returnStdout: true, script: 'git rev-parse --short HEAD').trim()
 
                         withCredentials([usernamePassword(credentialsId: "${env.DOCKER_CREDENTIALS_ID}", usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
-                            sh 'docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}'
                             sh """
                                 export MILVUS_IMAGE_REPO="${env.TARGET_REPO}/milvus-dev"
                                 export MILVUS_IMAGE_TAG="${env.BRANCH_NAME}-${date}-${gitShortCommit}"
                                 build/build_image.sh
+                                docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}
                                 docker push \${MILVUS_IMAGE_REPO}:\${MILVUS_IMAGE_TAG}
                                 docker tag \${MILVUS_IMAGE_REPO}:\${MILVUS_IMAGE_TAG} \${MILVUS_IMAGE_REPO}:${env.BRANCH_NAME}-latest
                                 docker push \${MILVUS_IMAGE_REPO}:${env.BRANCH_NAME}-latest
