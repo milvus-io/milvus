22b2b7dbbe0e34376100c7dbd008f231392ecbb0
quicksilver <zhifeng.zhang@zilliz.com>
2021-06-22 14:14:51 +0800

    [skip ci] Update PublishImages.groovy (#5966)
    Signed-off-by: quicksilver <zhifeng.zhang@zilliz.com>

--- build/ci/jenkins/PublishImages.groovy
+++ build/ci/jenkins/PublishImages.groovy
@@ -22,7 +22,7 @@ pipeline {
     environment {
         IMAGE_REPO = "dockerhub-mirror-sh.zilliz.cc/milvusdb"
         DOCKER_CREDENTIALS_ID = "f0aacc8e-33f2-458a-ba9e-2c44f431b4d2"
-        TARGET_REPO = "milvus"
+        TARGET_REPO = "milvusdb"
     }
 
     stages {
@@ -36,11 +36,11 @@ pipeline {
                         def gitShortCommit = sh(returnStdout: true, script: 'git rev-parse --short HEAD').trim()
 
                         withCredentials([usernamePassword(credentialsId: "${env.DOCKER_CREDENTIALS_ID}", usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
+                            sh 'docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}'
                             sh """
                                 export MILVUS_IMAGE_REPO="${env.TARGET_REPO}/milvus-dev"
                                 export MILVUS_IMAGE_TAG="${env.BRANCH_NAME}-${date}-${gitShortCommit}"
                                 build/build_image.sh
-                                docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}
                                 docker push \${MILVUS_IMAGE_REPO}:\${MILVUS_IMAGE_TAG}
                                 docker tag \${MILVUS_IMAGE_REPO}:\${MILVUS_IMAGE_TAG} \${MILVUS_IMAGE_REPO}:${env.BRANCH_NAME}-latest
                                 docker push \${MILVUS_IMAGE_REPO}:${env.BRANCH_NAME}-latest
