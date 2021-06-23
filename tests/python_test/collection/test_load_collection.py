7d14b2d6bf48f7e6dafa022b9cb4b7749f55b295
binbin <83755740+binbinlv@users.noreply.github.com>
2021-06-23 18:12:15 +0800

    Fix code for search after release (#6026)
    Signed-off-by: Binbin Lv <binbin.lv@zilliz.com>

--- tests/python_test/collection/test_load_collection.py
+++ tests/python_test/collection/test_load_collection.py
@@ -252,7 +252,7 @@ class TestLoadCollection:
         connect.load_collection(collection)
         connect.release_partitions(collection, [default_partition_name, default_tag])
         res = connect.search(collection, default_single_query)
-        assert len(res) == 0
+        assert len(res[0]) == 0
 
     @pytest.mark.tags(CaseLabel.tags_smoke)
     def test_load_partitions_release_collection(self, connect, collection):
@@ -308,7 +308,7 @@ class TestReleaseAdvanced:
         res = connect.search(collection, query, _async=True)
         connect.release_partitions(collection, [default_tag])
         res = connect.search(collection, default_single_query)
-        assert len(res) == 0
+        assert len(res[0]) == 0
 
     @pytest.mark.tags(CaseLabel.tags_smoke)
     def test_release_collection_during_searching_A(self, connect, collection):
