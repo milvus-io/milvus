008f420e66f74a0e05f2b6f805cfabafa93a54f9
fishpenguin <kun.yu@zilliz.com>
2021-06-24 20:59:32 +0800

    Add rocksdbPath in NewRmsFactory
    Signed-off-by: fishpenguin <kun.yu@zilliz.com>

--- internal/msgstream/mq_factory.go
+++ internal/msgstream/mq_factory.go
@@ -15,6 +15,7 @@ import (
 	"context"
 
 	"github.com/apache/pulsar-client-go/pulsar"
+	"github.com/milvus-io/milvus/internal/log"
 	"github.com/milvus-io/milvus/internal/util/mqclient"
 	"github.com/milvus-io/milvus/internal/util/rocksmq/client/rocksmq"
 	rocksmqserver "github.com/milvus-io/milvus/internal/util/rocksmq/server/rocksmq"
@@ -71,7 +72,6 @@ type RmsFactory struct {
 	// the following members must be public, so that mapstructure.Decode() can access them
 	ReceiveBufSize int64
 	RmqBufSize     int64
-	RocksmqPath    string
 }
 
 func (f *RmsFactory) SetParams(params map[string]interface{}) error {
@@ -104,14 +104,14 @@ func (f *RmsFactory) NewQueryMsgStream(ctx context.Context) (MsgStream, error) {
 	return NewMemMsgStream(ctx, f.ReceiveBufSize)
 }
 
-func NewRmsFactory() Factory {
+func NewRmsFactory(rocksmqPath string) Factory {
 	f := &RmsFactory{
 		dispatcherFactory: ProtoUDFactory{},
 		ReceiveBufSize:    1024,
 		RmqBufSize:        1024,
-		RocksmqPath:       "/tmp/milvus_rdb",
 	}
 
-	rocksmqserver.InitRocksMQ(f.RocksmqPath)
+	log.Debug("RocksmqPath=" + rocksmqPath)
+	rocksmqserver.InitRocksMQ(rocksmqPath)
 	return f
 }
