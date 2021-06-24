0116f9dd39cd14ab0ee4861eeda37a8449c3030a
yanliang567 <yanliang.qiao@zilliz.com>
2021-06-24 11:34:44 +0800

    [skip ci] Add more chaos tests
    Signed-off-by: yanliang567 <yanliang.qiao@zilliz.com>

--- tests20/python_client/chaos/chaos_commons.py
+++ tests20/python_client/chaos/chaos_commons.py
@@ -1,9 +1,10 @@
 import os
 import threading
 import glob
-from delayed_assert import expect
+import delayed_assert
 import constants
 from yaml import full_load
+from utils.util_log import test_log as log
 
 
 def check_config(chaos_config):
@@ -31,32 +32,22 @@ def gen_experiment_config(yaml):
 
 
 def start_monitor_threads(checkers={}):
-    for k in checkers.keys():
-        v = checkers[k]
-        t = threading.Thread(target=v.keep_running, args=())
+    threads = {}
+    for k, ch in checkers.items():
+        t = threading.Thread(target=ch.keep_running, args=())
         t.start()
-
-
-def assert_statistic(checkers, expectations={}):
-    for k in checkers.keys():
-        # expect succ if no expectations
-        succ_rate = checkers[k].succ_rate()
-        if expectations.get(k, '') == constants.FAIL:
-            print(f"Expect Fail: {str(checkers[k])} current succ rate {succ_rate}")
-            expect(succ_rate < 0.49)
-        else:
-            print(f"Expect Succ: {str(checkers[k])} current succ rate {succ_rate}")
-            expect(succ_rate > 0.90)
+        threads[k] = t
+    return threads
 
 
 def get_env_variable_by_name(name):
     """ get env variable by name"""
     try:
         env_var = os.environ[name]
-        print(f"env_variable: {env_var}")
+        log.debug(f"env_variable: {env_var}")
         return str(env_var)
     except Exception as e:
-        print(f"fail to get env variables, error: {str(e)}")
+        log.debug(f"fail to get env variables, error: {str(e)}")
         return None
 
 
@@ -64,14 +55,18 @@ def get_chaos_yamls():
     chaos_env = get_env_variable_by_name(constants.CHAOS_CONFIG_ENV)
     if chaos_env is not None:
         if os.path.isdir(chaos_env):
-            print(f"chaos_env is a dir: {chaos_env}")
+            log.debug(f"chaos_env is a dir: {chaos_env}")
             return glob.glob(chaos_env + 'chaos_*.yaml')
         elif os.path.isfile(chaos_env):
-            print(f"chaos_env is a file: {chaos_env}")
+            log.debug(f"chaos_env is a file: {chaos_env}")
             return [chaos_env]
         else:
             # not a valid directory, return default
             pass
-    print("not a valid directory or file, return default")
-    return glob.glob(constants.TESTS_CONFIG_LOCATION + 'chaos_*.yaml')
+    log.debug("not a valid directory or file, return default")
+    return glob.glob(constants.TESTS_CONFIG_LOCATION + constants.ALL_CHAOS_YAMLS)
 
+
+def reconnect(conn, host, port):
+    conn.add_connection(default={"host": host, "port": port})
+    return conn.connect(alias='default')
