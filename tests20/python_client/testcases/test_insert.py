b77f2878385dc15fa46745462be37100ed7a1b99
ThreadDao <yufen.zong@zilliz.com>
2021-06-25 10:22:47 +0800

    [skip ci] skip ci
    Signed-off-by: ThreadDao <yufen.zong@zilliz.com>

--- tests20/python_client/testcases/test_insert.py
+++ tests20/python_client/testcases/test_insert.py
@@ -3,7 +3,7 @@ import pandas as pd
 import pytest
 
 from base.client_base import TestcaseBase
-from utils.util_log import test_log as log
+# from utils.util_log import test_log as log
 from common import common_func as cf
 from common import common_type as ct
 from common.common_type import CaseLabel, CheckTasks
