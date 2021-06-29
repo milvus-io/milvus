3a202cad2be2e134398fde4837ad602de63f955e
congqixia <congqi.xia@zilliz.com>
2021-06-29 09:30:17 +0800

    Reduce connect timeout time (#6181)
    Signed-off-by: Congqi Xia <congqi.xia@zilliz.com>

--- internal/distributed/datanode/client/client.go
+++ internal/distributed/datanode/client/client.go
@@ -67,10 +67,11 @@ func (c *Client) connect(retryOptions ...retry.Option) error {
 		opts := trace.GetInterceptorOpts()
 		log.Debug("DataNode connect ", zap.String("address", c.addr))
 		conn, err := grpc.DialContext(c.ctx, c.addr,
-			grpc.WithInsecure(), grpc.WithBlock(), grpc.WithTimeout(2*time.Second),
+			grpc.WithInsecure(), grpc.WithBlock(), grpc.WithTimeout(200*time.Millisecond),
+			grpc.WithDisableRetry(),
 			grpc.WithUnaryInterceptor(
 				grpc_middleware.ChainUnaryClient(
-					grpc_retry.UnaryClientInterceptor(),
+					//grpc_retry.UnaryClientInterceptor(grpc_retry.WithMax(3), grpc_retry.WithPerRetryTimeout(time.Millisecond*50)),
 					grpc_opentracing.UnaryClientInterceptor(opts...),
 				)),
 			grpc.WithStreamInterceptor(
