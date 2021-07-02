74e35edf18badb6122aa04e76d16c302b4711c6f
yanliang567 <yanliang.qiao@zilliz.com>
2021-07-02 15:13:07 +0800

    [skip ci] Open writing logs
    Signed-off-by: yanliang567 <yanliang.qiao@zilliz.com>

--- tests20/python_client/utils/api_request.py
+++ tests20/python_client/utils/api_request.py
@@ -16,8 +16,8 @@ def api_request_catch():
         def inner_wrapper(*args, **kwargs):
             try:
                 res = func(*args, **kwargs)
-                # log_res = str(res)[0:log_row_length] + '......' if len(str(res)) > log_row_length else str(res)
-                # log.debug("(api_response) Response : %s " % log_res)
+                log_res = str(res)[0:log_row_length] + '......' if len(str(res)) > log_row_length else str(res)
+                log.debug("(api_response) Response : %s " % log_res)
                 return res, True
             except Exception as e:
                 log_e = str(e)[0:log_row_length] + '......' if len(str(e)) > log_row_length else str(e)
@@ -37,8 +37,8 @@ def api_request(_list, **kwargs):
             if len(_list) > 1:
                 for a in _list[1:]:
                     arg.append(a)
-            # log_arg = str(arg)[0:log_row_length] + '......' if len(str(arg)) > log_row_length else str(arg)
-            # log.debug("(api_request) Request: [%s] args: %s, kwargs: %s" % (str(func), log_arg, str(kwargs)))
+            log_arg = str(arg)[0:log_row_length] + '......' if len(str(arg)) > log_row_length else str(arg)
+            log.debug("(api_request) Request: [%s] args: %s, kwargs: %s" % (str(func), log_arg, str(kwargs)))
             return func(*arg, **kwargs)
     return False, False
 
