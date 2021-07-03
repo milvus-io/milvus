d23383b5c21cc13d1e8026a5f7d659ff9e55d733
quicksilver <zhifeng.zhang@zilliz.com>
2021-07-03 14:52:28 +0800

    Add pytest pipeline for pymilvus-orm (#6278)
    Signed-off-by: quicksilver <zhifeng.zhang@zilliz.com>

--- build/ci/jenkins/NightlyCI.groovy
+++ build/ci/jenkins/NightlyCI.groovy
@@ -25,6 +25,10 @@ pipeline {
                         name 'MILVUS_SERVER_TYPE'
                         values 'standalone', 'distributed'
                     }
+                    axis {
+                        name 'MILVUS_CLIENT'
+                        values 'pymilvus', 'pymilvus-orm'
+                    }
                 }
                 agent {
                     kubernetes {
@@ -40,7 +44,7 @@ pipeline {
                     SEMVER = "${BRANCH_NAME.contains('/') ? BRANCH_NAME.substring(BRANCH_NAME.lastIndexOf('/') + 1) : BRANCH_NAME}"
                     IMAGE_REPO = "dockerhub-mirror-sh.zilliz.cc/milvusdb"
                     DOCKER_BUILDKIT = 1
-                    ARTIFACTS = "${env.WORKSPACE}/artifacts"
+                    ARTIFACTS = "${env.WORKSPACE}/_artifacts"
                     DOCKER_CREDENTIALS_ID = "f0aacc8e-33f2-458a-ba9e-2c44f431b4d2"
                     TARGET_REPO = "milvusdb"
                 }
@@ -55,12 +59,24 @@ pipeline {
                                             clusterEnabled = "true"
                                         }
 
-                                        sh """
-                                        MILVUS_CLUSTER_ENABLED=${clusterEnabled} \
-                                        ./e2e-k8s.sh \
-                                        --kind-config "${env.WORKSPACE}/build/config/topology/trustworthy-jwt-ci.yaml" \
-                                        --node-image registry.zilliz.com/kindest/node:v1.20.2
-                                        """
+                                        if ("${MILVUS_CLIENT}" == "pymilvus") {
+                                            sh """
+                                            MILVUS_CLUSTER_ENABLED=${clusterEnabled} \
+                                            ./e2e-k8s.sh \
+                                            --kind-config "${env.WORKSPACE}/build/config/topology/trustworthy-jwt-ci.yaml" \
+                                            --node-image registry.zilliz.com/kindest/node:v1.20.2
+                                            """
+                                        } else if ("${MILVUS_CLIENT}" == "pymilvus-orm") {
+                                            sh """
+                                            MILVUS_CLUSTER_ENABLED=${clusterEnabled} \
+                                            ./e2e-k8s.sh \
+                                            --kind-config "${env.WORKSPACE}/build/config/topology/trustworthy-jwt-ci.yaml" \
+                                            --node-image registry.zilliz.com/kindest/node:v1.20.2 \
+                                            --test-extra-arg "--tags L0 L1 L2"
+                                            """
+                                        } else {
+                                            error "Error: Unsupported Milvus client: ${MILVUS_CLIENT}"
+                                        }
                                     }
                                 }
                             }
@@ -84,6 +100,9 @@ pipeline {
                             script {
                                 dir("${env.ARTIFACTS}") {
                                     sh "find ./kind -path '*/history/*' -type f | xargs tar -zcvf artifacts-${PROJECT_NAME}-${MILVUS_SERVER_TYPE}-${SEMVER}-${env.BUILD_NUMBER}-e2e-nightly-logs.tar.gz --transform='s:^[^/]*/[^/]*/[^/]*/[^/]*/::g' || true"
+                                    if ("${MILVUS_CLIENT}" == "pymilvus-orm") {
+                                        sh "tar -zcvf artifacts-${PROJECT_NAME}-${MILVUS_SERVER_TYPE}-${MILVUS_CLIENT}-pytest-logs.tar.gz ./tests/pytest_logs --remove-files || true"
+                                    }
                                     archiveArtifacts artifacts: "**.tar.gz", allowEmptyArchive: true
                                     sh 'docker rm -f \$(docker network inspect -f \'{{ range \$key, \$value := .Containers }}{{ printf "%s " \$key}}{{ end }}\' kind) || true'
                                     sh 'docker network rm kind 2>&1 > /dev/null || true'
