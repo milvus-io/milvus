1833fe8955213ba2f0a2b13e2030368530678195
ezeharia <ezeharia@gsitechnology.com>
2021-07-05 21:23:24 +0300

    Flat binary search using Apu

---
+++ core/src/index/knowhere/knowhere/index/vector_index/fpga/GsiHammingIndex.h
@@ -0,0 +1,32 @@
+//
+// Created by ezeharia on 5/2/21.
+//
+#ifdef MILVUS_APU_VERSION
+#pragma once
+
+#include "GsiBaseIndex.h"
+#include "Apu.h"
+
+
+
+namespace milvus {
+namespace knowhere {
+
+class GsiHammingIndex : public GsiBaseIndex {
+
+public :
+
+    GsiHammingIndex(uint32_t dim) : GsiBaseIndex(dim) {
+    index_type_ = IndexEnum::INDEX_FAISS_BIN_IDMAP;//GSI_FLAT_HAMMING;
+    }
+
+	void CopyIndexToFpga( uint32_t row_count , const std::string& location ) override ;
+
+    DatasetPtr Query(const DatasetPtr &dataset, const Config &config) override;
+
+
+};
+}//knowhere
+}//milvus
+#endif
+
---
+++ core/src/index/knowhere/knowhere/index/vector_index/fpga/GsiTanimotoIndex.cpp
@@ -0,0 +1,67 @@
+//
+// Created by ezeharia on 5/2/21.
+//
+
+#include "knowhere/index/vector_index/fpga/GsiTanimotoIndex.h"
+
+#include "scheduler/task/SearchTask.h"
+#include "ApuInst.h"
+
+
+namespace milvus{
+namespace knowhere {
+
+
+void
+GsiTanimotoIndex::CopyIndexToFpga( uint32_t row_count ,const std::string& location) {
+
+    num_bfeatures_ = Dim();
+    num_bytes_in_rec_=num_bfeatures_ / CHAR_BITS ;// duplicate to PopulateAPuParams
+    index_size_ = row_count*num_bytes_in_rec_;
+
+    auto apu = Fpga::ApuInst::getInstance();
+    apu->cleanApuResources(Fpga::APU_CLEAN_TYPE::NEW_DB);
+    apu->PopulateApuParams(num_bfeatures_ , row_count , location);
+    apu->createBdb();
+    apu->loadSeesionToApu(Fpga::APU_METRIC_TYPE::TANIMOTO);
+
+}
+
+DatasetPtr
+GsiTanimotoIndex::Query(const DatasetPtr &dataset, const Config &config) {
+
+    auto apu = Fpga::ApuInst::getInstance();
+
+    num_bfeatures_= Dim();
+    num_queries_ = static_cast<uint32_t>(dataset.get()->Get<long>(meta::ROWS));
+    num_bytes_in_rec_=num_bfeatures_ / CHAR_BITS ;
+    topK_=config[meta::TOPK];
+
+    apu->setTopK(topK_);
+    AllocateMemory(dataset , config);
+    apu->Query( indices_ , distances_, queries_ , Fpga::APU_METRIC_TYPE::TANIMOTO);
+
+    auto ret_ds = std::make_shared<Dataset>();
+    auto start_calloc = std::chrono::steady_clock::now();
+    int64_t* ids_int64 =(int64_t*)calloc(topK_ * num_queries_, sizeof(int64_t));
+    auto end_calloc = std::chrono::steady_clock::now();
+    std::cout << "Elapsed time of calloc in microseconds: "
+              << std::chrono::duration_cast<std::chrono::microseconds>(end_calloc - start_calloc).count() << " µs"
+              << std::endl;
+
+    auto start_conv = std::chrono::steady_clock::now();
+    convertToInt64_t(&indices_ , ids_int64);
+    auto end_conv = std::chrono::steady_clock::now();
+    std::cout << "Elapsed time for conversion in microseconds: "
+              << std::chrono::duration_cast<std::chrono::microseconds>(end_conv - start_conv).count() << " µs"
+              << std::endl;
+
+    ret_ds->Set(meta::IDS, ids_int64);
+    ret_ds->Set(meta::DISTANCE, distances_.rows_f32);
+    free((void*)indices_.rows_u32);
+    return ret_ds;
+
+}
+
+}//knowhere
+}//milvus
