76bb263b727f261c168e2c26d879d6669c099e3d
ThreadDao <yufen.zong@zilliz.com>
2021-07-14 11:55:43 +0800

    add deploy local with helm
    Signed-off-by: ThreadDao <yufen.zong@zilliz.com>

---
+++ tests20/python_client/scale/constants.py
@@ -0,0 +1,17 @@
+
+SERVER_HOST_DEFAULT = "127.0.0.1"
+SERVER_PORT_DEFAULT = 19530
+SERVER_VERSION = "2.0.0-RC2"
+
+# scale object
+IMAGE_REPOSITORY = "milvusdb/milvus-dev"
+IMAGE_TAG = "master-latest"
+IF_NOT_PRESENT = "IfNotPresent"
+ALWAYS = "Always"
+PROXY = "proxy"
+DATA_NODE = "dataNode"
+INDEX_NODE = "indexNode"
+QUERY_NODE = "queryNode"
+
+# my values.yaml path
+HELM_VALUES_PATH = '/home/zong/milvus-helm/charts/milvus'
