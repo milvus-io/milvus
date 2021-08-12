5c383d1d0228ebe457fb8ec13a7b049ed0898a44
sunby <bingyi.sun@zilliz.com>
2021-08-12 19:58:08 +0800

    Add a checker to listen for the timetick. (#7061)
    issue: #7060
    Signed-off-by: sunby <bingyi.sun@zilliz.com>

--- internal/datacoord/util.go
+++ internal/datacoord/util.go
@@ -11,8 +11,12 @@
 package datacoord
 
 import (
+	"context"
 	"errors"
+	"fmt"
+	"time"
 
+	"github.com/milvus-io/milvus/internal/log"
 	"github.com/milvus-io/milvus/internal/proto/commonpb"
 )
 
@@ -43,4 +47,44 @@ func VerifyResponse(response interface{}, err error) error {
 		return errUnknownResponseType
 	}
 	return nil
+}
+
+// LongTermChecker checks we receive at least one msg in d duration. If not, checker
+// will print a warn message.
+type LongTermChecker struct {
+	d    time.Duration
+	t    *time.Ticker
+	ctx  context.Context
+	warn string
+	name string
+}
+
+func NewLongTermChecker(ctx context.Context, name string, d time.Duration, warn string) *LongTermChecker {
+	c := &LongTermChecker{
+		name: name,
+		ctx:  ctx,
+		d:    d,
+		warn: warn,
+	}
+	return c
+}
+
+func (c *LongTermChecker) Start() {
+	c.t = time.NewTicker(c.d)
+	go func() {
+		for {
+			select {
+			case <-c.ctx.Done():
+				log.Warn(fmt.Sprintf("long term checker [%s] shutdown", c.name))
+				return
+			case <-c.t.C:
+				log.Warn(c.warn)
+			}
+		}
+	}()
+}
+
+// Check reset the time ticker
+func (c *LongTermChecker) Check() {
+	c.t.Reset(c.d)
 }
