848db338487b769c8cf9766a42a38ea9e4cb67c3
zhuwenxing <wenxing.zhu@zilliz.com>
2021-08-30 20:12:00 +0800

    Update search testcases (#7363)
    Signed-off-by: zhuwenxing <wenxing.zhu@zilliz.com>

--- tests/python_client/testcases/collection/test_load_collection.py
+++ tests/python_client/testcases/collection/test_load_collection.py
@@ -311,8 +311,8 @@ class TestReleaseAdvanced:
         connect.load_partitions(collection, [default_tag])
         res = connect.search(collection, query, _async=True)
         connect.release_partitions(collection, [default_tag])
-        res = connect.search(collection, default_single_query)
-        assert len(res[0]) == 0
+        with pytest.raises(Exception) as e:
+            res = connect.search(collection, default_single_query)
 
     @pytest.mark.tags(CaseLabel.tags_smoke)
     def test_release_collection_during_searching_A(self, connect, collection):
