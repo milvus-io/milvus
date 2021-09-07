663e52d1aaae6ba96230979e40583e9df1d4246f
yhmo <yihua.mo@zilliz.com>
2021-09-07 12:36:10 +0800

    Update unittest name of paramtable
    Signed-off-by: yhmo <yihua.mo@zilliz.com>

--- internal/util/paramtable/param_test.go
+++ internal/util/paramtable/param_test.go
@@ -20,7 +20,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestParam(t *testing.T) {
+func TestBaseParamTable(t *testing.T) {
 	Params.Init()
 
 	assert.NotZero(t, len(Params.EtcdEndpoints))
