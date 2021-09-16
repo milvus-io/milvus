2f5e139f0456a000a91d028b71d9d75224bf2705
zhuwenxing <wenxing.zhu@zilliz.com>
2021-09-16 18:53:49 +0800

    [skip ci]Add script to export logs for milvus k8s cluster (#8081)
    Signed-off-by: zhuwenxing <wenxing.zhu@zilliz.com>

---
+++ tests/scripts/export_log_k8s.sh
@@ -0,0 +1,11 @@
+array=($(kubectl get pod|grep chaos-milvus|awk '{print $1}')) # change cahos-milvus to your own pod name prefix
+echo ${array[@]}
+log_dir="k8s_logs"
+mkdir $log_dir
+echo "export logs start"
+for pod in ${array[*]}
+do
+echo "export logs for pod $pod "
+kubectl logs $pod > ./$log_dir/$pod.log
+done
+echo "export logs done"
