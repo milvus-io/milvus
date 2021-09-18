2ed9d29b7ab3baa08cd9a3750a5fb924ac6a6698
yhmo <yihua.mo@zilliz.com>
2021-09-18 11:20:23 +0800

    [skip ci] Export go coverage html report
    Signed-off-by: yhmo <yihua.mo@zilliz.com>

--- scripts/run_go_codecov.sh
+++ scripts/run_go_codecov.sh
@@ -7,6 +7,7 @@ for d in $(go list ./internal... | grep -v vendor); do
     go test -race -coverpkg=./... -coverprofile=profile.out -covermode=atomic "$d"
     if [ -f profile.out ]; then
         cat profile.out >> coverage.txt
+        go tool cover -html=profile.out -o go_coverage.html
         rm profile.out
     fi
 done
