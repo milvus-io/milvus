c34c8f26c93ddbeef27940dfeaf543f485f9b13e
wangting0128 <ting.wang@zilliz.com>
2021-09-23 19:29:34 +0800

    [skip ci] Add comment of benchmark file locust
    Signed-off-by: wangting0128 <ting.wang@zilliz.com>

--- tests/benchmark/milvus_benchmark/runners/locust.py
+++ tests/benchmark/milvus_benchmark/runners/locust.py
@@ -197,6 +197,7 @@ class LocustSearchRunner(LocustRunner):
         }
         self.init_metric(self.name, collection_info, index_info, None, run_params)
         case_metric = copy.deepcopy(self.metric)
+        # set metric type as case
         case_metric.set_case_metric_type()
         case_metrics = list()
         case_params = list()
