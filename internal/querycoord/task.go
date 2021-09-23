19baca314c0cff3bc92dff03a0b0b4c684719694
xige-16 <xi.ge@zilliz.com>
2021-09-23 21:06:02 +0800

    Add log for releasePartion task (#8222)
    Signed-off-by: xige-16 <xi.ge@zilliz.com>

--- internal/querycoord/task.go
+++ internal/querycoord/task.go
@@ -716,7 +716,7 @@ func (rpt *ReleasePartitionTask) Execute(ctx context.Context) error {
 	status := &commonpb.Status{
 		ErrorCode: commonpb.ErrorCode_UnexpectedError,
 	}
-
+	// if nodeID ==0, it means that the release request has not been assigned to the specified query node
 	if rpt.NodeID <= 0 {
 		nodes, err := rpt.cluster.onlineNodes()
 		if err != nil {
