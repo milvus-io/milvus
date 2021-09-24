f87316ab287e71a268f3af60162eea1ae10520c0
groot <yihua.mo@zilliz.com>
2021-09-24 16:41:55 +0800

    Update codecov api to v2 (#8471)
    Signed-off-by: yhmo <yihua.mo@zilliz.com>

--- scripts/run_go_codecov.sh
+++ scripts/run_go_codecov.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 set -e
-echo "mode: atomic" > coverage.txt
+echo "mode: atomic" > go_coverage.txt
 
 for d in $(go list ./internal... | grep -v vendor); do
     go test -race -coverpkg=./... -coverprofile=profile.out -covermode=atomic "$d"
     if [ -f profile.out ]; then
-        sed '1d' profile.out >> coverage.txt
+        sed '1d' profile.out >> go_coverage.txt
         rm profile.out
     fi
 done
 
-go tool cover -html=./coverage.txt -o ./go_coverage.html
+go tool cover -html=./go_coverage.txt -o ./go_coverage.html
