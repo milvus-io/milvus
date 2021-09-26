2d27258d3cf12584c8ce190b548c14c5b2021583
ThreadDao <yufen.zong@zilliz.com>
2021-09-26 17:43:58 +0800

    [skip ci] Add comment to scale constants (#8595)
    Signed-off-by: ThreadDao <yufen.zong@zilliz.com>

--- tests/python_client/scale/constants.py
+++ tests/python_client/scale/constants.py
@@ -1,14 +1,14 @@
 # scale object
-IMAGE_REPOSITORY = "milvusdb/milvus-dev"
-IMAGE_TAG = "master-latest"
-NAMESPACE = "chaos-testing"
-IF_NOT_PRESENT = "IfNotPresent"
-ALWAYS = "Always"
-PROXY = "proxy"
-DATA_NODE = "dataNode"
-INDEX_NODE = "indexNode"
-QUERY_NODE = "queryNode"
+IMAGE_REPOSITORY = "milvusdb/milvus-dev"  # repository of milvus image
+IMAGE_TAG = "master-latest"  # tag of milvus image
+NAMESPACE = "chaos-testing"  # namespace
+IF_NOT_PRESENT = "IfNotPresent"  # image pullPolicy IfNotPresent
+ALWAYS = "Always"  # image pullPolicy Always
+PROXY = "proxy"  # key proxy
+DATA_NODE = "dataNode"  # key dataNode
+INDEX_NODE = "indexNode"  # key indexNode
+QUERY_NODE = "queryNode"  # key queryNode
 
 # my values.yaml path
-MILVUS_CHART_ENV = 'MILVUS_CHART_ENV'
-MILVUS_LOGS_PATH = '/tmp/milvus'
+MILVUS_CHART_ENV = 'MILVUS_CHART_ENV'  # env of milvus chart path
+MILVUS_LOGS_PATH = '/tmp/milvus'  # path of milvus pod logs
