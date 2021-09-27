b20a32a282554f29d9b3f56c57dee0d29aede72b
dragondriver <jiquan.long@zilliz.com>
2021-09-27 18:13:29 +0800

    [skip ci] Add description for proxy api
    Signed-off-by: dragondriver <jiquan.long@zilliz.com>

--- internal/types/types.go
+++ internal/types/types.go
@@ -156,7 +156,31 @@ type RootCoordComponent interface {
 type Proxy interface {
 	Component
 
+	// InvalidateCollectionMetaCache notifies Proxy to clear all the meta cache of specific collection.
+	//
+	// InvalidateCollectionMetaCache should be called when there are any meta changes in specific collection.
+	// Such as `DropCollection`, `CreatePartition`, `DropPartition`, etc.
+	//
+	// ctx is the request to control request deadline and cancellation.
+	// request contains the request params, which are database name(not used now) and collection name.
+	//
+	// InvalidateCollectionMetaCache should always succeed even though the specific collection doesn't exist in Proxy.
+	// So the code of response `Status` should be always `Success`.
+	//
+	// error is returned only when some communication issue occurs.
 	InvalidateCollectionMetaCache(ctx context.Context, request *proxypb.InvalidateCollMetaCacheRequest) (*commonpb.Status, error)
+
+	// ReleaseDQLMessageStream notifies Proxy to release and close the search message stream of specific collection.
+	//
+	// ReleaseDQLMessageStream should be called when the specific collection was released.
+	//
+	// ctx is the request to control request deadline and cancellation.
+	// request contains the request params, which are database id(not used now) and collection id.
+	//
+	// ReleaseDQLMessageStream should always succeed even though the specific collection doesn't exist in Proxy.
+	// So the code of response `Status` should be always `Success`.
+	//
+	// error is returned only when some communication issue occurs.
 	ReleaseDQLMessageStream(ctx context.Context, in *proxypb.ReleaseDQLMessageStreamRequest) (*commonpb.Status, error)
 
 	//TODO: move to milvus service
