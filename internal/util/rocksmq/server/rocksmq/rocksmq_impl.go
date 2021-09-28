1eb3168254ddb2da815c6a0b5f6fe077b80c8d4a
yukun <kun.yu@zilliz.com>
2021-09-28 21:20:03 +0800

    Unset max open files in rocksdb (#8799)
    Signed-off-by: fishpenguin <kun.yu@zilliz.com>

--- internal/util/rocksmq/server/rocksmq/rocksmq_impl.go
+++ internal/util/rocksmq/server/rocksmq/rocksmq_impl.go
@@ -123,7 +123,7 @@ func NewRocksMQ(name string, idAllocator allocator.GIDAllocator) (*rocksmq, erro
 	opts.SetBlockBasedTableFactory(bbto)
 	opts.SetCreateIfMissing(true)
 	opts.SetPrefixExtractor(gorocksdb.NewFixedPrefixTransform(FixedChannelNameLen + 1))
-	opts.SetMaxOpenFiles(-1)
+	// opts.SetMaxOpenFiles(-1)
 
 	db, err := gorocksdb.OpenDb(opts, name)
 	if err != nil {
