7f61c1e8ea1072efd78c645d5102ff43c6227b7e
dragondriver <jiquan.long@zilliz.com>
2021-09-28 21:30:03 +0800

    Refactor alias ut (#8800)
    Signed-off-by: dragondriver <jiquan.long@zilliz.com>

--- internal/proxy/proxy_test.go
+++ internal/proxy/proxy_test.go
@@ -658,6 +658,103 @@ func TestProxy(t *testing.T) {
 		assert.NotEqual(t, commonpb.ErrorCode_Success, resp.ErrorCode)
 	})
 
+	t.Run("create alias", func(t *testing.T) {
+		// create alias
+		aliasReq := &milvuspb.CreateAliasRequest{
+			Base:           nil,
+			CollectionName: collectionName,
+			Alias:          "alias",
+		}
+		resp, err := proxy.CreateAlias(ctx, aliasReq)
+		assert.NoError(t, err)
+		assert.Equal(t, commonpb.ErrorCode_Success, resp.ErrorCode)
+
+		_, _ = proxy.InvalidateCollectionMetaCache(ctx, &proxypb.InvalidateCollMetaCacheRequest{
+			Base: &commonpb.MsgBase{
+				MsgType:   0,
+				MsgID:     0,
+				Timestamp: 0,
+				SourceID:  0,
+			},
+			DbName:         dbName,
+			CollectionName: collectionName,
+		})
+
+		sameAliasReq := &milvuspb.CreateAliasRequest{
+			Base:           nil,
+			CollectionName: collectionName,
+			Alias:          "alias",
+		}
+
+		resp, err = proxy.CreateAlias(ctx, sameAliasReq)
+		assert.NoError(t, err)
+		assert.NotEqual(t, commonpb.ErrorCode_Success, resp.ErrorCode)
+	})
+
+	t.Run("alter alias", func(t *testing.T) {
+		// alter alias
+		alterReq := &milvuspb.AlterAliasRequest{
+			Base:           nil,
+			CollectionName: collectionName,
+			Alias:          "alias",
+		}
+		resp, err := proxy.AlterAlias(ctx, alterReq)
+		assert.NoError(t, err)
+		assert.Equal(t, commonpb.ErrorCode_Success, resp.ErrorCode)
+
+		_, _ = proxy.InvalidateCollectionMetaCache(ctx, &proxypb.InvalidateCollMetaCacheRequest{
+			Base: &commonpb.MsgBase{
+				MsgType:   0,
+				MsgID:     0,
+				Timestamp: 0,
+				SourceID:  0,
+			},
+			DbName:         dbName,
+			CollectionName: collectionName,
+		})
+
+		nonExistingCollName := "coll_name_random_zarathustra"
+		faultyAlterReq := &milvuspb.AlterAliasRequest{
+			Base:           nil,
+			CollectionName: nonExistingCollName,
+			Alias:          "alias",
+		}
+		resp, err = proxy.AlterAlias(ctx, faultyAlterReq)
+		assert.NoError(t, err)
+		assert.NotEqual(t, commonpb.ErrorCode_Success, resp.ErrorCode)
+	})
+
+	t.Run("drop alias", func(t *testing.T) {
+		// drop alias
+		resp, err := proxy.DropAlias(ctx, &milvuspb.DropAliasRequest{
+			Base:  nil,
+			Alias: "alias",
+		})
+		assert.NoError(t, err)
+		assert.Equal(t, commonpb.ErrorCode_Success, resp.ErrorCode)
+
+		_, _ = proxy.InvalidateCollectionMetaCache(ctx, &proxypb.InvalidateCollMetaCacheRequest{
+			Base: &commonpb.MsgBase{
+				MsgType:   0,
+				MsgID:     0,
+				Timestamp: 0,
+				SourceID:  0,
+			},
+			DbName:         dbName,
+			CollectionName: collectionName,
+		})
+
+		sameDropReq := &milvuspb.DropAliasRequest{
+			Base:  nil,
+			Alias: "alias",
+		}
+
+		// Can't drop non-existing alias
+		resp, err = proxy.DropAlias(ctx, sameDropReq)
+		assert.NoError(t, err)
+		assert.NotEqual(t, commonpb.ErrorCode_Success, resp.ErrorCode)
+	})
+
 	t.Run("has collection", func(t *testing.T) {
 		resp, err := proxy.HasCollection(ctx, &milvuspb.HasCollectionRequest{
 			Base:           nil,
@@ -2033,81 +2130,6 @@ func TestProxy(t *testing.T) {
 		resp, err := proxy.Query(shortCtx, &milvuspb.QueryRequest{})
 		assert.NoError(t, err)
 		assert.NotEqual(t, commonpb.ErrorCode_Success, resp.Status.ErrorCode)
-	})
-
-	t.Run("create alias", func(t *testing.T) {
-		req := createCollectionReq
-		resp, err := proxy.CreateCollection(ctx, req)
-		assert.NoError(t, err)
-		assert.Equal(t, commonpb.ErrorCode_Success, resp.ErrorCode)
-		// create alias
-		aliasReq := &milvuspb.CreateAliasRequest{
-			Base:           nil,
-			CollectionName: collectionName,
-			Alias:          "alias",
-		}
-		resp, err = proxy.CreateAlias(ctx, aliasReq)
-		assert.NoError(t, err)
-		assert.Equal(t, commonpb.ErrorCode_Success, resp.ErrorCode)
-		sameAliasReq := &milvuspb.CreateAliasRequest{
-			Base:           nil,
-			CollectionName: collectionName,
-			Alias:          "alias",
-		}
-
-		resp, err = proxy.CreateAlias(ctx, sameAliasReq)
-		assert.NoError(t, err)
-		assert.Equal(t, commonpb.ErrorCode_UnexpectedError, resp.ErrorCode)
-	})
-
-	t.Run("alter alias", func(t *testing.T) {
-		// alter alias
-		alterReq := &milvuspb.AlterAliasRequest{
-			Base:           nil,
-			CollectionName: collectionName,
-			Alias:          "alias",
-		}
-		resp, err := proxy.AlterAlias(ctx, alterReq)
-		assert.NoError(t, err)
-		assert.Equal(t, commonpb.ErrorCode_Success, resp.ErrorCode)
-
-		nonExistingCollName := "coll_name_random_zarathustra"
-		faultyAlterReq := &milvuspb.AlterAliasRequest{
-			Base:           nil,
-			CollectionName: nonExistingCollName,
-			Alias:          "alias",
-		}
-		resp, err = proxy.AlterAlias(ctx, faultyAlterReq)
-		assert.NoError(t, err)
-		assert.Equal(t, commonpb.ErrorCode_UnexpectedError, resp.ErrorCode)
-	})
-
-	t.Run("drop alias", func(t *testing.T) {
-		// drop alias
-		resp, err := proxy.DropAlias(ctx, &milvuspb.DropAliasRequest{
-			Base:  nil,
-			Alias: "alias",
-		})
-		assert.NoError(t, err)
-		assert.Equal(t, commonpb.ErrorCode_Success, resp.ErrorCode)
-
-		sameDropReq := &milvuspb.DropAliasRequest{
-			Base:  nil,
-			Alias: "alias",
-		}
-
-		// Can't drop non-existing alias
-		resp, err = proxy.DropAlias(ctx, sameDropReq)
-		assert.NoError(t, err)
-		assert.Equal(t, commonpb.ErrorCode_UnexpectedError, resp.ErrorCode)
-
-		dropReq := &milvuspb.DropCollectionRequest{
-			DbName:         dbName,
-			CollectionName: collectionName,
-		}
-		resp, err = proxy.DropCollection(ctx, dropReq)
-		assert.NoError(t, err)
-		assert.Equal(t, commonpb.ErrorCode_Success, resp.ErrorCode)
 	})
 
 	cancel()
