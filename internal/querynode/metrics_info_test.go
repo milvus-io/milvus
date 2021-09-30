e7d92e4e798f999cdc1920dde94c105749603ab4
bigsheeper <yihao.dai@zilliz.com>
2021-09-30 21:54:08 +0800

    Add streaming segments memory to used memory when checkLoadMemory (#8940)
    Signed-off-by: bigsheeper <yihao.dai@zilliz.com>

--- internal/querynode/metrics_info_test.go
+++ internal/querynode/metrics_info_test.go
@@ -19,7 +19,9 @@ import (
 
 	"github.com/milvus-io/milvus/internal/proto/commonpb"
 	"github.com/milvus-io/milvus/internal/proto/milvuspb"
+	"github.com/milvus-io/milvus/internal/proto/querypb"
 	"github.com/milvus-io/milvus/internal/util/sessionutil"
+	"github.com/milvus-io/milvus/internal/util/typeutil"
 )
 
 func TestGetSystemInfoMetrics(t *testing.T) {
@@ -37,4 +39,68 @@ func TestGetSystemInfoMetrics(t *testing.T) {
 	resp, err := getSystemInfoMetrics(ctx, req, node)
 	assert.NoError(t, err)
 	resp.Status.ErrorCode = commonpb.ErrorCode_Success
+}
+
+func TestCheckSegmentMemory(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	genSegmentLoadInfo := func() *querypb.SegmentLoadInfo {
+		return &querypb.SegmentLoadInfo{
+			SegmentID:    defaultSegmentID,
+			PartitionID:  defaultPartitionID,
+			CollectionID: defaultCollectionID,
+			NumOfRows:    1,
+		}
+	}
+
+	t.Run("valid test", func(t *testing.T) {
+		node, err := genSimpleQueryNode(ctx)
+		assert.NoError(t, err)
+		err = checkSegmentMemory([]*querypb.SegmentLoadInfo{genSegmentLoadInfo()}, node.historical.replica, node.streaming.replica)
+		assert.NoError(t, err)
+	})
+
+	t.Run("test no collection", func(t *testing.T) {
+		node, err := genSimpleQueryNode(ctx)
+		assert.NoError(t, err)
+		node.historical.replica.freeAll()
+		err = checkSegmentMemory([]*querypb.SegmentLoadInfo{genSegmentLoadInfo()}, node.historical.replica, node.streaming.replica)
+		assert.Error(t, err)
+	})
+
+	t.Run("test OOM", func(t *testing.T) {
+		node, err := genSimpleQueryNode(ctx)
+		assert.NoError(t, err)
+		totalRAM := Params.CacheSize * 1024 * 1024 * 1024
+
+		col, err := node.historical.replica.getCollectionByID(defaultCollectionID)
+		assert.NoError(t, err)
+
+		sizePerRecord, err := typeutil.EstimateSizePerRecord(col.schema)
+		assert.NoError(t, err)
+
+		info := genSegmentLoadInfo()
+		info.NumOfRows = totalRAM / int64(sizePerRecord)
+		err = checkSegmentMemory([]*querypb.SegmentLoadInfo{info}, node.historical.replica, node.streaming.replica)
+		assert.Error(t, err)
+	})
+
+	t.Run("test EstimateSizePerRecord failed", func(t *testing.T) {
+		node, err := genSimpleQueryNode(ctx)
+		assert.NoError(t, err)
+
+		col, err := node.historical.replica.getCollectionByID(defaultCollectionID)
+		assert.NoError(t, err)
+
+		for _, param := range col.schema.Fields[0].TypeParams {
+			if param.Key == "dim" {
+				param.Value = "%&^%&"
+			}
+		}
+
+		info := genSegmentLoadInfo()
+		err = checkSegmentMemory([]*querypb.SegmentLoadInfo{info}, node.historical.replica, node.streaming.replica)
+		assert.Error(t, err)
+	})
 }
