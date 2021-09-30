e8ed607464b42bb6f109cf4f6512a1f9208493b0
yudong.cai <yudong.cai@zilliz.com>
2021-09-30 16:23:29 +0800

    [skip ci] Update comment for update state code
    Signed-off-by: yudong.cai <yudong.cai@zilliz.com>

--- internal/types/types.go
+++ internal/types/types.go
@@ -288,6 +288,8 @@ type RootCoord interface {
 type RootCoordComponent interface {
 	RootCoord
 
+	// UpdateStateCode updates state code for RootCoord
+	// State includes: Initializing, Healthy and Abnormal
 	UpdateStateCode(internalpb.StateCode)
 	SetDataCoord(context.Context, DataCoord) error
 	SetIndexCoord(IndexCoord) error
