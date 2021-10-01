49b5e8d4972b16f8b5cf119ddc8c129988c89d22
Binbin Lv <binbin.lv@zilliz.com>
2021-10-01 20:41:47 +0800

    [skip ci] Add test comments
    Signed-off-by: Binbin Lv <binbin.lv@zilliz.com>

--- tests/python_client/testcases/test_collection.py
+++ tests/python_client/testcases/test_collection.py
@@ -2484,6 +2484,11 @@ class TestHasCollectionInvalid(object):
 
     @pytest.mark.tags(CaseLabel.L2)
     def test_has_collection_with_invalid_collection_name(self, connect, get_collection_name):
+        """
+        target: test list collections with invalid scenario
+        method: show collection with invalid collection name
+        expected: raise exception
+        """
         collection_name = get_collection_name
         with pytest.raises(Exception) as e:
             connect.has_collection(collection_name)
