ac7c22d84e5bcf4997e92bad7962428e21d7b2d1
groot <yihua.mo@zilliz.com>
2021-10-01 22:54:09 +0800

    Add unittest for distributed/proxy (#8907)
    Signed-off-by: yhmo <yihua.mo@zilliz.com>

--- internal/distributed/proxy/client/client_test.go
+++ internal/distributed/proxy/client/client_test.go
@@ -13,26 +13,99 @@ package grpcproxyclient
 
 import (
 	"context"
+	"errors"
 	"testing"
 
+	"github.com/milvus-io/milvus/internal/proto/commonpb"
+	"github.com/milvus-io/milvus/internal/proto/internalpb"
+	"github.com/milvus-io/milvus/internal/proto/milvuspb"
+	"github.com/milvus-io/milvus/internal/proto/proxypb"
+	"github.com/milvus-io/milvus/internal/proxy"
 	"github.com/stretchr/testify/assert"
+	"google.golang.org/grpc"
 )
 
-func Test_NewClient(t *testing.T) {
-	client, err := NewClient(context.Background(), "")
-	assert.Nil(t, client)
-	assert.Error(t, err)
+type MockProxyClient struct {
+	err error
+}
 
-	client, err = NewClient(context.Background(), "localhost")
+func (m *MockProxyClient) GetComponentStates(ctx context.Context, in *internalpb.GetComponentStatesRequest, opts ...grpc.CallOption) (*internalpb.ComponentStates, error) {
+	return &internalpb.ComponentStates{}, m.err
+}
+
+func (m *MockProxyClient) GetStatisticsChannel(ctx context.Context, in *internalpb.GetStatisticsChannelRequest, opts ...grpc.CallOption) (*milvuspb.StringResponse, error) {
+	return &milvuspb.StringResponse{}, m.err
+}
+
+func (m *MockProxyClient) InvalidateCollectionMetaCache(ctx context.Context, in *proxypb.InvalidateCollMetaCacheRequest, opts ...grpc.CallOption) (*commonpb.Status, error) {
+	return &commonpb.Status{}, m.err
+}
+
+func (m *MockProxyClient) GetDdChannel(ctx context.Context, in *internalpb.GetDdChannelRequest, opts ...grpc.CallOption) (*milvuspb.StringResponse, error) {
+	return &milvuspb.StringResponse{}, m.err
+}
+
+func (m *MockProxyClient) ReleaseDQLMessageStream(ctx context.Context, in *proxypb.ReleaseDQLMessageStreamRequest, opts ...grpc.CallOption) (*commonpb.Status, error) {
+	return &commonpb.Status{}, m.err
+}
+
+func Test_NewClient(t *testing.T) {
+	proxy.Params.InitOnce()
+
+	ctx := context.Background()
+	client, err := NewClient(ctx, "")
+	assert.Nil(t, client)
+	assert.NotNil(t, err)
+
+	client, err = NewClient(ctx, "test")
 	assert.Nil(t, err)
 	assert.NotNil(t, client)
 
 	err = client.Start()
 	assert.Nil(t, err)
 
-	err = client.Stop()
+	err = client.Register()
 	assert.Nil(t, err)
 
-	err = client.Register()
+	checkFunc := func(retNotNil bool) {
+		retCheck := func(notNil bool, ret interface{}, err error) {
+			if notNil {
+				assert.NotNil(t, ret)
+				assert.Nil(t, err)
+			} else {
+				assert.Nil(t, ret)
+				assert.NotNil(t, err)
+			}
+		}
+
+		r1, err := client.GetComponentStates(ctx)
+		retCheck(retNotNil, r1, err)
+
+		r2, err := client.GetStatisticsChannel(ctx)
+		retCheck(retNotNil, r2, err)
+
+		r3, err := client.InvalidateCollectionMetaCache(ctx, nil)
+		retCheck(retNotNil, r3, err)
+
+		r4, err := client.ReleaseDQLMessageStream(ctx, nil)
+		retCheck(retNotNil, r4, err)
+	}
+
+	client.getGrpcClient = func() (proxypb.ProxyClient, error) {
+		return &MockProxyClient{err: nil}, errors.New("dummy")
+	}
+	checkFunc(false)
+
+	client.getGrpcClient = func() (proxypb.ProxyClient, error) {
+		return &MockProxyClient{err: errors.New("dummy")}, nil
+	}
+	checkFunc(false)
+
+	client.getGrpcClient = func() (proxypb.ProxyClient, error) {
+		return &MockProxyClient{err: nil}, nil
+	}
+	checkFunc(true)
+
+	err = client.Stop()
 	assert.Nil(t, err)
 }
