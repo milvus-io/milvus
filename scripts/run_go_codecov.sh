d7ff3ce4b39f703d5e6dabce0083f38a3ffaccfe
Yihua <michaelmo@YihuadeMacBook-Pro.local>
2021-10-03 14:54:41 +0800

    [skip ci] Add description to coverage script
    Signed-off-by: Yihua <michaelmo@YihuadeMacBook-Pro.local>

--- scripts/run_go_codecov.sh
+++ scripts/run_go_codecov.sh
@@ -6,6 +6,7 @@ FILE_COVERAGE_HTML="go_coverage.html"
 set -e
 echo "mode: atomic" > ${FILE_COVERAGE_INFO}
 
+# run unittest
 for d in $(go list ./internal... | grep -v vendor); do
     go test -race -coverpkg=./... -coverprofile=profile.out -covermode=atomic "$d"
     if [ -f profile.out ]; then
@@ -14,5 +15,6 @@ for d in $(go list ./internal... | grep -v vendor); do
     fi
 done
 
+# generate html report
 go tool cover -html=./${FILE_COVERAGE_INFO} -o ./${FILE_COVERAGE_HTML}
 echo "Export go coverage report to ${FILE_COVERAGE_HTML}"
