ff9016151f72d27e03c1d41c83699e35aeaca839
Congqi Xia <congqi.xia@zilliz.com>
2021-10-04 14:38:13 +0800

    Add IndexCoord implementation Assertion
    Signed-off-by: Congqi Xia <congqi.xia@zilliz.com>

--- internal/indexcoord/index_coord.go
+++ internal/indexcoord/index_coord.go
@@ -44,6 +44,9 @@ import (
 	"github.com/milvus-io/milvus/internal/util/typeutil"
 )
 
+// make sure IndexCoord implements types.IndexCoord
+var _ types.IndexCoord = (*IndexCoord)(nil)
+
 // IndexCoord is a component responsible for scheduling index construction tasks and maintaining index status.
 // IndexCoord accepts requests from rootcoord to build indexes, delete indexes, and query index information.
 // IndexCoord is responsible for assigning IndexBuildID to the request to build the index, and forwarding the
