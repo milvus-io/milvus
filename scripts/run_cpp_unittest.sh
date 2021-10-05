f4aaf4ee04f65aaa4ce7a333cbbc977c313b53a0
groot <yihua.mo@zilliz.com>
2021-10-05 12:26:02 +0800

    [skip ci] Modify description of script (#9254)
    Signed-off-by: groot <yihua.mo@zilliz.com>

--- scripts/run_cpp_unittest.sh
+++ scripts/run_cpp_unittest.sh
@@ -37,7 +37,7 @@ done
 
 # run cwrapper unittest
 if [ -f ${CWRAPPER_UNITTEST} ];then
-  echo "Running cwrapper unittest ..."
+  echo "Running cwrapper unittest: ${CWRAPPER_UNITTEST}"
   ${CWRAPPER_UNITTEST}
   if [ $? -ne 0 ]; then
       echo ${CWRAPPER_UNITTEST} " run failed"
