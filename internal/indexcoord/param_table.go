d9dcb3cfe901af9efb0f3b09b5544c4b9659f37e
cai.zhang <cai.zhang@zilliz.com>
2021-10-07 02:08:22 +0800

    [skip ci]Fix golint in indexcoord/prama_table.go (#9371)
    Signed-off-by: cai.zhang <cai.zhang@zilliz.com>

--- internal/indexcoord/param_table.go
+++ internal/indexcoord/param_table.go
@@ -136,6 +136,7 @@ func (pt *ParamTable) initMinIOSecretAccessKey() {
 	pt.MinIOSecretAccessKey = ret
 }
 
+// initMinIOUseSSL initializes the minio use SSL of configuration items.
 func (pt *ParamTable) initMinIOUseSSL() {
 	ret, err := pt.Load("minio.useSSL")
 	if err != nil {
