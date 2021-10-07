44c7e35855212b4af899c256172bd14e5c8be8e4
yukun <kun.yu@zilliz.com>
2021-10-07 20:56:15 +0800

    [skip ci]Add comments for rmqMessage
    Signed-off-by: yukun <kun.yu@zilliz.com>

--- internal/util/mqclient/rmq_message.go
+++ internal/util/mqclient/rmq_message.go
@@ -18,6 +18,7 @@ import (
 // Check rmqMessage implements ConsumerMessage
 var _ ConsumerMessage = (*rmqMessage)(nil)
 
+// rmqMessage wraps the message for rocksmq
 type rmqMessage struct {
 	msg rocksmq.ConsumerMessage
 }
