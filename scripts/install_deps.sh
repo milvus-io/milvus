4a32f1b0fcc430a1bd5537c290a58a3cc9aaaa41
XuanYang-cn <xuan.yang@zilliz.com>
2021-10-09 18:01:27 +0800

    [skip ci]Add LF AI&Data foundation license (#9507)
    Signed-off-by: yangxuan <xuan.yang@zilliz.com>

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env bash
 
+# Licensed to the LF AI & Data foundation under one
+# or more contributor license agreements. See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership. The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License. You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 if [[ -x "$(command -v apt)" ]]; then
     sudo apt install -y g++ gcc make ccache libssl-dev zlib1g-dev libboost-regex-dev \
         libboost-program-options-dev libboost-system-dev libboost-filesystem-dev \
--- scripts/run_cpp_unittest.sh
+++ scripts/run_cpp_unittest.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env bash
 
+# Licensed to the LF AI & Data foundation under one
+# or more contributor license agreements. See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership. The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License. You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 set -e
 
 SOURCE="${BASH_SOURCE[0]}"
@@ -17,13 +33,13 @@ CWRAPPER_UNITTEST="${SCRIPTS_DIR}/../internal/storage/cwrapper/output/wrapper_te
 
 # currently core will install target lib to "internal/core/output/lib"
 if [ -d "${CORE_INSTALL_PREFIX}/lib" ]; then
-	export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${CORE_INSTALL_PREFIX}/lib
+    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${CORE_INSTALL_PREFIX}/lib
 fi
 
 # run unittest
 for UNITTEST_DIR in "${UNITTEST_DIRS[@]}"; do
   if [ ! -d "${UNITTEST_DIR}" ]; then
-	echo "The unittest folder does not exist!"
+    echo "The unittest folder does not exist!"
     exit 1
   fi
 
