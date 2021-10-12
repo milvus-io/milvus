027ecfbe572b4f83b6dfd81ff2433aa53c2d0940
ThreadDao <yufen.zong@zilliz.com>
2021-10-12 20:02:08 +0800

    [skip ci] Add test delete file
    Signed-off-by: ThreadDao <yufen.zong@zilliz.com>

---
+++ tests/python_client/testcases/test_delete.py
@@ -0,0 +1,13 @@
+import pytest
+
+from base.client_base import TestcaseBase
+
+
+@pytest.mark.skip(reason="Waiting for development")
+class TestDeleteParams(TestcaseBase):
+    pass
+
+
+@pytest.mark.skip(reason="Waiting for development")
+class TestDeleteOperation(TestcaseBase):
+    pass
