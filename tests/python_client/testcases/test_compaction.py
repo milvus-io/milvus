ec16b5a6b21eaad81edfc0fb9125582be73c3d3a
ThreadDao <yufen.zong@zilliz.com>
2021-10-12 20:12:21 +0800

    [skip ci] Add test compact file
    Signed-off-by: ThreadDao <yufen.zong@zilliz.com>

---
+++ tests/python_client/testcases/test_compaction.py
@@ -0,0 +1,13 @@
+import pytest
+
+from base.client_base import TestcaseBase
+
+
+@pytest.mark.skip(reason="Waiting for development")
+class TestCompactionParams(TestcaseBase):
+    pass
+
+
+@pytest.mark.skip(reason="Waiting for development")
+class TestCompactionOperation(TestcaseBase):
+    pass
