cbb28f9bab53b3fd26f1023a15da2c5623956edd
ThreadDao <yufen.zong@zilliz.com>
2021-10-15 14:16:38 +0800

    [skip ci] Add skipped delete case (#9928)
    Signed-off-by: ThreadDao <yufen.zong@zilliz.com>

--- tests/python_client/testcases/test_delete_20.py
+++ tests/python_client/testcases/test_delete_20.py
@@ -3,7 +3,7 @@ import pytest
 from base.client_base import TestcaseBase
 from common import common_func as cf
 from common import common_type as ct
-# from utils.util_log import test_log as log
+from utils.util_log import test_log as log
 from common.common_type import CaseLabel, CheckTasks
 
 prefix = "delete"
@@ -33,24 +33,10 @@ class TestDeleteParams(TestcaseBase):
         # init collection with default_nb default data
         collection_w, _, _, ids = self.init_collection_general(prefix, insert_data=True, is_binary=is_binary)
         expr = f'{ct.default_int64_field_name} in {ids[0][:half_nb]}'
+
         # delete half of data
         collection_w.delete(expr)
         assert collection_w.num_entities == half_nb
-
-    @pytest.mark.skip(reason="Delete function is not implemented")
-    @pytest.mark.tags(CaseLabel.L1)
-    def test_delete_empty(self):
-        """
-        target: test delete with empty array expr
-        method: delete with expr: "id in []"
-        expected: assert num entities
-        """
-        # init collection with nb default data
-        collection_w = self.init_collection_general(prefix, nb=tmp_nb, insert_data=True)[0]
-        expr = f'{ct.default_int64_field_name} in {[]}'
-        # delete half of data
-        collection_w.delete(expr)
-        assert collection_w.num_entities == ct.default_nb
 
     @pytest.mark.tags(CaseLabel.L1)
     def test_delete_without_connection(self):
@@ -59,13 +45,58 @@ class TestDeleteParams(TestcaseBase):
         method: delete after remove connection
         expected: raise exception
         """
-        # init collection with nb default data
+        # init collection with tmp_nb default data
         collection_w = self.init_collection_general(prefix, nb=tmp_nb, insert_data=True)[0]
+
+        # remove connection and delete
         self.connection_wrap.remove_connection(ct.default_alias)
         res_list, _ = self.connection_wrap.list_connections()
         assert ct.default_alias not in res_list
         error = {ct.err_code: 0, ct.err_msg: "should create connect first"}
         collection_w.delete(expr=tmp_expr, check_task=CheckTasks.err_res, check_items=error)
+
+    @pytest.mark.skip(reason="Delete function is not implemented")
+    @pytest.mark.tags(CaseLabel.L1)
+    def test_delete_expr_none(self):
+        """
+        target: test delete with None expr
+        method: delete with None expr
+        expected: todo
+        """
+        # init collection with tmp_nb default data
+        collection_w = self.init_collection_general(prefix, nb=tmp_nb, insert_data=True)[0]
+        collection_w.delete(None)
+        log.debug(collection_w.num_entities)
+
+    @pytest.mark.tags(CaseLabel.L1)
+    @pytest.mark.parametrize("expr", [1, "12-s", "中文", [], ()])
+    @pytest.mark.skip(reason="Delete function is not implemented")
+    def test_delete_expr_non_string(self, expr):
+        """
+        target: test delete with non-string expression
+        method: delete with non-string expr
+        expected: raise exception
+        """
+        # init collection with tmp_nb default data
+        collection_w = self.init_collection_general(prefix, nb=tmp_nb, insert_data=True)[0]
+        error = {ct.err_code: 0, ct.err_msg: "..."}
+        collection_w.delete(expr, check_task=CheckTasks.err_res, check_items=error)
+
+    @pytest.mark.skip(reason="Delete function is not implemented")
+    @pytest.mark.tags(CaseLabel.L1)
+    def test_delete_expr_empty_value(self):
+        """
+        target: test delete with empty array expr
+        method: delete with expr: "id in []"
+        expected: assert num entities
+        """
+        # init collection with tmp_nb default data
+        collection_w = self.init_collection_general(prefix, nb=tmp_nb, insert_data=True)[0]
+        expr = f'{ct.default_int64_field_name} in {[]}'
+
+        # delete empty entities
+        collection_w.delete(expr)
+        assert collection_w.num_entities == ct.default_nb
 
 
 @pytest.mark.skip(reason="Waiting for development")
