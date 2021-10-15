08699a9377d3906bb3959ee8842681a785676699
Xiangyu Wang <xiangyu.wang@zilliz.com>
2021-10-15 14:41:07 +0800

    Fix golint warning in indexparamcheck
    Signed-off-by: Xiangyu Wang <xiangyu.wang@zilliz.com>

--- internal/util/indexparamcheck/conf_adapter.go
+++ internal/util/indexparamcheck/conf_adapter.go
@@ -110,7 +110,9 @@ const (
 var METRICS = []string{L2, IP} // const
 
 // BinIDMapMetrics is a set of all metric types supported for binary vector.
-var BinIDMapMetrics = []string{HAMMING, JACCARD, TANIMOTO, SUBSTRUCTURE, SUPERSTRUCTURE}   // const
+var BinIDMapMetrics = []string{HAMMING, JACCARD, TANIMOTO, SUBSTRUCTURE, SUPERSTRUCTURE} // const
+
+// BinIvfMetrics is a set of all metrics supported for binary vector index.
 var BinIvfMetrics = []string{HAMMING, JACCARD, TANIMOTO}                                   // const
 var supportDimPerSubQuantizer = []int{32, 28, 24, 20, 16, 12, 10, 8, 6, 4, 3, 2, 1}        // const
 var supportSubQuantizer = []int{96, 64, 56, 48, 40, 32, 28, 24, 20, 16, 12, 8, 4, 3, 2, 1} // const
