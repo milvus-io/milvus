378a04ff367a8f5046bfdc5069442ad4415d76fc
zhuwenxing <wenxing.zhu@zilliz.com>
2021-10-19 18:03:58 +0800

    [skip ci]Fix pod failure chaos github action
    Signed-off-by: zhuwenxing <wenxing.zhu@zilliz.com>

--- tests/python_client/chaos/constants.py
+++ tests/python_client/chaos/constants.py
@@ -27,4 +27,5 @@ CHAOS_CONFIG_ENV = 'CHAOS_CONFIG_PATH'      # env variables for chao path
 TESTS_CONFIG_LOCATION = 'chaos_objects/pod_kill/' # path to the chaos CRD
 ALL_CHAOS_YAMLS = 'chaos_datanode*.yaml'    # chaos file name(s) to be run against
 WAIT_PER_OP = 10                            # time to wait in seconds between operations
+CHAOS_DURATION = 40                         # chaos duration time in seconds
 DEFAULT_INDEX_PARAM = {"index_type": "IVF_SQ8", "metric_type": "L2", "params": {"nlist": 64}}
