d4fbaa0f684fffee6fc1f021c0919b8d25c372f4
yudong.cai <yudong.cai@zilliz.com>
2021-10-20 14:04:48 +0800

    Format CGoHelper.h
    Signed-off-by: yudong.cai <yudong.cai@zilliz.com>

--- internal/core/src/common/CGoHelper.h
+++ internal/core/src/common/CGoHelper.h
@@ -9,9 +9,13 @@
 // is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
 // or implied. See the License for the specific language governing permissions and limitations under the License
 
-#include "common/type_c.h"
+#pragma once
+
 #include <string>
+#include "common/type_c.h"
+
 namespace milvus {
+
 inline CProtoResult
 AllocCProtoResult(const google::protobuf::Message& msg) {
     auto size = msg.ByteSize();
