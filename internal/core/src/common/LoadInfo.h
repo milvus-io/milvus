dbdf6be1b13ff7da8befd0308e3569da8f98ee9f
yudong.cai <yudong.cai@zilliz.com>
2021-10-20 14:16:05 +0800

    Reorder header files for LoadInfo.h
    Signed-off-by: yudong.cai <yudong.cai@zilliz.com>

--- internal/core/src/common/LoadInfo.h
+++ internal/core/src/common/LoadInfo.h
@@ -10,8 +10,9 @@
 // or implied. See the License for the specific language governing permissions and limitations under the License
 
 #pragma once
-#include <string>
+
 #include <map>
+#include <string>
 
 #include "knowhere/index/vector_index/VecIndex.h"
 
