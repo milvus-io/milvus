4a8619619bb374b8d01242e25135060f7b0eda0e
yukun <kun.yu@zilliz.com>
2021-10-21 22:43:21 +0800

    [skip ci]Add comments for rocksmq (#10388)
    Signed-off-by: fishpenguin <kun.yu@zilliz.com>

--- internal/util/rocksmq/client/rocksmq/consumer_impl.go
+++ internal/util/rocksmq/client/rocksmq/consumer_impl.go
@@ -98,6 +98,7 @@ func (c *consumer) Chan() <-chan ConsumerMessage {
 	return c.messageCh
 }
 
+// Seek seek rocksmq position to id and notify consumer to consume
 func (c *consumer) Seek(id UniqueID) error { //nolint:govet
 	err := c.client.server.Seek(c.topic, c.consumerName, id)
 	if err != nil {
