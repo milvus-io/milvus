c7aea829d9fb4b1e83568f411c78dd75b0120428
bigsheeper <yihao.dai@zilliz.com>
2021-10-21 19:54:17 +0800

    [skip ci] Add comment for getSegmentNum (#10372)
    Signed-off-by: bigsheeper <yihao.dai@zilliz.com>

--- internal/querynode/collection_replica.go
+++ internal/querynode/collection_replica.go
@@ -485,6 +485,7 @@ func (colReplica *collectionReplica) hasSegmentPrivate(segmentID UniqueID) bool 
 	return ok
 }
 
+// getSegmentNum returns num of segments in collectionReplica
 func (colReplica *collectionReplica) getSegmentNum() int {
 	colReplica.mu.RLock()
 	defer colReplica.mu.RUnlock()
