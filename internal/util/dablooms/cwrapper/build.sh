b77efd9feb8b203b75923e1e06c80b27046b41f5
Dhruva Shaw <34207400+Dhruvacube@users.noreply.github.com>
2021-10-22 13:19:58 +0530

    shebang is not the first line

--- internal/util/dablooms/cwrapper/build.sh
+++ internal/util/dablooms/cwrapper/build.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Copyright (C) 2019-2020 Zilliz. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance
@@ -9,7 +11,6 @@
 # is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
 # or implied. See the License for the specific language governing permissions and limitations under the License.
 
-#!/bin/bash
 
 SOURCE=${BASH_SOURCE[0]}
 while [ -h $SOURCE ]; do # resolve $SOURCE until the file is no longer a symlink
