889845cd7dd022ca9543e038f124014f05dfcb06
wangting0128 <ting.wang@zilliz.com>
2021-10-22 17:44:08 +0800

    [skip ci] Add comment of update file on benchmark
    Signed-off-by: wangting0128 <ting.wang@zilliz.com>

--- tests/benchmark/milvus_benchmark/update.py
+++ tests/benchmark/milvus_benchmark/update.py
@@ -9,6 +9,7 @@ import utils
 
 
 def parse_server_tag(server_tag):
+    """ paser server tag from server config"""
     # tag format: "8c"/"8c16m"/"8c16m1g"
     if server_tag[-1] == "c":
         p = r"(\d+)c"
