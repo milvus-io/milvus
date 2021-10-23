88781064e2322339999f889684c5e7f9da09f789
godchen <qingxiang.chen@zilliz.com>
2021-10-23 15:27:55 +0800

    [skip ci]Fix global id typo error
    Signed-off-by: godchen <qingxiang.chen@zilliz.com>

--- internal/allocator/global_id.go
+++ internal/allocator/global_id.go
@@ -74,7 +74,7 @@ func (gia *GlobalIDAllocator) AllocOne() (typeutil.UniqueID, error) {
 	return idStart, nil
 }
 
-// UpdateID update timestamp of allocator.
+// UpdateID updates timestamp of allocator.
 func (gia *GlobalIDAllocator) UpdateID() error {
 	return gia.allocator.UpdateTSO()
 }
