4cd83e78ed6729fb388c26db819e91d74a4cc070
Cai Yudong <yudong.cai@zilliz.com>
2021-10-26 13:10:30 +0800

    Reorder header files for segcore/Collection.cpp (#10634)
    Signed-off-by: yudong.cai <yudong.cai@zilliz.com>

--- internal/core/src/segcore/Collection.cpp
+++ internal/core/src/segcore/Collection.cpp
@@ -9,13 +9,10 @@
 // is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
 // or implied. See the License for the specific language governing permissions and limitations under the License
 
-#include "segcore/Collection.h"
-#include "pb/common.pb.h"
-#include "pb/schema.pb.h"
-#include "pb/etcd_meta.pb.h"
-
 #include <google/protobuf/text_format.h>
-#include <knowhere/index/vector_index/adapter/VectorAdapter.h>
+
+#include "pb/schema.pb.h"
+#include "segcore/Collection.h"
 
 namespace milvus::segcore {
 
