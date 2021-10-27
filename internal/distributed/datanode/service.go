e5b90f15299b63675b872aa9bdf29c890992146d
yhmo <yihua.mo@zilliz.com>
2021-10-27 11:18:46 +0800

    Add log
    Signed-off-by: yhmo <yihua.mo@zilliz.com>

--- internal/distributed/datanode/service.go
+++ internal/distributed/datanode/service.go
@@ -129,12 +129,12 @@ func (s *Server) Run() error {
 	if err := s.init(); err != nil {
 		return err
 	}
-	log.Debug("data node init done ...")
+	log.Debug("DataNode init done ...")
 
 	if err := s.start(); err != nil {
 		return err
 	}
-	log.Debug("data node start done ...")
+	log.Debug("DataNode start done ...")
 	return nil
 }
 
