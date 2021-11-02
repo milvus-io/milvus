4e256dafc9f5d33560b6b24827de53ae84d757b8
ThreadDao <yufen.zong@zilliz.com>
2021-11-02 20:31:08 +0800

    [skip ci] Add skipped compact case
    Signed-off-by: ThreadDao <yufen.zong@zilliz.com>

--- tests/python_client/testcases/test_compaction.py
+++ tests/python_client/testcases/test_compaction.py
@@ -1,11 +1,20 @@
 import pytest
 
 from base.client_base import TestcaseBase
+from common.common_type import CaseLabel
 
 
 @pytest.mark.skip(reason="Waiting for development")
 class TestCompactionParams(TestcaseBase):
-    pass
+
+    @pytest.mark.tags(CaseLabel.L1)
+    def test_compact_without_connection(self):
+        """
+        target: test compact without connection
+        method: compact after remove connection
+        expected: raise exception
+        """
+        pass
 
 
 @pytest.mark.skip(reason="Waiting for development")
