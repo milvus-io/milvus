38a2316fbdda5560a9177e4033462e32e525ff1e
Jenny Li <93511422+jennyli-z@users.noreply.github.com>
2021-11-03 15:08:13 +0800

    [skip ci] Fix nightly ci test timeout (#11166)
    Signed-off-by: Jenny Li <jing.li@zilliz.com>

--- build/ci/jenkins/NightlyCI.groovy
+++ build/ci/jenkins/NightlyCI.groovy
@@ -6,7 +6,7 @@ String cron_timezone = "TZ=Asia/Shanghai"
 String cron_string = BRANCH_NAME == "master" ? "50 22 * * * " : ""
 
 int total_timeout_minutes = 660
-int e2e_timeout_seconds = 6 * 60 * 60
+
 
 pipeline {
     agent none
@@ -64,6 +64,7 @@ pipeline {
                                     script {
                                         sh 'printenv'
                                         def clusterEnabled = "false"
+                                        int e2e_timeout_seconds = 6 * 60 * 60
                                         if ("${MILVUS_SERVER_TYPE}" == "distributed") {
                                             clusterEnabled = "true"
                                             e2e_timeout_seconds = 10 * 60 * 60
