c50616711e7bf0dddf92a809680f010a28544bd1
cai.zhang <cai.zhang@zilliz.com>
2021-11-03 19:33:01 +0800

    [skip ci]Fix golint error for Init in QueryCoord (#11163)
    Signed-off-by: cai.zhang <cai.zhang@zilliz.com>

--- internal/distributed/querycoord/client/client.go
+++ internal/distributed/querycoord/client/client.go
@@ -129,6 +129,7 @@ func NewClient(ctx context.Context, metaRoot string, etcdEndpoints []string) (*C
 	return client, nil
 }
 
+// Init initializes QueryCoord's grpc client.
 func (c *Client) Init() error {
 	Params.Init()
 	return nil
