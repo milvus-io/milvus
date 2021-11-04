d1f4106b7c75b9ebfa35d34681d1b4314feb2bef
zhenshan.cao <zhenshan.cao@zilliz.com>
2021-11-04 21:13:00 +0800

    Fix release collection block in query node (#11249) (#11277)
    Signed-off-by: bigsheeper <yihao.dai@zilliz.com>
    
    Co-authored-by: bigsheeper <yihao.dai@zilliz.com>

--- internal/querynode/collection_replica.go
+++ internal/querynode/collection_replica.go
@@ -185,10 +185,10 @@ func (colReplica *collectionReplica) addCollection(collectionID UniqueID, schema
 
 // removeCollection removes the collection from collectionReplica
 func (colReplica *collectionReplica) removeCollection(collectionID UniqueID) error {
-	colReplica.mu.Lock()
 	colReplica.queryMu.Lock()
-	defer colReplica.queryMu.Unlock()
+	colReplica.mu.Lock()
 	defer colReplica.mu.Unlock()
+	defer colReplica.queryMu.Unlock()
 	return colReplica.removeCollectionPrivate(collectionID)
 }
 
@@ -335,10 +335,10 @@ func (colReplica *collectionReplica) addPartitionPrivate(collectionID UniqueID, 
 
 // removePartition removes the partition from collectionReplica
 func (colReplica *collectionReplica) removePartition(partitionID UniqueID) error {
-	colReplica.mu.Lock()
 	colReplica.queryMu.Lock()
-	defer colReplica.queryMu.Unlock()
+	colReplica.mu.Lock()
 	defer colReplica.mu.Unlock()
+	defer colReplica.queryMu.Unlock()
 	return colReplica.removePartitionPrivate(partitionID)
 }
 
