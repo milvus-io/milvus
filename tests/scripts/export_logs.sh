52469a891c24594dca9149bb9fe3ea1921d13e67
Jenny Li <jing.li@zilliz.com>
2021-11-08 20:38:51 +0800

    [skip ci]  Add comment into export_logs script (#11442)
    Signed-off-by: Jenny Li <jing.li@zilliz.com>

--- tests/scripts/export_logs.sh
+++ tests/scripts/export_logs.sh
@@ -11,6 +11,7 @@
 # is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
 # or implied. See the License for the specific language governing permissions and limitations under the License.
 
+# export kind logs to artifacts, logs can be seen in the jenkins 
 set -e
 set -x
 
