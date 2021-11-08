4a0d23cac48bea0df75ad086382bd4f2480f3ffb
xige-16 <xi.ge@zilliz.com>
2021-11-08 20:46:02 +0800

    [skip ci] Add comments for LoadBalance
    Signed-off-by: xige-16 <xi.ge@zilliz.com>

--- internal/distributed/querycoord/service.go
+++ internal/distributed/querycoord/service.go
@@ -310,6 +310,7 @@ func (s *Server) GetSegmentInfo(ctx context.Context, req *querypb.GetSegmentInfo
 	return s.queryCoord.GetSegmentInfo(ctx, req)
 }
 
+// LoadBalance migrate the sealed segments on the source node to the dst nodes
 func (s *Server) LoadBalance(ctx context.Context, req *querypb.LoadBalanceRequest) (*commonpb.Status, error) {
 	return s.queryCoord.LoadBalance(ctx, req)
 }
