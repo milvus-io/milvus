761904cca9fdc8424d10aee53bbedcc142c1c65a
yudong.cai <yudong.cai@zilliz.com>
2021-11-11 16:43:12 +0800

    Remove useless MarshalForCGo
    Signed-off-by: yudong.cai <yudong.cai@zilliz.com>

--- internal/querynode/cgo_helper_test.go
+++ internal/querynode/cgo_helper_test.go
@@ -2,15 +2,7 @@ package querynode
 
 import (
 	"testing"
-
-	"github.com/milvus-io/milvus/internal/proto/schemapb"
-	"github.com/stretchr/testify/assert"
 )
 
 func TestCGoHelper_Naive(t *testing.T) {
-	pb := schemapb.BoolArray{
-		Data: []bool{true, false, true, true, true},
-	}
-	_, err := MarshalForCGo(&pb)
-	assert.Nil(t, err)
 }
