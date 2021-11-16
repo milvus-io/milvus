7720e99b46d878c2a7663965e372a4ea97936136
ThreadDao <yufen.zong@zilliz.com>
2021-11-16 14:22:01 +0800

    Fix parameter changed in query result check
    Signed-off-by: ThreadDao <yufen.zong@zilliz.com>

--- tests/python_client/check/param_check.py
+++ tests/python_client/check/param_check.py
@@ -209,6 +209,7 @@ def equal_entities_list(exp, actual, with_vec=False):
     actual = [{"int": 1, "vec": [0.888888, 0.222222]}, {"int": 0, "vec": [0.999999, 0.111111]}]
     exp = actual
     """
+    exp = exp.copy()
     if len(exp) != len(actual):
         return False
 
