152d55dbfa9b546e598723b26b4efd8eaacab3ab
bigsheeper <yihao.dai@zilliz.com>
2021-11-17 23:37:11 +0800

    Fix the deadlock of the querycoord cluster (#11796)
    Signed-off-by: xige-16 <xi.ge@zilliz.com>
    
    Co-authored-by: xige-16 <xi.ge@zilliz.com>

--- internal/querycoord/mock_querynode_server_test.go
+++ internal/querycoord/mock_querynode_server_test.go
@@ -21,6 +21,7 @@ import (
 	"errors"
 	"net"
 	"strconv"
+	"sync"
 
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
@@ -67,6 +68,7 @@ type queryNodeServerMock struct {
 	getMetrics          func() (*milvuspb.GetMetricsResponse, error)
 
 	segmentInfos map[UniqueID]*querypb.SegmentInfo
+	segmentMu    sync.RWMutex
 
 	totalMem uint64
 }
@@ -212,7 +214,9 @@ func (qs *queryNodeServerMock) LoadSegments(ctx context.Context, req *querypb.Lo
 			MemSize:      info.NumOfRows * int64(sizePerRecord),
 			NumRows:      info.NumOfRows,
 		}
+		qs.segmentMu.Lock()
 		qs.segmentInfos[info.SegmentID] = segmentInfo
+		qs.segmentMu.Unlock()
 	}
 
 	return qs.loadSegment()
@@ -232,11 +236,13 @@ func (qs *queryNodeServerMock) ReleaseSegments(ctx context.Context, req *querypb
 
 func (qs *queryNodeServerMock) GetSegmentInfo(ctx context.Context, req *querypb.GetSegmentInfoRequest) (*querypb.GetSegmentInfoResponse, error) {
 	segmentInfos := make([]*querypb.SegmentInfo, 0)
+	qs.segmentMu.RLock()
 	for _, info := range qs.segmentInfos {
 		if info.CollectionID == req.CollectionID && info.NodeID == qs.queryNodeID {
 			segmentInfos = append(segmentInfos, info)
 		}
 	}
+	qs.segmentMu.RUnlock()
 
 	res, err := qs.getSegmentInfos()
 	if err == nil {
