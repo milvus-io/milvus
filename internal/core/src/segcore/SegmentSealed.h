97c2d2aec2cc890cc3b5680651d26ea22ad7c815
Cai Yudong <yudong.cai@zilliz.com>
2021-11-30 18:41:44 +0800

    Reorder header files for segcore/SegmentSealed.h (#12435)
    Signed-off-by: yudong.cai <yudong.cai@zilliz.com>

--- internal/core/src/segcore/SegmentSealed.h
+++ internal/core/src/segcore/SegmentSealed.h
@@ -8,13 +8,15 @@
 // Unless required by applicable law or agreed to in writing, software distributed under the License
 // is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
 // or implied. See the License for the specific language governing permissions and limitations under the License
-#pragma once
-#include <memory>
 
-#include "segcore/SegmentInterface.h"
-#include "pb/segcore.pb.h"
-#include "common/LoadInfo.h"
+#pragma once
+
+#include <memory>
 #include <utility>
+
+#include "common/LoadInfo.h"
+#include "pb/segcore.pb.h"
+#include "segcore/SegmentInterface.h"
 
 namespace milvus::segcore {
 
