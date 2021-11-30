f899644e88d071aca4992319e29267f0d98bca97
Cai Yudong <yudong.cai@zilliz.com>
2021-11-30 18:43:40 +0800

    Update header files for segcore/SegmentSealedImpl.cpp (#12438)
    Signed-off-by: yudong.cai <yudong.cai@zilliz.com>

--- internal/core/src/segcore/SegmentSealedImpl.cpp
+++ internal/core/src/segcore/SegmentSealedImpl.cpp
@@ -9,11 +9,11 @@
 // is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
 // or implied. See the License for the specific language governing permissions and limitations under the License
 
+#include "SegmentSealedImpl.h"
 #include "common/Consts.h"
 #include "query/SearchBruteForce.h"
 #include "query/SearchOnSealed.h"
 #include "query/ScalarIndex.h"
-#include "segcore/SegmentSealedImpl.h"
 
 namespace milvus::segcore {
 
