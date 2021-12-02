26405d13454ffd140f5925a4742410c77563c88e
Jenny Li <jing.li@zilliz.com>
2021-12-02 10:03:32 +0800

    [skip ci] Update minio storage class in PR to be consistent with Nightly.groovy.groovy (#12579)
    Signed-off-by: Jenny Li <jing.li@zilliz.com>

--- build/ci/jenkins/PR.groovy
+++ build/ci/jenkins/PR.groovy
@@ -122,7 +122,7 @@ pipeline {
                                                     --skip-test \
                                                     --skip-build \
                                                     --skip-build-image \
-                                                    --install-extra-arg "--set etcd.persistence.storageClass=local-path \
+                                                    --install-extra-arg "--set etcd.persistence.storageClass=local-path --set minio.persistence.storageClass=local-path \
                                                     --set metrics.serviceMonitor.enabled=true" 
                                                     """
                                                 }
