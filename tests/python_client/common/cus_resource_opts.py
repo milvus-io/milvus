7ed7e053cc018215f280e4c32efe814632a3ff1b
zhuwenxing <wenxing.zhu@zilliz.com>
2021-12-02 19:39:21 +0800

    [skip ci]Organize wait pods ready function
    Signed-off-by: zhuwenxing <wenxing.zhu@zilliz.com>

--- tests/python_client/common/cus_resource_opts.py
+++ tests/python_client/common/cus_resource_opts.py
@@ -97,32 +97,3 @@ class CustomResourceOperations(object):
             for item in cus_objects["items"]:
                 metadata_name = item["metadata"]["name"]
                 self.delete(metadata_name)
-
-    def wait_pods_ready(self, namespace, label_selector):
-        """wait pods with label selector all ready"""
-        config.load_kube_config()
-        api_instance = client.CoreV1Api()
-        try:
-            all_pos_ready_flag = False
-            timeout = 0
-            while (not all_pos_ready_flag and timeout < 360):
-                api_response = api_instance.list_namespaced_pod(namespace=namespace,label_selector=label_selector)
-                all_pos_ready_flag = True
-                for item in api_response.items:
-                    for c in item.status.container_statuses:
-                        log.info(f"{c.name} statu is {c.ready}")
-                        if c.ready is False:
-                            all_pos_ready_flag = False
-                            break
-                if not all_pos_ready_flag:
-                    log.info("all pods are not ready, please wait")
-                    time.sleep(30)
-                    timeout += 30
-            if all_pos_ready_flag:
-                log.info(f"all pods in namespace {namespace} with label {label_selector} are ready")
-            else:
-                log.info("timeout for waiting all pods in namespace {namespace} with label {label_selector} ready")
-        except ApiException as e:
-            log.error("Exception when calling CoreV1Api->list_namespaced_pod: %s\n" % e)
-            raise Exception(str(e))
-        return all_pos_ready_flag
