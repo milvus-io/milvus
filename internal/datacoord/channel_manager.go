58664bb6b96c5b1092740a3b5b26fa6862918d08
congqixia <congqi.xia@zilliz.com>
2021-12-02 19:17:38 +0800

    Fix zero length slice declaration in datacoord/channel_manager.go (#12582)
    Signed-off-by: Congqi Xia <congqi.xia@zilliz.com>

--- internal/datacoord/channel_manager.go
+++ internal/datacoord/channel_manager.go
@@ -175,7 +175,7 @@ func (c *ChannelManager) bgCheckChannelsWork(ctx context.Context) {
 
 func (c *ChannelManager) getNewOnlines(curr []int64, old []int64) []int64 {
 	mold := make(map[int64]struct{})
-	ret := make([]int64, 0)
+	ret := make([]int64, 0, len(curr))
 	for _, n := range old {
 		mold[n] = struct{}{}
 	}
@@ -189,7 +189,7 @@ func (c *ChannelManager) getNewOnlines(curr []int64, old []int64) []int64 {
 
 func (c *ChannelManager) getOfflines(curr []int64, old []int64) []int64 {
 	mcurr := make(map[int64]struct{})
-	ret := make([]int64, 0)
+	ret := make([]int64, 0, len(old))
 	for _, n := range curr {
 		mcurr[n] = struct{}{}
 	}
