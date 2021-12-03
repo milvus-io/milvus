2a98ce272ed78a961ab90b9c1ee9f4883ea4e0f1
bigsheeper <yihao.dai@zilliz.com>
2021-12-03 20:37:38 +0800

    [skip ci] Add comment for filterInvalidDeleteMessage (#12733)
    Signed-off-by: bigsheeper <yihao.dai@zilliz.com>

--- internal/querynode/flow_graph_filter_dm_node.go
+++ internal/querynode/flow_graph_filter_dm_node.go
@@ -104,6 +104,7 @@ func (fdmNode *filterDmNode) Operate(in []flowgraph.Msg) []flowgraph.Msg {
 	return []Msg{res}
 }
 
+// filterInvalidDeleteMessage would filter out invalid delete messages
 func (fdmNode *filterDmNode) filterInvalidDeleteMessage(msg *msgstream.DeleteMsg) *msgstream.DeleteMsg {
 	sp, ctx := trace.StartSpanFromContext(msg.TraceCtx())
 	msg.SetTraceCtx(ctx)
