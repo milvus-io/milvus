8a392c9a3745d30db7dcf17f38f716f05c1974af
Jenny Li <jing.li@zilliz.com>
2021-12-09 17:14:19 +0800

    [skip ci] add get author email
    Signed-off-by: Jenny Li <jing.li@zilliz.com>

---
+++ tests/scripts/get_author_email.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+function get_author_email(){
+    email=$(git --no-pager show -s --format=\'%ae\' HEAD )
+    if [[ "${email}" == "nobody@nowhere" ]]; then 
+        email=$(git --no-pager show -s --format=\'%ae\' HEAD^ )
+    fi 
+   echo ${email}
+}
+get_author_email
