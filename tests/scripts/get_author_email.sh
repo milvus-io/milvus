83d05e26d9cc667c9187042c39c647e3a9239425
Jenny Li <jing.li@zilliz.com>
2021-12-09 21:21:09 +0800

    [skip ci] fix get author email
    Signed-off-by: Jenny Li <jing.li@zilliz.com>

--- tests/scripts/get_author_email.sh
+++ tests/scripts/get_author_email.sh
@@ -3,7 +3,7 @@
 set -e
 function get_author_email(){
     email=$(git --no-pager show -s --format=\'%ae\' HEAD )
-    if [[ "${email}" == 'nobody@nowhere' ]]; then 
+    if [[ "${email}" == \'nobody@nowhere\' ]]; then 
         email=$(git --no-pager show -s --format=\'%ae\' HEAD^ )
     fi 
    echo ${email} | sed $'s/\'//g'
