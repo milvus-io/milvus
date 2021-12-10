80fb272f362e4829a0433574930854e1e447c097
Jenny Li <jing.li@zilliz.com>
2021-12-10 10:13:54 +0800

    [skip ci] Add comment for get_author_email.sh
    Signed-off-by: Jenny Li <jing.li@zilliz.com>

--- tests/scripts/get_author_email.sh
+++ tests/scripts/get_author_email.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+# author emaill will be jenkins's email, when the pr branch is not fast forward compared with master branch
 set -e
 function get_author_email(){
     email=$(git --no-pager show -s --format=\'%ae\' HEAD )
