f6c17ce4b60c3961091acfd0ee2f4e59d39c39e7
cai.zhang <cai.zhang@zilliz.com>
2021-12-14 16:45:34 +0800

    [skip ci]Add comment for LoadMetaFromETCD (#13343)
    Signed-off-by: Cai.Zhang <cai.zhang@zilliz.com>

--- internal/indexcoord/meta_table.go
+++ internal/indexcoord/meta_table.go
@@ -483,6 +483,8 @@ func (mt *metaTable) HasSameReq(req *indexpb.BuildIndexRequest) (bool, UniqueID)
 	return false, -1
 }
 
+// LoadMetaFromETCD load the meta of specified indexBuildID from ETCD.
+// If the version of meta in memory is greater equal to the version in ETCD, no need to reload.
 func (mt *metaTable) LoadMetaFromETCD(indexBuildID int64, revision int64) bool {
 	mt.lock.Lock()
 	defer mt.lock.Unlock()
