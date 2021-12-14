fa182c4b3ae47a724ed10a3b87fd065a45571aa4
wt <ting.wang@zilliz.com>
2021-12-14 16:32:22 +0800

    [skip ci] Update get_deploy_mode func of benchmark (#13327)
    Signed-off-by: wangting0128 <ting.wang@zilliz.com>

--- tests/benchmark/milvus_benchmark/utils.py
+++ tests/benchmark/milvus_benchmark/utils.py
@@ -128,7 +128,7 @@ def get_deploy_mode(deploy_params):
             deploy_mode = config.DEFUALT_DEPLOY_MODE
         elif "deploy_mode" in milvus_params:
             deploy_mode = milvus_params["deploy_mode"]
-            if deploy_mode not in [config.SINGLE_DEPLOY_MODE, config.CLUSTER_DEPLOY_MODE]:
+            if deploy_mode not in [config.SINGLE_DEPLOY_MODE, config.CLUSTER_DEPLOY_MODE, config.CLUSTER_3RD_DEPLOY_MODE]:
                 raise Exception("Invalid deploy mode: %s" % deploy_mode)
     return deploy_mode
 
