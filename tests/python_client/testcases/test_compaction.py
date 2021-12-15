a41db7a3e868aedeee6631dd04adebe87504c94f
ThreadDao <yufen.zong@zilliz.com>
2021-12-15 10:13:02 +0800

    Update compact case for dup id less search result
    Signed-off-by: ThreadDao <yufen.zong@zilliz.com>

--- tests/python_client/testcases/test_compaction.py
+++ tests/python_client/testcases/test_compaction.py
@@ -486,7 +486,7 @@ class TestCompactionOperation(TestcaseBase):
                 3.load and search
         expected: Verify search result
         """
-        collection_w = self.collection_insert_multi_segments_one_shard(prefix, nb_of_segment=ct.default_nb)
+        collection_w = self.collection_insert_multi_segments_one_shard(prefix, nb_of_segment=ct.default_nb, is_dup=False)
 
         # compact
         collection_w.compact()
