932b8f4b22aed1fa3e28c6c0d8166cb18134a6d7
jaime <cloud.poster@gmail.com>
2021-12-16 22:30:00 +0800

    [skip ci] Fix grammar mistake in segment (#13541)
    Signed-off-by: yun.zhang <yun.zhang@zilliz.com>
    
    Co-authored-by: yun.zhang <yun.zhang@zilliz.com>

--- internal/querynode/segment.go
+++ internal/querynode/segment.go
@@ -360,7 +360,7 @@ func (s *Segment) fillVectorFieldsData(collectionID UniqueID,
 			continue
 		}
 
-		// If the vector field doesn't have index. Vector data is in memory for
+		// If the vector field doesn't have indexed. Vector data is in memory for
 		// brute force search. No need to download data from remote.
 		if _, ok := s.indexInfos[fieldData.FieldId]; !ok {
 			continue
