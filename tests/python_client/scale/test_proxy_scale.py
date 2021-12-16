8b2caff7a1b4e150d652dea694a9257dc65d7f9d
ThreadDao <yufen.zong@zilliz.com>
2021-12-16 22:17:08 +0800

    [skip ci] Update scale proxy test and delete shrink proxy
    Signed-off-by: ThreadDao <yufen.zong@zilliz.com>

--- tests/python_client/scale/test_proxy_scale.py
+++ tests/python_client/scale/test_proxy_scale.py
@@ -1,28 +1,40 @@
 import multiprocessing
+
 import pytest
 from customize.milvus_operator import MilvusOperator
-from scale.helm_env import HelmEnv
 from common import common_func as cf
 from common.common_type import CaseLabel
 from scale import scale_common as sc, constants
 from utils.util_log import test_log as log
+from utils.util_k8s import wait_pods_ready
 
 prefix = "proxy_scale"
 
 
 class TestProxyScale:
 
+    def e2e_milvus_parallel(self, process_num, host, c_name):
+        process_list = []
+        for i in range(process_num):
+            p = multiprocessing.Process(target=sc.e2e_milvus, args=(host, c_name))
+            p.start()
+            process_list.append(p)
+
+        for p in process_list:
+            p.join()
+
     @pytest.mark.tags(CaseLabel.L3)
-    def test_expand_proxy(self):
+    def test_scale_proxy(self):
         """
         target: test milvus operation after proxy expand
-        method: 1.deploy two proxy pods
-                2.milvus e2e test
-                3.expand proxy pod from 1 to 2
+        method: 1.deploy 1 proxy replicas
+                2.milvus e2e test in parallel
+                3.expand proxy pod from 1 to 5
                 4.milvus e2e test
+                5.shrink proxy from 5 to 2
         expected: 1.verify data consistent and func work
         """
-        # deploy all nodes one pod cluster milvus with helm
+        # deploy milvus cluster with one proxy
         release_name = "scale-proxy"
         image = f'{constants.IMAGE_REPOSITORY}:{constants.IMAGE_TAG}'
         data_config = {
@@ -43,48 +55,21 @@ class TestProxyScale:
         # host = "10.98.0.7"
 
         c_name = cf.gen_unique_str(prefix)
-        process_list = []
-        for i in range(5):
-            p = multiprocessing.Process(target=sc.e2e_milvus, args=(host, c_name))
-            p.start()
-            process_list.append(p)
+        self.e2e_milvus_parallel(5, host, c_name)
+        log.info('Milvus test before expand')
 
-        for p in process_list:
-            p.join()
-
-        log.info('Milvus test before scale up')
-
+        # expand proxy replicas from 1 to 5
         mic.upgrade(release_name, {'spec.components.proxy.replicas': 5}, constants.NAMESPACE)
+        wait_pods_ready(constants.NAMESPACE, f"app.kubernetes.io/instance={release_name}")
 
-        for i in range(5):
-            p = multiprocessing.Process(target=sc.e2e_milvus, args=(host, c_name))
-            p.start()
-            process_list.append(p)
+        self.e2e_milvus_parallel(5, host, c_name)
+        log.info('Milvus test after expand')
 
-        for p in process_list:
-            p.join()
+        # expand proxy replicas from 5 to 2
+        mic.upgrade(release_name, {'spec.components.proxy.replicas': 2}, constants.NAMESPACE)
+        wait_pods_ready(constants.NAMESPACE, f"app.kubernetes.io/instance={release_name}")
 
-        log.info('Milvus test after scale up')
+        self.e2e_milvus_parallel(2, host, c_name)
+        log.info('Milvus test after shrink')
 
-    def test_shrink_proxy(self):
-        """
-        target: test shrink proxy pod from 2 to 1
-        method: 1.deploy two proxy node
-                2.e2e test
-                3.shrink proxy pods
-                4.e2e test
-        expected:
-        """
-        # deploy all nodes one pod cluster milvus with helm
-        release_name = "scale-proxy"
-        env = HelmEnv(release_name=release_name, proxy=2)
-        host = env.helm_install_cluster_milvus()
-
-        c_name = cf.gen_unique_str(prefix)
-        sc.e2e_milvus(host, c_name)
-
-        # scale proxy
-        env.helm_upgrade_cluster_milvus(proxy=1)
-
-        # c_name_2 = cf.gen_unique_str(prefix)
-        sc.e2e_milvus(host, c_name, collection_exist=True)
+        # mic.uninstall(release_name, namespace=constants.NAMESPACE)
