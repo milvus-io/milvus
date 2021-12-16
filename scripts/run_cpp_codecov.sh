27a0fab3b891e7b4ff3299f8a0c1a0afc98bf692
Jenny Li <jing.li@zilliz.com>
2021-12-16 09:15:27 +0800

    [skip ci] Add comment for run_cpp_codecov.sh (#13477)
    Signed-off-by: Jenny Li <jing.li@zilliz.com>

--- scripts/run_cpp_codecov.sh
+++ scripts/run_cpp_codecov.sh
@@ -16,6 +16,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Exit immediately for non zero status
 set -e
 
 SOURCE="${BASH_SOURCE[0]}"
