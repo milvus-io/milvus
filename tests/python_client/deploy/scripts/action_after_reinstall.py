f451befc66c2e58fbd258910bf7516c5ab236063
zhuwenxing <wenxing.zhu@zilliz.com>
2021-12-17 18:49:30 +0800

    [skip e2e]Add case for deploy test (#13655)
    Signed-off-by: zhuwenxing <wenxing.zhu@zilliz.com>

--- tests/python_client/deploy/scripts/action_after_reinstall.py
+++ tests/python_client/deploy/scripts/action_after_reinstall.py
@@ -5,8 +5,8 @@ from utils import *
 def task_1():
     """
     task_1:
-        before upgrade: create collection and insert data
-        after upgrade: get collection, load, search, create index, load, and search
+        before reinstall: create collection and insert data
+        after reinstall: get collection, load, search, create index, load, and search
     """
     prefix = "task_1_"
     connections.connect(host="127.0.0.1", port=19530, timeout=60)
@@ -18,8 +18,8 @@ def task_1():
 def task_2():
     """
     task_2:
-        before upgrade: create collection, insert data and create index
-        after upgrade: get collection, load, search, insert data, create index, load, and search
+        before reinstall: create collection, insert data and create index
+        after reinstall: get collection, load, search, insert data, create index, load, and search
     """
     prefix = "task_2_"
     connections.connect(host="127.0.0.1", port=19530, timeout=60)
--- tests/python_client/deploy/scripts/action_before_upgrade.py
+++ tests/python_client/deploy/scripts/action_before_upgrade.py
@@ -1,11 +1,35 @@
-# import docker
 from pymilvus import connections
 from utils import *
 
-connections.connect()
 
-get_collections()
+def task_1():
+    """
+    task_1:
+        before upgrade: create collection and insert data
+        after upgrade: get collection, load, search, create index, load, and search
+    """
+    prefix = "task_1_"
+    connections.connect(host="127.0.0.1", port=19530, timeout=60)
+    get_collections(prefix)
+    load_and_search(prefix)
+    create_collections_and_insert_data(prefix)
 
-load_and_search()
 
-create_collections_and_insert_data()
+def task_2():
+    """
+    task_2:
+        before upgrade: create collection, insert data and create index
+        after upgrade: get collection, load, search, insert data, create index, load, and search
+    """
+    prefix = "task_2_"
+    connections.connect(host="127.0.0.1", port=19530, timeout=60)
+    get_collections(prefix)
+    load_and_search(prefix)
+    create_collections_and_insert_data(prefix)
+    create_index(prefix)
+    load_and_search(prefix)
+
+
+if __name__ == '__main__':
+    task_1()
+    task_2()
