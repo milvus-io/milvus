43ef159d4eabef50448bf9b114bc1d50b20c456d
dragondriver <jiquan.long@zilliz.com>
2021-12-17 17:03:05 +0800

    [skip e2e] Add comment for validateMultipleVectorFields
    Signed-off-by: dragondriver <jiquan.long@zilliz.com>

--- internal/proxy/validate_util.go
+++ internal/proxy/validate_util.go
@@ -346,6 +346,7 @@ func validateSchema(coll *schemapb.CollectionSchema) error {
 	return nil
 }
 
+// validateMultipleVectorFields check if schema has multiple vector fields.
 func validateMultipleVectorFields(schema *schemapb.CollectionSchema) error {
 	vecExist := false
 	var vecName string
