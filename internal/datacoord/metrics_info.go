8bd768467c1aeb597274691238b94cfcda72c695
Xiangyu Wang <xiangyu.wang@zilliz.com>
2021-12-20 11:12:49 +0800

    Improve import statement
    Signed-off-by: Xiangyu Wang <xiangyu.wang@zilliz.com>

--- internal/datacoord/metrics_info.go
+++ internal/datacoord/metrics_info.go
@@ -20,17 +20,13 @@ import (
 	"context"
 	"errors"
 
-	"github.com/milvus-io/milvus/internal/util/uniquegenerator"
-
-	"github.com/milvus-io/milvus/internal/util/typeutil"
-
-	"go.uber.org/zap"
-
 	"github.com/milvus-io/milvus/internal/log"
-
 	"github.com/milvus-io/milvus/internal/proto/commonpb"
 	"github.com/milvus-io/milvus/internal/proto/milvuspb"
 	"github.com/milvus-io/milvus/internal/util/metricsinfo"
+	"github.com/milvus-io/milvus/internal/util/typeutil"
+	"github.com/milvus-io/milvus/internal/util/uniquegenerator"
+	"go.uber.org/zap"
 )
 
 // getSystemInfoMetrics composes data cluster metrics
