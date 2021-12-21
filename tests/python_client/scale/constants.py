312aa23c77fa7179898ed2f274c59c0f29fa1dc5
ThreadDao <yufen.zong@zilliz.com>
2021-12-21 16:05:08 +0800

    [skip e2e] Delete helm deploy in scale test (#13872)
    Signed-off-by: ThreadDao <yufen.zong@zilliz.com>

--- tests/python_client/scale/constants.py
+++ tests/python_client/scale/constants.py
@@ -4,13 +4,7 @@ IMAGE_TAG = "master-20211213-d14fff0"  # tag of milvus image
 NAMESPACE = "chaos-testing"  # namespace
 IF_NOT_PRESENT = "IfNotPresent"  # image pullPolicy IfNotPresent
 ALWAYS = "Always"  # image pullPolicy Always
-PROXY = "proxy"  # key proxy
-DATA_NODE = "dataNode"  # key dataNode
-INDEX_NODE = "indexNode"  # key indexNode
-QUERY_NODE = "queryNode"  # key queryNode
 
-# my values.yaml path
-MILVUS_CHART_ENV = 'MILVUS_CHART_ENV'  # env of milvus chart path
 MILVUS_LOGS_PATH = '/tmp/milvus'  # path of milvus pod logs
 
 # default scale config
