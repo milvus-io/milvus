756a09a44b7ad8349daee80bc400ff13d00e5d62
cxytz01 <xueyou.chen@zilliz.com>
2021-12-22 20:08:25 +0800

    [skip e2e] Grammar correction (#13929)
    Signed-off-by: cxytz01 <xueyou.chen@zilliz.com>

--- internal/core/src/indexbuilder/index_c.cpp
+++ internal/core/src/indexbuilder/index_c.cpp
@@ -255,7 +255,7 @@ GetIdsOfQueryResult(CIndexQueryResult res, int64_t* ids) {
     auto c_res = (milvus::indexbuilder::IndexWrapper::QueryResult*)res;
     auto nq = c_res->nq;
     auto k = c_res->topk;
-    // TODO: how could we avoid memory copy every time when this called
+    // TODO: how could we avoid memory copy whenever this called
     memcpy(ids, c_res->ids.data(), sizeof(int64_t) * nq * k);
 }
 
@@ -264,7 +264,7 @@ GetDistancesOfQueryResult(CIndexQueryResult res, float* distances) {
     auto c_res = (milvus::indexbuilder::IndexWrapper::QueryResult*)res;
     auto nq = c_res->nq;
     auto k = c_res->topk;
-    // TODO: how could we avoid memory copy every time when this called
+    // TODO: how could we avoid memory copy whenever this called
     memcpy(distances, c_res->distances.data(), sizeof(float) * nq * k);
 }
 
