5c003a023e0bef3bb3fe7709f4ab516a288ae258
edward.zeng <jie.zeng@zilliz.com>
2021-12-22 20:03:38 +0800

    [skip e2e] Refine indexcoord util comment (#13998)
    Signed-off-by: Edward Zeng <jie.zeng@zilliz.com>

--- internal/indexcoord/util.go
+++ internal/indexcoord/util.go
@@ -44,7 +44,7 @@ func getDimension(req *indexpb.BuildIndexRequest) (int64, error) {
 	return 0, errors.New(errMsg)
 }
 
-// estimateIndexSize estimate how much memory will be occupied by IndexNode when building an index.
+// estimateIndexSize estimates how much memory will be occupied by IndexNode when building an index.
 func estimateIndexSize(dim int64, numRows int64, dataType schemapb.DataType) (uint64, error) {
 	if dataType == schemapb.DataType_FloatVector {
 		return uint64(dim) * uint64(numRows) * 4, nil
