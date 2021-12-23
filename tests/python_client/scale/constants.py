396ccf7bd44d73c19381abd4889b06381608b46d
ThreadDao <yufen.zong@zilliz.com>
2021-12-23 14:36:16 +0800

    [skip e2e] Update milvus image harbor (#14047)
    Signed-off-by: ThreadDao <yufen.zong@zilliz.com>

--- tests/python_client/scale/constants.py
+++ tests/python_client/scale/constants.py
@@ -1,5 +1,5 @@
 # scale object
-IMAGE_REPOSITORY = "harbor.zilliz.cc/milvus/milvus"  # repository of milvus image
+IMAGE_REPOSITORY = "registry.milvus.io/milvus/milvus"  # repository of milvus image
 IMAGE_TAG = "master-20211213-d14fff0"  # tag of milvus image
 NAMESPACE = "chaos-testing"  # namespace
 IF_NOT_PRESENT = "IfNotPresent"  # image pullPolicy IfNotPresent
