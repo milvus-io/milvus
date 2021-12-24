d9bae7ba0dc4461730a658ad362b91b6cdda0ff0
godchen0212 <qingxiang.chen@zilliz.com>
2021-12-24 16:15:06 +0800

    [skip e2e]Fix comment typo
    Signed-off-by: godchen0212 <qingxiang.chen@zilliz.com>

--- internal/querynode/impl.go
+++ internal/querynode/impl.go
@@ -188,7 +188,7 @@ func (node *QueryNode) RemoveQueryChannel(ctx context.Context, in *queryPb.Remov
 	return status, nil
 }
 
-// WatchDmChannels create consumers on dmChannels to reveive Incremental data，which is the important part of real-time query
+// WatchDmChannels create consumers on dmChannels to receive Incremental data，which is the important part of real-time query
 func (node *QueryNode) WatchDmChannels(ctx context.Context, in *queryPb.WatchDmChannelsRequest) (*commonpb.Status, error) {
 	code := node.stateCode.Load().(internalpb.StateCode)
 	if code != internalpb.StateCode_Healthy {
