729885646a09895972da67a391eef7fdbc3d484b
ThreadDao <yufen.zong@zilliz.com>
2021-12-27 11:46:41 +0800

    [skip e2e] Remove unused scale constants
    Signed-off-by: ThreadDao <yufen.zong@zilliz.com>

--- tests/python_client/scale/constants.py
+++ tests/python_client/scale/constants.py
@@ -4,8 +4,3 @@ IMAGE_TAG = "master-20211213-d14fff0"  # tag of milvus image
 NAMESPACE = "chaos-testing"  # namespace
 IF_NOT_PRESENT = "IfNotPresent"  # image pullPolicy IfNotPresent
 ALWAYS = "Always"  # image pullPolicy Always
-
-MILVUS_LOGS_PATH = '/tmp/milvus'  # path of milvus pod logs
-
-# default scale config
-DEFAULT_RELEASE_PREFIX = "scale"
