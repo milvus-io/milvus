9ddbd5a1cc5e642b4c2b38040f0eb754edded82c
yudong.cai <yudong.cai@zilliz.com>
2021-12-27 10:56:22 +0800

    [skip e2e] Remove redundant header files for query/SubSearchResult.cpp
    Signed-off-by: yudong.cai <yudong.cai@zilliz.com>

--- internal/core/src/query/SubSearchResult.cpp
+++ internal/core/src/query/SubSearchResult.cpp
@@ -9,10 +9,10 @@
 // is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
 // or implied. See the License for the specific language governing permissions and limitations under the License
 
+#include <cmath>
+
 #include "exceptions/EasyAssert.h"
 #include "query/SubSearchResult.h"
-#include "segcore/Reduce.h"
-#include <cmath>
 
 namespace milvus::query {
 
