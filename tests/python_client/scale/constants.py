495e5ed2404b4eb322a06af505c99405584e44c1
ThreadDao <yufen.zong@zilliz.com>
2021-12-27 11:19:06 +0800

    [skip e2e] Update scale test image tag
    Signed-off-by: ThreadDao <yufen.zong@zilliz.com>

--- tests/python_client/scale/constants.py
+++ tests/python_client/scale/constants.py
@@ -1,6 +1,6 @@
 # scale object
 IMAGE_REPOSITORY = "registry.milvus.io/milvus/milvus"  # repository of milvus image
-IMAGE_TAG = "master-20211213-d14fff0"  # tag of milvus image
+IMAGE_TAG = "master-20211227-b022615"  # tag of milvus image
 NAMESPACE = "chaos-testing"  # namespace
 IF_NOT_PRESENT = "IfNotPresent"  # image pullPolicy IfNotPresent
 ALWAYS = "Always"  # image pullPolicy Always
