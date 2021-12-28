456dba4b3181da92d14d3887f6c133229b6fb0c8
Xieql <qianglong.xie@zilliz.com>
2021-12-28 13:24:51 +0800

    [skip e2e] Improve annotation (#14382)
    Signed-off-by: Xieql <qianglong.xie@zilliz.com>

--- internal/util/trace/util.go
+++ internal/util/trace/util.go
@@ -70,7 +70,7 @@ func initFromEnv(serviceName string) *config.Configuration {
 	return cfg
 }
 
-// StartSpanFromContext starts a opentracing span. The default operation name is
+// StartSpanFromContext starts an opentracing span. The default operation name is
 // upper two call stacks of the function
 func StartSpanFromContext(ctx context.Context, opts ...opentracing.StartSpanOption) (opentracing.Span, context.Context) {
 	return StartSpanFromContextWithSkip(ctx, 3, opts...)
