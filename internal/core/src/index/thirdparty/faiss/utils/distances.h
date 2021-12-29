722f95df025e8262ac4d1bfb0204162e820e3f05
Cai Yudong <yudong.cai@zilliz.com>
2021-12-29 11:43:24 +0800

    Abstract range search APIs into distances_range.cpp (#14485)
    Signed-off-by: yudong.cai <yudong.cai@zilliz.com>

--- internal/core/src/index/thirdparty/faiss/utils/distances.h
+++ internal/core/src/index/thirdparty/faiss/utils/distances.h
@@ -16,7 +16,6 @@
 
 #include <faiss/utils/Heap.h>
 #include <faiss/utils/BitsetView.h>
-#include <faiss/impl/AuxIndexStructures.h>
 
 
 namespace faiss {
@@ -213,42 +212,6 @@ void knn_L2sqr_by_idx (const float * x,
                        const int64_t * ids,
                        size_t d, size_t nx, size_t ny,
                        float_maxheap_array_t * res);
-
-/***************************************************************************
- * Range search
- ***************************************************************************/
-
-
-
-/// Forward declaration, see AuxIndexStructures.h
-struct RangeSearchResult;
-
-/** Return the k nearest neighors of each of the nx vectors x among the ny
- *  vector y, w.r.t to max inner product
- *
- * @param x      query vectors, size nx * d
- * @param y      database vectors, size ny * d
- * @param radius search radius around the x vectors
- * @param result result structure
- */
-void range_search_L2sqr (
-        const float * x,
-        const float * y,
-        size_t d, size_t nx, size_t ny,
-        float radius,
-        std::vector<RangeSearchPartialResult*> &result,
-        size_t buffer_size,
-        const BitsetView &bitset = nullptr);
-
-/// same as range_search_L2sqr for the inner product similarity
-void range_search_inner_product (
-    const float * x,
-    const float * y,
-    size_t d, size_t nx, size_t ny,
-    float radius,
-    std::vector<RangeSearchPartialResult*> &result,
-    size_t buffer_size,
-    const BitsetView &bitset = nullptr);
 
 
 /***************************************************************************
