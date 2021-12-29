3d593a5c30033e8be50e388aab8c8deecd41575b
godchen <qingxiang.chen@zilliz.com>
2021-12-29 20:36:03 +0800

    [skip e2e]Sort imports (#14315)
    Signed-off-by: godchen0212 <qingxiang.chen@zilliz.com>

--- internal/datacoord/meta.go
+++ internal/datacoord/meta.go
@@ -23,13 +23,14 @@ import (
 	"time"
 
 	"github.com/golang/protobuf/proto"
+	"go.uber.org/zap"
+
 	"github.com/milvus-io/milvus/internal/kv"
 	"github.com/milvus-io/milvus/internal/log"
 	"github.com/milvus-io/milvus/internal/proto/commonpb"
 	"github.com/milvus-io/milvus/internal/proto/datapb"
 	"github.com/milvus-io/milvus/internal/proto/internalpb"
 	"github.com/milvus-io/milvus/internal/proto/querypb"
-	"go.uber.org/zap"
 )
 
 const (
