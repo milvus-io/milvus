8a81f01f66096924e8c8a0f859cd626dbdf686ee
cxytz01 <xueyou.chen@zilliz.com>
2021-12-29 15:10:45 +0800

    [skip e2e] Grammar correction
    Signed-off-by: cxytz01 <xueyou.chen@zilliz.com>

--- internal/indexcoord/node_manager.go
+++ internal/indexcoord/node_manager.go
@@ -128,7 +128,7 @@ func (nm *NodeManager) PeekClient(meta Meta) (UniqueID, types.IndexNode) {
 	return nodeID, client
 }
 
-// ListNode list all IndexNodes in node manager.
+// ListNode lists all IndexNodes in node manager.
 func (nm *NodeManager) ListNode() []UniqueID {
 	//nm.lock.Lock()
 	//defer nm.lock.Unlock()
