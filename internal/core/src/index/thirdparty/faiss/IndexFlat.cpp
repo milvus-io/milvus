774abb7bfb48dded4cd9376c0323133fa927258e
yudong.cai <yudong.cai@zilliz.com>
2021-12-29 10:59:08 +0800

    Abstract range search APIs into distances_range.cpp
    Signed-off-by: yudong.cai <yudong.cai@zilliz.com>

--- internal/core/src/index/thirdparty/faiss/IndexFlat.cpp
+++ internal/core/src/index/thirdparty/faiss/IndexFlat.cpp
@@ -11,6 +11,7 @@
 
 #include <cstring>
 #include <faiss/utils/distances.h>
+#include <faiss/utils/distances_range.h>
 #include <faiss/utils/extra_distances.h>
 #include <faiss/utils/utils.h>
 #include <faiss/utils/Heap.h>
