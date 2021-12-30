62f002f9dfe44872905d2f11fed5f6b916e1d468
guoxiangzhou <xiangzhou.guo@zilliz.com>
2021-12-30 16:47:01 +0800

    [skip e2e] Modify log text according to the naming conventions (#14278)
    Signed-off-by: guoxiangzhou <xiangzhou.guo@zilliz.com>

--- internal/proxy/proxy_test.go
+++ internal/proxy/proxy_test.go
@@ -338,7 +338,7 @@ func TestProxy(t *testing.T) {
 	alias := "TestProxy"
 
 	rc := runRootCoord(ctx, localMsg)
-	log.Info("running root coordinator ...")
+	log.Info("running RootCoord ...")
 
 	if rc != nil {
 		defer func() {
