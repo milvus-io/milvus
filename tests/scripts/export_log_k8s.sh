2b2ded362b287358d5a1573ede23dd9a1dffc1a2
zhuwenxing <wenxing.zhu@zilliz.com>
2021-12-30 18:05:40 +0800

    [skip e2e]Add pod describe log for chaos test
    Signed-off-by: zhuwenxing <wenxing.zhu@zilliz.com>

--- tests/scripts/export_log_k8s.sh
+++ tests/scripts/export_log_k8s.sh
@@ -10,12 +10,14 @@ array=($(kubectl get pod -n ${ns_name}|grep ${prefix_name}|awk '{print $1}'))
 echo ${array[@]}
 if [ ! -d $log_dir  ];
 then
-    mkdir -p $log_dir
+    mkdir -p $log_dir/pod_log
+    mkdir -p $log_dir/pod_describe
 fi
 echo "export logs start"
 for pod in ${array[*]}
 do
 echo "export logs for pod $pod "
-kubectl logs $pod -n ${ns_name} > ./$log_dir/$pod.log 2>&1
+kubectl logs $pod -n ${ns_name} > ./$log_dir/pod_log/$pod.log 2>&1
+kubectl describe pod $pod -n ${ns_name} > ./$log_dir/pod_describe/$pod.log
 done
 echo "export logs done"
