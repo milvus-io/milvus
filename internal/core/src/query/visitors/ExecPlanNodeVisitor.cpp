66030aa84ebc7d66eb561f067acc722f83e1ce9e
yudong.cai <yudong.cai@zilliz.com>
2022-01-04 10:05:59 +0800

    [skip e2e] Optimize ExecPlanNodeVisitor.cpp
    Signed-off-by: yudong.cai <yudong.cai@zilliz.com>

--- internal/core/src/query/visitors/ExecPlanNodeVisitor.cpp
+++ internal/core/src/query/visitors/ExecPlanNodeVisitor.cpp
@@ -10,7 +10,7 @@
 // or implied. See the License for the specific language governing permissions and limitations under the License
 
 #include <utility>
-#include "boost_ext/dynamic_bitset_ext.hpp"
+#include <boost_ext/dynamic_bitset_ext.hpp>
 
 #include "query/PlanImpl.h"
 #include "query/generated/ExecPlanNodeVisitor.h"
