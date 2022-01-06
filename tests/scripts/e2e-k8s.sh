23e3375a273a9ecb32c5e4915c61cc99581ecf57
Jenny Li <jing.li@zilliz.com>
2022-01-06 13:28:41 +0800

    [skip e2e] Update copy right for e2e-k8s.sh (#14902)
    Signed-off-by: Jenny Li <jing.li@zilliz.com>

--- tests/scripts/e2e-k8s.sh
+++ tests/scripts/e2e-k8s.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright (C) 2019-2020 Zilliz. All rights reserved.
+# Copyright (C) 2019-2022 Zilliz. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance
 # with the License. You may obtain a copy of the License at
