6267bff06fe129901892cd5217a98921d46c6585
jingkl <jingjing.jia@zilliz.com>
2022-01-06 14:41:40 +0800

    [skip e2e]Add comments to conf adapter file
    Signed-off-by: jingkl <jingjing.jia@zilliz.com>

--- internal/util/indexparamcheck/conf_adapter.go
+++ internal/util/indexparamcheck/conf_adapter.go
@@ -368,6 +368,7 @@ func newANNOYConfAdapter() *ANNOYConfAdapter {
 	return &ANNOYConfAdapter{}
 }
 
+// RHNSWFlatConfAdapter checks if a rhnsw flat index can be built.
 type RHNSWFlatConfAdapter struct {
 	BaseConfAdapter
 }
