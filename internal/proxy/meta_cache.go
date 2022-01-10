74287fd447943dfa013a6e21bbc62b74ae100674
dragondriver <jiquan.long@zilliz.com>
2022-01-10 19:51:46 +0800

    [skip e2e] Add comment for GetCollectionSchema
    Signed-off-by: dragondriver <jiquan.long@zilliz.com>

--- internal/proxy/meta_cache.go
+++ internal/proxy/meta_cache.go
@@ -47,6 +47,7 @@ type Cache interface {
 	GetPartitions(ctx context.Context, collectionName string) (map[string]typeutil.UniqueID, error)
 	// GetPartitionInfo get partition's info.
 	GetPartitionInfo(ctx context.Context, collectionName string, partitionName string) (*partitionInfo, error)
+	// GetCollectionSchema get collection's schema.
 	GetCollectionSchema(ctx context.Context, collectionName string) (*schemapb.CollectionSchema, error)
 	RemoveCollection(ctx context.Context, collectionName string)
 	RemovePartition(ctx context.Context, collectionName string, partitionName string)
