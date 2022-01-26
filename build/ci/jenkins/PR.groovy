2f76267b664512ba4c3220f99a7cb4c524d4ca5a
Jenny Li <jing.li@zilliz.com>
2022-01-26 11:33:01 +0800

    Update Monitor& update storageclass to local-path
    Signed-off-by: Jenny Li <jing.li@zilliz.com>

--- build/ci/jenkins/PR.groovy
+++ build/ci/jenkins/PR.groovy
@@ -4,7 +4,7 @@ int total_timeout_minutes = 120
 int e2e_timeout_seconds = 70 * 60
 def imageTag=''
 int case_timeout_seconds = 10 * 60
-def chart_version='2.4.25'
+def chart_version='3.0.0'
 pipeline {
     options {
         timestamps()
