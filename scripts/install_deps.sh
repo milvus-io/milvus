178fd5201ead57fe64fd1e8dc2f1b42c5f257e4a
Jenny Li <jing.li@zilliz.com>
2022-02-21 18:45:34 +0800

    [skip e2e]Add ccache for mac install_deps.sh
    Signed-off-by: Jenny Li <jing.li@zilliz.com>

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -56,11 +56,11 @@ function install_linux_deps() {
 
 function install_mac_deps() {
   sudo xcode-select --install  > /dev/null 2>&1
-  brew install boost libomp ninja tbb cmake llvm
+  brew install boost libomp ninja tbb cmake llvm ccache
   brew uninstall grep
   brew install grep
   export PATH="/usr/local/opt/grep/libexec/gnubin:$PATH"
-  brew update && brew upgrade && brew cleanup
+  brew update && brew upgrade boost libomp ninja tbb cmake llvm ccache && brew cleanup
 
   if [[ $(arch) == 'arm64' ]]; then
     sudo mkdir /usr/local/include
