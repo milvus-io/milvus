022ce60d6af3233e333243202b69b783e11a77e2
bigsheeper <yihao.dai@zilliz.com>
2022-03-08 11:54:04 +0800

    Use Microseconds in performance tests
    Signed-off-by: bigsheeper <yihao.dai@zilliz.com>

--- internal/proxy/impl.go
+++ internal/proxy/impl.go
@@ -2536,11 +2536,15 @@ func (node *Proxy) Search(ctx context.Context, request *milvuspb.SearchRequest) 
 		strconv.FormatInt(qt.CollectionID, 10), metrics.SearchLabel, metrics.SuccessLabel).Inc()
 	metrics.ProxySearchVectors.WithLabelValues(strconv.FormatInt(Params.ProxyCfg.ProxyID, 10),
 		strconv.FormatInt(qt.CollectionID, 10), metrics.SearchLabel).Set(float64(qt.result.Results.NumQueries))
-	searchDur := tr.ElapseSpan().Milliseconds()
+	searchDur := tr.ElapseSpan()
 	metrics.ProxySearchLatency.WithLabelValues(strconv.FormatInt(Params.ProxyCfg.ProxyID, 10),
-		strconv.FormatInt(qt.CollectionID, 10), metrics.SearchLabel).Observe(float64(searchDur))
+		strconv.FormatInt(qt.CollectionID, 10), metrics.SearchLabel).Observe(float64(searchDur.Milliseconds()))
 	metrics.ProxySearchLatencyPerNQ.WithLabelValues(strconv.FormatInt(Params.ProxyCfg.ProxyID, 10),
-		strconv.FormatInt(qt.CollectionID, 10)).Observe(float64(searchDur) / float64(qt.result.Results.NumQueries))
+		strconv.FormatInt(qt.CollectionID, 10)).Observe(float64(searchDur.Milliseconds()) / float64(qt.result.Results.NumQueries))
+
+	log.Debug(log.BenchmarkRoot, zap.String(log.BenchmarkRole, typeutil.ProxyRole), zap.String(log.BenchmarkStep, "server search"),
+		zap.Int64(log.BenchmarkCollectionID, qt.CollectionID),
+		zap.Int64(log.BenchmarkMsgID, qt.ID()), zap.Int64(log.BenchmarkDuration, searchDur.Microseconds()))
 	return qt.result, nil
 }
 
