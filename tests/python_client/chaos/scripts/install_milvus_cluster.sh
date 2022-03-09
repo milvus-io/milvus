aaad381ee49d0a8fe5dac235c3e2d17ec9ddabfb
zhuwenxing <wenxing.zhu@zilliz.com>
2022-03-09 16:50:09 +0800

    [skip e2e]Fix install and test script for chaos
    Signed-off-by: zhuwenxing <wenxing.zhu@zilliz.com>

--- tests/python_client/chaos/scripts/install_milvus_cluster.sh
+++ tests/python_client/chaos/scripts/install_milvus_cluster.sh
@@ -9,8 +9,8 @@ bash uninstall_milvus.sh ${release} ${ns}|| true
 
 declare -A pod_map=(["querynode"]="queryNode" ["indexnode"]="indexNode" ["datanode"]="dataNode" ["proxy"]="proxy")
 echo "insatll cluster"
-helm install --wait --timeout 360s ${RELEASE_NAME:-$release} milvus/milvus \
-                    --set image.all.repository=${REPOSITORY:-"milvusdb/milvus-dev"} \
-                    --set image.all.tag=${IMAGE_TAG:-"master-latest"} \
-                    --set ${pod_map[${pod}]}.replicas=$node_num \
-                    -f ../cluster-values.yaml -n=${ns}
+helm install --wait --debug --timeout 360s ${RELEASE_NAME:-$release} milvus/milvus \
+                            --set image.all.repository=${REPOSITORY:-"milvusdb/milvus-dev"} \
+                            --set image.all.tag=${IMAGE_TAG:-"master-latest"} \
+                            --set ${pod_map[${pod}]}.replicas=$node_num \
+                            -f ../cluster-values.yaml -n=${ns}
