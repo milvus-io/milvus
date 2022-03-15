c51c8c0a74e1aaa9cfd581f925aef388a51e41b2
Xiaofan <83447078+xiaofan-luan@users.noreply.github.com>
2022-03-15 18:51:21 +0800

    Fix get flused segment errror handling (#15155)
    Signed-off-by: xiaofan-luan <xiaofan.luan@zilliz.com>

--- internal/rootcoord/task.go
+++ internal/rootcoord/task.go
@@ -756,20 +756,21 @@ func (t *DescribeSegmentReqTask) Execute(ctx context.Context) error {
 	if err != nil {
 		return err
 	}
-	exist := false
+
 	segIDs, err := t.core.CallGetFlushedSegmentsService(ctx, t.Req.CollectionID, -1)
 	if err != nil {
 		log.Debug("Get flushed segment from data coord failed", zap.String("collection_name", coll.Schema.Name), zap.Error(err))
-		exist = true
-	} else {
-		for _, id := range segIDs {
-			if id == t.Req.SegmentID {
-				exist = true
-				break
-			}
-		}
+		return err
 	}
 
+	// check if segment id exists
+	exist := false
+	for _, id := range segIDs {
+		if id == t.Req.SegmentID {
+			exist = true
+			break
+		}
+	}
 	if !exist {
 		return fmt.Errorf("segment id %d not belong to collection id %d", t.Req.SegmentID, t.Req.CollectionID)
 	}
