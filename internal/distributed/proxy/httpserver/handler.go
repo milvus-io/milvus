97e5d7795384c5f7851a93e106926abf47668a6a
shaoyue <shaoyue.chen@zilliz.com>
2022-03-21 11:21:23 +0800

    implements restful entities api (#15916)
    Signed-off-by: shaoyue.chen <shaoyue.chen@zilliz.com>

--- internal/distributed/proxy/httpserver/handler.go
+++ internal/distributed/proxy/httpserver/handler.go
@@ -24,6 +24,14 @@ func NewHandlers(proxy types.ProxyComponent) *Handlers {
 func (h *Handlers) RegisterRoutesTo(router gin.IRouter) {
 	router.GET("/health", wrapHandler(h.handleGetHealth))
 	router.POST("/dummy", wrapHandler(h.handlePostDummy))
+
+	router.POST("/entities", wrapHandler(h.handleInsert))
+	router.DELETE("/entities", wrapHandler(h.handleDelete))
+	router.POST("/search", wrapHandler(h.handleSearch))
+	router.POST("/query", wrapHandler(h.handleQuery))
+
+	router.POST("/persist", wrapHandler(h.handleFlush))
+	router.GET("/distance", wrapHandler(h.handleCalcDistance))
 }
 
 func (h *Handlers) handleGetHealth(c *gin.Context) (interface{}, error) {
@@ -33,9 +41,63 @@ func (h *Handlers) handleGetHealth(c *gin.Context) (interface{}, error) {
 func (h *Handlers) handlePostDummy(c *gin.Context) (interface{}, error) {
 	req := milvuspb.DummyRequest{}
 	// use ShouldBind to supports binding JSON, XML, YAML, and protobuf.
-	err := c.ShouldBind(&req)
+	err := shouldBind(c, &req)
 	if err != nil {
-		return nil, fmt.Errorf("%w: parse json failed: %v", errBadRequest, err)
+		return nil, fmt.Errorf("%w: parse body failed: %v", errBadRequest, err)
 	}
 	return h.proxy.Dummy(c, &req)
+}
+
+func (h *Handlers) handleInsert(c *gin.Context) (interface{}, error) {
+	req := milvuspb.InsertRequest{}
+	err := shouldBind(c, &req)
+	if err != nil {
+		return nil, fmt.Errorf("%w: parse body failed: %v", errBadRequest, err)
+	}
+	return h.proxy.Insert(c, &req)
+}
+
+func (h *Handlers) handleDelete(c *gin.Context) (interface{}, error) {
+	req := milvuspb.DeleteRequest{}
+	err := shouldBind(c, &req)
+	if err != nil {
+		return nil, fmt.Errorf("%w: parse body failed: %v", errBadRequest, err)
+	}
+	return h.proxy.Delete(c, &req)
+}
+
+func (h *Handlers) handleSearch(c *gin.Context) (interface{}, error) {
+	req := milvuspb.SearchRequest{}
+	err := shouldBind(c, &req)
+	if err != nil {
+		return nil, fmt.Errorf("%w: parse body failed: %v", errBadRequest, err)
+	}
+	return h.proxy.Search(c, &req)
+}
+
+func (h *Handlers) handleQuery(c *gin.Context) (interface{}, error) {
+	req := milvuspb.QueryRequest{}
+	err := shouldBind(c, &req)
+	if err != nil {
+		return nil, fmt.Errorf("%w: parse body failed: %v", errBadRequest, err)
+	}
+	return h.proxy.Query(c, &req)
+}
+
+func (h *Handlers) handleFlush(c *gin.Context) (interface{}, error) {
+	req := milvuspb.FlushRequest{}
+	err := shouldBind(c, &req)
+	if err != nil {
+		return nil, fmt.Errorf("%w: parse body failed: %v", errBadRequest, err)
+	}
+	return h.proxy.Flush(c, &req)
+}
+
+func (h *Handlers) handleCalcDistance(c *gin.Context) (interface{}, error) {
+	req := milvuspb.CalcDistanceRequest{}
+	err := shouldBind(c, &req)
+	if err != nil {
+		return nil, fmt.Errorf("%w: parse body failed: %v", errBadRequest, err)
+	}
+	return h.proxy.CalcDistance(c, &req)
 }
