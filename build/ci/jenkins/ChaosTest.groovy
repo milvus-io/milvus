63586472ba6760e3de64bb41f579c2b348b01283
zhuwenxing <wenxing.zhu@zilliz.com>
2022-03-29 18:23:30 +0800

    [skip e2e]Add deploy test jenkins file (#16261)
    Signed-off-by: zhuwenxing <wenxing.zhu@zilliz.com>
    Signed-off-by: kejiang <ke.jiang@zilliz.com>

--- build/ci/jenkins/ChaosTest.groovy
+++ build/ci/jenkins/ChaosTest.groovy
@@ -252,6 +252,8 @@ pipeline {
             container('main') {
                 dir ('tests/python_client/chaos') {
                     script {
+                        echo "get pod status"
+                        sh "kubectl get pods|grep ${env.RELEASE_NAME} || true"
                         echo "collecte logs"
                         sh "bash ../../scripts/export_log_k8s.sh ${env.NAMESPACE} ${env.RELEASE_NAME} k8s_log/${env.RELEASE_NAME} || true"                        
                         sh "tar -zcvf artifacts-${env.RELEASE_NAME}-pytest-logs.tar.gz /tmp/ci_logs/ --remove-files || true"
