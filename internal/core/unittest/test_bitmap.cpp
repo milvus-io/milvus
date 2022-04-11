cbf8aacafbfa06beee0f31b7f55edf2ff541cdde
yudong.cai <yudong.cai@zilliz.com>
2022-04-11 18:41:38 +0800

    Remove dependency on knowhere::scalar
    Signed-off-by: yudong.cai <yudong.cai@zilliz.com>

--- internal/core/unittest/test_bitmap.cpp
+++ internal/core/unittest/test_bitmap.cpp
@@ -11,7 +11,7 @@
 
 #include <gtest/gtest.h>
 #include "test_utils/DataGen.h"
-#include "knowhere/index/structured_index_simple/StructuredIndexSort.h"
+#include "index/ScalarIndexSort.h"
 
 TEST(Bitmap, Naive) {
     using namespace milvus;
@@ -22,10 +22,10 @@ TEST(Bitmap, Naive) {
     int N = 10000;
     auto raw_data = DataGen(schema, N);
     auto vec = raw_data.get_col<float>(0);
-    auto sort_index = std::make_shared<knowhere::scalar::StructuredIndexSort<float>>();
+    auto sort_index = std::make_shared<scalar::ScalarIndexSort<float>>();
     sort_index->Build(N, vec.data());
     {
-        auto res = sort_index->Range(0, knowhere::scalar::OperatorType::LT);
+        auto res = sort_index->Range(0, scalar::OperatorType::LT);
         double count = res->count();
         ASSERT_NEAR(count / N, 0.5, 0.01);
     }
