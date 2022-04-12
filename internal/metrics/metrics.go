d5390d950f7f7ae763a3ce5a799c9f046d842a64
Weida Zhu <weida.zhu@zilliz.com>
2022-04-12 18:44:22 +0800

    Add env variable for metrics port
    Signed-off-by: Weida Zhu <weida.zhu@zilliz.com>

--- internal/metrics/metrics.go
+++ internal/metrics/metrics.go
@@ -17,7 +17,11 @@
 package metrics
 
 import (
+	"fmt"
 	"net/http"
+	"os"
+	"strconv"
+
 	// nolint:gosec
 	_ "net/http/pprof"
 
@@ -28,6 +32,9 @@ import (
 )
 
 const (
+	DefaultListenPort = "9091"
+	ListenPortEnvKey  = "METRICS_PORT"
+
 	milvusNamespace = "milvus"
 
 	AbandonLabel = "abandon"
@@ -78,8 +85,20 @@ func ServeHTTP(r *prometheus.Registry) {
 	http.Handle("/metrics", promhttp.HandlerFor(r, promhttp.HandlerOpts{}))
 	http.Handle("/metrics_default", promhttp.Handler())
 	go func() {
-		if err := http.ListenAndServe(":9091", nil); err != nil {
+		bindAddr := getMetricsAddr()
+		log.Debug("metrics listen", zap.Any("addr", bindAddr))
+		if err := http.ListenAndServe(bindAddr, nil); err != nil {
 			log.Error("handle metrics failed", zap.Error(err))
 		}
 	}()
+}
+
+func getMetricsAddr() string {
+	port := os.Getenv(ListenPortEnvKey)
+	_, err := strconv.Atoi(port)
+	if err != nil {
+		return fmt.Sprintf(":%s", DefaultListenPort)
+	}
+
+	return fmt.Sprintf(":%s", port)
 }
