a88e4a90ae900816e85002c9e247a420ea00ecea
zhuwenxing <wenxing.zhu@zilliz.com>
2022-04-24 18:11:45 +0800

    [skip e2e]Fix deployment step in deploy test (#16582)
    Signed-off-by: zhuwenxing <wenxing.zhu@zilliz.com>

--- tests/python_client/deploy/utils.sh
+++ tests/python_client/deploy/utils.sh
@@ -1,5 +1,33 @@
 #!/bin/bash
 
+function replace_image_tag {
+    image_repo=$1
+    image_tag=$2
+    image_repo=${image_repo//\//\\\/}
+    platform='unknown'
+    unamestr=$(uname)
+    if [[ "$unamestr" == 'Linux' ]]; then
+    platform='Linux'
+    elif [[ "$unamestr" == 'Darwin' ]]; then
+    platform='Mac'
+    fi
+    echo "before replace: "
+    cat docker-compose.yml | grep milvusdb
+    if [[ "$platform" == "Mac" ]];
+    then
+        # for mac os
+        echo "replace image tag for mac start"
+        sed -i "" "s/milvusdb.*/${image_repo}\:${image_tag}/g" docker-compose.yml   
+        echo "replace image tag for mac done"
+    else
+        #for linux os 
+        sed -i "s/milvusdb.*/${image_repo}\:${image_tag}/g" docker-compose.yml
+    fi
+    echo "after replace: "
+    cat docker-compose.yml | grep milvusdb
+
+}
+
 #to check containers all running and minio is healthy
 function check_healthy {
     Expect=$(grep "container_name" docker-compose.yml | wc -l)
@@ -25,6 +53,4 @@ function check_healthy {
     echo "running num $cnt expect num $Expect"
     echo "healthy num $healthy expect num $Expect_health"
     done
-}
-
-check_healthy
+} 
