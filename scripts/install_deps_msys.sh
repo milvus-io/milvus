abdbc659883810889feb4df59a8a2e1e1f89a403
Ji Bin <matrixji@live.com>
2022-05-06 16:27:50 +0800

    [skip e2e] Force go 1.17 for Code Checker MinGW (#16797)
    - Also lock rocksdb to 6.26
    
    Signed-off-by: Ji Bin <matrixji@live.com>

--- scripts/install_deps_msys.sh
+++ scripts/install_deps_msys.sh
@@ -7,13 +7,12 @@ if [[ "${MSYSTEM}" != "MINGW64" ]] ; then
     exit 1
 fi
 
-pacman -Su --noconfirm --needed \
+pacman -S --noconfirm --needed \
     git make tar dos2unix zip unzip patch \
     mingw-w64-x86_64-toolchain \
     mingw-w64-x86_64-make \
     mingw-w64-x86_64-ccache \
     mingw-w64-x86_64-cmake \
-    mingw-w64-x86_64-go \
     mingw-w64-x86_64-boost \
     mingw-w64-x86_64-intel-tbb \
     mingw-w64-x86_64-openblas \
@@ -21,8 +20,12 @@ pacman -Su --noconfirm --needed \
     mingw-w64-x86_64-clang-tools-extra \
     mingw-w64-x86_64-python2 \
     mingw-w64-x86_64-diffutils \
-    mingw-w64-x86_64-arrow \
-    mingw-w64-x86_64-rocksdb
+    mingw-w64-x86_64-arrow
+
+# workaround for install older packages
+pacman -U --noconfirm \
+    https://repo.msys2.org/mingw/mingw64/mingw-w64-x86_64-go-1.17-1-any.pkg.tar.zst \
+    https://repo.msys2.org/mingw/mingw64/mingw-w64-x86_64-rocksdb-6.26.1-1-any.pkg.tar.zst
 
 
 # dummy empty dl, TODO: remove later
