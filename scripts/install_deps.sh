5b3f81bde336b30b2f1092272ff6e5a33367f1e9
longjiquan <jiquan.long@zilliz.com>
2022-05-24 15:28:34 +0800

    Fix missing dependencies in install_deps.sh
    Signed-off-by: longjiquan <jiquan.long@zilliz.com>

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -19,13 +19,13 @@
 function install_linux_deps() {
   if [[ -x "$(command -v apt)" ]]; then
       # for Ubuntu 18.04
-      sudo apt install -y g++ gcc make ccache libssl-dev zlib1g-dev libboost-regex-dev \
+      sudo apt install -y g++ gcc make lcov libtool m4 autoconf automake ccache libssl-dev zlib1g-dev libboost-regex-dev \
           libboost-program-options-dev libboost-system-dev libboost-filesystem-dev \
           libboost-serialization-dev python3-dev libboost-python-dev libcurl4-openssl-dev gfortran libtbb-dev
   elif [[ -x "$(command -v yum)" ]]; then
       # for CentOS 7
       sudo yum install -y epel-release centos-release-scl-rh && \
-      sudo yum install -y git make automake ccache openssl-devel zlib-devel \
+      sudo yum install -y git make lcov libtool m4 autoconf automake ccache openssl-devel zlib-devel \
           libcurl-devel python3-devel \
           devtoolset-7-gcc devtoolset-7-gcc-c++ devtoolset-7-gcc-gfortran \
           llvm-toolset-7.0-clang llvm-toolset-7.0-clang-tools-extra
