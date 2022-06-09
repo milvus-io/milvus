b0123bb79e4bd27273a93e872fb2e0f58cb6d280
zhuwenxing <wenxing.zhu@zilliz.com>
2022-06-09 15:26:47 +0800

    [skip e2e]Increase installation wait time
    Signed-off-by: zhuwenxing <wenxing.zhu@zilliz.com>

--- tests/python_client/chaos/scripts/install_milvus_cluster.sh
+++ tests/python_client/chaos/scripts/install_milvus_cluster.sh
@@ -9,7 +9,7 @@ bash uninstall_milvus.sh ${release} ${ns}|| true
 
 declare -A pod_map=(["querynode"]="queryNode" ["indexnode"]="indexNode" ["datanode"]="dataNode" ["proxy"]="proxy")
 echo "insatll cluster"
-helm install --wait --debug --timeout 360s ${RELEASE_NAME:-$release} milvus/milvus \
+helm install --wait --debug --timeout 600s ${RELEASE_NAME:-$release} milvus/milvus \
                             --set image.all.repository=${REPOSITORY:-"milvusdb/milvus-dev"} \
                             --set image.all.tag=${IMAGE_TAG:-"master-latest"} \
                             --set ${pod_map[${pod}]}.replicas=$node_num \
--- tests/python_client/chaos/scripts/install_milvus_standalone.sh
+++ tests/python_client/chaos/scripts/install_milvus_standalone.sh
@@ -5,7 +5,7 @@ release=${1:-"milvs-chaos"}
 ns=${2:-"chaos-testing"}
 bash uninstall_milvus.sh ${release} ${ns}|| true
 echo "insatll standalone"
-helm install --wait --debug --timeout 360s ${RELEASE_NAME:-$release} milvus/milvus \
+helm install --wait --debug --timeout 600s ${RELEASE_NAME:-$release} milvus/milvus \
                             --set image.all.repository=${REPOSITORY:-"milvusdb/milvus-dev"} \
                             --set image.all.tag=${IMAGE_TAG:-"master-latest"} \
                             -f ../standalone-values.yaml -n=${ns}
