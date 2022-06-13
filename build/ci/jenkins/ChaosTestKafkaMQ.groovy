eb4e39099cab17ce07d186b2e55b192cedb12689
zhuwenxing <wenxing.zhu@zilliz.com>
2022-06-13 11:40:09 +0800

    [skip e2e]Update pod template and etcd version (#17507)
    Signed-off-by: zhuwenxing <wenxing.zhu@zilliz.com>

--- build/ci/jenkins/ChaosTestKafkaMQ.groovy
+++ build/ci/jenkins/ChaosTestKafkaMQ.groovy
@@ -49,7 +49,7 @@ pipeline {
         string(
             description: 'Etcd Image Tag',
             name: 'etcd_image_tag',
-            defaultValue: "3.5.0-debian-10-r115"
+            defaultValue: "3.5.0-r1"
         )
         string(
             description: 'Query Replic Nums',
