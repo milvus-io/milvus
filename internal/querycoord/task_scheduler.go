725d5ac39489dbd95063087c3ed32e6e98629a74
Congqi Xia <congqi.xia@zilliz.com>
2022-06-14 12:10:42 +0800

    Add BindContext function for querycoord task scheduler
    Signed-off-by: Congqi Xia <congqi.xia@zilliz.com>

--- internal/querycoord/task_scheduler.go
+++ internal/querycoord/task_scheduler.go
@@ -521,6 +521,10 @@ func (scheduler *TaskScheduler) processTask(t task) error {
 		t.postExecute(ctx)
 	}()
 
+	// bind schedular context with task context
+	// cancel cannot be deferred here, since child task may rely on the parent context
+	ctx, _ = scheduler.BindContext(ctx)
+
 	// task preExecute
 	span.LogFields(oplog.Int64("processTask: scheduler process PreExecute", t.getTaskID()))
 	err = t.preExecute(ctx)
@@ -710,6 +714,8 @@ func (scheduler *TaskScheduler) scheduleLoop() {
 					triggerTask.notify(nil)
 				}
 			}
+			// calling context cancel so that bind context goroutine may quit
+			triggerTask.finishContext()
 		}
 	}
 }
@@ -885,6 +891,26 @@ func (scheduler *TaskScheduler) Close() {
 		scheduler.cancel()
 	}
 	scheduler.wg.Wait()
+}
+
+// BindContext binds input context with shceduler context.
+// the result context will be canceled when either context is done.
+func (scheduler *TaskScheduler) BindContext(ctx context.Context) (context.Context, context.CancelFunc) {
+	// use input context as parent
+	nCtx, cancel := context.WithCancel(ctx)
+	go func() {
+		select {
+		case <-scheduler.ctx.Done():
+			// scheduler done
+			cancel()
+		case <-ctx.Done():
+			// input ctx done
+			cancel()
+		case <-nCtx.Done():
+			// result context done, cancel called to make goroutine quit
+		}
+	}()
+	return nCtx, cancel
 }
 
 func updateSegmentInfoFromTask(ctx context.Context, triggerTask task, meta Meta) error {
