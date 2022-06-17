d69a41f63d1c57ead1e715fcde3f6058c36a90d0
Jenny Li <jing.li@zilliz.com>
2022-06-17 20:22:12 +0800

    [skip e2e] Upgrade chart to add kafka metrics for nightly (#17622)
    Signed-off-by: Jenny Li <jing.li@zilliz.com>

--- build/ci/jenkins/Nightly.groovy
+++ build/ci/jenkins/Nightly.groovy
@@ -8,7 +8,7 @@ String cron_string = BRANCH_NAME == "master" ? "50 22,2 * * * " : ""
 // Make timeout 4 hours so that we can run two nightly during the ci
 int total_timeout_minutes = 4 * 60
 def imageTag=''
-def chart_version='3.0.20'
+def chart_version='3.0.24'
 pipeline {
     triggers {
         cron """${cron_timezone}
