5ac575c776bf859ab7e218de675ab6d81069051c
SimFG <bang.fu@zilliz.com>
2022-06-23 20:49:44 +0800

    Remove the `WithOptions` for avoiding the log object being cloned continuously
    Signed-off-by: SimFG <bang.fu@zilliz.com>

---
+++ internal/util/logutil/logutil_test.go
@@ -0,0 +1,32 @@
+package logutil
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"go.uber.org/zap/zapcore"
+
+	"github.com/milvus-io/milvus/internal/log"
+	"go.uber.org/zap"
+)
+
+func TestName(t *testing.T) {
+	conf := &log.Config{Level: "debug", DisableTimestamp: true}
+	logger, _, _ := log.InitTestLogger(t, conf, zap.AddCallerSkip(1), zap.Hooks(func(entry zapcore.Entry) error {
+		assert.Equal(t, "Testing", entry.Message)
+		return nil
+	}))
+	wrapper := &zapWrapper{logger, 0}
+
+	wrapper.Info("Testing")
+	wrapper.Infoln("Testing")
+	wrapper.Infof("%s", "Testing")
+	wrapper.Warning("Testing")
+	wrapper.Warningln("Testing")
+	wrapper.Warningf("%s", "Testing")
+	wrapper.Error("Testing")
+	wrapper.Errorln("Testing")
+	wrapper.Errorf("%s", "Testing")
+
+}
