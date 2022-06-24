098d8ee471d1bf9a14d35e83ab70b74fe9e0984b
ThreadDao <yufen.zong@zilliz.com>
2022-06-24 17:16:32 +0800

    Upgrade set-tools to fix scale python install (#17764)
    Signed-off-by: ThreadDao <yufen.zong@zilliz.com>

--- build/ci/jenkins/Scale.groovy
+++ build/ci/jenkins/Scale.groovy
@@ -40,6 +40,7 @@ pipeline {
                 container('milvus-test') {
                     dir ('tests/python_client'){
                         sh """
+                        pip install --upgrade setuptools
                         pip install --upgrade pip
                         pip install -r requirements.txt
                         pip install --upgrade protobuf
