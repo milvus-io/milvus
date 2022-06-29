63b4c8621b7c70949ba627d23048cb36bcb208d5
zhuwenxing <wenxing.zhu@zilliz.com>
2022-06-29 12:34:17 +0800

    [skip e2e]Update etcd version for test (#17854)
    Signed-off-by: zhuwenxing <wenxing.zhu@zilliz.com>

--- build/ci/jenkins/ChaosTestKafkaMQ.groovy
+++ build/ci/jenkins/ChaosTestKafkaMQ.groovy
@@ -49,7 +49,7 @@ pipeline {
         string(
             description: 'Etcd Image Tag',
             name: 'etcd_image_tag',
-            defaultValue: "3.5.0-r4"
+            defaultValue: "3.5.0-r5"
         )
         string(
             description: 'QueryNode Nums',
