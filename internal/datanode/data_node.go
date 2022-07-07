e902dd97c8c633bb064cb0cd94f84751501f255e
Ten Thousand Leaves <69466447+soothing-rain@users.noreply.github.com>
2022-07-07 09:28:21 +0800

    Fix flush failure caused by empty stale segments (#18118)
    /bug
    
    issue: #18096
    Signed-off-by: Yuchen Gao <yuchen.gao@zilliz.com>

--- internal/datanode/data_node.go
+++ internal/datanode/data_node.go
@@ -621,8 +621,7 @@ func (node *DataNode) FlushSegments(ctx context.Context, req *datapb.FlushSegmen
 		log.Info("flow graph flushSegment tasks triggered",
 			zap.Bool("flushed", flushed),
 			zap.Int64("collection ID", req.GetCollectionID()),
-			zap.Int64s("segments", segmentIDs),
-			zap.Int64s("mark segments", req.GetMarkSegmentIDs()))
+			zap.Int64s("segments", segmentIDs))
 		return flushedSeg, noErr
 	}
 
