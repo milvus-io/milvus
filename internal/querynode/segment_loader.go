49ca8358202dbc87103565db5fd2ee883caaf4eb
yah01 <yang.cen@zilliz.com>
2022-07-25 16:33:40 +0800

    Limit the concurrency level for single load request
    Signed-off-by: yah01 <yang.cen@zilliz.com>

--- internal/querynode/segment_loader.go
+++ internal/querynode/segment_loader.go
@@ -47,6 +47,10 @@ import (
 	"github.com/milvus-io/milvus/internal/util/timerecord"
 )
 
+const (
+	requestConcurrencyLevelLimit = 8
+)
+
 // segmentLoader is only responsible for loading the field data from binlog
 type segmentLoader struct {
 	metaReplica ReplicaInterface
@@ -89,10 +93,19 @@ func (loader *segmentLoader) LoadSegment(req *querypb.LoadSegmentsRequest, segme
 		zap.Any("segmentType", segmentType.String()))
 
 	// check memory limit
-	concurrencyLevel := loader.cpuPool.Cap()
-	if concurrencyLevel > segmentNum {
-		concurrencyLevel = segmentNum
+	min := func(first int, values ...int) int {
+		minValue := first
+		for _, v := range values {
+			if v < minValue {
+				minValue = v
+			}
+		}
+		return minValue
 	}
+	concurrencyLevel := min(loader.cpuPool.Cap(),
+		len(req.Infos),
+		requestConcurrencyLevelLimit)
+
 	for ; concurrencyLevel > 1; concurrencyLevel /= 2 {
 		err := loader.checkSegmentSize(req.CollectionID, req.Infos, concurrencyLevel)
 		if err == nil {
@@ -847,6 +860,10 @@ func newSegmentLoader(
 			zap.Error(err))
 		panic(err)
 	}
+
+	log.Info("SegmentLoader created",
+		zap.Int("cpu-pool-size", cpuNum),
+		zap.Int("io-pool-size", ioPoolSize))
 
 	loader := &segmentLoader{
 		metaReplica: metaReplica,
