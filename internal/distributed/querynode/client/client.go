becd8273d40d2c67dd5095662c3bd752438378c9
Congqi Xia <congqi.xia@zilliz.com>
2022-07-26 19:17:56 +0800

    Remove Load Delta Channel Request
    Fix #18393
    
    remove watch delta channel task related logic
    
    Co-authored-by: xiaofan-luan <xiaofan.luan@zilliz.com>
    Signed-off-by: Congqi Xia <congqi.xia@zilliz.com>

--- internal/distributed/querynode/client/client.go
+++ internal/distributed/querynode/client/client.go
@@ -152,20 +152,6 @@ func (c *Client) WatchDmChannels(ctx context.Context, req *querypb.WatchDmChanne
 	return ret.(*commonpb.Status), err
 }
 
-// WatchDeltaChannels watches the channels about data manipulation.
-func (c *Client) WatchDeltaChannels(ctx context.Context, req *querypb.WatchDeltaChannelsRequest) (*commonpb.Status, error) {
-	ret, err := c.grpcClient.ReCall(ctx, func(client interface{}) (interface{}, error) {
-		if !funcutil.CheckCtxValid(ctx) {
-			return nil, ctx.Err()
-		}
-		return client.(querypb.QueryNodeClient).WatchDeltaChannels(ctx, req)
-	})
-	if err != nil || ret == nil {
-		return nil, err
-	}
-	return ret.(*commonpb.Status), err
-}
-
 // LoadSegments loads the segments to search.
 func (c *Client) LoadSegments(ctx context.Context, req *querypb.LoadSegmentsRequest) (*commonpb.Status, error) {
 	ret, err := c.grpcClient.ReCall(ctx, func(client interface{}) (interface{}, error) {
