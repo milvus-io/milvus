9b0da8e21a50f3f4d35a65fda9f85f39b0138749
yun.zhang <yun.zhang@zilliz.com>
2022-08-19 16:09:01 +0800

    Fix password verification miss cache
    Signed-off-by: yun.zhang <yun.zhang@zilliz.com>

--- internal/util/crypto/crypto_test.go
+++ internal/util/crypto/crypto_test.go
@@ -3,31 +3,9 @@ package crypto
 import (
 	"testing"
 
-	"github.com/milvus-io/milvus/internal/proto/internalpb"
 	"github.com/stretchr/testify/assert"
 	"golang.org/x/crypto/bcrypt"
 )
-
-func TestPasswordVerify_HitCache(t *testing.T) {
-	wrongPassword := "test_my_name"
-	correctPassword := "test_my_pass_new"
-	credInfo := &internalpb.CredentialInfo{
-		Username:       "root",
-		Sha256Password: "bcca79df9650cef1d7ed9f63449d7f8a27843d2678f5666f38ca65ab77d99a13",
-	}
-	assert.True(t, PasswordVerify(correctPassword, credInfo))
-	assert.False(t, PasswordVerify(wrongPassword, credInfo))
-}
-
-func TestPasswordVerify_MissCache(t *testing.T) {
-	wrongPassword := "test_my_name"
-	correctPassword := "test_my_pass_new"
-	credInfo := &internalpb.CredentialInfo{
-		EncryptedPassword: "$2a$10$3H9DLiHyPxJ29bMWRNyueOrGkbzJfE3BAR159ju3UetytAoKk7Ne2",
-	}
-	assert.True(t, PasswordVerify(correctPassword, credInfo))
-	assert.False(t, PasswordVerify(wrongPassword, credInfo))
-}
 
 //func BenchmarkPasswordVerify(b *testing.B) {
 //	correctPassword := "test_my_pass_new"
