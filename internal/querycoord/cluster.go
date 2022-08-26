6eab55a66a47cabae6c370a783e37271b41f5b4f
Wei Liu <wei.liu@zilliz.com>
2022-08-26 10:49:52 +0800

    fix watch dml channel remove offline node
    Signed-off-by: Wei Liu <wei.liu@zilliz.com>

--- internal/querycoord/cluster.go
+++ internal/querycoord/cluster.go
@@ -301,6 +301,11 @@ func (c *queryNodeCluster) WatchDmChannels(ctx context.Context, nodeID int64, in
 			old, ok := c.clusterMeta.getDmChannel(info.ChannelName)
 			if ok {
 				nodes = append(nodes, old.NodeIds...)
+				removeFromSlice(nodes, in.OfflineNodeID)
+				log.Debug("Remove offline node from dmChannel",
+					zap.String("channel", info.ChannelName),
+					zap.Int64("removedNode", in.OfflineNodeID),
+					zap.Int64s("leftNodes", nodes))
 			}
 
 			dmChannelWatchInfo[index] = &querypb.DmChannelWatchInfo{
