ac1982c8a871adc0605aad3ebfa9394a8f5372f9
XuanYang-cn <xuan.yang@zilliz.com>
2022-09-15 19:12:32 +0800

    Add merger for retrieve results (#18948)
    See also: #18893
    
    Signed-off-by: yangxuan <xuan.yang@zilliz.com>
    
    Signed-off-by: yangxuan <xuan.yang@zilliz.com>

--- internal/querynode/result_test.go
+++ internal/querynode/result_test.go
@@ -25,7 +25,173 @@ import (
 	"github.com/milvus-io/milvus/internal/common"
 	"github.com/milvus-io/milvus/internal/proto/internalpb"
 	"github.com/milvus-io/milvus/internal/proto/schemapb"
+	"github.com/milvus-io/milvus/internal/proto/segcorepb"
 )
+
+func TestResult_mergeSegcoreRetrieveResults(t *testing.T) {
+	const (
+		Dim                  = 8
+		Int64FieldName       = "Int64Field"
+		FloatVectorFieldName = "FloatVectorField"
+		Int64FieldID         = common.StartOfUserFieldID + 1
+		FloatVectorFieldID   = common.StartOfUserFieldID + 2
+	)
+	Int64Array := []int64{11, 22}
+	FloatVector := []float32{1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 11.0, 22.0, 33.0, 44.0, 55.0, 66.0, 77.0, 88.0}
+
+	var fieldDataArray1 []*schemapb.FieldData
+	fieldDataArray1 = append(fieldDataArray1, genFieldData(Int64FieldName, Int64FieldID, schemapb.DataType_Int64, Int64Array[0:2], 1))
+	fieldDataArray1 = append(fieldDataArray1, genFieldData(FloatVectorFieldName, FloatVectorFieldID, schemapb.DataType_FloatVector, FloatVector[0:16], Dim))
+
+	var fieldDataArray2 []*schemapb.FieldData
+	fieldDataArray2 = append(fieldDataArray2, genFieldData(Int64FieldName, Int64FieldID, schemapb.DataType_Int64, Int64Array[0:2], 1))
+	fieldDataArray2 = append(fieldDataArray2, genFieldData(FloatVectorFieldName, FloatVectorFieldID, schemapb.DataType_FloatVector, FloatVector[0:16], Dim))
+
+	t.Run("test skip dupPK 2", func(t *testing.T) {
+		result1 := &segcorepb.RetrieveResults{
+			Ids: &schemapb.IDs{
+				IdField: &schemapb.IDs_IntId{
+					IntId: &schemapb.LongArray{
+						Data: []int64{0, 1},
+					},
+				},
+			},
+			Offset:     []int64{0, 1},
+			FieldsData: fieldDataArray1,
+		}
+		result2 := &segcorepb.RetrieveResults{
+			Ids: &schemapb.IDs{
+				IdField: &schemapb.IDs_IntId{
+					IntId: &schemapb.LongArray{
+						Data: []int64{0, 1},
+					},
+				},
+			},
+			Offset:     []int64{0, 1},
+			FieldsData: fieldDataArray2,
+		}
+
+		result, err := mergeSegcoreRetrieveResultsV2(context.Background(), []*segcorepb.RetrieveResults{result1, result2}, unlimited)
+		assert.NoError(t, err)
+		assert.Equal(t, 2, len(result.GetFieldsData()))
+		assert.Equal(t, []int64{0, 1}, result.GetIds().GetIntId().GetData())
+		assert.Equal(t, Int64Array, result.GetFieldsData()[0].GetScalars().GetLongData().Data)
+		assert.InDeltaSlice(t, FloatVector, result.FieldsData[1].GetVectors().GetFloatVector().Data, 10e-10)
+	})
+
+	t.Run("test nil results", func(t *testing.T) {
+		ret, err := mergeSegcoreRetrieveResultsV2(context.Background(), nil, unlimited)
+		assert.NoError(t, err)
+		assert.Empty(t, ret.GetIds())
+		assert.Empty(t, ret.GetFieldsData())
+	})
+
+	t.Run("test no offset", func(t *testing.T) {
+		r := &segcorepb.RetrieveResults{
+			Ids: &schemapb.IDs{
+				IdField: &schemapb.IDs_IntId{
+					IntId: &schemapb.LongArray{
+						Data: []int64{0, 1},
+					},
+				},
+			},
+			FieldsData: fieldDataArray1,
+		}
+
+		ret, err := mergeSegcoreRetrieveResultsV2(context.Background(), []*segcorepb.RetrieveResults{r}, unlimited)
+		assert.NoError(t, err)
+		assert.Empty(t, ret.GetIds())
+		assert.Empty(t, ret.GetFieldsData())
+	})
+
+	t.Run("test merge", func(t *testing.T) {
+		r1 := &segcorepb.RetrieveResults{
+			Ids: &schemapb.IDs{
+				IdField: &schemapb.IDs_IntId{
+					IntId: &schemapb.LongArray{
+						Data: []int64{1, 3},
+					},
+				},
+			},
+			Offset:     []int64{0, 1},
+			FieldsData: fieldDataArray1,
+		}
+		r2 := &segcorepb.RetrieveResults{
+			Ids: &schemapb.IDs{
+				IdField: &schemapb.IDs_IntId{
+					IntId: &schemapb.LongArray{
+						Data: []int64{2, 4},
+					},
+				},
+			},
+			Offset:     []int64{0, 1},
+			FieldsData: fieldDataArray2,
+		}
+
+		resultFloat := []float32{
+			1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0,
+			1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0,
+			11.0, 22.0, 33.0, 44.0, 55.0, 66.0, 77.0, 88.0,
+			11.0, 22.0, 33.0, 44.0, 55.0, 66.0, 77.0, 88.0}
+
+		t.Run("test limited", func(t *testing.T) {
+			tests := []struct {
+				description string
+				limit       int
+			}{
+				{"limit 1", 1},
+				{"limit 2", 2},
+				{"limit 3", 3},
+				{"limit 4", 4},
+			}
+			resultIDs := []int64{1, 2, 3, 4}
+			resultField0 := []int64{11, 11, 22, 22}
+			for _, test := range tests {
+				t.Run(test.description, func(t *testing.T) {
+					result, err := mergeSegcoreRetrieveResultsV2(context.Background(), []*segcorepb.RetrieveResults{r1, r2}, test.limit)
+					assert.Equal(t, 2, len(result.GetFieldsData()))
+					assert.Equal(t, test.limit, len(result.GetIds().GetIntId().GetData()))
+					assert.Equal(t, resultIDs[0:test.limit], result.GetIds().GetIntId().GetData())
+					assert.Equal(t, resultField0[0:test.limit], result.GetFieldsData()[0].GetScalars().GetLongData().Data)
+					assert.InDeltaSlice(t, resultFloat[0:test.limit*Dim], result.FieldsData[1].GetVectors().GetFloatVector().Data, 10e-10)
+					assert.NoError(t, err)
+				})
+			}
+		})
+
+		t.Run("test int ID", func(t *testing.T) {
+			result, err := mergeSegcoreRetrieveResultsV2(context.Background(), []*segcorepb.RetrieveResults{r1, r2}, unlimited)
+			assert.Equal(t, 2, len(result.GetFieldsData()))
+			assert.Equal(t, []int64{1, 2, 3, 4}, result.GetIds().GetIntId().GetData())
+			assert.Equal(t, []int64{11, 11, 22, 22}, result.GetFieldsData()[0].GetScalars().GetLongData().Data)
+			assert.InDeltaSlice(t, resultFloat, result.FieldsData[1].GetVectors().GetFloatVector().Data, 10e-10)
+			assert.NoError(t, err)
+		})
+
+		t.Run("test string ID", func(t *testing.T) {
+			r1.Ids = &schemapb.IDs{
+				IdField: &schemapb.IDs_StrId{
+					StrId: &schemapb.StringArray{
+						Data: []string{"a", "c"},
+					}}}
+
+			r2.Ids = &schemapb.IDs{
+				IdField: &schemapb.IDs_StrId{
+					StrId: &schemapb.StringArray{
+						Data: []string{"b", "d"},
+					}}}
+
+			result, err := mergeSegcoreRetrieveResultsV2(context.Background(), []*segcorepb.RetrieveResults{r1, r2}, unlimited)
+			assert.NoError(t, err)
+			assert.Equal(t, 2, len(result.GetFieldsData()))
+			assert.Equal(t, []string{"a", "b", "c", "d"}, result.GetIds().GetStrId().GetData())
+			assert.Equal(t, []int64{11, 11, 22, 22}, result.GetFieldsData()[0].GetScalars().GetLongData().Data)
+			assert.InDeltaSlice(t, resultFloat, result.FieldsData[1].GetVectors().GetFloatVector().Data, 10e-10)
+			assert.NoError(t, err)
+		})
+
+	})
+}
 
 func TestResult_mergeInternalRetrieveResults(t *testing.T) {
 	const (
@@ -46,37 +212,128 @@ func TestResult_mergeInternalRetrieveResults(t *testing.T) {
 	fieldDataArray2 = append(fieldDataArray2, genFieldData(Int64FieldName, Int64FieldID, schemapb.DataType_Int64, Int64Array[0:2], 1))
 	fieldDataArray2 = append(fieldDataArray2, genFieldData(FloatVectorFieldName, FloatVectorFieldID, schemapb.DataType_FloatVector, FloatVector[0:16], Dim))
 
-	result1 := &internalpb.RetrieveResults{
-		Ids: &schemapb.IDs{
-			IdField: &schemapb.IDs_IntId{
-				IntId: &schemapb.LongArray{
-					Data: []int64{0, 1},
+	t.Run("test skip dupPK 2", func(t *testing.T) {
+		result1 := &internalpb.RetrieveResults{
+			Ids: &schemapb.IDs{
+				IdField: &schemapb.IDs_IntId{
+					IntId: &schemapb.LongArray{
+						Data: []int64{0, 1},
+					},
 				},
 			},
-		},
-		// Offset:     []int64{0, 1},
-		FieldsData: fieldDataArray1,
-	}
-	result2 := &internalpb.RetrieveResults{
-		Ids: &schemapb.IDs{
-			IdField: &schemapb.IDs_IntId{
-				IntId: &schemapb.LongArray{
-					Data: []int64{0, 1},
+			FieldsData: fieldDataArray1,
+		}
+		result2 := &internalpb.RetrieveResults{
+			Ids: &schemapb.IDs{
+				IdField: &schemapb.IDs_IntId{
+					IntId: &schemapb.LongArray{
+						Data: []int64{0, 1},
+					},
 				},
 			},
-		},
-		// Offset:     []int64{0, 1},
-		FieldsData: fieldDataArray2,
-	}
+			FieldsData: fieldDataArray2,
+		}
 
-	result, err := mergeInternalRetrieveResults(context.Background(), []*internalpb.RetrieveResults{result1, result2})
-	assert.NoError(t, err)
-	assert.Equal(t, 2, len(result.FieldsData[0].GetScalars().GetLongData().Data))
-	assert.Equal(t, 2*Dim, len(result.FieldsData[1].GetVectors().GetFloatVector().Data))
-	assert.InDeltaSlice(t, FloatVector, result.FieldsData[1].GetVectors().GetFloatVector().Data, 10e-10)
+		result, err := mergeInternalRetrieveResultsV2(context.Background(), []*internalpb.RetrieveResults{result1, result2}, unlimited)
+		assert.NoError(t, err)
+		assert.Equal(t, 2, len(result.GetFieldsData()))
+		assert.Equal(t, []int64{0, 1}, result.GetIds().GetIntId().GetData())
+		assert.Equal(t, Int64Array, result.GetFieldsData()[0].GetScalars().GetLongData().Data)
+		assert.InDeltaSlice(t, FloatVector, result.FieldsData[1].GetVectors().GetFloatVector().Data, 10e-10)
+	})
 
-	_, err = mergeInternalRetrieveResults(context.Background(), nil)
-	assert.NoError(t, err)
+	t.Run("test nil results", func(t *testing.T) {
+		ret, err := mergeInternalRetrieveResultsV2(context.Background(), nil, unlimited)
+		assert.NoError(t, err)
+		assert.Empty(t, ret.GetIds())
+		assert.Empty(t, ret.GetFieldsData())
+	})
+
+	t.Run("test merge", func(t *testing.T) {
+		r1 := &internalpb.RetrieveResults{
+			Ids: &schemapb.IDs{
+				IdField: &schemapb.IDs_IntId{
+					IntId: &schemapb.LongArray{
+						Data: []int64{1, 3},
+					},
+				},
+			},
+			FieldsData: fieldDataArray1,
+		}
+		r2 := &internalpb.RetrieveResults{
+			Ids: &schemapb.IDs{
+				IdField: &schemapb.IDs_IntId{
+					IntId: &schemapb.LongArray{
+						Data: []int64{2, 4},
+					},
+				},
+			},
+			FieldsData: fieldDataArray2,
+		}
+
+		resultFloat := []float32{
+			1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0,
+			1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0,
+			11.0, 22.0, 33.0, 44.0, 55.0, 66.0, 77.0, 88.0,
+			11.0, 22.0, 33.0, 44.0, 55.0, 66.0, 77.0, 88.0}
+
+		t.Run("test limited", func(t *testing.T) {
+			tests := []struct {
+				description string
+				limit       int
+			}{
+				{"limit 1", 1},
+				{"limit 2", 2},
+				{"limit 3", 3},
+				{"limit 4", 4},
+			}
+			resultIDs := []int64{1, 2, 3, 4}
+			resultField0 := []int64{11, 11, 22, 22}
+			for _, test := range tests {
+				t.Run(test.description, func(t *testing.T) {
+					result, err := mergeInternalRetrieveResultsV2(context.Background(), []*internalpb.RetrieveResults{r1, r2}, test.limit)
+					assert.Equal(t, 2, len(result.GetFieldsData()))
+					assert.Equal(t, test.limit, len(result.GetIds().GetIntId().GetData()))
+					assert.Equal(t, resultIDs[0:test.limit], result.GetIds().GetIntId().GetData())
+					assert.Equal(t, resultField0[0:test.limit], result.GetFieldsData()[0].GetScalars().GetLongData().Data)
+					assert.InDeltaSlice(t, resultFloat[0:test.limit*Dim], result.FieldsData[1].GetVectors().GetFloatVector().Data, 10e-10)
+					assert.NoError(t, err)
+				})
+			}
+		})
+
+		t.Run("test int ID", func(t *testing.T) {
+			result, err := mergeInternalRetrieveResultsV2(context.Background(), []*internalpb.RetrieveResults{r1, r2}, unlimited)
+			assert.Equal(t, 2, len(result.GetFieldsData()))
+			assert.Equal(t, []int64{1, 2, 3, 4}, result.GetIds().GetIntId().GetData())
+			assert.Equal(t, []int64{11, 11, 22, 22}, result.GetFieldsData()[0].GetScalars().GetLongData().Data)
+			assert.InDeltaSlice(t, resultFloat, result.FieldsData[1].GetVectors().GetFloatVector().Data, 10e-10)
+			assert.NoError(t, err)
+		})
+
+		t.Run("test string ID", func(t *testing.T) {
+			r1.Ids = &schemapb.IDs{
+				IdField: &schemapb.IDs_StrId{
+					StrId: &schemapb.StringArray{
+						Data: []string{"a", "c"},
+					}}}
+
+			r2.Ids = &schemapb.IDs{
+				IdField: &schemapb.IDs_StrId{
+					StrId: &schemapb.StringArray{
+						Data: []string{"b", "d"},
+					}}}
+
+			result, err := mergeInternalRetrieveResultsV2(context.Background(), []*internalpb.RetrieveResults{r1, r2}, unlimited)
+			assert.NoError(t, err)
+			assert.Equal(t, 2, len(result.GetFieldsData()))
+			assert.Equal(t, []string{"a", "b", "c", "d"}, result.GetIds().GetStrId().GetData())
+			assert.Equal(t, []int64{11, 11, 22, 22}, result.GetFieldsData()[0].GetScalars().GetLongData().Data)
+			assert.InDeltaSlice(t, resultFloat, result.FieldsData[1].GetVectors().GetFloatVector().Data, 10e-10)
+			assert.NoError(t, err)
+		})
+
+	})
 }
 
 func TestResult_reduceSearchResultData(t *testing.T) {
