8e1fe5030b41c9b1f41447026fc7fc3f4bd44a42
yah01 <yang.cen@zilliz.com>
2022-09-16 14:30:40 +0800

    Check whether leader view exists
    Signed-off-by: yah01 <yang.cen@zilliz.com>

--- internal/querycoordv2/job/job.go
+++ internal/querycoordv2/job/job.go
@@ -120,8 +120,7 @@ func NewLoadCollectionJob(
 
 func (job *LoadCollectionJob) PreExecute() error {
 	req := job.req
-	log := log.With(
-		zap.Int64("msgID", req.Base.GetMsgID()),
+	log := log.Ctx(job.ctx).With(
 		zap.Int64("collectionID", req.GetCollectionID()),
 	)
 
@@ -153,8 +152,7 @@ func (job *LoadCollectionJob) PreExecute() error {
 
 func (job *LoadCollectionJob) Execute() error {
 	req := job.req
-	log := log.With(
-		zap.Int64("msgID", req.GetBase().GetMsgID()),
+	log := log.Ctx(job.ctx).With(
 		zap.Int64("collectionID", req.GetCollectionID()),
 	)
 
@@ -249,8 +247,7 @@ func NewReleaseCollectionJob(ctx context.Context,
 
 func (job *ReleaseCollectionJob) Execute() error {
 	req := job.req
-	log := log.With(
-		zap.Int64("msgID", req.GetBase().GetMsgID()),
+	log := log.Ctx(job.ctx).With(
 		zap.Int64("collectionID", req.GetCollectionID()),
 	)
 	if !job.meta.CollectionManager.Exist(req.GetCollectionID()) {
@@ -314,8 +311,7 @@ func NewLoadPartitionJob(
 
 func (job *LoadPartitionJob) PreExecute() error {
 	req := job.req
-	log := log.With(
-		zap.Int64("msgID", req.Base.GetMsgID()),
+	log := log.Ctx(job.ctx).With(
 		zap.Int64("collectionID", req.GetCollectionID()),
 	)
 
@@ -357,8 +353,7 @@ func (job *LoadPartitionJob) PreExecute() error {
 
 func (job *LoadPartitionJob) Execute() error {
 	req := job.req
-	log := log.With(
-		zap.Int64("msgID", req.GetBase().GetMsgID()),
+	log := log.Ctx(job.ctx).With(
 		zap.Int64("collectionID", req.GetCollectionID()),
 	)
 
@@ -448,6 +443,9 @@ func NewReleasePartitionJob(ctx context.Context,
 }
 
 func (job *ReleasePartitionJob) PreExecute() error {
+	log := log.Ctx(job.ctx).With(
+		zap.Int64("collectionID", job.req.GetCollectionID()),
+	)
 	if job.meta.CollectionManager.GetLoadType(job.req.GetCollectionID()) == querypb.LoadType_LoadCollection {
 		msg := "releasing some partitions after load collection is not supported"
 		log.Warn(msg)
@@ -458,11 +456,9 @@ func (job *ReleasePartitionJob) PreExecute() error {
 
 func (job *ReleasePartitionJob) Execute() error {
 	req := job.req
-	log := log.With(
-		zap.Int64("msgID", req.GetBase().GetMsgID()),
+	log := log.Ctx(job.ctx).With(
 		zap.Int64("collectionID", req.GetCollectionID()),
 	)
-
 	if !job.meta.CollectionManager.Exist(req.GetCollectionID()) {
 		log.Info("release collection end, the collection has not been loaded into QueryNode")
 		return nil
