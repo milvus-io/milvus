68f8803a3b72e8cfcfdc0e2fd9c7ec11fe8db895
zhuwenxing <wenxing.zhu@zilliz.com>
2022-09-21 10:06:49 +0800

    [test]Update testcase (#19281)
    Signed-off-by: zhuwenxing <wenxing.zhu@zilliz.com>
    
    Signed-off-by: zhuwenxing <wenxing.zhu@zilliz.com>

--- tests/python_client/chaos/testcases/test_verify_all_collections.py
+++ tests/python_client/chaos/testcases/test_verify_all_collections.py
@@ -82,7 +82,7 @@ class TestOperations(TestBase):
         self.health_checkers = checkers
 
     @pytest.mark.tags(CaseLabel.L3)
-    def test_operations(self,collection_name):
+    def test_operations(self,collection_name, request_duration):
         # start the monitor threads to check the milvus ops
         log.info("*********************Test Start**********************")
         log.info(connections.get_connection_addr('default'))
@@ -90,9 +90,10 @@ class TestOperations(TestBase):
         self.init_health_checkers(collection_name=c_name)
         cc.start_monitor_threads(self.health_checkers)
         log.info("*********************Request Load Start**********************")
+        request_duration = eval(request_duration.replace("h","*3600+").replace("m","*60+").replace("s",""))
         # wait 200s for the load request to be finished
         for i in range(10):
-            sleep(20)
+            sleep(request_duration//10)
             for k,v in self.health_checkers.items():
                 v.check_result()
         log.info("******assert after chaos deleted: ")
