662c654fe3d2cc90e3bdf2d4d879de1596cf9b2b
SimFG <bang.fu@zilliz.com>
2022-09-24 09:56:51 +0800

    Add the `context` param for the `hook` interceptor (#19405)
    Signed-off-by: SimFG <bang.fu@zilliz.com>
    
    Signed-off-by: SimFG <bang.fu@zilliz.com>

--- api/hook/hook.go
+++ api/hook/hook.go
@@ -1,9 +1,11 @@
 package hook
 
+import "context"
+
 type Hook interface {
 	Init(params map[string]string) error
-	Mock(req interface{}, fullMethod string) (bool, interface{}, error)
-	Before(req interface{}, fullMethod string) error
-	After(result interface{}, err error, fullMethod string) error
+	Mock(ctx context.Context, req interface{}, fullMethod string) (bool, interface{}, error)
+	Before(ctx context.Context, req interface{}, fullMethod string) error
+	After(ctx context.Context, result interface{}, err error, fullMethod string) error
 	Release()
 }
