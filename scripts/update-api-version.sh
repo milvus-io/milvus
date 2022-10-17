2b512a3371aeadc309080683a046b59fcde19e13
SimFG <bang.fu@zilliz.com>
2022-10-17 19:19:25 +0800

    Improve the making milvus process (#19833)
    Signed-off-by: SimFG <bang.fu@zilliz.com>
    
    Signed-off-by: SimFG <bang.fu@zilliz.com>

--- scripts/update-api-version.sh
+++ scripts/update-api-version.sh
@@ -6,12 +6,11 @@ function line()
 }
 
 line
-echo "Update the milvus/api version"
-commitID=$(git ls-remote https://github.com/milvus-io/milvus-proto.git refs/heads/$GIT_BRANCH | cut -f 1)
+echo "Update the milvus-proto/api version"
+commitID=$(git ls-remote https://github.com/milvus-io/milvus-proto.git refs/heads/master | cut -f 1)
 go get github.com/milvus-io/milvus-proto/go-api@$commitID
 
 SCRIPTS_DIR=$(dirname "$0")
 EXAMPLE_DIR=$SCRIPTS_DIR/../cmake_build/thirdparty/protobuf/protobuf-src/examples
 rm -rf $EXAMPLE_DIR
 go mod tidy
-go get github.com/quasilyte/go-ruleguard/dsl@v0.3.21
