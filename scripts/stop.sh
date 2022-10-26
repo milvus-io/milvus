785add8c0d9d132d9041ac778ab94513eeedf4a7
SimFG <bang.fu@zilliz.com>
2022-10-26 18:00:23 +0800

    Fix the `stop` error when no `milvus` process
    Signed-off-by: SimFG <bang.fu@zilliz.com>

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -16,6 +16,10 @@
 
 echo "Stopping milvus..."
 PROCESS=$(ps -e | grep milvus | grep -v grep | awk '{print $1}')
+if [ -z "$PROCESS" ]; then
+  echo "No milvus process"
+  exit 0
+fi
 kill -9 $PROCESS
 echo "Milvus stopped"
 
