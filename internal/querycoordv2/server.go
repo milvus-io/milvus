13dbc0902ba4f2c51395e616dc8775fb892dc6c7
Jiquan Long <jiquan.long@zilliz.com>
2022-11-09 22:13:04 +0800

    Fix querycoord metrics (#20429) (#20448)
    Signed-off-by: longjiquan <jiquan.long@zilliz.com>
    
    Signed-off-by: longjiquan <jiquan.long@zilliz.com>
    
    Signed-off-by: longjiquan <jiquan.long@zilliz.com>

--- internal/querycoordv2/server.go
+++ internal/querycoordv2/server.go
@@ -295,6 +295,12 @@ func (s *Server) initObserver() {
 	)
 }
 
+func (s *Server) afterStart() {
+	now := time.Now()
+	Params.QueryCoordCfg.CreatedTime = now
+	Params.QueryCoordCfg.UpdatedTime = now
+}
+
 func (s *Server) Start() error {
 	log.Info("start watcher...")
 	sessions, revision, err := s.session.GetSessions(typeutil.QueryNodeRole)
@@ -351,6 +357,8 @@ func (s *Server) Start() error {
 		s.UpdateStateCode(commonpb.StateCode_Healthy)
 	}
 	log.Info("QueryCoord started")
+
+	s.afterStart()
 
 	return nil
 }
