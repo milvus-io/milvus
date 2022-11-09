2fab0c98f9f91652dd917bd84461773f577f5fbf
Jiquan Long <jiquan.long@zilliz.com>
2022-11-09 17:49:04 +0800

    Fix rootcoord metrics (#20427)
    Signed-off-by: longjiquan <jiquan.long@zilliz.com>

--- internal/rootcoord/meta_table.go
+++ internal/rootcoord/meta_table.go
@@ -22,21 +22,20 @@ import (
 	"fmt"
 	"sync"
 
-	"github.com/milvus-io/milvus/internal/common"
-
 	pb "github.com/milvus-io/milvus/internal/proto/etcdpb"
 
-	"go.uber.org/zap"
-
 	"github.com/milvus-io/milvus-proto/go-api/milvuspb"
+	"github.com/milvus-io/milvus/internal/common"
 	"github.com/milvus-io/milvus/internal/log"
 	"github.com/milvus-io/milvus/internal/metastore"
 	"github.com/milvus-io/milvus/internal/metastore/kv/rootcoord"
 	"github.com/milvus-io/milvus/internal/metastore/model"
+	"github.com/milvus-io/milvus/internal/metrics"
 	"github.com/milvus-io/milvus/internal/proto/internalpb"
 	"github.com/milvus-io/milvus/internal/util/contextutil"
 	"github.com/milvus-io/milvus/internal/util/funcutil"
 	"github.com/milvus-io/milvus/internal/util/typeutil"
+	"go.uber.org/zap"
 )
 
 const (
@@ -140,6 +139,9 @@ func (mt *MetaTable) reload() error {
 	mt.collName2ID = make(map[string]UniqueID)
 	mt.collAlias2ID = make(map[string]UniqueID)
 
+	collectionNum := int64(0)
+	partitionNum := int64(0)
+
 	// max ts means listing latest resources, meta table should always cache the latest version of catalog.
 	collections, err := mt.catalog.ListCollections(mt.ctx, typeutil.MaxTimestamp)
 	if err != nil {
@@ -148,6 +150,11 @@ func (mt *MetaTable) reload() error {
 	for name, collection := range collections {
 		mt.collID2Meta[collection.CollectionID] = collection
 		mt.collName2ID[name] = collection.CollectionID
+
+		if collection.Available() {
+			collectionNum++
+			partitionNum += int64(collection.GetPartitionNum(true))
+		}
 	}
 
 	// max ts means listing latest resources, meta table should always cache the latest version of catalog.
@@ -158,6 +165,9 @@ func (mt *MetaTable) reload() error {
 	for _, alias := range aliases {
 		mt.collAlias2ID[alias.Name] = alias.CollectionID
 	}
+
+	metrics.RootCoordNumOfCollections.Set(float64(collectionNum))
+	metrics.RootCoordNumOfPartitions.WithLabelValues().Set(float64(partitionNum))
 
 	return nil
 }
@@ -199,6 +209,16 @@ func (mt *MetaTable) ChangeCollectionState(ctx context.Context, collectionID Uni
 		return err
 	}
 	mt.collID2Meta[collectionID] = clone
+
+	switch state {
+	case pb.CollectionState_CollectionCreated:
+		metrics.RootCoordNumOfCollections.Inc()
+		metrics.RootCoordNumOfPartitions.WithLabelValues().Add(float64(coll.GetPartitionNum(true)))
+	default:
+		metrics.RootCoordNumOfCollections.Dec()
+		metrics.RootCoordNumOfPartitions.WithLabelValues().Sub(float64(coll.GetPartitionNum(true)))
+	}
+
 	log.Info("change collection state", zap.Int64("collection", collectionID),
 		zap.String("state", state.String()), zap.Uint64("ts", ts))
 
@@ -457,9 +477,13 @@ func (mt *MetaTable) AddPartition(ctx context.Context, partition *model.Partitio
 		return err
 	}
 	mt.collID2Meta[partition.CollectionID].Partitions = append(mt.collID2Meta[partition.CollectionID].Partitions, partition.Clone())
+
+	metrics.RootCoordNumOfPartitions.WithLabelValues().Inc()
+
 	log.Info("add partition to meta table",
 		zap.Int64("collection", partition.CollectionID), zap.String("partition", partition.PartitionName),
 		zap.Int64("partitionid", partition.PartitionID), zap.Uint64("ts", partition.PartitionCreatedTimestamp))
+
 	return nil
 }
 
@@ -480,9 +504,20 @@ func (mt *MetaTable) ChangePartitionState(ctx context.Context, collectionID Uniq
 				return err
 			}
 			mt.collID2Meta[collectionID].Partitions[idx] = clone
+
+			switch state {
+			case pb.PartitionState_PartitionCreated:
+				log.Warn("[should not happen] change partition to created",
+					zap.String("collection", coll.Name), zap.Int64("collection id", coll.CollectionID),
+					zap.String("partition", clone.PartitionName), zap.Int64("partition id", clone.PartitionID))
+			default:
+				metrics.RootCoordNumOfPartitions.WithLabelValues().Dec()
+			}
+
 			log.Info("change partition state", zap.Int64("collection", collectionID),
 				zap.Int64("partition", partitionID), zap.String("state", state.String()),
 				zap.Uint64("ts", ts))
+
 			return nil
 		}
 	}
