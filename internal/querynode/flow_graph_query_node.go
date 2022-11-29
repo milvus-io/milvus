072a54dea97632e325d95eb6467883a241453324
congqixia <congqi.xia@zilliz.com>
2022-11-29 15:15:15 +0800

    Fix delete operation scope to vchannel (#20866)
    Signed-off-by: Congqi Xia <congqi.xia@zilliz.com>
    
    Signed-off-by: Congqi Xia <congqi.xia@zilliz.com>

--- internal/querynode/flow_graph_query_node.go
+++ internal/querynode/flow_graph_query_node.go
@@ -143,7 +143,10 @@ func newQueryNodeDeltaFlowGraph(ctx context.Context,
 		return nil, err
 	}
 	var filterDeleteNode node = newFilteredDeleteNode(metaReplica, collectionID, vchannel)
-	var deleteNode node = newDeleteNode(metaReplica, collectionID, vchannel)
+	deleteNode, err := newDeleteNode(metaReplica, collectionID, vchannel)
+	if err != nil {
+		return nil, err
+	}
 	var serviceTimeNode node = newServiceTimeNode(tSafeReplica, collectionID, vchannel)
 
 	q.flowGraph.AddNode(dmStreamNode)
