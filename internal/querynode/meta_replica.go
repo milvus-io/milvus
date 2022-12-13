1c309a74eeb8843890ad246a89636513907564f8
zhenshan.cao <zhenshan.cao@zilliz.com>
2022-12-13 11:47:22 +0800

    Fix metric with wrong label (#21127) (#21174)
    Signed-off-by: zhenshan.cao <zhenshan.cao@zilliz.com>
    
    Signed-off-by: zhenshan.cao <zhenshan.cao@zilliz.com>
    
    Signed-off-by: zhenshan.cao <zhenshan.cao@zilliz.com>

--- internal/querynode/meta_replica.go
+++ internal/querynode/meta_replica.go
@@ -623,7 +623,7 @@ func (replica *metaReplica) addSegmentPrivate(segment *Segment) error {
 		fmt.Sprint(Params.QueryNodeCfg.GetNodeID()),
 		fmt.Sprint(segment.collectionID),
 		fmt.Sprint(segment.partitionID),
-		string(segType),
+		segType.String(),
 		fmt.Sprint(segment.indexedFieldInfos.Len()),
 	).Inc()
 	if rowCount > 0 {
@@ -731,7 +731,7 @@ func (replica *metaReplica) removeSegmentPrivate(segmentID UniqueID, segType seg
 			fmt.Sprint(Params.QueryNodeCfg.GetNodeID()),
 			fmt.Sprint(segment.collectionID),
 			fmt.Sprint(segment.partitionID),
-			string(segType),
+			segType.String(),
 			// Note: this field is mutable after segment is loaded.
 			fmt.Sprint(segment.indexedFieldInfos.Len()),
 		).Dec()
