2c95411e0f95391435c90e8dbc83e0576676face
zhuwenxing <wenxing.zhu@zilliz.com>
2022-12-14 19:29:27 +0800

    Create index before load for upgrading test (#21222)
    Signed-off-by: zhuwenxing <wenxing.zhu@zilliz.com>
    
    Signed-off-by: zhuwenxing <wenxing.zhu@zilliz.com>

--- tests/python_client/deploy/scripts/action_after_upgrade.py
+++ tests/python_client/deploy/scripts/action_after_upgrade.py
@@ -9,7 +9,7 @@ from utils import *
 def task_1(data_size, host):
     """
     task_1:
-        before upgrade: create collection and insert data with flush, load and search
+        before upgrade: create collection and insert data with flush, create index, load and search
         after upgrade: get collection, load, search, insert data with flush, release, create index, load, and search
     """
     prefix = "task_1_"
