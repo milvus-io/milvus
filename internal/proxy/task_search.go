758ecba77ecabf21b623c1b16df9e7b6f8bfca87
congqixia <congqi.xia@zilliz.com>
2022-12-29 23:29:32 +0800

    Invalidate cache first when shard leader list contains error (#21451)
    Signed-off-by: Congqi Xia <congqi.xia@zilliz.com>

--- internal/proxy/task_search.go
+++ internal/proxy/task_search.go
@@ -406,7 +406,10 @@ func (t *searchTask) Execute(ctx context.Context) error {
 
 	err := executeSearch(WithCache)
 	if err != nil {
-		log.Warn("first search failed, updating shardleader caches and retry search", zap.Error(err))
+		log.Warn("first search failed, updating shardleader caches and retry search",
+			zap.Error(err))
+		// invalidate cache first, since ctx may be canceled or timeout here
+		globalMetaCache.ClearShards(t.collectionName)
 		err = executeSearch(WithoutCache)
 	}
 	if err != nil {
