c88c2e82d1f3124e9ef8e1926c57a46e8fe0c6d0
Bingyi Sun <sunbingyi1992@gmail.com>
2023-01-18 17:29:43 +0800

    Change CreateAt to UpdatedAt when checking timeout (#21769)
    Signed-off-by: sunby <bingyi.sun@zilliz.com>
    Co-authored-by: sunby <bingyi.sun@zilliz.com>

--- internal/querycoordv2/observers/collection_observer.go
+++ internal/querycoordv2/observers/collection_observer.go
@@ -123,7 +123,7 @@ func (ob *CollectionObserver) observeTimeout() {
 		)
 		for _, partition := range partitions {
 			if partition.GetStatus() != querypb.LoadStatus_Loading ||
-				time.Now().Before(partition.CreatedAt.Add(Params.QueryCoordCfg.LoadTimeoutSeconds)) {
+				time.Now().Before(partition.UpdatedAt.Add(Params.QueryCoordCfg.LoadTimeoutSeconds)) {
 				continue
 			}
 
