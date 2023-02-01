6da10ef7181b6e75f3864a8a030fa83a2298c8d9
yah01 <yang.cen@zilliz.com>
2023-02-01 17:40:34 +0800

    Fix datanode unit test not stable
    Signed-off-by: yah01 <yang.cen@zilliz.com>

--- internal/datanode/data_node_test.go
+++ internal/datanode/data_node_test.go
@@ -293,11 +293,11 @@ func TestWatchChannel(t *testing.T) {
 
 		err = kv.RemoveWithPrefix(fmt.Sprintf("%s/%d", Params.CommonCfg.DataCoordWatchSubPath.GetValue(), paramtable.GetNodeID()))
 		assert.Nil(t, err)
-		//TODO there is not way to sync Release done, use sleep for now
-		time.Sleep(100 * time.Millisecond)
 
-		exist = node.flowgraphManager.exist(ch)
-		assert.False(t, exist)
+		assert.Eventually(t, func() bool {
+			exist = node.flowgraphManager.exist(ch)
+			return !exist
+		}, 3*time.Second, 100*time.Millisecond)
 	})
 
 	t.Run("Test release channel", func(t *testing.T) {
