4fa8b9014127ee455dd1f32410f19148e9ba5370
wei liu <wei.liu@zilliz.com>
2023-02-02 14:55:53 +0800

    fix 2.2 branch use master branch proto (#21914)
    Signed-off-by: Wei Liu <wei.liu@zilliz.com>

--- scripts/update-api-version.sh
+++ scripts/update-api-version.sh
@@ -7,7 +7,7 @@ function line()
 
 line
 echo "Update the milvus-proto/api version"
-commitID=$(git ls-remote https://github.com/milvus-io/milvus-proto.git refs/heads/master | cut -f 1)
+commitID=$(git ls-remote https://github.com/milvus-io/milvus-proto.git refs/heads/2.2 | cut -f 1)
 go get github.com/milvus-io/milvus-proto/go-api@$commitID
 
 SCRIPTS_DIR=$(dirname "$0")
@@ -19,6 +19,6 @@ line echo "Update the milvus-proto repo"
 THIRD_PARTY_DIR=$SCRIPTS_DIR/../cmake_build/thirdparty
 
 pushd $THIRD_PARTY_DIR/milvus-proto
-  git checkout master
-  git pull origin master
+  git checkout 2.2
+  git pull origin 2.2
 popd
