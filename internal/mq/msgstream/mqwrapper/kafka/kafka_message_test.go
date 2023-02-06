ffdb65291104564e83570804a5b907efb72a0a3b
smellthemoon <64083300+smellthemoon@users.noreply.github.com>
2023-02-06 16:33:54 +0800

    Fix kafka ut (#21943)
    Signed-off-by: lixinguo <xinguo.li@zilliz.com>
    Co-authored-by: lixinguo <xinguo.li@zilliz.com>

--- internal/mq/msgstream/mqwrapper/kafka/kafka_message_test.go
+++ internal/mq/msgstream/mqwrapper/kafka/kafka_message_test.go
@@ -11,8 +11,9 @@ func TestKafkaMessage_All(t *testing.T) {
 	topic := "t"
 	msg := &kafka.Message{TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: 0, Offset: 0}, Value: nil, Headers: nil}
 	km := &kafkaMessage{msg: msg}
+	properties := make(map[string]string)
 	assert.Equal(t, topic, km.Topic())
 	assert.Equal(t, int64(0), km.ID().(*kafkaID).messageID)
 	assert.Nil(t, km.Payload())
-	assert.Nil(t, km.Properties())
+	assert.Equal(t, properties, km.Properties())
 }
