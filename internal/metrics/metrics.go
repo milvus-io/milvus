0127509ec566970307a8c9d3a948294bdd55c2f2
SimFG <bang.fu@zilliz.com>
2023-02-08 16:14:31 +0800

    Add error log and metrics for the hook and privilege interceptor
    Signed-off-by: SimFG <bang.fu@zilliz.com>

--- internal/metrics/metrics.go
+++ internal/metrics/metrics.go
@@ -63,6 +63,10 @@ const (
 	Leader     = "OnLeader"
 	FromLeader = "FromLeader"
 
+	HookBefore = "before"
+	HookAfter  = "after"
+	HookMock   = "mock"
+
 	nodeIDLabelName          = "node_id"
 	statusLabelName          = "status"
 	indexTaskStatusLabelName = "index_task_status"
@@ -82,6 +86,7 @@ const (
 	cacheStateLabelName      = "cache_state"
 	indexCountLabelName      = "indexed_field_count"
 	requestScope             = "scope"
+	fullMethodLabelName      = "full_method"
 )
 
 var (
