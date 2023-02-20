76640e63c3ea14ad42d3bbd4f091b164923a850b
yah01 <yang.cen@zilliz.com>
2023-02-20 13:54:30 +0800

    Avoid counting all bits to improve query performance (#21909) (#22285)
    Signed-off-by: yah01 <yang.cen@zilliz.com>

--- internal/core/src/query/visitors/ExecPlanNodeVisitor.cpp
+++ internal/core/src/query/visitors/ExecPlanNodeVisitor.cpp
@@ -134,7 +134,7 @@ ExecPlanNodeVisitor::visit(RetrievePlanNode& node) {
 
     segment->mask_with_delete(bitset_holder, active_count, timestamp_);
     // if bitset_holder is all 1's, we got empty result
-    if (bitset_holder.count() == bitset_holder.size()) {
+    if (bitset_holder.all()) {
         retrieve_result_opt_ = std::move(retrieve_result);
         return;
     }
