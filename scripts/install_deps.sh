6fa8d6287980e37eff8bbfe1188c1c95cb986571
Enwei Jiao <enwei.jiao@zilliz.com>
2023-03-01 19:59:47 +0800

    Lock conan version at 1.58.0 (#22503)
    Signed-off-by: Enwei Jiao <enwei.jiao@zilliz.com>

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -24,7 +24,7 @@ function install_linux_deps() {
       clang-format-10 clang-tidy-10 lcov libtool m4 autoconf automake python3 python3-pip \
       pkg-config uuid-dev libaio-dev libgoogle-perftools-dev 
 
-    sudo pip3 install conan
+    sudo pip3 install conan==1.58.0
   elif [[ -x "$(command -v yum)" ]]; then
     # for CentOS devtoolset-11
     sudo yum install -y epel-release centos-release-scl-rh 
@@ -35,7 +35,7 @@ function install_linux_deps() {
       libaio libuuid-devel unzip \
       ccache lcov libtool m4 autoconf automake
 
-    sudo pip3 install conan
+    sudo pip3 install conan==1.58.0
     echo "source scl_source enable devtoolset-11" | sudo tee -a /etc/profile.d/devtoolset-11.sh
     echo "source scl_source enable llvm-toolset-11.0" | sudo tee -a /etc/profile.d/llvm-toolset-11.sh
     echo "export CLANG_TOOLS_PATH=/opt/rh/llvm-toolset-11.0/root/usr/bin" | sudo tee -a /etc/profile.d/llvm-toolset-11.sh
@@ -50,9 +50,11 @@ function install_linux_deps() {
 
 function install_mac_deps() {
   sudo xcode-select --install > /dev/null 2>&1
-  brew install conan libomp ninja cmake llvm ccache grep
+  brew install libomp ninja cmake llvm ccache grep
   export PATH="/usr/local/opt/grep/libexec/gnubin:$PATH"
   brew update && brew upgrade && brew cleanup
+
+  pip3 install conan==1.58.0
 
   if [[ $(arch) == 'arm64' ]]; then
     brew install openssl
