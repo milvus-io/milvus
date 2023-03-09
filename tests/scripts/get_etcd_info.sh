e0352db982cbdc6c676fdbf86f1ce35e656661bc
zhuwenxing <wenxing.zhu@zilliz.com>
2023-03-09 19:10:00 +0800

    [skip e2e]Add script to get ectd status (#22638)
    Signed-off-by: zhuwenxing <wenxing.zhu@zilliz.com>

--- tests/scripts/get_etcd_info.sh
+++ tests/scripts/get_etcd_info.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
+set -e # Exit immediately if a command exits with a non-zero status.
 instance_name=$1
 # Define the etcdctl command with endpoints option
 etcdctl_cmd="etcdctl endpoint status -w table --endpoints"
 
 # Get the ip of all the etcd pods with the specified labels
 etcd_pods=$(kubectl get pods -l app.kubernetes.io/name=etcd,app.kubernetes.io/instance=${instance_name} -o jsonpath='{.items[*].status.podIP}')
+
+# Check if the etcd pods are running
+if [ -z "$etcd_pods" ]; then
+    echo "No etcd pods found"
+    exit 1
+fi
 
 # Loop through the list of etcd pods and get their status
 endpoints=""
