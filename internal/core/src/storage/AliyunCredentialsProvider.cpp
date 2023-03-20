7c633e9b9d202d1cbec744baf100641e5d819436
zhagnlu <1542303831@qq.com>
2023-03-20 14:45:57 +0800

    Support aliyun OSS for chunk manager (#22663) (#22842)
    Signed-off-by: luzhang <luzhang@zilliz.com>
    Co-authored-by: luzhang <luzhang@zilliz.com>

---
+++ internal/core/src/storage/AliyunCredentialsProvider.cpp
@@ -0,0 +1,184 @@
+/**
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0.
+ */
+
+#include <aws/core/config/AWSProfileConfigLoader.h>
+#include <aws/core/platform/Environment.h>
+#include <aws/core/platform/FileSystem.h>
+#include <aws/core/utils/logging/LogMacros.h>
+#include <aws/core/utils/StringUtils.h>
+#include <aws/core/utils/FileSystemUtils.h>
+#include <aws/core/client/SpecifiedRetryableErrorsRetryStrategy.h>
+#include <aws/core/utils/UUID.h>
+#include <cstdlib>
+#include <fstream>
+#include <string.h>
+#include <climits>
+#include "AliyunCredentialsProvider.h"
+
+static const char STS_ASSUME_ROLE_WEB_IDENTITY_LOG_TAG[] =
+    "AliyunSTSAssumeRoleWebIdentityCredentialsProvider";  // [aliyun]
+static const int STS_CREDENTIAL_PROVIDER_EXPIRATION_GRACE_PERIOD =
+    180 *
+    1000;  // [aliyun] supports 1800 second at most, here we use their default: 180s -> 180*1k ms
+
+namespace Aws {
+namespace Auth {
+AliyunSTSAssumeRoleWebIdentityCredentialsProvider::
+    AliyunSTSAssumeRoleWebIdentityCredentialsProvider()
+    : m_initialized(false) {
+    // check environment variables
+    // not need in [aliyun]
+    // Aws::String tmpRegion = Aws::Environment::GetEnv("AWS_DEFAULT_REGION");
+    m_roleArn = Aws::Environment::GetEnv("ALIBABA_CLOUD_ROLE_ARN");  // [aliyun]
+    m_tokenFile =
+        Aws::Environment::GetEnv("ALIBABA_CLOUD_OIDC_TOKEN_FILE");  // [aliyun]
+    // optional, not existed in [aliyun]
+    m_sessionName = Aws::Environment::GetEnv("ALIBABA_CLOUD_ROLE_SESSION_NAME");
+
+    // check profile_config if either m_roleArn or m_tokenFile is not loaded from environment variable
+    // region source is not enforced, but we need it to construct sts endpoint, if we can't find from environment, we
+    // should check if it's set in config file.
+    if (m_roleArn.empty() ||
+        m_tokenFile.empty()) {  // || tmpRegion.empty() not need in [aliyun]
+        auto profile = Aws::Config::GetCachedConfigProfile(
+            Aws::Auth::GetConfigProfileName());
+        // If either of these two were not found from environment, use whatever found for all three in config file
+        if (m_roleArn.empty() || m_tokenFile.empty()) {
+            m_roleArn = profile.GetRoleArn();
+            m_tokenFile = profile.GetValue("web_identity_token_file");
+            m_sessionName = profile.GetValue("role_session_name");
+        }
+    }
+
+    if (m_tokenFile.empty()) {
+        AWS_LOGSTREAM_WARN(STS_ASSUME_ROLE_WEB_IDENTITY_LOG_TAG,
+                           "Token file must be specified to use STS AssumeRole "
+                           "web identity creds provider.");
+        return;  // No need to do further constructing
+    } else {
+        AWS_LOGSTREAM_DEBUG(STS_ASSUME_ROLE_WEB_IDENTITY_LOG_TAG,
+                            "Resolved token_file from profile_config or "
+                            "environment variable to be "
+                                << m_tokenFile);
+    }
+
+    if (m_roleArn.empty()) {
+        AWS_LOGSTREAM_WARN(STS_ASSUME_ROLE_WEB_IDENTITY_LOG_TAG,
+                           "RoleArn must be specified to use STS AssumeRole "
+                           "web identity creds provider.");
+        return;  // No need to do further constructing
+    } else {
+        AWS_LOGSTREAM_DEBUG(STS_ASSUME_ROLE_WEB_IDENTITY_LOG_TAG,
+                            "Resolved role_arn from profile_config or "
+                            "environment variable to be "
+                                << m_roleArn);
+    }
+
+    // not need in [aliyun]
+    // if (tmpRegion.empty())
+    // {
+    //     tmpRegion = Aws::Region::US_EAST_1;
+    // }
+    // else
+    // {
+    //     AWS_LOGSTREAM_DEBUG(STS_ASSUME_ROLE_WEB_IDENTITY_LOG_TAG, "Resolved region from profile_config or environment
+    //     variable to be " << tmpRegion);
+    // }
+
+    if (m_sessionName.empty()) {
+        m_sessionName = Aws::Utils::UUID::RandomUUID();
+    } else {
+        AWS_LOGSTREAM_DEBUG(STS_ASSUME_ROLE_WEB_IDENTITY_LOG_TAG,
+                            "Resolved session_name from profile_config or "
+                            "environment variable to be "
+                                << m_sessionName);
+    }
+
+    Aws::Client::ClientConfiguration config;
+    config.scheme = Aws::Http::Scheme::HTTPS;
+    // not need in [aliyun]
+    // config.region = tmpRegion;
+
+    Aws::Vector<Aws::String> retryableErrors;
+    retryableErrors.push_back("IDPCommunicationError");
+    retryableErrors.push_back("InvalidIdentityToken");
+
+    config.retryStrategy =
+        Aws::MakeShared<Aws::Client::SpecifiedRetryableErrorsRetryStrategy>(
+            STS_ASSUME_ROLE_WEB_IDENTITY_LOG_TAG,
+            retryableErrors,
+            3 /*maxRetries*/);
+
+    m_client = Aws::MakeUnique<Aws::Internal::AliyunSTSCredentialsClient>(
+        STS_ASSUME_ROLE_WEB_IDENTITY_LOG_TAG, config);
+    m_initialized = true;
+    AWS_LOGSTREAM_INFO(
+        STS_ASSUME_ROLE_WEB_IDENTITY_LOG_TAG,
+        "Creating STS AssumeRole with web identity creds provider.");
+}
+
+Aws::Auth::AWSCredentials
+AliyunSTSAssumeRoleWebIdentityCredentialsProvider::GetAWSCredentials() {
+    // A valid client means required information like role arn and token file were constructed correctly.
+    // We can use this provider to load creds, otherwise, we can just return empty creds.
+    if (!m_initialized) {
+        return Aws::Auth::AWSCredentials();
+    }
+    RefreshIfExpired();
+    Aws::Utils::Threading::ReaderLockGuard guard(m_reloadLock);
+    return m_credentials;
+}
+
+void
+AliyunSTSAssumeRoleWebIdentityCredentialsProvider::Reload() {
+    AWS_LOGSTREAM_INFO(
+        STS_ASSUME_ROLE_WEB_IDENTITY_LOG_TAG,
+        "Credentials have expired, attempting to renew from STS.");
+
+    Aws::IFStream tokenFile(m_tokenFile.c_str());
+    if (tokenFile) {
+        Aws::String token((std::istreambuf_iterator<char>(tokenFile)),
+                          std::istreambuf_iterator<char>());
+        m_token = token;
+    } else {
+        AWS_LOGSTREAM_ERROR(STS_ASSUME_ROLE_WEB_IDENTITY_LOG_TAG,
+                            "Can't open token file: " << m_tokenFile);
+        return;
+    }
+    Aws::Internal::AliyunSTSCredentialsClient::
+        STSAssumeRoleWithWebIdentityRequest request{
+            m_sessionName, m_roleArn, m_token};
+
+    auto result = m_client->GetAssumeRoleWithWebIdentityCredentials(request);
+    AWS_LOGSTREAM_TRACE(
+        STS_ASSUME_ROLE_WEB_IDENTITY_LOG_TAG,
+        "Successfully retrieved credentials with AWS_ACCESS_KEY: "
+            << result.creds.GetAWSAccessKeyId());
+    m_credentials = result.creds;
+}
+
+bool
+AliyunSTSAssumeRoleWebIdentityCredentialsProvider::ExpiresSoon() const {
+    return (
+        (m_credentials.GetExpiration() - Aws::Utils::DateTime::Now()).count() <
+        STS_CREDENTIAL_PROVIDER_EXPIRATION_GRACE_PERIOD);
+}
+
+void
+AliyunSTSAssumeRoleWebIdentityCredentialsProvider::RefreshIfExpired() {
+    Aws::Utils::Threading::ReaderLockGuard guard(m_reloadLock);
+    if (!m_credentials.IsEmpty() && !ExpiresSoon()) {
+        return;
+    }
+
+    guard.UpgradeToWriterLock();
+    if (!m_credentials.IsExpiredOrEmpty() && !ExpiresSoon()) {
+        return;
+    }
+
+    Reload();
+}
+}  // namespace Auth
+};  // namespace Aws
--- internal/core/unittest/test_minio_chunk_manager.cpp
+++ internal/core/unittest/test_minio_chunk_manager.cpp
@@ -30,8 +30,51 @@ class MinioChunkManagerTest : public testing::Test {
 
     virtual void
     SetUp() {
-        chunk_manager_ =
-            std::make_unique<MinioChunkManager>(get_default_storage_config());
+        chunk_manager_ = std::make_unique<MinioChunkManager>(get_default_storage_config());
+    }
+
+ protected:
+    MinioChunkManagerPtr chunk_manager_;
+};
+
+StorageConfig
+get_google_cloud_storage_config() {
+    auto endpoint = "storage.googleapis.com:443";
+    auto accessKey = "";
+    auto accessValue = "";
+    auto rootPath = "files";
+    auto useSSL = true;
+    auto useIam = true;
+    auto iamEndPoint = "";
+    auto bucketName = "gcp-zilliz-infra-test";
+
+    return StorageConfig{endpoint, bucketName, accessKey, accessValue, rootPath, "minio", iamEndPoint, useSSL, useIam};
+}
+
+StorageConfig
+get_aliyun_cloud_storage_config() {
+    auto endpoint = "oss-cn-shanghai.aliyuncs.com:443";
+    auto accessKey = "";
+    auto accessValue = "";
+    auto rootPath = "files";
+    auto useSSL = true;
+    auto useIam = true;
+    auto iamEndPoint = "";
+    auto bucketName = "vdc-infra-poc";
+
+    return StorageConfig{endpoint, bucketName, accessKey, accessValue, rootPath, "minio", iamEndPoint, useSSL, useIam};
+}
+
+class AliyunChunkManagerTest : public testing::Test {
+ public:
+    AliyunChunkManagerTest() {
+    }
+    ~AliyunChunkManagerTest() {
+    }
+
+    virtual void
+    SetUp() {
+        chunk_manager_ = std::make_unique<MinioChunkManager>(get_aliyun_cloud_storage_config());
     }
 
  protected:
@@ -219,4 +262,47 @@ TEST_F(MinioChunkManagerTest, ListWithPrefixPositive) {
     chunk_manager_->Remove(path2);
     chunk_manager_->Remove(path3);
     chunk_manager_->DeleteBucket(testBucketName);
+}
+
+TEST_F(AliyunChunkManagerTest, ReadPositive) {
+    string testBucketName = "vdc-infra-poc";
+    chunk_manager_->SetBucketName(testBucketName);
+    EXPECT_EQ(chunk_manager_->GetBucketName(), testBucketName);
+
+    uint8_t data[5] = {0x17, 0x32, 0x45, 0x34, 0x23};
+    string path = "1/4/6";
+    chunk_manager_->Write(path, data, sizeof(data));
+    bool exist = chunk_manager_->Exist(path);
+    EXPECT_EQ(exist, true);
+    auto size = chunk_manager_->Size(path);
+    EXPECT_EQ(size, 5);
+
+    uint8_t readdata[20] = {0};
+    size = chunk_manager_->Read(path, readdata, 20);
+    EXPECT_EQ(readdata[0], 0x17);
+    EXPECT_EQ(readdata[1], 0x32);
+    EXPECT_EQ(readdata[2], 0x45);
+    EXPECT_EQ(readdata[3], 0x34);
+    EXPECT_EQ(readdata[4], 0x23);
+
+    size = chunk_manager_->Read(path, readdata, 3);
+    EXPECT_EQ(size, 3);
+    EXPECT_EQ(readdata[0], 0x17);
+    EXPECT_EQ(readdata[1], 0x32);
+    EXPECT_EQ(readdata[2], 0x45);
+
+    uint8_t dataWithNULL[] = {0x17, 0x32, 0x00, 0x34, 0x23};
+    chunk_manager_->Write(path, dataWithNULL, sizeof(dataWithNULL));
+    exist = chunk_manager_->Exist(path);
+    EXPECT_EQ(exist, true);
+    size = chunk_manager_->Size(path);
+    EXPECT_EQ(size, 5);
+    size = chunk_manager_->Read(path, readdata, 20);
+    EXPECT_EQ(readdata[0], 0x17);
+    EXPECT_EQ(readdata[1], 0x32);
+    EXPECT_EQ(readdata[2], 0x00);
+    EXPECT_EQ(readdata[3], 0x34);
+    EXPECT_EQ(readdata[4], 0x23);
+
+    chunk_manager_->Remove(path);
 }
