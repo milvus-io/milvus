70d602b6e1cf6489d06710046e1142b73a7a44be
xiaofan-luan <xiaofan.luan@zilliz.com>
2023-03-27 00:35:08 +0800

    Add config for disable BF load
    Signed-off-by: xiaofan-luan <xiaofan.luan@zilliz.com>

--- internal/util/paramtable/component_param.go
+++ internal/util/paramtable/component_param.go
@@ -1567,6 +1567,8 @@ type dataNodeConfig struct {
 	// datanote send timetick interval per channel
 	DataNodeTimeTickInterval int
 
+	SkipBFStatsLoad bool
+
 	CreatedTime time.Time
 	UpdatedTime time.Time
 
@@ -1588,6 +1590,8 @@ func (p *dataNodeConfig) init(base *BaseTable) {
 	p.initSyncPeriod()
 	p.initIOConcurrency()
 	p.initDataNodeTimeTickInterval()
+
+	p.initSkipBFStatsLoad()
 
 	p.initChannelWatchPath()
 	p.initMemoryForceSyncEnable()
@@ -1651,6 +1655,10 @@ func (p *dataNodeConfig) initIOConcurrency() {
 
 func (p *dataNodeConfig) initDataNodeTimeTickInterval() {
 	p.DataNodeTimeTickInterval = p.Base.ParseIntWithDefault("datanode.timetick.interval", 500)
+}
+
+func (p *dataNodeConfig) initSkipBFStatsLoad() {
+	p.SkipBFStatsLoad = p.Base.ParseBool("dataNode.skip.BFStats.Load", false)
 }
 
 func (p *dataNodeConfig) SetNodeID(id UniqueID) {
