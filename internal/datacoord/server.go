fb47f7af9811acdc411452a3b0b14ba053a53225
yah01 <yang.cen@zilliz.com>
2023-03-27 18:58:39 +0800

    Fix DataCoord's meta may be broken with DataNode of prior version
    Signed-off-by: yah01 <yang.cen@zilliz.com>

--- internal/datacoord/server.go
+++ internal/datacoord/server.go
@@ -469,7 +469,7 @@ func (s *Server) initGarbageCollection(cli storage.ChunkManager) {
 }
 
 func (s *Server) initServiceDiscovery() error {
-	r := semver.MustParseRange(">=2.1.2")
+	r := semver.MustParseRange(">=2.2.3")
 	sessions, rev, err := s.session.GetSessionsWithVersionRange(typeutil.DataNodeRole, r)
 	if err != nil {
 		log.Warn("DataCoord failed to init service discovery", zap.Error(err))
