d4757749b8bfb2209dd56c4176020ff0ac91e69e
cai.zhang <cai.zhang@zilliz.com>
2023-03-28 16:30:21 +0800

    Fix bug for batch delete files on gcp of minio (#23052)
    Signed-off-by: cai.zhang <cai.zhang@zilliz.com>

--- internal/indexcoord/garbage_collector.go
+++ internal/indexcoord/garbage_collector.go
@@ -176,6 +176,8 @@ func (gc *garbageCollector) recycleSegIndexesMeta() {
 		if meta.IsDeleted || gc.metaTable.IsIndexDeleted(meta.CollectionID, meta.IndexID) {
 			if meta.NodeID != 0 {
 				// wait for releasing reference lock
+				log.Ctx(gc.ctx).Warn("nodeID is not zero, wait for releasing reference lock",
+					zap.Int64("buildID", meta.BuildID), zap.Int64("segID", meta.SegmentID), zap.Int64("nodeID", meta.NodeID))
 				continue
 			}
 			if err := gc.metaTable.RemoveSegmentIndex(meta.CollectionID, meta.PartitionID, meta.SegmentID, meta.BuildID); err != nil {
