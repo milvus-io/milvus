e8082b105754c4b5f967b79425b70358935369b0
ThreadDao <yufen.zong@zilliz.com>
2023-03-30 22:00:24 +0800

    Add flush all test cases (#23129)
    Signed-off-by: ThreadDao <yufen.zong@zilliz.com>

--- tests/python_client/common/common_type.py
+++ tests/python_client/common/common_type.py
@@ -62,6 +62,7 @@ max_compaction_interval = 60  # the max time interval (s) from the last compacti
 max_field_num = 64  # Maximum number of fields in a collection
 max_name_length = 255  # Maximum length of name for a collection or alias
 default_replica_num = 1
+max_shards_num = 64
 IMAGE_REPOSITORY_MILVUS = "harbor.milvus.io/dockerhub/milvusdb/milvus"
 NAMESPACE_CHAOS_TESTING = "chaos-testing"
 
