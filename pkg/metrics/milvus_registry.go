55202ce7d901bc20e651566a7b100218a1aff1ee
Cai Yudong <yudong.cai@zilliz.com>
2023-04-07 15:34:29 +0800

    Support show knowhere prometheus metrics (#23102)
    Signed-off-by: yudong.cai <yudong.cai@zilliz.com>

---
+++ pkg/metrics/milvus_registry.go
@@ -0,0 +1,52 @@
+// Copyright 2014 The Prometheus Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package metrics
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+	dto "github.com/prometheus/client_model/go"
+)
+
+func NewMilvusRegistry() *MilvusRegistry {
+	r := &MilvusRegistry{
+		GoRegistry: prometheus.NewRegistry(),
+		CRegistry:  NewCRegistry(),
+	}
+
+	r.GoRegistry.MustRegister(prometheus.NewProcessCollector(prometheus.ProcessCollectorOpts{}))
+	r.GoRegistry.MustRegister(prometheus.NewGoCollector())
+
+	return r
+}
+
+// re-write the implementation of Gather()
+type MilvusRegistry struct {
+	GoRegistry *prometheus.Registry
+	CRegistry  *CRegistry
+}
+
+// Gather implements Gatherer.
+func (r *MilvusRegistry) Gather() ([]*dto.MetricFamily, error) {
+	var res []*dto.MetricFamily
+	res1, err := r.GoRegistry.Gather()
+	if err != nil {
+		return res, err
+	}
+	res2, err := r.CRegistry.Gather()
+	if err != nil {
+		return res, err
+	}
+	res = append(res1, res2...)
+	return res, nil
+}
