de571ae9f43b410d0640e132ac361f4e9b8cf125
zhenshan.cao <zhenshan.cao@zilliz.com>
2023-04-12 13:22:11 +0800

    Revert "Restrain gc files by collection (#22504) (#22518)"
    This reverts commit a58abe166562258051b7b987d9f897b8ee752e43.
    
    Signed-off-by: zhenshan.cao <zhenshan.cao@zilliz.com>

--- internal/datacoord/server.go
+++ internal/datacoord/server.go
@@ -416,18 +416,6 @@ func (s *Server) initGarbageCollection(cli storage.ChunkManager) {
 		checkInterval:    Params.DataCoordCfg.GCInterval,
 		missingTolerance: Params.DataCoordCfg.GCMissingTolerance,
 		dropTolerance:    Params.DataCoordCfg.GCDropTolerance,
-		collValidator: func(collID int64) bool {
-			resp, err := s.rootCoordClient.DescribeCollectionInternal(context.Background(), &milvuspb.DescribeCollectionRequest{
-				Base: commonpbutil.NewMsgBase(
-					commonpbutil.WithMsgType(commonpb.MsgType_DescribeCollection),
-				),
-				CollectionID: collID,
-			})
-			if err != nil {
-				log.Warn("failed to check collection id", zap.Int64("collID", collID), zap.Error(err))
-			}
-			return resp.GetStatus().GetErrorCode() == commonpb.ErrorCode_Success
-		},
 	})
 }
 
