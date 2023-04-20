03ec804e68a0db8b1954e114731c34a1ab7ccdfb
zhenshan.cao <zhenshan.cao@zilliz.com>
2023-04-21 07:08:32 +0800

    Change default shard number to 1 (#23586)
    Signed-off-by: zhenshan.cao <zhenshan.cao@zilliz.com>

--- tests/integration/upsert_test.go
+++ tests/integration/upsert_test.go
@@ -98,7 +98,7 @@ func TestUpsert(t *testing.T) {
 		DbName:         dbName,
 		CollectionName: collectionName,
 		Schema:         marshaledSchema,
-		ShardsNum:      2,
+		ShardsNum:      common.DefaultShardsNum,
 	})
 	assert.NoError(t, err)
 
