de0032a1b88889f748eed397d45b78a9dd8c64b4
xiaofan-luan <xiaofan.luan@zilliz.com>
2023-04-25 22:32:47 -0700

    Fix Has Collection Failed on datacoord
    Signed-off-by: xiaofan-luan <xiaofan.luan@zilliz.com>

--- pkg/common/error.go
+++ pkg/common/error.go
@@ -18,6 +18,7 @@ package common
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/cockroachdb/errors"
 	"github.com/milvus-io/milvus-proto/go-api/commonpb"
@@ -112,10 +113,12 @@ func IsCollectionNotExistError(e error) bool {
 	}
 	// cycle import: common -> funcutil -> types -> sessionutil -> common
 	// return funcutil.SliceContain(collectionNotExistCodes, statusError.GetErrorCode())
-	for _, code := range collectionNotExistCodes {
-		if code == statusError.GetErrorCode() {
-			return true
-		}
+	if statusError.Status.ErrorCode == commonpb.ErrorCode_CollectionNotExists {
+		return true
+	}
+
+	if (statusError.Status.ErrorCode == commonpb.ErrorCode_UnexpectedError) && strings.Contains(statusError.Status.Reason, "can't find collection") {
+		return true
 	}
 	return false
 }
