73c0f2fa1994ada36bee07faaf7364409f262b81
jaime <yun.zhang@zilliz.com>
2023-05-10 21:28:15 +0800

    Update proto API
    Signed-off-by: jaime <yun.zhang@zilliz.com>

--- go.mod
+++ go.mod
@@ -28,7 +28,7 @@ require (
 	github.com/klauspost/compress v1.14.2
 	github.com/lingdor/stackerror v0.0.0-20191119040541-976d8885ed76
 	github.com/mgutz/ansi v0.0.0-20200706080929-d51e80ef957d
-	github.com/milvus-io/milvus-proto/go-api v0.0.0-20230504042240-fb280598be89
+	github.com/milvus-io/milvus-proto/go-api v0.0.0-20230509062636-f6c495229ceb
 	github.com/minio/minio-go/v7 v7.0.17
 	github.com/opentracing/opentracing-go v1.2.0
 	github.com/panjf2000/ants/v2 v2.4.8
@@ -184,10 +184,7 @@ require (
 
 require github.com/cockroachdb/errors v1.2.4
 
-require (
-	github.com/aliyun/credentials-go v1.2.7
-	github.com/cockroachdb/errors v1.2.4
-)
+require github.com/aliyun/credentials-go v1.2.7
 
 require (
 	github.com/alibabacloud-go/debug v0.0.0-20190504072949-9472017b5c68 // indirect
--- internal/core/src/pb/common.pb.cc
+++ internal/core/src/pb/common.pb.cc
@@ -494,10 +494,10 @@ const char descriptor_table_protodef_common_2eproto[] PROTOBUF_SECTION_VARIABLE(
   "teNotLoad\020\001\022\024\n\020LoadStateLoading\020\002\022\023\n\017Loa"
   "dStateLoaded\020\003:^\n\021privilege_ext_obj\022\037.go"
   "ogle.protobuf.MessageOptions\030\351\007 \001(\0132!.mi"
-  "lvus.proto.common.PrivilegeExtBU\n\016io.mil"
+  "lvus.proto.common.PrivilegeExtBf\n\016io.mil"
   "vus.grpcB\013CommonProtoP\001Z1github.com/milv"
-  "us-io/milvus-proto/go-api/commonpb\240\001\001b\006p"
-  "roto3"
+  "us-io/milvus-proto/go-api/commonpb\240\001\001\252\002\016"
+  "IO.Milvus.Grpcb\006proto3"
   ;
 static const ::PROTOBUF_NAMESPACE_ID::internal::DescriptorTable*const descriptor_table_common_2eproto_deps[1] = {
   &::descriptor_table_google_2fprotobuf_2fdescriptor_2eproto,
@@ -518,7 +518,7 @@ static ::PROTOBUF_NAMESPACE_ID::internal::SCCInfoBase*const descriptor_table_com
 static ::PROTOBUF_NAMESPACE_ID::internal::once_flag descriptor_table_common_2eproto_once;
 static bool descriptor_table_common_2eproto_initialized = false;
 const ::PROTOBUF_NAMESPACE_ID::internal::DescriptorTable descriptor_table_common_2eproto = {
-  &descriptor_table_common_2eproto_initialized, descriptor_table_protodef_common_2eproto, "common.proto", 6285,
+  &descriptor_table_common_2eproto_initialized, descriptor_table_protodef_common_2eproto, "common.proto", 6302,
   &descriptor_table_common_2eproto_once, descriptor_table_common_2eproto_sccs, descriptor_table_common_2eproto_deps, 11, 1,
   schemas, file_default_instances, TableStruct_common_2eproto::offsets,
   file_level_metadata_common_2eproto, 11, file_level_enum_descriptors_common_2eproto, file_level_service_descriptors_common_2eproto,
