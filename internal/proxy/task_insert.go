1a48b27d80b608681c963e662f009ead693a36f8
cai.zhang <cai.zhang@zilliz.com>
2023-05-23 14:27:25 +0800

    Hide dynamic field for describing collection (#24312)
    Signed-off-by: cai.zhang <cai.zhang@zilliz.com>

--- internal/proxy/task_insert.go
+++ internal/proxy/task_insert.go
@@ -594,22 +594,17 @@ func (it *insertTask) verifyDynamicFieldData() error {
 }
 
 func (it *insertTask) checkDynamicFieldData() error {
-	var dataNameSet = typeutil.NewSet[string]()
-
 	for _, data := range it.FieldsData {
-		dataNameSet.Insert(data.GetFieldName())
-	}
-	if _, ok := dataNameSet[common.MetaFieldName]; ok {
-		return it.verifyDynamicFieldData()
+		if data.IsDynamic {
+			data.FieldName = common.MetaFieldName
+			return it.verifyDynamicFieldData()
+		}
 	}
 	defaultData := make([][]byte, it.NRows())
 	for i := range defaultData {
 		defaultData[i] = []byte("{}")
 	}
-	dynamicData, err := autoGenDynamicFieldData(defaultData)
-	if err != nil {
-		return err
-	}
+	dynamicData := autoGenDynamicFieldData(defaultData)
 	it.FieldsData = append(it.FieldsData, dynamicData)
 	return nil
 }
