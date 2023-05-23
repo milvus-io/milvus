411374abde4efecb86a4bfbe727b50576a1b5922
zhuwenxing <wenxing.zhu@zilliz.com>
2023-05-23 13:27:25 +0800

    [skip e2e]Add logger for deploy test (#24322)
    Signed-off-by: zhuwenxing <wenxing.zhu@zilliz.com>

--- tests/python_client/deploy/scripts/utils.py
+++ tests/python_client/deploy/scripts/utils.py
@@ -28,10 +28,12 @@ NUM_REPLICAS = 2
 
 def filter_collections_by_prefix(prefix):
     col_list = list_collections()
+    logger.info(f"all collections: {col_list}")
     res = []
     for col in col_list:
         if col.startswith(prefix):
             res.append(col)
+    logger.info(f"filtered collections with prefix {prefix}: {res}")
     return res
 
 
