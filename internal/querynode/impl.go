a350c997fd4e3092d263d49f2d93c547da995e36
chasingegg <chao.gao@zilliz.com>
2023-05-31 15:12:07 +0800

    Disable search params rewrite while searching with filter
    Signed-off-by: chasingegg <chao.gao@zilliz.com>

--- internal/querynode/impl.go
+++ internal/querynode/impl.go
@@ -926,11 +926,13 @@ func (node *QueryNode) searchWithDmlChannel(ctx context.Context, req *querypb.Se
 		}
 		switch plan.GetNode().(type) {
 		case *planpb.PlanNode_VectorAnns:
+			withFilter := (plan.GetVectorAnns().GetPredicates() != nil)
 			qinfo := plan.GetVectorAnns().GetQueryInfo()
 			paramMap := map[string]interface{}{
 				common.TopKKey:        qinfo.GetTopk(),
 				common.SearchParamKey: qinfo.GetSearchParams(),
 				common.SegmentNumKey:  estSegmentNum,
+				common.WithFilterKey:  withFilter,
 			}
 			err := node.queryHook.Run(paramMap)
 			if err != nil {
@@ -946,6 +948,7 @@ func (node *QueryNode) searchWithDmlChannel(ctx context.Context, req *querypb.Se
 				return failRet, nil
 			}
 			req.Req.SerializedExprPlan = SerializedExprPlan
+			log.Debug("optimized search params done", zap.Any("queryInfo", qinfo))
 		}
 	}
 
