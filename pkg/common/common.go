8fd31067e6635d4f3d8785eb9ca2dc702753cfbc
chasingegg <chao.gao@zilliz.com>
2023-06-02 16:08:47 +0800

    Disable search params rewrite while searching with filter
    Signed-off-by: chasingegg <chao.gao@zilliz.com>

--- pkg/common/common.go
+++ pkg/common/common.go
@@ -83,6 +83,7 @@ const (
 	TopKKey        = "topk"
 	SearchParamKey = "search_param"
 	SegmentNumKey  = "segment_num"
+	WithFilterKey  = "with_filter"
 
 	IndexParamsKey = "params"
 	IndexTypeKey   = "index_type"
