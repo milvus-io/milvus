86d346156de54247b7af70342e4ae8b864e7bf55
Congqi Xia <congqi.xia@zilliz.com>
2023-06-09 15:21:52 +0800

    Fix list milvus-proto go.mod when try to parse API_VERSION
    Signed-off-by: Congqi Xia <congqi.xia@zilliz.com>

--- scripts/download_milvus_proto.sh
+++ scripts/download_milvus_proto.sh
@@ -2,13 +2,13 @@
 
 SCRIPTS_DIR=$(dirname "$0")
 THIRD_PARTY_DIR=$SCRIPTS_DIR/../cmake_build/thirdparty
+API_VERSION=$(go list -m github.com/milvus-io/milvus-proto/go-api/v2 | awk -F' ' '{print $2}')
 
 if [ ! -d "$THIRD_PARTY_DIR/milvus-proto" ]; then
   mkdir -p $THIRD_PARTY_DIR
   pushd $THIRD_PARTY_DIR
   git clone https://github.com/milvus-io/milvus-proto.git
   cd milvus-proto
-  API_VERSION=$(go list -m github.com/milvus-io/milvus-proto/go-api/v2 | awk -F' ' '{print $2}')
   # try tagged version first
   COMMIT_ID=$(git ls-remote https://github.com/milvus-io/milvus-proto.git refs/tags/${API_VERSION} | cut -f 1)
   if [[ -z $COMMIT_ID ]]; then 
