93ee113c128d2170cc8846a5d99a713f065de77f
congqixia <congqi.xia@zilliz.com>
2023-06-09 13:10:35 +0800

    Fix update-api-version.sh for master branch (#24773)
    Signed-off-by: Congqi Xia <congqi.xia@zilliz.com>

--- scripts/download_milvus_proto.sh
+++ scripts/download_milvus_proto.sh
@@ -9,6 +9,7 @@ if [ ! -d "$THIRD_PARTY_DIR/milvus-proto" ]; then
   git clone https://github.com/milvus-io/milvus-proto.git
   cd milvus-proto
   API_VERSION=$(go list -m github.com/milvus-io/milvus-proto/go-api/v2 | awk -F' ' '{print $2}')
+  # try tagged version first
   COMMIT_ID=$(git ls-remote https://github.com/milvus-io/milvus-proto.git refs/tags/${API_VERSION} | cut -f 1)
   if [[ -z $COMMIT_ID ]]; then 
     # parse commit from pseudo version (eg v0.0.0-20230608062631-c453ef1b870a => c453ef1b870a)  
