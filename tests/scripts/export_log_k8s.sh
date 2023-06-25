0b8ac22f8f768720bb99551fa04cc79313c268cc
zhuwenxing <wenxing.zhu@zilliz.com>
2023-06-25 15:37:20 +0800

    [skip e2e]Add script to export goroutine
    Signed-off-by: zhuwenxing <wenxing.zhu@zilliz.com>

--- tests/scripts/export_log_k8s.sh
+++ tests/scripts/export_log_k8s.sh
@@ -49,3 +49,21 @@ kubectl logs $pod --previous -n ${ns_name} > ./$log_dir/pod_log_previous/$pod.lo
 kubectl describe pod $pod -n ${ns_name} > ./$log_dir/pod_describe/$pod.log 2>&1 || echo "describe pod $pod failed"
 done
 echo "export logs done"
+
+# export goroutine of all milvus pods
+array=($(kubectl get pod -n ${ns_name} -l "app.kubernetes.io/instance=${instance_name}, app.kubernetes.io/name=milvus"| awk 'NR == 1 {next} {print $1}'))
+
+echo ${array[@]}
+if [ ! -d $log_dir/goroutine ];
+then
+    mkdir -p $log_dir/goroutine
+fi
+echo "export goroutine start"
+for pod in ${array[*]}
+do
+echo "export goroutine for pod $pod "
+ip=($(kubectl get pod ${pod} -n ${ns_name} -o jsonpath='{.status.podIP}'))
+echo $ip
+curl "http://${ip}:9091/debug/pprof/goroutine?debug=10" -o ./$log_dir/goroutine/$pod.log || echo "export goroutine for pod $pod failed"
+done
+echo "export goroutine done"
