35dd2a058d77e0ba7bf7e77abd5d00ee5cecc1a7
aoiasd <zhicheng.yue@zilliz.com>
2023-06-29 14:45:16 +0800

    Cancel reduce rocksdb level for compatibility
    Signed-off-by: aoiasd <zhicheng.yue@zilliz.com>

--- internal/mq/mqimpl/rocksmq/server/rocksmq_impl.go
+++ internal/mq/mqimpl/rocksmq/server/rocksmq_impl.go
@@ -158,12 +158,8 @@ func NewRocksMQ(params paramtable.BaseTable, name string, idAllocator allocator.
 	bbto.SetBlockCache(gorocksdb.NewLRUCache(rocksDBLRUCacheCapacity))
 
 	optsKV := gorocksdb.NewDefaultOptions()
-	// L0:No Compression
-	// L1,L2: ZSTD
-	optsKV.SetNumLevels(3)
-	optsKV.SetCompressionPerLevel([]gorocksdb.CompressionType{0, 7, 7})
+	optsKV.SetCompressionPerLevel([]gorocksdb.CompressionType{0, 0, 0, 0, 7, 7, 7})
 	optsKV.SetBlockBasedTableFactory(bbto)
-	optsKV.SetTargetFileSizeMultiplier(2)
 	optsKV.SetCreateIfMissing(true)
 	// by default there are only 1 thread for flush compaction, which may block each other.
 	// increase to a reasonable thread numbers
@@ -181,10 +177,8 @@ func NewRocksMQ(params paramtable.BaseTable, name string, idAllocator allocator.
 	// finish rocks mq store initialization, rocks mq store has to set the prefix extractor
 	optsStore := gorocksdb.NewDefaultOptions()
 	// share block cache with kv
-	optsStore.SetNumLevels(3)
-	optsStore.SetCompressionPerLevel([]gorocksdb.CompressionType{0, 7, 7})
+	optsStore.SetCompressionPerLevel([]gorocksdb.CompressionType{0, 0, 0, 0, 7, 7, 7})
 	optsStore.SetBlockBasedTableFactory(bbto)
-	optsStore.SetTargetFileSizeMultiplier(2)
 	optsStore.SetCreateIfMissing(true)
 	// by default there are only 1 thread for flush compaction, which may block each other.
 	// increase to a reasonable thread numbers
