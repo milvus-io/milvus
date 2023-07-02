597a4d92279f87154d3ce179f1ca5c8000fe8152
congqixia <congqi.xia@zilliz.com>
2023-07-02 19:50:23 +0800

    Treat small segment without index as sealed (#25237)
    Signed-off-by: Congqi Xia <congqi.xia@zilliz.com>

--- internal/datacoord/server_test.go
+++ internal/datacoord/server_test.go
@@ -2039,6 +2039,7 @@ func TestGetQueryVChanPositions(t *testing.T) {
 			MsgGroup:    "",
 			Timestamp:   0,
 		},
+		NumOfRows: 2048,
 	}
 	err = svr.meta.AddSegment(NewSegmentInfo(s1))
 	assert.NoError(t, err)
@@ -2119,9 +2120,8 @@ func TestGetQueryVChanPositions(t *testing.T) {
 	t.Run("get existed channel", func(t *testing.T) {
 		vchan := svr.handler.GetQueryVChanPositions(&channel{Name: "ch1", CollectionID: 0}, allPartitionID)
 		assert.EqualValues(t, 1, len(vchan.FlushedSegmentIds))
-		assert.EqualValues(t, 1, vchan.FlushedSegmentIds[0])
+		assert.ElementsMatch(t, []int64{1}, vchan.FlushedSegmentIds)
 		assert.EqualValues(t, 2, len(vchan.UnflushedSegmentIds))
-		assert.ElementsMatch(t, []int64{s2.ID, s3.ID}, vchan.UnflushedSegmentIds)
 	})
 
 	t.Run("empty collection", func(t *testing.T) {
@@ -2211,14 +2211,15 @@ func TestGetQueryVChanPositions_Retrieve_unIndexed(t *testing.T) {
 				Timestamp:   1,
 			},
 			CompactionFrom: []int64{1, 2}, // c, d
+			NumOfRows:      2048,
 		}
 
 		err = svr.meta.AddSegment(NewSegmentInfo(e))
 		assert.NoError(t, err)
 		vchan := svr.handler.GetQueryVChanPositions(&channel{Name: "ch1", CollectionID: 0}, allPartitionID)
-		assert.EqualValues(t, 0, len(vchan.FlushedSegmentIds))
-		assert.EqualValues(t, 2, len(vchan.UnflushedSegmentIds))
-		assert.ElementsMatch(t, []int64{c.GetID(), d.GetID()}, vchan.UnflushedSegmentIds) // expected c, d
+		assert.EqualValues(t, 2, len(vchan.FlushedSegmentIds))
+		assert.EqualValues(t, 0, len(vchan.UnflushedSegmentIds))
+		assert.ElementsMatch(t, []int64{c.GetID(), d.GetID()}, vchan.FlushedSegmentIds) // expected c, d
 	})
 
 	t.Run("a GC-ed, bcde unIndexed", func(t *testing.T) {
@@ -2296,14 +2297,15 @@ func TestGetQueryVChanPositions_Retrieve_unIndexed(t *testing.T) {
 				Timestamp:   1,
 			},
 			CompactionFrom: []int64{1, 2}, // c, d
+			NumOfRows:      2048,
 		}
 
 		err = svr.meta.AddSegment(NewSegmentInfo(e))
 		assert.NoError(t, err)
 		vchan := svr.handler.GetQueryVChanPositions(&channel{Name: "ch1", CollectionID: 0}, allPartitionID)
-		assert.EqualValues(t, 0, len(vchan.FlushedSegmentIds))
-		assert.EqualValues(t, 2, len(vchan.UnflushedSegmentIds))
-		assert.ElementsMatch(t, []int64{c.GetID(), d.GetID()}, vchan.UnflushedSegmentIds) // expected c, d
+		assert.EqualValues(t, 2, len(vchan.FlushedSegmentIds))
+		assert.EqualValues(t, 0, len(vchan.UnflushedSegmentIds))
+		assert.ElementsMatch(t, []int64{c.GetID(), d.GetID()}, vchan.FlushedSegmentIds) // expected c, d
 	})
 
 	t.Run("ab GC-ed, c unIndexed, de indexed", func(t *testing.T) {
@@ -2376,6 +2378,7 @@ func TestGetQueryVChanPositions_Retrieve_unIndexed(t *testing.T) {
 				Timestamp:   1,
 			},
 			CompactionFrom: []int64{1, 2}, // c, d
+			NumOfRows:      2048,
 		}
 		err = svr.meta.AddSegment(NewSegmentInfo(e))
 		assert.NoError(t, err)
@@ -2919,12 +2922,12 @@ func TestGetRecoveryInfo(t *testing.T) {
 		})
 		assert.NoError(t, err)
 
-		seg1 := createSegment(9, 0, 0, 100, 30, "vchan1", commonpb.SegmentState_Dropped)
-		seg2 := createSegment(10, 0, 0, 100, 40, "vchan1", commonpb.SegmentState_Dropped)
-		seg3 := createSegment(11, 0, 0, 100, 40, "vchan1", commonpb.SegmentState_Dropped)
+		seg1 := createSegment(9, 0, 0, 2048, 30, "vchan1", commonpb.SegmentState_Dropped)
+		seg2 := createSegment(10, 0, 0, 2048, 40, "vchan1", commonpb.SegmentState_Dropped)
+		seg3 := createSegment(11, 0, 0, 2048, 40, "vchan1", commonpb.SegmentState_Dropped)
 		seg3.CompactionFrom = []int64{9, 10}
-		seg4 := createSegment(12, 0, 0, 100, 40, "vchan1", commonpb.SegmentState_Dropped)
-		seg5 := createSegment(13, 0, 0, 100, 40, "vchan1", commonpb.SegmentState_Flushed)
+		seg4 := createSegment(12, 0, 0, 2048, 40, "vchan1", commonpb.SegmentState_Dropped)
+		seg5 := createSegment(13, 0, 0, 2048, 40, "vchan1", commonpb.SegmentState_Flushed)
 		seg5.CompactionFrom = []int64{11, 12}
 		err = svr.meta.AddSegment(NewSegmentInfo(seg1))
 		assert.NoError(t, err)
@@ -2977,8 +2980,8 @@ func TestGetRecoveryInfo(t *testing.T) {
 		assert.NotNil(t, resp.GetChannels()[0].SeekPosition)
 		assert.NotEqual(t, 0, resp.GetChannels()[0].GetSeekPosition().GetTimestamp())
 		assert.Len(t, resp.GetChannels()[0].GetDroppedSegmentIds(), 0)
-		assert.ElementsMatch(t, []UniqueID{9, 10}, resp.GetChannels()[0].GetUnflushedSegmentIds())
-		assert.ElementsMatch(t, []UniqueID{12}, resp.GetChannels()[0].GetFlushedSegmentIds())
+		assert.ElementsMatch(t, []UniqueID{}, resp.GetChannels()[0].GetUnflushedSegmentIds())
+		assert.ElementsMatch(t, []UniqueID{9, 10, 12}, resp.GetChannels()[0].GetFlushedSegmentIds())
 	})
 
 	t.Run("with closed server", func(t *testing.T) {
