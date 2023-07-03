608a8287377ba1d1a0607af412573eb7bcab1c36
Jiquan Long <jiquan.long@zilliz.com>
2023-07-03 19:24:25 +0800

    Fix expression on integer overflow case (#24867, #24893, #24920, #24943) (#24990)
    Signed-off-by: longjiquan <jiquan.long@zilliz.com>
    Co-authored-by: yah01 <yang.cen@zilliz.com>

--- internal/core/src/query/Parser.cpp
+++ internal/core/src/query/Parser.cpp
@@ -101,6 +101,7 @@ Parser::ParseRangeNode(const Json& out_body) {
     switch (data_type) {
         case DataType::BOOL:
             return ParseRangeNodeImpl<bool>(field_name, body);
+
         case DataType::INT8:
             return ParseRangeNodeImpl<int8_t>(field_name, body);
         case DataType::INT16:
@@ -109,6 +110,7 @@ Parser::ParseRangeNode(const Json& out_body) {
             return ParseRangeNodeImpl<int32_t>(field_name, body);
         case DataType::INT64:
             return ParseRangeNodeImpl<int64_t>(field_name, body);
+
         case DataType::FLOAT:
             return ParseRangeNodeImpl<float>(field_name, body);
         case DataType::DOUBLE:
@@ -297,31 +299,42 @@ Parser::ParseRangeNodeImpl(const FieldName& field_name, const Json& body) {
             } else if constexpr (std::is_integral_v<T>) {
                 Assert(right_operand.is_number_integer());
                 Assert(value.is_number_integer());
+                // see also: https://github.com/milvus-io/milvus/issues/23646.
+                return std::make_unique<BinaryArithOpEvalRangeExprImpl<int64_t>>(
+                    ColumnInfo(schema.get_field_id(field_name), schema[field_name].get_data_type()),
+                    proto::plan::GenericValue::ValCase::kInt64Val, arith_op_mapping_.at(arith_op_name), right_operand,
+                    mapping_.at(op_name), value);
             } else if constexpr (std::is_floating_point_v<T>) {
                 Assert(right_operand.is_number());
                 Assert(value.is_number());
+                return std::make_unique<BinaryArithOpEvalRangeExprImpl<T>>(
+                    ColumnInfo(schema.get_field_id(field_name), schema[field_name].get_data_type()),
+                    proto::plan::GenericValue::ValCase::kFloatVal, arith_op_mapping_.at(arith_op_name), right_operand,
+                    mapping_.at(op_name), value);
             } else {
                 static_assert(always_false<T>, "unsupported type");
             }
-
-            return std::make_unique<BinaryArithOpEvalRangeExprImpl<T>>(
-                ColumnInfo(schema.get_field_id(field_name), schema[field_name].get_data_type()),
-                proto::plan::GenericValue::ValCase::VAL_NOT_SET, arith_op_mapping_.at(arith_op_name), right_operand,
-                mapping_.at(op_name), value);
         }
 
         if constexpr (std::is_same_v<T, bool>) {
             Assert(item.value().is_boolean());
+            return std::make_unique<UnaryRangeExprImpl<T>>(
+                ColumnInfo(schema.get_field_id(field_name), schema[field_name].get_data_type()), mapping_.at(op_name),
+                item.value(), proto::plan::GenericValue::ValCase::kBoolVal);
         } else if constexpr (std::is_integral_v<T>) {
             Assert(item.value().is_number_integer());
+            // see also: https://github.com/milvus-io/milvus/issues/23646.
+            return std::make_unique<UnaryRangeExprImpl<int64_t>>(
+                ColumnInfo(schema.get_field_id(field_name), schema[field_name].get_data_type()), mapping_.at(op_name),
+                item.value(), proto::plan::GenericValue::ValCase::kInt64Val);
         } else if constexpr (std::is_floating_point_v<T>) {
             Assert(item.value().is_number());
+            return std::make_unique<UnaryRangeExprImpl<T>>(
+                ColumnInfo(schema.get_field_id(field_name), schema[field_name].get_data_type()), mapping_.at(op_name),
+                item.value(), proto::plan::GenericValue::ValCase::kFloatVal);
         } else {
             static_assert(always_false<T>, "unsupported type");
         }
-        return std::make_unique<UnaryRangeExprImpl<T>>(
-            ColumnInfo(schema.get_field_id(field_name), schema[field_name].get_data_type()), mapping_.at(op_name),
-            item.value(), proto::plan::GenericValue::ValCase::VAL_NOT_SET);
     } else if (body.size() == 2) {
         bool has_lower_value = false;
         bool has_upper_value = false;
