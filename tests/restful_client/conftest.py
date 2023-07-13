e232bb5b8f3d1f96a36db72e2792282719f82b34
zhuwenxing <wenxing.zhu@zilliz.com>
2023-07-13 19:38:47 +0800

    [test]Add restful api test
    Signed-off-by: zhuwenxing <wenxing.zhu@zilliz.com>

---
+++ tests/restful_client/conftest.py
@@ -0,0 +1,30 @@
+import pytest
+import yaml
+
+
+def pytest_addoption(parser):
+    parser.addoption("--host", action="store", default="127.0.0.1", help="host")
+    parser.addoption("--port", action="store", default="19530", help="port")
+    parser.addoption("--username", action="store", default="root", help="email")
+    parser.addoption("--password", action="store", default="Milvus", help="password")
+
+
+@pytest.fixture
+def host(request):
+    return request.config.getoption("--host")
+
+
+@pytest.fixture
+def port(request):
+    return request.config.getoption("--port")
+
+
+@pytest.fixture
+def username(request):
+    return request.config.getoption("--username")
+
+
+@pytest.fixture
+def password(request):
+    return request.config.getoption("--password")
+
