b0f59b479a8be120db75e2899336af73ad0d3b37
yiwangdr <yiwangdr@gmail.com>
2023-07-12 19:47:10 -0700

    Move revision api to WatchKV
    Signed-off-by: yiwangdr <yiwangdr@gmail.com>

--- internal/kv/mocks/MetaKv.go
+++ internal/kv/mocks/MetaKv.go
@@ -49,60 +49,6 @@ func (_c *MetaKv_Close_Call) RunAndReturn(run func()) *MetaKv_Close_Call {
 	return _c
 }
 
-// CompareVersionAndSwap provides a mock function with given fields: key, version, target
-func (_m *MetaKv) CompareVersionAndSwap(key string, version int64, target string) (bool, error) {
-	ret := _m.Called(key, version, target)
-
-	var r0 bool
-	var r1 error
-	if rf, ok := ret.Get(0).(func(string, int64, string) (bool, error)); ok {
-		return rf(key, version, target)
-	}
-	if rf, ok := ret.Get(0).(func(string, int64, string) bool); ok {
-		r0 = rf(key, version, target)
-	} else {
-		r0 = ret.Get(0).(bool)
-	}
-
-	if rf, ok := ret.Get(1).(func(string, int64, string) error); ok {
-		r1 = rf(key, version, target)
-	} else {
-		r1 = ret.Error(1)
-	}
-
-	return r0, r1
-}
-
-// MetaKv_CompareVersionAndSwap_Call is a *mock.Call that shadows Run/Return methods with type explicit version for method 'CompareVersionAndSwap'
-type MetaKv_CompareVersionAndSwap_Call struct {
-	*mock.Call
-}
-
-// CompareVersionAndSwap is a helper method to define mock.On call
-//   - key string
-//   - version int64
-//   - target string
-func (_e *MetaKv_Expecter) CompareVersionAndSwap(key interface{}, version interface{}, target interface{}) *MetaKv_CompareVersionAndSwap_Call {
-	return &MetaKv_CompareVersionAndSwap_Call{Call: _e.mock.On("CompareVersionAndSwap", key, version, target)}
-}
-
-func (_c *MetaKv_CompareVersionAndSwap_Call) Run(run func(key string, version int64, target string)) *MetaKv_CompareVersionAndSwap_Call {
-	_c.Call.Run(func(args mock.Arguments) {
-		run(args[0].(string), args[1].(int64), args[2].(string))
-	})
-	return _c
-}
-
-func (_c *MetaKv_CompareVersionAndSwap_Call) Return(_a0 bool, _a1 error) *MetaKv_CompareVersionAndSwap_Call {
-	_c.Call.Return(_a0, _a1)
-	return _c
-}
-
-func (_c *MetaKv_CompareVersionAndSwap_Call) RunAndReturn(run func(string, int64, string) (bool, error)) *MetaKv_CompareVersionAndSwap_Call {
-	_c.Call.Return(run)
-	return _c
-}
-
 // GetPath provides a mock function with given fields: key
 func (_m *MetaKv) GetPath(key string) string {
 	ret := _m.Called(key)
