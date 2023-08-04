19a38bec9269c34d73605c4abf7eadf8d6ccb099
Congqi Xia <congqi.xia@zilliz.com>
2023-08-04 15:03:30 +0800

    Add unit test for channel store reload & metrics
    Signed-off-by: Congqi Xia <congqi.xia@zilliz.com>

--- internal/datacoord/channel_store_test.go
+++ internal/datacoord/channel_store_test.go
@@ -18,14 +18,19 @@ package datacoord
 
 import (
 	"fmt"
+	"strconv"
 	"testing"
 
+	"github.com/golang/protobuf/proto"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/suite"
 
 	"github.com/milvus-io/milvus/internal/kv"
 	"github.com/milvus-io/milvus/internal/kv/mocks"
 	"github.com/milvus-io/milvus/internal/proto/datapb"
+	"github.com/milvus-io/milvus/pkg/metrics"
+	"github.com/milvus-io/milvus/pkg/util/testutils"
 )
 
 func genNodeChannelInfos(id int64, num int) *NodeChannelInfo {
@@ -68,12 +73,7 @@ func genChannelOperations(from, to int64, num int) ChannelOpSet {
 
 func TestChannelStore_Update(t *testing.T) {
 	txnKv := mocks.NewTxnKV(t)
-	txnKv.On("MultiSaveAndRemove",
-		mock.Anything,
-		mock.Anything,
-	).Run(func(args mock.Arguments) {
-		saves := args.Get(0).(map[string]string)
-		removals := args.Get(1).([]string)
+	txnKv.EXPECT().MultiSaveAndRemove(mock.Anything, mock.Anything).Run(func(saves map[string]string, removals []string) {
 		assert.False(t, len(saves)+len(removals) > 128, "too many operations")
 	}).Return(nil)
 
@@ -115,4 +115,87 @@ func TestChannelStore_Update(t *testing.T) {
 			assert.Equal(t, tt.wantErr, err != nil)
 		})
 	}
+}
+
+type ChannelStoreReloadSuite struct {
+	testutils.PromMetricsSuite
+
+	mockTxn *mocks.TxnKV
+}
+
+func (suite *ChannelStoreReloadSuite) SetupTest() {
+	suite.mockTxn = mocks.NewTxnKV(suite.T())
+}
+
+func (suite *ChannelStoreReloadSuite) generateWatchInfo(name string, state datapb.ChannelWatchState) *datapb.ChannelWatchInfo {
+	return &datapb.ChannelWatchInfo{
+		Vchan: &datapb.VchannelInfo{
+			ChannelName: name,
+		},
+		State: state,
+	}
+}
+
+func (suite *ChannelStoreReloadSuite) TestReload() {
+	type item struct {
+		nodeID      int64
+		channelName string
+	}
+	type testCase struct {
+		tag    string
+		items  []item
+		expect map[int64]int
+	}
+
+	cases := []testCase{
+		{
+			tag:    "empty",
+			items:  []item{},
+			expect: map[int64]int{},
+		},
+		{
+			tag: "normal",
+			items: []item{
+				{nodeID: 1, channelName: "dml1_v0"},
+				{nodeID: 1, channelName: "dml2_v1"},
+				{nodeID: 2, channelName: "dml3_v0"},
+			},
+			expect: map[int64]int{1: 2, 2: 1},
+		},
+		{
+			tag: "buffer",
+			items: []item{
+				{nodeID: bufferID, channelName: "dml1_v0"},
+			},
+			expect: map[int64]int{bufferID: 1},
+		},
+	}
+
+	for _, tc := range cases {
+		suite.Run(tc.tag, func() {
+			suite.mockTxn.ExpectedCalls = nil
+
+			var keys, values []string
+			for _, item := range tc.items {
+				keys = append(keys, fmt.Sprintf("channel_store/%d/%s", item.nodeID, item.channelName))
+				info := suite.generateWatchInfo(item.channelName, datapb.ChannelWatchState_WatchSuccess)
+				bs, err := proto.Marshal(info)
+				suite.Require().NoError(err)
+				values = append(values, string(bs))
+			}
+			suite.mockTxn.EXPECT().LoadWithPrefix(mock.AnythingOfType("string")).Return(keys, values, nil)
+
+			store := NewChannelStore(suite.mockTxn)
+			err := store.Reload()
+			suite.Require().NoError(err)
+
+			for nodeID, expect := range tc.expect {
+				suite.MetricsEqual(metrics.DataCoordDmlChannelNum.WithLabelValues(strconv.FormatInt(nodeID, 10)), float64(expect))
+			}
+		})
+	}
+}
+
+func TestChannelStore(t *testing.T) {
+	suite.Run(t, new(ChannelStoreReloadSuite))
 }
