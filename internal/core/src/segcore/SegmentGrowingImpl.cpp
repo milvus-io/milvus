5cb34263a0e452c714fca8610f454c01274a9ec3
lixinguo <xinguo.li@zilliz.com>
2023-08-07 14:19:20 +0800

    Fix func typo
    Signed-off-by: lixinguo <xinguo.li@zilliz.com>

--- internal/core/src/segcore/SegmentGrowingImpl.cpp
+++ internal/core/src/segcore/SegmentGrowingImpl.cpp
@@ -343,7 +343,7 @@ SegmentGrowingImpl::bulk_subscript(FieldId field_id,
             return CreateScalarDataArrayFrom(output.data(), count, field_meta);
         }
         case DataType::INT8: {
-            FixedVector<bool> output(count);
+            FixedVector<int8_t> output(count);
             bulk_subscript_impl<int8_t>(
                 *vec_ptr, seg_offsets, count, output.data());
             return CreateScalarDataArrayFrom(output.data(), count, field_meta);
