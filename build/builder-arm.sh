d43b1b80fd95e8840cfbc7d633ecbeea91400c96
Bennu-Li <yunmei.li@zilliz.com>
2023-08-16 17:10:01 +0800

    Publish milvus image on arm64
    Signed-off-by: Bennu-Li <yunmei.li@zilliz.com>

---
+++ build/builder-arm.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# Absolute path to the toplevel milvus directory.
+toplevel=$(dirname "$(cd "$(dirname "${0}")"; pwd)")
+
+export OS_NAME="${OS_NAME:-ubuntu20.04}"
+
+pushd "${toplevel}"
+
+if [[ "${1-}" == "pull" ]]; then
+    docker-compose pull --ignore-pull-failures armbuilder
+    exit 0
+fi
+
+if [[ "${1-}" == "down" ]]; then
+    docker-compose down
+    exit 0
+fi
+
+# Attempt to run in the container with the same UID/GID as we have on the host,
+# as this results in the correct permissions on files created in the shared
+# volumes. This isn't always possible, however, as IDs less than 100 are
+# reserved by Debian, and IDs in the low 100s are dynamically assigned to
+# various system users and groups. To be safe, if we see a UID/GID less than
+# 500, promote it to 501. This is notably necessary on macOS Lion and later,
+# where administrator accounts are created with a GID of 20. This solution is
+# not foolproof, but it works well in practice.
+uid=$(id -u)
+gid=$(id -g)
+[ "$uid" -lt 500 ] && uid=501
+[ "$gid" -lt 500 ] && gid=$uid
+
+echo "-------------------- mkdir cache --------------------"
+mkdir -p "${DOCKER_VOLUME_DIRECTORY:-.docker}/arm64-${OS_NAME}-ccache"
+mkdir -p "${DOCKER_VOLUME_DIRECTORY:-.docker}/arm64-${OS_NAME}-go-mod"
+mkdir -p "${DOCKER_VOLUME_DIRECTORY:-.docker}/arm64-${OS_NAME}-vscode-extensions"
+mkdir -p "${DOCKER_VOLUME_DIRECTORY:-.docker}/arm64-${OS_NAME}-conan"
+chmod -R 777 "${DOCKER_VOLUME_DIRECTORY:-.docker}"
+
+docker-compose pull --ignore-pull-failures armbuilder
+if [[ "${CHECK_BUILDER:-}" == "1" ]]; then
+    docker-compose build armbuilder
+fi
+
+echo "id -u: ${id -u}"
+echo "uid: $uid"
+
+if [[ "$(id -u)" != "0" ]]; then
+    docker-compose run --no-deps --rm -u "$uid:$gid" armbuilder "$@"
+else
+    docker-compose run --no-deps --rm --entrypoint "/tini -- /entrypoint.sh" armbuilder "$@"
+fi
+
+popd
--- ci/jenkins/PublishImages.groovy
+++ ci/jenkins/PublishImages.groovy
@@ -74,9 +74,38 @@ pipeline {
                     steps {
                         script {
                             sh "pwd"
+                            sh "build/builder.sh /bin/bash -c \"make install\""
+
                             def date = sh(returnStdout: true, script: 'date +%Y%m%d').trim()
                             def gitShortCommit = sh(returnStdout: true, script: 'git rev-parse --short HEAD').trim()
                             echo "image tag: ${env.BRANCH_NAME}-${date}-${gitShortCommit}-arm64"
+
+                            withCredentials([usernamePassword(credentialsId: "${env.DOCKER_CREDENTIALS_ID}", usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
+                                    sh 'docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}'
+                                    sh """
+                                        export MILVUS_IMAGE_REPO="${env.TARGET_REPO}/milvus"
+                                        export MILVUS_HARBOR_IMAGE_REPO="${env.HARBOR_REPO}/milvus/milvus"
+                                        export MILVUS_IMAGE_TAG="${env.BRANCH_NAME}-${date}-${gitShortCommit}-arm64"
+                                        export BUILD_ARGS="--build-arg MILVUS_BASE_IMAGE_TAG=ubuntu20.04-arm-test"
+                                        build/build_image.sh
+                                        docker push \${MILVUS_IMAGE_REPO}:\${MILVUS_IMAGE_TAG}
+                                        docker tag \${MILVUS_IMAGE_REPO}:\${MILVUS_IMAGE_TAG} \${MILVUS_IMAGE_REPO}:${env.BRANCH_NAME}-latest-arm64
+                                        docker tag \${MILVUS_IMAGE_REPO}:\${MILVUS_IMAGE_TAG} \${MILVUS_HARBOR_IMAGE_REPO}:\${MILVUS_IMAGE_TAG}
+                                        docker push \${MILVUS_IMAGE_REPO}:${env.BRANCH_NAME}-latest-arm64
+                                        docker logout
+                                    """
+                                }
+
+                                withCredentials([usernamePassword(credentialsId: "${env.CI_DOCKER_CREDENTIAL_ID}", usernameVariable: 'CI_REGISTRY_USERNAME', passwordVariable: 'CI_REGISTRY_PASSWORD')]){
+                                    sh "docker login ${env.HARBOR_REPO} -u '${CI_REGISTRY_USERNAME}' -p '${CI_REGISTRY_PASSWORD}'"
+                                    sh """
+                                        export MILVUS_HARBOR_IMAGE_REPO="${env.HARBOR_REPO}/milvus/milvus"
+                                        export MILVUS_IMAGE_TAG="${env.BRANCH_NAME}-${date}-${gitShortCommit}-arm64"
+                                        docker push \${MILVUS_HARBOR_IMAGE_REPO}:\${MILVUS_IMAGE_TAG}
+                                        docker logout
+                                    """
+                                }
+
 
                         }
 
