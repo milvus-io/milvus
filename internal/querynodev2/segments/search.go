ba806429fa24a3334dc31cd47c5f46776db2c433
Xiaofan <83447078+xiaofan-luan@users.noreply.github.com>
2023-08-17 15:44:53 +0800

    Remove frequent log on search growing segment (#26315)
    Signed-off-by: xiaofan-luan <xiaofan.luan@zilliz.com>

--- internal/querynodev2/segments/search.go
+++ internal/querynodev2/segments/search.go
@@ -89,7 +89,7 @@ func searchSegments(ctx context.Context, segments []Segment, segType SegmentType
 	}
 
 	if len(segmentsWithoutIndex) > 0 {
-		log.Ctx(ctx).Info("search growing/sealed segments without indexes", zap.Int64s("segmentIDs", segmentsWithoutIndex))
+		log.Ctx(ctx).Debug("search growing/sealed segments without indexes", zap.Int64s("segmentIDs", segmentsWithoutIndex))
 	}
 
 	return searchResults, nil
