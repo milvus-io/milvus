7e3323a518fdc22b7c31a2be37f14ca1c22f6ea1
cqy123456 <39671710+cqy123456@users.noreply.github.com>
2023-08-17 20:26:20 +0800

    update dimension range of diskann. (#26405)
    Signed-off-by: cqy123456 <qianya.cheng@zilliz.com>

--- pkg/util/indexparamcheck/diskann_checker.go
+++ pkg/util/indexparamcheck/diskann_checker.go
@@ -9,13 +9,6 @@ func (c diskannChecker) StaticCheck(params map[string]string) error {
 	return c.staticCheck(params)
 }
 
-func (c diskannChecker) CheckTrain(params map[string]string) error {
-	if !CheckIntByRange(params, DIM, DiskAnnMinDim, DefaultMaxDim) {
-		return errOutOfRange(DIM, DiskAnnMinDim, DefaultMaxDim)
-	}
-	return c.StaticCheck(params)
-}
-
 func newDiskannChecker() IndexChecker {
 	return &diskannChecker{}
 }
