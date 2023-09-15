895b94ab6b4c18d60816d88fa0d9b088c22e0017
zhuwenxing <wenxing.zhu@zilliz.com>
2023-09-15 22:27:20 +0800

    [skip e2e]Fix get latest image with wrong arch and etcd followers chaos apply (#27144)
    Signed-off-by: zhuwenxing <wenxing.zhu@zilliz.com>

--- tests/scripts/get_image_tag_by_short_name.py
+++ tests/scripts/get_image_tag_by_short_name.py
@@ -3,7 +3,7 @@ import argparse
 from tenacity import retry, stop_after_attempt
 
 @retry(stop=stop_after_attempt(7))
-def get_image_tag_by_short_name(repository, tag):
+def get_image_tag_by_short_name(repository, tag, arch):
 
     # Send API request to get all tags start with prefix
     # ${branch}-latest  means the tag is a dev build
@@ -28,8 +28,15 @@ def get_image_tag_by_short_name(repository, tag):
         if "digest" in tag_info["images"][0] and tag_info["images"][0]["digest"] == digest:
             # Extract the image name
             image_name = tag_info["name"].split(":")[0]
-            if image_name != tag:
+            if image_name != tag and arch in image_name:
                 res.append(image_name)
+    # In case of no match, try to find the latest tag with the same arch
+    # there is a case: push master-xxx-arm64 and master-latest, but master-latest-amd64 is not pushed,
+    # then there will be no tag matched, so we need to find the latest tag with the same arch even it is not the latest tag
+    for tag_info in data["results"]:
+        image_name = tag_info["name"].split(":")[0]
+        if image_name != tag and arch in image_name:
+            res.append(image_name)
     assert len(res) > 0
     return res[0]
 
@@ -37,6 +44,7 @@ if __name__ == "__main__":
     argparse = argparse.ArgumentParser()
     argparse.add_argument("--repository", type=str, default="milvusdb/milvus")
     argparse.add_argument("--tag", type=str, default="master-latest")
+    argparse.add_argument("--arch", type=str, default="amd64")
     args = argparse.parse_args()
-    res = get_image_tag_by_short_name(args.repository, args.tag)
+    res = get_image_tag_by_short_name(args.repository, args.tag, args.arch)
     print(res)
