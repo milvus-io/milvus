bf7f32b0251d9bf2d786f39cb6955eb604bd3990
jaime <yun.zhang@zilliz.com>
2023-10-26 10:24:09 +0800

    Refine Proxy stop (#27919)
    Signed-off-by: jaime <yun.zhang@zilliz.com>

--- internal/distributed/proxy/service.go
+++ internal/distributed/proxy/service.go
@@ -630,6 +630,7 @@ func (s *Server) start() error {
 func (s *Server) Stop() error {
 	Params := &paramtable.Get().ProxyGrpcServerCfg
 	log.Debug("Proxy stop", zap.String("internal address", Params.GetInternalAddress()), zap.String("external address", Params.GetInternalAddress()))
+	s.proxy.UpdateStateCode(commonpb.StateCode_Abnormal)
 	var err error
 
 	if s.etcdCli != nil {
