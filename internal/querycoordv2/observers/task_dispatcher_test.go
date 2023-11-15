2a31313dceb63f190c269c5804fe715e0d62f4eb
Congqi Xia <congqi.xia@zilliz.com>
2023-11-15 23:45:56 +0800

    Make qcv2 observer dispatcher execute exactly once
    See also #28466
    
    In `taskDispatcher.schedule`, same task may be resubmitted
    if the previous round did not finish
    In this case, TaskObserver.check may set current target by mistake,
    which may cause the random search/query failure
    
    Signed-off-by: Congqi Xia <congqi.xia@zilliz.com>

---
+++ internal/querycoordv2/observers/task_dispatcher_test.go
@@ -0,0 +1,46 @@
+package observers
+
+import (
+	"context"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/suite"
+
+	"github.com/milvus-io/milvus/pkg/util/paramtable"
+	"github.com/milvus-io/milvus/pkg/util/typeutil"
+)
+
+type taskDispatcherSuite struct {
+	suite.Suite
+}
+
+func (s *taskDispatcherSuite) SetupSuite() {
+	paramtable.Get().Init(paramtable.NewBaseTable())
+}
+
+func (s *taskDispatcherSuite) TestMultipleSubmit() {
+	var wg sync.WaitGroup
+	wg.Add(6)
+
+	set := typeutil.NewConcurrentSet[int64]()
+	dispatcher := newTaskDispatcher(func(ctx context.Context, key int64) {
+		defer wg.Done()
+		set.Insert(key)
+		time.Sleep(time.Second)
+	})
+	dispatcher.Start()
+
+	dispatcher.AddTask(1, 2, 3, 4, 5)
+	dispatcher.AddTask(2, 3, 4, 5, 6)
+	wg.Wait()
+
+	s.ElementsMatch([]int64{1, 2, 3, 4, 5, 6}, set.Collect())
+
+	dispatcher.Stop()
+}
+
+func TestTaskDispatcher(t *testing.T) {
+	suite.Run(t, new(taskDispatcherSuite))
+}
