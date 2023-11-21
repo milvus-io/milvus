a9e012ef71f69363f1c04e4edb58eb92747129a9
jaime <yun.zhang@zilliz.com>
2023-11-21 21:32:35 +0800

    Add logs for each step during service initialization
    Signed-off-by: jaime <yun.zhang@zilliz.com>

--- internal/datanode/data_node.go
+++ internal/datanode/data_node.go
@@ -257,8 +257,8 @@ func (node *DataNode) Init() error {
 		node.allocator = alloc
 
 		node.factory.Init(Params)
-		log.Info("DataNode server init succeeded",
-			zap.String("MsgChannelSubName", Params.CommonCfg.DataNodeSubName.GetValue()))
+		log.Info("init datanode done", zap.String("MsgChannelSubName", Params.CommonCfg.DataNodeSubName.GetValue()),
+			zap.Int64("nodeID", paramtable.GetNodeID()), zap.String("Address", node.address))
 	})
 	return initError
 }
--- internal/rootcoord/root_coord.go
+++ internal/rootcoord/root_coord.go
@@ -471,6 +471,8 @@ func (c *Core) initInternal() error {
 	c.factory.Init(Params)
 	chanMap := c.meta.ListCollectionPhysicalChannels()
 	c.chanTimeTick = newTimeTickSync(c.ctx, c.session.ServerID, c.factory, chanMap)
+	log.Info("create TimeTick sync done")
+
 	c.proxyClientManager = newProxyClientManager(c.proxyCreator)
 
 	c.broker = newServerBroker(c)
@@ -486,6 +488,7 @@ func (c *Core) initInternal() error {
 	)
 	c.proxyManager.AddSessionFunc(c.chanTimeTick.addSession, c.proxyClientManager.AddProxyClient)
 	c.proxyManager.DelSessionFunc(c.chanTimeTick.delSession, c.proxyClientManager.DelProxyClient)
+	log.Info("init proxy manager done")
 
 	c.metricsCacheManager = metricsinfo.NewMetricsCacheManager()
 
@@ -495,15 +498,18 @@ func (c *Core) initInternal() error {
 	if err := c.initImportManager(); err != nil {
 		return err
 	}
+	log.Info("init import manager done")
 
 	if err := c.initCredentials(); err != nil {
 		return err
 	}
+	log.Info("init credentials done")
 
 	if err := c.initRbac(); err != nil {
 		return err
 	}
 
+	log.Info("init rootcoord done", zap.Int64("nodeID", paramtable.GetNodeID()), zap.String("Address", c.address))
 	return nil
 }
 
@@ -673,7 +679,7 @@ func (c *Core) startInternal() error {
 			if err := c.proxyClientManager.RefreshPolicyInfoCache(c.ctx, &proxypb.RefreshPolicyInfoCacheRequest{
 				OpType: int32(typeutil.CacheRefresh),
 			}); err != nil {
-				log.Info("fail to refresh policy info cache", zap.Error(err))
+				log.RatedWarn(60, "fail to refresh policy info cache", zap.Error(err))
 				return err
 			}
 			return nil
