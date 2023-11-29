8c1a8736fb21cc78bf504bde6d2d32e15008bdcc
yah01 <yah2er0ne@outlook.com>
2023-11-29 11:54:43 +0800

    fix compile error
    Signed-off-by: yah01 <yah2er0ne@outlook.com>

--- internal/core/src/common/QueryResult.h
+++ internal/core/src/common/QueryResult.h
@@ -40,7 +40,7 @@ struct SearchResult {
         }
         AssertInfo(topk_per_nq_prefix_sum_.size() == total_nq_ + 1,
                    "wrong topk_per_nq_prefix_sum_ size {}",
-                   topk_per_nq_prefix_sum_);
+                   topk_per_nq_prefix_sum_.size());
         return topk_per_nq_prefix_sum_[total_nq_];
     }
 
