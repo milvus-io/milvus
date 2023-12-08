9facc860a74ea97947ebee0fa9f06c56edac9aa1
yah01 <yah2er0ne@outlook.com>
2023-12-08 22:31:58 +0800

    enhance: remove merger for load segments (#29062) (#29078)
    remove merger as now QueryNode could load segments concurrently
    fix https://github.com/milvus-io/milvus/issues/29063
    pr: #29062
    
    ---------
    
    Signed-off-by: yah01 <yah2er0ne@outlook.com>

--- pkg/util/paramtable/component_param.go
+++ pkg/util/paramtable/component_param.go
@@ -1189,8 +1189,10 @@ type queryCoordConfig struct {
 	// Deprecated: Since 2.2.0
 	RetryNum ParamItem `refreshable:"true"`
 	// Deprecated: Since 2.2.0
-	RetryInterval    ParamItem `refreshable:"true"`
-	TaskMergeCap     ParamItem `refreshable:"false"`
+	RetryInterval ParamItem `refreshable:"true"`
+	// Deprecated: Since 2.3.4
+	TaskMergeCap ParamItem `refreshable:"false"`
+
 	TaskExecutionCap ParamItem `refreshable:"true"`
 
 	// ---- Handoff ---
@@ -1258,7 +1260,7 @@ func (p *queryCoordConfig) init(base *BaseTable) {
 	p.TaskMergeCap = ParamItem{
 		Key:          "queryCoord.taskMergeCap",
 		Version:      "2.2.0",
-		DefaultValue: "16",
+		DefaultValue: "1",
 		Export:       true,
 	}
 	p.TaskMergeCap.Init(base.mgr)
