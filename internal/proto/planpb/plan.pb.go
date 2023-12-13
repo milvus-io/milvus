b01e30b85e155cf4fe3430bd60e458ffde6bdb97
Congqi Xia <congqi.xia@zilliz.com>
2023-12-14 00:00:33 +0800

    enhance: Add http method to control datacoord garbage collection
    See also #29051
    
    Signed-off-by: Congqi Xia <congqi.xia@zilliz.com>

--- internal/proto/planpb/plan.pb.go
+++ internal/proto/planpb/plan.pb.go
@@ -237,7 +237,6 @@ func (BinaryExpr_BinaryOp) EnumDescriptor() ([]byte, []int) {
 
 type GenericValue struct {
 	// Types that are valid to be assigned to Val:
-	//
 	//	*GenericValue_BoolVal
 	//	*GenericValue_Int64Val
 	//	*GenericValue_FloatVal
@@ -1298,7 +1297,6 @@ var xxx_messageInfo_AlwaysTrueExpr proto.InternalMessageInfo
 
 type Expr struct {
 	// Types that are valid to be assigned to Expr:
-	//
 	//	*Expr_TermExpr
 	//	*Expr_UnaryExpr
 	//	*Expr_BinaryExpr
@@ -1670,7 +1668,6 @@ func (m *QueryPlanNode) GetLimit() int64 {
 
 type PlanNode struct {
 	// Types that are valid to be assigned to Node:
-	//
 	//	*PlanNode_VectorAnns
 	//	*PlanNode_Predicates
 	//	*PlanNode_Query
