10f41994e14762fa5544dc3c132dd90efe1d9890
bigsheeper <yihao.dai@zilliz.com>
2023-12-14 17:12:57 +0800

    update
    Signed-off-by: bigsheeper <yihao.dai@zilliz.com>

--- scripts/run_go_unittest.sh
+++ scripts/run_go_unittest.sh
@@ -109,6 +109,7 @@ go test -race -cover -tags dynamic "${PKG_DIR}/util/retry/..." -failfast -count=
 go test -race -cover -tags dynamic "${MILVUS_DIR}/util/sessionutil/..." -failfast -count=1  -ldflags="-r ${RPATH}"
 go test -race -cover -tags dynamic "${MILVUS_DIR}/util/typeutil/..." -failfast -count=1  -ldflags="-r ${RPATH}"
 go test -race -cover -tags dynamic "${MILVUS_DIR}/util/importutil/..." -failfast -count=1  -ldflags="-r ${RPATH}"
+go test -race -cover -tags dynamic "${MILVUS_DIR}/util/importutilv2/..." -failfast -count=1  -ldflags="-r ${RPATH}"
 }
 
 function test_pkg()
