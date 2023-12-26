6e2a65ae6da857e3a465d30978955448498ff81a
Congqi Xia <congqi.xia@zilliz.com>
2023-12-26 11:25:37 +0800

    fix: Add scope limit for querynode DeleteRequest
    See also #27515
    
    When Delegator processes delete data, it forwards delete data with only
    segment id specified. When two segments has same segment id but one is
    growing and the other is sealed, the delete will be applied to both
    segments which causes delete data out of order when concurrent load
    segment occurs.
    
    Signed-off-by: Congqi Xia <congqi.xia@zilliz.com>

--- internal/querynodev2/services_test.go
+++ internal/querynodev2/services_test.go
@@ -1968,6 +1968,7 @@ func (suite *ServiceSuite) TestDelete_Int64() {
 		SegmentId:    suite.validSegmentIDs[0],
 		VchannelName: suite.vchannel,
 		Timestamps:   []uint64{0},
+		Scope:        querypb.DataScope_Historical,
 	}
 
 	// type int
@@ -2041,9 +2042,15 @@ func (suite *ServiceSuite) TestDelete_Failed() {
 		},
 	}
 
+	// segment not found
+	req.Scope = querypb.DataScope_Streaming
+	status, err := suite.node.Delete(ctx, req)
+	suite.NoError(err)
+	suite.False(merr.Ok(status))
+
 	// target not match
 	req.Base.TargetID = -1
-	status, err := suite.node.Delete(ctx, req)
+	status, err = suite.node.Delete(ctx, req)
 	suite.NoError(err)
 	suite.Equal(commonpb.ErrorCode_NodeIDNotMatch, status.GetErrorCode())
 
