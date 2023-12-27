eb11b1a56e93789d21121615fb3910decad79ced
congqixia <congqi.xia@zilliz.com>
2023-12-27 23:46:46 +0800

    enhance: [Cherry-pick] remove flushed segmentInfo in WatchChannelRequest (#29527)
    Cherry-pick from master
    pr: #29526
    `WatchDmChannel` only need growing segment info, this PR removes fetch
    segmentInfos when fill watch dml channel request.
    
    ---------
    
    Signed-off-by: Congqi Xia <congqi.xia@zilliz.com>

--- internal/querycoordv2/task/utils.go
+++ internal/querycoordv2/task/utils.go
@@ -185,7 +185,6 @@ func fillSubChannelRequest(
 ) error {
 	segmentIDs := typeutil.NewUniqueSet()
 	for _, vchannel := range req.GetInfos() {
-		segmentIDs.Insert(vchannel.GetFlushedSegmentIds()...)
 		segmentIDs.Insert(vchannel.GetUnflushedSegmentIds()...)
 	}
 
