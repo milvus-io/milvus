milvus-io/milvus

commit 277849a9152b35a2ff27e4e6d55a516308464ca9

enhance: separate serializer logic from sync task  (#29413)
See also #27675

Since serialization segment buffer does not related to sync manager can
shall be done before submit into sync manager. So that the pk statistic
file could be more accurate and reduce complex logic inside sync
manager.

---------

Signed-off-by: Congqi Xia <congqi.xia@zilliz.com>

internal/datanode/syncmgr/storage_serializer.go
@@ -1,0 +1,205 @@
+// Licensed to the LF AI & Data foundation under one
+// or more contributor license agreements. See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership. The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License. You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package syncmgr
+
+import (
+	"context"
+	"strconv"
+
+	"github.com/samber/lo"
+	"go.uber.org/zap"
+
+	"github.com/milvus-io/milvus-proto/go-api/v2/schemapb"
+	"github.com/milvus-io/milvus/internal/datanode/metacache"
+	"github.com/milvus-io/milvus/internal/proto/etcdpb"
+	"github.com/milvus-io/milvus/internal/storage"
+	"github.com/milvus-io/milvus/pkg/log"
+	"github.com/milvus-io/milvus/pkg/util/merr"
+)
+
+type storageV1Serializer struct {
+	collectionID int64
+	schema       *schemapb.CollectionSchema
+	pkField      *schemapb.FieldSchema
+
+	inCodec  *storage.InsertCodec
+	delCodec *storage.DeleteCodec
+
+	metacache  metacache.MetaCache
+	metaWriter MetaWriter
+}
+
+func NewStorageSerializer(metacache metacache.MetaCache, metaWriter MetaWriter) (*storageV1Serializer, error) {
+	collectionID := metacache.Collection()
+	schema := metacache.Schema()
+	pkField := lo.FindOrElse(schema.GetFields(), nil, func(field *schemapb.FieldSchema) bool { return field.GetIsPrimaryKey() })
+	if pkField == nil {
+		return nil, merr.WrapErrServiceInternal("cannot find pk field")
+	}
+	meta := &etcdpb.CollectionMeta{
+		Schema: schema,
+		ID:     collectionID,
+	}
+	inCodec := storage.NewInsertCodecWithSchema(meta)
+	return &storageV1Serializer{
+		collectionID: collectionID,
+		schema:       schema,
+		pkField:      pkField,
+
+		inCodec:    inCodec,
+		delCodec:   storage.NewDeleteCodec(),
+		metacache:  metacache,
+		metaWriter: metaWriter,
+	}, nil
+}
+
+func (s *storageV1Serializer) EncodeBuffer(ctx context.Context, pack *SyncPack) (Task, error) {
+	task := NewSyncTask()
+
+	log := log.Ctx(ctx).With(
+		zap.Int64("segmentID", pack.segmentID),
+		zap.Int64("collectionID", pack.collectionID),
+		zap.String("channel", pack.channelName),
+	)
+
+	if pack.insertData != nil {
+		memSize := make(map[int64]int64)
+		for fieldID, fieldData := range pack.insertData.Data {
+			memSize[fieldID] = int64(fieldData.GetMemorySize())
+		}
+		task.binlogMemsize = memSize
+
+		binlogBlobs, err := s.serializeBinlog(ctx, pack)
+		if err != nil {
+			log.Warn("failed to serialize binlog", zap.Error(err))
+			return nil, err
+		}
+		task.binlogBlobs = binlogBlobs
+
+		singlePKStats, batchStatsBlob, err := s.serializeStatslog(pack)
+		if err != nil {
+			log.Warn("failed to serialized statslog", zap.Error(err))
+			return nil, err
+		}
+
+		task.batchStatsBlob = batchStatsBlob
+		s.metacache.UpdateSegments(metacache.RollStats(singlePKStats), metacache.WithSegmentIDs(pack.segmentID))
+	}
+
+	if pack.isFlush {
+		mergedStatsBlob, err := s.serializeMergedPkStats(pack)
+		if err != nil {
+			log.Warn("failed to serialize merged stats log", zap.Error(err))
+			return nil, err
+		}
+
+		task.mergedStatsBlob = mergedStatsBlob
+		task.WithFlush()
+	}
+
+	if pack.deltaData != nil {
+		deltaBlob, err := s.serializeDeltalog(pack)
+		if err != nil {
+			log.Warn("failed to serialize delta log", zap.Error(err))
+			return nil, err
+		}
+		task.deltaBlob = deltaBlob
+		task.deltaRowCount = pack.deltaData.RowCount
+	}
+	if pack.isDrop {
+		task.WithDrop()
+	}
+
+	s.setTaskMeta(task, pack)
+	return task, nil
+}
+
+func (s *storageV1Serializer) setTaskMeta(task *SyncTask, pack *SyncPack) {
+	task.WithCollectionID(pack.collectionID).
+		WithPartitionID(pack.partitionID).
+		WithChannelName(pack.channelName).
+		WithSegmentID(pack.segmentID).
+		WithBatchSize(pack.batchSize).
+		WithSchema(s.metacache.Schema()).
+		WithStartPosition(pack.startPosition).
+		WithCheckpoint(pack.checkpoint).
+		WithLevel(pack.level).
+		WithTimeRange(pack.tsFrom, pack.tsTo).
+		WithMetaCache(s.metacache).
+		WithMetaWriter(s.metaWriter).
+		WithFailureCallback(func(err error) {
+			// TODO could change to unsub channel in the future
+			panic(err)
+		})
+}
+
+func (s *storageV1Serializer) serializeBinlog(ctx context.Context, pack *SyncPack) (map[int64]*storage.Blob, error) {
+	blobs, err := s.inCodec.Serialize(pack.partitionID, pack.segmentID, pack.insertData)
+	if err != nil {
+		return nil, err
+	}
+
+	result := make(map[int64]*storage.Blob)
+	for _, blob := range blobs {
+		fieldID, err := strconv.ParseInt(blob.GetKey(), 10, 64)
+		if err != nil {
+			log.Error("serialize buffer failed ... cannot parse string to fieldID ..", zap.Error(err))
+			return nil, err
+		}
+
+		result[fieldID] = blob
+	}
+	return result, nil
+}
+
+func (s *storageV1Serializer) serializeStatslog(pack *SyncPack) (*storage.PrimaryKeyStats, *storage.Blob, error) {
+	pkFieldData := pack.insertData.Data[s.pkField.GetFieldID()]
+	rowNum := int64(pkFieldData.RowNum())
+
+	stats, err := storage.NewPrimaryKeyStats(s.pkField.GetFieldID(), int64(s.pkField.GetDataType()), rowNum)
+	if err != nil {
+		return nil, nil, err
+	}
+	stats.UpdateByMsgs(pkFieldData)
+
+	blob, err := s.inCodec.SerializePkStats(stats, pack.batchSize)
+	if err != nil {
+		return nil, nil, err
+	}
+	return stats, blob, nil
+}
+
+func (s *storageV1Serializer) serializeMergedPkStats(pack *SyncPack) (*storage.Blob, error) {
+	segment, ok := s.metacache.GetSegmentByID(pack.segmentID)
+	if !ok {
+		return nil, merr.WrapErrSegmentNotFound(pack.segmentID)
+	}
+
+	return s.inCodec.SerializePkStatsList(lo.Map(segment.GetHistory(), func(pks *storage.PkStatistics, _ int) *storage.PrimaryKeyStats {
+		return &storage.PrimaryKeyStats{
+			FieldID: s.pkField.GetFieldID(),
+			MaxPk:   pks.MaxPK,
+			MinPk:   pks.MinPK,
+			BF:      pks.PkFilter,
+			PkType:  int64(s.pkField.GetDataType()),
+		}
+	}), segment.NumOfRows())
+}
+
+func (s *storageV1Serializer) serializeDeltalog(pack *SyncPack) (*storage.Blob, error) {
+	return s.delCodec.Serialize(pack.collectionID, pack.partitionID, pack.segmentID, pack.deltaData)
+}

internal/datanode/syncmgr/storage_v2_serializer.go
@@ -1,0 +1,246 @@
+// Licensed to the LF AI & Data foundation under one
+// or more contributor license agreements. See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership. The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License. You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package syncmgr
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/apache/arrow/go/v12/arrow"
+	"github.com/apache/arrow/go/v12/arrow/array"
+	"github.com/apache/arrow/go/v12/arrow/memory"
+	"go.uber.org/zap"
+
+	"github.com/milvus-io/milvus-proto/go-api/v2/schemapb"
+	milvus_storage "github.com/milvus-io/milvus-storage/go/storage"
+	"github.com/milvus-io/milvus-storage/go/storage/options"
+	"github.com/milvus-io/milvus-storage/go/storage/schema"
+	"github.com/milvus-io/milvus/internal/datanode/metacache"
+	"github.com/milvus-io/milvus/internal/querycoordv2/params"
+	"github.com/milvus-io/milvus/internal/storage"
+	iTypeutil "github.com/milvus-io/milvus/internal/util/typeutil"
+	"github.com/milvus-io/milvus/pkg/common"
+	"github.com/milvus-io/milvus/pkg/log"
+	"github.com/milvus-io/milvus/pkg/util/merr"
+	"github.com/milvus-io/milvus/pkg/util/typeutil"
+)
+
+type storageV2Serializer struct {
+	*storageV1Serializer
+
+	arrowSchema    *arrow.Schema
+	storageV2Cache *metacache.StorageV2Cache
+	inCodec        *storage.InsertCodec
+	metacache      metacache.MetaCache
+}
+
+func NewStorageV2Serializer(
+	storageV2Cache *metacache.StorageV2Cache,
+	metacache metacache.MetaCache,
+	metaWriter MetaWriter,
+) (*storageV2Serializer, error) {
+	v1Serializer, err := NewStorageSerializer(metacache, metaWriter)
+	if err != nil {
+		return nil, err
+	}
+
+	return &storageV2Serializer{
+		storageV1Serializer: v1Serializer,
+		storageV2Cache:      storageV2Cache,
+		arrowSchema:         storageV2Cache.ArrowSchema(),
+		metacache:           metacache,
+	}, nil
+}
+
+func (s *storageV2Serializer) EncodeBuffer(ctx context.Context, pack *SyncPack) (Task, error) {
+	task := NewSyncTaskV2()
+
+	space, err := s.storageV2Cache.GetOrCreateSpace(pack.segmentID, SpaceCreatorFunc(pack.segmentID, s.schema, s.arrowSchema))
+	if err != nil {
+		log.Warn("failed to get or create space", zap.Error(err))
+		return nil, err
+	}
+
+	task.space = space
+	if pack.insertData != nil {
+		insertReader, err := s.serializeInsertData(pack)
+		if err != nil {
+			log.Warn("failed to serialize insert data with storagev2", zap.Error(err))
+			return nil, err
+		}
+
+		task.reader = insertReader
+
+		singlePKStats, batchStatsBlob, err := s.serializeStatslog(pack)
+		if err != nil {
+			log.Warn("failed to serialized statslog", zap.Error(err))
+			return nil, err
+		}
+
+		task.batchStatsBlob = batchStatsBlob
+		s.metacache.UpdateSegments(metacache.RollStats(singlePKStats), metacache.WithSegmentIDs(pack.segmentID))
+	}
+
+	if pack.isFlush {
+		mergedStatsBlob, err := s.serializeMergedPkStats(pack)
+		if err != nil {
+			log.Warn("failed to serialize merged stats log", zap.Error(err))
+			return nil, err
+		}
+
+		task.mergedStatsBlob = mergedStatsBlob
+		task.WithFlush()
+	}
+
+	if pack.deltaData != nil {
+		deltaReader, err := s.serializeDeltaData(pack)
+		if err != nil {
+			log.Warn("failed to serialize delta data", zap.Error(err))
+			return nil, err
+		}
+		task.deleteReader = deltaReader
+	}
+
+	if pack.isDrop {
+		task.WithDrop()
+	}
+
+	s.setTaskMeta(task, pack)
+	return task, nil
+}
+
+func (s *storageV2Serializer) setTaskMeta(task *SyncTaskV2, pack *SyncPack) {
+	task.WithCollectionID(pack.collectionID).
+		WithPartitionID(pack.partitionID).
+		WithChannelName(pack.channelName).
+		WithSegmentID(pack.segmentID).
+		WithBatchSize(pack.batchSize).
+		WithSchema(s.metacache.Schema()).
+		WithStartPosition(pack.startPosition).
+		WithCheckpoint(pack.checkpoint).
+		WithLevel(pack.level).
+		WithTimeRange(pack.tsFrom, pack.tsTo).
+		WithMetaCache(s.metacache).
+		WithMetaWriter(s.metaWriter).
+		WithFailureCallback(func(err error) {
+			// TODO could change to unsub channel in the future
+			panic(err)
+		})
+}
+
+func (s *storageV2Serializer) serializeInsertData(pack *SyncPack) (array.RecordReader, error) {
+	builder := array.NewRecordBuilder(memory.DefaultAllocator, s.arrowSchema)
+	defer builder.Release()
+
+	if err := buildRecord(builder, pack.insertData, s.schema.GetFields()); err != nil {
+		return nil, err
+	}
+
+	rec := builder.NewRecord()
+	defer rec.Release()
+
+	itr, err := array.NewRecordReader(s.arrowSchema, []arrow.Record{rec})
+	if err != nil {
+		return nil, err
+	}
+	itr.Retain()
+
+	return itr, nil
+}
+
+func (s *storageV2Serializer) serializeDeltaData(pack *SyncPack) (array.RecordReader, error) {
+	fields := make([]*schemapb.FieldSchema, 0, 2)
+	tsField := &schemapb.FieldSchema{
+		FieldID:  common.TimeStampField,
+		Name:     common.TimeStampFieldName,
+		DataType: schemapb.DataType_Int64,
+	}
+	fields = append(fields, s.pkField, tsField)
+
+	deltaArrowSchema, err := iTypeutil.ConvertToArrowSchema(fields)
+	if err != nil {
+		return nil, err
+	}
+
+	builder := array.NewRecordBuilder(memory.DefaultAllocator, deltaArrowSchema)
+	defer builder.Release()
+
+	switch s.pkField.GetDataType() {
+	case schemapb.DataType_Int64:
+		pb := builder.Field(0).(*array.Int64Builder)
+		for _, pk := range pack.deltaData.Pks {
+			pb.Append(pk.GetValue().(int64))
+		}
+	case schemapb.DataType_VarChar:
+		pb := builder.Field(0).(*array.StringBuilder)
+		for _, pk := range pack.deltaData.Pks {
+			pb.Append(pk.GetValue().(string))
+		}
+	default:
+		return nil, merr.WrapErrParameterInvalidMsg("unexpected pk type %v", s.pkField.GetDataType())
+	}
+
+	for _, ts := range pack.deltaData.Tss {
+		builder.Field(1).(*array.Int64Builder).Append(int64(ts))
+	}
+
+	rec := builder.NewRecord()
+	defer rec.Release()
+
+	reader, err := array.NewRecordReader(deltaArrowSchema, []arrow.Record{rec})
+	if err != nil {
+		return nil, err
+	}
+	reader.Retain()
+
+	return reader, nil
+}
+
+func SpaceCreatorFunc(segmentID int64, collSchema *schemapb.CollectionSchema, arrowSchema *arrow.Schema) func() (*milvus_storage.Space, error) {
+	return func() (*milvus_storage.Space, error) {
+		url := fmt.Sprintf("%s://%s:%s@%s/%d?endpoint_override=%s",
+			params.Params.CommonCfg.StorageScheme.GetValue(),
+			params.Params.MinioCfg.AccessKeyID.GetValue(),
+			params.Params.MinioCfg.SecretAccessKey.GetValue(),
+			params.Params.MinioCfg.BucketName.GetValue(),
+			segmentID,
+			params.Params.MinioCfg.Address.GetValue())
+
+		pkSchema, err := typeutil.GetPrimaryFieldSchema(collSchema)
+		if err != nil {
+			return nil, err
+		}
+		vecSchema, err := typeutil.GetVectorFieldSchema(collSchema)
+		if err != nil {
+			return nil, err
+		}
+		space, err := milvus_storage.Open(
+			url,
+			options.NewSpaceOptionBuilder().
+				SetSchema(schema.NewSchema(
+					arrowSchema,
+					&schema.SchemaOptions{
+						PrimaryColumn: pkSchema.Name,
+						VectorColumn:  vecSchema.Name,
+						VersionColumn: common.TimeStampFieldName,
+					},
+				)).
+				Build(),
+		)
+		return space, err
+	}
+}

commit c60db8cce96b77436ae3731b94b3ffeb2a3287f1

fix: serializer shall bypass L0 segment merge stats step
See also #27675
Fix logic problem introduced by #29413, which is serializer tries to
merge statslog list while level segments do not have statslog. This
shall result returning error. `writeBufferBase` ignores this error but
it shall only ignore `ErrSegmentNotFound`.

This PR add logic checking segment level before execution of merging
statslog list. And add error type check for getSyncTask failure.

Signed-off-by: Congqi Xia <congqi.xia@zilliz.com>

internal/datanode/syncmgr/mock_serializer.go
@@ -1,0 +1,91 @@
+// Code generated by mockery v2.32.4. DO NOT EDIT.
+
+package syncmgr
+
+import (
+	context "context"
+
+	mock "github.com/stretchr/testify/mock"
+)
+
+// MockSerializer is an autogenerated mock type for the Serializer type
+type MockSerializer struct {
+	mock.Mock
+}
+
+type MockSerializer_Expecter struct {
+	mock *mock.Mock
+}
+
+func (_m *MockSerializer) EXPECT() *MockSerializer_Expecter {
+	return &MockSerializer_Expecter{mock: &_m.Mock}
+}
+
+// EncodeBuffer provides a mock function with given fields: ctx, pack
+func (_m *MockSerializer) EncodeBuffer(ctx context.Context, pack *SyncPack) (Task, error) {
+	ret := _m.Called(ctx, pack)
+
+	var r0 Task
+	var r1 error
+	if rf, ok := ret.Get(0).(func(context.Context, *SyncPack) (Task, error)); ok {
+		return rf(ctx, pack)
+	}
+	if rf, ok := ret.Get(0).(func(context.Context, *SyncPack) Task); ok {
+		r0 = rf(ctx, pack)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(Task)
+		}
+	}
+
+	if rf, ok := ret.Get(1).(func(context.Context, *SyncPack) error); ok {
+		r1 = rf(ctx, pack)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
+// MockSerializer_EncodeBuffer_Call is a *mock.Call that shadows Run/Return methods with type explicit version for method 'EncodeBuffer'
+type MockSerializer_EncodeBuffer_Call struct {
+	*mock.Call
+}
+
+// EncodeBuffer is a helper method to define mock.On call
+//   - ctx context.Context
+//   - pack *SyncPack
+func (_e *MockSerializer_Expecter) EncodeBuffer(ctx interface{}, pack interface{}) *MockSerializer_EncodeBuffer_Call {
+	return &MockSerializer_EncodeBuffer_Call{Call: _e.mock.On("EncodeBuffer", ctx, pack)}
+}
+
+func (_c *MockSerializer_EncodeBuffer_Call) Run(run func(ctx context.Context, pack *SyncPack)) *MockSerializer_EncodeBuffer_Call {
+	_c.Call.Run(func(args mock.Arguments) {
+		run(args[0].(context.Context), args[1].(*SyncPack))
+	})
+	return _c
+}
+
+func (_c *MockSerializer_EncodeBuffer_Call) Return(_a0 Task, _a1 error) *MockSerializer_EncodeBuffer_Call {
+	_c.Call.Return(_a0, _a1)
+	return _c
+}
+
+func (_c *MockSerializer_EncodeBuffer_Call) RunAndReturn(run func(context.Context, *SyncPack) (Task, error)) *MockSerializer_EncodeBuffer_Call {
+	_c.Call.Return(run)
+	return _c
+}
+
+// NewMockSerializer creates a new instance of MockSerializer. It also registers a testing interface on the mock and a cleanup function to assert the mocks expectations.
+// The first argument is typically a *testing.T value.
+func NewMockSerializer(t interface {
+	mock.TestingT
+	Cleanup(func())
+}) *MockSerializer {
+	mock := &MockSerializer{}
+	mock.Mock.Test(t)
+
+	t.Cleanup(func() { mock.AssertExpectations(t) })
+
+	return mock
+}

internal/datanode/syncmgr/storage_serializer.go
@@ -26,6 +26,7 @@ import (
 
 	"github.com/milvus-io/milvus-proto/go-api/v2/schemapb"
 	"github.com/milvus-io/milvus/internal/datanode/metacache"
+	"github.com/milvus-io/milvus/internal/proto/datapb"
 	"github.com/milvus-io/milvus/internal/proto/etcdpb"
 	"github.com/milvus-io/milvus/internal/storage"
 	"github.com/milvus-io/milvus/pkg/log"
@@ -107,13 +108,15 @@ func (s *storageV1Serializer) EncodeBuffer(ctx context.Context, pack *SyncPack) 
 	}
 
 	if pack.isFlush {
-		mergedStatsBlob, err := s.serializeMergedPkStats(pack)
-		if err != nil {
-			log.Warn("failed to serialize merged stats log", zap.Error(err))
-			return nil, err
+		if pack.level != datapb.SegmentLevel_L0 {
+			mergedStatsBlob, err := s.serializeMergedPkStats(pack)
+			if err != nil {
+				log.Warn("failed to serialize merged stats log", zap.Error(err))
+				return nil, err
+			}
+			task.mergedStatsBlob = mergedStatsBlob
 		}
 
-		task.mergedStatsBlob = mergedStatsBlob
 		task.WithFlush()
 	}
 

internal/datanode/syncmgr/storage_v2_serializer.go
@@ -30,6 +30,7 @@ import (
 	"github.com/milvus-io/milvus-storage/go/storage/options"
 	"github.com/milvus-io/milvus-storage/go/storage/schema"
 	"github.com/milvus-io/milvus/internal/datanode/metacache"
+	"github.com/milvus-io/milvus/internal/proto/datapb"
 	"github.com/milvus-io/milvus/internal/querycoordv2/params"
 	"github.com/milvus-io/milvus/internal/storage"
 	iTypeutil "github.com/milvus-io/milvus/internal/util/typeutil"
@@ -101,13 +102,15 @@ func (s *storageV2Serializer) EncodeBuffer(ctx context.Context, pack *SyncPack) 
 	}
 
 	if pack.isFlush {
-		mergedStatsBlob, err := s.serializeMergedPkStats(pack)
-		if err != nil {
-			log.Warn("failed to serialize merged stats log", zap.Error(err))
-			return nil, err
-		}
+		if pack.level != datapb.SegmentLevel_L0 {
+			mergedStatsBlob, err := s.serializeMergedPkStats(pack)
+			if err != nil {
+				log.Warn("failed to serialize merged stats log", zap.Error(err))
+				return nil, err
+			}
 
-		task.mergedStatsBlob = mergedStatsBlob
+			task.mergedStatsBlob = mergedStatsBlob
+		}
 		task.WithFlush()
 	}
 

internal/datanode/writebuffer/write_buffer.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"sync"
 
+	"github.com/cockroachdb/errors"
 	"github.com/samber/lo"
 	"go.uber.org/atomic"
 	"go.uber.org/zap"
@@ -253,13 +254,16 @@ func (wb *writeBufferBase) flushSegments(ctx context.Context, segmentIDs []int64
 }
 
 func (wb *writeBufferBase) syncSegments(ctx context.Context, segmentIDs []int64) {
+	log := log.Ctx(ctx)
 	for _, segmentID := range segmentIDs {
 		syncTask, err := wb.getSyncTask(ctx, segmentID)
 		if err != nil {
-			// TODO check err type
-			// segment info not found
-			log.Ctx(ctx).Warn("segment not found in meta", zap.Int64("segmentID", segmentID))
-			continue
+			if errors.Is(err, merr.ErrSegmentNotFound) {
+				log.Warn("segment not found in meta", zap.Int64("segmentID", segmentID))
+				continue
+			} else {
+				log.Fatal("failed to get sync task", zap.Int64("segmentID", segmentID), zap.Error(err))
+			}
 		}
 
 		// discard Future here, handle error in callback

internal/datanode/writebuffer/bf_write_buffer_test.go
@@ -211,7 +211,7 @@ func (s *BFWriteBufferSuite) TestAutoSync() {
 
 func (s *BFWriteBufferSuite) TestBufferDataWithStorageV2() {
 	params.Params.CommonCfg.EnableStorageV2.SwapTempValue("true")
-	defer params.Params.Reset(params.Params.CommonCfg.EnableStorageV2.Key)
+	defer paramtable.Get().CommonCfg.EnableStorageV2.SwapTempValue("false")
 	params.Params.CommonCfg.StorageScheme.SwapTempValue("file")
 	tmpDir := s.T().TempDir()
 	arrowSchema, err := typeutil.ConvertToArrowSchema(s.collSchema.Fields)
@@ -241,6 +241,7 @@ func (s *BFWriteBufferSuite) TestBufferDataWithStorageV2() {
 
 func (s *BFWriteBufferSuite) TestAutoSyncWithStorageV2() {
 	params.Params.CommonCfg.EnableStorageV2.SwapTempValue("true")
+	defer paramtable.Get().CommonCfg.EnableStorageV2.SwapTempValue("false")
 	paramtable.Get().Save(paramtable.Get().DataNodeCfg.FlushInsertBufferSize.Key, "1")
 	tmpDir := s.T().TempDir()
 	arrowSchema, err := typeutil.ConvertToArrowSchema(s.collSchema.Fields)

internal/datanode/writebuffer/write_buffer_test.go
@@ -15,6 +15,7 @@ import (
 	"github.com/milvus-io/milvus/internal/datanode/syncmgr"
 	"github.com/milvus-io/milvus/internal/proto/datapb"
 	"github.com/milvus-io/milvus/pkg/common"
+	"github.com/milvus-io/milvus/pkg/util/merr"
 	"github.com/milvus-io/milvus/pkg/util/paramtable"
 )
 
@@ -260,6 +261,39 @@ func (s *WriteBufferSuite) TestGetCheckpoint() {
 
 		checkpoint := s.wb.GetCheckpoint()
 		s.EqualValues(400, checkpoint.GetTimestamp())
+	})
+}
+
+func (s *WriteBufferSuite) TestSyncSegmentsError() {
+	wb, err := newWriteBufferBase(s.channelName, s.metacache, s.storageCache, s.syncMgr, &writeBufferOption{
+		pkStatsFactory: func(vchannel *datapb.SegmentInfo) *metacache.BloomFilterSet {
+			return metacache.NewBloomFilterSet()
+		},
+	})
+	s.Require().NoError(err)
+
+	serializer := syncmgr.NewMockSerializer(s.T())
+
+	wb.serializer = serializer
+
+	segment := metacache.NewSegmentInfo(&datapb.SegmentInfo{
+		ID: 1,
+	}, nil)
+	s.metacache.EXPECT().GetSegmentByID(int64(1)).Return(segment, true)
+	s.metacache.EXPECT().UpdateSegments(mock.Anything, mock.Anything).Return()
+
+	s.Run("segment_not_found", func() {
+		serializer.EXPECT().EncodeBuffer(mock.Anything, mock.Anything).Return(nil, merr.WrapErrSegmentNotFound(1)).Once()
+		s.NotPanics(func() {
+			wb.syncSegments(context.Background(), []int64{1})
+		})
+	})
+
+	s.Run("other_err", func() {
+		serializer.EXPECT().EncodeBuffer(mock.Anything, mock.Anything).Return(nil, merr.WrapErrServiceInternal("mocked")).Once()
+		s.Panics(func() {
+			wb.syncSegments(context.Background(), []int64{1})
+		})
 	})
 }
 