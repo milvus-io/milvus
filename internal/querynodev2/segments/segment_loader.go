b45d08b47bbae5d0c9ae72a0ae72a9a94185b1fb
wei liu <wei.liu@zilliz.com>
2024-01-05 14:24:49 +0800

    enhance: Add ctx for load index logs (#29686)
    This PR add ctx for load index logs
    
    Signed-off-by: Wei Liu <wei.liu@zilliz.com>

--- internal/querynodev2/segments/segment_loader.go
+++ internal/querynodev2/segments/segment_loader.go
@@ -680,6 +680,13 @@ func (loader *segmentLoader) loadFieldsIndex(ctx context.Context,
 	numRows int64,
 	indexedFieldInfos map[int64]*IndexedFieldInfo,
 ) error {
+	log := log.Ctx(ctx).With(
+		zap.Int64("collectionID", segment.Collection()),
+		zap.Int64("partitionID", segment.Partition()),
+		zap.Int64("segmentID", segment.ID()),
+		zap.Int64("rowCount", numRows),
+	)
+
 	for fieldID, fieldInfo := range indexedFieldInfos {
 		indexInfo := fieldInfo.IndexInfo
 		err := loader.loadFieldIndex(ctx, segment, indexInfo)
@@ -688,8 +695,6 @@ func (loader *segmentLoader) loadFieldsIndex(ctx context.Context,
 		}
 
 		log.Info("load field binlogs done for sealed segment with index",
-			zap.Int64("collection", segment.collectionID),
-			zap.Int64("segment", segment.segmentID),
 			zap.Int64("fieldID", fieldID),
 			zap.Any("binlog", fieldInfo.FieldBinlog.Binlogs),
 			zap.Int32("current_index_version", fieldInfo.IndexInfo.GetCurrentIndexVersion()),
