15db75779a4fc8f222ee22a3236c89eded156c28
zhuwenxing <wenxing.zhu@zilliz.com>
2024-01-16 14:22:06 +0800

    test: add restful client test in ci
    Signed-off-by: zhuwenxing <wenxing.zhu@zilliz.com>

--- tests/restful_client/conftest.py
+++ tests/restful_client/conftest.py
@@ -3,34 +3,17 @@ import yaml
 
 
 def pytest_addoption(parser):
-    parser.addoption("--protocol", action="store", default="http", help="host")
-    parser.addoption("--host", action="store", default="127.0.0.1", help="host")
-    parser.addoption("--port", action="store", default="19530", help="port")
-    parser.addoption("--username", action="store", default="root", help="email")
-    parser.addoption("--password", action="store", default="Milvus", help="password")
+    parser.addoption("--endpoint", action="store", default="http://127.0.0.1:19530", help="endpoint")
+    parser.addoption("--token", action="store", default="root:Milvus", help="token")
 
 
 @pytest.fixture
-def protocol(request):
-    return request.config.getoption("--protocol")
+def endpoint(request):
+    return request.config.getoption("--endpoint")
 
 
 @pytest.fixture
-def host(request):
-    return request.config.getoption("--host")
+def token(request):
+    return request.config.getoption("--token")
 
-
-@pytest.fixture
-def port(request):
-    return request.config.getoption("--port")
-
-
-@pytest.fixture
-def username(request):
-    return request.config.getoption("--username")
-
-
-@pytest.fixture
-def password(request):
-    return request.config.getoption("--password")
 
