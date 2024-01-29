a3dd2494e5a72e1e594ded05fa835689aad1f908
bigsheeper <yihao.dai@zilliz.com>
2024-01-29 14:44:33 +0800

    fix ut
    Signed-off-by: bigsheeper <yihao.dai@zilliz.com>

--- pkg/util/paramtable/component_param_test.go
+++ pkg/util/paramtable/component_param_test.go
@@ -410,7 +410,7 @@ func TestComponentParam(t *testing.T) {
 
 		maxConcurrentImportTaskNum := Params.MaxConcurrentImportTaskNum.GetAsInt()
 		t.Logf("maxConcurrentImportTaskNum: %d", maxConcurrentImportTaskNum)
-		assert.Equal(t, 10, maxConcurrentImportTaskNum)
+		assert.Equal(t, 32, maxConcurrentImportTaskNum)
 	})
 
 	t.Run("test indexNodeConfig", func(t *testing.T) {
