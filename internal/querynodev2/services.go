41c21d2effd03dcca7104f9c519a60a27fd56405
Congqi Xia <congqi.xia@zilliz.com>
2024-02-21 18:41:17 +0800

    fix: Release loaded growing if WatchDmlChannel fail
    See also #30734
    
    Signed-off-by: Congqi Xia <congqi.xia@zilliz.com>

--- internal/querynodev2/services.go
+++ internal/querynodev2/services.go
@@ -316,6 +316,9 @@ func (node *QueryNode) WatchDmChannels(ctx context.Context, req *querypb.WatchDm
 	}
 	err = loadGrowingSegments(ctx, delegator, req)
 	if err != nil {
+		// remove legacy growing
+		node.manager.Segment.RemoveBy(segments.WithChannel(channel.GetChannelName()),
+			segments.WithType(segments.SegmentTypeGrowing))
 		msg := "failed to load growing segments"
 		log.Warn(msg, zap.Error(err))
 		return merr.Status(err), nil
