1a2db89fc536bb4f535bf7f575a0c76bc57414a3
SimFG <bang.fu@zilliz.com>
2024-02-29 19:33:16 +0800

    enhance: make the watch dm channel request better compatibility
    Signed-off-by: SimFG <bang.fu@zilliz.com>

--- internal/querycoordv2/task/utils.go
+++ internal/querycoordv2/task/utils.go
@@ -195,9 +195,13 @@ func fillSubChannelRequest(
 	ctx context.Context,
 	req *querypb.WatchDmChannelsRequest,
 	broker meta.Broker,
+	includeFlushed bool,
 ) error {
 	segmentIDs := typeutil.NewUniqueSet()
 	for _, vchannel := range req.GetInfos() {
+		if includeFlushed {
+			segmentIDs.Insert(vchannel.GetFlushedSegmentIds()...)
+		}
 		segmentIDs.Insert(vchannel.GetUnflushedSegmentIds()...)
 		segmentIDs.Insert(vchannel.GetLevelZeroSegmentIds()...)
 	}
