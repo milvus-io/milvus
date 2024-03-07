3ad31abe9343edb3a1e934092f9f7e5f730c24b9
sunby <sunbingyi1992@gmail.com>
2024-03-07 18:40:31 +0800

    fix: merge index params when creating index
    Signed-off-by: sunby <sunbingyi1992@gmail.com>

--- internal/datacoord/index_service.go
+++ internal/datacoord/index_service.go
@@ -200,6 +200,20 @@ func (s *Server) CreateIndex(ctx context.Context, req *indexpb.CreateIndexReques
 	}
 
 	indexID, err := s.meta.indexMeta.CanCreateIndex(req)
+
+	// merge with previous params because create index would not pass mmap params
+	indexes := s.meta.indexMeta.GetFieldIndexes(req.GetCollectionID(), req.GetFieldID(), req.GetIndexName())
+	if len(indexes) == 1 {
+		req.UserIndexParams, err = UpdateParams(indexes[0], indexes[0].UserIndexParams, req.GetUserIndexParams())
+		if err != nil {
+			return merr.Status(err), nil
+		}
+		req.IndexParams, err = UpdateParams(indexes[0], indexes[0].IndexParams, req.GetIndexParams())
+		if err != nil {
+			return merr.Status(err), nil
+		}
+	}
+
 	if err != nil {
 		metrics.IndexRequestCounter.WithLabelValues(metrics.FailLabel).Inc()
 		return merr.Status(err), nil
