e5fcb2024e697e19258a236d203e6902d0eba48f
gcmutator <329964069@qq.com>
2024-03-09 13:03:52 +0800

    remove repetitive words
    Signed-off-by: gcmutator <329964069@qq.com>

--- internal/kv/etcd/etcd_kv.go
+++ internal/kv/etcd/etcd_kv.go
@@ -180,7 +180,7 @@ func (kv *etcdKV) LoadBytesWithPrefix(key string) ([]string, [][]byte, error) {
 	return keys, values, nil
 }
 
-// LoadBytesWithPrefix2 returns all the the keys,values and key versions with the given key prefix.
+// LoadBytesWithPrefix2 returns all the keys,values and key versions with the given key prefix.
 func (kv *etcdKV) LoadBytesWithPrefix2(key string) ([]string, [][]byte, []int64, error) {
 	start := time.Now()
 	key = path.Join(kv.rootPath, key)
