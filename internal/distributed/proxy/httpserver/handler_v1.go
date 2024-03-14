afa4193fc234cb964372760a151b71bd44ad071a
PowderLi <135960789+PowderLi@users.noreply.github.com>
2024-03-14 23:27:03 +0800

    fix: [restful v2] create collection & describe collection (#31269)
    1. complete the output of describe collection #31180
    2. add a new parameter `params` for create collection #31181 #31149
    3. create index according to indexConfig while create collection #31199
    4. autoID: false while create a collection quickly, take upsert into
    consideration
    5. polish error information while search without annsField #31010
    
    
    fix: [restful v1] insert / upsert with default partitionName JIRA-4952
    
    ---------
    
    Signed-off-by: PowderLi <min.li@zilliz.com>

--- internal/distributed/proxy/httpserver/handler_v1.go
+++ internal/distributed/proxy/httpserver/handler_v1.go
@@ -698,7 +698,6 @@ func (h *HandlersV1) insert(c *gin.Context) {
 	req := &milvuspb.InsertRequest{
 		DbName:         httpReq.DbName,
 		CollectionName: httpReq.CollectionName,
-		PartitionName:  "_default",
 		NumRows:        uint32(len(httpReq.Data)),
 	}
 	username, _ := c.Get(ContextUsername)
@@ -790,7 +789,6 @@ func (h *HandlersV1) upsert(c *gin.Context) {
 	req := &milvuspb.UpsertRequest{
 		DbName:         httpReq.DbName,
 		CollectionName: httpReq.CollectionName,
-		PartitionName:  "_default",
 		NumRows:        uint32(len(httpReq.Data)),
 	}
 	username, _ := c.Get(ContextUsername)
