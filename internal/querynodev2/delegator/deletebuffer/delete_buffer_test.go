3ebdca8528c531e629d883033cae0c270f6be4f3
aoiasd <45024769+aoiasd@users.noreply.github.com>
2024-03-23 21:09:07 +0800

    fix: double buffer was invalid when put entry which size larger than max size (#31549)
    relate: https://github.com/milvus-io/milvus/issues/31548
    
    ---------
    
    Signed-off-by: aoiasd <zhicheng.yue@zilliz.com>

--- internal/querynodev2/delegator/deletebuffer/delete_buffer_test.go
+++ internal/querynodev2/delegator/deletebuffer/delete_buffer_test.go
@@ -78,6 +78,58 @@ func (s *DoubleCacheBufferSuite) TestCache() {
 	s.Equal(1, len(buffer.ListAfter(12)))
 }
 
+func (s *DoubleCacheBufferSuite) TestPut() {
+	buffer := NewDoubleCacheDeleteBuffer[*Item](10, 1)
+	buffer.Put(&Item{
+		Ts: 11,
+		Data: []BufferItem{
+			{
+				PartitionID: 200,
+				DeleteData: storage.DeleteData{
+					Pks:      []storage.PrimaryKey{storage.NewVarCharPrimaryKey("test1")},
+					Tss:      []uint64{11},
+					RowCount: 1,
+				},
+			},
+		},
+	})
+
+	buffer.Put(&Item{
+		Ts: 12,
+		Data: []BufferItem{
+			{
+				PartitionID: 200,
+				DeleteData: storage.DeleteData{
+					Pks:      []storage.PrimaryKey{storage.NewVarCharPrimaryKey("test2")},
+					Tss:      []uint64{12},
+					RowCount: 1,
+				},
+			},
+		},
+	})
+
+	s.Equal(2, len(buffer.ListAfter(11)))
+	s.Equal(1, len(buffer.ListAfter(12)))
+
+	buffer.Put(&Item{
+		Ts: 13,
+		Data: []BufferItem{
+			{
+				PartitionID: 200,
+				DeleteData: storage.DeleteData{
+					Pks:      []storage.PrimaryKey{storage.NewVarCharPrimaryKey("test3")},
+					Tss:      []uint64{13},
+					RowCount: 1,
+				},
+			},
+		},
+	})
+
+	s.Equal(2, len(buffer.ListAfter(11)))
+	s.Equal(2, len(buffer.ListAfter(12)))
+	s.Equal(1, len(buffer.ListAfter(13)))
+}
+
 func TestDoubleCacheDeleteBuffer(t *testing.T) {
 	suite.Run(t, new(DoubleCacheBufferSuite))
 }
