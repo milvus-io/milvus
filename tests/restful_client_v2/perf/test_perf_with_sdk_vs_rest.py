3fc8533c52e7e06919b92683b6a0d33310b2cb19
zhuwenxing <wenxing.zhu@zilliz.com>
2024-03-27 20:20:47 +0800

    update sdk vs restful
    Signed-off-by: zhuwenxing <wenxing.zhu@zilliz.com>

--- tests/restful_client_v2/perf/test_perf_with_sdk_vs_rest.py
+++ tests/restful_client_v2/perf/test_perf_with_sdk_vs_rest.py
@@ -9,7 +9,9 @@ import requests
 import json
 import argparse
 from loguru import logger
+from faker import Faker
 
+fake = Faker()
 
 def main(host="127.0.0.1"):
     connections.connect(
@@ -22,7 +24,7 @@ def main(host="127.0.0.1"):
     ]
     search_params = {"metric_type": "L2", "params": {"ef": 150}}
     nb = 1000
-    insert_data = [{"id": random.randint(0, 1000), "emb": [random.random() for _ in range(768)]} for _ in range(nb)]
+    insert_data = [{"id": random.randint(0, 1000), "text": fake.text(), "text_emb": [random.random() for _ in range(768)], "image_emb": [random.random() for _ in range(768)]} for _ in range(nb)]
 
     headers = {
         'Content-Type': 'application/json',
@@ -39,9 +41,9 @@ def main(host="127.0.0.1"):
             logger.info(f"{op}...")
             if op == "search":
                 res = collection.search([vector_to_search[random_id]], "text_emb", search_params, 100, output_fields=["*"])
-            if op == "hybrid_search":
-                sq1=AnnSearchRequest([vector_to_search[random_id]], "text_emb", search_params, 100, output_fields=["*"])
-                sq2=AnnSearchRequest([vector_to_search[random_id]], "image_emb", search_params, 100, output_fields=["*"])
+            elif op == "hybrid_search":
+                sq1=AnnSearchRequest([vector_to_search[random_id]], "text_emb", search_params, 100)
+                sq2=AnnSearchRequest([vector_to_search[random_id]], "image_emb", search_params, 100)
                 res = collection.hybrid_search([sq1, sq2], RRFRanker(60), 100, output_fields=["*"])
             elif op == "query_id":
                 res = collection.query(expr=f"id in {[x for x in range(100)]}", output_fields=["*"], limit=100)
@@ -75,7 +77,7 @@ def main(host="127.0.0.1"):
                                       })
 
                 response = requests.request("POST", url, headers=headers, data=payload)
-            if op == "hybrid_search":
+            elif op == "hybrid_search":
                 payload = json.dumps({"collectionName": "test_restful_perf",
                                     "search": [
                                         {
