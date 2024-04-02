7e20f1a35ca7336346f3a2340150e33970cecbac
Cai Yudong <yudong.cai@zilliz.com>
2024-04-02 17:10:18 +0800

    Disable HNSW Binary
    Signed-off-by: Cai Yudong <yudong.cai@zilliz.com>

--- pkg/util/indexparamcheck/hnsw_checker.go
+++ pkg/util/indexparamcheck/hnsw_checker.go
@@ -32,8 +32,8 @@ func (c hnswChecker) CheckTrain(params map[string]string) error {
 
 func (c hnswChecker) CheckValidDataType(dType schemapb.DataType) error {
 	// TODO(SPARSE) we'll add sparse vector support in HNSW later in cardinal
-	if dType != schemapb.DataType_FloatVector && dType != schemapb.DataType_BinaryVector && dType != schemapb.DataType_Float16Vector && dType != schemapb.DataType_BFloat16Vector {
-		return fmt.Errorf("only support float vector or binary vector")
+	if dType != schemapb.DataType_FloatVector && dType != schemapb.DataType_Float16Vector && dType != schemapb.DataType_BFloat16Vector {
+		return fmt.Errorf("HNSW only support float vector data type")
 	}
 	return nil
 }
