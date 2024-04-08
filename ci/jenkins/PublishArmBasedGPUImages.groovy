42193e4d4389b1394749a50f97877afa9068ac6c
Liang Huang <sammy.huang@zilliz.com>
2024-04-08 15:02:29 +0800

    enhance: identify milvusdb/milvus-env from .env
    Signed-off-by: Liang Huang <sammy.huang@zilliz.com>

--- ci/jenkins/PublishArmBasedGPUImages.groovy
+++ ci/jenkins/PublishArmBasedGPUImages.groovy
@@ -26,7 +26,11 @@ pipeline {
             steps {
                     script {
                         sh """
-                        docker run -v \$(pwd):/root/milvus -v \$(pwd)/.docker/.conan:/root/.conan -w /root/milvus milvusdb/milvus-env:gpu-ubuntu22.04-20240330-4021fhl-arm64 sh -c "make clean && make gpu-install"
+                        set -a  # automatically export all variables from .env
+                        . ${WORKSPACE}/.env
+                        set +a  # stop automatically
+
+                        docker run -v \$(pwd):/root/milvus -v \$(pwd)/.docker/.conan:/root/.conan -w /root/milvus milvusdb/milvus-env:ubuntu22.04-\${GPU_DATE_VERSION} sh -c "make clean && make gpu-install"
                         """
 
                         def date = sh(returnStdout: true, script: 'date +%Y%m%d').trim()
