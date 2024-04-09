fede9312a2989e09789883193115b61e1cbf7913
xiaofanluan <xiaofan.luan@zilliz.com>
2024-04-09 15:30:01 -0700

    fix: reduce didn't handle offset without limit and reduceStopForBest correctly
    Signed-off-by: xiaofanluan <xiaofan.luan@zilliz.com>

--- pkg/util/typeutil/schema.go
+++ pkg/util/typeutil/schema.go
@@ -1097,7 +1097,7 @@ type ResultWithID interface {
 }
 
 // SelectMinPK select the index of the minPK in results T of the cursors.
-func SelectMinPK[T ResultWithID](results []T, cursors []int64) (int, bool) {
+func SelectMinPK[T ResultWithID](limit int64, results []T, cursors []int64) (int, bool) {
 	var (
 		sel               = -1
 		drainResult       = false
@@ -1107,7 +1107,8 @@ func SelectMinPK[T ResultWithID](results []T, cursors []int64) (int, bool) {
 		minStrPK string
 	)
 	for i, cursor := range cursors {
-		if int(cursor) >= GetSizeOfIDs(results[i].GetIds()) {
+		// if result size < limit, this means we should ignore the result from this segment
+		if int(cursor) >= GetSizeOfIDs(results[i].GetIds()) && (GetSizeOfIDs(results[i].GetIds()) == int(limit)) {
 			drainResult = true
 			continue
 		}
