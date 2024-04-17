14794b713b4cc4adece04adbd133d176ed8910e1
PowderLi <min.li@zilliz.com>
2024-04-17 14:07:27 +0800

    fix: [restful]use default search parameter `nq: 0` 1. v1 can only accept one vector, but v2 accept list of vectors 2. cannot get dbName from AliasReq
    Signed-off-by: PowderLi <min.li@zilliz.com>

--- internal/distributed/proxy/httpserver/handler_v2.go
+++ internal/distributed/proxy/httpserver/handler_v2.go
@@ -837,7 +837,6 @@ func (h *HandlersV2) search(ctx context.Context, c *gin.Context, anyReq any, dbN
 		PartitionNames:     httpReq.PartitionNames,
 		SearchParams:       searchParams,
 		GuaranteeTimestamp: BoundedTimestamp,
-		Nq:                 int64(1),
 	}
 	resp, err := wrapperProxy(ctx, c, req, h.checkAuth, false, func(reqCtx context.Context, req any) (interface{}, error) {
 		return h.proxy.Search(reqCtx, req.(*milvuspb.SearchRequest))
