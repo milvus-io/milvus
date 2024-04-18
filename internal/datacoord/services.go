e2c38750c76098a0725d780a0975e23d76cbf9fe
zhagnlu <1542303831@qq.com>
2024-04-18 14:25:14 +0800

    fix: modify retry error (#32351)
    #32322
    
    Signed-off-by: luzhang <luzhang@zilliz.com>
    Co-authored-by: luzhang <luzhang@zilliz.com>

--- internal/datacoord/services.go
+++ internal/datacoord/services.go
@@ -194,7 +194,13 @@ func (s *Server) AssignSegmentID(ctx context.Context, req *datapb.AssignSegmentI
 			r.CollectionID, r.PartitionID, r.ChannelName, int64(r.Count))
 		if err != nil {
 			log.Warn("failed to alloc segment", zap.Any("request", r), zap.Error(err))
-			return nil, err
+			assigns = append(assigns, &datapb.SegmentIDAssignment{
+				ChannelName:  r.ChannelName,
+				CollectionID: r.CollectionID,
+				PartitionID:  r.PartitionID,
+				Status:       merr.Status(err),
+			})
+			continue
 		}
 
 		log.Info("success to assign segments", zap.Int64("collectionID", r.GetCollectionID()), zap.Any("assignments", segmentAllocations))
