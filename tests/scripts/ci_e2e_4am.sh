9e3f3d99f7614ae06a70487cefb5912273d1f97b
zhuwenxing <wenxing.zhu@zilliz.com>
2024-05-13 19:35:32 +0800

    test: add timeout for import testcases (#33006)
    add timeout for import testcases
    
    ---------
    
    Signed-off-by: zhuwenxing <wenxing.zhu@zilliz.com>

--- tests/scripts/ci_e2e_4am.sh
+++ tests/scripts/ci_e2e_4am.sh
@@ -66,6 +66,23 @@ echo "prepare e2e test"
 install_pytest_requirements
 
 
+
+
+cd ${ROOT}/tests/python_client
+# Run bulk insert test
+# if MILVUS_HELM_RELEASE_NAME contains "msop", then it is one pod mode, skip the bulk insert test
+if [[ "${MILVUS_HELM_RELEASE_NAME}" != *"msop"* ]]; then
+  if [[ -n "${TEST_TIMEOUT:-}" ]]; then
+
+    timeout  "${TEST_TIMEOUT}" pytest testcases/test_bulk_insert.py --timeout=300 --host ${MILVUS_SERVICE_NAME} --port ${MILVUS_SERVICE_PORT} --minio_host ${MINIO_SERVICE_NAME} \
+                                      --html=${CI_LOG_PATH}/report_bulk_insert.html  --self-contained-html
+  else
+    pytest testcases/test_bulk_insert.py --timeout=300 --host ${MILVUS_SERVICE_NAME} --port ${MILVUS_SERVICE_PORT} --minio_host ${MINIO_SERVICE_NAME} \
+                                      --html=${CI_LOG_PATH}/report_bulk_insert.html --self-contained-html
+  fi
+fi
+
+
 # Run restful test v1
 
 cd ${ROOT}/tests/restful_client
@@ -122,10 +139,10 @@ fi
 if [[ "${MILVUS_HELM_RELEASE_NAME}" != *"msop"* ]]; then
   if [[ -n "${TEST_TIMEOUT:-}" ]]; then
 
-    timeout  "${TEST_TIMEOUT}" pytest testcases/test_bulk_insert.py --host ${MILVUS_SERVICE_NAME} --port ${MILVUS_SERVICE_PORT} --minio_host ${MINIO_SERVICE_NAME} \
+    timeout  "${TEST_TIMEOUT}" pytest testcases/test_bulk_insert.py --timeout=300 --host ${MILVUS_SERVICE_NAME} --port ${MILVUS_SERVICE_PORT} --minio_host ${MINIO_SERVICE_NAME} \
                                       --html=${CI_LOG_PATH}/report_bulk_insert.html  --self-contained-html
   else
-    pytest testcases/test_bulk_insert.py --host ${MILVUS_SERVICE_NAME} --port ${MILVUS_SERVICE_PORT} --minio_host ${MINIO_SERVICE_NAME} \
+    pytest testcases/test_bulk_insert.py --timeout=300 --host ${MILVUS_SERVICE_NAME} --port ${MILVUS_SERVICE_PORT} --minio_host ${MINIO_SERVICE_NAME} \
                                       --html=${CI_LOG_PATH}/report_bulk_insert.html --self-contained-html
   fi
 fi
