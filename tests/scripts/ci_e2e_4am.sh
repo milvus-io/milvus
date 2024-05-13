8db0815baf6fc060257db2b99d40d05db05a3f41
zhuwenxing <wenxing.zhu@zilliz.com>
2024-05-13 13:33:39 +0800

    test: add timeout for import testcases
    Signed-off-by: zhuwenxing <wenxing.zhu@zilliz.com>

--- tests/scripts/ci_e2e_4am.sh
+++ tests/scripts/ci_e2e_4am.sh
@@ -122,10 +122,10 @@ fi
 if [[ "${MILVUS_HELM_RELEASE_NAME}" != *"msop"* ]]; then
   if [[ -n "${TEST_TIMEOUT:-}" ]]; then
 
-    timeout  "${TEST_TIMEOUT}" pytest testcases/test_bulk_insert.py --host ${MILVUS_SERVICE_NAME} --port ${MILVUS_SERVICE_PORT} --minio_host ${MINIO_SERVICE_NAME} \
+    timeout  "${TEST_TIMEOUT}" pytest testcases/test_bulk_insert.py --timeout=300 --host ${MILVUS_SERVICE_NAME} --port ${MILVUS_SERVICE_PORT} --minio_host ${MINIO_SERVICE_NAME} \
                                       --html=${CI_LOG_PATH}/report_bulk_insert.html  --self-contained-html
   else
-    pytest testcases/test_bulk_insert.py --host ${MILVUS_SERVICE_NAME} --port ${MILVUS_SERVICE_PORT} --minio_host ${MINIO_SERVICE_NAME} \
+    pytest testcases/test_bulk_insert.py --timeout=300 --host ${MILVUS_SERVICE_NAME} --port ${MILVUS_SERVICE_PORT} --minio_host ${MINIO_SERVICE_NAME} \
                                       --html=${CI_LOG_PATH}/report_bulk_insert.html --self-contained-html
   fi
 fi
