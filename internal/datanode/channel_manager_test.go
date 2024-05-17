1752f075f4094f9480023f63ece92674a4f2c08c
yangxuan <xuan.yang@zilliz.com>
2024-05-17 14:26:08 +0800

    fix: [2.4]channel manager's goroutine run order
    See also: #33117
    
    Signed-off-by: yangxuan <xuan.yang@zilliz.com>

--- internal/datanode/channel_manager_test.go
+++ internal/datanode/channel_manager_test.go
@@ -20,6 +20,7 @@ import (
 	"context"
 	"testing"
 
+	"github.com/cockroachdb/errors"
 	"github.com/stretchr/testify/suite"
 
 	"github.com/milvus-io/milvus-proto/go-api/v2/commonpb"
@@ -56,7 +57,7 @@ func (s *OpRunnerSuite) TestWatchWithTimer() {
 	mockReleaseFunc := func(channel string) {
 		log.Info("mock release func")
 	}
-	runner := NewOpRunner(channel, s.node, mockReleaseFunc, commuCh)
+	runner := NewOpRunner(channel, s.node, mockReleaseFunc, executeWatch, commuCh)
 	err := runner.Enqueue(info)
 	s.Require().NoError(err)
 
@@ -65,6 +66,37 @@ func (s *OpRunnerSuite) TestWatchWithTimer() {
 	s.Equal(channel, opState.channel)
 
 	runner.FinishOp(100)
+}
+
+func (s *OpRunnerSuite) TestWatchTimeout() {
+	channel := "by-dev-rootcoord-dml-1000"
+	paramtable.Get().Save(Params.DataCoordCfg.WatchTimeoutInterval.Key, "0.000001")
+	defer paramtable.Get().Reset(Params.DataCoordCfg.WatchTimeoutInterval.Key)
+	info := getWatchInfoByOpID(100, channel, datapb.ChannelWatchState_ToWatch)
+
+	sig := make(chan struct{})
+	commuCh := make(chan *opState)
+
+	mockReleaseFunc := func(channel string) { log.Info("mock release func") }
+	mockWatchFunc := func(ctx context.Context, dn *DataNode, info *datapb.ChannelWatchInfo, tickler *tickler) (*dataSyncService, error) {
+		select {
+		case <-ctx.Done():
+			sig <- struct{}{}
+		}
+		return nil, errors.New("timeout")
+	}
+
+	runner := NewOpRunner(channel, s.node, mockReleaseFunc, mockWatchFunc, commuCh)
+	runner.Start()
+	defer runner.Close()
+	err := runner.Enqueue(info)
+	s.Require().NoError(err)
+
+	<-sig
+	opState := <-commuCh
+	s.Require().NotNil(opState)
+	s.Equal(info.GetOpID(), opState.opID)
+	s.Equal(datapb.ChannelWatchState_WatchFailure, opState.state)
 }
 
 type OpRunnerSuite struct {
@@ -126,8 +158,9 @@ func (s *ChannelManagerSuite) TearDownTest() {
 	}
 }
 
+<<<<<<< HEAD
 func (s *ChannelManagerSuite) TestWatchFail() {
-	channel := "by-dev-rootcoord-dml-2"
+	channel := "by-dev-rootcoord-dml-1000"
 	paramtable.Get().Save(Params.DataCoordCfg.WatchTimeoutInterval.Key, "0.000001")
 	defer paramtable.Get().Reset(Params.DataCoordCfg.WatchTimeoutInterval.Key)
 	info := getWatchInfoByOpID(100, channel, datapb.ChannelWatchState_ToWatch)
@@ -146,6 +179,8 @@ func (s *ChannelManagerSuite) TestWatchFail() {
 	s.Equal(datapb.ChannelWatchState_WatchFailure, resp.GetState())
 }
 
+=======
+>>>>>>> 1606df919 (fix: channel manager's goroutine run order)
 func (s *ChannelManagerSuite) TestReleaseStuck() {
 	var (
 		channel  = "by-dev-rootcoord-dml-2"
