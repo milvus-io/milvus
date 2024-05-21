7ab7e3a0048a087cccc7b7ace6f1af831968b934
sammy.huang <sammy.huang@zilliz.com>
2024-05-21 16:54:38 +0800

    feat: support arm-based image build and pull request (#33219)
    Signed-off-by: Liang Huang <sammy.huang@zilliz.com>

--- ci/jenkins/PublishArmBasedImages.groovy
+++ ci/jenkins/PublishArmBasedImages.groovy
@@ -12,6 +12,9 @@ pipeline {
             // idleMinutes 120
         }
     }
+    parameters {
+        string(name: 'image-tag', defaultValue: '', description: 'the image tag to be pushed to image registry')
+    }
 
     options {
         timestamps()
@@ -29,29 +32,43 @@ pipeline {
     }
 
     stages {
-        stage('Publish Milvus GPU Images'){
+        stage('Publish Milvus cpu Images'){
 
             steps {
                     script {
                         sh """
+                            git config --global --add safe.directory /home/jenkins/agent/workspace
+                        """
+
+                        def tag = ""
+                        if (params['image-tag'] == '') {
+                            def date = sh(returnStdout: true, script: 'date +%Y%m%d').trim()
+                            def gitShortCommit = sh(returnStdout: true, script: 'git rev-parse --short HEAD').trim()
+                            tag = "${env.BRANCH_NAME}-${date}-${gitShortCommit}-arm"
+                        }else{
+                            tag = params['image-tag']
+                        }
+
+                        sh """
+                        echo "Building image with tag: ${tag}"
+
                         set -a  # automatically export all variables from .env
                         . .env
                         set +a  # stop automatically
 
-                        docker run --net=host -v \$(pwd):/root/milvus -v /root/.conan:/root/.conan -w /root/milvus milvusdb/milvus-env:gpu-ubuntu22.04-\${GPU_DATE_VERSION}  sh -c "make clean && make gpu-install"
+
+                        docker run --net=host -v /root/.conan:/root/.conan  -v \$(pwd):/root/milvus -w /root/milvus milvusdb/milvus-env:ubuntu20.04-\${DATE_VERSION} sh -c "make clean && make install"
                         """
 
-                        def date = sh(returnStdout: true, script: 'date +%Y%m%d').trim()
-                        def gitShortCommit = sh(returnStdout: true, script: 'git rev-parse --short HEAD').trim()
 
                         withCredentials([usernamePassword(credentialsId: "${env.DOCKER_CREDENTIALS_ID}", usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
                             sh 'docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}'
                             sh """
                                 export MILVUS_IMAGE_REPO="${env.TARGET_REPO}/milvus"
                                 export MILVUS_HARBOR_IMAGE_REPO="${env.HARBOR_REPO}/milvus/milvus"
-                                export MILVUS_IMAGE_TAG="${env.BRANCH_NAME}-${date}-${gitShortCommit}-gpu-arm"
+                                export MILVUS_IMAGE_TAG="${tag}"
 
-                                docker build --build-arg TARGETARCH=arm64  -f "./build/docker/milvus/gpu/ubuntu22.04/Dockerfile" -t \${MILVUS_IMAGE_REPO}:\${MILVUS_IMAGE_TAG} .
+                                docker build --build-arg TARGETARCH=arm64  -f "./build/docker/milvus/ubuntu20.04/Dockerfile" -t \${MILVUS_IMAGE_REPO}:\${MILVUS_IMAGE_TAG} .
 
                                 docker push \${MILVUS_IMAGE_REPO}:\${MILVUS_IMAGE_TAG}
                                 docker tag \${MILVUS_IMAGE_REPO}:\${MILVUS_IMAGE_TAG} \${MILVUS_HARBOR_IMAGE_REPO}:\${MILVUS_IMAGE_TAG}
@@ -63,7 +80,7 @@ pipeline {
                             sh "docker login ${env.HARBOR_REPO} -u '${CI_REGISTRY_USERNAME}' -p '${CI_REGISTRY_PASSWORD}'"
                             sh """
                                 export MILVUS_HARBOR_IMAGE_REPO="${env.HARBOR_REPO}/milvus/milvus"
-                                export MILVUS_IMAGE_TAG="${env.BRANCH_NAME}-${date}-${gitShortCommit}-gpu-arm"
+                                export MILVUS_IMAGE_TAG="${tag}"
                                 docker push \${MILVUS_HARBOR_IMAGE_REPO}:\${MILVUS_IMAGE_TAG}
                                 docker logout
                             """
