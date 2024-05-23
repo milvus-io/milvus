d6bc95de55670fc2d4e67fc3156bd39f60eb3865
congqixia <congqi.xia@zilliz.com>
2024-05-23 15:27:41 +0800

    enhance: [2.4] Add param item to ignore bad message id in checkpoint (#33123) (#33249)
    Cherry-pick from master
    pr: #33123 #33158
    See also  #33122
    
    This pr add param item `mq.ignoreBadPosition` to control behavior when
    mq failed to parse message id from checkpoint
    
    ---------
    
    Signed-off-by: Congqi Xia <congqi.xia@zilliz.com>
    
    ---------
    
    Signed-off-by: Congqi Xia <congqi.xia@zilliz.com>

--- pkg/mq/msgstream/mq_msgstream.go
+++ pkg/mq/msgstream/mq_msgstream.go
@@ -481,7 +481,16 @@ func (ms *mqMsgStream) Seek(ctx context.Context, msgPositions []*msgpb.MsgPositi
 		}
 		messageID, err := ms.client.BytesToMsgID(mp.MsgID)
 		if err != nil {
-			return err
+			if paramtable.Get().MQCfg.IgnoreBadPosition.GetAsBool() {
+				// try to use latest message ID first
+				messageID, err = consumer.GetLatestMsgID()
+				if err != nil {
+					log.Ctx(ctx).Warn("Ignoring bad message id", zap.Error(err))
+					continue
+				}
+			} else {
+				return err
+			}
 		}
 
 		log.Info("MsgStream seek begin", zap.String("channel", mp.ChannelName), zap.Any("MessageID", mp.MsgID))
@@ -835,34 +844,44 @@ func (ms *MqTtMsgStream) Seek(ctx context.Context, msgPositions []*msgpb.MsgPosi
 	var consumer mqwrapper.Consumer
 	var mp *MsgPosition
 	var err error
-	fn := func() error {
+	fn := func() (bool, error) {
 		var ok bool
 		consumer, ok = ms.consumers[mp.ChannelName]
 		if !ok {
-			return fmt.Errorf("please subcribe the channel, channel name =%s", mp.ChannelName)
+			return false, fmt.Errorf("please subcribe the channel, channel name =%s", mp.ChannelName)
 		}
 
 		if consumer == nil {
-			return fmt.Errorf("consumer is nil")
+			return false, fmt.Errorf("consumer is nil")
 		}
 
 		seekMsgID, err := ms.client.BytesToMsgID(mp.MsgID)
 		if err != nil {
-			return err
+			if paramtable.Get().MQCfg.IgnoreBadPosition.GetAsBool() {
+				// try to use latest message ID first
+				seekMsgID, err = consumer.GetLatestMsgID()
+				if err != nil {
+					log.Ctx(ctx).Warn("Ignoring bad message id", zap.Error(err))
+					return false, nil
+				}
+			} else {
+				return false, err
+			}
 		}
+
 		log.Info("MsgStream begin to seek start msg: ", zap.String("channel", mp.ChannelName), zap.Any("MessageID", mp.MsgID))
 		err = consumer.Seek(seekMsgID, true)
 		if err != nil {
 			log.Warn("Failed to seek", zap.String("channel", mp.ChannelName), zap.Error(err))
 			// stop retry if consumer topic not exist
 			if errors.Is(err, merr.ErrMqTopicNotFound) {
-				return retry.Unrecoverable(err)
+				return false, err
 			}
-			return err
+			return true, err
 		}
 		log.Info("MsgStream seek finished", zap.String("channel", mp.ChannelName))
 
-		return nil
+		return false, nil
 	}
 
 	ms.consumerLock.Lock()
@@ -873,7 +892,8 @@ func (ms *MqTtMsgStream) Seek(ctx context.Context, msgPositions []*msgpb.MsgPosi
 		if len(mp.MsgID) == 0 {
 			return fmt.Errorf("when msgID's length equal to 0, please use AsConsumer interface")
 		}
-		err = retry.Do(ctx, fn, retry.Attempts(20), retry.Sleep(time.Millisecond*200), retry.MaxSleepTime(5*time.Second))
+		err = retry.Handle(ctx, fn, retry.Attempts(20), retry.Sleep(time.Millisecond*200), retry.MaxSleepTime(5*time.Second))
+		// err = retry.Do(ctx, fn, retry.Attempts(20), retry.Sleep(time.Millisecond*200), retry.MaxSleepTime(5*time.Second))
 		if err != nil {
 			return fmt.Errorf("failed to seek, error %s", err.Error())
 		}
