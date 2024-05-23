3ba4270c3937fc9ab2aac4eabfcea4c1a2f66939
bigsheeper <yihao.dai@zilliz.com>
2024-05-23 20:27:15 +0800

    fix: Fix frequent 'failed to get request info' log
    Signed-off-by: bigsheeper <yihao.dai@zilliz.com>

--- internal/proxy/rate_limit_interceptor_test.go
+++ internal/proxy/rate_limit_interceptor_test.go
@@ -257,7 +257,7 @@ func TestRateLimitInterceptor(t *testing.T) {
 		assert.Error(t, err)
 
 		_, _, _, _, err = getRequestInfo(context.Background(), &milvuspb.CalcDistanceRequest{})
-		assert.Error(t, err)
+		assert.NoError(t, err)
 	})
 
 	t.Run("test getFailedResponse", func(t *testing.T) {
