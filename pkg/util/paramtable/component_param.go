7384bfe3f80b1cdd306af99b847593d4223de650
yihao.dai <yihao.dai@zilliz.com>
2024-05-28 19:27:43 +0800

    fix: use seperate warmup pool and disable warmup by default (#33348) (#33349)
    1. use a small warmup pool to reduce the impact of warmup
    2. change the warmup pool to nonblocking mode
    3. disable warmup by default
    4. remove the maximum size limit of 16 for the load pool
    
    issue: https://github.com/milvus-io/milvus/issues/32772
    
    pr: https://github.com/milvus-io/milvus/pull/33348
    
    ---------
    
    Signed-off-by: bigsheeper <yihao.dai@zilliz.com>
    Co-authored-by: xiaofanluan <xiaofan.luan@zilliz.com>

--- pkg/util/paramtable/component_param.go
+++ pkg/util/paramtable/component_param.go
@@ -2347,13 +2347,13 @@ func (p *queryNodeConfig) init(base *BaseTable) {
 	p.ChunkCacheWarmingUp = ParamItem{
 		Key:          "queryNode.cache.warmup",
 		Version:      "2.3.6",
-		DefaultValue: "async",
-		Doc: `options: async, sync, off. 
+		DefaultValue: "disable",
+		Doc: `options: async, sync, disable. 
 Specifies the necessity for warming up the chunk cache. 
-1. If set to "sync" or "async," the original vector data will be synchronously/asynchronously loaded into the 
+1. If set to "sync" or "async" the original vector data will be synchronously/asynchronously loaded into the 
 chunk cache during the load process. This approach has the potential to substantially reduce query/search latency
 for a specific duration post-load, albeit accompanied by a concurrent increase in disk usage;
-2. If set to "off," original vector data will only be loaded into the chunk cache during search/query.`,
+2. If set to "disable" original vector data will only be loaded into the chunk cache during search/query.`,
 		Export: true,
 	}
 	p.ChunkCacheWarmingUp.Init(base.mgr)
