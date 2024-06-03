2b285e5573dae58ec350c35a6c6f550a639c03cc
congqixia <congqi.xia@zilliz.com>
2024-06-03 19:25:51 +0800

    fix: Wrap init segcore tracing with golang timeout (#33494)
    See also #33483
    
    Wrap `C.InitTrace` & `C.SetTrace` with timeout preventing otlp
    initializtion hangs forever when endpoint is not set correctly
    
    ---------
    
    Signed-off-by: Congqi Xia <congqi.xia@zilliz.com>

--- internal/util/initcore/init_core.go
+++ internal/util/initcore/init_core.go
@@ -29,6 +29,7 @@ import "C"
 
 import (
 	"fmt"
+	"time"
 	"unsafe"
 
 	"github.com/cockroachdb/errors"
@@ -61,7 +62,13 @@ func InitTraceConfig(params *paramtable.ComponentParam) {
 		otlpEndpoint:   endpoint,
 		nodeID:         nodeID,
 	}
-	C.InitTrace(&config)
+	// oltp grpc may hangs forever, add timeout logic at go side
+	timeout := params.TraceCfg.InitTimeoutSeconds.GetAsDuration(time.Second)
+	callWithTimeout(func() {
+		C.InitTrace(&config)
+	}, func() {
+		panic("init segcore tracing timeout, See issue #33483")
+	}, timeout)
 }
 
 func ResetTraceConfig(params *paramtable.ComponentParam) {
@@ -81,7 +88,31 @@ func ResetTraceConfig(params *paramtable.ComponentParam) {
 		otlpEndpoint:   endpoint,
 		nodeID:         nodeID,
 	}
-	C.SetTrace(&config)
+
+	// oltp grpc may hangs forever, add timeout logic at go side
+	timeout := params.TraceCfg.InitTimeoutSeconds.GetAsDuration(time.Second)
+	callWithTimeout(func() {
+		C.SetTrace(&config)
+	}, func() {
+		panic("set segcore tracing timeout, See issue #33483")
+	}, timeout)
+}
+
+func callWithTimeout(fn func(), timeoutHandler func(), timeout time.Duration) {
+	if timeout > 0 {
+		ch := make(chan struct{})
+		go func() {
+			defer close(ch)
+			fn()
+		}()
+		select {
+		case <-ch:
+		case <-time.After(timeout):
+			timeoutHandler()
+		}
+	} else {
+		fn()
+	}
 }
 
 func InitRemoteChunkManager(params *paramtable.ComponentParam) error {
