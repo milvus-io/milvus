1f99ca292d605a41b0c6a703e2dca84f3cf35b73
coldWater <254244460@qq.com>
2024-06-04 22:42:39 +0800

    client
    Signed-off-by: coldWater <254244460@qq.com>

---
+++ client/internal/protobr/protobr.go
@@ -0,0 +1,26 @@
+package protobr
+
+import (
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/protoadapt"
+)
+
+// Deprecated: remove later
+func Marshal(m protoadapt.MessageV1) ([]byte, error) {
+	return proto.Marshal(protoadapt.MessageV2Of(m))
+}
+
+// Deprecated: remove later
+func Unmarshal(b []byte, m protoadapt.MessageV1) error {
+	return proto.Unmarshal(b, protoadapt.MessageV2Of(m))
+}
+
+// Deprecated: remove later
+func Size(m protoadapt.MessageV1) int {
+	return proto.Size(protoadapt.MessageV2Of(m))
+}
+
+// Deprecated: remove later
+func Equal(m1, m2 protoadapt.MessageV1) bool {
+	return proto.Equal(protoadapt.MessageV2Of(m1), protoadapt.MessageV2Of(m2))
+}
