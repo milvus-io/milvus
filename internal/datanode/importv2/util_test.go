c3d2013931587c75b922b0a9e8d79b16ca875d2b
bigsheeper <yihao.dai@zilliz.com>
2024-06-07 10:29:03 +0800

    update ut
    Signed-off-by: bigsheeper <yihao.dai@zilliz.com>

--- internal/datanode/importv2/util_test.go
+++ internal/datanode/importv2/util_test.go
@@ -149,7 +149,7 @@ func Test_PickSegment(t *testing.T) {
 	importedSize := map[int64]int{}
 
 	totalSize := 8 * 1024 * 1024 * 1024
-	batchSize := 16 * 1024 * 1024
+	batchSize := 1 * 1024 * 1024
 
 	for totalSize > 0 {
 		picked := PickSegment(task, vchannel, partitionID)
