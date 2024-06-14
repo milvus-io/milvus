562369627de9dd28a165feb30096319cf440bc3c
cqy123456 <39671710+cqy123456@users.noreply.github.com>
2024-06-14 06:45:58 -0500

    enhance: [cherry-pick]check index with data type in knowhere api (#33878)
    issue: https://github.com/milvus-io/milvus/issues/22837
    related: https://github.com/milvus-io/milvus/pull/33880
    
    Signed-off-by: cqy123456 <qianya.cheng@zilliz.com>

--- internal/proxy/task_index.go
+++ internal/proxy/task_index.go
@@ -344,7 +344,12 @@ func fillDimension(field *schemapb.FieldSchema, indexParams map[string]string) e
 
 func checkTrain(field *schemapb.FieldSchema, indexParams map[string]string) error {
 	indexType := indexParams[common.IndexTypeKey]
-
+	if typeutil.IsVectorType(field.DataType) && indexType != indexparamcheck.AutoIndex {
+		exist := indexparamcheck.CheckVecIndexWithDataTypeExist(indexType, field.DataType)
+		if !exist {
+			return fmt.Errorf("data type %d can't build with this index %s", field.DataType, indexType)
+		}
+	}
 	checker, err := indexparamcheck.GetIndexCheckerMgrInstance().GetChecker(indexType)
 	if err != nil {
 		log.Warn("Failed to get index checker", zap.String(common.IndexTypeKey, indexType))
