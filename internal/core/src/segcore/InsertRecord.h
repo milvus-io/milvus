873c7840b4a0334467487e7a75a22391cc3ee835
congqixia <congqi.xia@zilliz.com>
2024-06-18 15:55:59 +0800

    fix: Return record with largest timestamp for entires with same PK (#33936)
    See also #33883
    
    Signed-off-by: Congqi Xia <congqi.xia@zilliz.com>

--- internal/core/src/segcore/InsertRecord.h
+++ internal/core/src/segcore/InsertRecord.h
@@ -112,9 +112,9 @@ class OffsetOrderedMap : public OffsetMap {
                bool false_filtered_out) const override {
         std::shared_lock<std::shared_mutex> lck(mtx_);
 
-        if (limit == Unlimited || limit == NoLimit) {
-            limit = map_.size();
-        }
+        // if (limit == Unlimited || limit == NoLimit) {
+        //     limit = map_.size();
+        // }
 
         // TODO: we can't retrieve pk by offset very conveniently.
         //      Selectivity should be done outside.
@@ -131,6 +131,9 @@ class OffsetOrderedMap : public OffsetMap {
         auto size = bitset.size();
         if (!false_filtered_out) {
             cnt = size - bitset.count();
+        }
+        if (limit == Unlimited || limit == NoLimit) {
+            limit = cnt;
         }
         limit = std::min(limit, cnt);
         std::vector<int64_t> seg_offsets;
