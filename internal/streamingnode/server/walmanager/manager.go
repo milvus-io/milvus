a629dd48ffc23756d03ea7356e88ca906ae4bfae
chyezh <chyezh@outlook.com>
2024-06-25 16:12:34 +0800

    enhance: implement wal managerment
    Signed-off-by: chyezh <chyezh@outlook.com>

---
+++ internal/streamingnode/server/walmanager/manager.go
@@ -0,0 +1,29 @@
+package walmanager
+
+import (
+	"context"
+
+	"github.com/milvus-io/milvus/internal/streamingnode/server/wal"
+	"github.com/milvus-io/milvus/pkg/streaming/util/types"
+)
+
+var _ Manager = (*managerImpl)(nil)
+
+// Manager is the interface for managing the wal instances.
+type Manager interface {
+	// Open opens a wal instance for the channel on this Manager.
+	Open(ctx context.Context, channel types.PChannelInfo) error
+
+	// GetAvailableWAL returns a available wal instance for the channel.
+	// Return nil if the wal instance is not found.
+	GetAvailableWAL(channelName string, term int64) (wal.WAL, error)
+
+	// GetAllAvailableWALInfo returns all available channel info.
+	GetAllAvailableChannels() ([]types.PChannelInfo, error)
+
+	// Remove removes the wal instance for the channel.
+	Remove(ctx context.Context, channel string, term int64) error
+
+	// Close these manager and release all managed WAL.
+	Close()
+}
--- pkg/streaming/util/types/pchannel_info.go
+++ pkg/streaming/util/types/pchannel_info.go
@@ -1,5 +1,9 @@
 package types
 
+const (
+	InitialTerm int64 = -1
+)
+
 // PChannelInfo is the struct for pchannel info.
 type PChannelInfo struct {
 	Name     string // name of pchannel.
