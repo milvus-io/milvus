ba2c232331d6d332886870662690b5042fd2b86c
PowderLi <135960789+PowderLi@users.noreply.github.com>
2024-07-05 09:52:10 +0800

    fix: [cherry-pick] [restful v2] count(*) & hook (#34433)
    issue: #31224 #34374
    pr: #34369
    
    for query api:
    
    1. param filter is not requried
    2. param limit is useless while outputFields = [count(*)]
    
    add hook about grpc call
    
    ---------
    
    Signed-off-by: PowderLi <min.li@zilliz.com>

--- internal/proxy/hook_interceptor.go
+++ internal/proxy/hook_interceptor.go
@@ -18,44 +18,49 @@ import (
 var hoo hook.Hook
 
 func UnaryServerHookInterceptor() grpc.UnaryServerInterceptor {
-	hookutil.InitOnceHook()
-	hoo = hookutil.Hoo
 	return func(ctx context.Context, req any, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
-		var (
-			fullMethod = info.FullMethod
-			newCtx     context.Context
-			isMock     bool
-			mockResp   interface{}
-			realResp   interface{}
-			realErr    error
-			err        error
-		)
-
-		if isMock, mockResp, err = hoo.Mock(ctx, req, fullMethod); isMock {
-			log.Info("hook mock", zap.String("user", getCurrentUser(ctx)),
-				zap.String("full method", fullMethod), zap.Error(err))
-			metrics.ProxyHookFunc.WithLabelValues(metrics.HookMock, fullMethod).Inc()
-			updateProxyFunctionCallMetric(fullMethod)
-			return mockResp, err
-		}
-
-		if newCtx, err = hoo.Before(ctx, req, fullMethod); err != nil {
-			log.Warn("hook before error", zap.String("user", getCurrentUser(ctx)), zap.String("full method", fullMethod),
-				zap.Any("request", req), zap.Error(err))
-			metrics.ProxyHookFunc.WithLabelValues(metrics.HookBefore, fullMethod).Inc()
-			updateProxyFunctionCallMetric(fullMethod)
-			return nil, err
-		}
-		realResp, realErr = handler(newCtx, req)
-		if err = hoo.After(newCtx, realResp, realErr, fullMethod); err != nil {
-			log.Warn("hook after error", zap.String("user", getCurrentUser(ctx)), zap.String("full method", fullMethod),
-				zap.Any("request", req), zap.Error(err))
-			metrics.ProxyHookFunc.WithLabelValues(metrics.HookAfter, fullMethod).Inc()
-			updateProxyFunctionCallMetric(fullMethod)
-			return nil, err
-		}
-		return realResp, realErr
+		return HookInterceptor(ctx, req, getCurrentUser(ctx), info.FullMethod, handler)
 	}
+}
+
+func HookInterceptor(ctx context.Context, req any, userName, fullMethod string, handler grpc.UnaryHandler) (interface{}, error) {
+	if hoo == nil {
+		hookutil.InitOnceHook()
+		hoo = hookutil.Hoo
+	}
+	var (
+		newCtx   context.Context
+		isMock   bool
+		mockResp interface{}
+		realResp interface{}
+		realErr  error
+		err      error
+	)
+
+	if isMock, mockResp, err = hoo.Mock(ctx, req, fullMethod); isMock {
+		log.Info("hook mock", zap.String("user", userName),
+			zap.String("full method", fullMethod), zap.Error(err))
+		metrics.ProxyHookFunc.WithLabelValues(metrics.HookMock, fullMethod).Inc()
+		updateProxyFunctionCallMetric(fullMethod)
+		return mockResp, err
+	}
+
+	if newCtx, err = hoo.Before(ctx, req, fullMethod); err != nil {
+		log.Warn("hook before error", zap.String("user", userName), zap.String("full method", fullMethod),
+			zap.Any("request", req), zap.Error(err))
+		metrics.ProxyHookFunc.WithLabelValues(metrics.HookBefore, fullMethod).Inc()
+		updateProxyFunctionCallMetric(fullMethod)
+		return nil, err
+	}
+	realResp, realErr = handler(newCtx, req)
+	if err = hoo.After(newCtx, realResp, realErr, fullMethod); err != nil {
+		log.Warn("hook after error", zap.String("user", userName), zap.String("full method", fullMethod),
+			zap.Any("request", req), zap.Error(err))
+		metrics.ProxyHookFunc.WithLabelValues(metrics.HookAfter, fullMethod).Inc()
+		updateProxyFunctionCallMetric(fullMethod)
+		return nil, err
+	}
+	return realResp, realErr
 }
 
 func updateProxyFunctionCallMetric(fullMethod string) {
