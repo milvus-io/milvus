a3c3d5657c2c8b5925530c8d05a71e181a593263
luzhang <luzhang@zilliz.com>
2024-07-12 18:43:20 +0800

    fix: fix auto merge failure
    Signed-off-by: luzhang <luzhang@zilliz.com>

--- internal/core/src/mmap/Utils.h
+++ internal/core/src/mmap/Utils.h
@@ -148,11 +148,18 @@ WriteFieldData(File& file,
                 break;
             }
             case DataType::VECTOR_SPARSE_FLOAT: {
-                // TODO(SPARSE): this is for mmap to write data to disk so that
-                // the file can be mmaped into memory.
-                PanicInfo(
-                    ErrorCode::NotImplemented,
-                    "WriteFieldData for VECTOR_SPARSE_FLOAT not implemented");
+                for (size_t i = 0; i < data->get_num_rows(); ++i) {
+                    auto vec =
+                        static_cast<const knowhere::sparse::SparseRow<float>*>(
+                            data->RawValue(i));
+                    ssize_t written =
+                        file.Write(vec->data(), vec->data_byte_size());
+                    if (written < vec->data_byte_size()) {
+                        break;
+                    }
+                    total_written += written;
+                }
+                break;
             }
             default:
                 PanicInfo(DataTypeInvalid,
