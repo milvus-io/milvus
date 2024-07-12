ecce55c29b12b20746faf1513505135a6912c2df
Cai Zhang <cai.zhang@zilliz.com>
2024-07-12 18:30:21 +0800

    fix
    Signed-off-by: Cai Zhang <cai.zhang@zilliz.com>

--- internal/datacoord/segment_manager.go
+++ internal/datacoord/segment_manager.go
@@ -286,8 +286,8 @@ func (s *SegmentManager) AllocSegment(ctx context.Context, collectionID UniqueID
 			invalidSegments[segmentID] = struct{}{}
 			continue
 		}
+		validSegments[segmentID] = struct{}{}
 		if !satisfy(segment, collectionID, partitionID, channelName) || !isGrowing(segment) || segment.GetLevel() == datapb.SegmentLevel_L0 {
-			validSegments[segmentID] = struct{}{}
 			continue
 		}
 		segments = append(segments, segment)
