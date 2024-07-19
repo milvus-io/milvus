8e1ac975c7569c869a082896be3d506959eaac77
jaime <yun.zhang@zilliz.com>
2024-07-19 15:49:42 +0800

    fix: unstable ut TestPickAnyNode
    Signed-off-by: jaime <yun.zhang@zilliz.com>

--- internal/datacoord/compaction_test.go
+++ internal/datacoord/compaction_test.go
@@ -346,7 +346,7 @@ func (s *CompactionPlanHandlerSuite) TestPickAnyNode() {
 	s.SetupTest()
 	nodeSlots := map[int64]int64{
 		100: 16,
-		101: 24,
+		101: 23,
 	}
 	node, useSlot := s.handler.pickAnyNode(nodeSlots, &mixCompactionTask{
 		CompactionTask: &datapb.CompactionTask{
