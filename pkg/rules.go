d11f2b3a852146a0959a92f1773389b351711b28
Yellow Shine <sammy.huang@zilliz.com>
2024-07-26 15:01:25 +0800

    .
    Signed-off-by: Yellow Shine <sammy.huang@zilliz.com>

--- pkg/rules.go
+++ pkg/rules.go
@@ -406,4 +406,5 @@ func badlock(m dsl.Matcher) {
 
 	m.Match(`$mu.Lock(); defer $mu.RUnlock()`).Report(`maybe $mu.RLock() was intended?`)
 	m.Match(`$mu.RLock(); defer $mu.Unlock()`).Report(`maybe $mu.Lock() was intended?`)
+
 }
