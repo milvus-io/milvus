Lorseri/123

enhance: add commit time in partitionStats proto
Signed-off-by: wayblink <anyang.wang@zilliz.com>

internal/datacoord/compaction_policy_clustering_test.go
@@ -18,11 +18,14 @@ package datacoord
 import (
 	"context"
 	"testing"
+	"time"
 
 	"github.com/cockroachdb/errors"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/suite"
+	"go.uber.org/atomic"
 
+	"github.com/milvus-io/milvus/internal/metastore/mocks"
 	"github.com/milvus-io/milvus/internal/proto/datapb"
 )
 
@@ -33,12 +36,12 @@ func TestClusteringCompactionPolicySuite(t *testing.T) {
 type ClusteringCompactionPolicySuite struct {
 	suite.Suite
 
-	mockAlloc          *NMockAllocator
-	mockTriggerManager *MockTriggerManager
-	testLabel          *CompactionGroupLabel
-	handler            *NMockHandler
-	mockPlanContext    *MockCompactionPlanContext
-
+	mockAlloc                  *NMockAllocator
+	mockTriggerManager         *MockTriggerManager
+	testLabel                  *CompactionGroupLabel
+	handler                    *NMockHandler
+	mockPlanContext            *MockCompactionPlanContext
+	catalog                    *mocks.DataCoordCatalog
 	clusteringCompactionPolicy *clusteringCompactionPolicy
 }
 
@@ -48,6 +51,11 @@ func (s *ClusteringCompactionPolicySuite) SetupTest() {
 		PartitionID:  10,
 		Channel:      "ch-1",
 	}
+
+	catalog := mocks.NewDataCoordCatalog(s.T())
+	catalog.EXPECT().SavePartitionStatsInfo(mock.Anything, mock.Anything).Return(nil).Maybe()
+	catalog.EXPECT().ListPartitionStatsInfos(mock.Anything).Return(nil, nil).Maybe()
+	s.catalog = catalog
 
 	segments := genSegmentsForMeta(s.testLabel)
 	meta := &meta{segments: NewSegmentsInfo()}
@@ -181,4 +189,115 @@ func (s *ClusteringCompactionPolicySuite) TestCollectionIsClusteringCompacting()
 			})
 		}
 	})
+}
+
+func (s *ClusteringCompactionPolicySuite) TestTimeIntervalLogic() {
+	ctx := context.TODO()
+	collectionID := int64(100)
+	partitionID := int64(101)
+	channel := "ch1"
+
+	tests := []struct {
+		description    string
+		partitionStats []*datapb.PartitionStatsInfo
+		currentVersion int64
+		segments       []*SegmentInfo
+		succeed        bool
+	}{
+		{"no partition stats and not enough new data", []*datapb.PartitionStatsInfo{}, emptyPartitionStatsVersion, []*SegmentInfo{}, false},
+		{"no partition stats and enough new data", []*datapb.PartitionStatsInfo{}, emptyPartitionStatsVersion, []*SegmentInfo{
+			{
+				size: *atomic.NewInt64(1024 * 1024 * 1024 * 10),
+			},
+		}, true},
+		{"very recent partition stats and enough new data",
+			[]*datapb.PartitionStatsInfo{
+				{
+					CollectionID: collectionID,
+					PartitionID:  partitionID,
+					VChannel:     channel,
+					CommitTime:   time.Now().UnixMilli(),
+					Version:      100,
+				},
+			},
+			100,
+			[]*SegmentInfo{
+				{
+					size: *atomic.NewInt64(1024 * 1024 * 1024 * 10),
+				},
+			}, false},
+		{"very old partition stats and not enough new data",
+			[]*datapb.PartitionStatsInfo{
+				{
+					CollectionID: collectionID,
+					PartitionID:  partitionID,
+					VChannel:     channel,
+					CommitTime:   time.UnixMilli(1704038400000).UnixMilli(),
+					Version:      100,
+				},
+			},
+			100,
+			[]*SegmentInfo{
+				{
+					size: *atomic.NewInt64(1024),
+				},
+			}, true},
+		{"partition stats and enough new data",
+			[]*datapb.PartitionStatsInfo{
+				{
+					CollectionID: collectionID,
+					PartitionID:  partitionID,
+					VChannel:     channel,
+					CommitTime:   time.Now().Add(-3 * time.Hour).UnixMilli(),
+					SegmentIDs:   []int64{100000},
+					Version:      100,
+				},
+			},
+			100,
+			[]*SegmentInfo{
+				{
+					SegmentInfo: &datapb.SegmentInfo{ID: 9999},
+					size:        *atomic.NewInt64(1024 * 1024 * 1024 * 10),
+				},
+			}, true},
+		{"partition stats and not enough new data",
+			[]*datapb.PartitionStatsInfo{
+				{
+					CollectionID: collectionID,
+					PartitionID:  partitionID,
+					VChannel:     channel,
+					CommitTime:   time.Now().Add(-3 * time.Hour).UnixMilli(),
+					SegmentIDs:   []int64{100000},
+					Version:      100,
+				},
+			},
+			100,
+			[]*SegmentInfo{
+				{
+					SegmentInfo: &datapb.SegmentInfo{ID: 9999},
+					size:        *atomic.NewInt64(1024),
+				},
+			}, false},
+	}
+
+	for _, test := range tests {
+		s.Run(test.description, func() {
+			partitionStatsMeta, err := newPartitionStatsMeta(ctx, s.catalog)
+			s.NoError(err)
+			for _, partitionStats := range test.partitionStats {
+				partitionStatsMeta.SavePartitionStatsInfo(partitionStats)
+			}
+			if test.currentVersion != 0 {
+				partitionStatsMeta.partitionStatsInfos[channel][partitionID].currentVersion = test.currentVersion
+			}
+
+			meta := &meta{
+				partitionStatsMeta: partitionStatsMeta,
+			}
+
+			succeed, err := triggerClusteringCompactionPolicy(ctx, meta, collectionID, partitionID, channel, test.segments)
+			s.NoError(err)
+			s.Equal(test.succeed, succeed)
+		})
+	}
 }

internal/datacoord/compaction_task_clustering.go
@@ -302,6 +302,7 @@ func (t *clusteringCompactionTask) completeTask() error {
 		VChannel:     t.GetChannel(),
 		Version:      t.GetPlanID(),
 		SegmentIDs:   t.GetResultSegments(),
+		CommitTime:   time.Now().UnixMilli(),
 	})
 	if err != nil {
 		return merr.WrapErrClusteringCompactionMetaError("SavePartitionStatsInfo", err)

internal/datacoord/compaction_task_clustering_test.go
@@ -355,6 +355,14 @@ func (s *ClusteringCompactionTaskSuite) TestProcessExecutingState() {
 	s.Equal(datapb.CompactionTaskState_failed, task.GetState())
 }
 
+// fix: https://github.com/milvus-io/milvus/issues/35110
+func (s *ClusteringCompactionTaskSuite) TestCompleteTask() {
+	task := s.generateBasicTask()
+	task.completeTask()
+	partitionStats := s.meta.GetPartitionStatsMeta().GetPartitionStats(task.GetCollectionID(), task.GetPartitionID(), task.GetChannel(), task.GetPlanID())
+	s.True(partitionStats.GetCommitTime() > time.Now().Add(-2*time.Second).UnixMilli())
+}
+
 const (
 	Int64Field    = "int64Field"
 	FloatVecField = "floatVecField"

internal/datacoord/partition_stats_meta_test.go
@@ -1,0 +1,83 @@
+// Licensed to the LF AI & Data foundation under one
+// or more contributor license agreements. See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership. The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License. You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package datacoord
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/suite"
+
+	"github.com/milvus-io/milvus/internal/metastore/mocks"
+	"github.com/milvus-io/milvus/internal/proto/datapb"
+)
+
+type PartitionStatsMetaSuite struct {
+	suite.Suite
+
+	catalog *mocks.DataCoordCatalog
+	meta    *partitionStatsMeta
+}
+
+func TestPartitionStatsMetaSuite(t *testing.T) {
+	suite.Run(t, new(PartitionStatsMetaSuite))
+}
+
+func (s *PartitionStatsMetaSuite) SetupTest() {
+	catalog := mocks.NewDataCoordCatalog(s.T())
+	catalog.EXPECT().SavePartitionStatsInfo(mock.Anything, mock.Anything).Return(nil).Maybe()
+	catalog.EXPECT().ListPartitionStatsInfos(mock.Anything).Return(nil, nil).Maybe()
+	s.catalog = catalog
+}
+
+func (s *PartitionStatsMetaSuite) TestGetPartitionStats() {
+	ctx := context.Background()
+	partitionStatsMeta, err := newPartitionStatsMeta(ctx, s.catalog)
+	s.NoError(err)
+	partitionStats := []*datapb.PartitionStatsInfo{
+		{
+			CollectionID: 1,
+			PartitionID:  2,
+			VChannel:     "ch-1",
+			SegmentIDs:   []int64{100000},
+			Version:      100,
+		},
+	}
+	for _, partitionStats := range partitionStats {
+		partitionStatsMeta.SavePartitionStatsInfo(partitionStats)
+	}
+
+	ps := partitionStatsMeta.GetPartitionStats(1, 2, "ch-1", 100)
+	s.NotNil(ps)
+
+	ps2 := partitionStatsMeta.GetPartitionStats(1, 2, "ch-1", 101)
+	s.Nil(ps2)
+
+	currentVersion := partitionStatsMeta.GetCurrentPartitionStatsVersion(1, 2, "ch-1")
+	s.Equal(emptyPartitionStatsVersion, currentVersion)
+
+	currentVersion2 := partitionStatsMeta.GetCurrentPartitionStatsVersion(1, 2, "ch-2")
+	s.Equal(emptyPartitionStatsVersion, currentVersion2)
+
+	currentVersion3 := partitionStatsMeta.GetCurrentPartitionStatsVersion(1, 3, "ch-1")
+	s.Equal(emptyPartitionStatsVersion, currentVersion3)
+
+	partitionStatsMeta.partitionStatsInfos["ch-1"][2].currentVersion = 100
+	currentVersion4 := partitionStatsMeta.GetCurrentPartitionStatsVersion(1, 2, "ch-1")
+	s.Equal(int64(100), currentVersion4)
+}

milvus-io/milvus

internal/datacoord/compaction_policy_clustering_test.go
@@ -18,11 +18,14 @@ package datacoord
 import (
 	"context"
 	"testing"
+	"time"
 
 	"github.com/cockroachdb/errors"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/suite"
+	"go.uber.org/atomic"
 
+	"github.com/milvus-io/milvus/internal/metastore/mocks"
 	"github.com/milvus-io/milvus/internal/proto/datapb"
 )
 
@@ -33,12 +36,12 @@ func TestClusteringCompactionPolicySuite(t *testing.T) {
 type ClusteringCompactionPolicySuite struct {
 	suite.Suite
 
-	mockAlloc          *NMockAllocator
-	mockTriggerManager *MockTriggerManager
-	testLabel          *CompactionGroupLabel
-	handler            *NMockHandler
-	mockPlanContext    *MockCompactionPlanContext
-
+	mockAlloc                  *NMockAllocator
+	mockTriggerManager         *MockTriggerManager
+	testLabel                  *CompactionGroupLabel
+	handler                    *NMockHandler
+	mockPlanContext            *MockCompactionPlanContext
+	catalog                    *mocks.DataCoordCatalog
 	clusteringCompactionPolicy *clusteringCompactionPolicy
 }
 
@@ -48,6 +51,11 @@ func (s *ClusteringCompactionPolicySuite) SetupTest() {
 		PartitionID:  10,
 		Channel:      "ch-1",
 	}
+
+	catalog := mocks.NewDataCoordCatalog(s.T())
+	catalog.EXPECT().SavePartitionStatsInfo(mock.Anything, mock.Anything).Return(nil).Maybe()
+	catalog.EXPECT().ListPartitionStatsInfos(mock.Anything).Return(nil, nil).Maybe()
+	s.catalog = catalog
 
 	segments := genSegmentsForMeta(s.testLabel)
 	meta := &meta{segments: NewSegmentsInfo()}
@@ -181,4 +189,115 @@ func (s *ClusteringCompactionPolicySuite) TestCollectionIsClusteringCompacting()
 			})
 		}
 	})
+}
+
+func (s *ClusteringCompactionPolicySuite) TestTimeIntervalLogic() {
+	ctx := context.TODO()
+	collectionID := int64(100)
+	partitionID := int64(101)
+	channel := "ch1"
+
+	tests := []struct {
+		description    string
+		partitionStats []*datapb.PartitionStatsInfo
+		currentVersion int64
+		segments       []*SegmentInfo
+		succeed        bool
+	}{
+		{"no partition stats and not enough new data", []*datapb.PartitionStatsInfo{}, emptyPartitionStatsVersion, []*SegmentInfo{}, false},
+		{"no partition stats and enough new data", []*datapb.PartitionStatsInfo{}, emptyPartitionStatsVersion, []*SegmentInfo{
+			{
+				size: *atomic.NewInt64(1024 * 1024 * 1024 * 10),
+			},
+		}, true},
+		{"very recent partition stats and enough new data",
+			[]*datapb.PartitionStatsInfo{
+				{
+					CollectionID: collectionID,
+					PartitionID:  partitionID,
+					VChannel:     channel,
+					CommitTime:   time.Now().Unix(),
+					Version:      100,
+				},
+			},
+			100,
+			[]*SegmentInfo{
+				{
+					size: *atomic.NewInt64(1024 * 1024 * 1024 * 10),
+				},
+			}, false},
+		{"very old partition stats and not enough new data",
+			[]*datapb.PartitionStatsInfo{
+				{
+					CollectionID: collectionID,
+					PartitionID:  partitionID,
+					VChannel:     channel,
+					CommitTime:   time.Unix(1704038400, 0).Unix(),
+					Version:      100,
+				},
+			},
+			100,
+			[]*SegmentInfo{
+				{
+					size: *atomic.NewInt64(1024),
+				},
+			}, true},
+		{"partition stats and enough new data",
+			[]*datapb.PartitionStatsInfo{
+				{
+					CollectionID: collectionID,
+					PartitionID:  partitionID,
+					VChannel:     channel,
+					CommitTime:   time.Now().Add(-3 * time.Hour).Unix(),
+					SegmentIDs:   []int64{100000},
+					Version:      100,
+				},
+			},
+			100,
+			[]*SegmentInfo{
+				{
+					SegmentInfo: &datapb.SegmentInfo{ID: 9999},
+					size:        *atomic.NewInt64(1024 * 1024 * 1024 * 10),
+				},
+			}, true},
+		{"partition stats and not enough new data",
+			[]*datapb.PartitionStatsInfo{
+				{
+					CollectionID: collectionID,
+					PartitionID:  partitionID,
+					VChannel:     channel,
+					CommitTime:   time.Now().Add(-3 * time.Hour).Unix(),
+					SegmentIDs:   []int64{100000},
+					Version:      100,
+				},
+			},
+			100,
+			[]*SegmentInfo{
+				{
+					SegmentInfo: &datapb.SegmentInfo{ID: 9999},
+					size:        *atomic.NewInt64(1024),
+				},
+			}, false},
+	}
+
+	for _, test := range tests {
+		s.Run(test.description, func() {
+			partitionStatsMeta, err := newPartitionStatsMeta(ctx, s.catalog)
+			s.NoError(err)
+			for _, partitionStats := range test.partitionStats {
+				partitionStatsMeta.SavePartitionStatsInfo(partitionStats)
+			}
+			if test.currentVersion != 0 {
+				partitionStatsMeta.partitionStatsInfos[channel][partitionID].currentVersion = test.currentVersion
+			}
+
+			meta := &meta{
+				partitionStatsMeta: partitionStatsMeta,
+			}
+
+			succeed, err := triggerClusteringCompactionPolicy(ctx, meta, collectionID, partitionID, channel, test.segments)
+			s.NoError(err)
+			s.Equal(test.succeed, succeed)
+		})
+	}
 }

internal/datacoord/compaction_task_clustering.go
@@ -302,6 +302,7 @@ func (t *clusteringCompactionTask) completeTask() error {
 		VChannel:     t.GetChannel(),
 		Version:      t.GetPlanID(),
 		SegmentIDs:   t.GetResultSegments(),
+		CommitTime:   time.Now().Unix(),
 	})
 	if err != nil {
 		return merr.WrapErrClusteringCompactionMetaError("SavePartitionStatsInfo", err)

internal/datacoord/compaction_task_clustering_test.go
@@ -355,6 +355,14 @@ func (s *ClusteringCompactionTaskSuite) TestProcessExecutingState() {
 	s.Equal(datapb.CompactionTaskState_failed, task.GetState())
 }
 
+// fix: https://github.com/milvus-io/milvus/issues/35110
+func (s *ClusteringCompactionTaskSuite) TestCompleteTask() {
+	task := s.generateBasicTask()
+	task.completeTask()
+	partitionStats := s.meta.GetPartitionStatsMeta().GetPartitionStats(task.GetCollectionID(), task.GetPartitionID(), task.GetChannel(), task.GetPlanID())
+	s.True(partitionStats.GetCommitTime() > time.Now().Add(-2*time.Second).Unix())
+}
+
 const (
 	Int64Field    = "int64Field"
 	FloatVecField = "floatVecField"

internal/datacoord/partition_stats_meta_test.go
@@ -1,0 +1,89 @@
+// Licensed to the LF AI & Data foundation under one
+// or more contributor license agreements. See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership. The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License. You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package datacoord
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/suite"
+
+	"github.com/milvus-io/milvus/internal/metastore/mocks"
+	"github.com/milvus-io/milvus/internal/proto/datapb"
+)
+
+type PartitionStatsMetaSuite struct {
+	suite.Suite
+
+	catalog *mocks.DataCoordCatalog
+	meta    *partitionStatsMeta
+}
+
+func TestPartitionStatsMetaSuite(t *testing.T) {
+	suite.Run(t, new(PartitionStatsMetaSuite))
+}
+
+func (s *PartitionStatsMetaSuite) SetupTest() {
+	catalog := mocks.NewDataCoordCatalog(s.T())
+	catalog.EXPECT().SavePartitionStatsInfo(mock.Anything, mock.Anything).Return(nil).Maybe()
+	catalog.EXPECT().ListPartitionStatsInfos(mock.Anything).Return(nil, nil).Maybe()
+	s.catalog = catalog
+}
+
+func (s *PartitionStatsMetaSuite) TestGetPartitionStats() {
+	ctx := context.Background()
+	partitionStatsMeta, err := newPartitionStatsMeta(ctx, s.catalog)
+	s.NoError(err)
+	partitionStats := []*datapb.PartitionStatsInfo{
+		{
+			CollectionID: 1,
+			PartitionID:  2,
+			VChannel:     "ch-1",
+			SegmentIDs:   []int64{100000},
+			Version:      100,
+		},
+	}
+	for _, partitionStats := range partitionStats {
+		partitionStatsMeta.SavePartitionStatsInfo(partitionStats)
+	}
+
+	ps1 := partitionStatsMeta.GetPartitionStats(1, 2, "ch-2", 100)
+	s.Nil(ps1)
+
+	ps2 := partitionStatsMeta.GetPartitionStats(1, 3, "ch-1", 100)
+	s.Nil(ps2)
+
+	ps3 := partitionStatsMeta.GetPartitionStats(1, 2, "ch-1", 101)
+	s.Nil(ps3)
+
+	ps := partitionStatsMeta.GetPartitionStats(1, 2, "ch-1", 100)
+	s.NotNil(ps)
+
+	currentVersion := partitionStatsMeta.GetCurrentPartitionStatsVersion(1, 2, "ch-1")
+	s.Equal(emptyPartitionStatsVersion, currentVersion)
+
+	currentVersion2 := partitionStatsMeta.GetCurrentPartitionStatsVersion(1, 2, "ch-2")
+	s.Equal(emptyPartitionStatsVersion, currentVersion2)
+
+	currentVersion3 := partitionStatsMeta.GetCurrentPartitionStatsVersion(1, 3, "ch-1")
+	s.Equal(emptyPartitionStatsVersion, currentVersion3)
+
+	partitionStatsMeta.partitionStatsInfos["ch-1"][2].currentVersion = 100
+	currentVersion4 := partitionStatsMeta.GetCurrentPartitionStatsVersion(1, 2, "ch-1")
+	s.Equal(int64(100), currentVersion4)
+}