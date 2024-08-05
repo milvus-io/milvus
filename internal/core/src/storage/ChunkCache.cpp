milvus-io/milvus

commit f8676178f70ff25143c92c3e8760d29e7d8e155b

fix: querynode hang when failing to allocate disk space for mmap(#35184) (#35189)
related: #35184
pr: https://github.com/milvus-io/milvus/pull/35187

Signed-off-by: MrPresent-Han <chun.han@gmail.com>
Co-authored-by: MrPresent-Han <chun.han@gmail.com>

internal/core/src/storage/ChunkCache.cpp
@@ -53,18 +53,36 @@ ChunkCache::Read(const std::string& filepath,
 
     // release lock and perform download and decode
     // other thread request same path shall get the future.
-    auto field_data = DownloadAndDecodeRemoteFile(cm_.get(), filepath);
-    auto column = Mmap(field_data->GetFieldData(), descriptor);
-
-    // set promise value to notify the future
-    lck.lock();
+    std::unique_ptr<DataCodec> field_data;
+    std::shared_ptr<ColumnBase> column;
+    bool allocate_success = false;
+    ErrorCode err_code = Success;
+    std::string err_msg = "";
+    try {
+        field_data = DownloadAndDecodeRemoteFile(cm_.get(), filepath);
+        column = Mmap(field_data->GetFieldData(), descriptor);
+        allocate_success = true;
+    } catch(const SegcoreError& e) {
+        err_code = e.get_error_code();
+        err_msg = fmt::format(
+                "failed to read for chunkCache, seg_core_err:{}",
+                e.what());
+    }
+    std::unique_lock mmap_lck(mutex_);
     it = columns_.find(filepath);
     if (it != columns_.end()) {
         // check pair exists then set value
         it->second.first.set_value(column);
+        if(allocate_success) {
+            AssertInfo(column, "unexpected null column, file={}", filepath);
+        }
+    } else {
+        PanicInfo(UnexpectedError, "Wrong code, the thread to download for cache should get the target entry");
     }
-    lck.unlock();
-    AssertInfo(column, "unexpected null column, file={}", filepath);
+    if(err_code != Success) {
+        columns_.erase(filepath);
+        throw SegcoreError(err_code, err_msg);
+    }
     return column;
 }
 

commit 98ab3e1dd7b0a4e98443df69f9a257c57c548e64

enhance: [2.4] Resolve chunk cache conflict cpp lint
Resolve cpp lint issue introduced in #35189

Signed-off-by: Congqi Xia <congqi.xia@zilliz.com>

internal/core/src/storage/ChunkCache.cpp
@@ -62,24 +62,25 @@ ChunkCache::Read(const std::string& filepath,
         field_data = DownloadAndDecodeRemoteFile(cm_.get(), filepath);
         column = Mmap(field_data->GetFieldData(), descriptor);
         allocate_success = true;
-    } catch(const SegcoreError& e) {
+    } catch (const SegcoreError& e) {
         err_code = e.get_error_code();
-        err_msg = fmt::format(
-                "failed to read for chunkCache, seg_core_err:{}",
-                e.what());
+        err_msg = fmt::format("failed to read for chunkCache, seg_core_err:{}",
+                              e.what());
     }
     std::unique_lock mmap_lck(mutex_);
     it = columns_.find(filepath);
     if (it != columns_.end()) {
         // check pair exists then set value
         it->second.first.set_value(column);
-        if(allocate_success) {
+        if (allocate_success) {
             AssertInfo(column, "unexpected null column, file={}", filepath);
         }
     } else {
-        PanicInfo(UnexpectedError, "Wrong code, the thread to download for cache should get the target entry");
+        PanicInfo(UnexpectedError,
+                  "Wrong code, the thread to download for cache should get the "
+                  "target entry");
     }
-    if(err_code != Success) {
+    if (err_code != Success) {
         columns_.erase(filepath);
         throw SegcoreError(err_code, err_msg);
     }