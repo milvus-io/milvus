23052f6ac26855c291e3f28ced84b97151b8c25d
smellthemoon <64083300+smellthemoon@users.noreply.github.com>
2024-08-12 18:58:25 +0800

    fix: upsert use the previous pk in insert when autoid(#34672) (#35130)
    issue: #34668 
    pr: #34672
    
    Signed-off-by: lixinguo <xinguo.li@zilliz.com>
    Co-authored-by: lixinguo <xinguo.li@zilliz.com>

--- internal/proxy/task_insert.go
+++ internal/proxy/task_insert.go
@@ -169,7 +169,7 @@ func (it *insertTask) PreExecute(ctx context.Context) error {
 	// check primaryFieldData whether autoID is true or not
 	// set rowIDs as primary data if autoID == true
 	// TODO(dragondriver): in fact, NumRows is not trustable, we should check all input fields
-	it.result.IDs, err = checkPrimaryFieldData(it.schema, it.insertMsg, true)
+	it.result.IDs, err = checkPrimaryFieldData(it.schema, it.insertMsg)
 	log := log.Ctx(ctx).With(zap.String("collectionName", collectionName))
 	if err != nil {
 		log.Warn("check primary field data and hash primary key failed",
