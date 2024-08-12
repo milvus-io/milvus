94e7d5382daad16ec290060c90f54c43a6ec590b
Yellow Shine <sammy.huang@zilliz.com>
2024-08-12 15:27:03 +0800

    test with docker
    Signed-off-by: Yellow Shine <sammy.huang@zilliz.com>

--- ci/jenkins/PR-for-go-sdk.groovy
+++ ci/jenkins/PR-for-go-sdk.groovy
@@ -39,41 +39,6 @@ pipeline {
     }
 
     stages {
-        stage('Build') {
-            steps {
-                container('main') {
-                    dir('build') {
-                            sh '''
-                            MIRROR_URL="https://docker-nexus-ci.zilliz.cc" ./set_docker_mirror.sh
-                            '''
-                    }
-                    dir('tests/scripts') {
-                        script {
-                            sh 'printenv'
-                            def date = sh(returnStdout: true, script: 'date +%Y%m%d').trim()
-                            sh 'git config --global --add safe.directory /home/jenkins/agent/workspace'
-                            def gitShortCommit = sh(returnStdout: true, script: 'git rev-parse --short HEAD').trim()
-                            imageTag = "${env.BRANCH_NAME}-${date}-${gitShortCommit}"
-                            withCredentials([usernamePassword(credentialsId: "${env.CI_DOCKER_CREDENTIAL_ID}", usernameVariable: 'CI_REGISTRY_USERNAME', passwordVariable: 'CI_REGISTRY_PASSWORD')]) {
-                                sh """
-                                TAG="${imageTag}" \
-                                ./e2e-k8s.sh \
-                                --skip-export-logs \
-                                --skip-install \
-                                --skip-cleanup \
-                                --skip-setup \
-                                --skip-test
-                                """
-
-                                // stash imageTag info for rebuild install & E2E Test only
-                                sh "echo ${imageTag} > imageTag.txt"
-                                stash includes: 'imageTag.txt', name: 'imageTag'
-                            }
-                        }
-                    }
-                }
-            }
-        }
 
         stage('Install & E2E Test') {
             matrix {
@@ -89,77 +54,6 @@ pipeline {
                 }
 
                 stages {
-                    stage('Install') {
-                        steps {
-                            container('main') {
-                                stash includes: 'tests/**', name: 'testCode', useDefaultExcludes: false
-                                stash includes: 'client/**', name: 'clientCode', useDefaultExcludes: false
-                                dir('tests/scripts') {
-                                    script {
-                                        sh 'printenv'
-                                        def clusterEnabled = 'false'
-                                        def valuesFile = 'pr-4am.yaml'
-                                        if ("${MILVUS_SERVER_TYPE}".contains('distributed')) {
-                                            clusterEnabled = 'true'
-                                        }
-                                            if ("${imageTag}" == '') {
-                                                dir('imageTag') {
-                                                    try {
-                                                        unstash 'imageTag'
-                                                        imageTag = sh(returnStdout: true, script: 'cat imageTag.txt | tr -d \'\n\r\'')
-                                                    }catch (e) {
-                                                        print 'No Image Tag info remained ,please rerun build to build new image.'
-                                                        exit 1
-                                                    }
-                                                }
-                                            }
-                                            // modify values file to enable kafka
-                                            if ("${MILVUS_SERVER_TYPE}".contains('kafka')) {
-                                                sh '''
-                                                    apt-get update
-                                                    apt-get install wget -y
-                                                    wget https://github.com/mikefarah/yq/releases/download/v4.34.1/yq_linux_amd64 -O /usr/bin/yq
-                                                    chmod +x /usr/bin/yq
-                                                '''
-                                                sh """
-                                                    cp values/ci/pr-4am.yaml values/ci/pr_kafka.yaml
-                                                    yq -i '.pulsar.enabled=false' values/ci/pr_kafka.yaml
-                                                    yq -i '.kafka.enabled=true' values/ci/pr_kafka.yaml
-                                                    yq -i '.kafka.metrics.kafka.enabled=true' values/ci/pr_kafka.yaml
-                                                    yq -i '.kafka.metrics.jmx.enabled=true' values/ci/pr_kafka.yaml
-                                                    yq -i '.kafka.metrics.serviceMonitor.enabled=true' values/ci/pr_kafka.yaml
-                                                """
-                                            }
-                                            withCredentials([usernamePassword(credentialsId: "${env.CI_DOCKER_CREDENTIAL_ID}", usernameVariable: 'CI_REGISTRY_USERNAME', passwordVariable: 'CI_REGISTRY_PASSWORD')]) {
-                                                    sh """
-                                                        MILVUS_CLUSTER_ENABLED=${clusterEnabled} \
-                                                        MILVUS_HELM_REPO="https://nexus-ci.zilliz.cc/repository/milvus-proxy" \
-                                                        TAG=${imageTag}\
-                                                        ./e2e-k8s.sh \
-                                                        --skip-export-logs \
-                                                        --skip-cleanup \
-                                                        --skip-setup \
-                                                        --skip-test \
-                                                        --skip-build \
-                                                        --skip-build-image \
-                                                        --install-extra-arg "
-                                                        --set etcd.metrics.enabled=true \
-                                                        --set etcd.metrics.podMonitor.enabled=true \
-                                                        --set indexCoordinator.gc.interval=1 \
-                                                        --set indexNode.disk.enabled=true \
-                                                        --set queryNode.disk.enabled=true \
-                                                        --set standalone.disk.enabled=true \
-                                                        --version ${chart_version} \
-                                                        -f values/ci/${valuesFile}"
-                                                        """
-                                            }
-
-                                            release_name = sh(returnStdout: true, script: './get_release_name.sh').trim()
-                                    }
-                                }
-                            }
-                        }
-                    }
                     stage('E2E Test') {
                         options {
                             skipDefaultCheckout()
@@ -174,28 +68,10 @@ pipeline {
                                 }
                         }
                         steps {
-                            container('gotestsum') {
-                                unstash('testCode')
-                                unstash('clientCode')
-                                dir('tests/scripts') {
-                                    script {
-                                        def clusterEnabled = 'false'
-                                        if ("${MILVUS_SERVER_TYPE}".contains('distributed')) {
-                                            clusterEnabled = 'true'
-                                        }
-                                    }
-                                }
-
-                                dir('tests/go_client') {
-
-                                    script {
-                                        sh """
-                                        gotestsum --format testname --hide-summary=output ./testcases/... --tags L0 --addr=${release_name}-milvus.milvus-ci:19530 -timeout=60m
-                                        """
-
-                                    }
-
-                                }
+                            container('main') {
+                                scripts{
+                                      sh 'sleep 3600'
+                }
                             }
                         }
                     }
