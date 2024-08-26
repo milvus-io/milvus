05b1e8f3a41ec1668054390c54a96b68fdaf7a62
yangxuan <xuan.yang@zilliz.com>
2024-08-26 14:14:44 +0800

    ok
    Signed-off-by: yangxuan <xuan.yang@zilliz.com>

--- internal/datacoord/channel_manager_v2.go
+++ internal/datacoord/channel_manager_v2.go
@@ -699,8 +699,9 @@ func (m *ChannelManagerImplV2) fillChannelWatchInfo(op *ChannelOp) error {
 		}
 
 		schema := ch.GetSchema()
-		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-		if ch.GetSchema() == nil {
+		if schema == nil {
+			ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+			defer cancel()
 			collInfo, err := m.h.GetCollection(ctx, ch.GetCollectionID())
 			if err != nil {
 				return err
@@ -714,6 +715,7 @@ func (m *ChannelManagerImplV2) fillChannelWatchInfo(op *ChannelOp) error {
 			State:   inferStateByOpType(op.Type),
 			Schema:  schema,
 			OpID:    opID,
+		}
 		ch.UpdateWatchInfo(info)
 	}
 	return nil
