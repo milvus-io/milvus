f2caecc577e58af106a85874e3f5a91fa3b0f2b6
XuanYang-cn <xuan.yang@zilliz.com>
2024-08-27 10:38:59 +0800

    fix: [cp]Fill nil schema so that Milvus can watch channel for those upgraded from 2.2 to 2.4 (#35695)
    See also: #35701 
    pr: #35694
    
    ---------
    
    Signed-off-by: yangxuan <xuan.yang@zilliz.com>

--- internal/datacoord/channel_manager_v2.go
+++ internal/datacoord/channel_manager_v2.go
@@ -698,11 +698,22 @@ func (m *ChannelManagerImplV2) fillChannelWatchInfo(op *ChannelOp) error {
 			return err
 		}
 
+		schema := ch.GetSchema()
+		if schema == nil {
+			ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+			defer cancel()
+			collInfo, err := m.h.GetCollection(ctx, ch.GetCollectionID())
+			if err != nil {
+				return err
+			}
+			schema = collInfo.Schema
+		}
+
 		info := &datapb.ChannelWatchInfo{
 			Vchan:   reduceVChanSize(vcInfo),
 			StartTs: startTs,
 			State:   inferStateByOpType(op.Type),
-			Schema:  ch.GetSchema(),
+			Schema:  schema,
 			OpID:    opID,
 		}
 		ch.UpdateWatchInfo(info)
