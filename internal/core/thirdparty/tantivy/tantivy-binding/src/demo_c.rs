a52ba3d09d3e25e91d0e40f9640e5849336098ce
Jiquan Long <jiquan.long@zilliz.com>
2024-08-28 11:30:59 +0800

    enhance: allow many segments for inverted index (#35616)
    fix: https://github.com/milvus-io/milvus/issues/35615
    
    ---------
    
    Signed-off-by: longjiquan <jiquan.long@zilliz.com>

--- internal/core/thirdparty/tantivy/tantivy-binding/src/demo_c.rs
+++ internal/core/thirdparty/tantivy/tantivy-binding/src/demo_c.rs
@@ -1,14 +1,13 @@
-use std::{ffi::{c_char, CStr}, slice};
+use std::{
+    ffi::{c_char, CStr},
+    slice,
+};
 
 #[no_mangle]
 pub extern "C" fn print_vector_of_strings(ptr: *const *const c_char, len: usize) {
-    let arr : &[*const c_char] = unsafe {
-        slice::from_raw_parts(ptr, len)
-    };
-	for element in arr {
-		let c_str = unsafe {
-			CStr::from_ptr(*element)
-		};
-		println!("{}", c_str.to_str().unwrap());
-	}
+    let arr: &[*const c_char] = unsafe { slice::from_raw_parts(ptr, len) };
+    for element in arr {
+        let c_str = unsafe { CStr::from_ptr(*element) };
+        println!("{}", c_str.to_str().unwrap());
+    }
 }
