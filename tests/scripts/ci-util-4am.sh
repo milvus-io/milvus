7847a8e01a4b7434f22c03068a2047971060d590
zhuwenxing <wenxing.zhu@zilliz.com>
2024-09-02 10:31:31 +0800

    update
    Signed-off-by: zhuwenxing <wenxing.zhu@zilliz.com>

--- tests/scripts/ci-util-4am.sh
+++ tests/scripts/ci-util-4am.sh
@@ -44,7 +44,8 @@ export PIP_TRUSTED_HOST="nexus-ci.zilliz.cc"
 export PIP_INDEX_URL="https://nexus-ci.zilliz.cc/repository/pypi-all/simple"
 export PIP_INDEX="https://nexus-ci.zilliz.cc/repository/pypi-all/pypi"
 export PIP_FIND_LINKS="https://nexus-ci.zilliz.cc/repository/pypi-all/pypi"
- python3 -m pip install --no-cache-dir -r requirements.txt --timeout 30 --retries 6 
+ python3 -m pip install --no-cache-dir -r requirements.txt --timeout 30 --retries 6
+ pip uninstall pytest-tags -y || true
 }
 
 # Login in ci docker registry
