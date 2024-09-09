ab3a336bea4844994291641afd5ccf409b23b80f
bigsheeper <yihao.dai@zilliz.com>
2024-09-09 10:04:52 +0800

    code format
    Signed-off-by: bigsheeper <yihao.dai@zilliz.com>

--- internal/core/src/segcore/SegmentSealedImpl.cpp
+++ internal/core/src/segcore/SegmentSealedImpl.cpp
@@ -154,11 +154,11 @@ SegmentSealedImpl::WarmupChunkCache(const FieldId field_id, bool mmap_enabled) {
     auto cc = storage::MmapManager::GetInstance().GetChunkCache();
     const bool mmap_rss_not_need = true;
     for (const auto& data_path : field_info.insert_files) {
-        auto column = cc->Read(data_path, 
-            mmap_descriptor_, 
-            field_meta, 
-            mmap_enabled, 
-            mmap_rss_not_need);
+        auto column = cc->Read(data_path,
+                               mmap_descriptor_,
+                               field_meta,
+                               mmap_enabled,
+                               mmap_rss_not_need);
     }
 }
 
