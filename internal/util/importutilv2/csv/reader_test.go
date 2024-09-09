3a381bc247b8f436c2eaf2980a98c596c07b1f81
OxalisCu <64067746+OxalisCu@users.noreply.github.com>
2024-09-09 19:17:07 +0800

    enhance: Bulkinsert supports null in csv formats (#35912)
    see details in this issue
    https://github.com/milvus-io/milvus/issues/35911
    
    ---------
    
    Signed-off-by: OxalisCu <2127298698@qq.com>

--- internal/util/importutilv2/csv/reader_test.go
+++ internal/util/importutilv2/csv/reader_test.go
@@ -8,7 +8,6 @@ import (
 	"os"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
 
 	"github.com/milvus-io/milvus-proto/go-api/v2/commonpb"
@@ -37,7 +36,7 @@ func (suite *ReaderSuite) SetupTest() {
 	suite.vecDataType = schemapb.DataType_FloatVector
 }
 
-func (suite *ReaderSuite) run(dataType schemapb.DataType, elemType schemapb.DataType) {
+func (suite *ReaderSuite) run(dataType schemapb.DataType, elemType schemapb.DataType, nullable bool) {
 	schema := &schemapb.CollectionSchema{
 		Fields: []*schemapb.FieldSchema{
 			{
@@ -74,25 +73,31 @@ func (suite *ReaderSuite) run(dataType schemapb.DataType, elemType schemapb.Data
 						Value: "128",
 					},
 				},
+				Nullable: nullable,
 			},
 		},
 	}
 
+	// config
+	// csv separator
+	sep := ','
+	// csv writer write null value as empty string
+	nullkey := ""
+
 	// generate csv data
 	insertData, err := testutil.CreateInsertData(schema, suite.numRows)
 	suite.NoError(err)
-	csvData, err := testutil.CreateInsertDataForCSV(schema, insertData)
+	csvData, err := testutil.CreateInsertDataForCSV(schema, insertData, nullkey)
 	suite.NoError(err)
 
 	// write to csv file
-	sep := '\t'
 	filePath := fmt.Sprintf("/tmp/test_%d_reader.csv", rand.Int())
-	defer os.Remove(filePath)
+	// defer os.Remove(filePath)
 	wf, err := os.OpenFile(filePath, os.O_RDWR|os.O_CREATE, 0o666)
-	assert.NoError(suite.T(), err)
+	suite.NoError(err)
 	writer := csv.NewWriter(wf)
 	writer.Comma = sep
-	writer.WriteAll(csvData)
+	err = writer.WriteAll(csvData)
 	suite.NoError(err)
 
 	// read from csv file
@@ -102,13 +107,13 @@ func (suite *ReaderSuite) run(dataType schemapb.DataType, elemType schemapb.Data
 	suite.NoError(err)
 
 	// check reader separate fields by '\t'
-	wrongSep := ','
-	_, err = NewReader(ctx, cm, schema, filePath, 64*1024*1024, wrongSep)
+	wrongSep := '\t'
+	_, err = NewReader(ctx, cm, schema, filePath, 64*1024*1024, wrongSep, nullkey)
 	suite.Error(err)
 	suite.Contains(err.Error(), "value of field is missed: ")
 
 	// check data
-	reader, err := NewReader(ctx, cm, schema, filePath, 64*1024*1024, sep)
+	reader, err := NewReader(ctx, cm, schema, filePath, 64*1024*1024, sep, nullkey)
 	suite.NoError(err)
 
 	checkFn := func(actualInsertData *storage.InsertData, offsetBegin, expectRows int) {
@@ -129,43 +134,63 @@ func (suite *ReaderSuite) run(dataType schemapb.DataType, elemType schemapb.Data
 }
 
 func (suite *ReaderSuite) TestReadScalarFields() {
-	suite.run(schemapb.DataType_Bool, schemapb.DataType_None)
-	suite.run(schemapb.DataType_Int8, schemapb.DataType_None)
-	suite.run(schemapb.DataType_Int16, schemapb.DataType_None)
-	suite.run(schemapb.DataType_Int32, schemapb.DataType_None)
-	suite.run(schemapb.DataType_Int64, schemapb.DataType_None)
-	suite.run(schemapb.DataType_Float, schemapb.DataType_None)
-	suite.run(schemapb.DataType_Double, schemapb.DataType_None)
-	suite.run(schemapb.DataType_String, schemapb.DataType_None)
-	suite.run(schemapb.DataType_VarChar, schemapb.DataType_None)
-	suite.run(schemapb.DataType_JSON, schemapb.DataType_None)
+	suite.run(schemapb.DataType_Bool, schemapb.DataType_None, false)
+	suite.run(schemapb.DataType_Int8, schemapb.DataType_None, false)
+	suite.run(schemapb.DataType_Int16, schemapb.DataType_None, false)
+	suite.run(schemapb.DataType_Int32, schemapb.DataType_None, false)
+	suite.run(schemapb.DataType_Int64, schemapb.DataType_None, false)
+	suite.run(schemapb.DataType_Float, schemapb.DataType_None, false)
+	suite.run(schemapb.DataType_Double, schemapb.DataType_None, false)
+	suite.run(schemapb.DataType_String, schemapb.DataType_None, false)
+	suite.run(schemapb.DataType_VarChar, schemapb.DataType_None, false)
+	suite.run(schemapb.DataType_JSON, schemapb.DataType_None, false)
 
-	suite.run(schemapb.DataType_Array, schemapb.DataType_Bool)
-	suite.run(schemapb.DataType_Array, schemapb.DataType_Int8)
-	suite.run(schemapb.DataType_Array, schemapb.DataType_Int16)
-	suite.run(schemapb.DataType_Array, schemapb.DataType_Int32)
-	suite.run(schemapb.DataType_Array, schemapb.DataType_Int64)
-	suite.run(schemapb.DataType_Array, schemapb.DataType_Float)
-	suite.run(schemapb.DataType_Array, schemapb.DataType_Double)
-	suite.run(schemapb.DataType_Array, schemapb.DataType_String)
+	suite.run(schemapb.DataType_Array, schemapb.DataType_Bool, false)
+	suite.run(schemapb.DataType_Array, schemapb.DataType_Int8, false)
+	suite.run(schemapb.DataType_Array, schemapb.DataType_Int16, false)
+	suite.run(schemapb.DataType_Array, schemapb.DataType_Int32, false)
+	suite.run(schemapb.DataType_Array, schemapb.DataType_Int64, false)
+	suite.run(schemapb.DataType_Array, schemapb.DataType_Float, false)
+	suite.run(schemapb.DataType_Array, schemapb.DataType_Double, false)
+	suite.run(schemapb.DataType_Array, schemapb.DataType_String, false)
+
+	suite.run(schemapb.DataType_Bool, schemapb.DataType_None, true)
+	suite.run(schemapb.DataType_Int8, schemapb.DataType_None, true)
+	suite.run(schemapb.DataType_Int16, schemapb.DataType_None, true)
+	suite.run(schemapb.DataType_Int32, schemapb.DataType_None, true)
+	suite.run(schemapb.DataType_Int64, schemapb.DataType_None, true)
+	suite.run(schemapb.DataType_Float, schemapb.DataType_None, true)
+	suite.run(schemapb.DataType_Double, schemapb.DataType_None, true)
+	suite.run(schemapb.DataType_String, schemapb.DataType_None, true)
+	suite.run(schemapb.DataType_VarChar, schemapb.DataType_None, true)
+	suite.run(schemapb.DataType_JSON, schemapb.DataType_None, true)
+
+	suite.run(schemapb.DataType_Array, schemapb.DataType_Bool, true)
+	suite.run(schemapb.DataType_Array, schemapb.DataType_Int8, true)
+	suite.run(schemapb.DataType_Array, schemapb.DataType_Int16, true)
+	suite.run(schemapb.DataType_Array, schemapb.DataType_Int32, true)
+	suite.run(schemapb.DataType_Array, schemapb.DataType_Int64, true)
+	suite.run(schemapb.DataType_Array, schemapb.DataType_Float, true)
+	suite.run(schemapb.DataType_Array, schemapb.DataType_Double, true)
+	suite.run(schemapb.DataType_Array, schemapb.DataType_String, true)
 }
 
 func (suite *ReaderSuite) TestStringPK() {
 	suite.pkDataType = schemapb.DataType_VarChar
-	suite.run(schemapb.DataType_Int32, schemapb.DataType_None)
+	suite.run(schemapb.DataType_Int32, schemapb.DataType_None, false)
 }
 
 func (suite *ReaderSuite) TestVector() {
 	suite.vecDataType = schemapb.DataType_BinaryVector
-	suite.run(schemapb.DataType_Int32, schemapb.DataType_None)
+	suite.run(schemapb.DataType_Int32, schemapb.DataType_None, false)
 	suite.vecDataType = schemapb.DataType_FloatVector
-	suite.run(schemapb.DataType_Int32, schemapb.DataType_None)
+	suite.run(schemapb.DataType_Int32, schemapb.DataType_None, false)
 	suite.vecDataType = schemapb.DataType_Float16Vector
-	suite.run(schemapb.DataType_Int32, schemapb.DataType_None)
+	suite.run(schemapb.DataType_Int32, schemapb.DataType_None, false)
 	suite.vecDataType = schemapb.DataType_BFloat16Vector
-	suite.run(schemapb.DataType_Int32, schemapb.DataType_None)
+	suite.run(schemapb.DataType_Int32, schemapb.DataType_None, false)
 	suite.vecDataType = schemapb.DataType_SparseFloatVector
-	suite.run(schemapb.DataType_Int32, schemapb.DataType_None)
+	suite.run(schemapb.DataType_Int32, schemapb.DataType_None, false)
 }
 
 func TestUtil(t *testing.T) {
