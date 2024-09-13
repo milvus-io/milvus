5cd860652dbc366eeece5dd539abdd578479adf1
wei liu <wei.liu@zilliz.com>
2024-09-13 17:05:15 +0800

    enhance: Enable dynamic update replica selection policy (#35824)
    issue: #35859
    pr: #35860
    
    ---------
    
    Signed-off-by: Wei Liu <wei.liu@zilliz.com>

--- internal/proxy/roundrobin_balancer.go
+++ internal/proxy/roundrobin_balancer.go
@@ -22,18 +22,14 @@ import (
 
 	"github.com/milvus-io/milvus/internal/proto/internalpb"
 	"github.com/milvus-io/milvus/pkg/util/merr"
-	"github.com/milvus-io/milvus/pkg/util/typeutil"
 )
 
 type RoundRobinBalancer struct {
-	// request num send to each node
-	nodeWorkload *typeutil.ConcurrentMap[int64, *atomic.Int64]
+	idx atomic.Int64
 }
 
 func NewRoundRobinBalancer() *RoundRobinBalancer {
-	return &RoundRobinBalancer{
-		nodeWorkload: typeutil.NewConcurrentMap[int64, *atomic.Int64](),
-	}
+	return &RoundRobinBalancer{}
 }
 
 func (b *RoundRobinBalancer) SelectNode(ctx context.Context, availableNodes []int64, cost int64) (int64, error) {
@@ -41,32 +37,11 @@ func (b *RoundRobinBalancer) SelectNode(ctx context.Context, availableNodes []in
 		return -1, merr.ErrNodeNotAvailable
 	}
 
-	targetNode := int64(-1)
-	var targetNodeWorkload *atomic.Int64
-	for _, node := range availableNodes {
-		workload, ok := b.nodeWorkload.Get(node)
-
-		if !ok {
-			workload = atomic.NewInt64(0)
-			b.nodeWorkload.Insert(node, workload)
-		}
-
-		if targetNodeWorkload == nil || workload.Load() < targetNodeWorkload.Load() {
-			targetNode = node
-			targetNodeWorkload = workload
-		}
-	}
-
-	targetNodeWorkload.Add(cost)
-	return targetNode, nil
+	idx := b.idx.Inc()
+	return availableNodes[int(idx)%len(availableNodes)], nil
 }
 
 func (b *RoundRobinBalancer) CancelWorkload(node int64, nq int64) {
-	load, ok := b.nodeWorkload.Get(node)
-
-	if ok {
-		load.Sub(nq)
-	}
 }
 
 func (b *RoundRobinBalancer) UpdateCostMetrics(node int64, cost *internalpb.CostAggregation) {}
