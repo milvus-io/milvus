faf5be2e7296c850c4b30e940061fa945051c621
jaime <yun.zhang@zilliz.com>
2024-09-13 20:09:15 +0800

    fix: metrics stored_index_files_size is never cleared (#36161)
    issue: ##36159
    pr: #36160
    
    Signed-off-by: jaime <yun.zhang@zilliz.com>

--- internal/datacoord/meta_test.go
+++ internal/datacoord/meta_test.go
@@ -670,7 +670,7 @@ func TestMeta_Basic(t *testing.T) {
 
 	t.Run("Test GetCollectionBinlogSize", func(t *testing.T) {
 		meta := createMeta(&datacoord.Catalog{}, nil, createIndexMeta(&datacoord.Catalog{}))
-		ret := meta.GetCollectionIndexFilesSize()
+		ret := meta.SetStoredIndexFileSizeMetric()
 		assert.Equal(t, uint64(0), ret)
 
 		meta.collections = map[UniqueID]*collectionInfo{
@@ -679,7 +679,7 @@ func TestMeta_Basic(t *testing.T) {
 				DatabaseName: "db",
 			},
 		}
-		ret = meta.GetCollectionIndexFilesSize()
+		ret = meta.SetStoredIndexFileSizeMetric()
 		assert.Equal(t, uint64(11), ret)
 	})
 
