526a672bae3b08c373af989a21e46d925c051a6b
wt <ting.wang@zilliz.com>
2024-09-16 11:03:09 +0800

    test: add more bitmap test cases (#36290)
    Signed-off-by: wangting0128 <ting.wang@zilliz.com>

--- tests/python_client/common/code_mapping.py
+++ tests/python_client/common/code_mapping.py
@@ -39,6 +39,7 @@ class IndexErrorMessage(ExceptionsMessage):
     CheckBitmapIndex = "bitmap index are only supported on bool, int, string and array field"
     CheckBitmapOnPK = "create bitmap index on primary key not supported"
     CheckBitmapCardinality = "failed to check bitmap cardinality limit, should be larger than 0 and smaller than 1000"
+    NotConfigable = "{0} is not configable index param"
 
 
 class QueryErrorMessage(ExceptionsMessage):
