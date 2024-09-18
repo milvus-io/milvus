11f4eea9da344213a8c8b74bbf8fbd013a4e67a0
Congqi Xia <congqi.xia@zilliz.com>
2024-09-18 14:55:26 +0800

    enhance: Add streaming forward policy switch for delegator
    Related to #35303
    
    Signed-off-by: Congqi Xia <congqi.xia@zilliz.com>

--- internal/querynodev2/delegator/delegator_delta_forward.go
+++ internal/querynodev2/delegator/delegator_delta_forward.go
@@ -18,8 +18,12 @@ package delegator
 
 import (
 	"context"
+	"fmt"
+	"time"
 
+	"github.com/samber/lo"
 	"go.uber.org/zap"
+	"golang.org/x/sync/errgroup"
 
 	"github.com/milvus-io/milvus-proto/go-api/v2/commonpb"
 	"github.com/milvus-io/milvus/internal/proto/datapb"
@@ -30,6 +34,7 @@ import (
 	"github.com/milvus-io/milvus/internal/storage"
 	"github.com/milvus-io/milvus/pkg/common"
 	"github.com/milvus-io/milvus/pkg/log"
+	"github.com/milvus-io/milvus/pkg/metrics"
 	"github.com/milvus-io/milvus/pkg/util/commonpbutil"
 	"github.com/milvus-io/milvus/pkg/util/merr"
 	"github.com/milvus-io/milvus/pkg/util/paramtable"
@@ -37,9 +42,11 @@ import (
 )
 
 const (
-	L0ForwardPolicyDefault    = ``
-	L0ForwardPolicyBF         = `FilterByBF`
-	L0ForwardPolicyRemoteLoad = `RemoteLoad`
+	ForwardPolicyDefault         = ``
+	L0ForwardPolicyBF            = `FilterByBF`
+	L0ForwardPolicyRemoteLoad    = `RemoteLoad`
+	StreamingForwardPolicyBF     = `FilterByBF`
+	StreamingForwardPolicyDirect = `Direct`
 )
 
 func (sd *shardDelegator) forwardL0Deletion(ctx context.Context,
@@ -50,12 +57,27 @@ func (sd *shardDelegator) forwardL0Deletion(ctx context.Context,
 	worker cluster.Worker,
 ) error {
 	switch policy := paramtable.Get().QueryNodeCfg.LevelZeroForwardPolicy.GetValue(); policy {
-	case L0ForwardPolicyDefault, L0ForwardPolicyBF:
+	case ForwardPolicyDefault, L0ForwardPolicyBF:
 		return sd.forwardL0ByBF(ctx, info, candidate, targetNodeID, worker)
 	case L0ForwardPolicyRemoteLoad:
 		return sd.forwardL0RemoteLoad(ctx, info, req, targetNodeID, worker)
 	default:
 		return merr.WrapErrServiceInternal("Unknown l0 forward policy: %s", policy)
+	}
+}
+
+func (sd *shardDelegator) forwardStreamingDeletion(ctx context.Context, deleteData []*DeleteData) {
+	// TODO add `auto` policy
+	// using direct when streaming size is too large
+	// need some experimental data to support this policy
+	switch policy := paramtable.Get().QueryNodeCfg.StreamingDeltaForwardPolicy.GetValue(); policy {
+	case ForwardPolicyDefault, StreamingForwardPolicyBF:
+		sd.forwardStreamingByBF(ctx, deleteData)
+	case StreamingForwardPolicyDirect:
+		// forward streaming deletion without bf filtering
+		sd.forwardStreamingDirect(ctx, deleteData)
+	default:
+		log.Fatal("unsupported streaming forward policy", zap.String("policy", policy))
 	}
 }
 
@@ -143,4 +165,170 @@ func (sd *shardDelegator) getLevel0Deltalogs(partitionID int64) []*datapb.FieldB
 	}
 
 	return deltalogs
+}
+
+func (sd *shardDelegator) forwardStreamingByBF(ctx context.Context, deleteData []*DeleteData) {
+	start := time.Now()
+	retMap := sd.applyBFInParallel(deleteData, segments.GetBFApplyPool())
+	// segment => delete data
+	delRecords := make(map[int64]DeleteData)
+	retMap.Range(func(key int, value *BatchApplyRet) bool {
+		startIdx := value.StartIdx
+		pk2SegmentIDs := value.Segment2Hits
+
+		pks := deleteData[value.DeleteDataIdx].PrimaryKeys
+		tss := deleteData[value.DeleteDataIdx].Timestamps
+
+		for segmentID, hits := range pk2SegmentIDs {
+			for i, hit := range hits {
+				if hit {
+					delRecord := delRecords[segmentID]
+					delRecord.PrimaryKeys = append(delRecord.PrimaryKeys, pks[startIdx+i])
+					delRecord.Timestamps = append(delRecord.Timestamps, tss[startIdx+i])
+					delRecord.RowCount++
+					delRecords[segmentID] = delRecord
+				}
+			}
+		}
+		return true
+	})
+	bfCost := time.Since(start)
+
+	offlineSegments := typeutil.NewConcurrentSet[int64]()
+
+	sealed, growing, version := sd.distribution.PinOnlineSegments()
+
+	start = time.Now()
+	eg, ctx := errgroup.WithContext(context.Background())
+	for _, entry := range sealed {
+		entry := entry
+		eg.Go(func() error {
+			worker, err := sd.workerManager.GetWorker(ctx, entry.NodeID)
+			if err != nil {
+				log.Warn("failed to get worker",
+					zap.Int64("nodeID", paramtable.GetNodeID()),
+					zap.Error(err),
+				)
+				// skip if node down
+				// delete will be processed after loaded again
+				return nil
+			}
+			offlineSegments.Upsert(sd.applyDelete(ctx, entry.NodeID, worker, func(segmentID int64) (DeleteData, bool) {
+				data, ok := delRecords[segmentID]
+				return data, ok
+			}, entry.Segments, querypb.DataScope_Historical)...)
+			return nil
+		})
+	}
+	if len(growing) > 0 {
+		eg.Go(func() error {
+			worker, err := sd.workerManager.GetWorker(ctx, paramtable.GetNodeID())
+			if err != nil {
+				log.Error("failed to get worker(local)",
+					zap.Int64("nodeID", paramtable.GetNodeID()),
+					zap.Error(err),
+				)
+				// panic here, local worker shall not have error
+				panic(err)
+			}
+			offlineSegments.Upsert(sd.applyDelete(ctx, paramtable.GetNodeID(), worker, func(segmentID int64) (DeleteData, bool) {
+				data, ok := delRecords[segmentID]
+				return data, ok
+			}, growing, querypb.DataScope_Streaming)...)
+			return nil
+		})
+	}
+	// not error return in apply delete
+	_ = eg.Wait()
+	forwardDeleteCost := time.Since(start)
+
+	sd.distribution.Unpin(version)
+	offlineSegIDs := offlineSegments.Collect()
+	if len(offlineSegIDs) > 0 {
+		log.Warn("failed to apply delete, mark segment offline", zap.Int64s("offlineSegments", offlineSegIDs))
+		sd.markSegmentOffline(offlineSegIDs...)
+	}
+
+	metrics.QueryNodeApplyBFCost.WithLabelValues("ProcessDelete", fmt.Sprint(paramtable.GetNodeID())).Observe(float64(bfCost.Milliseconds()))
+	metrics.QueryNodeForwardDeleteCost.WithLabelValues("ProcessDelete", fmt.Sprint(paramtable.GetNodeID())).Observe(float64(forwardDeleteCost.Milliseconds()))
+}
+
+func (sd *shardDelegator) forwardStreamingDirect(ctx context.Context, deleteData []*DeleteData) {
+	start := time.Now()
+
+	// group by partition id
+	groups := lo.GroupBy(deleteData, func(delData *DeleteData) int64 {
+		return delData.PartitionID
+	})
+
+	offlineSegments := typeutil.NewConcurrentSet[int64]()
+	eg, ctx := errgroup.WithContext(ctx)
+
+	for partitionID, group := range groups {
+		partitionID := partitionID
+		group := group
+		eg.Go(func() error {
+			partitions := []int64{partitionID}
+			// check if all partitions
+			if partitionID == common.AllPartitionsID {
+				partitions = []int64{}
+			}
+			sealed, growing, version := sd.distribution.PinOnlineSegments(partitions...)
+			defer sd.distribution.Unpin(version)
+
+			for _, item := range group {
+				deleteData := *item
+				for _, entry := range sealed {
+					entry := entry
+					worker, err := sd.workerManager.GetWorker(ctx, entry.NodeID)
+					if err != nil {
+						log.Warn("failed to get worker",
+							zap.Int64("nodeID", paramtable.GetNodeID()),
+							zap.Error(err),
+						)
+						// skip if node down
+						// delete will be processed after loaded again
+						continue
+					}
+
+					eg.Go(func() error {
+						offlineSegments.Upsert(sd.applyDelete(ctx, entry.NodeID, worker, func(segmentID int64) (DeleteData, bool) {
+							return deleteData, true
+						}, entry.Segments, querypb.DataScope_Historical)...)
+						return nil
+					})
+				}
+
+				if len(growing) > 0 {
+					worker, err := sd.workerManager.GetWorker(ctx, paramtable.GetNodeID())
+					if err != nil {
+						log.Error("failed to get worker(local)",
+							zap.Int64("nodeID", paramtable.GetNodeID()),
+							zap.Error(err),
+						)
+						// panic here, local worker shall not have error
+						panic(err)
+					}
+					eg.Go(func() error {
+						offlineSegments.Upsert(sd.applyDelete(ctx, paramtable.GetNodeID(), worker, func(segmentID int64) (DeleteData, bool) {
+							return deleteData, true
+						}, growing, querypb.DataScope_Streaming)...)
+						return nil
+					})
+				}
+			}
+			return nil
+		})
+	}
+	// not error return in apply delete
+	_ = eg.Wait()
+	forwardDeleteCost := time.Since(start)
+
+	offlineSegIDs := offlineSegments.Collect()
+	if len(offlineSegIDs) > 0 {
+		log.Warn("failed to apply delete, mark segment offline", zap.Int64s("offlineSegments", offlineSegIDs))
+		sd.markSegmentOffline(offlineSegIDs...)
+	}
+
+	metrics.QueryNodeForwardDeleteCost.WithLabelValues("ProcessDelete", fmt.Sprint(paramtable.GetNodeID())).Observe(float64(forwardDeleteCost.Milliseconds()))
 }
--- pkg/util/paramtable/component_param.go
+++ pkg/util/paramtable/component_param.go
@@ -2394,8 +2394,9 @@ type queryNodeConfig struct {
 	MaxSegmentDeleteBuffer ParamItem `refreshable:"false"`
 	DeleteBufferBlockSize  ParamItem `refreshable:"false"`
 
-	// level zero
-	LevelZeroForwardPolicy ParamItem `refreshable:"true"`
+	// delta forward
+	LevelZeroForwardPolicy      ParamItem `refreshable:"true"`
+	StreamingDeltaForwardPolicy ParamItem `refreshable:"true"`
 
 	// loader
 	IoPoolSize             ParamItem `refreshable:"false"`
@@ -3013,6 +3014,15 @@ Max read concurrency must greater than or equal to 1, and less than or equal to 
 		Export:       true,
 	}
 	p.LevelZeroForwardPolicy.Init(base.mgr)
+
+	p.StreamingDeltaForwardPolicy = ParamItem{
+		Key:          "queryNode.streamingDeltaForwardPolicy",
+		Version:      "2.4.12",
+		Doc:          "delegator level zero deletion forward policy, possible option[\"FilterByBF\", \"Direct\"]",
+		DefaultValue: "FilterByBF",
+		Export:       true,
+	}
+	p.StreamingDeltaForwardPolicy.Init(base.mgr)
 
 	p.IoPoolSize = ParamItem{
 		Key:          "queryNode.ioPoolSize",
