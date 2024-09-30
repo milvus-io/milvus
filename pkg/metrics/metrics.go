c5088b8527e0abdcd6e81968c890dcf8705de45a
yihao.dai <yihao.dai@zilliz.com>
2024-09-30 10:03:18 +0800

    enhance: Add metrics to monitor import throughput and imported rows (#36519) (#36588)
    issue: https://github.com/milvus-io/milvus/issues/36518
    
    pr: https://github.com/milvus-io/milvus/pull/36519
    
    Signed-off-by: bigsheeper <yihao.dai@zilliz.com>

--- pkg/metrics/metrics.go
+++ pkg/metrics/metrics.go
@@ -56,6 +56,9 @@ const (
 	FlushingSegmentLabel = "Flushing"
 	DroppedSegmentLabel  = "Dropped"
 
+	StreamingDataSourceLabel  = "streaming"
+	BulkinsertDataSourceLabel = "bulkinsert"
+
 	Leader     = "OnLeader"
 	FromLeader = "FromLeader"
 
@@ -99,6 +102,7 @@ const (
 	cacheNameLabelName       = "cache_name"
 	cacheStateLabelName      = "cache_state"
 	indexCountLabelName      = "indexed_field_count"
+	dataSourceLabelName      = "data_source"
 	requestScope             = "scope"
 	fullMethodLabelName      = "full_method"
 	reduceLevelName          = "reduce_level"
