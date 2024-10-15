bb3ef5349f0584ccf1431bc7fa28d7a0f90c09dd
SimFG <bang.fu@zilliz.com>
2024-10-15 10:53:22 +0800

    enhance: update the expr version to support automatic conversion of variable types (#36832)
    /kind improvement
    
    Signed-off-by: SimFG <bang.fu@zilliz.com>

--- internal/proxy/meta_cache.go
+++ internal/proxy/meta_cache.go
@@ -469,6 +469,9 @@ func (m *MetaCache) update(ctx context.Context, database, collectionName string,
 	})
 
 	collectionName = collection.Schema.GetName()
+	if database == "" {
+		log.Warn("database is empty, use default database name", zap.String("collectionName", collectionName), zap.Stack("stack"))
+	}
 	m.mu.Lock()
 	defer m.mu.Unlock()
 	_, dbOk := m.collInfo[database]
