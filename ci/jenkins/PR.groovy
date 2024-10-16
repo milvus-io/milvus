1bff284ad40a02934cc16738bc655201dbd1268c
Yellow Shine <sammy.huang@zilliz.com>
2024-10-16 14:52:47 +0800

    .
    Signed-off-by: Yellow Shine <sammy.huang@zilliz.com>

--- ci/jenkins/PR.groovy
+++ ci/jenkins/PR.groovy
@@ -39,6 +39,7 @@ pipeline {
                     script {
                         echo 'hello'
                         def changeLogSets = currentBuild.changeSets
+                        echo "${changeLogSets}"
                         for (int i = 0; i < changeLogSets.size(); i++) {
                             def entries = changeLogSets[i].items
                             for (int j = 0; j < entries.length; j++) {
