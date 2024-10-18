4e0f5845a1541b3c81da0858068d5ab27868e5a4
yihao.dai <yihao.dai@zilliz.com>
2024-10-18 18:13:25 +0800

    enhance: Limit import job number (#36891) (#36892)
    issue: https://github.com/milvus-io/milvus/issues/36890
    
    pr: https://github.com/milvus-io/milvus/pull/36891
    
    ---------
    
    Signed-off-by: bigsheeper <yihao.dai@zilliz.com>

--- pkg/util/paramtable/component_param.go
+++ pkg/util/paramtable/component_param.go
@@ -3227,6 +3227,7 @@ type dataCoordConfig struct {
 	ImportCheckIntervalHigh  ParamItem `refreshable:"true"`
 	ImportCheckIntervalLow   ParamItem `refreshable:"true"`
 	MaxFilesPerImportReq     ParamItem `refreshable:"true"`
+	MaxImportJobNum          ParamItem `refreshable:"true"`
 	WaitForIndex             ParamItem `refreshable:"true"`
 
 	GracefulStopTimeout ParamItem `refreshable:"true"`
@@ -3989,6 +3990,16 @@ During compaction, the size of segment # of rows is able to exceed segment max #
 		Export:       true,
 	}
 	p.MaxFilesPerImportReq.Init(base.mgr)
+
+	p.MaxImportJobNum = ParamItem{
+		Key:          "dataCoord.import.maxImportJobNum",
+		Version:      "2.4.14",
+		Doc:          "Maximum number of import jobs that are executing or pending.",
+		DefaultValue: "1024",
+		PanicIfEmpty: false,
+		Export:       true,
+	}
+	p.MaxImportJobNum.Init(base.mgr)
 
 	p.WaitForIndex = ParamItem{
 		Key:          "dataCoord.import.waitForIndex",
