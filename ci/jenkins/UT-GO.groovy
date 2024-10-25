64644bae4d2b18d52683bb243e69c204bb36203d
Yellow Shine <sammy.huang@zilliz.com>
2024-10-25 17:35:31 +0800

    enhance: enable go ut in idc
    Signed-off-by: Yellow Shine <sammy.huang@zilliz.com>

---
+++ ci/jenkins/UT-GO.groovy
@@ -0,0 +1,60 @@
+@Library('jenkins-shared-library@v0.65.0') _
+
+def pod = libraryResource 'io/milvus/pod/tekton-4am.yaml'
+def milvus_helm_chart_version = '4.2.8'
+
+pipeline {
+    options {
+        skipDefaultCheckout true
+        parallelsAlwaysFailFast()
+        buildDiscarder logRotator(artifactDaysToKeepStr: '30')
+        preserveStashes(buildCount: 5)
+        disableConcurrentBuilds(abortPrevious: true)
+    }
+    agent {
+        kubernetes {
+            cloud '4am'
+            yaml pod
+        }
+    }
+    stages {
+        stage('meta') {
+            steps {
+                container('jnlp') {
+                    script {
+                        isPr = env.CHANGE_ID != null
+                        gitMode = isPr ? 'merge' : 'fetch'
+                        gitBaseRef = isPr ? "$env.CHANGE_TARGET" : "$env.BRANCH_NAME"
+                    }
+                }
+            }
+        }
+        stage('build') {
+            steps {
+                container('tkn') {
+                    script {
+                        def job_name = tekton.ut arch: 'amd64',
+                                              isPr: isPr,
+                                              gitMode: gitMode ,
+                                              gitBaseRef: gitBaseRef,
+                                              pullRequestNumber: "$env.CHANGE_ID",
+                                              make_cmd: "make clean && make build-cpp-with-coverage",
+                                              test_entrypoint: "make codecov-go-without-build",
+                                              codecov_report_name: "go-unit-test",
+                                              codecov_files: "./go_coverage.txt",
+                                              tekton_log_timeout: '30m'
+                    }
+                }
+            }
+            post {
+                always {
+                    container('tkn') {
+                        script {
+                            tekton.sure_stop()
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
