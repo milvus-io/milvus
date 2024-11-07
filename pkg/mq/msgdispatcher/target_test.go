00f6d0ec518a2014578b85f1fc6d2314d114deb1
wei liu <wei.liu@zilliz.com>
2024-11-07 14:34:24 +0800

    fix: watch channel stuck due to misuse of timer.Reset (#37433)
    issue: #37166
    cause the misuse of timer.Reset, which cause dispatcher failed to send
    msg to virtual channel buffer, and dispatcher do splitting again and
    again, which hold the dispatcher manager's lock, block watching channel
    progress.
    
    This PR fix the misuse of timer.Reset
    
    Signed-off-by: Wei Liu <wei.liu@zilliz.com>

---
+++ pkg/mq/msgdispatcher/target_test.go
@@ -0,0 +1,30 @@
+package msgdispatcher
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"go.uber.org/zap"
+
+	"github.com/milvus-io/milvus-proto/go-api/v2/msgpb"
+	"github.com/milvus-io/milvus/pkg/log"
+	"github.com/milvus-io/milvus/pkg/mq/msgstream"
+	"github.com/milvus-io/milvus/pkg/util/paramtable"
+)
+
+func TestSendTimeout(t *testing.T) {
+	target := newTarget("test1", &msgpb.MsgPosition{})
+
+	time.Sleep(paramtable.Get().MQCfg.MaxTolerantLag.GetAsDuration(time.Second))
+
+	counter := 0
+	for i := 0; i < 10; i++ {
+		err := target.send(&msgstream.MsgPack{})
+		if err != nil {
+			log.Error("send failed", zap.Int("idx", i), zap.Error(err))
+			counter++
+		}
+	}
+	assert.Equal(t, counter, 0)
+}
