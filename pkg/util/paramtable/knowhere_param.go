a6c459accdd9ec6df936c2c95f50bc187898c0bf
Ted Xu <ted.xu@zilliz.com>
2024-11-12 16:31:03 +0800

    fix: config file validator missed the file tail validation
    Signed-off-by: Ted Xu <ted.xu@zilliz.com>

--- pkg/util/paramtable/knowhere_param.go
+++ pkg/util/paramtable/knowhere_param.go
@@ -30,6 +30,23 @@ func (p *knowhereConfig) init(base *BaseTable) {
 	p.IndexParam = ParamGroup{
 		KeyPrefix: "knowhere.",
 		Version:   "2.5.0",
+		Export:    true,
+		DocFunc: func(key string) string {
+			switch key {
+			case "DISKANN.build.max_degree":
+				return "Maximum degree of the Vamana graph"
+			case "DISKANN.build.pq_code_budget_gb_ratio":
+				return "Size limit on the PQ code (compared with raw data)"
+			case "DISKANN.build.search_cache_budget_gb_ratio":
+				return "Ratio of cached node numbers to raw data"
+			case "DISKANN.build.search_list_size":
+				return "Size of the candidate list during building graph"
+			case "DISKANN.search.beam_width_ratio":
+				return "Ratio between the maximum number of IO requests per search iteration and CPU number"
+			default:
+				return ""
+			}
+		},
 	}
 	p.IndexParam.Init(base.mgr)
 
@@ -37,6 +54,8 @@ func (p *knowhereConfig) init(base *BaseTable) {
 		Key:          "knowhere.enable",
 		Version:      "2.5.0",
 		DefaultValue: "true",
+		Export:       true,
+		Doc:          "When enable this configuration, the index parameters defined following will be automatically populated as index parameters, without requiring user input.",
 	}
 	p.Enable.Init(base.mgr)
 }
