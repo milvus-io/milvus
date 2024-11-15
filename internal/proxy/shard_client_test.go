75734634b6f2dd5f19ba19fe8114c88199dd94a8
Wei Liu <wei.liu@zilliz.com>
2024-11-15 18:37:22 +0800

    fix: Get shard client failed by client is closed
    This PR refine the shard client ref counter, dec ref counter won't
    release client anymore, and only permit shard client manager to remove client.
    
    Signed-off-by: Wei Liu <wei.liu@zilliz.com>

--- internal/proxy/shard_client_test.go
+++ internal/proxy/shard_client_test.go
@@ -2,11 +2,15 @@ package proxy
 
 import (
 	"context"
+	"sync"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 
 	"github.com/milvus-io/milvus/internal/mocks"
+	"github.com/milvus-io/milvus/internal/proto/internalpb"
 	"github.com/milvus-io/milvus/internal/types"
 	"github.com/milvus-io/milvus/pkg/util/paramtable"
 )
@@ -40,7 +44,7 @@ func TestShardClient(t *testing.T) {
 	}
 
 	qn := mocks.NewMockQueryNodeClient(t)
-	qn.EXPECT().Close().Return(nil)
+	qn.EXPECT().Close().Return(nil).Maybe()
 	creator := func(ctx context.Context, addr string, nodeID int64) (types.QueryNodeClient, error) {
 		return qn, nil
 	}
@@ -60,7 +64,51 @@ func TestShardClient(t *testing.T) {
 	shardClient.DecRef()
 	assert.Equal(t, int64(1), shardClient.refCnt.Load())
 
+	// only shard client manager can close shard client
 	shardClient.DecRef()
-	assert.Equal(t, int64(0), shardClient.refCnt.Load())
-	assert.Equal(t, true, shardClient.isClosed)
+	shardClient.DecRef()
+	shardClient.DecRef()
+	shardClient.DecRef()
+	assert.Equal(t, false, shardClient.isClosed)
+}
+
+func TestPurgeClient(t *testing.T) {
+	nodeInfo := nodeInfo{
+		nodeID: 1,
+	}
+
+	qn := mocks.NewMockQueryNodeClient(t)
+	qn.EXPECT().Close().Return(nil).Maybe()
+	creator := func(ctx context.Context, addr string, nodeID int64) (types.QueryNodeClient, error) {
+		return qn, nil
+	}
+
+	s := &shardClientMgrImpl{
+		clients: struct {
+			sync.RWMutex
+			data map[UniqueID]*shardClient
+		}{data: make(map[UniqueID]*shardClient)},
+		clientCreator:   creator,
+		closeCh:         make(chan struct{}),
+		purgeInterval:   1 * time.Second,
+		purgeExpiredAge: 3,
+	}
+	mockQC := mocks.NewMockQueryCoordClient(t)
+	mockRC := mocks.NewMockRootCoordClient(t)
+	mockRC.EXPECT().ListPolicy(mock.Anything, mock.Anything).Return(&internalpb.ListPolicyResponse{}, nil)
+	InitMetaCache(context.TODO(), mockRC, mockQC, s)
+
+	go s.PurgeClient()
+	defer s.Close()
+	// test client has been used
+	_, err := s.GetClient(context.Background(), nodeInfo)
+	assert.Nil(t, err)
+	time.Sleep(5 * time.Second)
+	// expected client has not been purged
+	assert.Equal(t, len(s.clients.data), 1)
+
+	s.ReleaseClientRef(1)
+	time.Sleep(5 * time.Second)
+	// expected client has been purged
+	assert.Equal(t, len(s.clients.data), 0)
 }
