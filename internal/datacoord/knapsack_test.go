c68558ba9a6c9fdbee0a2b53ded5f95c9d64c5e6
Ted Xu <ted.xu@zilliz.com>
2024-11-15 18:52:33 +0800

    remove useless code
    Signed-off-by: Ted Xu <ted.xu@zilliz.com>

--- internal/datacoord/knapsack_test.go
+++ internal/datacoord/knapsack_test.go
@@ -17,32 +17,10 @@
 package datacoord
 
 import (
-	"reflect"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
-
-func Test_newSegmentPacker(t *testing.T) {
-	type args struct {
-		name       string
-		candidates []*SegmentInfo
-	}
-	tests := []struct {
-		name string
-		args args
-		want Knapsack[*SegmentInfo]
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := newSegmentPacker(tt.args.name, tt.args.candidates); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("newSegmentPacker() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
 
 type element struct {
 	size int64
