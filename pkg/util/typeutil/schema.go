511edd29fd8dd6c98d0c778d869fd20df1173d69
Buqian Zheng <zhengbuqian@gmail.com>
2024-11-20 14:22:30 +0800

    enhance: disallow get raw vector data of a BM25 Function output field (#37800)
    issue: https://github.com/milvus-io/milvus/issues/35853
    
    Signed-off-by: Buqian Zheng <zhengbuqian@gmail.com>

--- pkg/util/typeutil/schema.go
+++ pkg/util/typeutil/schema.go
@@ -444,6 +444,20 @@ func (helper *SchemaHelper) GetFunctionByOutputField(field *schemapb.FieldSchema
 	return nil, fmt.Errorf("function not exist")
 }
 
+// As of now, only BM25 function output field is not supported to retrieve raw field data
+func (helper *SchemaHelper) CanRetrieveRawFieldData(field *schemapb.FieldSchema) bool {
+	if !field.IsFunctionOutput {
+		return true
+	}
+
+	f, err := helper.GetFunctionByOutputField(field)
+	if err != nil {
+		return false
+	}
+
+	return f.GetType() != schemapb.FunctionType_BM25
+}
+
 func (helper *SchemaHelper) GetCollectionName() string {
 	return helper.schema.Name
 }
