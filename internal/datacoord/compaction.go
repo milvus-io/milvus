33d9454ce7783d972a34a3675a89c64dda229863
wei liu <wei.liu@zilliz.com>
2024-11-21 17:26:46 +0800

    fix: Deadlock in compaction handler (#37868)
    issue: #37803
    
    Signed-off-by: Wei Liu <wei.liu@zilliz.com>

--- internal/datacoord/compaction.go
+++ internal/datacoord/compaction.go
@@ -301,6 +301,7 @@ func (c *compactionPlanHandler) schedule() []CompactionTask {
 		c.executingGuard.Lock()
 		c.executingTasks[t.GetPlanID()] = t
 		if len(c.executingTasks) >= parallelism {
+			c.executingGuard.Unlock()
 			break // 2. the parallelism of running tasks is reached
 		}
 		c.executingGuard.Unlock()
