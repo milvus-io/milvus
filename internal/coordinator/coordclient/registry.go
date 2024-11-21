346b322deaa2c8159cad00a742aa40b94b740446
chyezh <chyezh@outlook.com>
2024-11-21 15:25:57 +0800

    enhance: remove the rpc layer of coordinator when enabling standalone or mixcoord
    Signed-off-by: chyezh <chyezh@outlook.com>

---
+++ internal/coordinator/coordclient/registry.go
@@ -0,0 +1,164 @@
+package coordclient
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/fullstorydev/grpchan/inprocgrpc"
+	"go.uber.org/zap"
+
+	dcc "github.com/milvus-io/milvus/internal/distributed/datacoord/client"
+	qcc "github.com/milvus-io/milvus/internal/distributed/querycoord/client"
+	rcc "github.com/milvus-io/milvus/internal/distributed/rootcoord/client"
+	"github.com/milvus-io/milvus/internal/proto/datapb"
+	"github.com/milvus-io/milvus/internal/proto/querypb"
+	"github.com/milvus-io/milvus/internal/proto/rootcoordpb"
+	"github.com/milvus-io/milvus/internal/types"
+	"github.com/milvus-io/milvus/pkg/log"
+	"github.com/milvus-io/milvus/pkg/util/syncutil"
+	"github.com/milvus-io/milvus/pkg/util/typeutil"
+)
+
+// localClient is a client that can access local server directly
+type localClient struct {
+	queryCoordClient *syncutil.Future[types.QueryCoordClient]
+	dataCoordClient  *syncutil.Future[types.DataCoordClient]
+	rootCoordClient  *syncutil.Future[types.RootCoordClient]
+}
+
+var (
+	enableLocal  *LocalClientRoleConfig // a global map to store all can be local accessible roles.
+	glocalClient *localClient           // !!! WARNING: local client will ignore all interceptor of grpc client and server.
+)
+
+func init() {
+	enableLocal = &LocalClientRoleConfig{}
+	glocalClient = &localClient{
+		queryCoordClient: syncutil.NewFuture[types.QueryCoordClient](),
+		dataCoordClient:  syncutil.NewFuture[types.DataCoordClient](),
+		rootCoordClient:  syncutil.NewFuture[types.RootCoordClient](),
+	}
+}
+
+type LocalClientRoleConfig struct {
+	ServerType       string
+	EnableQueryCoord bool
+	EnableDataCoord  bool
+	EnableRootCoord  bool
+}
+
+// EnableLocalClientRole init localable roles
+func EnableLocalClientRole(cfg *LocalClientRoleConfig) {
+	if cfg.ServerType != typeutil.StandaloneRole && cfg.ServerType != typeutil.MixtureRole {
+		return
+	}
+	enableLocal = cfg
+}
+
+// RegisterQueryCoordServer register query coord server
+func RegisterQueryCoordServer(server querypb.QueryCoordServer) {
+	if !enableLocal.EnableQueryCoord {
+		return
+	}
+	channel := &inprocgrpc.Channel{}
+	channel.RegisterService(&querypb.QueryCoord_ServiceDesc, server)
+	newLocalClient := querypb.NewQueryCoordClient(channel)
+	glocalClient.queryCoordClient.Set(&nopCloseQueryCoordClient{newLocalClient})
+	log.Info("register query coord server", zap.Any("enableLocalClient", enableLocal))
+}
+
+// RegsterDataCoordServer register data coord server
+func RegisterDataCoordServer(server datapb.DataCoordServer) {
+	if !enableLocal.EnableDataCoord {
+		return
+	}
+	channel := &inprocgrpc.Channel{}
+	channel.RegisterService(&datapb.DataCoord_ServiceDesc, server)
+	newLocalClient := datapb.NewDataCoordClient(channel)
+	glocalClient.dataCoordClient.Set(&nopCloseDataCoordClient{newLocalClient})
+	log.Info("register data coord server", zap.Any("enableLocalClient", enableLocal))
+}
+
+// RegisterRootCoordServer register root coord server
+func RegisterRootCoordServer(server rootcoordpb.RootCoordServer) {
+	if !enableLocal.EnableRootCoord {
+		return
+	}
+	channel := &inprocgrpc.Channel{}
+	channel.RegisterService(&rootcoordpb.RootCoord_ServiceDesc, server)
+	newLocalClient := rootcoordpb.NewRootCoordClient(channel)
+	glocalClient.rootCoordClient.Set(&nopCloseRootCoordClient{newLocalClient})
+	log.Info("register root coord server", zap.Any("enableLocalClient", enableLocal))
+}
+
+// GetQueryCoordClient return query coord client
+func GetQueryCoordClient(ctx context.Context) types.QueryCoordClient {
+	var client types.QueryCoordClient
+	var err error
+	if enableLocal.EnableQueryCoord {
+		client, err = glocalClient.queryCoordClient.GetWithContext(ctx)
+	} else {
+		// TODO: we should make a singleton here. but most unittest rely on a dedicated client.
+		client, err = qcc.NewClient(ctx)
+	}
+	if err != nil {
+		panic(fmt.Sprintf("get query coord client failed: %v", err))
+	}
+	return client
+}
+
+// GetDataCoordClient return data coord client
+func GetDataCoordClient(ctx context.Context) types.DataCoordClient {
+	var client types.DataCoordClient
+	var err error
+	if enableLocal.EnableDataCoord {
+		client, err = glocalClient.dataCoordClient.GetWithContext(ctx)
+	} else {
+		// TODO: we should make a singleton here. but most unittest rely on a dedicated client.
+		client, err = dcc.NewClient(ctx)
+	}
+	if err != nil {
+		panic(fmt.Sprintf("get data coord client failed: %v", err))
+	}
+	return client
+}
+
+// GetRootCoordClient return root coord client
+func GetRootCoordClient(ctx context.Context) types.RootCoordClient {
+	var client types.RootCoordClient
+	var err error
+	if enableLocal.EnableRootCoord {
+		client, err = glocalClient.rootCoordClient.GetWithContext(ctx)
+	} else {
+		// TODO: we should make a singleton here. but most unittest rely on a dedicated client.
+		client, err = rcc.NewClient(ctx)
+	}
+	if err != nil {
+		panic(fmt.Sprintf("get root coord client failed: %v", err))
+	}
+	return client
+}
+
+type nopCloseQueryCoordClient struct {
+	querypb.QueryCoordClient
+}
+
+func (n *nopCloseQueryCoordClient) Close() error {
+	return nil
+}
+
+type nopCloseDataCoordClient struct {
+	datapb.DataCoordClient
+}
+
+func (n *nopCloseDataCoordClient) Close() error {
+	return nil
+}
+
+type nopCloseRootCoordClient struct {
+	rootcoordpb.RootCoordClient
+}
+
+func (n *nopCloseRootCoordClient) Close() error {
+	return nil
+}
