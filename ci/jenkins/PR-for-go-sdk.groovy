b48f531e63041de8c9400ae82e35c2a237124985
yellow-shine <sammy.huang@zilliz.com>
2024-11-22 14:14:38 +0800

    enhance: [pipeline] limit compute resource (#37888)
    Signed-off-by: Yellow Shine <sammy.huang@zilliz.com>

--- ci/jenkins/PR-for-go-sdk.groovy
+++ ci/jenkins/PR-for-go-sdk.groovy
@@ -1,4 +1,4 @@
-@Library('jenkins-shared-library@v0.67.0') _
+@Library('jenkins-shared-library@v0.70.0') _
 
 def pod = libraryResource 'io/milvus/pod/tekton-4am.yaml'
 
@@ -11,7 +11,7 @@ pipeline {
         buildDiscarder logRotator(artifactDaysToKeepStr: '30')
         preserveStashes(buildCount: 5)
         disableConcurrentBuilds(abortPrevious: true)
-        timeout(time: 6, unit: 'HOURS')
+        timeout(time: 2, unit: 'HOURS')
         throttleJobProperty(
             categories: ['go-sdk'],
             throttleEnabled: true,
