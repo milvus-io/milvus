2c0bfcd97f94e766e9b9b949aa97fd8443cc63ed
sunby <sunbingyi1992@gmail.com>
2024-11-26 10:28:58 +0800

    enhance: Handle rust error in c++
    Signed-off-by: sunby <sunbingyi1992@gmail.com>

--- internal/core/thirdparty/tantivy/tantivy-binding/src/error.rs
+++ internal/core/thirdparty/tantivy/tantivy-binding/src/error.rs
@@ -1,10 +1,12 @@
-use core::fmt;
+use core::{fmt, str};
 
 use serde_json as json;
 
 #[derive(Debug)]
 pub enum TantivyBindingError {
     JsonError(serde_json::Error),
+    TantivyError(tantivy::TantivyError),
+    InvalidArgument(String),
     InternalError(String),
 }
 
@@ -14,10 +16,18 @@ impl From<serde_json::Error> for TantivyBindingError {
     }
 }
 
+impl From<tantivy::TantivyError> for TantivyBindingError {
+    fn from(value: tantivy::TantivyError) -> Self {
+        TantivyBindingError::TantivyError(value)
+    }
+}
+
 impl fmt::Display for TantivyBindingError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             TantivyBindingError::JsonError(e) => write!(f, "JsonError: {}", e),
+            TantivyBindingError::TantivyError(e) => write!(f, "TantivyError: {}", e),
+            TantivyBindingError::InvalidArgument(e) => write!(f, "InvalidArgument: {}", e),
             TantivyBindingError::InternalError(e) => write!(f, "InternalError: {}", e),
         }
     }
@@ -27,8 +37,16 @@ impl std::error::Error for TantivyBindingError {
     fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
         match self {
             TantivyBindingError::JsonError(e) => Some(e),
+            TantivyBindingError::TantivyError(e) => Some(e),
+            TantivyBindingError::InvalidArgument(_) => None,
             TantivyBindingError::InternalError(_) => None,
         }
+    }
+}
+
+impl From<str::Utf8Error> for TantivyBindingError {
+    fn from(value: str::Utf8Error) -> Self {
+        TantivyBindingError::InternalError(value.to_string())
     }
 }
 
