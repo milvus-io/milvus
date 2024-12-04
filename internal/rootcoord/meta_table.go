ab88d23ec0677abe1d72b0be2379784cf55d25d7
smellthemoon <64083300+smellthemoon@users.noreply.github.com>
2024-12-04 10:40:39 +0800

    enhance: support db request in Restful api(#38140)(#38078) (#38188)
    pr: #38078
    pr: #38140
    issue: #38077
    
    ---------
    
    Signed-off-by: lixinguo <xinguo.li@zilliz.com>
    Co-authored-by: lixinguo <xinguo.li@zilliz.com>

--- internal/rootcoord/meta_table.go
+++ internal/rootcoord/meta_table.go
@@ -601,6 +601,11 @@ func (mt *MetaTable) getCollectionByNameInternal(ctx context.Context, dbName str
 		dbName = util.DefaultDBName
 	}
 
+	db, err := mt.getDatabaseByNameInternal(ctx, dbName, typeutil.MaxTimestamp)
+	if err != nil {
+		return nil, err
+	}
+
 	collectionID, ok := mt.aliases.get(dbName, collectionName)
 	if ok {
 		return mt.getCollectionByIDInternal(ctx, dbName, collectionID, ts, false)
@@ -613,11 +618,6 @@ func (mt *MetaTable) getCollectionByNameInternal(ctx context.Context, dbName str
 
 	if isMaxTs(ts) {
 		return nil, merr.WrapErrCollectionNotFoundWithDB(dbName, collectionName)
-	}
-
-	db, err := mt.getDatabaseByNameInternal(ctx, dbName, typeutil.MaxTimestamp)
-	if err != nil {
-		return nil, err
 	}
 
 	// travel meta information from catalog. No need to check time travel logic again, since catalog already did.
