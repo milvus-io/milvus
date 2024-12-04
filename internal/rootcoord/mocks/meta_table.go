a53b33089de3b62a43ac52b4e313c4dd35c3cd1b
bigsheeper <yihao.dai@zilliz.com>
2024-12-04 21:16:56 +0800

    fix mock
    Signed-off-by: bigsheeper <yihao.dai@zilliz.com>

--- internal/rootcoord/mocks/meta_table.go
+++ internal/rootcoord/mocks/meta_table.go
@@ -574,10 +574,6 @@ func (_c *IMetaTable_CreateDatabase_Call) RunAndReturn(run func(context.Context,
 func (_m *IMetaTable) CreatePrivilegeGroup(groupName string) error {
 	ret := _m.Called(groupName)
 
-	if len(ret) == 0 {
-		panic("no return value specified for CreatePrivilegeGroup")
-	}
-
 	var r0 error
 	if rf, ok := ret.Get(0).(func(string) error); ok {
 		r0 = rf(groupName)
@@ -891,10 +887,6 @@ func (_c *IMetaTable_DropGrant_Call) RunAndReturn(run func(string, *milvuspb.Rol
 // DropPrivilegeGroup provides a mock function with given fields: groupName
 func (_m *IMetaTable) DropPrivilegeGroup(groupName string) error {
 	ret := _m.Called(groupName)
-
-	if len(ret) == 0 {
-		panic("no return value specified for DropPrivilegeGroup")
-	}
 
 	var r0 error
 	if rf, ok := ret.Get(0).(func(string) error); ok {
@@ -1361,10 +1353,6 @@ func (_c *IMetaTable_GetDatabaseByName_Call) RunAndReturn(run func(context.Conte
 func (_m *IMetaTable) GetGeneralCount(ctx context.Context) int {
 	ret := _m.Called(ctx)
 
-	if len(ret) == 0 {
-		panic("no return value specified for GetGeneralCount")
-	}
-
 	var r0 int
 	if rf, ok := ret.Get(0).(func(context.Context) int); ok {
 		r0 = rf(ctx)
@@ -1403,62 +1391,9 @@ func (_c *IMetaTable_GetGeneralCount_Call) RunAndReturn(run func(context.Context
 	return _c
 }
 
-// GetPChannelInfo provides a mock function with given fields: ctx, pchannel
-func (_m *IMetaTable) GetPChannelInfo(ctx context.Context, pchannel string) *rootcoordpb.GetPChannelInfoResponse {
-	ret := _m.Called(ctx, pchannel)
-
-	if len(ret) == 0 {
-		panic("no return value specified for GetPChannelInfo")
-	}
-
-	var r0 *rootcoordpb.GetPChannelInfoResponse
-	if rf, ok := ret.Get(0).(func(context.Context, string) *rootcoordpb.GetPChannelInfoResponse); ok {
-		r0 = rf(ctx, pchannel)
-	} else {
-		if ret.Get(0) != nil {
-			r0 = ret.Get(0).(*rootcoordpb.GetPChannelInfoResponse)
-		}
-	}
-
-	return r0
-}
-
-// IMetaTable_GetPChannelInfo_Call is a *mock.Call that shadows Run/Return methods with type explicit version for method 'GetPChannelInfo'
-type IMetaTable_GetPChannelInfo_Call struct {
-	*mock.Call
-}
-
-// GetPChannelInfo is a helper method to define mock.On call
-//   - ctx context.Context
-//   - pchannel string
-func (_e *IMetaTable_Expecter) GetPChannelInfo(ctx interface{}, pchannel interface{}) *IMetaTable_GetPChannelInfo_Call {
-	return &IMetaTable_GetPChannelInfo_Call{Call: _e.mock.On("GetPChannelInfo", ctx, pchannel)}
-}
-
-func (_c *IMetaTable_GetPChannelInfo_Call) Run(run func(ctx context.Context, pchannel string)) *IMetaTable_GetPChannelInfo_Call {
-	_c.Call.Run(func(args mock.Arguments) {
-		run(args[0].(context.Context), args[1].(string))
-	})
-	return _c
-}
-
-func (_c *IMetaTable_GetPChannelInfo_Call) Return(_a0 *rootcoordpb.GetPChannelInfoResponse) *IMetaTable_GetPChannelInfo_Call {
-	_c.Call.Return(_a0)
-	return _c
-}
-
-func (_c *IMetaTable_GetPChannelInfo_Call) RunAndReturn(run func(context.Context, string) *rootcoordpb.GetPChannelInfoResponse) *IMetaTable_GetPChannelInfo_Call {
-	_c.Call.Return(run)
-	return _c
-}
-
-// GetPrivilegeGroupRoles provides a mock function with given fields: ctx, groupName
-func (_m *IMetaTable) GetPrivilegeGroupRoles(ctx context.Context, groupName string) ([]*milvuspb.RoleEntity, error) {
-	ret := _m.Called(ctx, groupName)
-
-	if len(ret) == 0 {
-		panic("no return value specified for GetPrivilegeGroupRoles")
-	}
+// GetPrivilegeGroupRoles provides a mock function with given fields: groupName
+func (_m *IMetaTable) GetPrivilegeGroupRoles(groupName string) ([]*milvuspb.RoleEntity, error) {
+	ret := _m.Called(groupName)
 
 	var r0 []*milvuspb.RoleEntity
 	var r1 error
@@ -1556,10 +1491,6 @@ func (_c *IMetaTable_IsAlias_Call) RunAndReturn(run func(string, string) bool) *
 // IsCustomPrivilegeGroup provides a mock function with given fields: groupName
 func (_m *IMetaTable) IsCustomPrivilegeGroup(groupName string) (bool, error) {
 	ret := _m.Called(groupName)
-
-	if len(ret) == 0 {
-		panic("no return value specified for IsCustomPrivilegeGroup")
-	}
 
 	var r0 bool
 	var r1 error
@@ -2020,10 +1951,6 @@ func (_c *IMetaTable_ListPolicy_Call) RunAndReturn(run func(string) ([]string, e
 func (_m *IMetaTable) ListPrivilegeGroups() ([]*milvuspb.PrivilegeGroupInfo, error) {
 	ret := _m.Called()
 
-	if len(ret) == 0 {
-		panic("no return value specified for ListPrivilegeGroups")
-	}
-
 	var r0 []*milvuspb.PrivilegeGroupInfo
 	var r1 error
 	if rf, ok := ret.Get(0).(func() ([]*milvuspb.PrivilegeGroupInfo, error)); ok {
@@ -2174,10 +2101,6 @@ func (_c *IMetaTable_OperatePrivilege_Call) RunAndReturn(run func(string, *milvu
 // OperatePrivilegeGroup provides a mock function with given fields: groupName, privileges, operateType
 func (_m *IMetaTable) OperatePrivilegeGroup(groupName string, privileges []*milvuspb.PrivilegeEntity, operateType milvuspb.OperatePrivilegeGroupType) error {
 	ret := _m.Called(groupName, privileges, operateType)
-
-	if len(ret) == 0 {
-		panic("no return value specified for OperatePrivilegeGroup")
-	}
 
 	var r0 error
 	if rf, ok := ret.Get(0).(func(string, []*milvuspb.PrivilegeEntity, milvuspb.OperatePrivilegeGroupType) error); ok {
