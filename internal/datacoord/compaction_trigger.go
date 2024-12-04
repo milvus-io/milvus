bae195cf176f98cff94a8139e433f89403485f23
bigsheeper <yihao.dai@zilliz.com>
2024-12-04 21:05:14 +0800

    Reduce mutex contention in datacoord meta
    Signed-off-by: bigsheeper <yihao.dai@zilliz.com>

--- internal/datacoord/compaction_trigger.go
+++ internal/datacoord/compaction_trigger.go
@@ -291,16 +291,21 @@ func (t *compactionTrigger) handleGlobalSignal(signal *compactionSignal) error {
 		zap.Int64("signal.collectionID", signal.collectionID),
 		zap.Int64("signal.partitionID", signal.partitionID),
 		zap.Int64("signal.segmentID", signal.segmentID))
-	partSegments := t.meta.GetSegmentsChanPart(func(segment *SegmentInfo) bool {
-		return (signal.collectionID == 0 || segment.CollectionID == signal.collectionID) &&
-			isSegmentHealthy(segment) &&
+	filters := []SegmentFilter{SegmentFilterFunc(func(segment *SegmentInfo) bool {
+		return isSegmentHealthy(segment) &&
 			isFlush(segment) &&
 			!segment.isCompacting && // not compacting now
 			!segment.GetIsImporting() && // not importing now
 			segment.GetLevel() != datapb.SegmentLevel_L0 && // ignore level zero segments
 			segment.GetLevel() != datapb.SegmentLevel_L2 && // ignore l2 segment
 			!segment.GetIsInvisible()
-	}) // partSegments is list of chanPartSegments, which is channel-partition organized segments
+	})} // partSegments is list of chanPartSegments, which is channel-partition organized segments
+
+	// get all segments if signal.collection == 0, otherwise get collection segments
+	if signal.collectionID != 0 {
+		filters = append(filters, WithCollection(signal.collectionID))
+	}
+	partSegments := GetSegmentsChanPart(t.meta, filters...)
 
 	if len(partSegments) == 0 {
 		log.Info("the length of SegmentsChanPart is 0, skip to handle compaction")
