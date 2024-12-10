a61cec5820b6304c78988fdf274dbf93d62d123a
SimFG <bang.fu@zilliz.com>
2024-12-10 15:25:27 +0800

    enhance: [2.4] keep the latest snapshot key if the origin key is existed
    Signed-off-by: SimFG <bang.fu@zilliz.com>

--- internal/metastore/kv/rootcoord/suffix_snapshot.go
+++ internal/metastore/kv/rootcoord/suffix_snapshot.go
@@ -596,6 +596,10 @@ func (ss *SuffixSnapshot) batchRemoveExpiredKvs(keyGroup []string, originalKey s
 
 	// to protect txn finished with ascend order, reverse the latest kv with tombstone to tail of array
 	sort.Strings(keyGroup)
+	if !includeOriginalKey && len(keyGroup) > 0 {
+		// keep the latest snapshot key for historical version compatibility
+		keyGroup = keyGroup[0 : len(keyGroup)-1]
+	}
 	removeFn := func(partialKeys []string) error {
 		return ss.MetaKv.MultiRemove(partialKeys)
 	}
