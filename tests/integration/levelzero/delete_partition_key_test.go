a427526fa2a2f3520fe94c8e14916b15875ba780
yangxuan <xuan.yang@zilliz.com>
2024-12-10 16:59:45 +0800

    fix coding style
    Signed-off-by: yangxuan <xuan.yang@zilliz.com>

--- tests/integration/levelzero/delete_partition_key_test.go
+++ tests/integration/levelzero/delete_partition_key_test.go
@@ -14,6 +14,7 @@ import (
 	"github.com/milvus-io/milvus/pkg/util/funcutil"
 	"github.com/milvus-io/milvus/pkg/util/merr"
 	"github.com/milvus-io/milvus/pkg/util/metric"
+	"github.com/milvus-io/milvus/pkg/util/paramtable"
 	"github.com/milvus-io/milvus/pkg/util/typeutil"
 	"github.com/milvus-io/milvus/tests/integration"
 )
