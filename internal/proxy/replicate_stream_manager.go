e8529e7fa8eb721d96f2839899ddf612f0257f94
SimFG <bang.fu@zilliz.com>
2024-12-13 15:27:27 +0800

    fix: [2.4] replicate message exception when the ttMsgEnable config is changed dynamically
    Signed-off-by: SimFG <bang.fu@zilliz.com>

--- internal/proxy/replicate_stream_manager.go
+++ internal/proxy/replicate_stream_manager.go
@@ -43,7 +43,7 @@ func (m *ReplicateStreamManager) newMsgStreamResource(channel string) resource.N
 		}
 		msgStream.SetRepackFunc(replicatePackFunc)
 		msgStream.AsProducer([]string{channel})
-		msgStream.EnableProduce(true)
+		msgStream.ForceEnableProduce(true)
 
 		res := resource.NewSimpleResource(msgStream, ReplicateMsgStreamTyp, channel, ReplicateMsgStreamExpireTime, func() {
 			msgStream.Close()
