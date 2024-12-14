fa8ac09550e9cd1a38b434f5819408168d9028ac
SimFG <bang.fu@zilliz.com>
2024-12-14 23:24:51 +0800

    fix: the issue of replicate message exception when the ttMsgEnable config is changed dynamically (#38178)
    - issue: #38177
    
    Signed-off-by: SimFG <bang.fu@zilliz.com>

--- internal/datacoord/mock_test.go
+++ internal/datacoord/mock_test.go
@@ -18,7 +18,6 @@ package datacoord
 
 import (
 	"context"
-	"github.com/milvus-io/milvus/pkg/kv"
 	"testing"
 	"time"
 
@@ -38,6 +37,7 @@ import (
 	"github.com/milvus-io/milvus/internal/proto/proxypb"
 	"github.com/milvus-io/milvus/internal/proto/rootcoordpb"
 	"github.com/milvus-io/milvus/pkg/common"
+	"github.com/milvus-io/milvus/pkg/kv"
 	"github.com/milvus-io/milvus/pkg/util/merr"
 	"github.com/milvus-io/milvus/pkg/util/metricsinfo"
 	"github.com/milvus-io/milvus/pkg/util/tsoutil"
