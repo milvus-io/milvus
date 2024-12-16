15c8e5ec96775353220ef0e74ea0af1de2535ab9
Sumit Dubey <sumit.dubey2@ibm.com>
2024-12-16 07:24:01 -0500

    CI fix for Mac Code Checker and Code Checker
    Signed-off-by: Sumit Dubey <sumit.dubey2@ibm.com>

--- internal/json/sonic_ppc64le.go
+++ internal/json/sonic_ppc64le.go
@@ -1,26 +1,25 @@
 package json
 
 import (
-       "encoding/json"
+	"encoding/json"
 )
 
 var (
-       // Marshal is exported by gin/json package.
-       Marshal = json.Marshal
-       // Unmarshal is exported by gin/json package.
-       Unmarshal = json.Unmarshal
-       // MarshalIndent is exported by gin/json package.
-       MarshalIndent = json.MarshalIndent
-       // NewDecoder is exported by gin/json package.
-       NewDecoder = json.NewDecoder
-       // NewEncoder is exported by gin/json package.
-       NewEncoder = json.NewEncoder
+	// Marshal is exported by gin/json package.
+	Marshal = json.Marshal
+	// Unmarshal is exported by gin/json package.
+	Unmarshal = json.Unmarshal
+	// MarshalIndent is exported by gin/json package.
+	MarshalIndent = json.MarshalIndent
+	// NewDecoder is exported by gin/json package.
+	NewDecoder = json.NewDecoder
+	// NewEncoder is exported by gin/json package.
+	NewEncoder = json.NewEncoder
 )
 
 type (
-        Delim      = json.Delim
-        Decoder    = json.Decoder
-        Number     = json.Number
-        RawMessage = json.RawMessage
+	Delim      = json.Delim
+	Decoder    = json.Decoder
+	Number     = json.Number
+	RawMessage = json.RawMessage
 )
-
