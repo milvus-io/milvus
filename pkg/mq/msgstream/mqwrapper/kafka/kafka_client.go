b18a3cfce2932d0e86a9d2af01a320f62ed7ba9a
Zhen Ye <chyezh@outlook.com>
2024-12-17 23:10:45 +0800

    fix: kafka use shared channel to receive produce result (#38532)
    issue: #38531
    
    ---------
    
    Signed-off-by: chyezh <chyezh@outlook.com>

--- pkg/mq/msgstream/mqwrapper/kafka/kafka_client.go
+++ pkg/mq/msgstream/mqwrapper/kafka/kafka_client.go
@@ -220,8 +220,7 @@ func (kc *kafkaClient) CreateProducer(ctx context.Context, options common.Produc
 	metrics.MsgStreamRequestLatency.WithLabelValues(metrics.CreateProducerLabel).Observe(float64(elapsed.Milliseconds()))
 	metrics.MsgStreamOpCounter.WithLabelValues(metrics.CreateProducerLabel, metrics.SuccessLabel).Inc()
 
-	deliveryChan := make(chan kafka.Event, 128)
-	producer := &kafkaProducer{p: pp, deliveryChan: deliveryChan, topic: options.Topic}
+	producer := &kafkaProducer{p: pp, stopCh: make(chan struct{}), topic: options.Topic}
 	return producer, nil
 }
 
