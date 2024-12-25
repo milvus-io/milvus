1342f38d5f04eb9727d55ae512232f5be27f1988
chasingegg <chao.gao@zilliz.com>
2024-12-25 10:38:58 +0800

    update
    Signed-off-by: chasingegg <chao.gao@zilliz.com>

--- internal/core/src/query/PlanProto.cpp
+++ internal/core/src/query/PlanProto.cpp
@@ -67,8 +67,14 @@ ProtoParser::PlanNodeFromProto(const planpb::PlanNode& plan_node_proto) {
             }
             if (!search_info.iterative_filter_execution &&
                 search_info.search_params_.contains(HINTS)) {
-                search_info.iterative_filter_execution =
-                    (search_info.search_params_[HINTS] == ITERATIVE_FILTER);
+                if (search_info.search_params_[HINTS] == ITERATIVE_FILTER) {
+                    search_info.iterative_filter_execution = true;
+                } else {
+                    // check if hints is valid
+                    PanicInfo(ConfigInvalid,
+                              "hints: {} not supported",
+                              search_info.search_params_[HINTS]);
+                }
             }
         }
 
