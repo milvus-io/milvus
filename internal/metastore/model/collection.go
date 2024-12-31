5ded8c2fbbfdcce7b5431bf841b26f9718fec56f
bigsheeper <yihao.dai@zilliz.com>
2024-12-31 17:44:51 +0800

    fix
    Signed-off-by: bigsheeper <yihao.dai@zilliz.com>

--- internal/metastore/model/collection.go
+++ internal/metastore/model/collection.go
@@ -1,3 +1,19 @@
+// Licensed to the LF AI & Data foundation under one
+// or more contributor license agreements. See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership. The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License. You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package model
 
 import (
@@ -42,6 +58,7 @@ func (c *Collection) ShadowClone() *Collection {
 		DBID:                 c.DBID,
 		CollectionID:         c.CollectionID,
 		Name:                 c.Name,
+		DBName:               c.DBName,
 		Description:          c.Description,
 		AutoID:               c.AutoID,
 		Fields:               c.Fields,
@@ -56,6 +73,7 @@ func (c *Collection) ShadowClone() *Collection {
 		Properties:           c.Properties,
 		State:                c.State,
 		EnableDynamicField:   c.EnableDynamicField,
+		Functions:            c.Functions,
 	}
 }
 
