57e18ea368057559578bda97dbbe3b927d0c269a
Bingyi Sun <sunbingyi1992@gmail.com>
2025-01-03 17:50:54 +0800

    enhance: Fix using wrong pool for warmup (#38941)
    Signed-off-by: sunby <sunbingyi1992@gmail.com>

--- internal/querynodev2/segments/segment.go
+++ internal/querynodev2/segments/segment.go
@@ -1713,7 +1713,7 @@ func (d *AsyncWarmupDispatcher) Run(ctx context.Context) {
 				case <-ctx.Done():
 					return
 				default:
-					GetDynamicPool().Submit(task)
+					GetWarmupPool().Submit(task)
 				}
 			}
 
