2e3fc7cff21863b3eb6825eccdfd2e84f8a84565
Ted Xu <ted.xu@zilliz.com>
2025-01-07 19:48:36 +0800

    fix UT failures and code check breaks
    Signed-off-by: Ted Xu <ted.xu@zilliz.com>

--- internal/flushcommon/syncmgr/pack_writer_test.go
+++ internal/flushcommon/syncmgr/pack_writer_test.go
@@ -23,6 +23,9 @@ import (
 	"testing"
 	"time"
 
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+
 	"github.com/milvus-io/milvus-proto/go-api/v2/commonpb"
 	"github.com/milvus-io/milvus-proto/go-api/v2/schemapb"
 	"github.com/milvus-io/milvus/internal/allocator"
@@ -33,8 +36,6 @@ import (
 	"github.com/milvus-io/milvus/pkg/common"
 	"github.com/milvus-io/milvus/pkg/util/paramtable"
 	"github.com/milvus-io/milvus/pkg/util/tsoutil"
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/mock"
 )
 
 func TestNextID(t *testing.T) {
