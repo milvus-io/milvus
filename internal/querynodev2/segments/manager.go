74d527371e4105df294f9aa11f050342d5d06181
bigsheeper <yihao.dai@zilliz.com>
2025-01-09 14:28:35 +0800

    code format
    Signed-off-by: bigsheeper <yihao.dai@zilliz.com>

--- internal/querynodev2/segments/manager.go
+++ internal/querynodev2/segments/manager.go
@@ -27,7 +27,7 @@ import "C"
 import (
 	"context"
 	"fmt"
-	"github.com/milvus-io/milvus/pkg/util/lock"
+
 	"go.uber.org/zap"
 	"golang.org/x/sync/singleflight"
 
@@ -39,6 +39,7 @@ import (
 	"github.com/milvus-io/milvus/pkg/log"
 	"github.com/milvus-io/milvus/pkg/metrics"
 	"github.com/milvus-io/milvus/pkg/util/cache"
+	"github.com/milvus-io/milvus/pkg/util/lock"
 	"github.com/milvus-io/milvus/pkg/util/merr"
 	"github.com/milvus-io/milvus/pkg/util/metautil"
 	"github.com/milvus-io/milvus/pkg/util/paramtable"
