4b554861e0921e65035eb81c85a0720e84c61d15
Congqi Xia <congqi.xia@zilliz.com>
2025-01-17 09:59:22 +0800

    Skip erase in sorted mode only
    Signed-off-by: Congqi Xia <congqi.xia@zilliz.com>

--- internal/core/src/segcore/ChunkedSegmentSealedImpl.cpp
+++ internal/core/src/segcore/ChunkedSegmentSealedImpl.cpp
@@ -229,10 +229,10 @@ ChunkedSegmentSealedImpl::LoadScalarIndex(const LoadIndexInfo& info) {
     set_bit(index_ready_bitset_, field_id, true);
     update_row_count(row_count);
     // release field column if the index contains raw data
-    // only release non-primary field
+    // only release non-primary field when in pk sorted mode
     if (scalar_indexings_[field_id]->HasRawData() &&
         get_bit(field_data_ready_bitset_, field_id) &&
-        schema_->get_primary_field_id() != field_id) {
+        (schema_->get_primary_field_id() != field_id || !is_sorted_by_pk_)) {
         fields_.erase(field_id);
         set_bit(field_data_ready_bitset_, field_id, false);
     }
