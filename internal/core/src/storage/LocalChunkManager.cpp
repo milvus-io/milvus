96eed06568f9cb365cf0896176141a8aa4b14d01
Bingyi Sun <sunbingyi1992@gmail.com>
2025-01-20 17:33:05 +0800

    enhance: add workaround for concurrent tantivy reload and directory removal (#39441)
    related pr: https://github.com/milvus-io/milvus/pull/39440
    issue: https://github.com/milvus-io/milvus/issues/39428
    
    ---------
    
    Signed-off-by: sunby <sunbingyi1992@gmail.com>

--- internal/core/src/storage/LocalChunkManager.cpp
+++ internal/core/src/storage/LocalChunkManager.cpp
@@ -208,7 +208,17 @@ void
 LocalChunkManager::RemoveDir(const std::string& dir) {
     boost::filesystem::path dirPath(dir);
     boost::system::error_code err;
-    boost::filesystem::remove_all(dirPath, err);
+    int retry_times = 10;
+    // workaround for concurrent tantivy reload and remove dir
+    while (retry_times > 0) {
+        boost::filesystem::remove_all(dirPath, err);
+        if (err) {
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+            retry_times--;
+            continue;
+        }
+        break;
+    }
     if (err) {
         THROWLOCALERROR(FileCreateFailed, RemoveDir);
     }
