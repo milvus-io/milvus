e8f7e1566903fc3565ed121e7f94749b69e1da1d
sunby <sunbingyi1992@gmail.com>
2025-01-20 14:38:46 +0800

    enhance: add workaround for concurrent tantivy reload and directory removal
    Signed-off-by: sunby <sunbingyi1992@gmail.com>

--- internal/core/src/storage/LocalChunkManager.cpp
+++ internal/core/src/storage/LocalChunkManager.cpp
@@ -215,7 +215,16 @@ void
 LocalChunkManager::RemoveDir(const std::string& dir) {
     boost::filesystem::path dirPath(dir);
     boost::system::error_code err;
-    boost::filesystem::remove_all(dirPath, err);
+    int retry_times = 10;
+    // workaround for concurrent tantivy reload and remove dir
+    while (retry_times > 0) {
+        boost::filesystem::remove_all(dirPath, err);
+        if (err) {
+            retry_times--;
+            continue;
+        }
+        break;
+    }
     if (err) {
         PanicInfo(FileWriteFailed,
                   fmt::format("remove local directory:{} failed, error: {}",
