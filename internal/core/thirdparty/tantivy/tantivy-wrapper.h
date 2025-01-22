cc1dda4cc7b79b6b31777c8639f0e554e6d34ae9
sunby <sunbingyi1992@gmail.com>
2025-01-22 14:18:29 +0800

    fix
    Signed-off-by: sunby <sunbingyi1992@gmail.com>

--- internal/core/thirdparty/tantivy/tantivy-wrapper.h
+++ internal/core/thirdparty/tantivy/tantivy-wrapper.h
@@ -10,62 +10,6 @@
 #include "tantivy-binding.h"
 
 namespace milvus::tantivy {
-struct RustArrayWrapper {
-    explicit RustArrayWrapper(RustArray array) : array_(array) {
-    }
-
-    RustArrayWrapper(RustArrayWrapper&) = delete;
-    RustArrayWrapper&
-    operator=(RustArrayWrapper&) = delete;
-
-    RustArrayWrapper(RustArrayWrapper&& other) noexcept {
-        array_.array = other.array_.array;
-        array_.len = other.array_.len;
-        array_.cap = other.array_.cap;
-        other.array_.array = nullptr;
-        other.array_.len = 0;
-        other.array_.cap = 0;
-    }
-
-    RustArrayWrapper&
-    operator=(RustArrayWrapper&& other) noexcept {
-        if (this != &other) {
-            free();
-            array_.array = other.array_.array;
-            array_.len = other.array_.len;
-            array_.cap = other.array_.cap;
-            other.array_.array = nullptr;
-            other.array_.len = 0;
-            other.array_.cap = 0;
-        }
-        return *this;
-    }
-
-    ~RustArrayWrapper() {
-        free();
-    }
-
-    void
-    debug() {
-        std::stringstream ss;
-        ss << "[ ";
-        for (int i = 0; i < array_.len; i++) {
-            ss << array_.array[i] << " ";
-        }
-        ss << "]";
-        std::cout << ss.str() << std::endl;
-    }
-
-    RustArray array_;
-
- private:
-    void
-    free() {
-        if (array_.array != nullptr) {
-            free_rust_array(array_);
-        }
-    }
-};
 
 template <typename T>
 inline TantivyDataType
