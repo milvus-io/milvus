5fb597b37bade8d7a8f08220ece838289a1d6a37
yihao.dai <yihao.dai@zilliz.com>
2025-01-24 10:31:07 +0800

    fix: Remove frequently updating metric to avoid mutex contention (#38775)
    issue: https://github.com/milvus-io/milvus/issues/37630
    
    Reduce the frequency updating metrics to avoid holding the mutex for
    long periods.
    
    ---------
    
    Signed-off-by: bigsheeper <yihao.dai@zilliz.com>

--- internal/querycoordv2/task/scheduler.go
+++ internal/querycoordv2/task/scheduler.go
@@ -245,7 +245,9 @@ type taskScheduler struct {
 	channelTasks *ConcurrentMap[replicaChannelIndex, Task]
 	processQueue *taskQueue
 	waitQueue    *taskQueue
-	taskStats    *expirable.LRU[UniqueID, Task]
+
+	taskStats            *expirable.LRU[UniqueID, Task]
+	lastUpdateMetricTime atomic.Time
 
 	// nodeID -> collectionID -> taskDelta
 	segmentTaskDelta *ExecutingTaskDelta
@@ -364,6 +366,9 @@ func (scheduler *taskScheduler) Add(task Task) error {
 }
 
 func (scheduler *taskScheduler) updateTaskMetrics() {
+	if time.Since(scheduler.lastUpdateMetricTime.Load()) < 30*time.Second {
+		return
+	}
 	segmentGrowNum, segmentReduceNum, segmentUpdateNum, segmentMoveNum := 0, 0, 0, 0
 	leaderGrowNum, leaderReduceNum, leaderUpdateNum := 0, 0, 0
 	channelGrowNum, channelReduceNum, channelMoveNum := 0, 0, 0
@@ -421,6 +426,7 @@ func (scheduler *taskScheduler) updateTaskMetrics() {
 	metrics.QueryCoordTaskNum.WithLabelValues(metrics.ChannelGrowTaskLabel).Set(float64(channelGrowNum))
 	metrics.QueryCoordTaskNum.WithLabelValues(metrics.ChannelReduceTaskLabel).Set(float64(channelReduceNum))
 	metrics.QueryCoordTaskNum.WithLabelValues(metrics.ChannelMoveTaskLabel).Set(float64(channelMoveNum))
+	scheduler.lastUpdateMetricTime.Store(time.Now())
 }
 
 // check whether the task is valid to add,
