7a6ccd62e1737cf8fb83abebe05cd5bd8cf6482d
congqixia <congqi.xia@zilliz.com>
2025-02-13 17:34:14 +0800

    fix: [2.4] Check collection released before target checks (#39842)
    Cherry-pick from master
    pr: #39841
    Related to #39840
    
    The target could be updated async in previous code. This PR make remove
    collection from target observer block until all tasks related in
    dispatchers are removed preventing the metrics being updated after
    collection released.
    
    ---------
    
    Signed-off-by: Congqi Xia <congqi.xia@zilliz.com>

--- internal/querycoordv2/observers/target_observer.go
+++ internal/querycoordv2/observers/target_observer.go
@@ -220,7 +220,9 @@ func (ob *TargetObserver) schedule(ctx context.Context) {
 				delete(ob.readyNotifiers, req.CollectionID)
 				ob.mut.Unlock()
 
+				ob.keylocks.Lock(req.CollectionID)
 				ob.targetMgr.RemoveCollection(req.CollectionID)
+				ob.keylocks.Unlock(req.CollectionID)
 				req.Notifier <- nil
 			case ReleasePartition:
 				ob.targetMgr.RemovePartition(req.CollectionID, req.PartitionIDs...)
@@ -250,6 +252,11 @@ func (ob *TargetObserver) TriggerUpdateCurrentTarget(collectionID int64) {
 func (ob *TargetObserver) check(ctx context.Context, collectionID int64) {
 	ob.keylocks.Lock(collectionID)
 	defer ob.keylocks.Unlock(collectionID)
+
+	// if collection release, skip check
+	if ob.meta.CollectionManager.GetCollection(collectionID) == nil {
+		return
+	}
 
 	if ob.shouldUpdateCurrentTarget(ctx, collectionID) {
 		ob.updateCurrentTarget(collectionID)
