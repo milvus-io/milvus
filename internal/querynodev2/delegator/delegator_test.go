9ea652ab21c8af955984ac86a4ca2362d24b13b3
Wei Liu <wei.liu@zilliz.com>
2025-02-14 14:44:01 +0800

    fix get delete record from delete buffer
    Signed-off-by: Wei Liu <wei.liu@zilliz.com>

--- internal/querynodev2/delegator/delegator_test.go
+++ internal/querynodev2/delegator/delegator_test.go
@@ -325,7 +325,7 @@ func (s *DelegatorSuite) initSegments() {
 			Version:     2001,
 		},
 	)
-	s.delegator.SyncTargetVersion(2001, []int64{500, 501}, []int64{1004}, []int64{1000, 1001, 1002, 1003}, []int64{}, &msgpb.MsgPosition{}, []int64{})
+	s.delegator.SyncTargetVersion(2001, []int64{500, 501}, []int64{1004}, []int64{1000, 1001, 1002, 1003}, []int64{}, &msgpb.MsgPosition{}, &msgpb.MsgPosition{})
 }
 
 func (s *DelegatorSuite) TestSearch() {
