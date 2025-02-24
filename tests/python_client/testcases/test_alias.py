e7d116a16d8b6c1f180fbbed64c94b9177b7121f
bigsheeper <yihao.dai@zilliz.com>
2025-02-24 21:17:46 +0800

    fix case
    Signed-off-by: bigsheeper <yihao.dai@zilliz.com>

--- tests/python_client/testcases/test_alias.py
+++ tests/python_client/testcases/test_alias.py
@@ -194,7 +194,6 @@ class TestAliasOperation(TestcaseBase):
 
         assert res is True
 
-    @pytest.mark.skip(reason="https://github.com/milvus-io/milvus/issues/40030, TODO: need to fix")
     @pytest.mark.tags(CaseLabel.L2)
     def test_alias_called_by_utility_drop_collection(self):
         """
@@ -223,6 +222,7 @@ class TestAliasOperation(TestcaseBase):
         self.utility_wrap.drop_collection(alias_name,
                                           check_task=CheckTasks.err_res,
                                           check_items=error)
+        self.utility_wrap.drop_alias(alias_name)
         self.utility_wrap.drop_collection(c_name)
         assert not self.utility_wrap.has_collection(c_name)[0]
 
@@ -425,7 +425,6 @@ class TestAliasOperationInvalid(TestcaseBase):
                  ct.err_msg: f"cannot drop the collection via alias = {alias_name}"}
         collection_alias.drop(check_task=CheckTasks.err_res, check_items=error)
 
-    @pytest.mark.skip(reason="https://github.com/milvus-io/milvus/issues/40030, TODO: need to fix")
     @pytest.mark.tags(CaseLabel.L0)
     def test_alias_reuse_alias_name_from_dropped_collection(self):
         """
@@ -449,6 +448,7 @@ class TestAliasOperationInvalid(TestcaseBase):
         assert len(res) == 1
 
         # dropping collection that has an alias shall drop the alias as well
+        self.utility_wrap.drop_alias(alias_name)
         collection_w.drop()
         collection_w = self.init_collection_wrap(name=c_name, schema=default_schema,
                                                  check_task=CheckTasks.check_collection_property,
@@ -456,15 +456,10 @@ class TestAliasOperationInvalid(TestcaseBase):
         res2 = self.utility_wrap.list_aliases(c_name)[0]
         assert len(res2) == 0
         # the same alias name can be reused for another collection
-        error = {ct.err_code: 999,
-                 ct.err_msg: f"{alias_name} is alias to another collection: {collection_w.name}: alias already exist"}
-        self.utility_wrap.create_alias(c_name, alias_name,
-                                       check_task=CheckTasks.err_res,
-                                       check_items=error)
-        # res2 = self.utility_wrap.list_aliases(c_name)[0]
-        # assert len(res2) == 1
+        self.utility_wrap.create_alias(c_name, alias_name)
+        res2 = self.utility_wrap.list_aliases(c_name)[0]
+        assert len(res2) == 1
 
-    @pytest.mark.skip(reason="https://github.com/milvus-io/milvus/issues/40030, TODO: need to fix")
     @pytest.mark.tags(CaseLabel.L0)
     def test_alias_rename_collection_to_alias_name(self):
         """
@@ -472,7 +467,7 @@ class TestAliasOperationInvalid(TestcaseBase):
         method:
                 1.create a collection
                 2.create an alias for the collection
-                3.rename the collection to the alias name no matter the collection was dropped or not
+                3.rename the collection to the alias name
         expected: in step 3, rename collection to alias name failed
         """
         self._connect()
@@ -484,12 +479,5 @@ class TestAliasOperationInvalid(TestcaseBase):
         self.utility_wrap.create_alias(collection_w.name, alias_name)
         error = {ct.err_code: 999,
                  ct.err_msg: f"cannot rename collection to an existing alias: {alias_name}"}
-        self.utility_wrap.rename_collection(collection_w.name, alias_name,
-                                            check_task=CheckTasks.err_res, check_items=error)
-
-        collection_w.drop()
-        collection_w = self.init_collection_wrap(name=c_name, schema=default_schema,
-                                                 check_task=CheckTasks.check_collection_property,
-                                                 check_items={exp_name: c_name, exp_schema: default_schema})
         self.utility_wrap.rename_collection(collection_w.name, alias_name,
                                             check_task=CheckTasks.err_res, check_items=error)
