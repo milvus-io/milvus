9fbd456317b4b31d7ab559406eee8e0ae0fecbc1
Wei Liu <wei.liu@zilliz.com>
2025-02-25 21:19:50 +0800

    fix ut
    Signed-off-by: Wei Liu <wei.liu@zilliz.com>

--- internal/querycoordv2/task/scheduler.go
+++ internal/querycoordv2/task/scheduler.go
@@ -176,7 +176,7 @@ func (etd *ExecutingTaskDelta) Add(task Task) {
 	}
 	etd.taskIDRecords.Insert(task.ID())
 
-	collectionID := task.ID()
+	collectionID := task.CollectionID()
 	for _, action := range task.Actions() {
 		nodeID := action.Node()
 		delta := action.WorkLoadEffect()
@@ -201,7 +201,7 @@ func (etd *ExecutingTaskDelta) Sub(task Task) {
 	}
 	etd.taskIDRecords.Remove(task.ID())
 
-	collectionID := task.ID()
+	collectionID := task.CollectionID()
 	for _, action := range task.Actions() {
 		nodeID := action.Node()
 		delta := action.WorkLoadEffect()
