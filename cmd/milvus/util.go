586ea9d28d6e65615e7c6e6bf51f7bb902232bd7
chyezh <chyezh@outlook.com>
2025-02-25 15:04:00 +0800

    enhance: always enable streaming service
    Signed-off-by: chyezh <chyezh@outlook.com>

--- cmd/milvus/util.go
+++ cmd/milvus/util.go
@@ -149,7 +149,6 @@ func GetMilvusRoles(args []string, flags *flag.FlagSet) *roles.MilvusRoles {
 	case typeutil.IndexNodeRole:
 		role.EnableIndexNode = true
 	case typeutil.StreamingNodeRole:
-		streamingutil.MustEnableStreamingService()
 		streamingutil.EnableEmbededQueryNode()
 		role.EnableStreamingNode = true
 		role.EnableQueryNode = true
@@ -205,10 +204,6 @@ func formatFlags(args []string, flags *flag.FlagSet) (alias string, enableRootCo
 	flags.BoolVar(&enableIndexNode, typeutil.IndexNodeRole, false, "enable index node")
 	flags.BoolVar(&enableProxy, typeutil.ProxyRole, false, "enable proxy node")
 	flags.BoolVar(&enableStreamingNode, typeutil.StreamingNodeRole, false, "enable streaming node")
-
-	if enableStreamingNode {
-		streamingutil.MustEnableStreamingService()
-	}
 
 	serverType := args[2]
 	if serverType == typeutil.EmbeddedRole {
