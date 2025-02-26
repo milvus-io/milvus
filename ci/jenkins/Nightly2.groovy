ec9f7a0864fc6cfb183a33b0b6b844080eefb287
nico <cheng.yuan@zilliz.com>
2025-02-26 18:52:31 +0800

    enhance: [skip e2e] change nightly running time[2.5]
    Signed-off-by: nico <cheng.yuan@zilliz.com>

--- ci/jenkins/Nightly2.groovy
+++ ci/jenkins/Nightly2.groovy
@@ -3,7 +3,7 @@
 def pod = libraryResource 'io/milvus/pod/tekton-4am.yaml'
 
 String cron_timezone = 'TZ=Asia/Shanghai'
-String cron_string = BRANCH_NAME == '2.5' ? '50 2 * * * ' : ''
+String cron_string = BRANCH_NAME == '2.5' ? '50 23 * * * ' : ''
 
 // Make timeout 4 hours so that we can run two nightly during the ci
 int total_timeout_minutes = 7 * 60
