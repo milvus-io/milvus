3fe66ec8347e25238611e2ec898806b501852b75
SimFG <bang.fu@zilliz.com>
2025-03-04 14:41:52 +0800

    fix: [2.5] improve error handling and unit tests for InitMetaCache function
    Signed-off-by: SimFG <bang.fu@zilliz.com>

--- internal/datacoord/task_scheduler_test.go
+++ internal/datacoord/task_scheduler_test.go
@@ -23,11 +23,10 @@ import (
 	"testing"
 	"time"
 
-	"go.uber.org/zap"
-
 	"github.com/cockroachdb/errors"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/suite"
+	"go.uber.org/zap"
 	"google.golang.org/grpc"
 
 	"github.com/milvus-io/milvus-proto/go-api/v2/commonpb"
