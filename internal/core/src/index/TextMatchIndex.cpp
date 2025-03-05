345ca01634ccacb44e4d22d704392b955d11f588
Spade  A <71589810+SpadeA-Tang@users.noreply.github.com>
2025-03-05 16:54:09 +0800

    fix: [2.5]fix concurrent issues in null offset #40363 (#40365)
    issue: https://github.com/milvus-io/milvus/issues/40308
    cherry-pick: https://github.com/milvus-io/milvus/pull/40363
    
    ---------
    
    Signed-off-by: SpadeA <tangchenjie1210@gmail.com>

--- internal/core/src/index/TextMatchIndex.cpp
+++ internal/core/src/index/TextMatchIndex.cpp
@@ -147,8 +147,9 @@ TextMatchIndex::Load(const Config& config) {
             binary_set.Append(key, buf, size);
         }
         auto index_valid_data = binary_set.GetByName("index_null_offset");
-        null_offset.resize((size_t)index_valid_data->size / sizeof(size_t));
-        memcpy(null_offset.data(),
+        folly::SharedMutex::WriteHolder lock(mutex_);
+        null_offset_.resize((size_t)index_valid_data->size / sizeof(size_t));
+        memcpy(null_offset_.data(),
                index_valid_data->data.get(),
                (size_t)index_valid_data->size);
     }
@@ -177,7 +178,10 @@ TextMatchIndex::AddText(const std::string& text,
 
 void
 TextMatchIndex::AddNull(int64_t offset) {
-    null_offset.push_back(offset);
+    {
+        folly::SharedMutex::WriteHolder lock(mutex_);
+        null_offset_.push_back(offset);
+    }
     // still need to add null to make offset is correct
     std::string empty = "";
     wrapper_->add_multi_data(&empty, 0, offset);
@@ -192,7 +196,8 @@ TextMatchIndex::AddTexts(size_t n,
         for (int i = 0; i < n; i++) {
             auto offset = i + offset_begin;
             if (!valids[i]) {
-                null_offset.push_back(offset);
+                folly::SharedMutex::WriteHolder lock(mutex_);
+                null_offset_.push_back(offset);
             }
         }
     }
@@ -212,12 +217,16 @@ TextMatchIndex::BuildIndexFromFieldData(
         for (const auto& data : field_datas) {
             total += data->get_null_count();
         }
-        null_offset.reserve(total);
+        {
+            folly::SharedMutex::WriteHolder lock(mutex_);
+            null_offset_.reserve(total);
+        }
         for (const auto& data : field_datas) {
             auto n = data->get_num_rows();
             for (int i = 0; i < n; i++) {
                 if (!data->is_valid(i)) {
-                    null_offset.push_back(i);
+                    folly::SharedMutex::WriteHolder lock(mutex_);
+                    null_offset_.push_back(i);
                 }
                 wrapper_->add_data(
                     static_cast<const std::string*>(data->RawValue(i)),
