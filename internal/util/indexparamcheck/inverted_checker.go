1318870d041643bf5ff25c367832a5368cf82f42
sunby <sunbingyi1992@gmail.com>
2025-03-13 21:54:53 +0800

    fix format
    Signed-off-by: sunby <sunbingyi1992@gmail.com>

--- internal/util/indexparamcheck/inverted_checker.go
+++ internal/util/indexparamcheck/inverted_checker.go
@@ -5,8 +5,6 @@ import (
 
 	"github.com/samber/lo"
 
-	"github.com/samber/lo"
-
 	"github.com/milvus-io/milvus-proto/go-api/v2/schemapb"
 	"github.com/milvus-io/milvus/pkg/v2/common"
 	"github.com/milvus-io/milvus/pkg/v2/util/merr"
