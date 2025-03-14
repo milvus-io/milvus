7ee75723222d2a45b11eafe79d1cb7ca1e72911f
sunby <sunbingyi1992@gmail.com>
2025-03-14 15:33:47 +0800

    fix ut
    Signed-off-by: sunby <sunbingyi1992@gmail.com>

--- internal/util/indexparamcheck/inverted_checker_test.go
+++ internal/util/indexparamcheck/inverted_checker_test.go
@@ -26,7 +26,7 @@ func Test_INVERTEDIndexChecker(t *testing.T) {
 
 func Test_CheckTrain(t *testing.T) {
 	c := newINVERTEDChecker()
-	assert.NoError(t, c.CheckTrain(schemapb.DataType_JSON, map[string]string{"json_cast_type": "bool", "json_path": "json['a']"}))
+	assert.NoError(t, c.CheckTrain(schemapb.DataType_JSON, map[string]string{"json_cast_type": "BOOL", "json_path": "json['a']"}))
 	assert.Error(t, c.CheckTrain(schemapb.DataType_JSON, map[string]string{"json_cast_type": "array", "json_path": "json['a']"}))
 	assert.Error(t, c.CheckTrain(schemapb.DataType_JSON, map[string]string{"json_cast_type": "abc", "json_path": "json['a']"}))
 }
