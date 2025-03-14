6982f007e21e5aeafcef168a2479889324ab2dd5
Zhen Ye <chyezh@outlook.com>
2025-03-14 10:58:11 +0800

    enhance: add walimpls access mode options (#40591)
    issue: #40532
    
    Signed-off-by: chyezh <chyezh@outlook.com>

--- pkg/streaming/walimpls/impls/rmq/opener.go
+++ pkg/streaming/walimpls/impls/rmq/opener.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/milvus-io/milvus/pkg/v2/mq/mqimpl/rocksmq/client"
+	"github.com/milvus-io/milvus/pkg/v2/streaming/util/types"
 	"github.com/milvus-io/milvus/pkg/v2/streaming/walimpls"
 	"github.com/milvus-io/milvus/pkg/v2/streaming/walimpls/helper"
 )
@@ -17,11 +18,17 @@ type openerImpl struct {
 
 // Open opens a new wal.
 func (o *openerImpl) Open(ctx context.Context, opt *walimpls.OpenOption) (walimpls.WALImpls, error) {
-	p, err := o.c.CreateProducer(client.ProducerOptions{
-		Topic: opt.Channel.Name,
-	})
-	if err != nil {
+	if err := opt.Validate(); err != nil {
 		return nil, err
+	}
+	var p client.Producer
+	if opt.Channel.AccessMode == types.AccessModeRW {
+		var err error
+		if p, err = o.c.CreateProducer(client.ProducerOptions{
+			Topic: opt.Channel.Name,
+		}); err != nil {
+			return nil, err
+		}
 	}
 	return &walImpl{
 		WALHelper: helper.NewWALHelper(opt),
