db8d20f735a7efbdb6d5740f87bf725066905a12
SimFG <bang.fu@zilliz.com>
2025-03-14 11:08:32 +0800

    enhance: add GetService method to retrieve MilvusServiceClient
    Signed-off-by: SimFG <bang.fu@zilliz.com>

--- client/milvusclient/client.go
+++ client/milvusclient/client.go
@@ -236,3 +236,7 @@ func (c *Client) callService(fn func(milvusService milvuspb.MilvusServiceClient)
 
 	return fn(c.service)
 }
+
+func (c *Client) GetService() milvuspb.MilvusServiceClient {
+	return c.service
+}
