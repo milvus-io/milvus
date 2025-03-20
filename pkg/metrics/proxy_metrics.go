5b9042587adcd467a74cb9390b51c72357062c14
Buqian Zheng <zhengbuqian@gmail.com>
2025-03-20 14:24:13 +0800

    enhance: [2.4] improve sparse query nnz metric (#40715)
    add query type and field id label; add metric for hybrid search
    
    issue: https://github.com/milvus-io/milvus/issues/35853
    pr: https://github.com/milvus-io/milvus/pull/40713
    
    Signed-off-by: Buqian Zheng <zhengbuqian@gmail.com>

--- pkg/metrics/proxy_metrics.go
+++ pkg/metrics/proxy_metrics.go
@@ -416,7 +416,7 @@ var (
 			Name:      "search_sparse_num_non_zeros",
 			Help:      "the number of non-zeros in each sparse search task",
 			Buckets:   buckets,
-		}, []string{nodeIDLabelName, collectionName})
+		}, []string{nodeIDLabelName, collectionName, queryTypeLabelName, fieldIDLabelName})
 
 	// ProxyRecallSearchCount records the counter that users issue recall evaluation requests, which are cpu-intensive
 	ProxyRecallSearchCount = prometheus.NewCounterVec(
