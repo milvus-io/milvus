eab262e401c150fc7ad98b931a9ad6f083f371ed
SpadeA <tangchenjie1210@gmail.com>
2025-03-21 17:38:08 +0800

    fix tests
    Signed-off-by: SpadeA <tangchenjie1210@gmail.com>

--- internal/core/src/index/IndexInfo.h
+++ internal/core/src/index/IndexInfo.h
@@ -26,8 +26,8 @@ struct CreateIndexInfo {
     IndexVersion index_engine_version;
     std::string field_name;
     int64_t dim;
-    int32_t scalar_index_engine_version;
-    uint32_t tantivy_index_version;
+    int32_t scalar_index_engine_version{1};
+    uint32_t tantivy_index_version{7};
     DataType json_cast_type;
     std::string json_path;
 };
