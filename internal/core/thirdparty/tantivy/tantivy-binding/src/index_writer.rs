d88593a250bba71a447ee83ba9715728c16479f5
SpadeA <tangchenjie1210@gmail.com>
2025-04-01 10:40:29 +0800

    simplify batch
    Signed-off-by: SpadeA <tangchenjie1210@gmail.com>

--- internal/core/thirdparty/tantivy/tantivy-binding/src/index_writer.rs
+++ internal/core/thirdparty/tantivy/tantivy-binding/src/index_writer.rs
@@ -197,44 +197,20 @@ impl IndexWriterWrapper {
                 let json_offsets =
                     unsafe { std::slice::from_raw_parts(*json_offsets, *json_offsets_len) };
 
-                if *json_offsets_len > BATCH_SIZE {
-                    if !batch.is_empty() {
+                for offset in json_offsets {
+                    batch.push(UserOperation::Add(doc!(
+                        id_field => *offset,
+                        self.field => key,
+                    )));
+
+                    if batch.len() >= BATCH_SIZE {
                         writer.run(std::mem::replace(
                             &mut batch,
                             Vec::with_capacity(BATCH_SIZE),
                         ))?;
                     }
-                    if *json_offsets_len > BATCH_SIZE {
-                        for chunk in json_offsets.chunks(BATCH_SIZE) {
-                            let ops: Vec<_> = chunk
-                                .iter()
-                                .map(|offset| {
-                                    UserOperation::Add(doc!(
-                                        id_field => *offset,
-                                        self.field => key,
-                                    ))
-                                })
-                                .collect();
-                            writer.run(ops)?;
-                        }
-
-                        return Ok(());
-                    }
                 }
 
-                if batch.len() + *json_offsets_len > BATCH_SIZE {
-                    writer.run(std::mem::replace(
-                        &mut batch,
-                        Vec::with_capacity(BATCH_SIZE),
-                    ))?;
-                }
-
-                batch.extend(json_offsets.iter().map(|offset| {
-                    UserOperation::Add(doc!(
-                        id_field => *offset,
-                        self.field => key,
-                    ))
-                }));
                 Ok(())
             })?;
 
