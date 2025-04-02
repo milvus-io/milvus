37a533fe6dd712c12915be2be768ef05559670a0
wei liu <wei.liu@zilliz.com>
2025-04-03 01:26:23 +0800

    fix: [2.5] Address manual balance and balance check issues (#41038)
    issue: #37651
    pr: #41037
    - Fix context propagation for manual balance segment task creation from
    PR #38080.
    - Optimize stopping balance by preventing redundant checks per round,
    addressing performance regression from PR #40297.
    - Decrease default `checkBalanceInterval` from 3000ms to 300ms.
    - Correct minor log messages in `BalanceChecker`.
    
    ---------
    
    Signed-off-by: Wei Liu <wei.liu@zilliz.com>

--- pkg/util/paramtable/component_param.go
+++ pkg/util/paramtable/component_param.go
@@ -2213,7 +2213,7 @@ If this parameter is set false, Milvus simply searches the growing segments with
 	p.BalanceCheckInterval = ParamItem{
 		Key:          "queryCoord.checkBalanceInterval",
 		Version:      "2.3.0",
-		DefaultValue: "3000",
+		DefaultValue: "300",
 		PanicIfEmpty: true,
 		Export:       true,
 	}
