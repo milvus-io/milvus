milvus-io/milvus

commit 0ad55c6c44dfd48de2e2ed563510822fd4ad2729

fix: fix loaded entity num is inaccurate (#33522)
issue: #33520

Signed-off-by: jaime <yun.zhang@zilliz.com>

internal/querynodev2/metrics_info.go
@@ -103,10 +103,11 @@ func getQuotaMetrics(node *QueryNode) (*metricsinfo.QueryNodeQuotaMetrics, error
 	}
 
 	minTsafeChannel, minTsafe := node.tSafeManager.Min()
-
 	collections := node.manager.Collection.List()
-
 	nodeID := fmt.Sprint(node.GetNodeID())
+
+	metrics.QueryNodeNumEntities.Reset()
+	metrics.QueryNodeEntitiesSize.Reset()
 
 	var totalGrowingSize int64
 	growingSegments := node.manager.Segment.GetBy(segments.WithType(segments.SegmentTypeGrowing))
@@ -125,6 +126,7 @@ func getQuotaMetrics(node *QueryNode) (*metricsinfo.QueryNodeQuotaMetrics, error
 	growingGroupByPartition := lo.GroupBy(growingSegments, func(seg segments.Segment) int64 {
 		return seg.Partition()
 	})
+
 	for _, segs := range growingGroupByPartition {
 		numEntities := lo.SumBy(segs, func(seg segments.Segment) int64 {
 			return seg.RowNum()
@@ -136,7 +138,6 @@ func getQuotaMetrics(node *QueryNode) (*metricsinfo.QueryNodeQuotaMetrics, error
 			fmt.Sprint(segment.Collection()),
 			fmt.Sprint(segment.Partition()),
 			segments.SegmentTypeGrowing.String(),
-			fmt.Sprint(len(segment.Indexes())),
 		).Set(float64(numEntities))
 	}
 
@@ -166,7 +167,6 @@ func getQuotaMetrics(node *QueryNode) (*metricsinfo.QueryNodeQuotaMetrics, error
 			fmt.Sprint(segment.Collection()),
 			fmt.Sprint(segment.Partition()),
 			segments.SegmentTypeSealed.String(),
-			fmt.Sprint(len(segment.Indexes())),
 		).Set(float64(numEntities))
 	}
 

pkg/metrics/querynode_metrics.go
@@ -370,7 +370,6 @@ var (
 			collectionIDLabelName,
 			partitionIDLabelName,
 			segmentStateLabelName,
-			indexCountLabelName,
 		})
 
 	QueryNodeEntitiesSize = prometheus.NewGaugeVec(

commit 3656625669bd7c67363a814f1f0439911e6fc20f

fix: remove metrics reset calls to ensure accurate reporting
Fixes issue introduced in PR #33522 where metric resets caused incomplete data collection by monitoring systems.

Signed-off-by: Wei Liu <wei.liu@zilliz.com>

internal/querynodev2/metrics_info.go
@@ -75,9 +75,6 @@ func getQuotaMetrics(node *QueryNode) (*metricsinfo.QueryNodeQuotaMetrics, error
 	collections := node.manager.Collection.ListWithName()
 	nodeID := fmt.Sprint(node.GetNodeID())
 
-	metrics.QueryNodeNumEntities.Reset()
-	metrics.QueryNodeEntitiesSize.Reset()
-
 	var totalGrowingSize int64
 	growingSegments := node.manager.Segment.GetBy(segments.WithType(segments.SegmentTypeGrowing))
 	growingGroupByCollection := lo.GroupBy(growingSegments, func(seg segments.Segment) int64 {