e4a38ac3b103d10e4d77b147129c5b66d2a3732a
Divya <divyaruhil999@gmail.com>
2025-04-07 02:41:11 -0700

    Security Fixes for RESTful APIs
    Signed-off-by: Divya <divyaruhil999@gmail.com>

--- internal/distributed/proxy/httpserver/utils.go
+++ internal/distributed/proxy/httpserver/utils.go
@@ -1749,6 +1749,8 @@ func RequestHandlerFunc(c *gin.Context) {
 	c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 	c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 	c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, HEAD, POST, PUT, DELETE, OPTIONS, PATCH")
+	c.Writer.Header().Set("Strict-Transport-Security", "max-age=31536000; includeSubDomains") // Ensures HTTPS usage
+	c.Writer.Header().Set("X-Content-Type-Options", "nosniff") // Prevents MIME sniffing
 	if c.Request.Method == "OPTIONS" {
 		c.AbortWithStatus(204)
 		return
