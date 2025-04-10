971ca06eca60779ed2d01eabb612c48443a290da
Divya <divyaruhil999@gmail.com>
2025-04-10 02:41:24 -0700

    Made changes acc to Review Comments for Security Fixes for RESTful APIs
    Signed-off-by: Divya <divyaruhil999@gmail.com>

--- internal/distributed/proxy/httpserver/utils.go
+++ internal/distributed/proxy/httpserver/utils.go
@@ -1749,8 +1749,17 @@ func RequestHandlerFunc(c *gin.Context) {
 	c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 	c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 	c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, HEAD, POST, PUT, DELETE, OPTIONS, PATCH")
-	c.Writer.Header().Set("Strict-Transport-Security", "max-age=31536000; includeSubDomains") // Ensures HTTPS usage
 	c.Writer.Header().Set("X-Content-Type-Options", "nosniff") // Prevents MIME sniffing
+
+	maxAge := paramtable.Get().HTTPCfg.HSTSMaxAge.GetValue()
+	includeSubDomains := paramtable.Get().HTTPCfg.HSTSIncludeSubDomains.GetAsBool()
+
+	hstsValue := fmt.Sprintf("max-age=%s", maxAge)
+	if includeSubDomains {
+		hstsValue += "; includeSubDomains"
+	}
+	c.Writer.Header().Set("Strict-Transport-Security", hstsValue)
+
 	if c.Request.Method == "OPTIONS" {
 		c.AbortWithStatus(204)
 		return
