21586081176e670270034bb0df207610197ab277
Congqi Xia <congqi.xia@zilliz.com>
2025-04-11 10:41:50 +0800

    enhance: [GoSDK] Add more example for godoc
    Related to #31293
    
    Signed-off-by: Congqi Xia <congqi.xia@zilliz.com>

--- client/milvusclient/collection_example_test.go
+++ client/milvusclient/collection_example_test.go
@@ -197,7 +197,6 @@ func ExampleClient_CreateCollection_quickSetupCustomize() {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	collectionName := `quick_setup_3`
 	cli, err := milvusclient.New(ctx, &milvusclient.ClientConfig{
 		Address: milvusAddr,
 	})
@@ -205,9 +204,13 @@ func ExampleClient_CreateCollection_quickSetupCustomize() {
 		// handle err
 	}
 
-	err = cli.CreateCollection(ctx, milvusclient.SimpleCreateCollectionOptions(collectionName, 512).
-		WithVarcharPK(true, 64).
-		WithShardNum(1),
+	err = cli.CreateCollection(ctx, milvusclient.SimpleCreateCollectionOptions("custom_quick_setup", 512).
+		WithPKFieldName("my_id").
+		WithVarcharPK(true, 512).
+		WithVectorFieldName("my_vector").
+		WithMetricType(entity.L2).
+		WithShardNum(5).
+		WithAutoID(true),
 	)
 	if err != nil {
 		log.Println(err.Error())
@@ -234,6 +237,132 @@ func ExampleClient_CreateCollection_consistencyLevel() {
 		WithField(entity.NewField().WithName("my_varchar").WithDataType(entity.FieldTypeVarChar).WithMaxLength(512))
 
 	err = cli.CreateCollection(ctx, milvusclient.NewCreateCollectionOption(collectionName, schema).WithConsistencyLevel(entity.ClBounded))
+	if err != nil {
+		// handle error
+	}
+}
+
+func ExampleClient_CreateCollection_withIndexes() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	collectionName := `customized_setup_5`
+
+	cli, err := milvusclient.New(ctx, &milvusclient.ClientConfig{
+		Address: milvusAddr,
+	})
+	if err != nil {
+		// handle err
+	}
+
+	schema := entity.NewSchema().WithDynamicFieldEnabled(true).
+		WithField(entity.NewField().WithName("my_id").WithIsAutoID(true).WithDataType(entity.FieldTypeInt64).WithIsPrimaryKey(true)).
+		WithField(entity.NewField().WithName("my_vector").WithDataType(entity.FieldTypeFloatVector).WithDim(5)).
+		WithField(entity.NewField().WithName("my_varchar").WithDataType(entity.FieldTypeVarChar).WithMaxLength(512))
+
+	idx := index.NewAutoIndex(entity.IP)
+	indexOption := milvusclient.NewCreateIndexOption("my_dense_collection", "dense_vector", idx)
+
+	err = cli.CreateCollection(ctx,
+		milvusclient.NewCreateCollectionOption(collectionName, schema).
+			WithIndexOptions(indexOption))
+	if err != nil {
+		// handle error
+	}
+}
+
+func ExampleClient_CreateCollection_binaryVector() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	collectionName := `my_binary_collection`
+
+	cli, err := milvusclient.New(ctx, &milvusclient.ClientConfig{
+		Address: milvusAddr,
+	})
+	if err != nil {
+		// handle err
+	}
+
+	schema := entity.NewSchema()
+	schema.WithField(entity.NewField().
+		WithName("pk").
+		WithDataType(entity.FieldTypeVarChar).
+		WithMaxLength(100).
+		WithIsAutoID(true),
+	).WithField(entity.NewField().
+		WithName("binary_vector").
+		WithDataType(entity.FieldTypeBinaryVector).
+		WithDim(128),
+	)
+
+	idx := index.NewAutoIndex(entity.HAMMING)
+	indexOption := milvusclient.NewCreateIndexOption("my_binary_collection", "binary_vector", idx)
+
+	err = cli.CreateCollection(ctx,
+		milvusclient.NewCreateCollectionOption(collectionName, schema).
+			WithIndexOptions(indexOption))
+	if err != nil {
+		// handle error
+	}
+}
+
+func ExampleClient_CreateCollection_jsonField() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	cli, err := milvusclient.New(ctx, &milvusclient.ClientConfig{
+		Address: milvusAddr,
+	})
+	if err != nil {
+		// handle err
+	}
+
+	schema := entity.NewSchema()
+	schema.WithField(entity.NewField().
+		WithName("pk").
+		WithDataType(entity.FieldTypeInt64).
+		WithIsAutoID(true),
+	).WithField(entity.NewField().
+		WithName("embedding").
+		WithDataType(entity.FieldTypeFloatVector).
+		WithDim(3),
+	).WithField(entity.NewField().
+		WithName("metadata").
+		WithDataType(entity.FieldTypeJSON),
+	)
+
+	jsonIndex1 := index.NewJSONPathIndex(index.Inverted, "varchar", `metadata["product_info"]["category"]`)
+	jsonIndex2 := index.NewJSONPathIndex(index.Inverted, "double", `metadata["price"]`)
+	indexOpt1 := milvusclient.NewCreateIndexOption("my_json_collection", "meta", jsonIndex1)
+	indexOpt2 := milvusclient.NewCreateIndexOption("my_json_collection", "meta", jsonIndex2)
+
+	vectorIndex := index.NewAutoIndex(entity.COSINE)
+	indexOpt := milvusclient.NewCreateIndexOption("my_json_collection", "embedding", vectorIndex)
+
+	err = cli.CreateCollection(ctx, milvusclient.NewCreateCollectionOption("my_json_collection", schema).
+		WithIndexOptions(indexOpt1, indexOpt2, indexOpt))
+	if err != nil {
+		// handler err
+	}
+}
+
+func ExampleClient_CreateCollection_dynamicSchema() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// collectionName := `my_dynamic_collection`
+
+	cli, err := milvusclient.New(ctx, &milvusclient.ClientConfig{
+		Address: milvusAddr,
+	})
+	if err != nil {
+		// handle err
+	}
+
+	err = cli.CreateCollection(ctx,
+		milvusclient.SimpleCreateCollectionOptions("my_dynamic_collection", 5).
+			WithDynamicSchema(true))
 	if err != nil {
 		// handle error
 	}
