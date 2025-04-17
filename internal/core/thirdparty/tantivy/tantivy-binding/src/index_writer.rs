25cad013df811fe31316587b530558dfeb5192e3
SpadeA <tangchenjie1210@gmail.com>
2025-04-17 12:11:20 +0800

    remove redundant test
    Signed-off-by: SpadeA <tangchenjie1210@gmail.com>

--- internal/core/thirdparty/tantivy/tantivy-binding/src/index_writer.rs
+++ internal/core/thirdparty/tantivy/tantivy-binding/src/index_writer.rs
@@ -158,9 +158,10 @@ impl IndexWriterWrapper {
 
 #[cfg(test)]
 mod tests {
-    use std::ops::Bound;
+    use std::{ffi::CString, ops::Bound};
 
-    use tempfile::TempDir;
+    use rand::Rng;
+    use tempfile::{tempdir, TempDir};
 
     use crate::{data_type::TantivyDataType, TantivyIndexVersion};
 
@@ -189,7 +190,7 @@ mod tests {
             index_wrapper.commit().unwrap();
         }
 
-        use tantivy_5::{collector, query, Index, ReloadPolicy};
+        use tantivy_5::{query, Index, ReloadPolicy};
         let index = Index::open_in_dir(dir.path()).unwrap();
         let reader = index
             .reader_builder()
@@ -332,74 +333,5 @@ mod tests {
         index_writer.commit().unwrap();
         let count = index_writer.create_reader().unwrap().count().unwrap();
         assert_eq!(count, total_count);
-    }
-
-    #[test]
-    pub fn test_add_strings_by_batch() {
-        use crate::data_type::TantivyDataType;
-        use crate::index_writer::IndexWriterWrapper;
-
-        let temp_dir = tempdir().unwrap();
-        let mut index_writer = IndexWriterWrapper::new(
-            "test",
-            TantivyDataType::Keyword,
-            temp_dir.path().to_str().unwrap().to_string(),
-            1,
-            15 * 1024 * 1024,
-            TantivyIndexVersion::V7,
-        )
-        .unwrap();
-
-        let keys = (0..10000)
-            .map(|i| format!("key{:05}", i))
-            .collect::<Vec<_>>();
-
-        let c_keys: Vec<CString> = keys.into_iter().map(|k| CString::new(k).unwrap()).collect();
-        let key_ptrs: Vec<*const libc::c_char> = c_keys.iter().map(|cs| cs.as_ptr()).collect();
-
-        index_writer
-            .add_string_by_batch(&key_ptrs, Some(0))
-            .unwrap();
-        index_writer.commit().unwrap();
-        let reader = index_writer.create_reader().unwrap();
-        let count: u32 = reader.count().unwrap();
-        assert_eq!(count, 10000);
-    }
-
-    #[test]
-    pub fn test_add_data_by_batch() {
-        use crate::data_type::TantivyDataType;
-        use crate::index_writer::IndexWriterWrapper;
-
-        let temp_dir = tempdir().unwrap();
-        let mut index_writer = IndexWriterWrapper::new(
-            "test",
-            TantivyDataType::I64,
-            temp_dir.path().to_str().unwrap().to_string(),
-            1,
-            15 * 1024 * 1024,
-            TantivyIndexVersion::V7,
-        )
-        .unwrap();
-
-        let keys = (0..10000).collect::<Vec<_>>();
-
-        let mut count = 0;
-        for i in keys {
-            index_writer
-                .add_data_by_batch::<i64>(&[i], Some(i as i64))
-                .unwrap();
-
-            count += 1;
-
-            if count % 1000 == 0 {
-                index_writer.commit().unwrap();
-            }
-        }
-
-        index_writer.commit().unwrap();
-        let reader = index_writer.create_reader().unwrap();
-        let count: u32 = reader.count().unwrap();
-        assert_eq!(count, 10000);
     }
 }
