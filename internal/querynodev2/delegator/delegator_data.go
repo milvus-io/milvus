389245188079f5ccac67861c64e7e041c4c26aa9
aoiasd <45024769+aoiasd@users.noreply.github.com>
2025-04-27 17:34:38 +0800

    fix: bm25 search failed when avgdl == nan (#41502)
    relate: https://github.com/milvus-io/milvus/issues/41490
    
    ---------
    
    Signed-off-by: aoiasd <zhicheng.yue@zilliz.com>

--- internal/querynodev2/delegator/delegator_data.go
+++ internal/querynodev2/delegator/delegator_data.go
@@ -954,11 +954,6 @@ func (sd *shardDelegator) ReleaseSegments(ctx context.Context, req *querypb.Rele
 			pkoracle.WithSegmentType(commonpb.SegmentState_Sealed),
 			pkoracle.WithWorkerID(targetNodeID),
 		)
-		if sd.idfOracle != nil {
-			for _, segment := range sealed {
-				sd.idfOracle.Remove(segment.SegmentID, commonpb.SegmentState_Sealed)
-			}
-		}
 	}
 	if len(growing) > 0 {
 		sd.pkOracle.Remove(
@@ -967,7 +962,7 @@ func (sd *shardDelegator) ReleaseSegments(ctx context.Context, req *querypb.Rele
 		)
 		if sd.idfOracle != nil {
 			for _, segment := range growing {
-				sd.idfOracle.Remove(segment.SegmentID, commonpb.SegmentState_Growing)
+				sd.idfOracle.RemoveGrowing(segment.SegmentID)
 			}
 		}
 	}
@@ -1124,6 +1119,10 @@ func (sd *shardDelegator) buildBM25IDF(req *internalpb.SearchRequest) (float64, 
 	idfSparseVector, avgdl, err := sd.idfOracle.BuildIDF(req.GetFieldId(), tfArray)
 	if err != nil {
 		return 0, err
+	}
+
+	if avgdl <= 0 {
+		return 0, nil
 	}
 
 	for _, idf := range idfSparseVector {
