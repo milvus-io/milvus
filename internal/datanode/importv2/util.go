881eeb6cf4ac09b412d48a4273d30883957a0618
aoiasd <zhicheng.yue@zilliz.com>
2025-04-27 20:03:44 +0800

    bulk insert should use embedding function's input instead meta
    Signed-off-by: aoiasd <zhicheng.yue@zilliz.com>

--- internal/datanode/importv2/util.go
+++ internal/datanode/importv2/util.go
@@ -228,13 +228,17 @@ func RunBm25Function(task *ImportTask, data *storage.InsertData) error {
 		if err != nil {
 			return err
 		}
+
 		if runner == nil {
 			continue
 		}
-		inputDatas := make([]any, 0, len(fn.InputFieldIds))
-		for _, inputFieldID := range fn.InputFieldIds {
+
+		inputFieldIDs := lo.Map(runner.GetInputFields(), func(field *schemapb.FieldSchema, _ int) int64 { return field.GetFieldID() })
+		inputDatas := make([]any, 0, len(inputFieldIDs))
+		for _, inputFieldID := range inputFieldIDs {
 			inputDatas = append(inputDatas, data.Data[inputFieldID].GetDataRows())
 		}
+
 		outputFieldData, err := runner.BatchRun(inputDatas...)
 		if err != nil {
 			return err
