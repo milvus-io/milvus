f65f69b597f3d58b6977b83aaa59201a4cf967fb
Congqi Xia <congqi.xia@zilliz.com>
2025-05-12 11:33:09 +0800

    fix: [AddField] Use latest schema instead of cached one
    Related to #41713 #41710
    
    Signed-off-by: Congqi Xia <congqi.xia@zilliz.com>

--- internal/flushcommon/pipeline/flow_graph_write_node.go
+++ internal/flushcommon/pipeline/flow_graph_write_node.go
@@ -28,7 +28,6 @@ type writeNode struct {
 	wbManager   writebuffer.BufferManager
 	updater     util.StatsUpdater
 	metacache   metacache.MetaCache
-	collSchema  *schemapb.CollectionSchema
 	pkField     *schemapb.FieldSchema
 }
 
@@ -83,7 +82,7 @@ func (wNode *writeNode) Operate(in []Msg) []Msg {
 	start, end := fgMsg.StartPositions[0], fgMsg.EndPositions[0]
 
 	if fgMsg.InsertData == nil {
-		insertData, err := writebuffer.PrepareInsert(wNode.collSchema, wNode.pkField, fgMsg.InsertMessages)
+		insertData, err := writebuffer.PrepareInsert(wNode.metacache.Schema(), wNode.pkField, fgMsg.InsertMessages)
 		if err != nil {
 			log.Error("failed to prepare data", zap.Error(err))
 			panic(err)
@@ -161,7 +160,6 @@ func newWriteNode(
 		wbManager:   writeBufferManager,
 		updater:     updater,
 		metacache:   config.metacache,
-		collSchema:  collSchema,
 		pkField:     pkField,
 	}, nil
 }
