9ba8e4dc03b372f681a701edf23ee6b16bbb0a74
yanliang567 <yanliang.qiao@zilliz.com>
2025-05-20 11:02:05 +0800

    Skip async patition for an pymilvus issue
    Signed-off-by: yanliang567 <yanliang.qiao@zilliz.com>

--- tests/python_client/utils/api_request.py
+++ tests/python_client/utils/api_request.py
@@ -47,10 +47,9 @@ def api_request(_list, **kwargs):
             arg = _list[1:]
             arg_str = str(arg)
             log_arg = arg_str[0:log_row_length] + '......' if len(arg_str) > log_row_length else arg_str
-            # if enable_traceback == "True":
             if kwargs.get("enable_traceback", True):
-                log_kwargs = kwargs[0:log_row_length] + '......' if len(kwargs) > log_row_length else kwargs
-                log.debug("(api_request)  : [%s] args: %s, kwargs: %s" % (func.__qualname__, log_arg, str(log_kwargs)))
+                log_kwargs = str(kwargs)[0:log_row_length] + '......' if len(str(kwargs)) > log_row_length else str(kwargs)
+                log.debug("(api_request)  : [%s] args: %s, kwargs: %s" % (func.__qualname__, log_arg, log_kwargs))
             return func(*arg, **kwargs)
     return False, False
 
@@ -62,8 +61,8 @@ def logger_interceptor():
             arg_str = str(arg)
             log_arg = arg_str[0:log_row_length] + '......' if len(arg_str) > log_row_length else arg_str
             if kwargs.get("enable_traceback", True):
-                log_kwargs = kwargs[0:log_row_length] + '......' if len(kwargs) > log_row_length else kwargs
-                log.debug("(api_request)  : [%s] args: %s, kwargs: %s" % (func.__name__, log_arg, str(log_kwargs)))
+                log_kwargs = str(kwargs)[0:log_row_length] + '......' if len(str(kwargs)) > log_row_length else str(kwargs)
+                log.debug("(api_request)  : [%s] args: %s, kwargs: %s" % (func.__name__, log_arg, log_kwargs))
 
         def log_response(res, **kwargs):
             if kwargs.get("enable_traceback", True):
