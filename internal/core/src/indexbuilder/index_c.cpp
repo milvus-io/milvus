dbfffcbba8e0d8e978c20baecb00bcd81bd7eb64
shaoting-huang <shaoting.huang@zilliz.com>
2025-06-03 16:18:01 +0800

    fix asan
    Signed-off-by: shaoting-huang <shaoting.huang@zilliz.com>

--- internal/core/src/indexbuilder/index_c.cpp
+++ internal/core/src/indexbuilder/index_c.cpp
@@ -254,11 +254,13 @@ CreateIndex(CIndex* res_index,
         auto status = CStatus();
         status.error_code = e.get_error_code();
         status.error_msg = strdup(e.what());
+        milvus_storage::ArrowFileSystemSingleton::GetInstance().Release();
         return status;
     } catch (std::exception& e) {
         auto status = CStatus();
         status.error_code = UnexpectedError;
         status.error_msg = strdup(e.what());
+        milvus_storage::ArrowFileSystemSingleton::GetInstance().Release();
         return status;
     }
 }
