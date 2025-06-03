10e039fddd9964ac820de9775c57a354d4e8caa0
Cai Zhang <cai.zhang@zilliz.com>
2025-06-03 14:52:36 +0800

    Importing segments stats task must be triggered by import_checker
    Signed-off-by: Cai Zhang <cai.zhang@zilliz.com>

--- internal/datacoord/stats_inspector.go
+++ internal/datacoord/stats_inspector.go
@@ -154,7 +154,8 @@ func (si *statsInspector) triggerStatsTaskLoop() {
 
 func (si *statsInspector) triggerSortStatsTask() {
 	invisibleSegments := si.mt.SelectSegments(si.ctx, SegmentFilterFunc(func(seg *SegmentInfo) bool {
-		return isFlushed(seg) && seg.GetLevel() != datapb.SegmentLevel_L0 && !seg.GetIsSorted() && seg.GetIsInvisible()
+		return isFlushed(seg) && seg.GetLevel() != datapb.SegmentLevel_L0 && !seg.GetIsSorted() &&
+			!seg.GetIsImporting() && seg.GetIsInvisible()
 	}))
 
 	for _, seg := range invisibleSegments {
@@ -162,7 +163,8 @@ func (si *statsInspector) triggerSortStatsTask() {
 	}
 
 	visibleSegments := si.mt.SelectSegments(si.ctx, SegmentFilterFunc(func(seg *SegmentInfo) bool {
-		return isFlushed(seg) && seg.GetLevel() != datapb.SegmentLevel_L0 && !seg.GetIsSorted() && !seg.GetIsInvisible()
+		return isFlushed(seg) && seg.GetLevel() != datapb.SegmentLevel_L0 && !seg.GetIsSorted() &&
+			!seg.GetIsImporting() && !seg.GetIsInvisible()
 	}))
 
 	for _, segment := range visibleSegments {
