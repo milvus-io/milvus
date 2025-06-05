9b3fd6bee73d8d6a02dd250a8214f8b91f488d1e
sunby <sunbingyi1992@gmail.com>
2025-06-05 11:52:02 +0800

    format
    Signed-off-by: sunby <sunbingyi1992@gmail.com>

--- internal/core/src/index/IndexFactory.h
+++ internal/core/src/index/IndexFactory.h
@@ -108,13 +108,13 @@ class IndexFactory {
             storage::FileManagerContext());
 
     IndexBasePtr
-    CreateJsonIndex(IndexType index_type,
-                    JsonCastType cast_dtype,
-                    const std::string& nested_path,
-                    const storage::FileManagerContext& file_manager_context =
-                        storage::FileManagerContext(),
-                    const std::string& json_cast_function =
-                        UNKNOW_CAST_FUNCTION_NAME);
+    CreateJsonIndex(
+        IndexType index_type,
+        JsonCastType cast_dtype,
+        const std::string& nested_path,
+        const storage::FileManagerContext& file_manager_context =
+            storage::FileManagerContext(),
+        const std::string& json_cast_function = UNKNOW_CAST_FUNCTION_NAME);
 
     IndexBasePtr
     CreateScalarIndex(const CreateIndexInfo& create_index_info,
