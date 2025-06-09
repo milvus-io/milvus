937edab32ca6c7810a92de7a8c4f5e9978b1d59e
Cai Zhang <cai.zhang@zilliz.com>
2025-06-09 16:31:04 +0800

    Unsorted small segments should not be considered as indexed
    Signed-off-by: Cai Zhang <cai.zhang@zilliz.com>

--- internal/datacoord/handler.go
+++ internal/datacoord/handler.go
@@ -200,7 +200,7 @@ func (h *ServerHandler) GetQueryVChanPositions(channel RWChannel, partitionIDs .
 	// ================================================
 
 	segmentIndexed := func(segID UniqueID) bool {
-		return indexed.Contain(segID) || validSegmentInfos[segID].GetNumOfRows() < Params.DataCoordCfg.MinSegmentNumRowsToEnableIndex.GetAsInt64()
+		return indexed.Contain(segID) || (validSegmentInfos[segID].GetIsSorted() && validSegmentInfos[segID].GetNumOfRows() < Params.DataCoordCfg.MinSegmentNumRowsToEnableIndex.GetAsInt64())
 	}
 
 	flushedIDs, droppedIDs = retrieveSegment(validSegmentInfos, flushedIDs, droppedIDs, segmentIndexed)
