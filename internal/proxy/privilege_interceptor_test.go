ce87b6526760accc74377f0729e922cd9a6a9f16
bigsheeper <yihao.dai@zilliz.com>
2025-06-10 17:57:59 +0800

    fix ut
    Signed-off-by: bigsheeper <yihao.dai@zilliz.com>

--- internal/proxy/privilege_interceptor_test.go
+++ internal/proxy/privilege_interceptor_test.go
@@ -24,6 +24,7 @@ func TestUnaryServerInterceptor(t *testing.T) {
 
 func TestPrivilegeInterceptor(t *testing.T) {
 	ctx := context.Background()
+	defer paramtable.Get().Reset(Params.CommonCfg.AuthorizationEnabled.Key)
 
 	t.Run("Authorization Disabled", func(t *testing.T) {
 		paramtable.Get().Save(Params.CommonCfg.AuthorizationEnabled.Key, "false")
@@ -211,6 +212,7 @@ func TestResourceGroupPrivilege(t *testing.T) {
 
 	t.Run("Resource Group Privilege", func(t *testing.T) {
 		paramtable.Get().Save(Params.CommonCfg.AuthorizationEnabled.Key, "true")
+		defer paramtable.Get().Reset(Params.CommonCfg.AuthorizationEnabled.Key)
 
 		_, err := PrivilegeInterceptor(ctx, &milvuspb.ListResourceGroupsRequest{})
 		assert.Error(t, err)
@@ -265,6 +267,7 @@ func TestResourceGroupPrivilege(t *testing.T) {
 
 func TestPrivilegeGroup(t *testing.T) {
 	ctx := context.Background()
+	defer paramtable.Get().Reset(Params.CommonCfg.AuthorizationEnabled.Key)
 
 	t.Run("grant ReadOnly to single collection", func(t *testing.T) {
 		paramtable.Get().Save(Params.CommonCfg.AuthorizationEnabled.Key, "true")
@@ -593,6 +596,7 @@ func TestPrivilegeGroup(t *testing.T) {
 func TestBuiltinPrivilegeGroup(t *testing.T) {
 	t.Run("ClusterAdmin", func(t *testing.T) {
 		paramtable.Get().Save(Params.CommonCfg.AuthorizationEnabled.Key, "true")
+		defer paramtable.Get().Reset(Params.CommonCfg.AuthorizationEnabled.Key)
 		initPrivilegeGroups()
 
 		var err error
