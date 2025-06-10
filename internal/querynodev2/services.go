milvus-io/milvus

commit 69b8b8936977a36207753a1d7515bbfd4e493000

enhance: Remove QueryCoord's scheduling of L0 segments (#39552)
issue: #39551
This PR remove querycoord's scheduling of l0 segments:
  - only load l0 segment when watch channel
- only release l0 segment when release channel or sync data distribution

---------

Signed-off-by: Wei Liu <wei.liu@zilliz.com>

internal/querynodev2/services.go
@@ -272,16 +272,9 @@ func (node *QueryNode) WatchDmChannels(ctx context.Context, req *querypb.WatchDm
 	defer func() {
 		if err != nil {
 			node.delegators.GetAndRemove(channel.GetChannelName())
+			delegator.Close()
 		}
 	}()
-
-	// create tSafe
-	// node.tSafeManager.Add(ctx, channel.ChannelName, channel.GetSeekPosition().GetTimestamp())
-	// defer func() {
-	// 	if err != nil {
-	// 		node.tSafeManager.Remove(ctx, channel.ChannelName)
-	// 	}
-	// }()
 
 	pipeline, err := node.pipelineManager.Add(req.GetCollectionID(), channel.GetChannelName())
 	if err != nil {
@@ -306,9 +299,6 @@ func (node *QueryNode) WatchDmChannels(ctx context.Context, req *querypb.WatchDm
 			// remove legacy growing
 			node.manager.Segment.RemoveBy(ctx, segments.WithChannel(channel.GetChannelName()),
 				segments.WithType(segments.SegmentTypeGrowing))
-			// remove legacy l0 segments
-			node.manager.Segment.RemoveBy(ctx, segments.WithChannel(channel.GetChannelName()),
-				segments.WithLevel(datapb.SegmentLevel_L0))
 		}
 	}()
 
@@ -371,10 +361,7 @@ func (node *QueryNode) UnsubDmChannel(ctx context.Context, req *querypb.UnsubDmC
 
 		node.pipelineManager.Remove(req.GetChannelName())
 		node.manager.Segment.RemoveBy(ctx, segments.WithChannel(req.GetChannelName()), segments.WithType(segments.SegmentTypeGrowing))
-		_, sealed := node.manager.Segment.RemoveBy(ctx, segments.WithChannel(req.GetChannelName()), segments.WithLevel(datapb.SegmentLevel_L0))
-		// node.tSafeManager.Remove(ctx, req.GetChannelName())
-
-		node.manager.Collection.Unref(req.GetCollectionID(), uint32(1+sealed))
+		node.manager.Collection.Unref(req.GetCollectionID(), 1)
 	}
 	log.Info("unsubscribed channel")
 
@@ -1322,8 +1309,12 @@ func (node *QueryNode) SyncDistribution(ctx context.Context, req *querypb.SyncDi
 				return id, action.GetCheckpoint().Timestamp
 			})
 			shardDelegator.AddExcludedSegments(flushedInfo)
+			deleteCP := &msgpb.MsgPosition{}
+			if deleteCP.GetTimestamp() == 0 {
+				deleteCP = action.GetCheckpoint()
+			}
 			shardDelegator.SyncTargetVersion(action.GetTargetVersion(), req.GetLoadMeta().GetPartitionIDs(), action.GetGrowingInTarget(),
-				action.GetSealedInTarget(), action.GetDroppedInTarget(), action.GetCheckpoint())
+				action.GetSealedInTarget(), action.GetDroppedInTarget(), action.GetCheckpoint(), deleteCP)
 		case querypb.SyncType_UpdatePartitionStats:
 			log.Info("sync update partition stats versions")
 			shardDelegator.SyncPartitionStats(ctx, action.PartitionStatsVersions)

internal/datacoord/handler.go
@@ -133,10 +133,11 @@ func (h *ServerHandler) GetQueryVChanPositions(channel RWChannel, partitionIDs .
 	}
 
 	var (
-		flushedIDs   = make(typeutil.UniqueSet)
-		droppedIDs   = make(typeutil.UniqueSet)
-		growingIDs   = make(typeutil.UniqueSet)
-		levelZeroIDs = make(typeutil.UniqueSet)
+		flushedIDs       = make(typeutil.UniqueSet)
+		droppedIDs       = make(typeutil.UniqueSet)
+		growingIDs       = make(typeutil.UniqueSet)
+		levelZeroIDs     = make(typeutil.UniqueSet)
+		deleteCheckPoint *msgpb.MsgPosition
 	)
 
 	// cannot use GetSegmentsByChannel since dropped segments are needed here
@@ -171,6 +172,10 @@ func (h *ServerHandler) GetQueryVChanPositions(channel RWChannel, partitionIDs .
 			growingIDs.Insert(s.GetID())
 		case s.GetLevel() == datapb.SegmentLevel_L0:
 			levelZeroIDs.Insert(s.GetID())
+			// use smallest start position of l0 segments as deleteCheckPoint, so query coord will only maintain stream delete record  after this ts
+			if deleteCheckPoint == nil || s.GetStartPosition().GetTimestamp() < deleteCheckPoint.GetTimestamp() {
+				deleteCheckPoint = s.GetStartPosition()
+			}
 		default:
 			flushedIDs.Insert(s.GetID())
 		}

internal/querycoordv2/checkers/index_checker.go
@@ -29,7 +29,6 @@ import (
 	"github.com/milvus-io/milvus/internal/querycoordv2/task"
 	"github.com/milvus-io/milvus/internal/querycoordv2/utils"
 	"github.com/milvus-io/milvus/pkg/v2/log"
-	"github.com/milvus-io/milvus/pkg/v2/proto/datapb"
 	"github.com/milvus-io/milvus/pkg/v2/proto/indexpb"
 	"github.com/milvus-io/milvus/pkg/v2/proto/querypb"
 	"github.com/milvus-io/milvus/pkg/v2/util/typeutil"
@@ -117,12 +116,6 @@ func (c *IndexChecker) checkReplica(ctx context.Context, collection *meta.Collec
 	for _, segment := range segments {
 		// skip update index in read only node
 		if roNodeSet.Contain(segment.Node) {
-			continue
-		}
-
-		// skip update index for l0 segment
-		segmentInTarget := c.targetMgr.GetSealedSegment(ctx, collection.GetCollectionID(), segment.GetID(), meta.CurrentTargetFirst)
-		if segmentInTarget == nil || segmentInTarget.GetLevel() == datapb.SegmentLevel_L0 {
 			continue
 		}
 

internal/querycoordv2/checkers/leader_checker.go
@@ -29,7 +29,6 @@ import (
 	"github.com/milvus-io/milvus/internal/util/streamingutil"
 	"github.com/milvus-io/milvus/pkg/v2/common"
 	"github.com/milvus-io/milvus/pkg/v2/log"
-	"github.com/milvus-io/milvus/pkg/v2/proto/datapb"
 )
 
 var _ Checker = (*LeaderChecker)(nil)
@@ -169,10 +168,7 @@ func (c *LeaderChecker) findNeedLoadedSegments(ctx context.Context, replica *met
 	latestNodeDist := utils.FindMaxVersionSegments(dist)
 	for _, s := range latestNodeDist {
 		segment := c.target.GetSealedSegment(ctx, leaderView.CollectionID, s.GetID(), meta.CurrentTargetFirst)
-		existInTarget := segment != nil
-		isL0Segment := existInTarget && segment.GetLevel() == datapb.SegmentLevel_L0
-		// shouldn't set l0 segment location to delegator. l0 segment should be reload in delegator
-		if !existInTarget || isL0Segment {
+		if segment == nil {
 			continue
 		}
 
@@ -223,8 +219,7 @@ func (c *LeaderChecker) findNeedRemovedSegments(ctx context.Context, replica *me
 		_, ok := distMap[sid]
 		segment := c.target.GetSealedSegment(ctx, leaderView.CollectionID, sid, meta.CurrentTargetFirst)
 		existInTarget := segment != nil
-		isL0Segment := existInTarget && segment.GetLevel() == datapb.SegmentLevel_L0
-		if ok || existInTarget || isL0Segment {
+		if ok || existInTarget {
 			continue
 		}
 		log.Debug("leader checker append a segment to remove",

internal/querycoordv2/checkers/leader_checker_test.go
@@ -162,29 +162,6 @@ func (suite *LeaderCheckerTestSuite) TestSyncLoadedSegments() {
 	suite.Equal(tasks[0].Actions()[0].(*task.LeaderAction).GetLeaderID(), node2)
 	suite.Equal(tasks[0].Actions()[0].(*task.LeaderAction).SegmentID(), int64(1))
 	suite.Equal(tasks[0].Priority(), task.TaskPriorityLow)
-
-	// test skip sync l0 segment
-	segments = []*datapb.SegmentInfo{
-		{
-			ID:            1,
-			PartitionID:   1,
-			InsertChannel: "test-insert-channel",
-			Level:         datapb.SegmentLevel_L0,
-		},
-	}
-	suite.broker.ExpectedCalls = nil
-	suite.broker.EXPECT().GetRecoveryInfoV2(mock.Anything, int64(1)).Return(
-		channels, segments, nil)
-	observer.target.UpdateCollectionNextTarget(ctx, int64(1))
-	observer.target.UpdateCollectionCurrentTarget(ctx, 1)
-	// mock l0 segment exist on non delegator node, doesn't set to leader view
-	observer.dist.SegmentDistManager.Update(1, utils.CreateTestSegment(1, 1, 1, 1, loadVersion, "test-insert-channel"))
-	observer.dist.ChannelDistManager.Update(2, utils.CreateTestChannel(1, 2, 1, "test-insert-channel"))
-	view = utils.CreateTestLeaderView(2, 1, "test-insert-channel", map[int64]int64{}, map[int64]*meta.Segment{})
-	view.TargetVersion = observer.target.GetCollectionTargetVersion(ctx, 1, meta.CurrentTarget)
-	observer.dist.LeaderViewManager.Update(2, view)
-	tasks = suite.checker.Check(context.TODO())
-	suite.Len(tasks, 0)
 }
 
 func (suite *LeaderCheckerTestSuite) TestActivation() {
@@ -423,30 +400,6 @@ func (suite *LeaderCheckerTestSuite) TestSyncRemovedSegments() {
 	suite.Equal(tasks[0].Actions()[0].(*task.LeaderAction).SegmentID(), int64(3))
 	suite.Equal(tasks[0].Actions()[0].(*task.LeaderAction).Version(), int64(0))
 	suite.Equal(tasks[0].Priority(), task.TaskPriorityLow)
-
-	// skip sync l0 segments
-	segments := []*datapb.SegmentInfo{
-		{
-			ID:            3,
-			PartitionID:   1,
-			InsertChannel: "test-insert-channel",
-			Level:         datapb.SegmentLevel_L0,
-		},
-	}
-	suite.broker.ExpectedCalls = nil
-	suite.broker.EXPECT().GetRecoveryInfoV2(mock.Anything, int64(1)).Return(
-		channels, segments, nil)
-
-	observer.target.UpdateCollectionNextTarget(ctx, int64(1))
-	observer.target.UpdateCollectionCurrentTarget(ctx, 1)
-
-	observer.dist.ChannelDistManager.Update(2, utils.CreateTestChannel(1, 2, 1, "test-insert-channel"))
-	view = utils.CreateTestLeaderView(2, 1, "test-insert-channel", map[int64]int64{3: 1}, map[int64]*meta.Segment{})
-	view.TargetVersion = observer.target.GetCollectionTargetVersion(ctx, 1, meta.CurrentTarget)
-	observer.dist.LeaderViewManager.Update(2, view)
-
-	tasks = suite.checker.Check(context.TODO())
-	suite.Len(tasks, 0)
 }
 
 func (suite *LeaderCheckerTestSuite) TestIgnoreSyncRemovedSegments() {

internal/querycoordv2/checkers/segment_checker.go
@@ -21,7 +21,6 @@ import (
 	"sort"
 	"time"
 
-	"github.com/blang/semver/v4"
 	"github.com/samber/lo"
 	"go.opentelemetry.io/otel/trace"
 	"go.uber.org/zap"
@@ -245,37 +244,8 @@ func (c *SegmentChecker) getSealedSegmentDiff(
 		distMap[s.GetID()] = s.Node
 	}
 
-	versionRangeFilter := semver.MustParseRange(">2.3.x")
-	checkLeaderVersion := func(leader *meta.LeaderView, segmentID int64) bool {
-		// if current shard leader's node version < 2.4, skip load L0 segment
-		info := c.nodeMgr.Get(leader.ID)
-		if info != nil && !versionRangeFilter(info.Version()) {
-			log.Warn("l0 segment is not supported in current node version, skip it",
-				zap.Int64("collection", replica.GetCollectionID()),
-				zap.Int64("segmentID", segmentID),
-				zap.String("channel", leader.Channel),
-				zap.Int64("leaderID", leader.ID),
-				zap.String("nodeVersion", info.Version().String()))
-			return false
-		}
-		return true
-	}
-
 	isSegmentLack := func(segment *datapb.SegmentInfo) bool {
-		node, existInDist := distMap[segment.ID]
-
-		if segment.GetLevel() == datapb.SegmentLevel_L0 {
-			// the L0 segments have to been in the same node as the channel watched
-			leader := c.dist.LeaderViewManager.GetLatestShardLeaderByFilter(meta.WithReplica2LeaderView(replica), meta.WithChannelName2LeaderView(segment.GetInsertChannel()))
-
-			// if the leader node's version doesn't match load l0 segment's requirement, skip it
-			if leader != nil && checkLeaderVersion(leader, segment.ID) {
-				l0WithWrongLocation := node != leader.ID
-				return !existInDist || l0WithWrongLocation
-			}
-			return false
-		}
-
+		_, existInDist := distMap[segment.ID]
 		return !existInDist
 	}
 
@@ -285,18 +255,6 @@ func (c *SegmentChecker) getSealedSegmentDiff(
 
 	// Segment which exist on next target, but not on dist
 	for _, segment := range nextTargetMap {
-		if isSegmentLack(segment) {
-			toLoad = append(toLoad, segment)
-		}
-	}
-
-	// l0 Segment which exist on current target, but not on dist
-	for _, segment := range currentTargetMap {
-		// to avoid generate duplicate segment task
-		if nextTargetMap[segment.ID] != nil {
-			continue
-		}
-
 		if isSegmentLack(segment) {
 			toLoad = append(toLoad, segment)
 		}
@@ -313,16 +271,6 @@ func (c *SegmentChecker) getSealedSegmentDiff(
 		}
 	}
 
-	level0Segments := lo.Filter(toLoad, func(segment *datapb.SegmentInfo, _ int) bool {
-		return segment.GetLevel() == datapb.SegmentLevel_L0
-	})
-	// L0 segment found,
-	// QueryCoord loads the L0 segments first,
-	// to make sure all L0 delta logs will be delivered to the other segments.
-	if len(level0Segments) > 0 {
-		toLoad = level0Segments
-	}
-
 	return
 }
 
@@ -336,14 +284,6 @@ func (c *SegmentChecker) findRepeatedSealedSegments(ctx context.Context, replica
 	dist := c.dist.SegmentDistManager.GetByFilter(meta.WithCollectionID(replica.GetCollectionID()), meta.WithReplica(replica))
 	versions := make(map[int64]*meta.Segment)
 	for _, s := range dist {
-		// l0 segment should be release with channel together
-		segment := c.targetMgr.GetSealedSegment(ctx, s.GetCollectionID(), s.GetID(), meta.CurrentTargetFirst)
-		existInTarget := segment != nil
-		isL0Segment := existInTarget && segment.GetLevel() == datapb.SegmentLevel_L0
-		if isL0Segment {
-			continue
-		}
-
 		maxVer, ok := versions[s.GetID()]
 		if !ok {
 			versions[s.GetID()] = s
@@ -414,7 +354,6 @@ func (c *SegmentChecker) createSegmentLoadTasks(ctx context.Context, segments []
 		return nil
 	}
 
-	isLevel0 := segments[0].GetLevel() == datapb.SegmentLevel_L0
 	shardSegments := lo.GroupBy(segments, func(s *datapb.SegmentInfo) string {
 		return s.GetInsertChannel()
 	})
@@ -430,11 +369,6 @@ func (c *SegmentChecker) createSegmentLoadTasks(ctx context.Context, segments []
 		rwNodes := replica.GetChannelRWNodes(shard)
 		if len(rwNodes) == 0 {
 			rwNodes = replica.GetRWNodes()
-		}
-
-		// L0 segment can only be assign to shard leader's node
-		if isLevel0 {
-			rwNodes = []int64{leader.ID}
 		}
 
 		segmentInfos := lo.Map(segments, func(s *datapb.SegmentInfo, _ int) *meta.Segment {

internal/querycoordv2/checkers/segment_checker_test.go
@@ -33,7 +33,6 @@ import (
 	"github.com/milvus-io/milvus/internal/querycoordv2/session"
 	"github.com/milvus-io/milvus/internal/querycoordv2/task"
 	"github.com/milvus-io/milvus/internal/querycoordv2/utils"
-	"github.com/milvus-io/milvus/pkg/v2/common"
 	"github.com/milvus-io/milvus/pkg/v2/kv"
 	"github.com/milvus-io/milvus/pkg/v2/proto/datapb"
 	"github.com/milvus-io/milvus/pkg/v2/util/etcd"
@@ -168,170 +167,6 @@ func (suite *SegmentCheckerTestSuite) TestLoadSegments() {
 	suite.True(checker.IsActive())
 	tasks = checker.Check(context.TODO())
 	suite.Len(tasks, 1)
-}
-
-func (suite *SegmentCheckerTestSuite) TestLoadL0Segments() {
-	ctx := context.Background()
-	checker := suite.checker
-	// set meta
-	checker.meta.CollectionManager.PutCollection(ctx, utils.CreateTestCollection(1, 1))
-	checker.meta.CollectionManager.PutPartition(ctx, utils.CreateTestPartition(1, 1))
-	checker.meta.ReplicaManager.Put(ctx, utils.CreateTestReplica(1, 1, []int64{1, 2}))
-	suite.nodeMgr.Add(session.NewNodeInfo(session.ImmutableNodeInfo{
-		NodeID:   1,
-		Address:  "localhost",
-		Hostname: "localhost",
-		Version:  common.Version,
-	}))
-	suite.nodeMgr.Add(session.NewNodeInfo(session.ImmutableNodeInfo{
-		NodeID:   2,
-		Address:  "localhost",
-		Hostname: "localhost",
-		Version:  common.Version,
-	}))
-	checker.meta.ResourceManager.HandleNodeUp(ctx, 1)
-	checker.meta.ResourceManager.HandleNodeUp(ctx, 2)
-
-	// set target
-	segments := []*datapb.SegmentInfo{
-		{
-			ID:            1,
-			PartitionID:   1,
-			InsertChannel: "test-insert-channel",
-			Level:         datapb.SegmentLevel_L0,
-		},
-	}
-
-	channels := []*datapb.VchannelInfo{
-		{
-			CollectionID: 1,
-			ChannelName:  "test-insert-channel",
-		},
-	}
-
-	suite.broker.EXPECT().GetRecoveryInfoV2(mock.Anything, int64(1)).Return(
-		channels, segments, nil)
-	checker.targetMgr.UpdateCollectionNextTarget(ctx, int64(1))
-
-	// set dist
-	checker.dist.ChannelDistManager.Update(2, utils.CreateTestChannel(1, 2, 1, "test-insert-channel"))
-	checker.dist.LeaderViewManager.Update(2, utils.CreateTestLeaderView(2, 1, "test-insert-channel", map[int64]int64{}, map[int64]*meta.Segment{}))
-
-	// test load l0 segments in next target
-	tasks := checker.Check(context.TODO())
-	suite.Len(tasks, 1)
-	suite.Len(tasks[0].Actions(), 1)
-	action, ok := tasks[0].Actions()[0].(*task.SegmentAction)
-	suite.True(ok)
-	suite.EqualValues(1, tasks[0].ReplicaID())
-	suite.Equal(task.ActionTypeGrow, action.Type())
-	suite.EqualValues(1, action.GetSegmentID())
-	suite.EqualValues(2, action.Node())
-	suite.Equal(tasks[0].Priority(), task.TaskPriorityNormal)
-
-	checker.targetMgr.UpdateCollectionCurrentTarget(ctx, int64(1))
-	// test load l0 segments in current target
-	tasks = checker.Check(context.TODO())
-	suite.Len(tasks, 1)
-	suite.Len(tasks[0].Actions(), 1)
-	action, ok = tasks[0].Actions()[0].(*task.SegmentAction)
-	suite.True(ok)
-	suite.EqualValues(1, tasks[0].ReplicaID())
-	suite.Equal(task.ActionTypeGrow, action.Type())
-	suite.EqualValues(1, action.GetSegmentID())
-	suite.EqualValues(2, action.Node())
-	suite.Equal(tasks[0].Priority(), task.TaskPriorityNormal)
-
-	// seg l0 segment exist on a non delegator node
-	checker.targetMgr.UpdateCollectionNextTarget(ctx, int64(1))
-	checker.dist.SegmentDistManager.Update(1, utils.CreateTestSegment(1, 1, 1, 1, 1, "test-insert-channel"))
-	// test load l0 segments to delegator
-	tasks = checker.Check(context.TODO())
-	suite.Len(tasks, 1)
-	suite.Len(tasks[0].Actions(), 1)
-	action, ok = tasks[0].Actions()[0].(*task.SegmentAction)
-	suite.True(ok)
-	suite.EqualValues(1, tasks[0].ReplicaID())
-	suite.Equal(task.ActionTypeGrow, action.Type())
-	suite.EqualValues(1, action.GetSegmentID())
-	suite.EqualValues(2, action.Node())
-	suite.Equal(tasks[0].Priority(), task.TaskPriorityNormal)
-}
-
-func (suite *SegmentCheckerTestSuite) TestReleaseL0Segments() {
-	ctx := context.Background()
-	checker := suite.checker
-	// set meta
-	checker.meta.CollectionManager.PutCollection(ctx, utils.CreateTestCollection(1, 1))
-	checker.meta.CollectionManager.PutPartition(ctx, utils.CreateTestPartition(1, 1))
-	checker.meta.ReplicaManager.Put(ctx, utils.CreateTestReplica(1, 1, []int64{1, 2}))
-	suite.nodeMgr.Add(session.NewNodeInfo(session.ImmutableNodeInfo{
-		NodeID:   1,
-		Address:  "localhost",
-		Hostname: "localhost",
-	}))
-	suite.nodeMgr.Add(session.NewNodeInfo(session.ImmutableNodeInfo{
-		NodeID:   2,
-		Address:  "localhost",
-		Hostname: "localhost",
-	}))
-	checker.meta.ResourceManager.HandleNodeUp(ctx, 1)
-	checker.meta.ResourceManager.HandleNodeUp(ctx, 2)
-
-	// set target
-	segments := []*datapb.SegmentInfo{
-		{
-			ID:            1,
-			PartitionID:   1,
-			InsertChannel: "test-insert-channel",
-			Level:         datapb.SegmentLevel_L0,
-		},
-	}
-
-	channels := []*datapb.VchannelInfo{
-		{
-			CollectionID: 1,
-			ChannelName:  "test-insert-channel",
-		},
-	}
-
-	suite.broker.EXPECT().GetRecoveryInfoV2(mock.Anything, int64(1)).Return(
-		channels, segments, nil)
-	checker.targetMgr.UpdateCollectionNextTarget(ctx, int64(1))
-	checker.targetMgr.UpdateCollectionCurrentTarget(ctx, int64(1))
-
-	// set dist
-	checker.dist.ChannelDistManager.Update(2, utils.CreateTestChannel(1, 2, 1, "test-insert-channel"))
-	checker.dist.LeaderViewManager.Update(2, utils.CreateTestLeaderView(2, 1, "test-insert-channel", map[int64]int64{}, map[int64]*meta.Segment{}))
-
-	// seg l0 segment exist on a non delegator node
-	checker.dist.SegmentDistManager.Update(1, utils.CreateTestSegment(1, 1, 1, 1, 1, "test-insert-channel"))
-	checker.dist.SegmentDistManager.Update(2, utils.CreateTestSegment(1, 1, 1, 2, 100, "test-insert-channel"))
-
-	// release duplicate l0 segment
-	tasks := checker.Check(context.TODO())
-	suite.Len(tasks, 0)
-
-	checker.dist.SegmentDistManager.Update(1)
-
-	// test release l0 segment which doesn't exist in target
-	suite.broker.ExpectedCalls = nil
-	suite.broker.EXPECT().GetRecoveryInfoV2(mock.Anything, int64(1)).Return(
-		channels, nil, nil)
-	checker.targetMgr.UpdateCollectionNextTarget(ctx, int64(1))
-	checker.targetMgr.UpdateCollectionCurrentTarget(ctx, int64(1))
-	checker.targetMgr.UpdateCollectionNextTarget(ctx, int64(1))
-
-	tasks = checker.Check(context.TODO())
-	suite.Len(tasks, 1)
-	suite.Len(tasks[0].Actions(), 1)
-	action, ok := tasks[0].Actions()[0].(*task.SegmentAction)
-	suite.True(ok)
-	suite.EqualValues(1, tasks[0].ReplicaID())
-	suite.Equal(task.ActionTypeReduce, action.Type())
-	suite.EqualValues(1, action.GetSegmentID())
-	suite.EqualValues(2, action.Node())
-	suite.Equal(tasks[0].Priority(), task.TaskPriorityNormal)
 }
 
 func (suite *SegmentCheckerTestSuite) TestSkipLoadSegments() {

internal/querycoordv2/dist/dist_handler.go
@@ -151,6 +151,11 @@ func (dh *distHandler) handleDistResp(ctx context.Context, resp *querypb.GetData
 func (dh *distHandler) updateSegmentsDistribution(ctx context.Context, resp *querypb.GetDataDistributionResponse) {
 	updates := make([]*meta.Segment, 0, len(resp.GetSegments()))
 	for _, s := range resp.GetSegments() {
+		// To maintain compatibility with older versions of QueryNode,
+		// QueryCoord should neither process nor interact with L0 segments.
+		if s.GetLevel() == datapb.SegmentLevel_L0 {
+			continue
+		}
 		segmentInfo := dh.target.GetSealedSegment(ctx, s.GetCollection(), s.GetID(), meta.CurrentTargetFirst)
 		if segmentInfo == nil {
 			segmentInfo = &datapb.SegmentInfo{
@@ -211,8 +216,13 @@ func (dh *distHandler) updateLeaderView(ctx context.Context, resp *querypb.GetDa
 	collectionsToSync := typeutil.NewUniqueSet()
 	for _, lview := range resp.GetLeaderViews() {
 		segments := make(map[int64]*meta.Segment)
-
 		for ID, position := range lview.GrowingSegments {
+			// To maintain compatibility with older versions of QueryNode,
+			// QueryCoord should neither process nor interact with L0 segments.
+			segmentInfo := dh.target.GetSealedSegment(ctx, lview.GetCollection(), ID, meta.CurrentTargetFirst)
+			if segmentInfo != nil && segmentInfo.GetLevel() == datapb.SegmentLevel_L0 {
+				continue
+			}
 			segments[ID] = &meta.Segment{
 				SegmentInfo: &datapb.SegmentInfo{
 					ID:            ID,

commit 6a033d63e2cb52e0e65422f4ec8d3b12a0113b09

fix: Use delete checkpoint to prevent delete record loss in L0 refactoring
issue: #39333 #41570
Fix delete record missing issue introduced in PR #39552 L0 refactoring:
- Use delete checkpoint as consume start position when deleteCP < channelCP
- Add logging when delete checkpoint is used instead of seek position
- Prevent delete record loss when deleteCP is earlier than default channelCP

Signed-off-by: Wei Liu <wei.liu@zilliz.com>

internal/querynodev2/services.go
@@ -322,11 +322,32 @@ func (node *QueryNode) WatchDmChannels(ctx context.Context, req *querypb.WatchDm
 		log.Warn(msg, zap.Error(err))
 		return merr.Status(err), nil
 	}
-	position := &msgpb.MsgPosition{
-		ChannelName: channel.SeekPosition.ChannelName,
-		MsgID:       channel.SeekPosition.MsgID,
-		Timestamp:   channel.SeekPosition.Timestamp,
+
+	var position *msgpb.MsgPosition
+	if channel.GetSeekPosition().GetTimestamp() > channel.GetDeleteCheckpoint().GetTimestamp() {
+		msg := "channel seek position is greater than delete checkpoint, use delete checkpoint to seek"
+		log.Info(msg,
+			zap.Time("seekPosition", tsoutil.PhysicalTime(channel.GetSeekPosition().GetTimestamp())),
+			zap.Time("deleteCheckpoint", tsoutil.PhysicalTime(channel.GetDeleteCheckpoint().GetTimestamp())),
+		)
+		position = &msgpb.MsgPosition{
+			ChannelName: channel.DeleteCheckpoint.ChannelName,
+			MsgID:       channel.DeleteCheckpoint.MsgID,
+			Timestamp:   channel.DeleteCheckpoint.Timestamp,
+		}
+	} else {
+		msg := "channel seek position is smaller than delete checkpoint, use seek position to seek"
+		log.Info(msg,
+			zap.Time("seekPosition", tsoutil.PhysicalTime(channel.GetSeekPosition().GetTimestamp())),
+			zap.Time("deleteCheckpoint", tsoutil.PhysicalTime(channel.GetDeleteCheckpoint().GetTimestamp())),
+		)
+		position = &msgpb.MsgPosition{
+			ChannelName: channel.SeekPosition.ChannelName,
+			MsgID:       channel.SeekPosition.MsgID,
+			Timestamp:   channel.SeekPosition.Timestamp,
+		}
 	}
+
 	err = pipeline.ConsumeMsgStream(ctx, position)
 	if err != nil {
 		err = merr.WrapErrServiceUnavailable(err.Error(), "InitPipelineFailed")