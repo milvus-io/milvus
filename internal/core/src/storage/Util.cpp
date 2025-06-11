499e9a0a73e565f35fac0219f9eccf6d6a7a7ac1
congqixia <congqi.xia@zilliz.com>
2025-06-11 11:54:34 +0800

    fix: [AddField] Use corresponding datatype for int8/int16 def val (#42633)
    Related to #42629
    
    This PR handles converting default value to int8/int18 scalar with int32
    default value definition
    
    Signed-off-by: Congqi Xia <congqi.xia@zilliz.com>

--- internal/core/src/storage/Util.cpp
+++ internal/core/src/storage/Util.cpp
@@ -344,7 +344,11 @@ CreateArrowScalarFromDefaultValue(const FieldMeta& field_meta) {
             return std::make_shared<arrow::BooleanScalar>(
                 default_value.bool_data());
         case DataType::INT8:
+            return std::make_shared<arrow::Int8Scalar>(
+                default_value.int_data());
         case DataType::INT16:
+            return std::make_shared<arrow::Int16Scalar>(
+                default_value.int_data());
         case DataType::INT32:
             return std::make_shared<arrow::Int32Scalar>(
                 default_value.int_data());
