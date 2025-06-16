ef92478d48c1f85e97351a410a76603bb7ecc377
Congqi Xia <congqi.xia@zilliz.com>
2025-06-16 15:23:30 +0800

    Separate options between reader&writer
    Signed-off-by: Congqi Xia <congqi.xia@zilliz.com>

--- internal/storagev2/packed/constant.go
+++ internal/storagev2/packed/constant.go
@@ -18,7 +18,9 @@ const (
 	// ColumnGroupSizeThreshold is the threshold of column group size per row.
 	ColumnGroupSizeThreshold = 1024 // 1KB
 	// DefaultBufferSize is the default buffer size for writing data to storage.
-	DefaultWriteBufferSize = -1 // use -1 for unlimited
+	DefaultWriteBufferSize = 32 * 1024 * 1024 // 32MB
+	// DefaultBufferSize is the default buffer size for reading data from storage.
+	DefaultReadBufferSize = -1 // use -1 for unlimited
 	// DefaultMultiPartUploadSize is the default size of each part of a multipart upload.
 	DefaultMultiPartUploadSize = 10 * 1024 * 1024 // 10MB
 	// Arrow will convert these field IDs to a metadata key named PARQUET:field_id on the appropriate field.
