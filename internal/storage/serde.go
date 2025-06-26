947fe24a09d95871e23b3e89effc1a3db79c5af6
shaoting-huang <shaoting.huang@zilliz.com>
2025-06-26 14:39:35 +0800

    fix ut
    Signed-off-by: shaoting-huang <shaoting.huang@zilliz.com>

--- internal/storage/serde.go
+++ internal/storage/serde.go
@@ -892,6 +892,9 @@ func BuildRecord(b *array.RecordBuilder, data *InsertData, fields []*schemapb.Fi
 		if !ok {
 			panic("unknown type")
 		}
+		if data.Data[field.FieldID].RowNum() == 0 {
+			return merr.WrapErrServiceInternal(fmt.Sprintf("row num is 0 for field %s", field.Name))
+		}
 		for j := 0; j < data.Data[field.FieldID].RowNum(); j++ {
 			ok = typeEntry.serialize(fBuilder, data.Data[field.FieldID].GetRow(j))
 			if !ok {
