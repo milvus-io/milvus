3b5d98f32428baa3359e4654a65ce3f9b8f01e37
Congqi Xia <congqi.xia@zilliz.com>
2025-07-02 18:55:36 +0800

    fix: Check field mmap property before apply collection level one
    Related to #43089
    
    Signed-off-by: Congqi Xia <congqi.xia@zilliz.com>

--- internal/querycoordv2/task/utils.go
+++ internal/querycoordv2/task/utils.go
@@ -142,18 +142,8 @@ func packLoadSegmentRequest(
 	if task.Source() == utils.LeaderChecker {
 		loadScope = querypb.LoadScope_Delta
 	}
-	// field mmap enabled if collection-level mmap enabled or the field mmap enabled
-	collectionMmapEnabled, exist := common.IsMmapDataEnabled(collectionProperties...)
-	for _, field := range schema.GetFields() {
-		if exist {
-			field.TypeParams = append(field.TypeParams, &commonpb.KeyValuePair{
-				Key:   common.MmapEnabledKey,
-				Value: strconv.FormatBool(collectionMmapEnabled),
-			})
-		}
-	}
 
-	schema.Properties = mergeCollectonProps(schema.Properties, collectionProperties)
+	applyCollectionMmapSetting(schema, collectionProperties)
 
 	return &querypb.LoadSegmentsRequest{
 		Base: commonpbutil.NewMsgBase(
@@ -206,12 +196,14 @@ func packSubChannelRequest(
 	task *ChannelTask,
 	action Action,
 	schema *schemapb.CollectionSchema,
+	collectionProperties []*commonpb.KeyValuePair,
 	loadMeta *querypb.LoadMetaInfo,
 	channel *meta.DmChannel,
 	indexInfo []*indexpb.IndexInfo,
 	partitions []int64,
 	targetVersion int64,
 ) *querypb.WatchDmChannelsRequest {
+	applyCollectionMmapSetting(schema, collectionProperties)
 	return &querypb.WatchDmChannelsRequest{
 		Base: commonpbutil.NewMsgBase(
 			commonpbutil.WithMsgType(commonpb.MsgType_WatchDmChannels),
@@ -270,5 +262,23 @@ func packUnsubDmChannelRequest(task *ChannelTask, action Action) *querypb.UnsubD
 		NodeID:       action.Node(),
 		CollectionID: task.CollectionID(),
 		ChannelName:  task.Channel(),
+	}
+}
+
+func applyCollectionMmapSetting(schema *schemapb.CollectionSchema,
+	collectionProperties []*commonpb.KeyValuePair,
+) {
+	schema.Properties = mergeCollectonProps(schema.Properties, collectionProperties)
+	// field mmap enabled if collection-level mmap enabled or the field mmap enabled
+	collectionMmapEnabled, exist := common.IsMmapDataEnabled(collectionProperties...)
+	for _, field := range schema.GetFields() {
+		if exist &&
+			// field-level mmap setting has higher priority than collection-level mmap setting, skip if field-level mmap enabled
+			!common.FieldHasMmapKey(schema, field.GetFieldID()) {
+			field.TypeParams = append(field.TypeParams, &commonpb.KeyValuePair{
+				Key:   common.MmapEnabledKey,
+				Value: strconv.FormatBool(collectionMmapEnabled),
+			})
+		}
 	}
 }
