690cc1ac43d9628ffeb5efd7f5d04587ee5d43f3
sunby <sunbingyi1992@gmail.com>
2025-07-03 11:41:10 +0000

    fix: Fix the bug when offsets is nullptr in bulk api
    Signed-off-by: sunby <sunbingyi1992@gmail.com>

--- internal/core/src/mmap/ChunkedColumnGroup.h
+++ internal/core/src/mmap/ChunkedColumnGroup.h
@@ -370,8 +370,7 @@ class ProxyChunkColumn : public ChunkedColumnInterface {
                 auto chunk = group_chunk->GetChunk(field_id_);
                 auto valid = chunk->isValid(offsets_in_chunk[i]);
                 auto value = static_cast<StringChunk*>(chunk.get())
-                                 ->
-                                 operator[](offsets_in_chunk[i]);
+                                 ->operator[](offsets_in_chunk[i]);
                 fn(value, i, valid);
             }
         }
@@ -387,35 +386,19 @@ class ProxyChunkColumn : public ChunkedColumnInterface {
                 ErrorCode::Unsupported,
                 "RawJsonAt only supported for ProxyChunkColumn of Json type");
         }
-        if (offsets == nullptr) {
-            int64_t current_offset = 0;
-            for (cid_t cid = 0; cid < num_chunks(); ++cid) {
-                auto group_chunk = group_->GetGroupChunk(cid);
-                auto chunk = group_chunk.get()->GetChunk(field_id_);
-                auto chunk_rows = chunk->RowNums();
-                for (int64_t i = 0; i < chunk_rows; ++i) {
-                    auto valid = chunk->isValid(i);
-                    auto str_view =
-                        static_cast<StringChunk*>(chunk.get())->operator[](i);
-                    fn(Json(str_view.data(), str_view.size()),
-                       current_offset + i,
-                       valid);
-                }
-                current_offset += chunk_rows;
-            }
-        } else {
-            auto [cids, offsets_in_chunk] = ToChunkIdAndOffset(offsets, count);
-            auto ca = group_->GetGroupChunks(cids);
+        if (count == 0) {
+            return;
+        }
+        auto [cids, offsets_in_chunk] = ToChunkIdAndOffset(offsets, count);
+        auto ca = group_->GetGroupChunks(cids);
 
-            for (int64_t i = 0; i < count; i++) {
-                auto* group_chunk = ca->get_cell_of(cids[i]);
-                auto chunk = group_chunk->GetChunk(field_id_);
-                auto valid = chunk->isValid(offsets_in_chunk[i]);
-                auto str_view = static_cast<StringChunk*>(chunk.get())
-                                    ->
-                                    operator[](offsets_in_chunk[i]);
-                fn(Json(str_view.data(), str_view.size()), i, valid);
-            }
+        for (int64_t i = 0; i < count; i++) {
+            auto* group_chunk = ca->get_cell_of(cids[i]);
+            auto chunk = group_chunk->GetChunk(field_id_);
+            auto valid = chunk->isValid(offsets_in_chunk[i]);
+            auto str_view = static_cast<StringChunk*>(chunk.get())
+                                ->operator[](offsets_in_chunk[i]);
+            fn(Json(str_view.data(), str_view.size()), i, valid);
         }
     }
 
