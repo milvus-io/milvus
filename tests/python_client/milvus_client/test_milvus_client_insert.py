370ad205326037e17da2780d3d4b6c6f8f5bedc5
Congqi Xia <congqi.xia@zilliz.com>
2025-07-18 10:05:14 +0800

    Fix cases
    Signed-off-by: Congqi Xia <congqi.xia@zilliz.com>

--- tests/python_client/milvus_client/test_milvus_client_insert.py
+++ tests/python_client/milvus_client/test_milvus_client_insert.py
@@ -88,6 +88,7 @@ class TestMilvusClientInsertInvalid(TestMilvusClientV2Base):
 
     @pytest.mark.tags(CaseLabel.L1)
     @pytest.mark.parametrize("collection_name", ["12-s", "12 s", "(mn)", "中文", "%$#"])
+    @pytest.mark.skip(reason="https://github.com/milvus-io/milvus/issues/43031, pr#43299, remove name check for describe collection")
     def test_milvus_client_insert_invalid_collection_name(self, collection_name):
         """
         target: test high level api: client.create_collection
@@ -630,6 +631,7 @@ class TestMilvusClientUpsertInvalid(TestMilvusClientV2Base):
 
     @pytest.mark.tags(CaseLabel.L1)
     @pytest.mark.parametrize("collection_name", ["12-s", "12 s", "(mn)", "中文", "%$#"])
+    @pytest.mark.skip(reason="https://github.com/milvus-io/milvus/issues/43031, pr#43299, remove name check for describe collection")
     def test_milvus_client_upsert_invalid_collection_name(self, collection_name):
         """
         target: test high level api: client.create_collection
