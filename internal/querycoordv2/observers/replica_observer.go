ad0bf9cad8f48a2d959ab3ebfe8a8dc6573301ab
wei liu <wei.liu@zilliz.com>
2025-07-21 17:04:54 +0800

    enhance: Optimize channel node balancing for uneven QN distribution (#42786) (#43423)
    issue: #42860
    pr: #42786
    Fix channel node allocation when QueryNode count is not a multiple of
    channel count. The previous algorithm used simple division which caused
    uneven distribution with remainders.
    
    Key improvements:
    - Implement smart remainder distribution algorithm
    - Refactor large function into focused helper functions
    - Support two-phase rebalancing (release then allocate)
    - Handle edge cases like insufficient nodes gracefully
    
    ---------
    
    Signed-off-by: Wei Liu <wei.liu@zilliz.com>

--- internal/querycoordv2/observers/replica_observer.go
+++ internal/querycoordv2/observers/replica_observer.go
@@ -21,21 +21,24 @@ import (
 	"sync"
 	"time"
 
+	"github.com/samber/lo"
 	"go.uber.org/zap"
 
 	"github.com/milvus-io/milvus/internal/querycoordv2/meta"
 	"github.com/milvus-io/milvus/internal/querycoordv2/params"
 	"github.com/milvus-io/milvus/internal/querycoordv2/utils"
 	"github.com/milvus-io/milvus/pkg/v2/log"
+	"github.com/milvus-io/milvus/pkg/v2/util/paramtable"
 	"github.com/milvus-io/milvus/pkg/v2/util/syncutil"
 )
 
 // check replica, find read only nodes and remove it from replica if all segment/channel has been moved
 type ReplicaObserver struct {
-	cancel  context.CancelFunc
-	wg      sync.WaitGroup
-	meta    *meta.Meta
-	distMgr *meta.DistributionManager
+	cancel    context.CancelFunc
+	wg        sync.WaitGroup
+	meta      *meta.Meta
+	distMgr   *meta.DistributionManager
+	targetMgr meta.TargetManagerInterface
 
 	startOnce sync.Once
 	stopOnce  sync.Once
@@ -93,26 +96,41 @@ func (ob *ReplicaObserver) waitNodeChangedOrTimeout(ctx context.Context, listene
 
 func (ob *ReplicaObserver) checkNodesInReplica() {
 	ctx := context.Background()
-	log := log.Ctx(ctx).WithRateGroup("qcv2.replicaObserver", 1, 60)
+	log := log.Ctx(ctx).WithRateGroup("qcv2.checkNodesInReplica", 1, 60)
 	collections := ob.meta.GetAll(ctx)
 	for _, collectionID := range collections {
 		utils.RecoverReplicaOfCollection(ctx, ob.meta, collectionID)
 	}
 
+	balancePolicy := paramtable.Get().QueryCoordCfg.Balancer.GetValue()
+	enableChannelExclusiveMode := balancePolicy == meta.ChannelLevelScoreBalancerName
+
 	// check all ro nodes, remove it from replica if all segment/channel has been moved
 	for _, collectionID := range collections {
 		replicas := ob.meta.ReplicaManager.GetByCollection(ctx, collectionID)
 		for _, replica := range replicas {
+			if enableChannelExclusiveMode && !replica.IsChannelExclusiveModeEnabled() {
+				// register channel for enable exclusive mode
+				mutableReplica := replica.CopyForWrite()
+				channels := ob.targetMgr.GetDmChannelsByCollection(ctx, collectionID, meta.CurrentTargetFirst)
+				mutableReplica.TryEnableChannelExclusiveMode(lo.Keys(channels)...)
+				replica = mutableReplica.IntoReplica()
+				ob.meta.ReplicaManager.Put(ctx, replica)
+			}
+
 			roNodes := replica.GetRONodes()
 			rwNodes := replica.GetRWNodes()
 			if len(roNodes) == 0 {
 				continue
 			}
-			log.RatedInfo(10, "found ro nodes in replica",
+			logger := log.With(
 				zap.Int64("collectionID", replica.GetCollectionID()),
 				zap.Int64("replicaID", replica.GetID()),
-				zap.Int64s("RONodes", roNodes),
+				zap.Int64s("roNodes", roNodes),
+				zap.Int64s("rwNodes", rwNodes),
 			)
+
+			log.RatedInfo(10, "found ro nodes in replica")
 			removeNodes := make([]int64, 0, len(roNodes))
 			for _, node := range roNodes {
 				channels := ob.distMgr.ChannelDistManager.GetByCollectionAndFilter(replica.GetCollectionID(), meta.WithNodeID2Channel(node))
@@ -124,18 +142,15 @@ func (ob *ReplicaObserver) checkNodesInReplica() {
 			if len(removeNodes) == 0 {
 				continue
 			}
-			logger := log.With(
-				zap.Int64("collectionID", replica.GetCollectionID()),
-				zap.Int64("replicaID", replica.GetID()),
-				zap.Int64s("removedNodes", removeNodes),
-				zap.Int64s("roNodes", roNodes),
-				zap.Int64s("rwNodes", rwNodes),
-			)
 			if err := ob.meta.ReplicaManager.RemoveNode(ctx, replica.GetID(), removeNodes...); err != nil {
-				logger.Warn("fail to remove node from replica", zap.Error(err))
+				logger.Warn("fail to remove node from replica",
+					zap.Int64s("removedNodes", removeNodes),
+					zap.Error(err))
 				continue
 			}
-			logger.Info("all segment/channel has been removed from ro node, try to remove it from replica")
+			logger.Info("all segment/channel has been removed from ro node, remove it from replica",
+				zap.Int64s("removedNodes", removeNodes),
+			)
 		}
 	}
 }
