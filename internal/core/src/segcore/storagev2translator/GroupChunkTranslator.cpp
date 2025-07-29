7d4007c1254d5139c66bdefa81be2ef9f4f8a2e4
Congqi Xia <congqi.xia@zilliz.com>
2025-07-29 17:29:54 +0800

    Fix channel pop variable
    Signed-off-by: Congqi Xia <congqi.xia@zilliz.com>

--- internal/core/src/segcore/storagev2translator/GroupChunkTranslator.cpp
+++ internal/core/src/segcore/storagev2translator/GroupChunkTranslator.cpp
@@ -223,7 +223,7 @@ GroupChunkTranslator::get_cells(const std::vector<cachinglayer::cid_t>& cids) {
     std::shared_ptr<milvus::ArrowDataWrapper> r;
     std::unordered_set<cachinglayer::cid_t> filled_cids;
     filled_cids.reserve(cids.size());
-    while (column_group_info_.arrow_reader_channel->pop(r)) {
+    while (channel->pop(r)) {
         for (const auto& table_info : r->arrow_tables) {
             // Convert file_index and row_group_index to global cid
             auto cid = get_cid_from_file_and_row_group_index(
