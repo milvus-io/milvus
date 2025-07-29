173efe2b98347451891e15b9099da4b55ebc4f85
tinswzy <zhenyuan.wei@zilliz.com>
2025-07-29 14:51:35 +0800

    enhance: wp metrics and update deps to v0.1.0 (#43569)
    #43574   #43604 #43431  #43603 
    Fix wp metrics not registered bug;
    Update the version dependent on wp to v0.1.2-rc1;
    improve advanced reader with concurrent prefetch blks;
    add the segment rolling policy based on the number of blocks;
    improve concurrent compaction
    release lock failed bug
    
    Signed-off-by: tinswzy <zhenyuan.wei@zilliz.com>

--- pkg/util/paramtable/service_param_test.go
+++ pkg/util/paramtable/service_param_test.go
@@ -105,6 +105,7 @@ func TestServiceParam(t *testing.T) {
 		assert.Equal(t, wpCfg.AppendMaxRetries.GetAsInt(), 3)
 		assert.Equal(t, wpCfg.SegmentRollingMaxSize.GetAsSize(), int64(256*1024*1024))
 		assert.Equal(t, wpCfg.SegmentRollingMaxTime.GetAsDurationByParse().Seconds(), float64(600))
+		assert.Equal(t, wpCfg.SegmentRollingMaxBlocks.GetAsInt64(), int64(1000))
 		assert.Equal(t, wpCfg.AuditorMaxInterval.GetAsDurationByParse().Seconds(), float64(10))
 
 		assert.Equal(t, wpCfg.SyncMaxInterval.GetAsDurationByParse().Milliseconds(), int64(200))
@@ -116,6 +117,10 @@ func TestServiceParam(t *testing.T) {
 		assert.Equal(t, wpCfg.FlushMaxThreads.GetAsInt(), 32)
 		assert.Equal(t, wpCfg.RetryInterval.GetAsDurationByParse().Milliseconds(), int64(1000))
 		assert.Equal(t, wpCfg.CompactionSize.GetAsSize(), int64(2*1024*1024))
+		assert.Equal(t, wpCfg.CompactionMaxParallelUploads.GetAsInt(), 4)
+		assert.Equal(t, wpCfg.CompactionMaxParallelReads.GetAsInt(), 8)
+		assert.Equal(t, wpCfg.ReaderMaxBatchSize.GetAsSize(), int64(16*1024*1024))
+		assert.Equal(t, wpCfg.ReaderMaxFetchThreads.GetAsInt(), 32)
 
 		assert.Equal(t, wpCfg.StorageType.GetValue(), "minio")
 		assert.Equal(t, wpCfg.RootPath.GetValue(), "/var/lib/milvus/woodpecker")
