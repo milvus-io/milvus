5732ff3a8fd358729f1880040f47ad60c86e5721
SpadeA <tangchenjie1210@gmail.com>
2025-08-05 20:12:40 +0800

    fix test
    Signed-off-by: SpadeA <tangchenjie1210@gmail.com>

--- internal/util/indexparamcheck/trie_checker_test.go
+++ internal/util/indexparamcheck/trie_checker_test.go
@@ -11,7 +11,7 @@ import (
 func Test_TrieIndexChecker(t *testing.T) {
 	c := newTRIEChecker()
 
-	assert.NoError(t, c.CheckTrain(schemapb.DataType_VarChar, map[string]string{}))
+	assert.NoError(t, c.CheckTrain(schemapb.DataType_VarChar, schemapb.DataType_None, map[string]string{}))
 
 	assert.NoError(t, c.CheckValidDataType(IndexTRIE, &schemapb.FieldSchema{DataType: schemapb.DataType_VarChar}))
 	assert.NoError(t, c.CheckValidDataType(IndexTRIE, &schemapb.FieldSchema{DataType: schemapb.DataType_String}))
