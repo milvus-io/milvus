44692a1cce05adfb77aa58b5272c8b83b2563454
Zhikun Yao <zhikun.yao@zilliz.com>
2025-08-18 17:23:11 +0800

    test: just test v2 for master 1
    Signed-off-by: Zhikun Yao <zhikun.yao@zilliz.com>

--- internal/storagev2/packed/arrow/c/helpers.h
+++ internal/storagev2/packed/arrow/c/helpers.h
@@ -11,6 +11,7 @@ extern "C" {
 #endif
 
 /// Query whether the C schema is released
+/// just test
 static inline int
 ArrowSchemaIsReleased(const struct ArrowSchema* schema) {
     return schema->release == NULL;
