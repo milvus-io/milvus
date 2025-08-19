4518dcbfc326f147c7fad0c1405066fa94431b45
xtx <xtianx@smail.nju.edu.cn>
2025-08-20 00:57:23 +0800

    feat: support user define timezone in search/query
    Signed-off-by: xtx <xtianx@smail.nju.edu.cn>

--- internal/proxy/task_query.go
+++ internal/proxy/task_query.go
@@ -83,6 +83,7 @@ type queryParams struct {
 	reduceType   reduce.IReduceType
 	isIterator   bool
 	collectionID int64
+	timezone 	 string
 }
 
 // translateToOutputFieldIDs translates output fields name to output fields id.
@@ -165,6 +166,7 @@ func parseQueryParams(queryParamsPair []*commonpb.KeyValuePair) (*queryParams, e
 		isIterator        bool
 		err               error
 		collectionID      int64
+		timezone 					string
 	)
 	reduceStopForBestStr, err := funcutil.GetAttrByKeyFromRepeatedKV(ReduceStopForBestKey, queryParamsPair)
 	// if reduce_stop_for_best is provided
@@ -223,6 +225,11 @@ func parseQueryParams(queryParamsPair []*commonpb.KeyValuePair) (*queryParams, e
 		}
 	}
 
+	timezone_str, err := funcutil.GetAttrByKeyFromRepeatedKV(TimezoneKey, queryParamsPair)
+	if err == nil {
+		timezone = timezone_str
+	}
+
 	// validate max result window.
 	if err = validateMaxQueryResultWindow(offset, limit); err != nil {
 		return nil, fmt.Errorf("invalid max query result window, %w", err)
@@ -234,6 +241,7 @@ func parseQueryParams(queryParamsPair []*commonpb.KeyValuePair) (*queryParams, e
 		reduceType:   reduceType,
 		isIterator:   isIterator,
 		collectionID: collectionID,
+		timezone: timezone,
 	}, nil
 }
 
@@ -688,7 +696,14 @@ func (t *queryTask) PostExecute(ctx context.Context) error {
 	}
 	colTimezone := getColTimezone(colInfo)
 	dbTimezone := getDbTimezone(dbInfo)
-	timestamptzUtc2IsoStr(t.result.GetFieldsData(), "", colTimezone, dbTimezone) // TODO: support user define
+	if !t.reQuery {
+		log.Debug("Translate timstamp to ISO string", zap.String("timezone", t.queryParams.timezone))
+		err = timestamptzUtc2IsoStr(t.result.GetFieldsData(), t.queryParams.timezone, colTimezone, dbTimezone)
+		if err != nil {
+			log.Warn("fail to convert timestamp", zap.Error(err))
+			return err
+		}
+	}
 	log.Debug("Query PostExecute done")
 	return nil
 }
