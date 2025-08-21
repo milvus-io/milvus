e0824307ffa346f1e4e89b108ce002da6cdae269
bigsheeper <yihao.dai@zilliz.com>
2025-08-21 10:53:26 +0800

    update
    Signed-off-by: bigsheeper <yihao.dai@zilliz.com>

---
+++ internal/util/replicateutil/util.go
@@ -0,0 +1,36 @@
+package replicateutil
+
+import (
+	"fmt"
+
+	"github.com/samber/lo"
+	"go.uber.org/zap"
+
+	"github.com/milvus-io/milvus-proto/go-api/v2/milvuspb"
+)
+
+func GetMilvusCluster(clusterID string, config *milvuspb.ReplicateConfiguration) *milvuspb.MilvusCluster {
+	for _, cluster := range config.GetClusters() {
+		if cluster.GetClusterID() == clusterID {
+			return cluster
+		}
+	}
+	panic(fmt.Sprintf("cluster %s not found in replicate configuration", clusterID))
+}
+
+func ConfigLogFields(config *milvuspb.ReplicateConfiguration) []zap.Field {
+	fields := make([]zap.Field, 0)
+	fields = append(fields, zap.Int("clusterCount", len(config.GetClusters())))
+	fields = append(fields, zap.Strings("clusters", lo.Map(config.GetClusters(), func(cluster *milvuspb.MilvusCluster, _ int) string {
+		return cluster.GetClusterID()
+	})))
+	fields = append(fields, zap.Int("topologyCount", len(config.GetCrossClusterTopology())))
+	fields = append(fields, zap.Strings("topologies", lo.Map(config.GetCrossClusterTopology(), func(topology *milvuspb.CrossClusterTopology, _ int) string {
+		return fmt.Sprintf("%s->%s", topology.GetSourceClusterID(), topology.GetTargetClusterID())
+	})))
+	for _, cluster := range config.GetClusters() {
+		fields = append(fields, zap.String("clusterInfo", fmt.Sprintf("clusterID: %s, uri: %s, pchannels: %v",
+			cluster.GetClusterID(), cluster.GetConnectionParam().GetUri(), cluster.GetPchannels())))
+	}
+	return fields
+}
