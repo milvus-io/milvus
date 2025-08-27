09b29a88aa357ff099e2a8505e0bb918e36e77d9
XuanYang-cn <xuan.yang@zilliz.com>
2025-08-27 14:31:50 +0800

    enhance: Remove not inused allocator (#43821)
    See also: #44039
    
    ---------
    
    Signed-off-by: yangxuan <xuan.yang@zilliz.com>

--- internal/flushcommon/pipeline/data_sync_service_test.go
+++ internal/flushcommon/pipeline/data_sync_service_test.go
@@ -31,7 +31,7 @@ import (
 	"github.com/milvus-io/milvus-proto/go-api/v2/commonpb"
 	"github.com/milvus-io/milvus-proto/go-api/v2/msgpb"
 	"github.com/milvus-io/milvus-proto/go-api/v2/schemapb"
-	"github.com/milvus-io/milvus/internal/datanode/allocator"
+	"github.com/milvus-io/milvus/internal/allocator"
 	"github.com/milvus-io/milvus/internal/flushcommon/broker"
 	"github.com/milvus-io/milvus/internal/flushcommon/metacache"
 	"github.com/milvus-io/milvus/internal/flushcommon/syncmgr"
