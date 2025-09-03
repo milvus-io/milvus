69ad5cbfbf6872432423fb6715a4de54ad63a1ac
chyezh <chyezh@outlook.com>
2025-09-03 20:23:29 +0800

    enhance: refactor update replicate config operation using wal-broadcast-based DDL/DCL framework
    Signed-off-by: chyezh <chyezh@outlook.com>

--- internal/metastore/catalog.go
+++ internal/metastore/catalog.go
@@ -221,6 +221,8 @@ type ReplicationCatalog interface {
 }
 
 // StreamingCoordCataLog is the interface for streamingcoord catalog
+// All write operation of catalog is reliable, the error will only be returned if the ctx is canceled,
+// otherwise it will retry until success.
 type StreamingCoordCataLog interface {
 	ReplicationCatalog
 
@@ -228,12 +230,14 @@ type StreamingCoordCataLog interface {
 	GetCChannel(ctx context.Context) (*streamingpb.CChannelMeta, error)
 
 	// SaveCChannel save the control channel to metastore.
+	// Only return error if the ctx is canceled, otherwise it will retry until success.
 	SaveCChannel(ctx context.Context, info *streamingpb.CChannelMeta) error
 
 	// GetVersion get the streaming version from metastore.
 	GetVersion(ctx context.Context) (*streamingpb.StreamingVersion, error)
 
 	// SaveVersion save the streaming version to metastore.
+	// Only return error if the ctx is canceled, otherwise it will retry until success.
 	SaveVersion(ctx context.Context, version *streamingpb.StreamingVersion) error
 
 	// physical channel watch related
@@ -242,6 +246,7 @@ type StreamingCoordCataLog interface {
 	ListPChannel(ctx context.Context) ([]*streamingpb.PChannelMeta, error)
 
 	// SavePChannel save a pchannel info to metastore.
+	// Only return error if the ctx is canceled, otherwise it will retry until success.
 	SavePChannels(ctx context.Context, info []*streamingpb.PChannelMeta) error
 
 	// ListBroadcastTask list all broadcast tasks.
@@ -251,9 +256,11 @@ type StreamingCoordCataLog interface {
 	// SaveBroadcastTask save the broadcast task to metastore.
 	// Make the task recoverable after restart.
 	// When broadcast task is done, it will be removed from metastore.
+	// Only return error if the ctx is canceled, otherwise it will retry until success.
 	SaveBroadcastTask(ctx context.Context, broadcastID uint64, task *streamingpb.BroadcastTask) error
 
 	// SaveReplicateConfiguration saves the replicate configuration to metastore.
+	// Only return error if the ctx is canceled, otherwise it will retry until success.
 	SaveReplicateConfiguration(ctx context.Context, config *streamingpb.ReplicateConfigurationMeta, replicatingTasks []*streamingpb.ReplicatePChannelMeta) error
 
 	// GetReplicateConfiguration gets the replicate configuration from metastore.
