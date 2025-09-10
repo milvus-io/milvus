fb2309d2005ceb6b6a00ec30baabf882ee00ad18
Congqi Xia <congqi.xia@zilliz.com>
2025-09-10 12:40:05 +0800

    fix: Adapt all valid nullable data without valid data flags
    Related to milvus-io/pymilvus#2971
    
    Signed-off-by: Congqi Xia <congqi.xia@zilliz.com>

--- internal/proxy/validate_util.go
+++ internal/proxy/validate_util.go
@@ -5,6 +5,7 @@ import (
 	"math"
 	"reflect"
 
+	"github.com/samber/lo"
 	"go.uber.org/zap"
 
 	"github.com/milvus-io/milvus-proto/go-api/v2/schemapb"
@@ -339,6 +340,11 @@ func (v *validateUtil) fillWithValue(data []*schemapb.FieldData, schema *typeuti
 			return err
 		}
 
+		// adapt all valid data for nullable column
+		if fieldSchema.GetNullable() && len(field.GetValidData()) == 0 {
+			field.ValidData = lo.RepeatBy(numRows, func(i int) bool { return true })
+		}
+
 		if fieldSchema.GetDefaultValue() == nil {
 			err = v.fillWithNullValue(field, fieldSchema, numRows)
 			if err != nil {
@@ -356,13 +362,13 @@ func (v *validateUtil) fillWithValue(data []*schemapb.FieldData, schema *typeuti
 }
 
 func (v *validateUtil) fillWithNullValue(field *schemapb.FieldData, fieldSchema *schemapb.FieldSchema, numRows int) error {
+	if !fieldSchema.GetNullable() {
+		return nil
+	}
+
 	err := nullutil.CheckValidData(field.GetValidData(), fieldSchema, numRows)
 	if err != nil {
 		return err
-	}
-
-	if !fieldSchema.GetNullable() {
-		return nil
 	}
 
 	switch field.Field.(type) {
