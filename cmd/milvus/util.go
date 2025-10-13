milvus-io/milvus

commit 23085ae437ad2eae8d01635a2256e1e68913b817

fix: use query node label check if streamingnode (#44099)
issue: #44014

- Because the session of querynode and streamingnode is different.
- So when streamingnode session down first, a streaming query node will
be treated as querynode.
- Use label but not streaming node session to fix it.

Signed-off-by: chyezh <chyezh@outlook.com>

internal/querycoordv2/meta/channel_dist_manager.go
@@ -22,7 +22,7 @@ import (
 	"github.com/samber/lo"
 	"google.golang.org/protobuf/proto"
 
-	"github.com/milvus-io/milvus/internal/coordinator/snmanager"
+	"github.com/milvus-io/milvus/internal/querycoordv2/session"
 	"github.com/milvus-io/milvus/internal/util/metrics"
 	"github.com/milvus-io/milvus/pkg/v2/proto/datapb"
 	"github.com/milvus-io/milvus/pkg/v2/proto/querypb"
@@ -242,12 +242,15 @@ type ChannelDistManager struct {
 
 	// CollectionID -> Channels
 	collectionIndex map[int64][]*DmChannel
+
+	nodeManager *session.NodeManager
 }
 
-func NewChannelDistManager() *ChannelDistManager {
+func NewChannelDistManager(nodeManager *session.NodeManager) *ChannelDistManager {
 	return &ChannelDistManager{
 		channels:        make(map[typeutil.UniqueID]nodeChannels),
 		collectionIndex: make(map[int64][]*DmChannel),
+		nodeManager:     nodeManager,
 	}
 }
 
@@ -354,10 +357,9 @@ func (m *ChannelDistManager) GetShardLeader(channelName string, replica *Replica
 				// Prioritize serviceability first, then version number
 				candidatesServiceable := candidates.IsServiceable()
 				channelServiceable := channel.IsServiceable()
-				streamingNodes := snmanager.StaticStreamingNodeManager.GetStreamingQueryNodeIDs()
-				candidateIsStreamingNode := streamingNodes.Contain(candidates.Node)
-				channelIsStreamingNode := streamingNodes.Contain(channel.Node)
 
+				candidateIsStreamingNode := m.checkIfStreamingNode(candidates.Node)
+				channelIsStreamingNode := m.checkIfStreamingNode(channel.Node)
 				if channelIsStreamingNode && !candidateIsStreamingNode {
 					// When upgrading from 2.5 to 2.6, the delegator leader may not locate at streaming node.
 					// We always use the streaming node as the delegator leader to avoid the delete data lost when loading segment.
@@ -386,6 +388,17 @@ func (m *ChannelDistManager) GetShardLeader(channelName string, replica *Replica
 	}
 
 	return candidates
+}
+
+// checkIfStreamingNode checks if the node is a streaming node.
+// Because the session of streaming node and embedded query node are different,
+// So we need to check if the node is a streaming node from the query node session but not streaming node session to avoid the wrong check result.
+func (m *ChannelDistManager) checkIfStreamingNode(nodeID int64) bool {
+	node := m.nodeManager.Get(nodeID)
+	if node == nil {
+		return false
+	}
+	return node.IsEmbeddedQueryNodeInStreamingNode()
 }
 
 func (m *ChannelDistManager) GetChannelDist(collectionID int64) []*metricsinfo.DmChannel {

internal/querycoordv2/meta/channel_dist_manager_test.go
@@ -17,21 +17,16 @@
 package meta
 
 import (
-	"context"
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/suite"
 
-	"github.com/milvus-io/milvus/internal/coordinator/snmanager"
-	"github.com/milvus-io/milvus/internal/mocks/streamingcoord/server/mock_balancer"
+	"github.com/milvus-io/milvus/internal/querycoordv2/session"
+	"github.com/milvus-io/milvus/internal/util/sessionutil"
 	"github.com/milvus-io/milvus/pkg/v2/proto/datapb"
 	"github.com/milvus-io/milvus/pkg/v2/proto/querypb"
-	"github.com/milvus-io/milvus/pkg/v2/streaming/util/types"
 	"github.com/milvus-io/milvus/pkg/v2/util/metricsinfo"
-	"github.com/milvus-io/milvus/pkg/v2/util/typeutil"
 )
 
 type ChannelDistManagerSuite struct {
@@ -86,8 +81,7 @@ func (suite *ChannelDistManagerSuite) SetupSuite() {
 }
 
 func (suite *ChannelDistManagerSuite) SetupTest() {
-	snmanager.ResetDoNothingStreamingNodeManager(suite.T())
-	suite.dist = NewChannelDistManager()
+	suite.dist = NewChannelDistManager(session.NewNodeManager())
 	// Distribution:
 	// node 0 contains channel dmc0
 	// node 1 contains channel dmc0, dmc1
@@ -248,7 +242,7 @@ func TestDmChannelIsServiceable(t *testing.T) {
 }
 
 func (suite *ChannelDistManagerSuite) TestUpdateReturnsNewServiceableChannels() {
-	dist := NewChannelDistManager()
+	dist := NewChannelDistManager(session.NewNodeManager())
 
 	// Create a non-serviceable channel
 	nonServiceableChannel := suite.channels["dmc0"].Clone()
@@ -280,7 +274,8 @@ func (suite *ChannelDistManagerSuite) TestUpdateReturnsNewServiceableChannels() 
 }
 
 func (suite *ChannelDistManagerSuite) TestGetShardLeader() {
-	dist := NewChannelDistManager()
+	nodeManager := session.NewNodeManager()
+	dist := NewChannelDistManager(nodeManager)
 
 	// Create a replica
 	replicaPB := &querypb.Replica{
@@ -348,25 +343,12 @@ func (suite *ChannelDistManagerSuite) TestGetShardLeader() {
 	suite.Nil(leader)
 
 	// Test streaming node
-	snmanager.ResetStreamingNodeManager()
-	balancer := mock_balancer.NewMockBalancer(suite.T())
-	balancer.EXPECT().WatchChannelAssignments(mock.Anything, mock.Anything).RunAndReturn(func(ctx context.Context, cb func(typeutil.VersionInt64Pair, []types.PChannelInfoAssigned) error) error {
-		<-ctx.Done()
-		return ctx.Err()
-	})
-	balancer.EXPECT().GetAllStreamingNodes(mock.Anything).Return(map[int64]*types.StreamingNodeInfo{
-		4: {
-			ServerID: 4,
-			Address:  "localhost:1",
-		},
-	}, nil)
-	snmanager.StaticStreamingNodeManager.SetBalancerReady(balancer)
-	defer snmanager.ResetStreamingNodeManager()
-	suite.Eventually(func() bool {
-		nodeIDs := snmanager.StaticStreamingNodeManager.GetStreamingQueryNodeIDs()
-		return nodeIDs.Contain(4)
-	}, 10*time.Second, 100*time.Millisecond)
-
+	nodeManager.Add(session.NewNodeInfo(session.ImmutableNodeInfo{
+		NodeID:   4,
+		Address:  "localhost:1",
+		Hostname: "localhost",
+		Labels:   map[string]string{sessionutil.LabelStreamingNodeEmbeddedQueryNode: "1"},
+	}))
 	channel1Node4 := suite.channels["dmc0"].Clone()
 	channel1Node4.Node = 4
 	channel1Node4.Version = 3
@@ -381,7 +363,7 @@ func (suite *ChannelDistManagerSuite) TestGetShardLeader() {
 }
 
 func TestGetChannelDistJSON(t *testing.T) {
-	manager := NewChannelDistManager()
+	manager := NewChannelDistManager(session.NewNodeManager())
 	channel1 := &DmChannel{
 		VchannelInfo: &datapb.VchannelInfo{
 			CollectionID: 100,

internal/querycoordv2/balance/channel_level_score_balancer_test.go
@@ -72,7 +72,7 @@ func (suite *ChannelLevelScoreBalancerTestSuite) SetupTest() {
 	testMeta := meta.NewMeta(idAllocator, store, nodeManager)
 	testTarget := meta.NewTargetManager(suite.broker, testMeta)
 
-	distManager := meta.NewDistributionManager()
+	distManager := meta.NewDistributionManager(nodeManager)
 	suite.mockScheduler = task.NewMockScheduler(suite.T())
 	suite.balancer = NewChannelLevelScoreBalancer(suite.mockScheduler, nodeManager, distManager, testMeta, testTarget)
 

internal/querycoordv2/balance/rowcount_based_balancer_test.go
@@ -74,7 +74,7 @@ func (suite *RowCountBasedBalancerTestSuite) SetupTest() {
 	testMeta := meta.NewMeta(idAllocator, store, nodeManager)
 	testTarget := meta.NewTargetManager(suite.broker, testMeta)
 
-	distManager := meta.NewDistributionManager()
+	distManager := meta.NewDistributionManager(nodeManager)
 	suite.mockScheduler = task.NewMockScheduler(suite.T())
 	suite.balancer = NewRowCountBasedBalancer(suite.mockScheduler, nodeManager, distManager, testMeta, testTarget)
 

internal/querycoordv2/balance/score_based_balancer_test.go
@@ -74,7 +74,7 @@ func (suite *ScoreBasedBalancerTestSuite) SetupTest() {
 	testMeta := meta.NewMeta(idAllocator, store, nodeManager)
 	testTarget := meta.NewTargetManager(suite.broker, testMeta)
 
-	distManager := meta.NewDistributionManager()
+	distManager := meta.NewDistributionManager(nodeManager)
 	suite.mockScheduler = task.NewMockScheduler(suite.T())
 	suite.balancer = NewScoreBasedBalancer(suite.mockScheduler, nodeManager, distManager, testMeta, testTarget)
 

internal/querycoordv2/checkers/channel_checker_test.go
@@ -78,7 +78,7 @@ func (suite *ChannelCheckerTestSuite) SetupTest() {
 	suite.broker = meta.NewMockBroker(suite.T())
 	targetManager := meta.NewTargetManager(suite.broker, suite.meta)
 
-	distManager := meta.NewDistributionManager()
+	distManager := meta.NewDistributionManager(suite.nodeMgr)
 
 	balancer := suite.createMockBalancer()
 	suite.checker = NewChannelChecker(suite.meta, distManager, targetManager, suite.nodeMgr, func() balance.Balance { return balancer })

internal/querycoordv2/checkers/controller_base_test.go
@@ -71,7 +71,7 @@ func (suite *ControllerBaseTestSuite) SetupTest() {
 	idAllocator := RandomIncrementIDAllocator()
 	suite.nodeMgr = session.NewNodeManager()
 	suite.meta = meta.NewMeta(idAllocator, store, suite.nodeMgr)
-	suite.dist = meta.NewDistributionManager()
+	suite.dist = meta.NewDistributionManager(suite.nodeMgr)
 	suite.broker = meta.NewMockBroker(suite.T())
 	suite.targetManager = meta.NewTargetManager(suite.broker, suite.meta)
 

internal/querycoordv2/checkers/controller_test.go
@@ -77,7 +77,7 @@ func (suite *CheckerControllerSuite) SetupTest() {
 	idAllocator := RandomIncrementIDAllocator()
 	suite.nodeMgr = session.NewNodeManager()
 	suite.meta = meta.NewMeta(idAllocator, store, suite.nodeMgr)
-	suite.dist = meta.NewDistributionManager()
+	suite.dist = meta.NewDistributionManager(suite.nodeMgr)
 	suite.broker = meta.NewMockBroker(suite.T())
 	suite.targetManager = meta.NewTargetManager(suite.broker, suite.meta)
 

commit 53e8f150e865ff715a4456c888341f36b352a798

fix: check if qn is sqn with label and streamingnode list (#44792)
issue: #44014

- On standalone, the query node inside need to load segment and watch
channel, so the querynode is not a embeded querynode in streamingnode
without `LabelStreamingNodeEmbeddedQueryNode`. The channel dist manager
can not confirm a standalone node is a embededStreamingNode.

Bug is introduced by #44099

Signed-off-by: chyezh <chyezh@outlook.com>

cmd/milvus/util.go
@@ -21,7 +21,6 @@ import (
 
 	"github.com/milvus-io/milvus/cmd/roles"
 	"github.com/milvus-io/milvus/internal/util/sessionutil"
-	"github.com/milvus-io/milvus/internal/util/streamingutil"
 	"github.com/milvus-io/milvus/pkg/v2/log"
 	"github.com/milvus-io/milvus/pkg/v2/util/etcd"
 	"github.com/milvus-io/milvus/pkg/v2/util/hardware"
@@ -138,7 +137,7 @@ func GetMilvusRoles(args []string, flags *flag.FlagSet) *roles.MilvusRoles {
 	case typeutil.DataNodeRole:
 		role.EnableDataNode = true
 	case typeutil.StreamingNodeRole:
-		streamingutil.EnableEmbededQueryNode()
+		sessionutil.EnableEmbededQueryNodeLabel()
 		role.EnableStreamingNode = true
 		role.EnableQueryNode = true
 	case typeutil.StandaloneRole, typeutil.EmbeddedRole:
@@ -149,9 +148,9 @@ func GetMilvusRoles(args []string, flags *flag.FlagSet) *roles.MilvusRoles {
 		role.EnableStreamingNode = true
 		role.Local = true
 		role.Embedded = serverType == typeutil.EmbeddedRole
+		sessionutil.EnableStandaloneLabel()
 	case typeutil.MixCoordRole:
 		role.EnableMixCoord = true
-
 	case typeutil.MixtureRole:
 		role.EnableRootCoord = enableRootCoord
 		role.EnableQueryCoord = enableQueryCoord
@@ -162,7 +161,7 @@ func GetMilvusRoles(args []string, flags *flag.FlagSet) *roles.MilvusRoles {
 		role.EnableStreamingNode = enableStreamingNode
 		if enableStreamingNode && !enableQueryNode {
 			role.EnableQueryNode = true
-			streamingutil.EnableEmbededQueryNode()
+			sessionutil.EnableEmbededQueryNodeLabel()
 		}
 
 	case typeutil.CDCRole:

internal/querycoordv2/meta/channel_dist_manager.go
@@ -424,7 +424,7 @@ func (m *ChannelDistManager) checkIfStreamingNode(nodeID int64) bool {
 	if node == nil {
 		return false
 	}
-	return node.IsEmbeddedQueryNodeInStreamingNode()
+	return node.IsEmbeddedQueryNodeInStreamingNode() || node.IsInStandalone()
 }
 
 func (m *ChannelDistManager) GetChannelDist(collectionID int64) []*metricsinfo.DmChannel {

internal/querycoordv2/session/node_manager.go
@@ -153,6 +153,11 @@ func (n *NodeInfo) Labels() map[string]string {
 	return n.immutableInfo.Labels
 }
 
+// IsInStandalone returns true if the node is in standalone.
+func (n *NodeInfo) IsInStandalone() bool {
+	return n.immutableInfo.Labels[sessionutil.LabelStandalone] == "1"
+}
+
 func (n *NodeInfo) IsEmbeddedQueryNodeInStreamingNode() bool {
 	return n.immutableInfo.Labels[sessionutil.LabelStreamingNodeEmbeddedQueryNode] == "1"
 }

internal/util/sessionutil/session_util.go
@@ -53,8 +53,19 @@ const (
 	DefaultIDKey                        = "id"
 	SupportedLabelPrefix                = "MILVUS_SERVER_LABEL_"
 	LabelStreamingNodeEmbeddedQueryNode = "QUERYNODE_STREAMING-EMBEDDED"
+	LabelStandalone                     = "STANDALONE"
 	MilvusNodeIDForTesting              = "MILVUS_NODE_ID_FOR_TESTING"
 )
+
+// EnableEmbededQueryNodeLabel set server labels for embedded query node.
+func EnableEmbededQueryNodeLabel() {
+	os.Setenv(SupportedLabelPrefix+LabelStreamingNodeEmbeddedQueryNode, "1")
+}
+
+// EnableStandaloneLabel set server labels for standalone.
+func EnableStandaloneLabel() {
+	os.Setenv(SupportedLabelPrefix+LabelStandalone, "1")
+}
 
 // SessionEventType session event type
 type SessionEventType int

internal/util/streamingutil/env.go
@@ -2,8 +2,6 @@ package streamingutil
 
 import (
 	"os"
-
-	"github.com/milvus-io/milvus/internal/util/sessionutil"
 )
 
 const MilvusStreamingServiceEnabled = "MILVUS_STREAMING_SERVICE_ENABLED"
@@ -27,15 +25,4 @@ func MustEnableStreamingService() {
 	if !IsStreamingServiceEnabled() {
 		panic("start a streaming node without enabling streaming service, please set environment variable MILVUS_STREAMING_SERVICE_ENABLED = 1")
 	}
-}
-
-// EnableEmbededQueryNode set server labels for embedded query node.
-func EnableEmbededQueryNode() {
-	MustEnableStreamingService()
-	os.Setenv(sessionutil.SupportedLabelPrefix+sessionutil.LabelStreamingNodeEmbeddedQueryNode, "1")
-}
-
-// IsEmbeddedQueryNode returns whether the current node is an embedded query node in streaming node.
-func IsEmbeddedQueryNode() bool {
-	return os.Getenv(sessionutil.SupportedLabelPrefix+sessionutil.LabelStreamingNodeEmbeddedQueryNode) == "1"
 }

internal/querycoordv2/meta/channel_dist_manager_test.go
@@ -22,6 +22,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
 
+	"github.com/milvus-io/milvus/internal/coordinator/snmanager"
 	"github.com/milvus-io/milvus/internal/querycoordv2/session"
 	"github.com/milvus-io/milvus/internal/util/sessionutil"
 	"github.com/milvus-io/milvus/pkg/v2/proto/datapb"
@@ -81,6 +82,8 @@ func (suite *ChannelDistManagerSuite) SetupSuite() {
 }
 
 func (suite *ChannelDistManagerSuite) SetupTest() {
+	snmanager.ResetDoNothingStreamingNodeManager(suite.T())
+
 	suite.dist = NewChannelDistManager(session.NewNodeManager())
 	// Distribution:
 	// node 0 contains channel dmc0