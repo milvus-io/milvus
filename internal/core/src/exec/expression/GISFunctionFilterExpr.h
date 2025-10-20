2516b8b9c43d0f89c1c62bde249732cab54cb32c
Cai Zhang <cai.zhang@zilliz.com>
2025-10-20 16:19:54 +0800

    Fix bug for gis function to filter geometry
    Signed-off-by: Cai Zhang <cai.zhang@zilliz.com>

--- internal/core/src/exec/expression/GISFunctionFilterExpr.h
+++ internal/core/src/exec/expression/GISFunctionFilterExpr.h
@@ -56,6 +56,16 @@ class PhyGISFunctionFilterExpr : public SegmentExpr {
         return expr_->column_;
     }
 
+    std::string
+    ToString() const {
+        return fmt::format("{}", expr_->ToString());
+    }
+
+    void
+    MoveCursor() {
+    }
+
+
  private:
     VectorPtr
     EvalForIndexSegment();
