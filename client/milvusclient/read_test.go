72150bd53ee242664b83cf96bdccead6d1cba73a
Amit Kumar <amit.kumar@reddit.com>
2025-10-21 06:50:05 -0700

    Fixes: go linter issues
    Signed-off-by: Amit Kumar <amit.kumar@reddit.com>

--- client/milvusclient/read_test.go
+++ client/milvusclient/read_test.go
@@ -12,10 +12,9 @@ import (
 
 	"github.com/milvus-io/milvus-proto/go-api/v2/milvuspb"
 	"github.com/milvus-io/milvus-proto/go-api/v2/schemapb"
-	"github.com/milvus-io/milvus/pkg/v2/util/merr"
-
 	"github.com/milvus-io/milvus/client/v2/entity"
 	"github.com/milvus-io/milvus/client/v2/index"
+	"github.com/milvus-io/milvus/pkg/v2/util/merr"
 )
 
 type ReadSuite struct {
