6765e19b486d0a538942d0b519737f2d2cc1d156
chasingegg <chao.gao@zilliz.com>
2025-10-31 12:16:19 +0800

    enhance: make knowhere thread pool config refreshable
    Signed-off-by: chasingegg <chao.gao@zilliz.com>

--- pkg/util/paramtable/component_param.go
+++ pkg/util/paramtable/component_param.go
@@ -3037,8 +3037,8 @@ type queryNodeConfig struct {
 	StatsPublishInterval ParamItem `refreshable:"true"`
 
 	// segcore
-	KnowhereFetchThreadPoolSize   ParamItem `refreshable:"false"`
-	KnowhereThreadPoolSize        ParamItem `refreshable:"false"`
+	KnowhereFetchThreadPoolSize   ParamItem `refreshable:"true"`
+	KnowhereThreadPoolSize        ParamItem `refreshable:"true"`
 	ChunkRows                     ParamItem `refreshable:"false"`
 	EnableInterminSegmentIndex    ParamItem `refreshable:"false"`
 	InterimIndexNlist             ParamItem `refreshable:"false"`
@@ -3495,13 +3495,13 @@ If set to 0, time based eviction is disabled.`,
 		Version:      "2.0.0",
 		DefaultValue: "4",
 		Formatter: func(v string) string {
-			factor := getAsInt64(v)
+			factor := getAsFloat(v)
 			if factor <= 0 || !p.EnableDisk.GetAsBool() {
 				factor = 1
 			} else if factor > 32 {
 				factor = 32
 			}
-			knowhereThreadPoolSize := uint32(hardware.GetCPUNum()) * uint32(factor)
+			knowhereThreadPoolSize := uint32(float64(hardware.GetCPUNum()) * factor)
 			return strconv.FormatUint(uint64(knowhereThreadPoolSize), 10)
 		},
 		Doc:    "The number of threads in knowhere's thread pool. If disk is enabled, the pool size will multiply with knowhereThreadPoolNumRatio([1, 32]).",
@@ -3514,13 +3514,13 @@ If set to 0, time based eviction is disabled.`,
 		Version:      "2.6.0",
 		DefaultValue: "4",
 		Formatter: func(v string) string {
-			factor := getAsInt64(v)
+			factor := getAsFloat(v)
 			if factor <= 0 {
 				factor = 1
 			} else if factor > 32 {
 				factor = 32
 			}
-			knowhereFetchThreadPoolSize := uint32(hardware.GetCPUNum()) * uint32(factor)
+			knowhereFetchThreadPoolSize := uint32(float64(hardware.GetCPUNum()) * factor)
 			return strconv.FormatUint(uint64(knowhereFetchThreadPoolSize), 10)
 		},
 		Doc:    "The number of threads in knowhere's fetch thread pool for object storage. The pool size will multiply with knowhereThreadPoolNumRatio([1, 32])",
