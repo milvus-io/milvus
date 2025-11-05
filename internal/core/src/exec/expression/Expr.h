3d4c4674aa663912724399a10631e1349a4425e8
luzhang <luzhang@zilliz.com>
2025-11-05 14:36:10 +0800

    fix: not use json_shredding for json path is null
    Signed-off-by: luzhang <luzhang@zilliz.com>

--- internal/core/src/exec/expression/Expr.h
+++ internal/core/src/exec/expression/Expr.h
@@ -1449,8 +1449,10 @@ class SegmentExpr : public Expr {
             return false;
         };
 
+        // if path is empty, json stats can not know key name,
+        // so we can't use json shredding data
         return PlanUseJsonStats(context) && HasJsonStats(field_id) &&
-               !path_contains_integer(nested_path);
+               !nested_path.empty() && !path_contains_integer(nested_path);
     }
 
     virtual bool
