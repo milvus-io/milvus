23dd3331a53ba535cccd8a7fcbeec09bb508074e
Amit Kumar <amit.kumar@reddit.com>
2025-11-05 08:28:04 -0800

    Removes duplicative function -bad merge fix
    Signed-off-by: Amit Kumar <amit.kumar@reddit.com>

--- internal/core/thirdparty/tantivy/tantivy-binding/src/index_reader_text.rs
+++ internal/core/thirdparty/tantivy/tantivy-binding/src/index_reader_text.rs
@@ -71,37 +71,6 @@ impl IndexReaderWrapper {
         self.search(&query, bitset)
     }
 
-    pub(crate) fn match_query_with_minimum(
-        &self,
-        q: &str,
-        min_should_match: usize,
-        bitset: *mut c_void,
-    ) -> Result<()> {
-        let mut tokenizer = self
-            .index
-            .tokenizer_for_field(self.field)
-            .unwrap_or(standard_analyzer(vec![]))
-            .clone();
-        let mut token_stream = tokenizer.token_stream(q);
-        let mut terms: Vec<Term> = Vec::new();
-        while token_stream.advance() {
-            let token = token_stream.token();
-            terms.push(Term::from_field_text(self.field, &token.text));
-        }
-        use tantivy::query::{Occur, TermQuery};
-        use tantivy::schema::IndexRecordOption;
-        let mut subqueries: Vec<(Occur, Box<dyn tantivy::query::Query>)> = Vec::new();
-        for term in terms.into_iter() {
-            subqueries.push((
-                Occur::Should,
-                Box::new(TermQuery::new(term, IndexRecordOption::Basic)),
-            ));
-        }
-        let effective_min = std::cmp::max(1, min_should_match);
-        let query = BooleanQuery::with_minimum_required_clauses(subqueries, effective_min);
-        self.search(&query, bitset)
-    }
-
     // split the query string into multiple tokens using index's default tokenizer,
     // and then execute the disconjunction of term query.
     pub(crate) fn phrase_match_query(&self, q: &str, slop: u32, bitset: *mut c_void) -> Result<()> {
