55bfd610b6fe2f6da95a49debc20536ce68f3560
congqixia <congqi.xia@zilliz.com>
2025-11-05 19:57:34 +0800

    enhance: [StorageV2] Integrate FFI interface for packed reader (#45132)
    Related to #44956
    
    Integrate the StorageV2 FFI interface as the unified storage layer for
    reading packed columnar data, replacing the custom iterative reader with
    a manifest-based approach using the milvus-storage library.
    
    Changes:
    - Add C++ FFI reader implementation (ffi_reader_c.cpp/h) with Arrow C
    Stream interface
    - Implement utility functions to convert CStorageConfig to
    milvus-storage Properties
    - Create ManifestReader in Go that generates manifests from binlogs
    - Add FFI packed reader CGO bindings (packed_reader_ffi.go)
    - Refactor NewBinlogRecordReader to use ManifestReader for V2 storage
    - Support both manifest file paths and direct manifest content
    - Enable configurable buffer sizes and column projection
    
    Technical improvements:
    - Zero-copy data exchange using Arrow C Data Interface
    - Optimized I/O operations through milvus-storage library
    - Simplified code path with manifest-based reading
    - Better performance with batched streaming reads
    
    ---------
    
    Signed-off-by: Congqi Xia <congqi.xia@zilliz.com>

---
+++ internal/core/src/storage/loon_ffi/util.h
@@ -0,0 +1,38 @@
+// Copyright (C) 2019-2020 Zilliz. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance
+// with the License. You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License
+// is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+// or implied. See the License for the specific language governing permissions and limitations under the License
+
+#pragma once
+
+#include <memory>
+#include "common/common_type_c.h"
+#include "common/type_c.h"
+#include "milvus-storage/ffi_c.h"
+
+/**
+ * @brief Creates a shared pointer to Properties from CStorageConfig
+ *
+ * This utility function converts a CStorageConfig structure into a Properties
+ * object by calling the FFI properties_create function. All configuration fields
+ * from CStorageConfig are mapped to corresponding key-value pairs in Properties.
+ *
+ * The following fields are converted:
+ * - String fields: address, bucket_name, access_key_id, access_key_value,
+ *   root_path, storage_type, cloud_provider, iam_endpoint, log_level,
+ *   region, ssl_ca_cert, gcp_credential_json
+ * - Boolean fields: use_ssl, use_iam, use_virtual_host, use_custom_part_upload
+ * - Integer fields: request_timeout_ms
+ *
+ * @param c_storage_config The storage configuration to convert
+ * @return std::shared_ptr<Properties> Shared pointer to the created Properties
+ * @throws std::runtime_error If properties_create fails with error message from FFI
+ */
+std::shared_ptr<Properties>
+MakePropertiesFromStorageConfig(CStorageConfig c_storage_config);
