d91c4fd79b7fc76f9496d6d36373046716a39df7
aoiasd <zhicheng.yue@zilliz.com>
2025-11-13 10:58:21 +0800

    support use synonym filter
    Signed-off-by: aoiasd <zhicheng.yue@zilliz.com>

--- internal/core/thirdparty/tantivy/tantivy-binding/src/analyzer/filter/mod.rs
+++ internal/core/thirdparty/tantivy/tantivy-binding/src/analyzer/filter/mod.rs
@@ -2,10 +2,12 @@ mod filter;
 mod regex_filter;
 mod remove_punct_filter;
 pub(crate) mod stop_words;
+mod synonym_filter;
 mod util;
 
 use regex_filter::RegexFilter;
 use remove_punct_filter::RemovePunctFilter;
+use synonym_filter::SynonymFilter;
 
 pub(crate) use filter::*;
 pub(crate) use util::*;
