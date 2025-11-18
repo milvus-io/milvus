85a61d5ec44fb410d3758b24a1569ecad6fab2a5
yangxuan <xuan.yang@zilliz.com>
2025-11-18 17:28:17 +0800

    feat: Add force merge
    See also: #46043
    
    Signed-off-by: yangxuan <xuan.yang@zilliz.com>

--- internal/datacoord/compaction_trigger.go
+++ internal/datacoord/compaction_trigger.go
@@ -411,7 +411,7 @@ func (t *compactionTrigger) handleSignal(signal *compactionSignal) error {
 				ResultSegments: []int64{},
 				TotalRows:      totalRows,
 				Schema:         coll.Schema,
-				MaxSize:        getExpandedSize(expectedSize),
+				MaxSize:        expectedSize,
 				PreAllocatedSegmentIDs: &datapb.IDRange{
 					Begin: startID + 1,
 					End:   endID,
@@ -429,6 +429,7 @@ func (t *compactionTrigger) handleSignal(signal *compactionSignal) error {
 			log.Info("time cost of generating compaction",
 				zap.Int64("planID", task.GetPlanID()),
 				zap.Int64("time cost", time.Since(start).Milliseconds()),
+				zap.Int64("target size", task.GetMaxSize()),
 				zap.Int64s("inputSegments", inputSegmentIDs))
 		}
 	}
@@ -813,6 +814,7 @@ func (t *compactionTrigger) squeezeSmallSegmentsToBuckets(small []*SegmentInfo, 
 	return small
 }
 
+// deprecated
 func getExpandedSize(size int64) int64 {
 	return int64(float64(size) * Params.DataCoordCfg.SegmentExpansionRate.GetAsFloat())
 }
