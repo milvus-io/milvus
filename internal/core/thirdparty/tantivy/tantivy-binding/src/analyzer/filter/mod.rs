94ef83065c403739a22e4332a8fc68993a6e7460
aoiasd <zhicheng.yue@zilliz.com>
2025-11-24 20:22:10 +0800

    support use  pinyin filter
    Signed-off-by: aoiasd <zhicheng.yue@zilliz.com>

--- internal/core/thirdparty/tantivy/tantivy-binding/src/analyzer/filter/mod.rs
+++ internal/core/thirdparty/tantivy/tantivy-binding/src/analyzer/filter/mod.rs
@@ -1,11 +1,13 @@
 mod filter;
 mod regex_filter;
+mod pinyin_filter;
 mod remove_punct_filter;
 pub(crate) mod stop_words;
 mod util;
 
 use regex_filter::RegexFilter;
 use remove_punct_filter::RemovePunctFilter;
+use pinyin_filter::PinyinFilter;
 
 pub(crate) use filter::*;
 pub(crate) use util::*;
