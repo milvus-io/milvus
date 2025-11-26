6c890e6ba46e764429d0545f7dd56a51361986d6
chyezh <chyezh@outlook.com>
2025-11-26 12:09:19 +0800

    remove the streamingnode checking when loading segment
    If we enable checking when loading segments, all segment should always be loaded by streamingnode but not 2.5 querynode, make some search and query failure when upgrading.
    Otherwise, some search and query result will be wrong when upgrading. We choose to disable this checking for now to promise available search and query when upgrading.
    
    Signed-off-by: chyezh <chyezh@outlook.com>

--- internal/querycoordv2/task/executor.go
+++ internal/querycoordv2/task/executor.go
@@ -31,12 +31,10 @@ import (
 
 	"github.com/milvus-io/milvus-proto/go-api/v2/commonpb"
 	"github.com/milvus-io/milvus-proto/go-api/v2/milvuspb"
-	"github.com/milvus-io/milvus/internal/coordinator/snmanager"
 	"github.com/milvus-io/milvus/internal/querycoordv2/meta"
 	. "github.com/milvus-io/milvus/internal/querycoordv2/params"
 	"github.com/milvus-io/milvus/internal/querycoordv2/session"
 	"github.com/milvus-io/milvus/internal/querycoordv2/utils"
-	"github.com/milvus-io/milvus/internal/util/streamingutil"
 	"github.com/milvus-io/milvus/pkg/v2/common"
 	"github.com/milvus-io/milvus/pkg/v2/log"
 	"github.com/milvus-io/milvus/pkg/v2/proto/datapb"
@@ -243,11 +241,6 @@ func (ex *Executor) loadSegment(task *SegmentTask, step int) error {
 		return err
 	}
 
-	if err := ex.checkIfShardLeaderIsStreamingNode(view); err != nil {
-		log.Warn("shard leader is not a streamingnode, skip load segment", zap.Error(err))
-		return err
-	}
-
 	log = log.With(zap.Int64("shardLeader", view.Node))
 
 	// NOTE: for balance segment task, expected load and release execution on the same shard leader
@@ -270,25 +263,29 @@ func (ex *Executor) loadSegment(task *SegmentTask, step int) error {
 	return nil
 }
 
-// checkIfShardLeaderIsStreamingNode checks if the shard leader is a streamingnode.
+// If we enable following checking when loading segments,
+// 1. all segment should always be loaded by streamingnode but not 2.5 querynode, make some search and query failure when upgrading.
+// Otherwise, some search and query result will be wrong when upgrading.
+// We choose to disable this checking for now to promise available search and query when upgrading.
+//
 // Because the L0 management at 2.6 and 2.5 is different, so when upgrading mixcoord,
 // the new mixcoord will make a wrong plan when balancing a segment from one query node to another by 2.5 delegator.
 // We need to balance the 2.5 delegator to 2.6 delegator before balancing any segment by 2.6 mixcoord.
-func (ex *Executor) checkIfShardLeaderIsStreamingNode(view *meta.DmChannel) error {
-	if !streamingutil.IsStreamingServiceEnabled() {
-		return nil
-	}
-
-	node := ex.nodeMgr.Get(view.Node)
-	if node == nil {
-		return merr.WrapErrServiceInternal(fmt.Sprintf("node %d is not found", view.Node))
-	}
-	nodes := snmanager.StaticStreamingNodeManager.GetStreamingQueryNodeIDs()
-	if !nodes.Contain(view.Node) {
-		return merr.WrapErrServiceInternal(fmt.Sprintf("channel %s at node %d is not working at streamingnode, skip load segment", view.GetChannelName(), view.Node))
-	}
-	return nil
-}
+// func (ex *Executor) checkIfShardLeaderIsStreamingNode(view *meta.DmChannel) error {
+// 	if !streamingutil.IsStreamingServiceEnabled() {
+// 		return nil
+// 	}
+//
+// 	node := ex.nodeMgr.Get(view.Node)
+// 	if node == nil {
+// 		return merr.WrapErrServiceInternal(fmt.Sprintf("node %d is not found", view.Node))
+// 	}
+// 	nodes := snmanager.StaticStreamingNodeManager.GetStreamingQueryNodeIDs()
+// 	if !nodes.Contain(view.Node) {
+// 		return merr.WrapErrServiceInternal(fmt.Sprintf("channel %s at node %d is not working at streamingnode, skip load segment", view.GetChannelName(), view.Node))
+// 	}
+// 	return nil
+// }
 
 func (ex *Executor) releaseSegment(task *SegmentTask, step int) {
 	defer ex.removeTask(task, step)
