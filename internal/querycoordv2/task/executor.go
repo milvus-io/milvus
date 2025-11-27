31976d8adb6ab4db68db26958654988982f87d42
Zhen Ye <chyezh@outlook.com>
2025-11-28 06:59:08 +0800

    fix: executor/scheduler should be latest replica meta but not replica copy (#45877)
    issue: #45865
    
    ---------
    
    Signed-off-by: chyezh <chyezh@outlook.com>

--- internal/querycoordv2/task/executor.go
+++ internal/querycoordv2/task/executor.go
@@ -228,7 +228,7 @@ func (ex *Executor) loadSegment(task *SegmentTask, step int) error {
 	)
 
 	// get segment's replica first, then get shard leader by replica
-	replica := ex.meta.ReplicaManager.GetByCollectionAndNode(ctx, task.CollectionID(), action.Node())
+	replica := ex.meta.ReplicaManager.Get(ctx, task.ReplicaID())
 	if replica == nil {
 		msg := "node doesn't belong to any replica"
 		err := merr.WrapErrNodeNotAvailable(action.Node())
@@ -331,7 +331,7 @@ func (ex *Executor) releaseSegment(task *SegmentTask, step int) {
 
 		if ex.meta.CollectionManager.Exist(ctx, task.CollectionID()) {
 			// get segment's replica first, then get shard leader by replica
-			replica := ex.meta.ReplicaManager.GetByCollectionAndNode(ctx, task.CollectionID(), action.Node())
+			replica := ex.meta.ReplicaManager.Get(ctx, task.ReplicaID())
 			if replica == nil {
 				msg := "node doesn't belong to any replica, try to send release to worker"
 				err := merr.WrapErrNodeNotAvailable(action.Node())
@@ -577,7 +577,13 @@ func (ex *Executor) executeDropIndexAction(task *DropIndexTask, step int) {
 		ex.removeTask(task, step)
 	}()
 
-	view := ex.dist.ChannelDistManager.GetShardLeader(task.Shard(), task.replica)
+	replica := ex.meta.ReplicaManager.Get(ctx, task.ReplicaID())
+	if replica == nil {
+		err = merr.WrapErrNodeNotAvailable(action.Node())
+		log.Warn("node doesn't belong to any replica", zap.Error(err))
+		return
+	}
+	view := ex.dist.ChannelDistManager.GetShardLeader(task.Shard(), replica)
 	if view == nil {
 		err = merr.WrapErrChannelNotFound(task.Shard(), "shard delegator not found")
 		log.Warn("failed to get shard leader", zap.Error(err))
