c619e66ac3552c416baed0759e5983bd6d43bf59
yusheng.ma <yushengma@bytedance.com>
2025-12-01 17:37:04 +0800

    fix rootcoord_entity_num metric bug
    Signed-off-by: yusheng.ma <yushengma@bytedance.com>

--- internal/rootcoord/quota_center.go
+++ internal/rootcoord/quota_center.go
@@ -402,7 +402,8 @@ func (q *QuotaCenter) collectMetrics() error {
 
 	ctx, cancel := context.WithTimeout(q.ctx, GetMetricsTimeout)
 	defer cancel()
-
+	metrics.RootCoordNumEntities.Reset()
+	metrics.RootCoordIndexedNumEntities.Reset()
 	group := &errgroup.Group{}
 
 	// get Query cluster metrics
