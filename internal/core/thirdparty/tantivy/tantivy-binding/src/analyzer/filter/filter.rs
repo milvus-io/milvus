7114cd13904661cea2518480f612558aff0fc0db
aoiasd <zhicheng.yue@zilliz.com>
2025-12-05 17:13:32 +0800

    suppot use file as dictionary for analyzer filter
    Signed-off-by: aoiasd <zhicheng.yue@zilliz.com>

--- internal/core/thirdparty/tantivy/tantivy-binding/src/analyzer/filter/filter.rs
+++ internal/core/thirdparty/tantivy/tantivy-binding/src/analyzer/filter/filter.rs
@@ -1,8 +1,9 @@
 use serde_json as json;
 use tantivy::tokenizer::*;
 
-use super::util::*;
-use super::{RegexFilter, RemovePunctFilter, SynonymFilter};
+use super::{
+    CnAlphaNumOnlyFilter, CnCharOnlyFilter, RegexFilter, RemovePunctFilter, SynonymFilter,
+};
 use crate::error::{Result, TantivyBindingError};
 
 pub(crate) enum SystemFilter {
@@ -19,6 +20,12 @@ pub(crate) enum SystemFilter {
     Stemmer(Stemmer),
     Regex(RegexFilter),
     Synonym(SynonymFilter),
+}
+
+pub(crate) trait FilterBuilder {
+    fn from_json(params: &json::Map<String, json::Value>) -> Result<Self>
+    where
+        Self: Sized;
 }
 
 impl SystemFilter {
@@ -58,19 +65,6 @@ fn get_length_filter(params: &json::Map<String, json::Value>) -> Result<SystemFi
     Ok(SystemFilter::Length(RemoveLongFilter::limit(limit + 1)))
 }
 
-fn get_stop_words_filter(params: &json::Map<String, json::Value>) -> Result<SystemFilter> {
-    let value = params.get("stop_words");
-    if value.is_none() {
-        return Err(TantivyBindingError::InternalError(
-            "stop filter stop_words can't be empty".to_string(),
-        ));
-    }
-    let str_list = get_string_list(value.unwrap(), "stop_words filter")?;
-    Ok(SystemFilter::Stop(StopWordFilter::remove(
-        get_stop_words_list(str_list),
-    )))
-}
-
 fn get_decompounder_filter(params: &json::Map<String, json::Value>) -> Result<SystemFilter> {
     let value = params.get("word_list");
     if value.is_none() || !value.unwrap().is_array() {
@@ -82,13 +76,12 @@ fn get_decompounder_filter(params: &json::Map<String, json::Value>) -> Result<Sy
     let stop_words = value.unwrap().as_array().unwrap();
     let mut str_list = Vec::<String>::new();
     for element in stop_words {
-        match element.as_str() {
-            Some(word) => str_list.push(word.to_string()),
-            _ => {
-                return Err(TantivyBindingError::InternalError(
-                    "decompounder word list item should be string".to_string(),
-                ))
-            }
+        if let Some(word) = element.as_str() {
+            str_list.push(word.to_string());
+        } else {
+            return Err(TantivyBindingError::InternalError(
+                "decompounder word list item should be string".to_string(),
+            ));
         }
     }
 
@@ -101,57 +94,7 @@ fn get_decompounder_filter(params: &json::Map<String, json::Value>) -> Result<Sy
     }
 }
 
-fn get_stemmer_filter(params: &json::Map<String, json::Value>) -> Result<SystemFilter> {
-    let value = params.get("language");
-    if value.is_none() || !value.unwrap().is_string() {
-        return Err(TantivyBindingError::InternalError(
-            "stemmer language field should be string".to_string(),
-        ));
-    }
-
-    match value.unwrap().as_str().unwrap().into_language() {
-        Ok(language) => Ok(SystemFilter::Stemmer(Stemmer::new(language))),
-        Err(e) => Err(TantivyBindingError::InternalError(format!(
-            "create stemmer failed : {}",
-            e.to_string()
-        ))),
-    }
-}
-
-trait LanguageParser {
-    fn into_language(self) -> Result<Language>;
-}
-
-impl LanguageParser for &str {
-    fn into_language(self) -> Result<Language> {
-        match self.to_lowercase().as_str() {
-            "arabic" => Ok(Language::Arabic),
-            "arabig" => Ok(Language::Arabic), // typo
-            "danish" => Ok(Language::Danish),
-            "dutch" => Ok(Language::Dutch),
-            "english" => Ok(Language::English),
-            "finnish" => Ok(Language::Finnish),
-            "french" => Ok(Language::French),
-            "german" => Ok(Language::German),
-            "greek" => Ok(Language::Greek),
-            "hungarian" => Ok(Language::Hungarian),
-            "italian" => Ok(Language::Italian),
-            "norwegian" => Ok(Language::Norwegian),
-            "portuguese" => Ok(Language::Portuguese),
-            "romanian" => Ok(Language::Romanian),
-            "russian" => Ok(Language::Russian),
-            "spanish" => Ok(Language::Spanish),
-            "swedish" => Ok(Language::Swedish),
-            "tamil" => Ok(Language::Tamil),
-            "turkish" => Ok(Language::Turkish),
-            other => Err(TantivyBindingError::InternalError(format!(
-                "unsupport language: {}",
-                other
-            ))),
-        }
-    }
-}
-
+// fetch build-in filter from string
 impl From<&str> for SystemFilter {
     fn from(value: &str) -> Self {
         match value {
@@ -180,9 +123,11 @@ impl TryFrom<&json::Map<String, json::Value>> for SystemFilter {
 
                 match value.as_str().unwrap() {
                     "length" => get_length_filter(params),
-                    "stop" => get_stop_words_filter(params),
-                    "decompounder" => get_decompounder_filter(params),
-                    "stemmer" => get_stemmer_filter(params),
+                    "stop" => StopWordFilter::from_json(params).map(|f| SystemFilter::Stop(f)),
+                    "decompounder" => {
+                        SplitCompoundWords::from_json(params).map(|f| SystemFilter::Decompounder(f))
+                    }
+                    "stemmer" => Stemmer::from_json(params).map(|f| SystemFilter::Stemmer(f)),
                     "regex" => RegexFilter::from_json(params).map(|f| SystemFilter::Regex(f)),
                     "synonym" => SynonymFilter::from_json(params).map(|f| SystemFilter::Synonym(f)),
                     other => Err(TantivyBindingError::InternalError(format!(
@@ -195,102 +140,5 @@ impl TryFrom<&json::Map<String, json::Value>> for SystemFilter {
                 "no type field in filter params".to_string(),
             )),
         }
-    }
-}
-
-pub struct CnCharOnlyFilter;
-
-pub struct CnCharOnlyFilterStream<T> {
-    regex: regex::Regex,
-    tail: T,
-}
-
-impl TokenFilter for CnCharOnlyFilter {
-    type Tokenizer<T: Tokenizer> = CnCharOnlyFilterWrapper<T>;
-
-    fn transform<T: Tokenizer>(self, tokenizer: T) -> CnCharOnlyFilterWrapper<T> {
-        CnCharOnlyFilterWrapper(tokenizer)
-    }
-}
-
-#[derive(Clone)]
-pub struct CnCharOnlyFilterWrapper<T>(T);
-
-impl<T: Tokenizer> Tokenizer for CnCharOnlyFilterWrapper<T> {
-    type TokenStream<'a> = CnCharOnlyFilterStream<T::TokenStream<'a>>;
-
-    fn token_stream<'a>(&'a mut self, text: &'a str) -> Self::TokenStream<'a> {
-        CnCharOnlyFilterStream {
-            regex: regex::Regex::new("\\p{Han}+").unwrap(),
-            tail: self.0.token_stream(text),
-        }
-    }
-}
-
-impl<T: TokenStream> TokenStream for CnCharOnlyFilterStream<T> {
-    fn advance(&mut self) -> bool {
-        while self.tail.advance() {
-            if self.regex.is_match(&self.tail.token().text) {
-                return true;
-            }
-        }
-
-        false
-    }
-
-    fn token(&self) -> &Token {
-        self.tail.token()
-    }
-
-    fn token_mut(&mut self) -> &mut Token {
-        self.tail.token_mut()
-    }
-}
-
-pub struct CnAlphaNumOnlyFilter;
-
-pub struct CnAlphaNumOnlyFilterStream<T> {
-    regex: regex::Regex,
-    tail: T,
-}
-
-impl TokenFilter for CnAlphaNumOnlyFilter {
-    type Tokenizer<T: Tokenizer> = CnAlphaNumOnlyFilterWrapper<T>;
-
-    fn transform<T: Tokenizer>(self, tokenizer: T) -> CnAlphaNumOnlyFilterWrapper<T> {
-        CnAlphaNumOnlyFilterWrapper(tokenizer)
-    }
-}
-#[derive(Clone)]
-pub struct CnAlphaNumOnlyFilterWrapper<T>(T);
-
-impl<T: Tokenizer> Tokenizer for CnAlphaNumOnlyFilterWrapper<T> {
-    type TokenStream<'a> = CnAlphaNumOnlyFilterStream<T::TokenStream<'a>>;
-
-    fn token_stream<'a>(&'a mut self, text: &'a str) -> Self::TokenStream<'a> {
-        CnAlphaNumOnlyFilterStream {
-            regex: regex::Regex::new(r"[\p{Han}a-zA-Z0-9]+").unwrap(),
-            tail: self.0.token_stream(text),
-        }
-    }
-}
-
-impl<T: TokenStream> TokenStream for CnAlphaNumOnlyFilterStream<T> {
-    fn advance(&mut self) -> bool {
-        while self.tail.advance() {
-            if self.regex.is_match(&self.tail.token().text) {
-                return true;
-            }
-        }
-
-        false
-    }
-
-    fn token(&self) -> &Token {
-        self.tail.token()
-    }
-
-    fn token_mut(&mut self) -> &mut Token {
-        self.tail.token_mut()
     }
 }
