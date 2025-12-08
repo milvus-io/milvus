459425ac8459473835d72e76ec519cce4348f3ff
Zhen Ye <chyezh@outlook.com>
2025-12-08 21:47:12 +0800

    fix: wrong context using by session of grpc client (#46183)
    issue: #46182
    
    Signed-off-by: chyezh <chyezh@outlook.com>

--- internal/querynodev2/server.go
+++ internal/querynodev2/server.go
@@ -327,7 +327,7 @@ func (node *QueryNode) Init() error {
 				return NewLocalWorker(node), nil
 			}
 
-			sessions, _, err := node.session.GetSessions(typeutil.QueryNodeRole)
+			sessions, _, err := node.session.GetSessions(node.ctx, typeutil.QueryNodeRole)
 			if err != nil {
 				return nil, err
 			}
