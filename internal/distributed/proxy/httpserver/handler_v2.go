3dd4f4c1473184f149e845b99383702c2400bd70
Zhen Ye <chyezh@outlook.com>
2025-12-09 10:59:13 +0800

    fix: lost database in restful v2 (#46171)
    issue: #45812
    
    ---------
    
    Signed-off-by: chyezh <chyezh@outlook.com>

--- internal/distributed/proxy/httpserver/handler_v2.go
+++ internal/distributed/proxy/httpserver/handler_v2.go
@@ -34,7 +34,6 @@ import (
 	"go.opentelemetry.io/otel/trace"
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/metadata"
 	"google.golang.org/protobuf/proto"
 
 	"github.com/milvus-io/milvus-proto/go-api/v2/commonpb"
@@ -50,7 +49,6 @@ import (
 	"github.com/milvus-io/milvus/pkg/v2/log"
 	"github.com/milvus-io/milvus/pkg/v2/metrics"
 	"github.com/milvus-io/milvus/pkg/v2/proto/internalpb"
-	"github.com/milvus-io/milvus/pkg/v2/util"
 	"github.com/milvus-io/milvus/pkg/v2/util/crypto"
 	"github.com/milvus-io/milvus/pkg/v2/util/funcutil"
 	"github.com/milvus-io/milvus/pkg/v2/util/merr"
@@ -383,13 +381,10 @@ func wrapperProxyWithLimit(ctx context.Context, ginCtx *gin.Context, req any, ch
 
 	forwardHandler := func(reqCtx context.Context, req any) (any, error) {
 		interceptor := streaming.ForwardLegacyProxyUnaryServerInterceptor()
-		newCtx := reqCtx
 		if token, ok := ginCtx.Get(ContextToken); ok {
-			newCtx = metadata.NewIncomingContext(reqCtx, metadata.MD{
-				util.HeaderAuthorize: []string{token.(string)},
-			})
+			interceptor = streaming.ForwardLegacyProxyUnaryServerInterceptor(streaming.OptForwardAuth(token.(string)))
 		}
-		return interceptor(newCtx, req, &grpc.UnaryServerInfo{FullMethod: fullMethod}, func(ctx context.Context, req any) (interface{}, error) {
+		return interceptor(reqCtx, req, &grpc.UnaryServerInfo{FullMethod: fullMethod}, func(ctx context.Context, req any) (interface{}, error) {
 			return handler(ctx, req)
 		})
 	}
