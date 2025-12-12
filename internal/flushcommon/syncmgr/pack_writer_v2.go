c01be562c14b71188e5a68f180a1ab4f882dc63b
chyezh <chyezh@outlook.com>
2025-12-12 12:00:50 +0800

    fix: remove id allocator cache from bulkwriter
    Signed-off-by: chyezh <chyezh@outlook.com>

--- internal/flushcommon/syncmgr/pack_writer_v2.go
+++ internal/flushcommon/syncmgr/pack_writer_v2.go
@@ -80,12 +80,6 @@ func (bw *BulkPackWriterV2) Write(ctx context.Context, pack *SyncPack) (
 	size int64,
 	err error,
 ) {
-	err = bw.prefetchIDs(pack)
-	if err != nil {
-		log.Warn("failed allocate ids for sync task", zap.Error(err))
-		return
-	}
-
 	if inserts, err = bw.writeInserts(ctx, pack); err != nil {
 		log.Error("failed to write insert data", zap.Error(err))
 		return
@@ -191,7 +185,11 @@ func (bw *BulkPackWriterV2) writeInsertsIntoStorage(_ context.Context,
 	logs := make(map[int64]*datapb.FieldBinlog)
 	paths := make([]string, 0)
 	for _, columnGroup := range columnGroups {
-		path := metautil.BuildInsertLogPath(bw.getRootPath(), pack.collectionID, pack.partitionID, pack.segmentID, columnGroup.GroupID, bw.nextID())
+		id, err := bw.allocator.AllocOne()
+		if err != nil {
+			return nil, err
+		}
+		path := metautil.BuildInsertLogPath(bw.getRootPath(), pack.collectionID, pack.partitionID, pack.segmentID, columnGroup.GroupID, id)
 		paths = append(paths, path)
 	}
 	bucketName := bw.getBucketName()
