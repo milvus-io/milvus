60d82fdf86e7f60342d8ebf5c89709dc43bb34b1
chyezh <chyezh@outlook.com>
2025-12-19 11:01:25 +0800

    enhance: support milvus version when coordinator startup
    Signed-off-by: chyezh <chyezh@outlook.com>

--- internal/util/sessionutil/session_util_test.go
+++ internal/util/sessionutil/session_util_test.go
@@ -771,6 +771,67 @@ func (s *SessionSuite) TestGetSessions() {
 	assert.Equal(s.T(), "value2", ret["key2"])
 }
 
+func (s *SessionSuite) TestVersionKey() {
+	ctx := context.Background()
+	session := NewSessionWithEtcd(ctx, s.metaRoot, s.client)
+	session.Init(typeutil.MixCoordRole, "normal", false, false)
+
+	session.Register()
+
+	resp, err := s.client.Get(ctx, session.versionKey)
+	s.Require().NoError(err)
+	s.Equal(1, len(resp.Kvs))
+	s.Equal(common.Version.String(), string(resp.Kvs[0].Value))
+
+	common.Version = semver.MustParse("2.5.6")
+
+	s.Panics(func() {
+		session2 := NewSessionWithEtcd(ctx, s.metaRoot, s.client)
+		session2.Init(typeutil.MixCoordRole, "normal", false, false)
+		session2.Register()
+
+		resp, err = s.client.Get(ctx, session2.versionKey)
+		s.Require().NoError(err)
+		s.Equal(1, len(resp.Kvs))
+		s.Equal(common.Version.String(), string(resp.Kvs[0].Value))
+	})
+
+	session.Stop()
+
+	common.Version = semver.MustParse("2.6.4")
+	session = NewSessionWithEtcd(ctx, s.metaRoot, s.client)
+	session.Init(typeutil.MixCoordRole, "normal", false, false)
+	session.Register()
+
+	resp, err = s.client.Get(ctx, session.versionKey)
+	s.Require().NoError(err)
+	s.Equal(1, len(resp.Kvs))
+	s.Equal(common.Version.String(), string(resp.Kvs[0].Value))
+
+	session.Stop()
+
+	common.Version = semver.MustParse("2.6.7")
+	session = NewSessionWithEtcd(ctx, s.metaRoot, s.client)
+	session.Init(typeutil.MixCoordRole, "normal", false, false)
+	session.Register()
+
+	resp, err = s.client.Get(ctx, session.versionKey)
+	s.Require().NoError(err)
+	s.Equal(1, len(resp.Kvs))
+	s.Equal(common.Version.String(), string(resp.Kvs[0].Value))
+	session.Stop()
+
+	common.Version = semver.MustParse("3.0.0")
+	session = NewSessionWithEtcd(ctx, s.metaRoot, s.client)
+	session.Init(typeutil.MixCoordRole, "normal", false, false)
+	session.Register()
+
+	resp, err = s.client.Get(ctx, session.versionKey)
+	s.Require().NoError(err)
+	s.Equal(1, len(resp.Kvs))
+	s.Equal(common.Version.String(), string(resp.Kvs[0].Value))
+}
+
 func (s *SessionSuite) TestSessionLifetime() {
 	ctx := context.Background()
 	session := NewSessionWithEtcd(ctx, s.metaRoot, s.client)
