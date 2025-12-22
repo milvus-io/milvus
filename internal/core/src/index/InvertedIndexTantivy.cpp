d54522f780d61937f6f136ba25145b5ea7da4927
Buqian Zheng <zhengbuqian@gmail.com>
2025-12-22 23:17:01 +0800

    fix issue
    Signed-off-by: Buqian Zheng <zhengbuqian@gmail.com>

--- internal/core/src/index/InvertedIndexTantivy.cpp
+++ internal/core/src/index/InvertedIndexTantivy.cpp
@@ -651,7 +651,6 @@ InvertedIndexTantivy<T>::BuildWithFieldData(
                       fmt::format("Inverted index not supported on {}",
                                   schema_.data_type()));
     }
-    ComputeByteSize();
 }
 
 template <typename T>
