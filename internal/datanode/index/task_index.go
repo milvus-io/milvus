5443bf0063927f36229fbc31b5eae1b6ef0c9540
yangxuan <xuan.yang@zilliz.com>
2025-12-23 14:48:27 +0800

    enhance: Tidy build index logs
    Signed-off-by: yangxuan <xuan.yang@zilliz.com>

--- internal/datanode/index/task_index.go
+++ internal/datanode/index/task_index.go
@@ -221,8 +221,11 @@ func (it *indexBuildTask) PreExecute(ctx context.Context) error {
 }
 
 func (it *indexBuildTask) Execute(ctx context.Context) error {
-	log := log.Ctx(ctx).With(zap.String("clusterID", it.req.GetClusterID()), zap.Int64("buildID", it.req.GetBuildID()),
-		zap.Int64("collection", it.req.GetCollectionID()), zap.Int64("segmentID", it.req.GetSegmentID()),
+	log := log.Ctx(ctx).With(
+		zap.String("clusterID", it.req.GetClusterID()),
+		zap.Int64("buildID", it.req.GetBuildID()),
+		zap.Int64("collection", it.req.GetCollectionID()),
+		zap.Int64("segmentID", it.req.GetSegmentID()),
 		zap.Int32("currentIndexVersion", it.req.GetCurrentIndexVersion()))
 
 	indexType := it.newIndexParams[common.IndexTypeKey]
@@ -298,11 +301,6 @@ func (it *indexBuildTask) Execute(ctx context.Context) error {
 		LackBinlogRows:            it.req.GetLackBinlogRows(),
 		StorageVersion:            it.req.GetStorageVersion(),
 	}
-
-	if it.pluginContext != nil {
-		buildIndexParams.StoragePluginContext = it.pluginContext
-	}
-
 	if buildIndexParams.StorageVersion == storage.StorageV2 {
 		buildIndexParams.SegmentInsertFiles = util.GetSegmentInsertFiles(
 			it.req.GetInsertLogs(),
@@ -313,6 +311,11 @@ func (it *indexBuildTask) Execute(ctx context.Context) error {
 		buildIndexParams.Manifest = it.req.GetManifest()
 	}
 	log.Info("create index", zap.Any("buildIndexParams", buildIndexParams))
+
+	// set plugin context after logging the indexParams to avoid logging sensitive data
+	if it.pluginContext != nil {
+		buildIndexParams.StoragePluginContext = it.pluginContext
+	}
 
 	it.index, err = indexcgowrapper.CreateIndex(ctx, buildIndexParams)
 	if err != nil {
