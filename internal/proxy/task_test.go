5f425d913262705510f6f31ab6266fe387b98ccd
chasingegg <chao.gao@zilliz.com>
2026-01-07 21:38:22 +0800

    update
    Signed-off-by: chasingegg <chao.gao@zilliz.com>

--- internal/proxy/task_test.go
+++ internal/proxy/task_test.go
@@ -5375,6 +5375,9 @@ func TestMmapUserControlEnabled(t *testing.T) {
 		).Return(UniqueID(100), nil)
 		globalMetaCache = cache
 
+		mixCoord := NewMixCoordMock()
+		defer mixCoord.Close()
+
 		task := &alterIndexTask{
 			req: &milvuspb.AlterIndexRequest{
 				Base:           &commonpb.MsgBase{MsgType: commonpb.MsgType_AlterIndex},
@@ -5384,6 +5387,7 @@ func TestMmapUserControlEnabled(t *testing.T) {
 					{Key: common.MmapEnabledKey, Value: "true"},
 				},
 			},
+			mixCoord: mixCoord,
 		}
 		err := task.PreExecute(context.Background())
 		// No error should be returned for mmap check (may fail for other reasons in test env)
