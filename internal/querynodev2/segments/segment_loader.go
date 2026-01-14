f2491036eb2639350c6e09a98611e122110d2ee4
Cai Zhang <cai.zhang@zilliz.com>
2026-01-14 15:27:58 +0800

    fix: Avoid double counting index memory in segment loading estimation
    Signed-off-by: Cai Zhang <cai.zhang@zilliz.com>

--- internal/querynodev2/segments/segment_loader.go
+++ internal/querynodev2/segments/segment_loader.go
@@ -1948,6 +1948,7 @@ func estimateLoadingResourceUsageOfSegment(schema *schemapb.CollectionSchema, lo
 		log.Warn("failed to create schema helper", zap.String("name", schema.GetName()), zap.Error(err))
 		return nil, err
 	}
+	indexedFields := make(map[int64]struct{})
 	ctx := context.Background()
 
 	// PART 1: calculate size of indexes
@@ -1958,6 +1959,7 @@ func estimateLoadingResourceUsageOfSegment(schema *schemapb.CollectionSchema, lo
 			if err != nil {
 				return nil, err
 			}
+			indexedFields[fieldID] = struct{}{}
 
 			isVectorType := typeutil.IsVectorType(fieldSchema.GetDataType())
 
@@ -2070,10 +2072,12 @@ func estimateLoadingResourceUsageOfSegment(schema *schemapb.CollectionSchema, lo
 			continue
 		}
 
-		if !multiplyFactor.TieredEvictionEnabled || needWarmup {
-			interimIndexEnable := multiplyFactor.EnableInterminSegmentIndex && !isGrowingMmapEnable() && supportInterimIndexDataType
-			if interimIndexEnable {
-				segMemoryLoadingSize += uint64(float64(binlogSize) * multiplyFactor.tempSegmentIndexFactor)
+		if _, ok := indexedFields[fieldID]; !ok {
+			if !multiplyFactor.TieredEvictionEnabled || needWarmup {
+				interimIndexEnable := multiplyFactor.EnableInterminSegmentIndex && !isGrowingMmapEnable() && supportInterimIndexDataType
+				if interimIndexEnable {
+					segMemoryLoadingSize += uint64(float64(binlogSize) * multiplyFactor.tempSegmentIndexFactor)
+				}
 			}
 		}
 
