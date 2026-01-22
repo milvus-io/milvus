e78de98a7b451a1c68e582f301b762ea84df724f
Bingyi Sun <sunbingyi1992@gmail.com>
2026-01-22 11:51:37 +0800

    enhance: Add target manager to ReplicaObserver initialization (#47093)
    issue: https://github.com/milvus-io/milvus/issues/47072
    pr: https://github.com/milvus-io/milvus/pull/47094
    
    Signed-off-by: sunby <sunbingyi1992@gmail.com>

--- internal/querycoordv2/server.go
+++ internal/querycoordv2/server.go
@@ -434,6 +434,7 @@ func (s *Server) initObserver() {
 	s.replicaObserver = observers.NewReplicaObserver(
 		s.meta,
 		s.dist,
+		s.targetMgr,
 	)
 
 	s.resourceObserver = observers.NewResourceObserver(s.meta)
