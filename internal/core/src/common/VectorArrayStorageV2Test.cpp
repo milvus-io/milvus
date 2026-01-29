d5dc1a041bc870683fd674eb52cdcfaf16e241c1
chasingegg <chao.gao@zilliz.com>
2026-01-29 02:37:01 +0000

    update
    Signed-off-by: chasingegg <chao.gao@zilliz.com>

--- internal/core/src/common/VectorArrayStorageV2Test.cpp
+++ internal/core/src/common/VectorArrayStorageV2Test.cpp
@@ -245,8 +245,7 @@ class TestVectorArrayStorageV2 : public testing::Test {
         // Clean up test data directory
         auto fs = milvus_storage::ArrowFileSystemSingleton::GetInstance()
                       .GetArrowFileSystem();
-        auto status = fs->DeleteDir("/tmp/test_vector_array_for_storage_v2");
-        ASSERT_TRUE(status.ok());
+        fs->DeleteDir("/tmp/test_vector_array_for_storage_v2");
     }
 
  protected:
