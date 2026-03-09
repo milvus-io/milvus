29ae30450a2f648483f56474e5bf5b57208c8f41
Buqian Zheng <zhengbuqian@gmail.com>
2026-03-09 16:22:30 +0800

    fix: rustfmt formatting for log_c.rs
    Signed-off-by: Buqian Zheng <zhengbuqian@gmail.com>

--- internal/core/thirdparty/tantivy/tantivy-binding/src/log_c.rs
+++ internal/core/thirdparty/tantivy/tantivy-binding/src/log_c.rs
@@ -2,9 +2,7 @@ use std::ffi::{c_char, CStr};
 
 #[no_mangle]
 pub extern "C" fn tantivy_set_log_level(level: *const c_char) {
-    let level_str = unsafe { CStr::from_ptr(level) }
-        .to_str()
-        .unwrap_or("info");
+    let level_str = unsafe { CStr::from_ptr(level) }.to_str().unwrap_or("info");
 
     let filter = match level_str {
         "trace" => log::LevelFilter::Trace,
