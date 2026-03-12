246291798324387d4bdf6345e54034b941e27680
junjie.jiang <junjie.jiang@zilliz.com>
2026-03-12 15:16:37 +0800

    Add gemini embedding
    Signed-off-by: junjie.jiang <junjie.jiang@zilliz.com>

--- internal/util/function/embedding/text_embedding_function.go
+++ internal/util/function/embedding/text_embedding_function.go
@@ -50,6 +50,7 @@ const (
 	teiProvider          string = "tei"
 	ycProvider           string = "yc"
 	zillizProvider       string = "zilliz"
+	geminiProvider       string = "gemini"
 )
 
 func hasEmptyString(texts []string) bool {
@@ -142,8 +143,10 @@ func NewTextEmbeddingFunction(coll *schemapb.CollectionSchema, functionSchema *s
 	case zillizProvider:
 		conf := paramtable.Get().FunctionCfg.ZillizProviders.GetValue()
 		embP, newProviderErr = NewZillizEmbeddingProvider(base.outputFields[0], functionSchema, conf, extraInfo)
+	case geminiProvider:
+		embP, newProviderErr = NewGeminiEmbeddingProvider(base.outputFields[0], functionSchema, conf, credentials, extraInfo)
 	default:
-		return nil, fmt.Errorf("Unsupported text embedding service provider: [%s] , list of supported [%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s]", base.provider, openAIProvider, azureOpenAIProvider, aliDashScopeProvider, bedrockProvider, vertexAIProvider, voyageAIProvider, cohereProvider, siliconflowProvider, teiProvider, ycProvider, zillizProvider)
+		return nil, fmt.Errorf("Unsupported text embedding service provider: [%s] , list of supported [%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s]", base.provider, openAIProvider, azureOpenAIProvider, aliDashScopeProvider, bedrockProvider, vertexAIProvider, voyageAIProvider, cohereProvider, siliconflowProvider, teiProvider, ycProvider, zillizProvider, geminiProvider)
 	}
 
 	if newProviderErr != nil {
