2305218f0bc3f52720497ce36b143a04612335d4
yangxuan <xuan.yang@zilliz.com>
2026-03-20 10:55:35 +0800

    fix coding style
    Signed-off-by: yangxuan <xuan.yang@zilliz.com>

--- internal/util/initcore/init_core.go
+++ internal/util/initcore/init_core.go
@@ -258,7 +258,7 @@ func InitRemoteChunkManager(params *paramtable.ComponentParam) error {
 		useVirtualHost:      C.bool(params.MinioCfg.UseVirtualHost.GetAsBool()),
 		requestTimeoutMs:    C.int64_t(params.MinioCfg.RequestTimeoutMs.GetAsInt64()),
 		gcp_credential_json: cGcpCredentialJSON,
-		max_connections:      C.uint32_t(params.MinioCfg.MaxConnections.GetAsInt()),
+		max_connections:     C.uint32_t(params.MinioCfg.MaxConnections.GetAsInt()),
 		tls_min_version:     cTLSMinVersion,
 		use_crc32c_checksum: C.bool(params.MinioCfg.UseCRC32C.GetAsBool()),
 	}
