091528545c3db2926d3347c837a0379d0bcd22de
houseme <housemecn@gmail.com>
2026-03-31 20:39:55 +0800

    add cloud provider rustfs
    Signed-off-by: houseme <housemecn@gmail.com>

--- pkg/objectstorage/util.go
+++ pkg/objectstorage/util.go
@@ -38,6 +38,7 @@ const (
 	CloudProviderGCPNative = "gcpnative"
 	CloudProviderAWS       = "aws"
 	CloudProviderAliyun    = "aliyun"
+	CloudProviderRustFS    = "rustfs"
 	CloudProviderAzure     = "azure"
 	CloudProviderTencent   = "tencent"
 	CloudProviderHuawei    = "huawei"
