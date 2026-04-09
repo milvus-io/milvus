08bec0a33b0e36781a18cad9a01e48ab53c5fe4e
Buqian Zheng <zhengbuqian@gmail.com>
2026-04-09 16:37:39 +0800

    enhance: [3.0] remove kScalarIndexUseV3 global flag and migrate UT to V3 APIs (#48870)
    pr: #48839
    issue: https://github.com/milvus-io/milvus/issues/47417
    
    Signed-off-by: Buqian Zheng <zhengbuqian@gmail.com>

--- internal/core/unittest/init_gtest.cpp
+++ internal/core/unittest/init_gtest.cpp
@@ -121,6 +121,6 @@ main(int argc, char** argv) {
         std::chrono::milliseconds(0),
         std::chrono::milliseconds(-1));
 
-    milvus::index::kScalarIndexUseV3 = true;
+    milvus::index::kOverrideRootPathForUT = "files";
     return RUN_ALL_TESTS();
 }
