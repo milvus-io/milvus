7f3cb219b58bbbff8c6ec1c0e40d6f984ee6be40
Congqi Xia <congqi.xia@zilliz.com>
2026-05-18 14:51:11 +0800

    enhance: Disable storage v3 by default
    Set configuration to disable for storage v3
    
    Signed-off-by: Congqi Xia <congqi.xia@zilliz.com>

--- pkg/util/paramtable/component_param.go
+++ pkg/util/paramtable/component_param.go
@@ -1037,7 +1037,7 @@ Large numeric passwords require double quotes to avoid yaml parsing precision is
 	p.UseLoonFFI = ParamItem{
 		Key:          "common.storage.useLoonFFI",
 		Version:      "2.6.7",
-		DefaultValue: "true",
+		DefaultValue: "false",
 		Export:       true,
 	}
 	p.UseLoonFFI.Init(base.mgr)
