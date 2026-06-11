6a2dbf4af68be71a250f89d030c7b9212b0262b2
chyezh <chyezh@outlook.com>
2026-06-11 16:09:29 +0800

    fix: format backpressure delegator test imports (#49707)
    Signed-off-by: chyezh <chyezh@outlook.com>

--- internal/querynodev2/delegator/execute_sub_tasks_test.go
+++ internal/querynodev2/delegator/execute_sub_tasks_test.go
@@ -23,13 +23,12 @@ import (
 	"github.com/cockroachdb/errors"
 	"github.com/stretchr/testify/require"
 
+	"github.com/milvus-io/milvus/internal/querynodev2/cluster"
 	"github.com/milvus-io/milvus/pkg/v3/log"
 	"github.com/milvus-io/milvus/pkg/v3/proto/internalpb"
 	"github.com/milvus-io/milvus/pkg/v3/proto/querypb"
 	"github.com/milvus-io/milvus/pkg/v3/util/merr"
 	"github.com/milvus-io/milvus/pkg/v3/util/paramtable"
-
-	"github.com/milvus-io/milvus/internal/querynodev2/cluster"
 )
 
 func TestExecuteSubTasksPreservesWorkerStatusError(t *testing.T) {
