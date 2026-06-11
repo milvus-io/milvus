58749c7973823617b05b87ebcb36fd90e746d8e3
chyezh <chyezh@outlook.com>
2026-06-11 15:12:24 +0800

    fix: adjust backpressure test imports for 2.6 (#49707)
    Signed-off-by: chyezh <chyezh@outlook.com>

--- internal/querynodev2/delegator/execute_sub_tasks_test.go
+++ internal/querynodev2/delegator/execute_sub_tasks_test.go
@@ -23,11 +23,11 @@ import (
 	"github.com/cockroachdb/errors"
 	"github.com/stretchr/testify/require"
 
-	"github.com/milvus-io/milvus/pkg/v3/log"
-	"github.com/milvus-io/milvus/pkg/v3/proto/internalpb"
-	"github.com/milvus-io/milvus/pkg/v3/proto/querypb"
-	"github.com/milvus-io/milvus/pkg/v3/util/merr"
-	"github.com/milvus-io/milvus/pkg/v3/util/paramtable"
+	"github.com/milvus-io/milvus/pkg/v2/log"
+	"github.com/milvus-io/milvus/pkg/v2/proto/internalpb"
+	"github.com/milvus-io/milvus/pkg/v2/proto/querypb"
+	"github.com/milvus-io/milvus/pkg/v2/util/merr"
+	"github.com/milvus-io/milvus/pkg/v2/util/paramtable"
 
 	"github.com/milvus-io/milvus/internal/querynodev2/cluster"
 )
