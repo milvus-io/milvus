48e08cabf6f5ef0d8d1c92ed622a2b61c5879631
Buqian Zheng <zhengbuqian@gmail.com>
2026-06-12 06:19:32 +0000

    Fix vector array search validation tests
    Signed-off-by: Buqian Zheng <zhengbuqian@gmail.com>

--- internal/proxy/search_util.go
+++ internal/proxy/search_util.go
@@ -296,6 +296,10 @@ func parseSearchInfo(searchParamsPair []*commonpb.KeyValuePair, schema *schemapb
 		return nil, fmt.Errorf("parse iterator v2 info failed: %w", err)
 	}
 
+	if err := validateVectorArraySearchInfo(searchParamsPair, schema, searchParamStr, groupByFieldId, isIterator || planSearchIteratorV2Info != nil); err != nil {
+		return nil, err
+	}
+
 	return &SearchInfo{
 		planInfo: &planpb.QueryInfo{
 			Topk:                 queryTopK,
@@ -315,6 +319,36 @@ func parseSearchInfo(searchParamsPair []*commonpb.KeyValuePair, schema *schemapb
 		isIterator:   isIterator,
 		collectionID: collectionId,
 	}, nil
+}
+
+func validateVectorArraySearchInfo(searchParamsPair []*commonpb.KeyValuePair, schema *schemapb.CollectionSchema, searchParamStr string, groupByFieldId int64, isIterator bool) error {
+	if schema == nil {
+		return nil
+	}
+
+	annsFieldName, err := funcutil.GetAttrByKeyFromRepeatedKV(AnnsFieldKey, searchParamsPair)
+	if err != nil || annsFieldName == "" {
+		return nil
+	}
+
+	annsField := typeutil.GetFieldByName(schema, annsFieldName)
+	if annsField == nil || annsField.GetDataType() != schemapb.DataType_ArrayOfVector {
+		return nil
+	}
+
+	if strings.Contains(searchParamStr, radiusKey) {
+		return merr.WrapErrParameterInvalid("", "",
+			"range search is not supported for vector array (embedding list) fields, fieldName:"+annsField.GetName())
+	}
+	if groupByFieldId > 0 {
+		return merr.WrapErrParameterInvalid("", "",
+			"group by search is not supported for vector array (embedding list) fields, fieldName:"+annsField.GetName())
+	}
+	if isIterator {
+		return merr.WrapErrParameterInvalid("", "",
+			"search iterator is not supported for vector array (embedding list) fields, fieldName:"+annsField.GetName())
+	}
+	return nil
 }
 
 func getOutputFieldIDs(schema *schemaInfo, outputFields []string) (outputFieldIDs []UniqueID, err error) {
