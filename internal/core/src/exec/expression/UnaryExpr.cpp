65db7f1853ef92ab94769f826e10642cd86996e4
Buqian Zheng <zhengbuqian@gmail.com>
2026-07-07 07:17:10 +0000

    fix: cast float array contains literals
    Signed-off-by: Buqian Zheng <zhengbuqian@gmail.com>

--- internal/core/src/exec/expression/UnaryExpr.cpp
+++ internal/core/src/exec/expression/UnaryExpr.cpp
@@ -312,7 +312,19 @@ PhyUnaryRangeFilterExpr::Eval(EvalCtx& context, VectorPtr& result) {
                     result = ExecRangeVisitorImplArray<int64_t>(context);
                     break;
                 case proto::plan::GenericValue::ValCase::kFloatVal:
-                    result = ExecRangeVisitorImplArray<double>(context);
+                    switch (expr_->column_.element_type_) {
+                        case DataType::FLOAT:
+                            result = ExecRangeVisitorImplArray<float>(context);
+                            break;
+                        case DataType::DOUBLE:
+                            result = ExecRangeVisitorImplArray<double>(context);
+                            break;
+                        default:
+                            ThrowInfo(DataTypeInvalid,
+                                      "floating point value is not supported "
+                                      "for array element type: {}",
+                                      expr_->column_.element_type_);
+                    }
                     break;
                 case proto::plan::GenericValue::ValCase::kStringVal:
                     result = ExecRangeVisitorImplArray<std::string>(context);
