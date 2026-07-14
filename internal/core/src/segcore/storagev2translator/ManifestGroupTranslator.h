cebba8ce97bf93b9af82221650e031b6da74f7a3
Shawn Wang <shawn.wang@zilliz.com>
2026-07-14 13:44:41 +0000

    enhance: enable async manifest field loading
    Signed-off-by: Shawn Wang <shawn.wang@zilliz.com>

--- internal/core/src/segcore/storagev2translator/ManifestGroupTranslator.h
+++ internal/core/src/segcore/storagev2translator/ManifestGroupTranslator.h
@@ -39,6 +39,7 @@
 #include "common/protobuf_utils.h"
 #include "milvus-storage/reader.h"
 #include "pb/common.pb.h"
+#include "segcore/memory_planner.h"
 #include "segcore/storagev2translator/GroupCTMeta.h"
 
 namespace milvus::segcore::storagev2translator {
@@ -102,7 +103,8 @@ class ManifestGroupTranslator
         std::string shard = "",
         std::optional<ColumnSizeEstimateResult> column_size_estimate =
             std::nullopt,
-        MmapChunkWritebackConfig writeback_config = {});
+        MmapChunkWritebackConfig writeback_config = {},
+        bool enable_async_load = false);
     ~ManifestGroupTranslator() = default;
 
     /**
@@ -193,6 +195,18 @@ class ManifestGroupTranslator
     }
 
  private:
+    using CellResult = std::pair<milvus::cachinglayer::cid_t,
+                                 std::unique_ptr<milvus::GroupChunk>>;
+
+    std::vector<CellResult>
+    get_cells_legacy(milvus::OpContext* ctx,
+                     const std::vector<milvus::cachinglayer::cid_t>& cids,
+                     std::vector<milvus::segcore::CellSpec> cell_specs);
+
+    std::vector<CellResult>
+    get_cells_async(milvus::OpContext* ctx,
+                    std::vector<milvus::segcore::CellSpec> cell_specs);
+
     /**
      * @brief Load a cell from multiple Arrow Tables
      *
@@ -225,6 +239,7 @@ class ManifestGroupTranslator
     MmapChunkWritebackConfig writeback_config_;
     milvus::proto::common::LoadPriority load_priority_{
         milvus::proto::common::LoadPriority::HIGH};
+    bool enable_async_load_{false};
 };
 
 }  // namespace milvus::segcore::storagev2translator
