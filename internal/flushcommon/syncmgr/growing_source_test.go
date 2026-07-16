4d07150791ae28f09b9b44fa4a092408e0898ce0
chyezh <chyezh@outlook.com>
2026-07-16 22:43:06 +0800

    fix: avoid counting stale growing source errors (#51418)
    Signed-off-by: chyezh <chyezh@outlook.com>

--- internal/flushcommon/syncmgr/growing_source_test.go
+++ internal/flushcommon/syncmgr/growing_source_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/cockroachdb/errors"
+	"github.com/prometheus/client_golang/prometheus/testutil"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 
@@ -18,6 +19,7 @@ import (
 	"github.com/milvus-io/milvus/internal/storage"
 	"github.com/milvus-io/milvus/internal/storagecommon"
 	"github.com/milvus-io/milvus/internal/storagev2/packed"
+	"github.com/milvus-io/milvus/pkg/v3/metrics"
 	"github.com/milvus-io/milvus/pkg/v3/proto/datapb"
 	"github.com/milvus-io/milvus/pkg/v3/util/merr"
 	"github.com/milvus-io/milvus/pkg/v3/util/paramtable"
@@ -101,6 +103,7 @@ func TestGrowingSourceSyncTaskHandleErrorSkipsFailureCallbackForStaleMetaErrors(
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
+			metrics.DataNodeFlushBufferCount.Reset()
 			callbackCalled := false
 			task := NewGrowingSourceSyncTask().
 				WithFailureCallback(func(error) {
@@ -110,8 +113,14 @@ func TestGrowingSourceSyncTaskHandleErrorSkipsFailureCallbackForStaleMetaErrors(
 
 			require.NotPanics(t, func() {
 				task.HandleError(tc.err)
+				task.HandleError(tc.err)
 			})
 			require.False(t, callbackCalled)
+			require.Zero(t, testutil.ToFloat64(metrics.DataNodeFlushBufferCount.WithLabelValues(
+				paramtable.GetStringNodeID(),
+				metrics.FailLabel,
+				task.level.String(),
+			)))
 		})
 	}
 }
