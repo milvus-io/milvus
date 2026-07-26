91f48cc4d68bcdc7992e5e4ad3cd0c4e17923e80
Buqian Zheng <zhengbuqian@gmail.com>
2026-07-26 14:34:37 +0800

    test: format Python metrics helper
    Signed-off-by: Buqian Zheng <zhengbuqian@gmail.com>

--- tests/python_client/common/milvus_sys.py
+++ tests/python_client/common/milvus_sys.py
@@ -12,7 +12,7 @@ sys_logs_req = ujson.dumps({"metric_type": "system_logs"})
 
 
 class MilvusSys:
-    def __init__(self, alias='default'):
+    def __init__(self, alias="default"):
         self.alias = alias
         self.handler = connections._fetch_handler(alias=self.alias)
         if self.handler is None:
@@ -37,26 +37,25 @@ class MilvusSys:
         # self.sys_logs = self.handler._stub.GetMetrics(req, wait_for_ready=True, timeout=None)
         # log.debug(f"sys info response: {self.sys_info.response}")
 
-
     @property
     def system_version(self):
         """get the first node's build version as milvus build version"""
-        return self.nodes[0].get('infos').get('system_info').get('system_version')
+        return self.nodes[0].get("infos").get("system_info").get("system_version")
 
     @property
     def build_version(self):
         """get the first node's build version as milvus build version"""
-        return self.nodes[0].get('infos').get('system_info').get('build_version')
+        return self.nodes[0].get("infos").get("system_info").get("build_version")
 
     @property
     def build_time(self):
         """get the first node's build time as milvus build time"""
-        return self.nodes[0].get('infos').get('system_info').get('build_time')
+        return self.nodes[0].get("infos").get("system_info").get("build_time")
 
     @property
     def deploy_mode(self):
         """get the first node's deploy_mode as milvus deploy_mode"""
-        return self.nodes[0].get('infos').get('system_info').get('deploy_mode')
+        return self.nodes[0].get("infos").get("system_info").get("deploy_mode")
 
     @property
     def simd_type(self):
@@ -65,7 +64,7 @@ class MilvusSys:
         return the first query node's simd type
         """
         for node in self.query_nodes:
-            return node.get('infos').get('system_configurations').get('simd_type')
+            return node.get("infos").get("system_configurations").get("simd_type")
         raise Exception("No query node found")
 
     @property
@@ -78,7 +77,7 @@ class MilvusSys:
         """get all query nodes in Milvus deployment"""
         query_nodes = []
         for node in self.nodes:
-            if 'querynode' == node.get('infos').get('type'):
+            if "querynode" == node.get("infos").get("type"):
                 query_nodes.append(node)
         return query_nodes
 
@@ -87,7 +86,7 @@ class MilvusSys:
         """get all data nodes in Milvus deployment"""
         data_nodes = []
         for node in self.nodes:
-            if 'datanode' == node.get('infos').get('type'):
+            if "datanode" == node.get("infos").get("type"):
                 data_nodes.append(node)
         return data_nodes
 
@@ -96,7 +95,7 @@ class MilvusSys:
         """get all proxy nodes in Milvus deployment"""
         proxy_nodes = []
         for node in self.nodes:
-            if 'proxy' == node.get('infos').get('type'):
+            if "proxy" == node.get("infos").get("type"):
                 proxy_nodes.append(node)
         return proxy_nodes
 
@@ -104,7 +103,7 @@ class MilvusSys:
     def nodes(self):
         """get all the nodes in Milvus deployment"""
         self.refresh()
-        all_nodes = json.loads(self.sys_info.response).get('nodes_info')
+        all_nodes = json.loads(self.sys_info.response).get("nodes_info")
         online_nodes = [node for node in all_nodes if node["infos"]["has_error"] is False]
         return online_nodes
 
@@ -113,12 +112,12 @@ class MilvusSys:
         target_nodes = []
         if node_type is not None:
             for node in self.nodes:
-                if str(node_type).lower() == str(node.get('infos').get('type')).lower():
+                if str(node_type).lower() == str(node.get("infos").get("type")).lower():
                     target_nodes.append(node)
         return target_nodes
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     uri = ""
     token = ""
     connections.connect(uri=uri, token=token)
