888612ac79e6757668cebc85eb5b52b943e23022
Lior Friedman <lior.friedman@il.kioxia.com>
2026-08-03 09:58:12 +0000

    Fix task_analyze.go file format. Signed-off-by: Lior Friedman <lior.friedman@il.kioxia.com> Signed-off-by: Gili Buzaglo <Gili.Buzaglo@il.kioxia.com> Signed-off-by: Hiroshi Murayama <hiroshi4.murayama@kioxia.com>

--- internal/datanode/index/task_analyze.go
+++ internal/datanode/index/task_analyze.go
@@ -29,7 +29,6 @@ import (
 	"github.com/milvus-io/milvus/pkg/v3/proto/clusteringpb"
 	"github.com/milvus-io/milvus/pkg/v3/proto/datapb"
 	"github.com/milvus-io/milvus/pkg/v3/proto/indexcgopb"
-
 	"github.com/milvus-io/milvus/pkg/v3/proto/indexpb"
 	"github.com/milvus-io/milvus/pkg/v3/proto/workerpb"
 	"github.com/milvus-io/milvus/pkg/v3/util/hardware"
