06b17dd95860391d0160051adf1e4c05bea135f4
bigsheeper <yihao.dai@zilliz.com>
2026-08-06 02:30:30 +0900

    enhance: avoid hash helper allocations
    Use the one-shot murmur3 Sum32 helper for Hash32Bytes and a stack buffer for Hash32Uint64 so routing hash calls avoid per-key heap allocations.
    
    Add benchmarks for Hash32Bytes, Hash32Uint64, and Hash32Int64.
    
    Signed-off-by: bigsheeper <yihao.dai@zilliz.com>

--- pkg/util/typeutil/hash.go
+++ pkg/util/typeutil/hash.go
@@ -21,7 +21,6 @@ import (
 	"math"
 	"strconv"
 	"strings"
-	"unsafe"
 
 	"github.com/spaolacci/murmur3"
 
@@ -53,20 +52,14 @@ func (stringRoutingHasher) Hash(key string) (uint64, error) {
 
 // Hash32Bytes hashing a byte array to uint32
 func Hash32Bytes(b []byte) (uint32, error) {
-	h := murmur3.New32()
-	if _, err := h.Write(b); err != nil {
-		return 0, err
-	}
-	return h.Sum32() & 0x7fffffff, nil
+	return murmur3.Sum32(b) & 0x7fffffff, nil
 }
 
 // Hash32Uint64 hashing an uint64 nubmer to uint32
 func Hash32Uint64(v uint64) (uint32, error) {
-	// need unsafe package to get element byte size
-	/* #nosec G103 */
-	b := make([]byte, unsafe.Sizeof(v))
-	common.Endian.PutUint64(b, v)
-	return Hash32Bytes(b)
+	var b [8]byte
+	common.Endian.PutUint64(b[:], v)
+	return Hash32Bytes(b[:])
 }
 
 // Hash32Int64 hashing an int64 number to uint32
