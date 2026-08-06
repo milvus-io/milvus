252880a12c012965b0e90ef1cac34aa6b64f8048
Shawn Wang <shawn.wang@zilliz.com>
2026-08-06 03:42:42 +0000

    fix: [3.0] restore dropped load transient budget fixes
    Cherry-pick of #51403.
    
    (cherry picked from commit 731a4f9dfa11800f0d8c0c38cd7bc4852c7a8e22)
    Signed-off-by: Shawn Wang <shawn.wang@zilliz.com>

--- internal/core/src/segcore/storagev1translator/SealedIndexTranslator.h
+++ internal/core/src/segcore/storagev1translator/SealedIndexTranslator.h
@@ -61,7 +61,9 @@ class SealedIndexTranslator
 
  private:
     LoadResourceRequest
-    EstimateLoadResource() const;
+    EstimateLoadResource(
+        std::optional<milvus::storage::EntryStreamLoadInfo>* stream_load_info,
+        bool* use_shared_memory_overhead_group) const;
 
     struct IndexLoadInfo {
         bool enable_mmap;
