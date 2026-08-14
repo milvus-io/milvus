e2e28b7931f6b4099f90f7a7e2b78e4f3b790508
Buqian Zheng <zhengbuqian@gmail.com>
2026-08-14 17:00:29 +0800

    enhance: [2.6] simplify index raw data check (#52032)
    issue: https://github.com/milvus-io/milvus/issues/44452
    pr: https://github.com/milvus-io/milvus/pull/52030
    
    Signed-off-by: Buqian Zheng <zhengbuqian@gmail.com>

--- internal/core/src/segcore/SegmentLoadInfo.cpp
+++ internal/core/src/segcore/SegmentLoadInfo.cpp
@@ -128,21 +128,6 @@ SegmentLoadInfo::ConvertFieldIndexInfoToLoadIndexInfo(
     return load_index_info;
 }
 
-bool
-SegmentLoadInfo::CheckIndexHasRawData(const LoadIndexInfo& load_index_info) {
-    auto request = milvus::index::IndexFactory::GetInstance().IndexLoadResource(
-        load_index_info.field_type,
-        load_index_info.element_type,
-        load_index_info.index_engine_version,
-        load_index_info.index_size,
-        load_index_info.index_params,
-        load_index_info.enable_mmap,
-        load_index_info.num_rows,
-        load_index_info.dim);
-
-    return request.has_raw_data;
-}
-
 void
 SegmentLoadInfo::ComputeDiffIndexes(LoadDiff& diff, SegmentLoadInfo& new_info) {
     // Get current index IDs from converted cache
