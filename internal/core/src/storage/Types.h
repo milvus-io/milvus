3ac4a7e1778442083797ece7f8a13499431fd963
Shawn Wang <shawn.wang@zilliz.com>
2026-08-21 04:28:48 +0000

    fix: initialize IndexMeta members
    Signed-off-by: Shawn Wang <shawn.wang@zilliz.com>

--- internal/core/src/storage/Types.h
+++ internal/core/src/storage/Types.h
@@ -92,15 +92,15 @@ enum CodecType {
 
 // index meta information corresponding to index file data
 struct IndexMeta {
-    int64_t segment_id;
-    int64_t field_id;
-    int64_t build_id;
-    int64_t index_version;
+    int64_t segment_id{0};
+    int64_t field_id{0};
+    int64_t build_id{0};
+    int64_t index_version{0};
     std::string key;
     std::string field_name;
-    DataType field_type;
-    int64_t dim;
-    bool index_non_encoding;
+    DataType field_type{DataType::NONE};
+    int64_t dim{0};
+    bool index_non_encoding{false};
     // Path format version used by the worker/loader to assemble the object-storage prefix.
     // See milvus.proto.index.IndexStorePathVersion.
     milvus::proto::index::IndexStorePathVersion index_store_path_version{
