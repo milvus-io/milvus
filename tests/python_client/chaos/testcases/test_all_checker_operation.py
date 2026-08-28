6e78f4448af3aed2d8e04ecace8c3f1669edd335
zhuwenxing <wenxing.zhu@zilliz.com>
2026-08-28 15:37:28 +0800

    test: [3.0] stabilize chaos checker lifecycle and snapshot restore validation
    Backport the chaos checker lifecycle and snapshot restore stabilization from master.
    
    pr: #52901
    
    (cherry picked from commit 4461a40a2d91a474a42bae73db545ac891cf0adf)
    Signed-off-by: zhuwenxing <wenxing.zhu@zilliz.com>

--- tests/python_client/chaos/testcases/test_all_checker_operation.py
+++ tests/python_client/chaos/testcases/test_all_checker_operation.py
@@ -1,39 +1,39 @@
 import time
+from time import sleep
 
 import pytest
-from time import sleep
-from pymilvus import connections, db
+from chaos import chaos_commons as cc
+from chaos import constants
+from chaos.chaos_commons import assert_statistic
 from chaos.checker import (
-    DatabaseCreateChecker,
-    DatabaseDropChecker,
     CollectionCreateChecker,
     CollectionDropChecker,
-    PartitionCreateChecker,
-    PartitionDropChecker,
     CollectionLoadChecker,
     CollectionReleaseChecker,
-    PartitionLoadChecker,
-    PartitionReleaseChecker,
+    DatabaseCreateChecker,
+    DatabaseDropChecker,
+    DeleteChecker,
+    EventRecords,
+    FlushChecker,
     IndexCreateChecker,
     IndexDropChecker,
     InsertChecker,
-    UpsertChecker,
-    DeleteChecker,
-    FlushChecker,
-    SearchChecker,
-    QueryChecker,
     Op,
-    EventRecords,
-    ResultAnalyzer
+    PartitionCreateChecker,
+    PartitionDropChecker,
+    PartitionLoadChecker,
+    PartitionReleaseChecker,
+    QueryChecker,
+    ResultAnalyzer,
+    SearchChecker,
+    UpsertChecker,
 )
-from utils.util_log import test_log as log
-from utils.util_k8s import wait_pods_ready, get_milvus_instance_name
-from chaos import chaos_commons as cc
 from common.common_type import CaseLabel
 from common.milvus_sys import MilvusSys
-from chaos.chaos_commons import assert_statistic
-from chaos import constants
 from delayed_assert import assert_expectations
+from pymilvus import connections, db
+from utils.util_k8s import get_milvus_instance_name, wait_pods_ready
+from utils.util_log import test_log as log
 
 
 class TestBase:
@@ -43,14 +43,13 @@ class TestBase:
     expect_index = constants.SUCC
     expect_search = constants.SUCC
     expect_query = constants.SUCC
-    host = '127.0.0.1'
+    host = "127.0.0.1"
     port = 19530
     _chaos_config = None
     health_checkers = {}
 
 
 class TestOperations(TestBase):
-
     @pytest.fixture(scope="function", autouse=True)
     def connection(self, host, port, user, password, uri, token, milvus_ns, database_name):
         # Prioritize uri and token for connection
@@ -65,9 +64,9 @@ class TestOperations(TestBase):
             actual_token = f"{user}:{password}" if user and password else None
 
         if actual_token:
-            connections.connect('default', uri=actual_uri, token=actual_token)
+            connections.connect("default", uri=actual_uri, token=actual_token)
         else:
-            connections.connect('default', uri=actual_uri)
+            connections.connect("default", uri=actual_uri)
 
         if connections.has_connection("default") is False:
             raise Exception("no connections")
@@ -82,7 +81,7 @@ class TestOperations(TestBase):
         self.password = password
         self.uri = actual_uri
         self.token = actual_token
-        self.milvus_sys = MilvusSys(alias='default')
+        self.milvus_sys = MilvusSys(alias="default")
         self.milvus_ns = milvus_ns
         self.release_name = get_milvus_instance_name(self.milvus_ns, milvus_sys=self.milvus_sys)
 
@@ -107,7 +106,7 @@ class TestOperations(TestBase):
             Op.search: SearchChecker(collection_name=c_name),
             Op.query: QueryChecker(collection_name=c_name),
             Op.delete: DeleteChecker(collection_name=c_name),
-            Op.drop: CollectionDropChecker(collection_name=c_name)
+            Op.drop: CollectionDropChecker(collection_name=c_name),
         }
         self.health_checkers = checkers
 
@@ -115,35 +114,35 @@ class TestOperations(TestBase):
     def test_operations(self, request_duration, is_check):
         # start the monitor threads to check the milvus ops
         log.info("*********************Test Start**********************")
-        log.info(connections.get_connection_addr('default'))
+        log.info(connections.get_connection_addr("default"))
         event_records = EventRecords()
         c_name = None
         event_records.insert("init_health_checkers", "start")
         self.init_health_checkers(collection_name=c_name)
         event_records.insert("init_health_checkers", "finished")
         tasks = cc.start_monitor_threads(self.health_checkers)
-        log.info("*********************Load Start**********************")
-        # wait request_duration
-        request_duration = request_duration.replace("h", "*3600+").replace("m", "*60+").replace("s", "")
-        if request_duration[-1] == "+":
-            request_duration = request_duration[:-1]
-        request_duration = eval(request_duration)
-        for i in range(10):
-            sleep(request_duration // 10)
-            # add an event so that the chaos can start to apply
-            if i == 3:
-                event_records.insert("init_chaos", "ready")
-            for k, v in self.health_checkers.items():
-                v.check_result()
-        if is_check:
-            assert_statistic(self.health_checkers, succ_rate_threshold=0.98)
-            assert_expectations()
-        # wait all pod ready
-        wait_pods_ready(self.milvus_ns, f"app.kubernetes.io/instance={self.release_name}")
-        time.sleep(60)
-        cc.check_thread_status(tasks)
-        for k, v in self.health_checkers.items():
-            v.pause()
+        try:
+            log.info("*********************Load Start**********************")
+            # wait request_duration
+            request_duration = request_duration.replace("h", "*3600+").replace("m", "*60+").replace("s", "")
+            if request_duration[-1] == "+":
+                request_duration = request_duration[:-1]
+            request_duration = eval(request_duration)
+            for i in range(10):
+                sleep(request_duration // 10)
+                # add an event so that the chaos can start to apply
+                if i == 3:
+                    event_records.insert("init_chaos", "ready")
+                for checker in self.health_checkers.values():
+                    checker.check_result()
+            if is_check:
+                assert_statistic(self.health_checkers, succ_rate_threshold=0.98)
+                assert_expectations()
+            # wait all pod ready
+            wait_pods_ready(self.milvus_ns, f"app.kubernetes.io/instance={self.release_name}")
+            time.sleep(60)
+        finally:
+            cc.stop_monitor_threads(self.health_checkers, tasks)
         ra = ResultAnalyzer()
         ra.get_stage_success_rate()
         ra.show_result_table()
