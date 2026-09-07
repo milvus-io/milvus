183733ba7caa46bdfb203097945b08673335a204
yangxuan <xuan.yang@zilliz.com>
2026-09-07 14:31:01 +0800

    test: revert unrelated integration test fixes
    Signed-off-by: yangxuan <xuan.yang@zilliz.com>

--- tests/integration/expression/expression_test.go
+++ tests/integration/expression/expression_test.go
@@ -229,7 +229,7 @@ func (s *ExpressionSuite) searchWithExpression() {
 	}
 }
 
-func (s *ExpressionSuite) testDivisionByZeroError() {
+func (s *ExpressionSuite) TestDivisionByZeroError() {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
@@ -297,7 +297,7 @@ func (s *ExpressionSuite) TestExpression() {
 	s.setParams()
 	s.setupData()
 	s.searchWithExpression()
-	s.testDivisionByZeroError()
+	s.TestDivisionByZeroError()
 }
 
 func TestExpression(t *testing.T) {
